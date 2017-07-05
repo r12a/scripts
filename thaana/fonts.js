577c7c132c392abd0ddf0f36a24d82c6e5e0159e
r12a <ishida@w3.org>
2017-07-05 12:08:30 +0100

    thaana/block, new

---
+++ thaana/fonts.js
@@ -0,0 +1,14 @@
+ var fontSelection = '<optgroup label="Webfont">\
+      <option value="Noto Sans Thaana WF">Noto Sans Thaana WF</option>\
+      </optgroup>\
+      <optgroup label="Other fonts">\
+                <option value="Mv Jadheedh">Mv Jadheedh</option>\
+                <option value="A_Faseyha">A_Faseyha</option>\
+                <option value="A_Utheem">A_Utheem</option>\
+                <option value="A_Waheed">A_Waheed</option>\
+                <option value="A_Ilham">A_Ilham</option>\
+                <option value="Arial Unicode MS">Arial Unicode MS</option>\
+                <option value="Code2000">Code2000</option>\
+                <option value="sans-serif">sans-serif</option>\
+                <option value="serif">serif</option>\
+      </optgroup>'
