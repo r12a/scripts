52b3721182ea8cef02d328b00f1e48b9674f5140
r12a <ishida@w3.org>
2017-07-19 21:00:06 +0100

    summaries, common files for script notes

--- summaries/functions.js
+++ summaries/functions.js
@@ -1,3 +1,57 @@
+var featureName = { 
+	type:"Script type", 
+	chars:"Number of characters", 
+	cchars:"Combining characters", 
+	mcchars:"Multiple combining characters", 
+	gpos:"Context-based positioning", 
+	cs:"Case distinction", 
+	cursive:"Cursive script", 
+	gsub:"Contextual shaping", 
+	dir:"Text direction", 
+	baseline:"Baseline", 
+	wordsep:"Word separator", 
+	wrap:"Wraps at", 
+	justify:"Justification", 
+	digits:"Native digits?", 
+	region:"Region" 
+	}
+
+
+
+function makeSidePanel (script, otherlinks) {
+	console.log(script, otherlinks)
+	var ptr = ''
+	for (var i=0;i<scriptData.length;i++) if  (scriptData[i].script === script) {
+		ptr = scriptData[i]
+		break
+		}
+
+	var out = ''
+	out += '<tr><th>Script name</th><td><p>'+script+'</td></tr>'
+	for (var feature in featureName) {
+		out += '<tr><th>'+featureName[feature]+'</th><td>'
+		if (document.getElementById(feature)) out +='<a href="#'+feature+'">'+ptr[feature]+'</a>'
+		else out += ptr[feature]
+		out += '</td></tr>'
+		}
+	
+	// process other links, eg. Punctuation:punctuation,Tones:tones
+	var links = otherlinks.split(',')
+	for (i=0;i<links.length;i++) {
+		var parts = links[i].split(':')
+		out += '<tr><th>'
+		if (i===0) out += 'Other information'
+		out += '</th><td>'
+		if (document.getElementById(parts[1])) out +='<a href="#'+parts[1]+'">'+parts[0]+'</a>'
+		else out += parts[0]
+		out += '</td></tr>'
+		}
+	return out
+	}
+	
+
+
+
 function clearExamples () {
 	examples = document.getElementById('freeText').getElementsByTagName('span');
 	for (var i=0; i<examples.length; i++) {
@@ -15,38 +69,32 @@ function highlightExample (text) {
 
 isJustified = false;
 
-function toggleJustification () {
-	var sample = document.getElementById( 'freeText' );
-	var justifyButton = document.getElementById( 'justifyButton' ).childNodes[0];
-	if (isJustified) { 
-		sample.style.textAlign = '';
-		isJustified = false;
-		justifyButton.replaceData( 0, 100, ' Justify ' );
+function toggleJustification (sample, justifyButton) { 
+	if (sample.style.textAlign === 'justify') { 
+		sample.style.textAlign = 'start'
+		justifyButton.textContent = ' Justify '
 		}
 	else {
-		sample.style.textAlign = 'justify';
-		isJustified = true;
-		justifyButton.replaceData( 0, 100, ' Remove justification ' );
-		if (document.getElementById( 'kashidaValue' ) && document.getElementById( 'kashidaValue' ).value != 0) { 
-			sample.style.textKashida = document.getElementById( 'kashidaValue' ).value; 
-			}
+		sample.style.textAlign = 'justify'
+		justifyButton.textContent = ' Remove justification '
+		}
+	}
+
+
+function toggleDirection (sample, toggleButton) { 
+	if (sample.className === 'set_horizontal') { 
+		sample.className = 'set_vertical_rl'
+		justifyButton.textContent = ' Set horizontally '
+		}
+	else {
+		sample.className = 'set_horizontal'
+		justifyButton.textContent = ' Set vertically '
 		}
 	}
 
 
 var currDirection = 'horizontal';
 
-function toggleDirection () {
-	var uText = document.getElementById( 'freeText' );
-	if (currDirection == 'horizontal') { 
-		uText.className = 'tb-rl';
-		currDirection = 'vertical';
-		}
-	else {
-		uText.className = 'lr-tb';
-		currDirection = 'horizontal';
-		}
-	}
 
 
 function showFeatureInfo (examples, featureInfoId) {
@@ -72,4 +120,69 @@ function showAllFeatureInfo () {
 	for (var i=0;i<notes.length;i++) {
 		notes[i].style.display = 'block'
 		}
+	}
+
+
+
+function initialiseShowNames (base, target) {
+// add function to all images with class ex
+// function will display character by character names for example in the panel
+// base (string), path for link to character detail
+
+	// check whether the calling page has set a base and target window
+	if(typeof base === 'undefined') { base = ''; }
+	if(typeof target === 'undefined') { target = ''; } 
+	
+	var examples = document.querySelectorAll('.ex')
+	for (e=0;e<examples.length;e++) {
+		if (examples[e].nodeName.toLowerCase() == 'img') {
+			shownames_setImgOnclick(examples[e], base, target)
+			}
+		else { shownames_setOnclick(examples[e], base, target) }
+		}
+	}
+
+
+function shownames_setImgOnclick ( node, base, target ) {
+	node.onclick = function(){ showNameDetails(node.alt, node.lang, base, target, document.getElementById('panel') ) }
+	}
+
+function shownames_setOnclick ( node, base, target ) {
+	node.onclick = function(){ showNameDetails(node.firstChild.data, getLanguage(node), base, target, document.getElementById('panel')) }
+	}
+
+
+function getLanguage(node) {
+	// given a node, returns lang value of node or nearest parent
+	if(node.lang) return node.lang
+	else if(node.parentNode) return getLanguage(node.parentNode)
+	else return ''
+	}
+
+
+function showtext () {
+	// when text is highlighted in the freeText area, display the character list
+	text=getSelected()
+	if (text.focusOffset > text.anchorOffset) {
+		var start = text.anchorOffset
+		var end = text.focusOffset
+		}
+	else {
+		var start = text.focusOffset
+		var end = text.anchorOffset
+		}
+	var highlight = text.focusNode.nodeValue.substr(start, end-start)
+	var source = document.getElementById('freeText')
+	showNameDetails(highlight, source.lang, source.dataset.base, source.dataset.target, document.getElementById('panel') )
+	}
+
+function getSelected() {
+	if (window.getSelection) return window.getSelection()
+	else if (document.getSelection) return document.getSelection()
+	else {
+		var selection = document.selection && document.selection.createRange()
+		if (selection.text) return selection.text
+		return false
+		}
+	return false
 	}
---
+++ summaries/scriptdata.js
@@ -0,0 +1,66 @@
+
+var scriptData = [ 
+
+{ script:"Arabic", type:"abjad", chars:390, cchars:95, cs:"no", gpos:"yes", mcchars:"yes", gsub:"yes", cursive:"yes", dir:"rtl", wordsep:"space", baseline:"mid", wrap:"word", justify:"word/space", digits:"yes", region:"M East", linked:"arabic" },
+
+{ script:"Armenian", type:"alpha", chars:89, cchars:0, cs:"yes", gpos:"no", mcchars:"no", gsub:"no", cursive:"no", dir:"ltr", wordsep:"space", baseline:"mid", wrap:"word", justify:"space", digits:"no", region:"M East" },
+
+{ script:"Bengali", type:"abugida", chars:95, cchars:19, cs:"no", gpos:"yes", mcchars:"yes", gsub:"yes", cursive:"no", dir:"ltr", wordsep:"space", baseline:"high", wrap:"word", justify:"space", digits:"yes", region:"Asia S" },
+
+{ script:"Cherokee", type:"syllabic", chars:172, cchars:0, cs:"yes", gpos:"no", mcchars:"no", gsub:"no", cursive:"no", dir:"ltr", wordsep:"space", baseline:"mid", wrap:"word", justify:"space", digits:"no", region:"America", linked:"cherokee" },
+
+{ script:"Cyrillic", type:"alpha", chars:441, cchars:55, cs:"yes", gpos:"no", mcchars:"no", gsub:"no", cursive:"no", dir:"ltr", wordsep:"space", baseline:"mid", wrap:"word", justify:"space", digits:"no", region:"Europe", linked:"cyrillic" },
+
+{ script:"Devanagari", type:"abugida", chars:158, cchars:52, cs:"no", gpos:"yes", mcchars:"yes", gsub:"yes", cursive:"no", dir:"ltr", wordsep:"space", baseline:"high", wrap:"word", justify:"space", digits:"yes", region:"Asia S", linked:"devanagari" },
+
+{ script:"Ethiopic", type:"syllabic", chars:495, cchars:3, cs:"no", gpos:"no", mcchars:"?", gsub:"no", cursive:"no", dir:"ltr", wordsep:"፡", baseline:"mid", wrap:"word", justify:"፡", digits:"yes", region:"Africa" },
+
+{ script:"Greek", type:"alpha", chars:368, cchars:0, cs:"yes", gpos:"no", mcchars:"no", gsub:"no", cursive:"no", dir:"ltr", wordsep:"space", baseline:"mid", wrap:"word", justify:"space", digits:"no", region:"Europe", linked:"greek" },
+
+{ script:"Gujarati", type:"abugida", chars:91, cchars:26, cs:"no", gpos:"yes", mcchars:"yes", gsub:"yes", cursive:"no", dir:"ltr", wordsep:"space", baseline:"high?", wrap:"word", justify:"space", digits:"yes", region:"Asia S" },
+
+{ script:"Gurmukhi", type:"abugida", chars:79, cchars:18, cs:"no", gpos:"yes", mcchars:"yes", gsub:"yes", cursive:"no", dir:"ltr", wordsep:"space", baseline:"high", wrap:"word", justify:"space", digits:"yes", region:"Asia S" },
+
+{ script:"Han", type:"ideogr", chars:87915, cchars:0, cs:"no", gpos:"no", mcchars:"no", gsub:"no", cursive:"no", dir:"ltr/tbrl", wordsep:"no", baseline:"low", wrap:"char", justify:"char", digits:"yes", region:"Asia E", linked:"han" },
+
+{ script:"Hangul", type:"ideogr", chars:11183, cchars:0, cs:"no", gpos:"no", mcchars:"no", gsub:"no", cursive:"no", dir:"ltr/tbrl", wordsep:"space", baseline:"low", wrap:"char", justify:"space", digits:"no", region:"Asia E", linked:"hangul" },
+
+{ script:"Hebrew", type:"abjad", chars:87, cchars:51, cs:"no", gpos:"yes", mcchars:"yes", gsub:"no", cursive:"no", dir:"rtl", wordsep:"space", baseline:"mid", wrap:"word", justify:"space", digits:"no", region:"M East", linked:"hebrew" },
+
+{ script:"Kana", type:"ideogr", chars:492, cchars:0, cs:"no", gpos:"no", mcchars:"no", gsub:"no", cursive:"no", dir:"ltr/tbrl", wordsep:"no", baseline:"low", wrap:"char", justify:"char", digits:"no", region:"Asia E", linked:"kana" },
+
+{ script:"Kannada", type:"abugida", chars:88, cchars:22, cs:"no", gpos:"yes", mcchars:"yes", gsub:"yes", cursive:"no", dir:"ltr", wordsep:"space", baseline:"mid", wrap:"word", justify:"space", digits:"yes", region:"Asia S" },
+
+{ script:"Khmer", type:"abugida", chars:146, cchars:33, cs:"no", gpos:"yes", mcchars:"yes", gsub:"yes", cursive:"no", dir:"ltr", wordsep:"no", baseline:"mid", wrap:"word", justify:"cluster", digits:"yes", region:"Asia SE" },
+
+{ script:"Lao", type:"alpha", chars:67, cchars:15, cs:"no", gpos:"yes", mcchars:"yes", gsub:"yes", cursive:"no", dir:"ltr", wordsep:"no", baseline:"mid", wrap:"word", justify:"cluster", digits:"yes", region:"Asia SE" },
+
+{ script:"Latin", type:"alpha", chars:1286, cchars:0, cs:"yes", gpos:"no", mcchars:"no", gsub:"no", cursive:"no", dir:"ltr", wordsep:"space", baseline:"mid", wrap:"word", justify:"space", digits:"yes", region:"Eur/Am" },
+
+{ script:"Malayalam", type:"abugida", chars:117, cchars:23, cs:"no", gpos:"yes", mcchars:"yes", gsub:"yes", cursive:"no", dir:"ltr", wordsep:"space", baseline:"mid", wrap:"word", justify:"space", digits:"yes", region:"Asia S" },
+
+{ script:"Mongolian", type:"alpha", chars:156, cchars:6, cs:"no", gpos:"yes", mcchars:"no?", gsub:"yes", cursive:"yes", dir:"tblr", wordsep:"space", baseline:"vertical", wrap:"?", justify:"?", digits:"yes", region:"Asia C" },
+
+{ script:"Myanmar", type:"abugida", chars:223, cchars:62, cs:"no", gpos:"yes", mcchars:"yes", gsub:"yes", cursive:"no", dir:"ltr", wordsep:"no", baseline:"mid", wrap:"word", justify:"cluster", digits:"yes", region:"Asia SE" },
+
+{ script:"N'Ko", type:"alpha", chars:59, cchars:9, cs:"no", gpos:"yes", mcchars:"yes", gsub:"yes", cursive:"yes", dir:"rtl", wordsep:"yes", baseline:"mid", wrap:"word", justify:"wd/str", digits:"yes", region:"Africa", linked:"nko" },
+
+{ script:"Oriya", type:"abugida", chars:90, cchars:20, cs:"no", gpos:"yes", mcchars:"yes", gsub:"yes", cursive:"no", dir:"ltr", wordsep:"space", baseline:"mid", wrap:"word", justify:"space", digits:"yes", region:"Asia S" },
+
+{ script:"Tamil", type:"abugida", chars:72, cchars:14, cs:"no", gpos:"yes", mcchars:"yes", gsub:"yes", cursive:"no", dir:"ltr", wordsep:"space", baseline:"mid", wrap:"word", justify:"space", digits:"yes", region:"Asia S" },
+
+{ script:"Telugu", type:"abugida", chars:96, cchars:22, cs:"no", gpos:"yes", mcchars:"yes", gsub:"yes", cursive:"no", dir:"ltr", wordsep:"space", baseline:"mid", wrap:"word", justify:"space", digits:"yes", region:"Asia S" },
+
+{ script:"Thaana", type:"alpha", chars:50, cchars:11, cs:"no", gpos:"no", mcchars:"no", gsub:"no", cursive:"no", dir:"rtl", wordsep:"space", baseline:"mid", wrap:"word", justify:"space", digits:"no", region:"Asia S", linked:"thaana" },
+
+{ script:"Thai", type:"abugida", chars:87, cchars:16, cs:"no", gpos:"yes", mcchars:"yes", gsub:"yes", cursive:"no", dir:"ltr", wordsep:"no", baseline:"mid", wrap:"word", justify:"cluster", digits:"yes", region:"Asia SE", linked:"thai" },
+
+{ script:"Tibetan", type:"abugida", chars:211, cchars:77, cs:"no", gpos:"yes", mcchars:"yes", gsub:"yes", cursive:"no", dir:"ltr", wordsep:"no", baseline:"high", wrap:"syllable", justify:"special", digits:"yes", region:"Asia C", linked:"tibetan" },
+	   
+{ script:"Tifinagh", type:"alpha", chars:32, cchars:0, cs:"no", gpos:"no", mcchars:"no", gsub:"no", cursive:"no", dir:"ltr+", wordsep:"space", baseline:"mid", wrap:"word", justify:"space", digits:"yes", region:"Africa" },
+	   
+{ script:"UCAS", type:"?", chars:710, cchars:0, cs:"no", gpos:"no", mcchars:"no", gsub:"no", cursive:"no", dir:"ltr", wordsep:"space", baseline:"mid", wrap:"word", justify:"space", digits:"no", region:"America" },
+	   
+]
+
+
