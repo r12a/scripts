ae509184a7bc951fabc65ced573a796e86eb36dc
r12a <ishida@w3.org>
2018-07-12 08:56:38 +0100

    armenian block: Upgrade unicodenotes handling; point to initialise()

---
+++ armenian/desc.js
@@ -0,0 +1,16 @@
+var desc = []
+
+desc[1369]="→ (modifier letter reversed comma - 02BD)¶→ (modifier letter left half ring - 02BF)¶→ (combining reversed comma above - 0314)"
+desc[1370]="= armenian modifier letter right half ring (1.0)¶→ (modifier letter apostrophe - 02BC)¶→ (modifier letter right half ring - 02BE)¶→ (combining comma above - 0313)"
+desc[1371]="= shesht¶→ (modifier letter acute accent - 02CA)"
+desc[1372]="= batsaganchakan nshan¶• marks intonation on word, rather than occurring finally"
+desc[1373]="= bowt¶→ (modifier letter grave accent - 02CB)"
+desc[1374]="= hartsakan nshan¶• marks intonation on word, rather than occurring finally"
+desc[1375]="= patiw"
+desc[1376]="• used for phonetic notation"
+desc[1415]="≈ 0565 0582"
+desc[1416]="• used for phonetic notation"
+desc[1417]="= vertsaket¶• may also be used for historic Georgian texts¶→ (colon - 003A)"
+desc[1418]="= yentamna"
+desc[1422]="• maps to AST 34.005:1997"
+
