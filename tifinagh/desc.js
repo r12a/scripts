76701d53925422b441b53197f9aa5b1e69b9a6ae
r12a <ishida@w3.org>
2018-07-13 10:18:49 +0100

    tifinagh block: Upgrade unicodenotes handling; point to initialise()

---
+++ tifinagh/desc.js
@@ -0,0 +1,9 @@
+var desc = []   // unicode v11
+
+desc[11584]="= Tuareg yab"
+desc[11603]="= Tuareg yaw"
+desc[11608]="= Adrar yaj"
+desc[11620]="= harpoon yaz"
+desc[11631]="= tamatart¶≈ [super] 2D61"
+desc[11632]="= tazarast"
+desc[11647]="• suppresses an inherent vowel¶• functions to indicate that previous and following character are part of a bi-consonant cluster¶• shape shown is arbitrary and is not visibly rendered"
