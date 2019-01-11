ee872ec57fa6d72f9912d494efafdde0dc96533f
r12a <ishida@w3.org>
2019-01-11 09:32:41 +0000

    scriptdata.js, + New tai lue

--- featurelist/scriptdata.js
+++ featurelist/scriptdata.js
@@ -51,6 +51,8 @@ var scriptData = [
 
 { script:"Myanmar", type:"abugida", chars:223, cchars:62, cs:"no", gpos:"yes", mcchars:"yes", matras: "yes", gsub:"yes", cursive:"no", dir:"ltr", wordsep:"no", baseline:"mid", wrap:"word", hyphenation:"no", justify:"cluster", digits:"yes", region:"Asia SE", linked:"myanmar" },
 
+{ script:"New Tai Lü", type:"abugida", chars:83, cchars:0, cs:"no", gpos:"no", mcchars:"no", matras: "yes", gsub:"no", cursive:"no", dir:"ltr", wordsep:"no", baseline:"mid", wrap:"?", hyphenation:"?", justify:"?", digits:"yes", region:"Asia SE", linked:"newtailue" },
+
 { script:"N'Ko", type:"alpha", chars:59, cchars:9, cs:"no", gpos:"yes", mcchars:"yes", matras: "no", gsub:"yes", cursive:"yes", dir:"rtl", wordsep:"space", baseline:"mid", wrap:"word", hyphenation:"?", justify:"wd/str", digits:"yes", region:"Africa", linked:"nko" },
 
 { script:"Oriya", type:"abugida", chars:90, cchars:20, cs:"no", gpos:"yes", mcchars:"yes", matras: "yes", gsub:"yes", cursive:"no", dir:"ltr", wordsep:"space", baseline:"mid", wrap:"word", hyphenation:"?", justify:"space", digits:"yes", region:"Asia S" },
