9470d7b19ef8867c556a3143fe3f35c9c07766f9
r12a <ishida@w3.org>
2019-01-27 07:02:41 +0000

    taml, Support db.js and new structural model

---
+++ newtailue/db.js
@@ -0,0 +1,162 @@
+var spreadsheet = `
+ᦂ	k	k	k	high tone consonant				ᦅ	
+ᦅ	k	ḵ	k	low tone consonant					ᦂ
+ᦦ	k	kʷ	kʷ	high tone consonant				ᦨ	
+ᦨ	k	ḵʷ	kʷ	low tone consonant					ᦦ
+ᦄ	n	ŋ	ŋa	high tone consonant				ᦇ	
+ᦇ	n	ŋ̱	ŋa	low tone consonant					ᦄ
+									
+ᦈ	t	ʦ	ʦ ʨ	high tone consonant				ᦋ	
+ᦋ	t	ʦ̱	ʦ ʨ	low tone consonant					ᦈ
+									
+ᦎ	t	t	t	high tone consonant				ᦑ	
+ᦑ	t	ṯ	t	low tone consonant					ᦎ
+ᦏ	t	tʰ	tʰ	high tone consonant				ᦒ	
+ᦒ	t	ṯʰ	tʰ	low tone consonant					ᦏ
+ᦡ	d	d	d	high tone consonant				ᦤ	
+ᦤ	d	ḏ	d	low tone consonant					ᦡ
+ᦐ	n	n	n	high tone consonant				ᦓ	
+ᦓ	n	ṉ	n	low tone consonant					ᦐ
+									
+ᦔ	p	p	p	high tone consonant				ᦗ	
+ᦗ	p	p̱	p	low tone consonant					ᦔ
+ᦕ	p	pʰ	pʰ	high tone consonant				ᦘ	
+ᦘ	p	p̱ʰ	pʰ	low tone consonant					ᦕ
+ᦢ	b	b	b	high tone consonant				ᦥ	
+ᦥ	b	ḇ	b	low tone consonant					ᦢ
+ᦖ	m	m	m	high tone consonant				ᦙ	
+ᦙ	m	m̱	m	low tone consonant					ᦖ
+									
+ᦜ	l	l	l	high tone consonant				ᦟ	
+ᦟ	l	ḻ	l	low tone consonant					ᦜ
+ᦊ	y	y	j	high tone consonant				ᦍ	
+ᦍ	y	y̱	j	low tone consonant					ᦊ
+ᦛ	w	w	w	high tone consonant				ᦞ	
+ᦞ	w	w̱	w	low tone consonant					ᦛ
+									
+ᦠ	h	h	h	high tone consonant				ᦣ	
+ᦣ	h	ẖ	h	low tone consonant					ᦠ
+ᦃ	x	x	x	high tone consonant				ᦆ	
+ᦆ	x	x̱	x	low tone consonant					ᦃ
+ᦧ	x	xʷ	xʷ	high tone consonant				ᦩ	
+ᦩ	x	x̱ʷ	xʷ	low tone consonant					ᦧ
+ᦉ	s	s	s ɕ	high tone consonant				ᦌ	
+ᦌ	s	s̱	s ɕ	low tone consonant					ᦉ
+ᦪ	s	sʷ	sʷ	high tone consonant				ᦫ	
+ᦫ	s	s̱ʷ	sʷ	low tone consonant					ᦪ
+ᦚ	f	f	f	high tone consonant				ᦝ	
+ᦝ	f	f̱	f	low tone consonant					ᦚ
+									
+ᦀ	a	ạ	ʔ	high tone consonant				ᦁ	
+ᦁ	a	ạ̱	ʔ	low tone consonant					ᦀ
+									
+									
+									
+ᧅ	K	k̽	k	final consonant					
+ᧂ	N	ŋ̽	ŋ	final consonant					
+ᧆ	T	t̽	t	final consonant					
+ᧃ	N	n̽	n	final consonant					
+ᧇ	P	p̽	p	final consonant					
+ᧄ	M	m̽	m	final consonant					
+ᧁ	W	w̽	u̯	final consonant					
+									
+									
+									
+᧞	L	lˀ	lɛʔ	ligature					
+᧟	L	lʷ	lɛu	ligature					
+									
+									
+									
+ᦺ	a	a͕ʲ	ai̯	dependent vowel					
+ᦵ	e	e͕	eː	dependent vowel					
+ᦶ	e	ɛ͕	ɛ	dependent vowel					
+ᦷ	o	o͕	oː	dependent vowel					
+									
+									
+ᦱ	a	ā	aː	dependent vowel					
+ᦲ	i	i	i	dependent vowel					
+ᦹ	u	ɯ	ɯ	dependent vowel					
+ᦳ	u	u	uʔ	dependent vowel					
+ᦴ	u	ū	u	dependent vowel					
+ᦸ	o	ɔ	ɔ	dependent vowel					
+ᦰ	}	ʔ	ʔ	vowel shortener					
+									
+ᦻ	a	aʲ	aːi̯	dipthong					
+ᦿ	u	ɯʲ	ɯi̯	dipthong					
+ᦼ	u	uʲ	ui̯	dipthong					
+ᦽ	o	oʲ	oi̯	dipthong					
+ᦾ	o	ɔʲ	ɔi̯	dipthong					
+ᧀ	e	əʲ	əi̯	dipthong					
+									
+									
+									
+ᧈ	^	¹		tone mark					
+ᧉ	^	²		tone mark					
+									
+									
+									
+									
+									
+									
+									
+									
+᧐	0	0		digit		nɯŋ			
+᧑	1	1		digit		sɔ́ŋ			
+᧒	2	2		digit		sám			
+᧓	3	3		digit		si᷄ː			
+᧔	4	4		digit		ha᷅ː			
+᧕	5	5		digit		hók			
+᧖	6	6		digit		ʨét			
+᧗	7	7		digit		pɛ᷄t			
+᧘	8	8		digit		ka᷅w			
+᧙	9	9		digit		síp			
+									
+᧚	1	1̣		alternative digit					
+
+
+〈	(	<		fullwidth bracket						
+〉	)	>		fullwidth bracket						
+《	(	{		fullwidth bracket						
+》	)	}		fullwidth bracket						
+（	(	(		fullwidth bracket						
+）	)	)		fullwidth bracket						
+！	!	!		fullwidth bracket						
+？	?	?								
+：	:	:								
+；	;	;								
+。	.	.								
+、	,	,								
+，	,	,								
+．	.	.								
+										
+										
+										
+										
+										
+										
+-ᦲᦰ	i	iʔ	iʔ							
+ᦵ-ᦰ	e	eʔ	eʔ							
+ᦶ-ᦰ	e	ɛʔ	ɛʔ							
+ᦷ-ᦰ	o	oʔ	oʔ							
+-ᦸᦰ	o	ɔʔ	ɔʔ							
+ᦵ-ᦲᦰ	e	əʔ	əʔ							
+ᦵ-ᦲ	e	əː	əː							
+-ᦹᦰ	u	ɯ	ɯ							`
+
+
+var cols = {
+"transLoc": 2,
+"ipaLoc": 3,
+"typeLoc": 4,
+"nnameLoc": 5,
+"nameLoc": 6,
+"numLoc": 0,
+"statusLoc": 7,
+"dvowel": 0,
+"ivowel": 0,
+"subj":0,
+"ltone":8,
+"htone":9,
+"transcription":0,
+//"othertranscriptions": [[4, 'ISO 15919']]
+}
