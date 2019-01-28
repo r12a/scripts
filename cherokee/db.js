5370ecc69b7ab366bdb47663028db0b54953ed2b
r12a <ishida@w3.org>
2019-01-28 05:56:54 +0000

    cher, Add db.js & restructure layout

---
+++ cherokee/db.js
@@ -0,0 +1,197 @@
+var spreadsheet = `
+Ꭰ	a	a ʔa	syllable		ꭰ	
+ꭰ	a	a ʔa	syllable			Ꭰ
+Ꭱ	e	e ʔ	syllable		ꭱ	
+ꭱ	e	e ʔ	syllable			Ꭱ
+Ꭲ	i	i ʔ	syllable		ꭲ	
+ꭲ	i	i ʔ	syllable			Ꭲ
+Ꭳ	o	o ʔ	syllable		ꭳ	
+ꭳ	o	o ʔ	syllable			Ꭳ
+Ꭴ	u	u ʔu	syllable		ꭴ	
+ꭴ	u	u ʔu	syllable			Ꭴ
+Ꭵ	v	ə̃ ʔə̃	syllable		ꭵ	
+ꭵ	v	ə̃ ʔə̃	syllable			Ꭵ
+Ꭶ	ga	ka	syllable		ꭶ	
+ꭶ	ga	ka	syllable			Ꭶ
+Ꭷ	ka	kʰa	syllable		ꭷ	
+ꭷ	ka	kʰa	syllable			Ꭷ
+Ꭸ	ge	ke kʰe…	syllable		ꭸ	
+ꭸ	ge	ke kʰe…	syllable			Ꭸ
+Ꭹ	gi	ki kʰi…	syllable		ꭹ	
+ꭹ	gi	ki kʰi…	syllable			Ꭹ
+Ꭺ	go	ko kʰo…	syllable		ꭺ	
+ꭺ	go	ko kʰo…	syllable			Ꭺ
+Ꭻ	gu	ku kʰu…	syllable		ꭻ	
+ꭻ	gu	ku kʰu…	syllable			Ꭻ
+Ꭼ	gv	kə̃ kʰə̃…	syllable		ꭼ	
+ꭼ	gv	kə̃ kʰə̃…	syllable			Ꭼ
+Ꭽ	ha	ha	syllable		ꭽ	
+ꭽ	ha	ha	syllable			Ꭽ
+Ꭾ	he	he	syllable		ꭾ	
+ꭾ	he	he	syllable			Ꭾ
+Ꭿ	hi	hi	syllable		ꭿ	
+ꭿ	hi	hi	syllable			Ꭿ
+Ꮀ	ho	ho	syllable		ꮀ	
+ꮀ	ho	ho	syllable			Ꮀ
+Ꮁ	hu	hu	syllable		ꮁ	
+ꮁ	hu	hu	syllable			Ꮁ
+Ꮂ	hv	hə̃	syllable		ꮂ	
+ꮂ	hv	hə̃	syllable			Ꮂ
+Ꮃ	la	la ɬa…	syllable		ꮃ	
+ꮃ	la	la ɬa…	syllable			Ꮃ
+Ꮄ	le	le ɬe…	syllable		ꮄ	
+ꮄ	le	le ɬe…	syllable			Ꮄ
+Ꮅ	li	li ɬi…	syllable		ꮅ	
+ꮅ	li	li ɬi…	syllable			Ꮅ
+Ꮆ	lo	lo ɬo…	syllable		ꮆ	
+ꮆ	lo	lo ɬo…	syllable			Ꮆ
+Ꮇ	lu	lu ɬu…	syllable		ꮇ	
+ꮇ	lu	lu ɬu…	syllable			Ꮇ
+Ꮈ	lv	lə̃ ɬə̃…	syllable		ꮈ	
+ꮈ	lv	lə̃ ɬə̃…	syllable			Ꮈ
+Ꮉ	ma	ma	syllable		ꮉ	
+ꮉ	ma	ma	syllable			Ꮉ
+Ꮊ	me	me	syllable		ꮊ	
+ꮊ	me	me	syllable			Ꮊ
+Ꮋ	mi	mi	syllable		ꮋ	
+ꮋ	mi	mi	syllable			Ꮋ
+Ꮌ	mo	mo	syllable		ꮌ	
+ꮌ	mo	mo	syllable			Ꮌ
+Ꮍ	mu	mu	syllable		ꮍ	
+ꮍ	mu	mu	syllable			Ꮍ
+Ꮎ	na	na	syllable		ꮎ	
+ꮎ	na	na	syllable			Ꮎ
+Ꮏ	hna	hna	syllable		ꮏ	
+ꮏ	hna	hna	syllable			Ꮏ
+Ꮐ	nah	nah	syllable		ꮐ	
+ꮐ	nah	nah	syllable			Ꮐ
+Ꮑ	ne	ne hne…	syllable		ꮑ	
+ꮑ	ne	ne hne…	syllable			Ꮑ
+Ꮒ	ni	ni hni…	syllable		ꮒ	
+ꮒ	ni	ni hni…	syllable			Ꮒ
+Ꮓ	no	no hno…	syllable		ꮓ	
+ꮓ	no	no hno…	syllable			Ꮓ
+Ꮔ	nu	nu hnu…	syllable		ꮔ	
+ꮔ	nu	nu hnu…	syllable			Ꮔ
+Ꮕ	nv	nə̃ hnə̃…	syllable		ꮕ	
+ꮕ	nv	nə̃ hnə̃…	syllable			Ꮕ
+Ꮖ	qua	kʷa kʷʰa…	syllable		ꮖ	
+ꮖ	qua	kʷa kʷʰa…	syllable			Ꮖ
+Ꮗ	que	kʷe kʷʰe…	syllable		ꮗ	
+ꮗ	que	kʷe kʷʰe…	syllable			Ꮗ
+Ꮘ	qui	kʷi kʷʰi…	syllable		ꮘ	
+ꮘ	qui	kʷi kʷʰi…	syllable			Ꮘ
+Ꮙ	quo	kʷo kʷʰo…	syllable		ꮙ	
+ꮙ	quo	kʷo kʷʰo…	syllable			Ꮙ
+Ꮚ	quu	kʷu kʷʰu…	syllable		ꮚ	
+ꮚ	quu	kʷu kʷʰu…	syllable			Ꮚ
+Ꮛ	quv	kʷə̃ kʷʰə̃…	syllable		ꮛ	
+ꮛ	quv	kʷə̃ kʷʰə̃…	syllable			Ꮛ
+Ꮜ	sa	sa	syllable		ꮜ	
+ꮜ	sa	sa	syllable			Ꮜ
+Ꮝ	s	s	syllable		ꮝ	
+ꮝ	s	s	syllable			Ꮝ
+Ꮞ	se	se	syllable		ꮞ	
+ꮞ	se	se	syllable			Ꮞ
+Ꮟ	si	si	syllable		ꮟ	
+ꮟ	si	si	syllable			Ꮟ
+Ꮠ	so	so	syllable		ꮠ	
+ꮠ	so	so	syllable			Ꮠ
+Ꮡ	su	su	syllable		ꮡ	
+ꮡ	su	su	syllable			Ꮡ
+Ꮢ	sv	sə̃	syllable		ꮢ	
+ꮢ	sv	sə̃	syllable			Ꮢ
+Ꮣ	da	ta	syllable		ꮣ	
+ꮣ	da	ta	syllable			Ꮣ
+Ꮤ	ta	tʰa	syllable		ꮤ	
+ꮤ	ta	tʰa	syllable			Ꮤ
+Ꮥ	de	te	syllable		ꮥ	
+ꮥ	de	te	syllable			Ꮥ
+Ꮦ	te	tʰe	syllable		ꮦ	
+ꮦ	te	tʰe	syllable			Ꮦ
+Ꮧ	di	ti	syllable		ꮧ	
+ꮧ	di	ti	syllable			Ꮧ
+Ꮨ	ti	tʰi	syllable		ꮨ	
+ꮨ	ti	tʰi	syllable			Ꮨ
+Ꮩ	do	to tʰo…	syllable		ꮩ	
+ꮩ	do	to tʰo…	syllable			Ꮩ
+Ꮪ	du	tu tʰu…	syllable		ꮪ	
+ꮪ	du	tu tʰu…	syllable			Ꮪ
+Ꮫ	dv	tə̃ tʰə̃…	syllable		ꮫ	
+ꮫ	dv	tə̃ tʰə̃…	syllable			Ꮫ
+Ꮬ	dla	tˡa	syllable		ꮬ	
+ꮬ	dla	tˡa	syllable			Ꮬ
+Ꮭ	tla	tˡʰa	syllable		ꮭ	
+ꮭ	tla	tˡʰa	syllable			Ꮭ
+Ꮮ	tle	tˡe tˡʰe…	syllable		ꮮ	
+ꮮ	tle	tˡe tˡʰe…	syllable			Ꮮ
+Ꮯ	tli	tˡi tˡʰi…	syllable		ꮯ	
+ꮯ	tli	tˡi tˡʰi…	syllable			Ꮯ
+Ꮰ	tlo	tˡo tˡʰo…	syllable		ꮰ	
+ꮰ	tlo	tˡo tˡʰo…	syllable			Ꮰ
+Ꮱ	tlu	tˡu tˡʰu…	syllable		ꮱ	
+ꮱ	tlu	tˡu tˡʰu…	syllable			Ꮱ
+Ꮲ	tlv	tˡə̃ tˡʰə̃…	syllable		ꮲ	
+ꮲ	tlv	tˡə̃ tˡʰə̃…	syllable			Ꮲ
+Ꮳ	tsa	ʦa ʧa…	syllable		ꮳ	
+ꮳ	tsa	ʦa ʧa…	syllable			Ꮳ
+Ꮴ	tse	ʦe ʧe…	syllable		ꮴ	
+ꮴ	tse	ʦe ʧe…	syllable			Ꮴ
+Ꮵ	tsi	ʦi ʧi…	syllable		ꮵ	
+ꮵ	tsi	ʦi ʧi…	syllable			Ꮵ
+Ꮶ	tso	ʦo ʧo…	syllable		ꮶ	
+ꮶ	tso	ʦo ʧo…	syllable			Ꮶ
+Ꮷ	tsu	ʦu ʧu…	syllable		ꮷ	
+ꮷ	tsu	ʦu ʧu…	syllable			Ꮷ
+Ꮸ	tsv	ʦə̃ ʧə̃…	syllable		ꮸ	
+ꮸ	tsv	ʦə̃ ʧə̃…	syllable			Ꮸ
+Ꮹ	wa	wa hwa…	syllable		ꮹ	
+ꮹ	wa	wa hwa…	syllable			Ꮹ
+Ꮺ	we	we hwe…	syllable		ꮺ	
+ꮺ	we	we hwe…	syllable			Ꮺ
+Ꮻ	wi	wi hwi…	syllable		ꮻ	
+ꮻ	wi	wi hwi…	syllable			Ꮻ
+Ꮼ	wo	wo hwo…	syllable		ꮼ	
+ꮼ	wo	wo hwo…	syllable			Ꮼ
+Ꮽ	wu	wu hwu…	syllable		ꮽ	
+ꮽ	wu	wu hwu…	syllable			Ꮽ
+Ꮾ	wv	wə̃ hwə̃…	syllable		ꮾ	
+ꮾ	wv	wə̃ hwə̃…	syllable			Ꮾ
+Ꮿ	ya	ja hja…	syllable		ꮿ	
+ꮿ	ya	ja hja…	syllable			Ꮿ
+Ᏸ	ye	je hje…	syllable		ᏸ	
+ᏸ	ye	je hje…	syllable			Ᏸ
+Ᏹ	yi	ji hji…	syllable		ᏹ	
+ᏹ	yi	ji hji…	syllable			Ᏹ
+Ᏺ	yo	jo hjo…	syllable		ᏺ	
+ᏺ	yo	jo hjo…	syllable			Ᏺ
+Ᏻ	yu	ju hju…	syllable		ᏻ	
+ᏻ	yu	ju hju…	syllable			Ᏻ
+Ᏼ	yv	jə̃ hjə̃…	syllable		ᏼ	
+ᏼ	yv	jə̃ hjə̃…	syllable			Ᏼ
+Ᏽ	mv	mə̃	syllable	a	ᏽ	
+ᏽ	mv	mə̃	syllable	a		Ᏽ
+`
+
+var cols = {
+"transLoc": 1,
+"ipaLoc": 2,
+"typeLoc": 3,
+"nnameLoc": 0,
+"nameLoc": 0,
+"numLoc": 0,
+"statusLoc": 4,
+"dvowel": 0,
+"ivowel": 0,
+"subj":0,
+"ltone":0,
+"htone":0,
+"numLoc":9,
+"meaning":0,
+"shape":8,
+"lc":5,
+"uc":6,
+"transcription": 0,
+
+//"othertranscriptions": [[4, 'ISO 9985']]
+}
