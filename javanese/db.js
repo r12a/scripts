afced772ffe3eef85c44d476b95d2ff61f96283a
r12a <ishida@w3.org>
2019-02-01 05:55:56 +0000

    java, Add db.js & restructure

---
+++ javanese/db.js
@@ -0,0 +1,156 @@
+var spreadsheet = `
+ꦏ	k	k	k	ka kɔ	consonant				ꦏ ꦥ꧀ꦏ				
+ꦐ	q	q	q	qa qɔ	sasak consonant				ꦐ ꦥ꧀ꦐ				
+ꦒ	g	g	g	ɡa gɔ	consonant				ꦒ ꦥ꧀ꦒ				
+ꦔ	n	ŋ	ng	ŋa nɔ	consonant				ꦔ ꦥ꧀ꦔ				
+ꦕ	t	ʧ	c	ʧa ʧɔ	consonant				ꦕ ꦥ꧀ꦕ				
+ꦗ	d	ʤ	j	ʤa ʤɔ	consonant				ꦗ ꦥ꧀ꦗ				
+ꦚ	n	ɲ	ny	ɲa ɲɔ	consonant				ꦚ ꦥ꧀ꦚ				
+ꦛ	t	ʈ	th	ʈa ʈɔ	consonant				ꦛ ꦥ꧀ꦛ				
+ꦝ	d	ɖ	dh	ɖa ɖɔ	consonant				ꦝ ꦥ꧀ꦝ				
+ꦠ	t¶	t	t	ta tɔ	consonant				ꦠ ꦥ꧀ꦠ				
+ꦢ	d¶	d	d	da dɔ	consonant				ꦢ ꦥ꧀ꦢ				
+ꦤ	n¶	n	n	na nɔ	consonant				ꦤ ꦥ꧀ꦤ				
+ꦥ	p	p	p	pa pɔ	consonant				ꦥ ꦥ꧀ꦥ	ꦥ꧀ꦥ			
+ꦧ	b	b	b	ba bɔ	consonant				ꦧ ꦥ꧀ꦧ				
+ꦩ	m	m	m	ma mɔ	consonant				ꦩ ꦥ꧀ꦩ				
+ꦪ	y	y	y	ja jɔ	consonant				ꦪ ꦥ꧀ꦪ				
+ꦭ	l	l	l	la lɔ	consonant				ꦭ ꦥ꧀ꦭ				
+ꦫ	r	r	r	ra rɔ	consonant				ꦫ ꦥ꧀ꦫ				
+ꦬ	R	ṙ	r	ra rɔ	consonant		ra agung	a	ꦬ ꦥ꧀ꦬ				
+ꦱ	s	s	s	sa sɔ	consonant				ꦱ ꦥ꧀ꦱ	ꦥ꧀ꦱ			
+ꦮ	w	w	w	wa wɔ	consonant				ꦮ ꦥ꧀ꦮ				
+ꦲ	h	h	h	h a ∅	consonant				ꦲ ꦥ꧀ꦲ	ꦥ꧀ꦲ			
+													
+ꦑ	K	K	K	ka kɔ	murda consonant				ꦑ ꦥ꧀ꦑ				
+ꦓ	G	G	G	ɡa gɔ	murda consonant				ꦓ ꦥ꧀ꦓ				
+ꦖ	C	C	C	ʧa ʧɔ	murda consonant				ꦖ ꦥ꧀ꦖ				
+ꦘ	N	Ñ	NY	ɲa ɲɔ	murda consonant				ꦘ ꦥ꧀ꦘ				
+ꦡ	T	T	T	ta tɔ	murda consonant				ꦡ ꦥ꧀ꦡ				
+ꦟ	N	N	N	na nɔ	murda consonant				ꦟ ꦥ꧀ꦟ				
+ꦦ	P	P	P	pa pɔ	murda consonant				ꦦ ꦥ꧀ꦦ	ꦥ꧀ꦦ			
+ꦨ	B	B	B	ba bɔ	murda consonant				ꦨ ꦥ꧀ꦨ				
+ꦯ	S	S	S	sa sɔ	murda consonant				ꦯ ꦥ꧀ꦯ				
+													
+ꦰ	S	S̱	S	sa sɔ	mahaprana consonant				ꦰ ꦥ꧀ꦰ	ꦥ꧀ꦰ			
+ꦣ	D	Ḏ	D	da dɔ	mahaprana consonant				ꦣ ꦥ꧀ꦣ				
+ꦞ	D	Ḓ	DH	ɖa ɖɔ	mahaprana consonant				ꦞ ꦥ꧀ꦞ				
+ꦙ	J	J̱	J	ʤa ʤɔ	mahaprana consonant				ꦙ ꦥ꧀ꦙ				
+ꦜ	T	Ṯ	TH	ʈa ʈɔ	mahaprana consonant				ꦜ ꦥ꧀ꦜ				
+													
+													
+ꦉ	R	r̥	re	rə	independent vocalic		pa cerek		ꦉ ꦥ꧀ꦉ	ꦥ꧀ꦉ			
+ꦊ	L	l̥	le	lə	independent vocalic		nga lelet		ꦊ ꦥ꧀ꦊ				
+ꦋ	L	ŀ̥	leu	lɤ	independent vocalic		nga lelet raswadi		ꦋ ꦥ꧀ꦋ				
+													
+													
+ꦾ	y	y̆	y	j	medial consonant		pengkal						
+ꦿ	y	r̆	r	r	medial consonant		cakra						
+ꦽ	r	r̥̆	re	rə	medial consonant		kere						
+													
+													
+ꦀ	m	m̽	m	m	syllable-final consonant		panyangga						
+ꦁ	n	ŋ̽	ng	ŋ	syllable-final consonant		cecak						
+ꦂ	r	r̽	r	r	syllable-final consonant		layar						
+ꦃ	h	h̽	h	h	syllable-final consonant		wignyan						
+													
+													
+ꦄ	A	ạ	a	a	independent vowel								
+ꦆ	I	ị	i	i	independent vowel							ꦲꦶ	
+ꦇ	I	ị̄	i	i	independent vowel								
+ꦅ	I	ị̂	i	i	kawi independent vowel								
+ꦈ	U	ụ	u	u	independent vowel							ꦲꦸ	
+ꦌ	E	ẹ	e	e ɛ	independent vowel							ꦲꦺ	
+ꦍ			e		independent vowel			a					
+ꦎ	O	ọ	o	o	independent vowel							ꦲꦺꦴ	
+													
+													
+ꦴ	:	ː	a	aː	vowel sign/length mark		tarung						
+ꦶ	i	i	i	i	vowel sign		wulu						ꦆ
+ꦸ	u	u	u	u	vowel sign		suku						ꦈ
+ꦺ	e	e	e	e	vowel sign		taling						ꦌ
+ꦼ	e	ə	e	ə	vowel sign		pepet						
+ꦵ	o	ȯ	o	o	vowel sign		tolong	a					
+ꦷ	i	ī	i	i	vowel sign			a					ꦇ
+ꦹ	u	ū	u	u	vowel sign			a					ꦈꦴ
+ꦻ	a	aʲ	ai		vowel sign		dirga mure	a					
+													
+													
+꧀	*	͓			vowel-killer		pangkon						
+꦳	/	ˑ			sound changer		cecak telu						
+													
+													
+꧊	|	|			parenthesis/quotation marks		pada adeg						
+꧋	|	¶			paragraph/section start		para adeg adeg						
+꧌	(	(			parenthesis/quotation marks		pada piseleh						
+꧍	)	)			parenthesis/quotation marks		pada piseleh						
+꧈	,	,			comma/full stop		pada lingsa						
+꧉	.	.			full stop		pada lungsi						
+꧇	:	:			colon/number indicator		pada pangkat						
+ꧏ	&	²			repetition sign		pada rangkep						
+꧁	#	<ᵗ			title marker		rerengan						
+꧂	#	ᵗ>			title marker		rerengan						
+꧅	#	<ʰ			honorific symbol		pada luhur						
+꧄	#	[ʰ			honorific symbol		pada madya						
+꧃	#	{ʰ			honorific symbol		pada andap						
+꧞					correction mark		pada tirta tumétés						
+꧟					correction mark		pada isèn-isèn						
+꧆	#	°			punctuation		pada windu						
+													
+													
+꧐	0	0	0		digit		nol						
+꧑	1	1	1		digit		siji						
+꧒	2	2	2		digit		loro						
+꧓	3	3	3		digit		telu						
+꧔	4	4	4		digit		papat						
+꧕	5	5	5		digit		lima						
+꧖	6	6	6		digit		nem						
+꧗	7	7	7		digit		pitu						
+꧘	8	8	8		digit		wolu						
+꧙	9	9	9		digit		sanga						
+													
+													
+													
+													
+													
+													
+\u200B	]	␣		 									
+\u200C	=	ₓ											
+\u200D	=	₊											
+													
+													
+													
+													
+ꦻꦴ		aʲː	aʷ	aʷ									
+ꦺꦴ		eː	o	o	vowel sign combination								
+ꦼꦵ		əȯ	eu	ɤ	archaic vowel sign combination								
+													
+													
+꧋꧆꧋													
+													
+													
+ꦲ꦳	h	hˑ	h	ħ	foriegn sound								
+ꦏ꦳	x	kˑ	x	x	foriegn sound								
+ꦢ꦳		dˑ	ð	ð	foriegn sound								
+ꦗ꦳		ʤˑ	z	z	foriegn sound								
+ꦱ꦳	s	sˑ	sh	ʃ	foriegn sound								
+ꦒ꦳	y	gˑ	gh	ɣ	foriegn sound								
+ꦥ꦳		pˑ	f	f	foriegn sound								
+ꦔ꦳	}	ŋˑ		ʔ	foriegn sound								
+`
+
+var cols = {
+"transLoc": 2,
+"ipaLoc": 4,
+"typeLoc": 5,
+"nnameLoc": 6,
+"nameLoc": 7,
+"numLoc": 0,
+"statusLoc": 8,
+"dvowel": 11,
+"ivowel": 12,
+"shape":9,
+"transcription":3,
+
+//"othertranscriptions": [[4, 'ISO 15919']]
+}
