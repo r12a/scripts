c7657cbcc44f439c23862d9ea1b6ab71c3d1073c
r12a <ishida@w3.org>
2019-02-01 09:41:54 +0000

    kana, Add db.js, etc.; Add sections on kanji & kana & punctuation

---
+++ kana/db.js
@@ -0,0 +1,121 @@
+var spreadsheet = `
+ァ		
+ア	a	
+ィ		
+イ	i	
+ゥ		
+ウ	u	
+ェ		
+エ	e	
+ォ		
+オ	o	
+カ	ka	
+ガ	ga	
+キ	ki	
+ギ	gi	
+ク	ku	
+グ	gu	
+ケ	ke	
+ゲ	ge	
+コ	ko	
+ゴ	go	
+サ	sa	
+ザ	za	
+シ	ʃi	
+ジ	d͡ʒi	
+ス	su	
+ズ	zu	
+セ	se	
+ゼ	ze	
+ソ	so	
+ゾ	zo	
+タ	ta	
+ダ	da	
+チ	t͡ʃi	
+ヂ	d͡ʒi	
+ッ		
+ツ	t͡s	
+ヅ	d͡z	
+テ	te	
+デ	de	
+ト	to	
+ド	do	
+ナ	na	
+ニ	ni	
+ヌ	nu	
+ネ	ne	
+ノ	no	
+ハ	ha	
+バ	ba	
+パ	pa	
+ヒ	hi	
+ビ	bi	
+ピ	pi	
+フ	hu	
+ブ	bu	
+プ	pu	
+ヘ	he	
+ベ	be	
+ペ	pe	
+ホ	ho	
+ボ	bo	
+ポ	po	
+マ	ma	
+ミ	mi	
+ム	mu	
+メ	me	
+モ	mo	
+ャ	ja	
+ヤ	ja	
+ュ	ju	
+ユ	ju	
+ョ	jo	
+ヨ	jo	
+ラ	ra	
+リ	ri	
+ル	re	
+レ	ru	
+ロ	ro	
+ヮ		
+ワ		
+ヰ		
+ヱ		
+ヲ		
+ン	n	
+ヴ		
+ヵ		
+ヶ		
+ヷ		
+ヸ		
+ヹ		
+ヺ		
+ー		
+ヽ		
+ヾ		
+ヿ		
+`
+
+
+
+var cols = {
+"transLoc": 0,
+"ipaLoc": 1,
+"typeLoc": 2,
+"nnameLoc": 0,
+"nameLoc": 0,
+"numLoc": 0,
+"statusLoc": 0,
+"dvowel": 0,
+"ivowel": 0,
+"subj":0,
+"ltone":0,
+"htone":0,
+"numLoc":0,
+"meaning":0,
+"shape":0,
+"lc":0,
+"uc":0,
+"transcription":0,
+
+//"othertranscriptions": [[3, 'LOC']]
+}
