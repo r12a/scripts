6ea7a1647973db197700c579d383cfc796a7caec
r12a <ishida@w3.org>
2019-02-02 14:57:12 +0000

    lao, Add db.js & restructure

---
+++ lao/db.js
@@ -0,0 +1,152 @@
+var spreadsheet = `
+ກ	k	k̯	k	k	mid class consonant	 ກ ໄກ່ 	kɔː kāi	
+ຂ	k	kʰ	kh	kʰ x	high class consonant	 ຂ ໄຂ່ 	kʰɔ̌ː kʰāi 	
+ຄ	k	ḵʰ	kh	kʰ x	low class consonant	 ຄ ຄວາຢ 	kʰɔ́ː kʷʰáːj	
+ງ	n	ŋ̱	ng	ŋ	low class consonant	ງ ງົວ	ŋɔ́ː ŋúə 	
+								
+ຈ	c	c	ch	tɕ	mid class consonant	 ຈ ຈອກ 	tɕɔ̀ː tɕɔ̏ːk 	
+ຍ	n	ɲ̱	ny -j	ɲ -j	low class consonant	 ຍ ຍຸງ 	ɲɔ́ː ɲúŋ	
+								
+ຕ	t	t̯	t	t	mid class consonant	ຕ ຕາ	tɔ̀ː tàː 	
+ຖ	t	tʰ	th	tʰ	high class consonant	ຖ ຖົງ	tʰɔ̌ː tʰǒŋ	
+ທ	t	ṯʰ	th	tʰ	low class consonant	ທ ທຸງ	tʰɔ́ː tʰúŋ	
+ດ	d	d̯	d/-t	d -t	mid class consonant	 ດ ເດັກ 	dɔ̀ː dék	
+ນ	n¶	ṉ	n	n	low class consonant	ນ ນົກ	nɔ́ː nōk	
+								
+ປ	p	p̯	p	p	mid class consonant	ປ ປາ	pɔ̀ː paː	
+ຜ	p	pʰ	ph	pʰ	high class consonant	ຜ ເຜິ້ງ	pʰɔ̌ː pʰɤ̏ŋ	
+ພ	p	p̱ʰ	ph	pʰ	low class consonant	ພ ພູ	pʰɔ́ː pʰúː 	
+ບ	b	b̯	b -p	b -p	mid class consonant	ບ ແບ້	bɔ̀ː bɛ̑ː 	
+ມ	m	m̱	m	m	low class consonant	ມ ແມວ	mɔ́ː mɛ́ːw	
+								
+ລ	l	ḻ	l	l	low class consonant	ລ ລີງ	lɔ́ː líːŋ	
+ຢ	y	y̯	y	j	mid class consonant	ຢ ຢາ	jɔ̀ː jaː	
+ຣ	r	ṟ	r -n	r l -n	low class consonant	ຣະຄັງ	rākʰáŋ	o
+ວ	w	w̱	v w	ʋ w -w	low class consonant	ວ ວີ	vɔ́ː víː	
+								
+ຫ	h	h	h	h	high class consonant	ຫ ຫ່ານ	hɔ̌ː hāːn 	
+ຮ	h	ẖ	h	h	low class consonant	ຮ ເຮືອນ	hɔ́ː hɯ́ːən	
+ສ	s	s	s	s	high class consonant	ສ ເສືອ	sɔ̌ː sɯ̌ːə	
+ຊ	s	s̱	x	s	low class consonant	ຊ ຊ້າງ	sɔ́ː sâːŋ	
+ຝ	f	f	f	f	high class consonant	ຝ ຝົນ	fɔ̌ː fǒn	
+ຟ	f	f̱	f	f	low class consonant	 ຟ ໄຟ 	fɔ́ː fáj 	
+								
+ອ	}	ʔ	-	ʔ	mid class consonant	ອ ໂອ	ɔ̀ː ʔòː	
+								
+								
+								
+ໜ	h	n	n	n	high class digraph consonant			
+ໝ	h	m	m	m	high class digraph consonant			
+								
+								
+ຼ	l	l̆	l	l	high class subjoined consonant			
+								
+								
+								
+								
+								
+ເ	e	ē	ē	eː	prescript long vowel			
+ແ	e	ɛ̄	ǣ	ɛː	prescript long vowel			
+ໂ	o	ō	ō	oː	prescript long vowel			
+ໄ	a	aʲ	ai	aj	prescript long vowel	ໄມ້ມາຽ	mâj máːj	
+ໃ	a	äʲ	ai	aj	prescript long vowel	ໄມ້ມ້ວນ	mâj muân	
+								
+								
+								
+າ	a	ā	ā	aː	long vowel			
+ະ	a	a	a	aʔ a	short vowel			
+ັ	a	ä	a	aʔ a	short vowel	ໄມ້ກັນ	mâj kàn	
+ິ	i	i	i	i	short vowel			
+ີ	i	ī	ī	iː	long vowel			
+ຶ	w	ɯ	ư	ɯ	short vowel			
+ື	w	ɯ̄	ư̄	ɯː	long vowel			
+ຸ	u	u	u	u	short vowel			
+ູ	u	ū	ū	uː	long vowel			
+ຽ	i	iᵊ	īa	iːə	vowel	ວິຣາມ	vī ráːm	
+ໍ	o	ɔ̄	ǭ	ɔː	long vowel	ນິກຄະຫິດ	nīk kʰā hǐt	
+ົ	o	o	o	oʔ o	short vowel	ໄມ້ກົງ	mâj kòŋ	
+ຳ	a	aᵐ	am	am	long vowel			
+								
+								
+								
+								
+່	^	¹		̀	tone mark	ໄນ້ເອກ	mâj eːk	
+້	^	²		̂	tone mark	ໄນ້ໂທ	mâj toː	
+໊	^	³		́	tone mark	ໄນ້ຕີ	mâj tiː	
+໋	^	⁴		̌	tone mark	ໄນ້ຈັດຕະວາ	mâj cat-ta-waː	
+								
+								
+໌	*	̒			cancellation mark			
+								
+								
+໐	0	0	0	0	digit	ສູນ	sun	
+໑	1	1	1	1	digit	ໜຶ່ງ	neung	
+໒	2	2	2	2	digit	ສອງ	song	
+໓	3	3	3	3	digit	ສາມ	sam	
+໔	4	4	4	4	digit	ສີ	si	
+໕	5	5	5	5	digit	ຫ້າ	ha	
+໖	6	6	6	6	digit	ຫົກ	hok	
+໗	7	7	7	7	digit	ເຈັດ	chet	
+໘	8	8	8	8	digit	ແປດ	paet	
+໙	9	9	9	9	digit	ເກົ້າ	kao	
+								
+								
+ຯ	.	...			ellipsis			
+ໆ	&	˖			repetition mark	ເຄ່ຶອງໝາຍຊ້ຳ	kʰɯaŋ-mǎːj-sâm
+
+
+
+ຫງ		hṅ	ng	ŋ	high class			
+ຫຍ		hñ	j	ɲ	high class			
+ຫນ		hn	n	n	high class			
+ຫມ		hm	m	m	high class			
+ຫຼ		hl̆	l	l	high class			
+ຫລ		hl	l	l	high class			
+ຫວ		hw	v w	ʋ w	high class			
+								
+								
+-ັຍ		-äɲ̱	ai	aj				
+-ັອ		-äʔ	ǫ	ɔʔ ɔ				
+-ັຽ		-äiᵊ	ia	iə				
+-ົວ		-ow̱	ūa	uə				
+-ິວ		-iw̱	iu	iw				
+-ີວ		-īw̱	īu	iːw				
+-າຍ		-āɲ̱	āi	aːj				
+-າຽ		-āiᵊ	āi	aːj				
+ເ-ິ		ē-i	œ	ɤ				
+ເ-ີ		ē-ī	œ̄	ɤː				
+ເ-ັ		ē-ä	e	eʔ e				
+ເ-ະ		ē-a	e	eʔ e				
+ແ-ະ		ɛ̄-a	æ	ɛʔ ɛ				
+ເ-ຍ		ē-ɲ̱	īa	iːə				
+ແ-ັ		ɛ̄-ä	æ	ɛʔ ɛ				
+ແ-ວ		ɛ̄-w̱	æw	ɛːw				
+ໂ-ະ		ō-a	o	oʔ o				
+-ຽວ		iᵊw̱	īeo					
+-ົວະ		-ow̱a	ua	uə				
+ເ-ົາ		ē-oā	ao	aw				
+ເ-ັຍ		ē-äɲ̱	ia	iːə				
+ເ-ຶອ		ē-ɯʔ	ưa	ɯə				
+ເ-ືອ		ē-ɯ̄ʔ	ư̄a	ɯːə				
+ເ-າະ		ē-āa	ǫ	ɔʔ ɔ				
+ເ-ັຍະ		ē-äɲ̱a		ia				
+ເ-ັຽວ		e-äiᵊw̱	īeo													
+ອ	}	ʔ	ǭ	ʔ ɔː				
+ວ	w	w̱	ūa	ʋ w -w uːə				
+								
+								
+ຯ ລ ຯ			la	la					
+`
+
+var cols = {
+"transLoc": 2,
+"ipaLoc": 4,
+"typeLoc": 5,
+"nameLoc": 7,
+"nnameLoc": 6,
+"numLoc": 0,
+"statusLoc": 8,
+"transcription":3,
+
+"othertranscriptions": [[3, 'LOC']]
+}
