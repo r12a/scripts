8c276eb00e325888476a2d9da8543319e958bf29
r12a <ishida@w3.org>
2019-02-04 08:59:20 +0000

    sinh, Add db.js & new list format

---
+++ sinhala/db.js
@@ -0,0 +1,154 @@
+var spreadsheet = `
+ක	k	k	k	k	k	 śuddha consonant					
+ග	g	g	g	g	g	 śuddha consonant					
+ඟ	N	ᵑɡ	N	ňg	ᵑɡ	 śuddha prenasalised consonant					
+ච	c	ʧ	c	c	ʧ	 śuddha consonant					
+ජ	d	ʤ	j	j	ʤ	 śuddha consonant					
+ට	t	ʈ	t	ṭ	ʈ	 śuddha consonant					
+ඩ	d	ɖ	d	ḍ	ɖ	 śuddha consonant					
+ණ	n	n	n	ṇ	n	 śuddha consonant					
+ඬ	N	ⁿɖ	N	ňḍ	ⁿɖ	 śuddha prenasalised consonant					
+ත	t¶	t	t¶	t	t	 śuddha consonant					
+ද	d¶	d	d¶	d	d	 śuddha consonant					
+ඳ	N	ⁿd	N	ňd	ⁿd	 śuddha prenasalised consonant					
+ප	p	p	p	p	p	 śuddha consonant					
+බ	b	b	b	b	b	 śuddha consonant					
+ම	m	m	m	m	m	 śuddha consonant					
+ඹ	M	ᵐb	M	m̌b	ᵐb	 śuddha prenasalised consonant					
+ය	y	y	y	y	j	 śuddha consonant					
+ර	r	r	r	r	r	 śuddha consonant					
+ල	l	l	l	l	l	 śuddha consonant					
+ළ	l	ɭ	l	ḷ	l	 śuddha consonant					
+ව	v	v	v	v	ʋ	 śuddha consonant					
+ස	s	s	s	s	s	 śuddha consonant					
+හ	h	h	h	h	ɦ	 śuddha consonant					
+											
+											
+ඛ	K	kʰ	K	kh	k	miśra consonant					
+ඝ	G	gʰ	G	gh	g	miśra consonant					
+ඥ	n	ᵑǰ	j	jñ	ɲ ɡn	miśra prenasalised consonant					
+ඞ	n	ŋ	n	ṅ	ŋ	miśra consonant					
+ඡ	C	ʧʰ	C	ch	ʧ	miśra consonant					
+ඣ	D	ʤʰ	J	jh	ʤ	miśra consonant					
+ඤ	n	ɲ	n	ñ	ɲ	miśra consonant			archaic		
+ඦ	N	ⁿʤ	N	n̆j	nd͡ʒ	miśra prenasalised consonant			archaic		
+ඨ	T	ʈʰ	T	ṭh	ʈ	miśra consonant					
+ඪ	D	ɖʰ	D	ḍh	ɖ	miśra consonant					
+ථ	T¶	tʰ	T¶	th	t	miśra consonant					
+ධ	D¶	dʰ	D¶	dh	d	miśra consonant					
+න	n	ṅ	n	n	n	miśra consonant					
+ඵ	P	pʰ	P	ph	p	miśra consonant					
+භ	B	bʰ	B	bh	b	miśra consonant					
+ශ	s	ʃ	s	ś	ʃ	miśra consonant					
+ෂ	s	ʃ̇	s	ṣ	ʃ	miśra consonant					
+ෆ	f	f	f	f	f	miśra consonant					
+											
+											
+ං	n	ŋ̽	n	n	ŋ	anusvara	binduva				
+ඃ	h	h̽	h	h	?	visarga					
+											
+											
+අ	A	ạ	A	a	a ə	śuddha independent vowel					
+ආ	A	ạ̄	A	ā	aː a	śuddha independent vowel				ා	
+ඇ	A	æ̣	A	æ	æ	śuddha independent vowel				ැ	
+ඈ	A	ǣ̣	A	ǣ	æː	śuddha independent vowel				ෑ	
+ඉ	I	ị	I	i	i	śuddha independent vowel				ි	
+ඊ	I	ị̄	I	ī	iː	śuddha independent vowel				ී	
+උ	U	ụ	U	u	u	śuddha independent vowel				ු	
+ඌ	U	ụ̄	U	ū	uː	śuddha independent vowel				ූ	
+එ	E	ẹ	E	e	e	śuddha independent vowel				ෙ	
+ඒ	E	ẹ̄	E	ē	eː	śuddha independent vowel				ේ	
+ඔ	O	ọ	O	o	o	śuddha independent vowel				ො	
+ඕ	O	ọ̄	O	ō	oː	śuddha independent vowel				ෝ	
+											
+ා	a	ā	a	ā	aː a	śuddha vowel sign	æ̣lapil͓la	ඇලපිල්ල			ආ
+ැ	a	æ	a	æ	æ	śuddha vowel sign	æ̣daya	ඇදය			ඇ
+ෑ	a	ǣ	a	ǣ	æː	śuddha vowel sign	diga æ̣daya	දිග ඇදය			ඈ
+ි	i	i	i	i	i	śuddha vowel sign	ịs͓pil͓la	ඉස්පිල්ල			ඉ
+ී	i	ī	i	ī	iː	śuddha vowel sign	diga ịs͓pil͓la	දිග ඉස්පිල්ල			ඊ
+ු	u	u	u	u	u	śuddha vowel sign	pāpil͓la	පාපිල්ල			උ
+ූ	u	ū	u	ū	uː	śuddha vowel sign	diga pāpil͓la	දිග පාපිල්ල			ඌ
+ෙ	e	e	e	e	e	śuddha vowel sign	kom͓buva	කොම්බුව			එ
+ේ	e	ē	e	ē	eː	śuddha vowel sign	kom͓buva saha hal͓kirīma	කොම්බුව සහ හල්කිරීම			ඒ
+ො	o	o	o	o	o	śuddha vowel sign	kom͓buva saha æ̣lapil͓la	කොම්බුව සහ ඇලපිල්ල			ඔ
+ෝ	o	ō	o	ō	oː	śuddha vowel sign	kom͓buva saha hal͓æ̣lapil͓la	කොම්බුව සහ හල්ඇලපිල්ල			ඕ
+											
+											
+ඓ	A	ạʲ	A	ai	ɑj	miśra independent vowel				ෛ	
+ඖ	A	ạʷ	A	au	ɑw	miśra independent vowel				ෞ	
+											
+ෛ	a	aʲ	a	ai	ɑj	miśra vowel sign	kom͓bu deka	කොම්බු දෙක			ඓ
+ෞ	a	aʷ	a	au	ɑw	miśra vowel sign	kom͓buva saha gayaṅukit͓ta	කොම්බුව සහ ගයනුකිත්ත			ඖ
+											
+											
+ඍ	R	r̥̣			ri ru	miśra independent vowel				ෘ	
+ඎ	R	r̥̣̄			riː ruː	miśra independent vowel			archaic	ෲ	
+ඏ	L	l̥̣			li	miśra independent vowel			archaic	ෟ	
+ඐ	L	l̥̣̄			liː	miśra independent vowel			archaic	ෳ	
+ෘ	R	r̥			ri ru	miśra vowel sign	gæʈaya sahita æ̣lapil͓la	ගැටය සහිත ඇලපිල්ල			ඍ
+ෲ	R	r̥̄			riː ruː	miśra vowel sign	gæʈaya sahita æ̣lapili deka	ගැටය සහිත ඇලපිලි දෙක			ඎ
+ෟ	L	l̥			li	miśra vowel sign	gayaṅukit͓ta	ගයනුකිත්ත	archaic		ඏ
+ෳ	L	l̥̄			liː	miśra vowel sign			archaic		ඐ
+											
+											
+්	x	͓				virama	hal͓ kirima	හල් කිරිම			
+		ˣ									
+											
+෴	|	¶	[			paragraph terminator					
+											
+											
+෦	0	0	0	0	0	astrological digit			archaic		
+෧	1	1	1	1	1	astrological digit			archaic		
+෨	2	2	2	2	2	astrological digit			archaic		
+෩	3	3	3	3	3	astrological digit			archaic		
+෪	4	4	4	4	4	astrological digit			archaic		
+෫	5	5	5	5	5	astrological digit			archaic		
+෬	6	6	6	6	6	astrological digit			archaic		
+෭	7	7	7	7	7	astrological digit			archaic		
+෮	8	8	8	8	8	astrological digit			archaic		
+෯	9	9	9	9	9	astrological digit			archaic		
+											
+𑇡	1	1̣	1	1	1	historical number			archaic		
+𑇢	2	2̣	2	2	2	historical number			archaic		
+𑇣	3	3̣	3	3	3	historical number			archaic		
+𑇤	4	4̣	4	4	4	historical number			archaic		
+𑇥	5	5̣	5	5	5	historical number			archaic		
+𑇦	6	6̣	6	6	6	historical number			archaic		
+𑇧	7	7̣	7	7	7	historical number			archaic		
+𑇨	8	8̣	8	8	8	historical number			archaic		
+𑇩	9	9̣	9	9	9	historical number			archaic		
+𑇪	§	{10}			10	historical number			archaic		
+𑇫	§	{20}			20	historical number			archaic		
+𑇬	§	{30}			30	historical number			archaic		
+𑇭	§	{40}			40	historical number			archaic		
+𑇮	§	{50}			50	historical number			archaic		
+𑇯	§	{60}			60	historical number			archaic		
+𑇰	§	{70}			70	historical number			archaic		
+𑇱	§	{80}			80	historical number			archaic		
+𑇲	§	{90}			90	historical number			archaic		
+𑇳	§	{100}			100	historical number			archaic		
+𑇴	±	{1000}			1000	historical number			archaic		
+`
+
+var cols = {
+"transLoc": 2,
+"ipaLoc": 5,
+"typeLoc": 6,
+"nnameLoc": 8,
+"nameLoc": 7,
+"numLoc": 0,
+"statusLoc": 9,
+"dvowel": 10,
+"ivowel": 11,
+"subj":0,
+"ltone":0,
+"htone":0,
+"numLoc":0,
+"meaning":0,
+"shape":0,
+"lc":0,
+"uc":0,
+"transcription":4,
+
+"othertranscriptions": [[4, 'ISO 15919']]
+}
