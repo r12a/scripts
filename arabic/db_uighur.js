837cbde7afa782494c8e4f18d3fc8abccb72ba30
r12a <ishida@w3.org>
2019-02-09 22:25:13 +0000

    uighur, Add db.js & restructure

---
+++ arabic/db_uighur.js
@@ -0,0 +1,76 @@
+var spreadsheet = `
+م	m	m	m	м	m	consonant				
+ن	n	n	n	н	n	consonant				
+د	d	d	d	д	d	consonant				
+ت	t	t	t	т	t	consonant				
+ب	b	b	b	б	b	consonant				
+پ	p	p	p	п	p	consonant				
+ف	f	f	f	ф	f	consonant				
+ق	q	q	q	қ	q	consonant				
+ك	k	k	k	к	k	consonant				
+ڭ	n	ŋ	ng	ң	ŋ	consonant				ڭ ڭڭڭ
+گ	g	g	g	г	g	consonant				
+غ	r	ʁ	gh	ғ	ʁ	consonant				
+ھ	h	h	h	һ	h	consonant				
+خ	x	χ	x	х	χ	consonant				
+چ	c	č	ch	ч	ʧ	consonant				
+ج	d	ʤ	j	җ	ʤ	consonant				
+ژ	z	ʒ	zh	ж	ʒ	consonant				
+ز	z¶	z	z	з	z	consonant				
+س	s	s	s	с	s	consonant				
+ش	s	ʃ	sh	ш	ʃ	consonant				
+ر	r¶	r	r	р	r	consonant				
+ل	l	l	l	л	l	consonant				
+ئ	{	ʿ	- ’	-	-	consonant				
+										
+										
+ي	y	y	y	й	j	semivowel				
+ۋ	w	w	w	в	v w	semivowel				
+										
+										
+ا	a	a	a	а	ɑ	vowel				
+ە	e	e	e	ә	ɛ	vowel				
+ې	e	ë	ë	е	e	vowel				
+ى	i	i	i	и	i	vowel				
+ۆ	o	ø	ö	ө	ø	vowel				
+و	o	o	o	о	o	vowel				
+ۇ	u	u	u	у	u	vowel				
+ۈ	u	ü	ü	ү	y	vowel				
+										
+										
+٪	%	%	%			percent sign				
+؟	?	?	?			question mark				
+؛	;	;	;			semicolon				
+،	,	,	,			comma				
+
+
+ئا	A	’a	a	а	ɑ		
+ئە	E	’e	e	ә	ɛ		
+ئې	E	 ’ë	ë	е	e		
+ئى	I	’i	i	и	i		
+ئو	O	’o	ö	ө	o		
+ئۆ	O	’ø	o	о	ø		
+ئۇ	U	’u	u	у	u		
+ئۈ	U	’ü	ü	ү	y		
+`
+
+var cols = {
+"transLoc": 2,
+"ipaLoc": 5,
+"typeLoc": 6,
+"nnameLoc": 0,
+"nameLoc": 0,
+"numLoc": 0,
+"statusLoc": 0,
+"dvowel": 0,
+"ivowel": 0,
+"subj":0,
+"ltone":0,
+"htone":0,
+"meaning":0,
+"shape":10,
+"transcription":3,
+"transcription2":4,
+
+"othertranscriptions": [[3, 'ULY'], [4, 'USY']]
+}
