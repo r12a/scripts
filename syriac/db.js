5fc1213dbc3576895375d4108d416f5d9cc40ff4
r12a <ishida@w3.org>
2019-02-09 10:27:13 +0000

    syrc, Add db.js & restructure

---
+++ syriac/db.js
@@ -0,0 +1,152 @@
+var spreadsheet = `
+ܐ	}	ʾ	ʔ ɑ	} a	ʔ	ʾ ȃ	consonant	ʾālap̄	ܐܠܦ	1
+ܒ	b	b	b v w	b	b	b ḇ	consonant	bēṯ	ܒܝܬ	2
+ܓ	g	g	ɡ ɣ	g	g	g ḡ ǧ	consonant	gāmal	ܓܡܠ	3
+ܕ	d	d	d ð	d	d	d ḏ	consonant	dālaṯ	ܕܠܬ	4
+ܗ	h	h	h	h	h	h	consonant	hē	ܗܐ	5
+ܘ	w	w	w u o	w	w	w û ô	consonant	waw	ܘܘ	6
+ܙ	z	z	z ʒ	z	z	z ž	consonant	zayn	ܙܝܢ	7
+ܚ	h	ħ	ħ x χ	h	ħ	ḥ	consonant	ḥēṯ	ܚܝܬ	8
+ܛ	t	tˤ	tˤ	t	tˤ	ṭ	consonant	ṭēṯ	ܛܝܬ	9
+ܝ	y	y	j i e	y	j	y î	consonant/mater lectionis	yōḏ	ܝܘܕ	10
+ܟ	k	k	k x ʧ	k	k	k ḵ č	consonant	kāp̄	ܟܦ	20
+ܠ	l	l	l	l	l	l	consonant	lāmaḏ	ܠܡܕ	30
+ܡ	m	m	m	m	m	m	consonant	mīm	ܡܝܡ	40
+ܢ	n	n	n	n	n	n	consonant	nūn	ܢܘܢ	50
+ܣ	s	s	s	s	s	s	consonant	semkaṯ	ܣܡܟܬ	60
+ܥ	{	ʿ	ʕ	{	ʕ	ʿ	consonant	ʿē	ܥܐ	70
+ܦ	p	p	p f w	p	p	p p̄	consonant	pē	ܦܐ	80
+ܨ	s	sˤ	sˤ	s	sˤ	ṣ	consonant	ṣāḏē	ܨܕܐ	90
+ܩ	q	q	q	q	q	q	consonant	qōp̄	ܩܘܦ	100
+ܪ	r	r	r	r	r	r	consonant	rēš	ܪܝܫ	200
+ܫ	s	ʃ 	ʃ ʒ	s	ʃ 	š ẑ	consonant	šīn	ܫܝܢ	300
+ܬ	t	t	t θ	t	t	t ṯ	consonant	taw	ܬܘ	400
+										
+										
+ܖ		ḋ	d ð		d	d ḏ	alternative consonant	dālaṯ rish		
+ܞ	j	jʰ		j	jʰ	jh	ligature			
+ܤ	s	ṡ		s	ṡ	s	consonant			
+ܧ	p	ṗ			p	p	consonant			
+										
+										
+ܔ							garshuni letter			
+ܜ							garshuni letter			
+ً							arabic diacritic			
+ٌ							arabic diacritic			
+ٍ							arabic diacritic			
+َ							arabic diacritic			
+ُ							arabic diacritic			
+ِ							arabic diacritic			
+ّ							arabic diacritic			
+ْ							arabic diacritic			
+ٰ							arabic diacritic			
+ٕ							arabic diacritic			
+ٔ							arabic diacritic			
+ٓ							arabic diacritic			
+										
+										
+ܭ			v		v		consonant			
+ܮ			ɣ		ɣ		consonant			
+ܯ			ð		ð		consonant			
+										
+										
+ݍ			ʒ		ʒ		consonant			
+ݎ			x		x		consonant			
+ݏ			f		f		consonant			
+										
+										
+ܲ	a	a	a	a	a	a	vowel diacritic	pṯāḥā	ܦܬ݂ܳܚܳܐ‎	
+ܵ	a	ā	ɑ	a	ɑ	ā	vowel diacritic	zqāp̄ā	ܙܩܵܦ݂ܵܐ‎	
+ܸ	e	e	ɛ ɪ e	e i e	ɛ ɪ e	e	vowel diacritic	rḇāṣā ʾărīḵā / zlāmā pšīqā	ܪܒ݂ܵܨܵܐ ܐܲܪܝܼܟ݂ܵܐ‎ / ܙܠܵܡܵܐ ܦܫܝܼܩܵܐ‎	
+ܹ	e	ē	e	e	e	ē	vowel diacritic	rḇāṣā karyā / zlāmā qašyā	ܪܒ݂ܵܨܵܐ ܟܲܪܝܵܐ‎ / ܙܠܵܡܵܐ ܩܲܫܝܵܐ‎	
+ܼ	i	i	i u	i	i	i	vowel diacritic	ḥbāṣā		
+ܿ	o	o	o	o	o	o	vowel diacritic	ʿṣāṣā rwīḥā / rwāḥā	ܥܨܵܨܵܐ ܪܘܝܼܚܵܐ‎ / ܪܘܵܚܵܐ‎	
+										
+										
+ܰ	a	ȧ	a	a	a	ȧ	vowel diacritic	pṯāḥā	ܦܬ݂ܳܚܳܐ‎	
+ܱ	a	ạ	a	a	a	ạ	vowel diacritic	pṯāḥā	ܦܬ݂ܳܚܳܐ‎	
+ܳ	a	å	ɑ	a	ɑ	å	vowel diacritic	zqāp̄ā	ܙܩܵܦ݂ܵܐ‎	
+ܴ	a	ḁ	ɑ	a	ɑ	ḁ	vowel diacritic	zqāp̄ā	ܙܩܵܦ݂ܵܐ‎	
+ܶ	e	ė	ɛ e	e e	ɛ e	ė	vowel diacritic	rḇāṣā	ܪܒ݂ܵܨܵܐ	
+ܷ	e	ẹ	ɛ e	e e	ɛ e	ẹ	vowel diacritic	rḇāṣā	ܪܒ݂ܵܨܵܐ	
+ܺ	i	ï	i	i	i	ï	vowel diacritic	ḥḇāṣā	ܚܒ݂ܵܨܵܐ‎	
+ܻ	i	ị	i	i	i	ị	vowel diacritic	ḥḇāṣā	ܚܒ݂ܵܨܵܐ‎	
+ܽ	u	ů	u	u	u	ů	vowel diacritic	ṣāṣā	ܥܨܳܨܳܐ‎	
+ܾ	u	ụ	u	u	u	ụ	vowel diacritic	ṣāṣā	ܥܨܳܨܳܐ‎	
+ّ	o	ȯ	o	o	o	ȯ	vowel diacritic			
+										
+										
+̃							maǧliyana			
+̰							maǧliyana			
+݁							hard-sound marker	qūššāyā	ܩܘܫܝܐ	
+݂		̣					hard/soft sound marker			
+̮							fricative indicator			
+݇							silent letter marker			
+݈							silent letter marker			
+̈	[	̋		[	̋	\u030B	syame			
+̇							disambiguation mark			
+̣							disambiguation mark			
+݀	[	ʺ		[	ʺ	ʺ	feminine marker			
+										
+										
+̄							phonological marker			
+̠							phonological marker			
+̊							hard/soft sound marker			
+̥							hard/soft sound marker			
+̤							phonological marker			
+										
+݃							accent mark			
+݄							accent mark			
+݅							diacritic			
+݆							diacritic			
+݉							music mark			
+݊							diacritic cross			
+ܑ							aleph			
+										
+										
+										
+܀				[	¶		end of paragraph			
+܁		.		.	.	.	full stop			
+܂		.		.	.	.	full stop			
+܃		:		:	:	:	colon			
+܄		:		:	:	:	colon			
+܅		:		:	:	:	colon			
+܆		:		:	:	:	colon			
+܇		:		:	:	:	colon			
+܈		;		;	;	;	phrase division			
+܉		?		?	?	?	question mark			
+										
+܊		.		.	.	.	contraction mark			
+\u070F							abbreviation mark			
+܋							note marker			
+܌							note marker			
+܍							note marker			
+										
+،	,	,		,	,		comma			
+؛	;	;		;	;	;	semi-colon			
+.				.	.		full stop			
+؟	?	?		?	?	?	question mark			
+`
+
+var cols = {
+"transLoc": 2,
+"ipaLoc": 3,
+"typeLoc": 7,
+"nnameLoc": 9,
+"nameLoc": 8,
+"numLoc": 10,
+"statusLoc": 0,
+"dvowel": 0,
+"ivowel": 0,
+"subj":0,
+"ltone":0,
+"htone":0,
+"numLoc":0,
+"meaning":0,
+"shape":0,
+"lc":0,
+"uc":0,
+"transcription":6,
+
+"othertranscriptions": [[6, 'Transcription']]
+}
