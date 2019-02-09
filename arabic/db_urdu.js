b4793ae525f1270300d7ccb9bd2884608c556e2a
r12a <ishida@w3.org>
2019-02-09 17:42:33 +0000

    urdu, Add db.js & restructure

---
+++ arabic/db_urdu.js
@@ -0,0 +1,140 @@
+var spreadsheet = `
+ء	}	ʾ	ʾ –	-	vowel separator / calendar indicator	hamzaː			ء
+ا	a	ɑ	ā ʾ –	a/ɪ/u iː/e/ɛ uː/o/ɔ ʊ/∅ ɑː	consonant+vowel	alɪf			ا ـا
+آ	a	ɑ̄		ɑː	vowel	əlɪf mədd			آ ـآ
+أ	a	ɑ͑			consonant+vowel				أ ـأ
+ؤ	u	u͑		uː o	vowel separator+vowel				ؤ ـؤ
+ئ	i	ɪ͑		ɪ/a iː ɛ	vowel separator / vowel				ئ ئئئ
+ے	e	ɛ	ai e	e ɛ	vowel	baɽiː je			ے
+ۓ	e	ɛ͑		e ɛ	izafat	ɪzɑːfat			ۓ
+ب	b	b	b	b	consonant	be			ب ببب
+چ	c	č	c	ʧ	consonant	ʧe			چ چچچ
+ت	t¶	t	t	t	consonant	te			ت تتت
+ٹ	t¶	ʈ	ṭ	ʈ	consonant	ʈe			ٹ ٹٹٹ
+ث	s	ṡ	th	s	consonant	se			ث ثثث
+پ	p	p	p	p	consonant	pe			پ پپپ
+ج	D	ʤ	j	ʤ	consonant	ʤiːm			ج ججج
+ح	H	h	ḥ	h	consonant	baɽiː he			ح ححح
+خ	x	x	kh	x	consonant	xe			خ خخخ
+ذ	z	z	ṭh	z	consonant	zɑːl			ذ ـذ
+د	d	d	d	d	consonant	dɑːl			د ـد
+ڈ	d	ɖ	ḍ	ɖ	consonant	ɖɑːl			ڈ ـڈ
+ر	r	r	r	r	consonant	re			ر ـر
+ڑ	r	ɽ	ṛ	ɽ	consonant	ɽe			ڑ ـڑ
+ز	z	z	z	z	consonant	xe			ز ـز
+ژ	z	ʒ	zh	ʒ	consonant	ʒe			ژ ـژ
+س	s¶	s	s	s	consonant	siːn			س سسس
+ش	s	ʃ	sh	ʃ	consonant	ʃiːn			ش ششش
+ص	S	ŝ	ṣ	s	consonant	svɑːd			ص صصص
+ض	Z	ẑ	d	z	consonant	zvɑːd			ض ضضض
+ط	T	t̂	t̤	t	consonant	toe			ط ططط
+ظ	Z	ž	țh	z	consonant	zoe			ظ ظظظ
+ع	{	ʿ	ā o e ʿ –	∅	consonant	ain			ع ععع
+غ	Y	ɣ	gh	ɣ	consonant	ɣain			غ غغغ
+ف	f	f	f	f	consonant	fe			ف ففف
+ق	q	q	q	q	consonant	qɑːf			ق ققق
+گ	g	g	gh	g	consonant	gɑːf			گ گگگ
+ک	k¶	k	k	k	consonant	kɑːf			ک ککک
+ل	l	l	l	l	consonant	lɑːm			ل للل
+م	m	m	m	m	consonant	miːm			م ممم
+ن	n	n	n	n ◌̃	consonant	nuːn			ن ننن
+ں	n	ñ	ṉ	◌̃	nasalisation indicator	nuːn ɣunna			ں ںںں
+ہ	h¶	ḫ	h	h ɑː ɛ ∅	consonant	ʧʰoʈiː he			ہ ہہہ
+ھ	h¶	ʰ	h	ʰ	aspiration marker / calendar indicator	do cašmī he			ھ ھھھ
+ۂ	E	e͑		hɛ	izafat	ɪzɑːfat			ۂ ۂۂۂ
+ۀ	h	h͑							ۀ ـۀ
+و	v	v	v ū o au	β uː o ɔ ∅	consonant+vowel	vɑːuː			و ـو
+ی	y	y	y ī á	j iː e ɛ	consonant+vowel	ye			ی ییی
+									
+									
+َ	A	a		a	vowel	zəbər			
+ُ	U	u		u	vowel	peʃ			
+ِ	I	i		i	vowel	zer			
+ً	A	aⁿ		an	vowel	an			
+ٌ	U	uⁿ		un	vowel	un			
+ٍ	I	iⁿ		in	vowel	in			
+ّ	w	ᵚ			gemination mark	taʃdiːd			
+ْ	*	͓			vowel absence marker	sukuːn/ʤazm			
+ٰ	a	ɑ̇		aː	superscript alef				
+ٔ	\'	‘		ʾ	izafat	ɪzɑːfat			
+ٖ		ᵢ		i	vowel quality indicator				
+ٗ	u	ᵘ		u	vowel quality indicator				
+٘	n	ᵑ		̃	nasalisation mark				
+									
+									
+؀					number sign				
+؁					calendar indicator	sənh			
+؂					footnote marker				
+؃					page number marker	səfəh			
+؄					era marker				
+؎					poetic verse sign				
+؏					poetic line marker	misrə			
+ؐ					honorific	sallallao alae va sallam			
+ؑ					honorific	alejsallam			
+ؒ					honorific	raːmatʊlla alee			
+ؓ					honorific	raziallaːo ano			
+ؔ					author name marker				
+									
+									
+ـ	_	_			baseline extender				
+،	,	,		,	comma	əʃɑːrɪjɑ			
+۔	.	.		.	full stop				
+؛	;	;		;	semi-colon				
+؟	?	?		?	question mark				
+٪	%	%		%	percent mark				
+٫					decimal separator				
+٬					thousands separator				
+									
+									
+۰	0	0̣	0		digit	sɪfr			٠
+۱	1	1̣	1		digit	ek			١
+۲	2	2̣	2		digit	do			٢
+۳	3	3̣	3		digit	tiːn			٣
+۴	4	4̣	4		digit	ʧɑːr			٤
+۵	5	5̣	5		digit	pɑ̃ːʧ			٥
+۶	6	6̣	6		digit	ʧʰe			٦
+۷	7	7̣	7		digit	sɑːt			٧
+۸	8	8̣	8		digit	ɑːʈʰ			٨
+۹	9	9̣	9		digit	nəʊ			٩									
+									
+									
+									
+بھ		bʰ	bh	bʱ					
+پھ		pʰ	ph	pʰ					
+تھ		tʰ	th	t̪ʰ					
+ٹھ		ʈʰ	ṭh	ʈʰ					
+جھ		ʤʰ	jh	d͡ʒʰ					
+چھ		čʰ	ch	t͡ʃʰ					
+دھ		dʰ	dh	d̪ʱ					
+ڈھ		ɖʰ	ḍh	ɖʱ					
+رھ		rʰ	ṛh	rʱ					
+ڑھ		ɽʰ	ṛh	ɽʱ					
+کھ		kʰ	kh	kʰ					
+گھ		gʰ	gh	ɡʱ					
+لھ		lʰ	lh	lʱ					
+مھ		mʰ	mh	mʱ					
+نھ		nʰ	nh	nʱ					
+هھ		هʰ	hh	hʱ					
+وھ		vʰ	wh	ʋʱ					
+یھ		yʰ	yh	jʱ					
+`
+
+var cols = {
+"transLoc": 2,
+"ipaLoc": 4,
+"typeLoc": 5,
+"nnameLoc": 0,
+"nameLoc": 6,
+"numLoc": 0,
+"statusLoc": 0,
+"dvowel": 0,
+"ivowel": 0,
+"subj":0,
+"ltone":0,
+"htone":0,
+"meaning":0,
+"shape":9,
+"transcription":3,
+
+//"othertranscriptions": [[3, 'UN']]
+}
