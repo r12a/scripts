32c8750e599ea006297994eb40fd8e8f21937f64
r12a <ishida@w3.org>
2019-02-14 10:02:46 +0000

    + Malayalam, first draft

---
+++ malayalam/db.js
@@ -0,0 +1,138 @@
+var spreadsheet = `
+ക	k	k	k	k	k	consonant					
+ഖ	K	kʰ	K	kh	kʰ	consonant					
+ഗ	g	g	g	g	g	consonant					
+ഘ	G	gʰ	G	gh	gʰ	consonant					
+ങ	N	ŋ	N	ṅ	ŋ	consonant					
+ച	c	c	c	c	ʧ	consonant					
+ഛ	C	cʰ	C	ch	ʧʰ	consonant					
+ജ	j	j	j	j	ɟ	consonant					
+ഝ	J	jʰ	J	jh	ɟʰ	consonant					
+ഞ	n	ɲ	n	ñ	ɲ	consonant					
+ട	t	ʈ	t	ṭ	ʈ	consonant					
+ഠ	T	ʈʰ	T	ṭh	ʈʰ	consonant					
+ഡ	d	ɖ	d	ḍ	ɖ	consonant					
+ഢ	D	ɖʰ	D	ḍh	ɖʰ	consonant					
+ണ	n	ɳ	n	ṇ	ɳ	consonant					
+ത	t¶	t	t¶	t	t̪	consonant					
+ഥ	T¶	tʰ	T¶	th	t̪ʰ	consonant					
+ദ	d¶	d	d¶	d	d̪	consonant					
+ധ	D¶	dʰ	D¶	dh	d̪ʰ	consonant					
+ന	n¶	n	n¶	n	n̪ n	consonant					
+ഩ	n					consonant			archaic		
+പ	p	p	p	p	p	consonant					
+ഫ	P	pʰ	P	ph	pʰ	consonant					
+ബ	b	b	b	b	b	consonant					
+ഭ	B	bʰ	B	bh	bʰ	consonant					
+മ	m	m	m	m	m	consonant					
+യ	y	y	y	y	j	consonant					
+ര	r	r	r	r	r̪	consonant					
+റ	r	ṙ	r	ṟ	r t	consonant					
+ല	l	l	l	l	l	consonant					
+ള	l	ɭ	l	ḷ	ɭ,	consonant					
+ഴ	r	ɻ	l	ḻ	ɻ	consonant					
+വ	v	ʋ	v	v	ʋ	consonant					
+ശ	s	ʃ	s	ś	ɕ ʃ	consonant					
+ഷ	s	ʂ	s	ṣ	ʂ	consonant					
+സ	s¶	s	s¶	s	s	consonant					
+ഹ	h	h	h	h	ɦ	consonant					
+ഺ						consonant			archaic		
+											
+											
+ൺ	N	ɳ̽	N	ṇ^	n	chillu consonant					
+ൻ	N	n̽	N	n^	n	chillu consonant					
+ർ	R	r̽	R	r^	r	chillu consonant					
+ൽ	L	l̽	L	l^	l	chillu consonant					
+ൾ	L	ɭ̽	L	ḷ^	ɭ	chillu consonant					
+ൿ	K	k̽	K	k^	k	chillu consonant					
+											
+											
+ം	m	m̽	m	ṃ	m	anusvara					
+ഃ	h	h̽	h	ḥ	ɦ	visarga					
+											
+											
+											
+അ	A	ạ̄	A	a	a	independent vowel					
+ആ	A	ậ		ā	aː	independent vowel				ാ	
+ഇ	I	ị	I	i	i	independent vowel				ി	
+ഈ	I	ị̄	I	ī	iː	independent vowel				ീ	
+ഉ	U	ụ	U	u	u	independent vowel				ു	
+ഊ	U	ụ̄	U	ū	uː	independent vowel				ൂ	
+എ	E¶	ẹ	E¶	e	e	independent vowel				െ	
+ഏ	E	ẹ̄	E	ē	eː	independent vowel				േ	
+ഒ	O	ọ	O	o	o	independent vowel				ൊ	
+ഓ	O	ọ̄	O	ō	oː	independent vowel				ോ	
+ഐ	A	ạʲ	A	ai	ai̯	independent vowel				ൈ	
+ഔ	A	ạʷ	A	au	au̯	independent vowel				ൗ	
+											
+											
+ാ	a	ā	a	ā	aː	vowel sign					ആ
+ി	i	i	i	i	i	vowel sign					ഇ
+ീ	i	ī	i	ī	iː	vowel sign					ഈ
+ു	u	u	u	u	u	vowel sign					ഉ
+ൂ	u	ū	u	ū	uː	vowel sign					ഊ
+െ	e¶	e	e¶	e	e	vowel sign					എ
+േ	e	ē	e	ē	eː	vowel sign					ഏ
+ൊ	o	o	o	o	o	vowel sign					ഒ
+ോ	o	ō	o	ō	oː	vowel sign					ഓ
+ൈ	a	aʲ	a	ai	ai̯	vowel sign					ഐ
+ൗ	a	aʷ	a	au	au̯	vowel sign					ഔ
+ൌ	a	ȧʷ	a	au	au̯	historic vowel sign			archaic		
+											
+											
+ൃ	R	r̥	R	r̥	rɨ	vowel sign					ഋ
+ൄ	R	r̥̄	R	r̥̄		vowel sign			very rare		ൠ
+ഋ	R	r̥̣	R	r̥̣	rɨ	independent vowel				ൃ	
+ൠ	R	r̥̣̄	R	r̥̣̄	riː	independent vowel			very rare	ൄ	
+ൢ	L	l̥	L	l̥	lu	vowel sign			very rare		ഌ
+ൣ	L	l̥̄	L	l̥̄		vowel sign			very rare		ൡ
+ഌ	L	l̥̣	L	l̥̣		independent vowel			(in one Sanskrit word only)	ൢ	
+ൡ	L	l̥̣̄	L	l̥̣̄		independent vowel			very rare	ൣ	
+											
+											
+൦	0	0	0	0		digit					
+൧	1	1	1	1		digit					
+൨	2	2	2	2		digit					
+൩	3	3	3	3		digit					
+൪	4	4	4	4		digit					
+൫	5	5	5	5		digit					
+൬	6	6	6	6		digit					
+൭	7	7	7	7		digit					
+൮	8	8	8	8		digit					
+൯	9	9	9	9		digit					
+൰	§	{10}				number sign					
+൱	§	{100}				number sign					
+൲	§	{1000}				number sign					
+൳	§	¼				number sign					
+൴	§	½				number sign					
+൵	§	¾				number sign					
+											
+											
+											
+											
+											
+്	*	͓				virama	chandrakkala				
+`
+
+var cols = {
+"transLoc": 2,
+"ipaLoc": 5,
+"typeLoc": 6,
+"nnameLoc": 8,
+"nameLoc": 7,
+"numLoc": 0,
+"statusLoc": 9,
+"dvowel": 10,
+"ivowel": 11,
+"subj":0,
+"ltone":0,
+"htone":0,
+"numLoc":0,
+"meaning":0,
+"shape":0,
+"lc":0,
+"uc":0,
+"transcription":4,
+
+"othertranscriptions": [[4, 'ISO 15919']]
+}
