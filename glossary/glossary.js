bc212d17f30d06cfe36da3e422c8a703f26c7688
r12a <ishida@w3.org>
2019-12-03 08:29:22 +0000

    glossary/glossary.js

---
+++ glossary/glossary.js
@@ -0,0 +1,7 @@
+defList={
+"independentvowel": 'An <a class="termref" target="_blank" href="../glossary/#inherentvowel">inherent vowel</a> is a vowel sound that is automatically pronounced after a consonant letter, unless specifically suppressed.',
+"vowelsign": '<a class="termref" target="_blank" href="../glossary/#vowelsign">Vowel-signs</a> are shapes that appear alongside a consonant to express a following (non-inherent) vowel sound.  Sometimes multiple shapes are used, on various sides of the base consonant. These are known as <span class="name">\'matras\'</span> in Sanskrit.',
+"standalonevowel": 'Standalone vowels are not preceded by a consonant, and may appear at the beginning or in the middle of a word.',
+"grapheme": 'A grapheme is a user-perceived unit of text. The Unicode Standard uses generalised rules to define \'grapheme clusters\', which approximate the likely grapheme boundaries in a writing system.',
+"wordboundary": 'The concept of \'word\' is difficult to define in any language (see <a href="https://www.w3.org/International/articles/typography/linebreak.en#whatisword">What is a word?</a>). Here, a word is a vaguely-defined, but recognisable  semantic unit that is typically smaller than a phrase and may comprise one or more syllables.',
+}
