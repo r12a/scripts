985d23516d3a622efab09a8055a294ed20887dcc
r12a <ishida@w3.org>
2019-12-09 16:34:58 +0000

    content: Add definitions for conjunct, consonant cluster, & vocalic

--- glossary/glossary.js
+++ glossary/glossary.js
@@ -1,7 +1,19 @@
 defList={
+"conjunct": 'When the the shapes of constituent consonants in a cluster are changed or merged to indicate a lack of intervening vowels, this is referred to as a <a class="termref" target="_blank" href="../glossary/#conjunct">conjunct</a>. Not all consonant clusters are displayed as conjuncts.',
+
+"conjunct": 'A <a class="termref" target="_blank" href="../glossary/#conjunct">conjunct</a> is a consonant cluster where the lack of intervening vowels is indicated by one or more of stacking, changing and merging the shapes of the constituent letter forms (usually in abugidas). Not all consonant clusters are displayed as conjuncts.',
+
+"consonantcluster": 'A consonant cluster is a sequence of consonants with no intervening vowels.',
+
 "independentvowel": 'An <a class="termref" target="_blank" href="../glossary/#inherentvowel">inherent vowel</a> is a vowel sound that is automatically pronounced after a consonant letter, unless specifically suppressed.',
-"vowelsign": '<a class="termref" target="_blank" href="../glossary/#vowelsign">Vowel-signs</a> are shapes that appear alongside a consonant to express a following (non-inherent) vowel sound.  Sometimes multiple shapes are used, on various sides of the base consonant. These are known as <span class="name">\'matras\'</span> in Sanskrit.',
+
 "standalonevowel": 'Standalone vowels are not preceded by a consonant, and may appear at the beginning or in the middle of a word.',
+
 "grapheme": 'A grapheme is a user-perceived unit of text. The Unicode Standard uses generalised rules to define \'grapheme clusters\', which approximate the likely grapheme boundaries in a writing system.',
+
+"vocalic": 'Vocalics are letters derived from Sanskrit that generally behave like vowels, but represent r/l followed by a vowel. They are often available both as vowel-signs and independent vowel letters.',
+
+"vowelsign": '<a class="termref" target="_blank" href="../glossary/#vowelsign">Vowel-signs</a> are shapes that appear alongside a consonant to express a following (non-inherent) vowel sound.  Sometimes multiple shapes are used, on various sides of the base consonant. These are known as <span class="name">\'matras\'</span> in Sanskrit.',
+
 "wordboundary": 'The concept of \'word\' is difficult to define in any language (see <a href="https://www.w3.org/International/articles/typography/linebreak.en#whatisword">What is a word?</a>). Here, a word is a vaguely-defined, but recognisable  semantic unit that is typically smaller than a phrase and may comprise one or more syllables.',
 }
