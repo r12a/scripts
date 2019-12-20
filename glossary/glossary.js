67e54440719eaf2d03f75ec1a5de1dce3d830e89
r12a <ishida@w3.org>
2019-12-20 05:08:30 +0000

    Content: glossary, + checked/unchecked, + added circumgraph to vowel-sign definition

--- glossary/glossary.js
+++ glossary/glossary.js
@@ -1,4 +1,6 @@
 defList={
+"checked": '\'Checked\' means ending in the sound <span class="ipa">-p</span>, <span class="ipa">-t</span>, <span class="ipa">-k</span>, or <span class="ipa">-ʔ</span>. \'Unchecked\' means ending in  sonorants <span class="ipa">m, n, ŋ, w, j</span> or vowels.</p>',
+
 "conjunct": 'When the the shapes of constituent consonants in a cluster are changed or merged to indicate a lack of intervening vowels, this is referred to as a <a class="termref" target="_blank" href="../glossary/#conjunct">conjunct</a>. Not all consonant clusters are displayed as conjuncts.',
 
 "conjunct": 'A <a class="termref" target="_blank" href="../glossary/#conjunct">conjunct</a> is a consonant cluster where the lack of intervening vowels is indicated by one or more of stacking, changing and merging the shapes of the constituent letter forms (usually in abugidas). Not all consonant clusters are displayed as conjuncts.',
@@ -11,9 +13,11 @@ defList={
 
 "grapheme": 'A grapheme is a user-perceived unit of text. The Unicode Standard uses generalised rules to define \'grapheme clusters\', which approximate the likely grapheme boundaries in a writing system.',
 
+"unchecked": '\'Checked\' means ending in the sound <span class="ipa">-p</span>, <span class="ipa">-t</span>, <span class="ipa">-k</span>, or <span class="ipa">-ʔ</span>. \'Unchecked\' means ending in  sonorants <span class="ipa">m, n, ŋ, w, j</span> or vowels.</p>',
+
 "vocalic": 'Vocalics are letters derived from Sanskrit that generally behave like vowels, but represent r/l followed by a vowel. They are often available both as vowel-signs and independent vowel letters.',
 
-"vowelsign": '<a class="termref" target="_blank" href="../glossary/#vowelsign">Vowel-signs</a> are shapes that appear alongside a consonant to express a following (non-inherent) vowel sound.  Sometimes multiple shapes are used, on various sides of the base consonant. These are known as <span class="name">\'matras\'</span> in Sanskrit.',
+"vowelsign": '<a class="termref" target="_blank" href="../glossary/#vowelsign">Vowel-signs</a> are shapes that appear alongside a consonant to express a following (non-inherent) vowel sound.  These are known as <span class="name">\'matras\'</span> in Sanskrit. Sometimes multiple shapes are used, on various sides of the base consonant (circumgraphs). ',
 
 "wordboundary": 'The concept of \'word\' is difficult to define in any language (see <a href="https://www.w3.org/International/articles/typography/linebreak.en#whatisword">What is a word?</a>). Here, a word is a vaguely-defined, but recognisable  semantic unit that is typically smaller than a phrase and may comprise one or more syllables.',
 }
