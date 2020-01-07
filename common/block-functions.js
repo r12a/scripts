7f5f828c8238483b58bcb531f20c5f277140a84d
r12a <ishida@w3.org>
2020-01-07 07:43:20 +0000

    common/block, Add header/footer code

--- common/block-functions.js
+++ common/block-functions.js
@@ -2,14 +2,57 @@
 /* globals cl, langs */
 
 
-function initialise (base) {
+function initialise (base, dir) {
     createtoc(3)
+	doHeadersFooters(dir)
     initialiseShowNames(base, '')
     checkForCharParam()
     addLangInfo()
     listCharsWithNotes()
     addUnicodeNotes()
     }
+
+
+
+function doHeadersFooters (dir) {
+	// adds links to top of document
+	// dir is of the form arabic/index or arabic/urdu
+	
+	if (document.getElementById('versionTop') === null) return
+	
+
+	//parse the dir
+	var filename = ''
+	var directory = ''
+	var path = dir.split('/')
+	directory = path[0]
+	if (path.length === 1) {
+		filename = path[0]
+		}
+	else {
+		filename = path[1]
+		}
+		
+
+	var out = '&bull; recent changes <a target="_blank" href="https://github.com/r12a/scripts/commits/gh-pages" title="Show commits for the whole scripts repository.">scripts</a>/<a target="_blank" href="https://github.com/r12a/scripts/commits/gh-pages/'+directory+'" title="Show commits for scripts/'+directory+'.">'+directory+'</a>'
+	
+	out += '/<a target="_blank" href="https://github.com/r12a/scripts/commits/gh-pages/'+directory+'/block.html" title="Show commits for scripts/'+filename+'/block.">block</a>'
+	
+	out += ' &bull; leave a <a target="_blank" href="https://github.com/w3c/i18n-drafts/issues/new?title=['+dir.replace(/index/,'')+'/block]%20%20BRIEF_TITLE_GOES_HERE&body=%5Bsource%5D%20https%3A%2F%2Fr12a.github.io%2Fscripts%2F'+dir+'/block%0A%0A" title="Leave a comment.">comment</a>'
+	
+	document.getElementById('versionTop').innerHTML = out
+	
+	
+	out = ''
+	out += 'See <a target="_blank" href="https://github.com/r12a/scripts/commits/gh-pages/'+directory+'/block.html">recent changes</a>. &nbsp;&bull;&nbsp; Make a <a href="https://github.com/r12a/scripts/issues/new?title=%5B'+dir+'%2Fblock%5D%20TITLE_GOES_HERE&body=Comment%20on%20http%3A%2F%2Fr12a.github.io%2Fscripts%2F'+directory+'%2Fblock%0A%0A" target="_blank">comment</a>. &nbsp;&bull;&nbsp; Licence <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">CC-By</a> © <a href="mailto:r12a@w3.org">r12a</a>.</span></div>'
+	
+	
+	document.getElementById('version').innerHTML = out
+	}
+
+
+
+
 
 function checkForCharParam () {
 	// check for parameters; if char= found with value, set style to show only that character
