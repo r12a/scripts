b5c6b6b4df0529491f8583feb190dc5216e19541
r12a <ishida@w3.org>
2020-06-21 11:27:44 +0100

    cans, Add auto examples mechanism

---
+++ cans/examples.js
@@ -0,0 +1,73 @@
+var examples = `
+ᐃᓄᒃᑎᑐᑦ‎|Inuktitut|inuktitut‎
+ᐊᐃᕕᖅ|walrus|aiviq
+ᑐᒃᑐ|caribou|tuktu
+ᑕᐃᓇ|that one|taina
+ᐅᖃᖅᑐᖅ|she speaks|uqaqtuq
+ᐅᒥᐊᖓ|his boat|umiaŋa (umianga)
+ᐅᒥᐊᙳᐊᖅ|toy boat|umiaŋŋuaq (umiannguaq)
+ᐃᒡᓗ|house|iglu
+ᐊᐅᓪᓚᓛᖅ ᑐᑦ|they will depart|aullalaːq tut (aullalaaq tut)
+ᖃᔭᖅ|kayak|qajaq
+ᐃᒡᓗᕐᔪᐊᖅ|big house|igluʁjuaq (iglurjuaq)
+ᑯᒡᔪᐊᖅ|big river|kugjuaq
+ᑕᓯᕐᔪᐊᖅ|big lake|tasiʁjuaq (tasirjuaq)
+ᑲᑎᕝᕕᒃ/ᑲᑎᕕᒃ|meeting-place|kativvik/kativik|(latter in Nunavik & S/E Baffin)
+ᐱᓱᒡᕕᒃ|walkway|pisugvik
+ᓂᐅᕕᕐᕕᒃ|store|niuviʁvik (niuvirvik)
+ᓄᓇ|land|nuna
+ᓄᓇᒧᑦ|to the land|nunamut
+ᓄᓇᒥ|on the land|nunami
+ᑐᑭᓯᔪᖓ|I understand|tukisijuŋa (tukisijunga)
+ᑐᑭᓯᕕᑦ?|do you understand?|tukisivit?
+ᓈᓚᒃᑐᖓ|I listen|naːlaktuŋa (naalaktunga)
+ᓈᓚᒃᐱᑦ?|are you listening?|naːlakpit? (naalakpit?)
+ᐊᑖᑕ|father|ataːta (ataata)
+ᐊᑖᑕᒐ|my father|ataːtaga (ataataga)
+ᐸᓂᒃ|daughter|panik
+ᐸᓂᒐ|my daughter|paniga
+ᐃᕐᓂᖅ|son|iʁniq|irniq
+ᐃᕐᓂᕋ|my son|iʁniʁa (irnira)
+ᐃᓕᓴᐃᔨ|teacher|ilisaiji
+ᓂᐅᕕᖅᑎ|trader|niuviqti
+ᐊᓂᔪᒪᔪᖓ|I want to go out|anijumajuŋa (anijumajunga)
+ᐃᖏᑦᑐᒪᔪᖓ|I want to sit down|iŋittumajuŋa (ingittumajunga)
+ᓯᓂᒍᒪᔪᖓ|I want to sleep|sinigumajuŋa (sinigumajunga)
+ᐅᑎᕈᒪᔪᖓ|I want to return|utiʁumajuŋa (utirumajunga)
+ᐅᒥᐊᖅ|boat|umiaq
+ᐅᒥᐊᕐᔪᐊᖅ|big boat, ship|umiaʁjuaq (umiarjuaq)
+ᐅᒥᐊᕐᔪᐊᕌᓗᒃ|big ship|umiaʁjuaʁaːluk (umiarjuaraaluk)
+ᑐᒃᑐᐃᑦ|caribou pl.|tuktuit
+ᐃᓄᒃ|person|inuk
+ᐃᓄᐃᑦ|people|inuit
+ᕿᒻᒥᖅ|dog|qimmiq
+ᕿᒻᒦᑦ|dogs|qimmiːt (qimmiit)
+ᐅᒥᐊᑦ|boats|umiat
+ᐃᓅᒃ|two people|inuːk (inuuk)
+ᑐᐱᖅ|tent|tupiq
+ᑐᐲᒃ|two tents|tupiːk (tupiik)
+ᐅᒥᐊᒃ|two boats|umiak
+ᓇᑦᓯᖅ|seal|natsiq
+ᑕᑯᒐᑉᓯ/ᑕᑯᒐᑦᓯ|because you (pl) see|takugapsi/takugatsi|normal/SBaffin & Nunavik
+ᐃᒃᓯᕙᐅᑕᖅ/ᐃᑦᓯᕙᐅᑕᖅ|chair|iksivautaq/itsivautaq|normal/SBaffin & Nunavik
+ᑐᙵᓱᒋᑦ|Welcome!|tuŋŋasugit (tunngasugit)
+ᐃᓄᒃᑎᑑᓲᖑᕕᑦ?|Do you speak Inuktitut?|inuktituːsuːŋuvit? (inuktituusuunguvit?)
+ᐊᕕᙵᙳᐊᖅ|computer mouse|aviŋŋaŋŋuaq (avinngannguaq)
+ᐊᑦᑐᓈᖅ|rope|attunaːq (attunaaq)|South Qikiqtaaluk
+ᐊᒃᓱᓈᖅ|rope|akhunaːq (aksunaaq)|Inuinnaqtun
+ᐊᒃᖢᓈᖅ|rope|akɬunaːq (akɫunaaq)|North Qikiqtaaluk
+ᖃᑉᓗ|eyebrow|qablu|Inuinnaqtun
+ᖃᓪᓗ|eyebrow|qallu|South Qikiqtaaluk
+ᒪ'ᓇ|thank you|maʔna|Paallirmiutut
+ᓇᑯᕐᒦᒃ|thank you|nakuʁmiːk (nakurmiik)| South Qikiqtaaluk
+ᑲᖏᖅᑐᒃ|inlet, fjord|kaŋiqtuk
+ᑭᒍᑕᖕᒋᕐᓇᖅ|blueberry|kigutaŋgiʁnaq (kigutanggirnaq) 
+ᓂᔾᔭᔪᑦ|music|nid͡ʒajut|nijjajut
+ikiaqqivik|website|ikiaqqivik
+ᑭᓴᖅ|anchor|kihaq|Paallirmiutut
+ᑭᓴᐅᑦ|anchor|kisaut|South Qikiqtaaluk
+ᐆᒪ|this one's|uːma (uuma)
+ᐃᓄᖕᒧᑦ|to a person|inuŋmut (inungmut)
+
+`
+
