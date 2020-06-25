ce9d94521eb051b5f1e21f658e9ffe025d90bfee
r12a <ishida@w3.org>
2020-06-25 18:34:54 +0100

    Use new markup for examples, which are generated at run time. And fix templates for intros.

---
+++ bengali/examples.js
@@ -0,0 +1,157 @@
+var examples = `
+
+অঙ্ক|mathematics|ɔŋko
+অনলাইন|online|ɔn.lai̯n
+অভিধান|dictionary|obʰidʰɑn
+অ্যাটর্নি|attorney|ɔæʈorni
+আকাশ|sky|ɑkɑʃ
+আজিজ|Aziz|aziz
+আয়রন|iron|ae̯.rɔn
+আষাঢ়|June-July|ɑʃɑɽ
+আস্থা|trust|ɑʃtʰɑ
+ইংরেজ|English|iŋreʤ
+ঈদ|Eid|iːd
+উঁচু|height|ũt͡ʃu
+উচ্চারণ|pronunciation|ut͡ʃɑrɔn
+উচ্চারন|pronunciation|ut͡ʃt͡ʃɑrɔn
+উন্মত্ত|insane|unmɔtto
+উৎসব|festival|utʃɔb
+ঋতু|season|ritu
+একটু|a little|ekʈu
+একবার|once|ækbɑr
+একাউন্ট|account|æ.kau̯nʈ
+এছাড়াও|also|e.t͡ʃʰa.rao̯
+এ্যাডভোকেট|advocate|eæɖbʰokeʈ
+ওই|demonstrative|oi̯
+ওড়া|to fly|oɽɑ
+ওদিকে|in that direction|odike
+ঔষুধ|medecine|owʃudʰ
+কড়া|strict|kɔɽa
+করতাল|cymbal|kɔrtɑl
+করা|do|kɔɾa
+কলম|pen|kɔlom
+কাটা|to cut|kɑʈɑ
+কারসাজি|trickery|kɑrʃɑʤi
+কিন্তু|but|kintu
+কুর্তা|Indian shirt|kurtɑ
+কেউ|somebody|keu̯
+কেন|why|kæno
+কেমন|how|kæmɔn
+কোকিল|cuckoo|kukil
+ক্যম্পাস|campus|kyæmpas
+ক্ষণ|moment|kʃon
+ক্ষুদ্র|small|kʰudro
+খবর|news|kʰɔbor
+খাওয়া|to eat, drink|kʰao̯wa
+খ্রিষ্টান|christian|kʰriʂʈan
+গতকাল|yesterday|gɔtokɑl
+গন্ধ|scent|gɔndʰo
+গ্রাম|village
+ঘর|room, house|gʰɔr
+চক্র|wheel|t͡ʃɔkro
+চাঁদ|moon
+ছাতা|umbrella|t͡ʃʰɑtɑ
+জগৎ|world|ʤɔɡot
+জন্য|for|ʤɔnno
+জাকাত|Zakah charity|zakat
+জ্ঞান|knowledge|ɡæn
+জ্বর|fever|ʤɔr
+ঝড়|storm|ʤʰɔɽ
+ঝিল্লি|grasshopper|ʤʰilli
+টাকা|money|ʈɑkɑ
+ঠাণ্ডা|cold|ʈʰɑnɖɑ
+ডাক্তার|doctor|ɖɑktɑr
+ঢেউ|wave|ɖʰeu
+তওবার|repentance|tɔo̯.bar
+তাথৈ|there|ta.tʰoi̯ (terị)
+তারা|star|tɑrɑ
+তেরই|thirteen|tæ.roi̯
+তৈরি|to make|toi̯ri
+ত্যাগ|leave|tɛg
+থামা|to stop|tʰɑmɑ
+দাদী|grandmother|dɑdiː
+দুঃখ|sorrow|dukkʰo
+দুঃখও|sadness|duk.kʰoo̯
+ধন্যবাদ|thank you|dʰonjobɑd
+ধৌত|wash|dʰou̯.to
+নদী|river|nodiː
+নম্বর|number|nɔmbor
+নামাজ|prayers
+নিঃশব্দ|silence|niʃʃɔbdo
+নোংরা|foul
+নৌকো|boat|nou̯ko
+পথ|path, road|pɔtʰ
+পাখিই|birds|pa.kʰii̯
+পারফিউম|perfume|par.fiu̯m
+পারেই|maybe|pa.rei̯
+ফটো|photo|pfoʈo
+ফ্‌ল্যাট|flat|pʰlæʈ
+বই|book|boi̯
+বড়|big|bɔɽo
+বর্ণ|colour|bɔrno
+বাংলা|Bengali (language)|bɑŋlɑ
+বাঃ|left|bɑːɦ
+বাঙালী|Bengali|bɑŋɡɑliː
+বাবুই|baby|ba.bui̯
+বিজ্ঞান|science|biɡɡæn
+বিড়াল|cat|beɽɑl
+বিড়ি|cigarette|biɽi
+বিশ্ব|universe|biʃʃo
+বিষ্ণু|Vishnu|biʃnu
+বীর|hero|biːr
+বুক|breast, chest|buk
+বুড়ো|old man|buɽo
+বৃহৎ|huge|brihɔt
+বেগ|speed|beg
+বেগুন|eggplant|begun
+বেলা|time of day|bælɑ
+বোকা|stupid|bɔkɑ
+বোন|sister|bon
+ব্যক্তি|person|bekti
+ব্যথা|pain|bætʰɑ
+ব্যাংক|bank|bæŋk
+ব্যাখ্যা|explanation|bækkʰa
+ব্যাঙ্ক|bank|bæŋk
+ভয়|fear|bʰɔe̯
+ভস্ম|ashes|bʰɔ̃ʃʃo
+ভাই|brother|bʰɑi
+ভাল|good|bʰɑlo
+ভুরু|eyebrow|bʰuru
+মজার|funny|mɔʤɑr
+মূল|root|muːl
+মেয়ে|girl, woman|mee
+যখন|when|ʤɔkʰon
+যাত্রা|journey|ʤɑtrɑ
+যুদ্ধ|war|ʤuddʰo
+যেকেউ|anyone|d͡ʒe.keu̯
+রং|colour|rɔŋ
+রওয়া|to remain|rowɑ
+রাঢ়|Radha
+রিকশা|rickshaw|rikʃɑ
+রুগী|ill, sick|ruɡiː
+রূপ|figure, form|ruːp
+লওয়া|to take|lɔowɑ
+লান্‌চ|lunch|lɑnt͡ʃ
+লিফ্ট|lift, elevator
+শাপ|curse|ʃɑp
+শুধু|only|ʃudʰu
+ষড়যন্ত্র|conspiracy|ʃɔɽod͡ʒɔntro (ɔ)
+সকাল|morning|ʃɔkɑl
+সন্ধ্যা|evening|ʃonddʰa
+সম্বন্ধে|about
+সাংঘাতিক|terrible|ʃɑŋɡʰɑtik
+স্কুল|school
+স্বামী|husband|ʃɑmiː
+স্মৃতি|memories|sriti
+স্মৃতি|memory|sriti
+সৎমা|step-mother|ʃɔtmɑ
+হঠাৎ|suddenly|hɔʈʰɑt
+হরিণ|deer|horin
+হল|(student) hall|hɔl
+হাঁপান|to pant|hɑ̃pɑn
+হৃদয়|heart|hridɔj
+হোয়াটসঅ্যাপ|WhatsApp|hoyɑʈʃɔæp
+হ্যাঁ|yes|hæ̃
+
+`
+
