55d6db78e3780d97ec358c2e693b5c039dfd51be
r12a <ishida@w3.org>
2020-06-25 07:13:05 +0100

    Use new markup for examples, which are generated at run time.

---
+++ sundanese/examples.js
@@ -0,0 +1,18 @@
+var examples = `
+
+ᮃᮀᮊᮥᮒᮔ᮪|transport|(angkutan)
+ᮃᮌᮢᮤᮊᮥᮜ᮪ᮒᮥᮁ|agriculture|(agrikultur)
+ᮃᮘᮂ-ᮃᮘᮂ|goods|(abah-abah)
+ᮃᮜᮥᮔᮺᮌᮥᮀ|tidal wave|(alun-agung)
+ᮄᮊᮣᮤᮙ᮪|climate|(iklim)
+ᮅᮃᮕ᮪|steam|(uap)
+ᮆᮜ᮪ᮙᮥ|science|(élmu)
+ᮌᮨᮜᮧᮙ᮪ᮘᮀ ᮙᮤᮊᮢᮧ|microwave|(gelombang mikro)
+ᮕᮞᮤᮁ|hill|(pasir)
+ᮙᮀᮌᮥ|mangosteen|(manggu)
+ᮛᮦᮌᮀ|stick|(régang)
+ᮮᮥᮯᮥ|state of grace|(xušu خاشع)
+ᮃᮊ᮪ᮞᮛ|syllable|(aksara)
+
+`
+
