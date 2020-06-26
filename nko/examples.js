d063835af2e9d1e4e71bef885fbfb2a3204e63dc
r12a <ishida@w3.org>
2020-06-26 07:48:59 +0100

    Use new markup for examples, which are generated at run time. And fix templates for intros.

---
+++ nko/examples.js
@@ -0,0 +1,17 @@
+var examples = `
+߁߭|first
+߂߲|second
+ߊ߲ ߧߴߌ ߝߏ߬ ߟߊ߫ ߸ ߞߵߌ ߞߎߟߎ߲ߖߋ߲.|We give you thanks.
+ߓߐߗ߭ߎߙ. ߓߌߢߍ߲ ߝ߭ߋߣߎ߳.|Bonjour. Bien venue.|(bɔʒur. bi‌ñɛ̃ veny)
+ߕߏ߲ߜߊ|litre
+ߘߋ߲ߣߍ߲ ߠߎ߬ ߟߊ߫ ߓߋߟߋ߲ߠߊ|a speech from the children|dẽnɛ̃ łu la belẽła
+ߛߌ ߺ ߌ ߺ ߛߌ|anyone
+ߛߌߘߐ|gram
+ߜߟߊ߬ߗߡߍ߬ߝߘߎ߬ߓߍ߲|cm²
+ߜߟߊ߬ߥߊ߰ߘߋ߲|millimetre
+ߞߎߘߍ|kilometer
+ߡߐ߰ ߺ ߐ ߺ ߡߐ߰|everyone
+߁߂߃߲|123rd
+
+`
+
