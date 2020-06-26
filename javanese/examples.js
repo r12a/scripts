1200e7ddfa310dc335ed4dfb89fdf9c45315e762
r12a <ishida@w3.org>
2020-06-26 06:57:31 +0100

    Use new markup for examples, which are generated at run time. And fix templates for intros.

---
+++ javanese/examples.js
@@ -0,0 +1,28 @@
+var examples = `
+:꧑꧕:ꦎꦏ꧀ꦠꦺꦴꦧꦼꦂ:꧒꧐꧑꧒:|15 october 2012
+ꦄꦪꦸ|Ayu (personal name)
+ꦊꦩꦃꦊꦩ꧀ꦧꦸꦠ꧀|gentle|(lemah lembut)
+ꦏꦿꦸꦥꦸꦏ꧀|crackers|(krupuk)
+ꦒꦧꦃ|grain of rice|(gabah)
+ꦒꦧꦲꦺ|that grain of rice|(gabahe)
+ꦒꦼꦭꦱ꧀ꦉꦱꦶꦏ꧀|clean glass|(gelas resik)
+ꦢꦲꦂ|eat|(dahar)
+ꦢꦲꦫ|dahara|(dhr)
+ꦣꦂꦩ|dharma
+ꦥꦼꦠꦼꦁ|dark|(peteng)
+ꦥꦼꦫꦲꦸꦭꦪꦂ|sailboat|(perahu layar)
+ꦥꦿꦩꦸꦏ|scout|(pramuka)
+ꦧꦶꦱ꧀ꦠꦿꦤ꧀​ꦱ꧀ꦗꦏꦂꦠ|Transjakarta bus|(bis transjakarta)
+ꦧꦸꦏꦸꧏ|books|(buku-buku)
+ꦩꦧꦸꦫꦸ|hunting
+ꦫ꧈ꦩ꧈ꦯꦸꦭꦂꦠ|R. M. Sularta
+ꦭꦠꦲꦤ꧀ꦚꦚꦶ|singing practice|(latihan nyanyi)
+ꦱꦼꦏꦺꦴꦭꦃ|school|(sekolah)
+ꦱꦼꦭꦱ꧇꧑꧙꧇ꦩꦉꦠ꧀꧇꧒꧐꧑꧓꧇|Tuesday, 19 March 2013|(selasa 19 maret 2013)
+ꦲꦏ꧀ꦱꦫ|alphabet|(aksara)
+ꦲꦪꦸ|graceful|(ayu)
+ꦲꦸꦧꦶꦉꦧꦸꦱ꧀|boiled tubers|(ubi rebus)
+꧈ꦢꦺ꧈ꦲ꧈ꦌꦭ꧀꧈|DHL
+
+`
+
