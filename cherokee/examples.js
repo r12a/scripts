9b55b66521f1b29292485d851f2e476d7276d6fa
r12a <ishida@w3.org>
2020-06-26 05:55:40 +0100

    Use new markup for examples, which are generated at run time. And fix templates for intros.

---
+++ cherokee/examples.js
@@ -0,0 +1,16 @@
+var examples = `
+ᎠᎹ|salt|aːma
+ᎠᎹ|water|ama
+ᎦᎵᏉᎩ|seven|kaɬkʷoːki
+ᎧᎦᎵ|February|kʰaːkaʔli
+ᎬᎾ|I'm alive|kə̃ːna
+ᎬᎿ|she's alive|kə̃ːhna
+ᎬᏙᎠ|it's hanging|ktʰoːʔa
+ᎯᎠ|this|hiʔa
+ᎯᏴᏫᏯᏍ|Are you a Native American?|hijə̃ːwiːjaːs
+ᏍᎪᎯ|ten|skoːhi
+ᏑᏗ|fishhook|suhti
+ᏔᎵ|two|tʰaʔli
+
+`
+
