a40ea8c57bb07742d08885d9de617cc1a7623dff
r12a <ishida@w3.org>
2020-06-28 15:27:07 +0100

    Use new markup for examples, which are generated at run time. And fix templates for intros.

---
+++ gurmukhi/pa-examples.js
@@ -0,0 +1,22 @@
+autoExpandExamples.pa = `
+
+ਉਤਸੁਕ|curious|utsuk
+ਕਹਿਣਾ|to say|kǽɳɑ
+ਕਿਹੜਾ|who|kéɽɑ
+ਕੁਝ|something|kúʤ
+ਕੁਹੜਾ|leper|kóɽɑ
+ਘੋੜਾ|horse|kòɽɑ
+ਚੜ੍ਹ|climb|ʧə́ɽ
+ਪਘਾਰਨਾ|to melt|pəɡɑ̀rnɑ
+ਪ੍ਰਬੰਧ|management, government|prəbə́nd̪
+ਪੱਕੀ|ripe|pəkki
+ਮੀਹ|rain|mí
+ਮੂੰਡਾ|boy|muɳɖɑ
+ਲੰਮੀ|long|ləmmi
+ਵਹੁਟੀ|bride|wɔ́ʈi
+ਸ਼ਾਂਤ|peaceful|ʃɑ̃t
+ਸ੍ਵਰਗ|heaven|svərəg
+ਹਰੀ|green|həri
+
+`
+
