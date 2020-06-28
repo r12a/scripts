ba6677f8d05e31f0247c7a9f90d505d272a8ffc6
r12a <ishida@w3.org>
2020-06-28 07:57:51 +0100

    Use new markup for examples, which are generated at run time. And fix templates for intros.

---
+++ mongolian/mn-examples.js
@@ -0,0 +1,23 @@
+autoExpandExamples.mn = `
+ᠡᠳᠦᠭᠡ|now
+ᠪᠡᠯᠡᠭ|gift
+ᠬᠠᠨ᠎ᠠ|wall of a tent
+ᠬᠠᠨᠠ|outer casing of a vein
+ᠬᠠᠭᠠᠨ|Kahn
+ᠭ᠋ᠠᠯ|fire
+ᠭᠠᠵᠠᠷ|place
+ᠭᠠᠵᠠᠷ ᠠ|to the country
+ᠭᠦᠮᠦᠨ|person
+ᠰᠠᠮ|comb|sam
+ᠰᠠᠶ᠋ᠢᠨ/ᠰᠠᠶᠢᠨ|good
+ᠰᠡᠮ|quietly|səm
+ᠴᠢᠷᠢᠭ᠌|soldier
+ᠵᠠᠷᠯᠢᠭ|order
+ᠵᠣᠷ|good fortune, luck
+ᠵᠣᠷᠢᠭᠲᠠᠢ|to be brave
+ᠵᠣᠷᠲᠠᠢ|lucky
+ᠸᠢᠺᠢᠫᠧᠳᠢᠶᠠ|Wikipedia
+ᠼᠠᠭᠠᠨ|white
+
+`
+
