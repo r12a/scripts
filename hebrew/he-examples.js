2432d6298ffd28d756591f5a17e2429827f045c6
r12a <ishida@w3.org>
2020-06-28 14:17:38 +0100

    Use new markup for examples, which are generated at run time. And fix templates for intros.

---
+++ hebrew/he-examples.js
@@ -0,0 +1,49 @@
+autoExpandExamples.he = `
+
+
+אִם|if|ʔim
+בִּי|in me|bi
+בֵּן|son|bɛn
+גְּבֶרֶת|Mrs.
+גַּג|roof|gag
+ג׳וּק|roach|ʤuk
+ד׳ו אל-חיג׳ה|Dhu al-Hijjah (ذو الحجة)
+הֵד|echo|hɛd
+הוּא|he|hu
+זֶה|this|ze
+זָ׳רְגוֹן|jargon|ʒarɡon
+חַם|hot|χam
+טוֹב|good|tɔv
+יָם|sea|yam
+לִי|to me|li
+לוֹ|to him|lo
+מֵידָע|information|mɛda
+מוֹעִיל|useful|moʔil
+מוּם|defect|mum
+נִין|grandson|nin
+סְכָךְ|branch-roofing|sχaχ
+סוֹף|end|sof
+עִיר|city|iʁ
+עַדְלֹאיָדַע|Purim-parade|adlɔjada
+עָוָנְטָה|boastful act|awanta
+פִסְפֵס|missed|fisfɛs
+פֹּה|here|po
+צִיץ|bud|ʦiʦ
+קָט|tiny|kat
+קוֹל|sound|kɔl
+רְגוֹן|jargon|ʒarɡon
+רִאשׁוֹן|first|riʃɔn
+ריצ׳רץ׳|zip|ʁiʧʁaʧ
+ר׳ג׳ר|Ghajar
+שָׁאַל|asked|ʃaʔal
+שָׁם|there|ʃam
+שָׂם|put|sam
+שׁוֹפּ|shop|ʃop
+שייח|Sheikh (شيخ)
+שייח׳|Sheikh (شيخ)
+תּוּת|strawberry|tut
+תל־אביב|Tel Aviv
+ת׳רסטון|Thurston
+
+`
+
