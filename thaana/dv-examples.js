d20a7ccef0681e9f69cabae8966ae418d642a978
r12a <ishida@w3.org>
2020-06-28 13:47:39 +0100

    Use new markup for examples, which are generated at run time. And fix templates for intros.

---
+++ thaana/dv-examples.js
@@ -0,0 +1,17 @@
+autoExpandExamples.dv = `
+
+
+ރަށަކީ|the island is|raʂakeː
+ރަށެއް|an island|raʂeh
+ރަށް|island|raʔ
+ރަތް|rat|rajʔ
+ބައްޓެއް|eggplant|baʈʈeʔ
+ކަނޑު|sea|kaⁿɖu
+އަށްޑިހަ|eighty|aɖɖiha
+އަތްޕުޅު|hand|ajppuɭu
+އިރުގައި|time|irugai
+އެންމެ|only|emme
+ފައް|sawtooth|faʔ
+
+`
+
