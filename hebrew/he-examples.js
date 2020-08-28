f986b587d6c4aff8419c9cad6cc48c27c316e21a
r12a <ishida@w3.org>
2020-08-28 13:10:51 +0100

    Apply new ordering for notes and alt

--- hebrew/he-examples.js
+++ hebrew/he-examples.js
@@ -9,106 +9,112 @@
 */
 autoExpandExamples.he = `
 
-אם|if|ʔim||אִם
-בּי|in me|bi||בִּי
+אם|if|ʔim|אִם
+בּי|in me|bi|בִּי
 בִּי|in me|bi
-בּן|son|ben||בֵּן
-בּן|son|bɛn||בֵּן
-גּברת|Mrs.|||גְּבֶרֶת
-גּג|roof|gag||גַּג
+בּן|son|ben|בֵּן
+בן|son|bɛn|בֵּן
+גּברת|Mrs.||גְּבֶרֶת
+גּג|roof|gag|גַּג
+גג|roof|gag|גַּג
 ג׳וּק|roach|ʤuk
-ד׳ו אל-חיג׳ה|(Dhu al-Hijjah)||ذو الحجة
-הד|echo|hɛd||הֵד
+ד׳ו אל-חיג׳ה|(Dhu al-Hijjah)|ذو الحجة
+הד|echo|hɛd|הֵד
 הוּא|he|hu
-זה|this|ze||זֶה
-ז׳רגון|jargon|ʒarɡon||זָ׳רְגוֹן
-חם|hot|χam||חַם
-טוב|good|tov||טוֹב
+זה|this|ze|זֶה
+ז׳רגון|jargon|ʒarɡon|זָ׳רְגוֹן
+חם|hot|χam|חַם
+טוב|good|tov|טוֹב
 טוֹב|good|tɔv
-ים|sea|yam||יָם
-לי|to me|li||לִי
-לו|to him|lo||לוֹ
+ים|sea|yam|יָם
+לי|to me|li|לִי
+לו|to him|lo|לוֹ
 מוּם|defect|mum
-סכך|branch-roofing|sχaχ||סְכָךְ
-סוף|end|sof||סוֹף
-עונטה|boastful act|awanta||עָוָנְטָה
-פּה|here|po||פֹּה
-קול|sound|kol||קוֹל
+סכך|branch-roofing|sχaχ|סְכָךְ
+סוף|end|sof|סוֹף
+עונטה|boastful act|awanta|עָוָנְטָה
+פּה|here|po|פֹּה
+קול|sound|kol|קוֹל
 ריצ׳רץ׳|zip|ʁiʧʁaʧ
 ר׳ג׳ר|Ghajar
-שׁם|ʃam|there||שָׁם
-שׂם|put|sam||שָׂם
-שופּ|shop|ʃop||שׁוֹפּ
+שׁם|there|ʃam|שָׁם
+שם|there/put|ʃam/sam|שָׁם/שָׂם
+שׂם|put|sam|שָׂם
+שופּ|shop|ʃop|שׁוֹפּ
+שופ|shop|ʃop|שׁוֹפּ
 שייח|Sheikh (شيخ)
 שייח׳|Sheikh (شيخ)
 תּוּת|strawberry|tut
+תות|strawberry|tut|תּוּת
 תל־אביב|Tel Aviv
 ת׳רסטון|Thurston
 דּוּד|dud|boiler
-כּה|so|ko||כֹּה
+דוד|dud|boiler|דּוּד
+כּה|so|ko|כֹּה
+כה|so|ko|כֹּה
 
-קט|tiny|kat||קָט
-ציץ|bud|ʦiʦ||צִיץ
-פספס|missed|fisfɛs||פִסְפֵס
-עדלאידע|Purim parade|adlɔjada||עַדְלֹאיָדַע
-עיר|city|iʁ||עִיר
-מועיל|useful|moʔil||מוֹעִיל
-מידע|information|meda||מֵידָע
+קט|tiny|kat|קָט
+ציץ|bud|ʦiʦ|צִיץ
+פספס|missed|fisfɛs|פִסְפֵס
+עדלאידע|Purim parade|adlɔjada|עַדְלֹאיָדַע
+עיר|city|iʁ|עִיר
+מועיל|useful|moʔil|מוֹעִיל
+מידע|information|meda|מֵידָע
 מֵידָע|information|meda
 
-פּה|mouth|ˈpe||פֶּה
-מה|what|ma||מָה
-אופה|baker|oˈfe||אוֹפֶה
-תּן|jackal|ˈtan||תַּן
-צי|fleet|ˈtsi||צִי
-סוף|end|ˈsof||סוֹף
-נס|miracle|ˈnes||נֵס
-תּשוּקה|passion|tʃuˈka||תְּשׁוּקָה
-שנה|year|ʃaˈna||שָׁנָה
-יום|day|ˈjom||יוֹם
-כּל|all|ˈkol||כֹּל
-איך|how|ex||אֵיךְ
-חם|hot|ˈħam||חַם
-ראיון|interview|reʔaˈjon||רֵאָיוֹן
-נבל|harp|ˈnevel||נֵבֶל
+פּה|mouth|ˈpe|פֶּה
+מה|what|ma|מָה
+אופה|baker|oˈfe|אוֹפֶה
+תּן|jackal|ˈtan|תַּן
+צי|fleet|ˈtsi|צִי
+סוף|end|ˈsof|סוֹף
+נס|miracle|ˈnes|נֵס
+תּשוּקה|passion|tʃuˈka|תְּשׁוּקָה
+שנה|year|ʃaˈna|שָׁנָה
+יום|day|ˈjom|יוֹם
+כּל|all|ˈkol|כֹּל
+איך|how|ex|אֵיךְ
+חם|hot|ˈħam|חַם
+ראיון|interview|reʔaˈjon|רֵאָיוֹן
+נבל|harp|ˈnevel|נֵבֶל
 נֵבֶל|harp|ˈnevel
-דּלק|fuel|ˈdelek||דֶּלֶק
-זה|this|ze||זֶה
-גּ׳ירפה|giraffe|dʒiˈrafa||גִּ׳ירָפָה
-בּז|beige|ˈbeʒ||בֵּז
-פּינגּוין|penguin|ˈpinɡwin||פִּינְגְּוִין
-גּם|also|gam||גַּם
-ראש|head|ˈroʃ||רֹאשׁ
-עם|with|ʕim||עִם
-הד|echo|ˈhed||הֵד
+דּלק|fuel|ˈdelek|דֶּלֶק
+זה|this|ze|זֶה
+גּ׳ירפה|giraffe|dʒiˈrafa|גִּ׳ירָפָה
+בּז|beige|ˈbeʒ|בֵּז
+פּינגּוין|penguin|ˈpinɡwin|פִּינְגְּוִין
+גּם|also|gam|גַּם
+ראש|head|ˈroʃ|רֹאשׁ
+עם|with|ʕim|עִם
+הד|echo|ˈhed|הֵד
 
 
-נמלים|ants|nemaˈlim||נְמָלִים
-גּמלים|camels|ɡmaˈlim||גְּמָלִים
+נמלים|ants|nemaˈlim|נְמָלִים
+גּמלים|camels|ɡmaˈlim|גְּמָלִים
 נְמָלִים|ants|nemaˈlim
 גְּמָלִים|camels|ɡmaˈlim
-שאל|asked|ʃaʔal||שָׁאַל
+שאל|asked|ʃaʔal|שָׁאַל
 שָׁאַל|asked|ʃaʔal
-שנה|year|ʃaˈna||שָׁנָה
+שנה|year|ʃaˈna|שָׁנָה
 שָׁנָה|year|ʃaˈna
-פּה‎|here|po||פֹּה‎
+פּה‎|here|po|פֹּה‎
 פֹּה‎‎|here|po
-קול|sound|kɔl||קוֹל
+קול|sound|kɔl|קוֹל
 קוֹל|sound|kɔl
-לגוע|to agonise|liɡˈvo.a||לִגְוֺעַ
+לגוע|to agonise|liɡˈvo.a|לִגְוֺעַ
 לִגְוֺעַ|to agonise|liɡˈvo.a
-לא|no|ˈlo||לֹא
+לא|no|ˈlo|לֹא
 לֹא|no|lo
-תּאר|epithet|ˈto.aʁ||תֹּאַר
+תּאר|epithet|ˈto.aʁ|תֹּאַר
 תֹּאַר|epithet|ˈto.aʁ
 כְּתֻמִּים|oranges|kətumˈmim
-כּתמּים|oranges|kətumˈmim||כְּתֻמִּים
-ראשון|first|riʃɔn||רִאשׁוֹן
+כּתמּים|oranges|kətumˈmim|כְּתֻמִּים
+ראשון|first|riʃɔn|רִאשׁוֹן
 רִאשׁוֹן|first|riʃɔn
-נין|grandson|nin||נִין
+נין|grandson|nin|נִין
 נִין|grandson|nin
 
-וו|hook|vav||וָו
+וו|hook|vav|וָו
 
 `
 
