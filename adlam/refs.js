4e941320fac77a84f2ce815fd6b069e794f358a1
r12a <ishida@w3.org>
2020-11-30 22:40:25 +0000

    adlm, Add automatic Reference section and footnote links

---
+++ adlam/refs.js
@@ -0,0 +1,89 @@
+refs = {
+
+"b": {
+"url":"https://www.unicode.org/L2/L2018/18282r-adlam.pdf",
+"authors": "Abdoulaye Barry, Ibrahima Barry, Peter Constable, Andrew Glass",
+"title":"Proposal to encode ADLAM NASALIZATION MARK for ADLaM script",
+},
+
+"e": {
+"url":"http://std.dkuug.dk/jtc1/sc2/wg2/docs/n4628.pdf",
+"authors": "Michael Everson",
+"title":"Proposal for encoding the Adlam script in the SMP of the UCS",
+},
+
+"g": {
+"url":"https://github.com/w3c/afrlreq/issues/3",
+"authors": "Afrlreq GitHub issues list",
+"title":"Direction of slant for italic/oblique RTL text",
+},
+
+"n": {
+"url":"#refs",
+"authors": "Neil Patel",
+"title":"Private correspondence",
+},
+
+"o": {
+"url":"https://www.omniglot.com/writing/adlam.htm",
+"authors": "Omniglot",
+"title":"Adlam",
+},
+
+"p": {
+"url":"https://www.unicode.org/L2/L2019/19119r-adlam-font-repl.pdf",
+"authors": "Neil Patel and Mark Jamra (JamraPatel), Craig Cornelius (Google), Ibrahima Barry, Abdoulaye Barry",
+"title":"Replacement of Adlam Reference Font in Codesheet to Updated Design",
+},
+
+"s": {
+"url":"http://scriptsource.org/cms/scripts/page.php?item_id=script_detail&amp;uid=aggc6af8cc",
+"authors": "ScriptSource",
+"title":"Adlam",
+},
+
+"u": {
+"url":"http://www.unicode.org/versions/Unicode11.0.0/ch19.pdf",
+"authors": "Unicode Consortium",
+"title":"The Unicode Standard, Version 13.0",
+"sub":"Chapter 19.9: Africa, Adlam",
+"pages":"784-785",
+"isbn":"978-1-936213-16-0.",
+},
+
+"ul": {
+"url":"https://www.unicode.org/reports/tr14/",
+"authors": "Unicode Consortium",
+"title":"Unicode Line Breaking Algorithm (UAX#14)",
+},
+
+"wl": {
+"url":"https://en.wikipedia.org/wiki/Fula_language",
+"authors": "Wikipedia",
+"title":"Fula language",
+},
+
+"ws": {
+"url":"http://en.wikipedia.org/wiki/Adlam_script",
+"authors": "Wikipedia",
+"title":"Adlam script",
+},
+
+"ya": {
+"url":"https://www.youtube.com/watch?v=NPQ7ufjN2i8",
+"authors": "Abdoulaye and Ibrahim Barry",
+"title":"The ADLaM Story",
+"sub":"YouTube"
+},
+
+"yi": {
+"url":"https://www.youtube.com/watch?v=PXN4aciv19U",
+"authors": "Abdoulaye &amp; Ibrahima Barry, Mark Jamara, Neil Patel",
+"title":"Inventing the Adlam Script",
+"sub":"YouTube"
+},
+
+
+}
+
+
