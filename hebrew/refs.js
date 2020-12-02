e651bf79c60f88d3a16296be992f48d4f5287fd4
r12a <ishida@w3.org>
2020-12-02 14:04:45 +0000

    Move sliding selector before Character Index Add back block-level headings to character block pages Add refs.js file and populate Add refs links to top of index and block files: Change all .fnref spans to tt In intro, change the links etc.

---
+++ hebrew/refs.js
@@ -0,0 +1,101 @@
+reflist = {
+
+
+"a": {
+"url":"https://hebrew-academy.org.il/topic/hahlatot/punctuation/",
+"authors": "Hebrew Academy",
+"title":"האקרמיה ללשון העברת",
+},
+
+"d": {
+"url":"#refs",
+"authors": "Peter T. Daniels and William Bright",
+"title":"The World's Writing Systems",
+"publisher":"Oxford University Press",
+"pages":"487-497",
+"isbn":"0-19-507993-0",
+},
+
+"l": {
+"url":"https://www.typotalks.com/videos/go-bolder-just-slant-it/",
+"authors": "Liron Lavi Turkenich",
+"title":"Go bolder, just slant it—secondary styles in Hebrew (video)",
+"sub":"in TYPO  International Design Talks",
+},
+
+"s": {
+"url":"https://scriptsource.org/scr/Hebr",
+"authors": "ScriptSource",
+"title":"Hebrew",
+},
+
+"u": {
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch9.pdf",
+"authors": "Unicode Consortium",
+"title":"The Unicode Standard, Version 13.0",
+"sub":"Chapter 9.1: Middle East-I, Hebrew",
+"pages":"359-364",
+"isbn":"978-1-936213-16-0.",
+},
+
+"ul": {
+"url":"https://www.unicode.org/reports/tr14/",
+"authors": "Unicode Consortium",
+"title":"Unicode Line Breaking Algorithm (UAX#14)",
+"reviewed":true
+},
+
+"wc": {
+"url":"http://en.wikipedia.org/wiki/Cantillation",
+"authors": "Wikipedia",
+"title":"Cantillation",
+},
+
+"wg": {
+"url":"http://en.wikipedia.org/wiki/Geresh",
+"authors": "Wikipedia",
+"title":"Geresh",
+},
+
+"wh": {
+"url":"http://en.wikipedia.org/wiki/Holam",
+"authors": "Wikipedia",
+"title":"Holam",
+},
+
+"whp": {
+"url":"http://en.wikipedia.org/wiki/Hebrew_punctuation",
+"authors": "Wikipedia",
+"title":"Hebrew punctuation",
+},
+
+"wi": {
+"url":"https://en.wikipedia.org/wiki/Help:IPA/Hebrew",
+"authors": "Wikipedia",
+"title":"Help:IPA/Hebrew",
+},
+
+"wp": {
+"url":"https://en.wikipedia.org/wiki/Modern_Hebrew_phonology",
+"authors": "Wikipedia",
+"title":"Modern Hebrew Phonology",
+},
+
+"wsv": {
+"url":"http://en.wikipedia.org/wiki/Schwa",
+"authors": "Wikipedia",
+"title":"Schwa",
+},
+
+"ws": {
+"url":"http://en.wikipedia.org/wiki/Hebrew_alphabet",
+"authors": "Wikipedia",
+"title":"Hebrew alphabet",
+},
+
+
+}
+
+
+
+// <a class="fn">ws,#tones</a>
