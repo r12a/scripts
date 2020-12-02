e848c3b460048cdc5cf965fe379171c63303d810
r12a <ishida@w3.org>
2020-12-02 19:42:45 +0000

    Move sliding selector before Character Index Add back block-level headings to character block pages Add refs.js file and populate Add refs links to top of index and block files: Change all .fnref spans to tt In intro, change the links etc.

---
+++ cherokee/refs.js
@@ -0,0 +1,56 @@
+reflist = {
+
+
+"d": {
+"url":"#refs",
+"authors": "Peter T. Daniels and William Bright",
+"title":"The World's Writing Systems",
+"publisher":"Oxford University Press",
+"isbn":"0-19-507993-0",
+},
+
+"e": {
+"url":"http://www.unicode.org/L2/L2014/14064r-n4537r-cherokee.pdf",
+"authors": "Michael Everson & Durbin Feeling",
+"title":"Revised proposal for the addition of Cherokee characters to the UCS",
+},
+
+"o": {
+"url":"http://www.omniglot.com/writing/cherokee.htm",
+"authors": "Omniglot",
+"title":"Cherokee",
+},
+
+"s": {
+"url":"http://scriptsource.org/scr/Cher",
+"authors": "ScriptSource",
+"title":"Cherokee",
+},
+
+"u": {
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch20.pdf",
+"authors": "Unicode Consortium",
+"title":"The Unicode Standard, Version 13.0",
+"sub":"Chapter 20.1: Americas, Cherokee",
+"pages":"788-789",
+"isbn":"978-1-936213-16-0.",
+},
+
+"ul": {
+"url":"https://www.unicode.org/reports/tr14/",
+"authors": "Unicode Consortium",
+"title":"Unicode Line Breaking Algorithm (UAX#14)",
+"reviewed":true
+},
+
+"ws": {
+"url":"https://en.wikipedia.org/wiki/Cherokee_syllabary",
+"authors": "Wikipedia",
+"title":"Cherokee syllabary",
+},
+
+
+}
+
+
+
