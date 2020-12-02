eb942c39461914ee27d30517e5c25770cc8a3946
r12a <ishida@w3.org>
2020-12-02 19:46:20 +0000

    vai, Move sliding selector before Character Index Add back block-level headings to character block pages Add refs.js file and populate Add refs links to top of index and block files: Change all .fnref spans to tt In intro, change the links etc.

---
+++ vai/refs.js
@@ -0,0 +1,50 @@
+reflist = {
+
+
+"d": {
+"url":"#refs",
+"authors": "Peter T. Daniels and William Bright",
+"title":"The World's Writing Systems",
+"publisher":"Oxford University Press",
+"isbn":"0-19-507993-0",
+},
+
+"s": {
+"url":"http://scriptsource.org/cms/scripts/page.php?item_id=script_detail&key=Vaii",
+"authors": "ScriptSource",
+"title":"Vai",
+},
+
+"u": {
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch19.pdf",
+"authors": "Unicode Consortium",
+"title":"The Unicode Standard, Version 13.0",
+"sub":"Chapter 19.5: Africa, Vai",
+"pages":"777-778",
+"isbn":"978-1-936213-16-0.",
+},
+
+"ul": {
+"url":"https://www.unicode.org/reports/tr14/",
+"authors": "Unicode Consortium",
+"title":"Unicode Line Breaking Algorithm (UAX#14)",
+"reviewed":true
+},
+
+"wl": {
+"url":"http://en.wikipedia.org/wiki/Vai_language",
+"authors": "Wikipedia",
+"title":"Vai language",
+},
+
+"ws": {
+"url":"http://en.wikipedia.org/wiki/Vai_syllabary",
+"authors": "Wikipedia",
+"title":"Vai syllabary",
+},
+
+
+}
+
+
+
