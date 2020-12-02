23ae69cbca12af80a52c226d984cbd121e728e12
r12a <ishida@w3.org>
2020-12-02 19:44:50 +0000

    Move sliding selector before Character Index Add back block-level headings to character block pages Add refs.js file and populate Add refs links to top of index and block files: Change all .fnref spans to tt In intro, change the links etc.

---
+++ lisu/refs.js
@@ -0,0 +1,44 @@
+reflist = {
+
+
+"d": {
+"url":"#refs",
+"authors": "Peter T. Daniels and William Bright",
+"title":"The World's Writing Systems",
+"publisher":"Oxford University Press",
+"isbn":"0-19-507993-0",
+},
+
+"u": {
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch19.pdf",
+"authors": "Unicode Consortium",
+"title":"The Unicode Standard, Version 13.0",
+"sub":"Chapter 18.9: East Asia, Lisu",
+"pages":"752-754",
+"isbn":"978-1-936213-16-0.",
+},
+
+"ul": {
+"url":"https://www.unicode.org/reports/tr14/",
+"authors": "Unicode Consortium",
+"title":"Unicode Line Breaking Algorithm (UAX#14)",
+},
+
+"wl": {
+"url":"https://en.wikipedia.org/wiki/Lisu_language",
+"authors": "Wikipedia",
+"title":"Lisu language",
+},
+
+"ws": {
+"url":"http://en.wikipedia.org/wiki/Lisu_script",
+"authors": "Wikipedia",
+"title":"Lisu script",
+},
+
+
+}
+
+
+
+// <a class="fn">ws,#tones</a>
