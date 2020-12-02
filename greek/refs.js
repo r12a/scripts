7204ad830ecd3b724f7031ec9ace54cdda7453f6
r12a <ishida@w3.org>
2020-12-02 14:03:26 +0000

    Move sliding selector before Character Index Add back block-level headings to character block pages Add refs.js file and populate Add refs links to top of index and block files: Change all .fnref spans to tt In intro, change the links etc.

---
+++ greek/refs.js
@@ -0,0 +1,51 @@
+reflist = {
+
+
+"d": {
+"url":"#refs",
+"authors": "Peter T. Daniels and William Bright",
+"title":"The World's Writing Systems",
+"publisher":"Oxford University Press",
+"isbn":"0-19-507993-0",
+"pages":"559-563",
+},
+
+"u": {
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch07.pdf",
+"authors": "Unicode Consortium",
+"title":"The Unicode Standard, Version 13.0",
+"sub":"Chapter 7.2: Europe-I, Greek",
+"pages":"301-307",
+"isbn":"978-1-936213-16-0.",
+},
+
+"ul": {
+"url":"https://www.unicode.org/reports/tr14/",
+"authors": "Unicode Consortium",
+"title":"Unicode Line Breaking Algorithm (UAX#14)",
+"reviewed":true
+},
+
+"wp": {
+"url":"https://en.wikipedia.org/wiki/Modern_Greek_phonology",
+"authors": "Wikipedia",
+"title":"Modern Greek phonology",
+},
+
+"wq": {
+"url":"https://en.wikipedia.org/wiki/Quotation_mark",
+"authors": "Wikipedia",
+"title":"Quotation mark",
+},
+
+"ws": {
+"url":"http://en.wikipedia.org/wiki/Greek_alphabet",
+"authors": "Wikipedia",
+"title":"Greek alphabet",
+},
+
+
+}
+
+
+
