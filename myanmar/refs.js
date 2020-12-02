22ada0d484f33ebcfbc08b7643eca3a4bb7dfda2
r12a <ishida@w3.org>
2020-12-02 19:45:35 +0000

    Move sliding selector before Character Index Add back block-level headings to character block pages Add refs.js file and populate Add refs links to top of index and block files: Change all .fnref spans to tt In intro, change the links etc.

---
+++ myanmar/refs.js
@@ -0,0 +1,113 @@
+reflist = {
+
+
+"d": {
+"url":"#refs",
+"authors": "Peter T. Daniels and William Bright",
+"title":"The World's Writing Systems",
+"publisher":"Oxford University Press",
+"isbn":"0-19-507993-0",
+},
+
+"h": {
+"url":"http://www.unicode.org/notes/tn11/",
+"authors": "Martin Hosken & Maung Tuntunlwin",
+"title":"Unicode Technical Note #11, Representing Myanmar in Unicode: Details and Examples",
+},
+
+"g": {
+"url":"https://github.com/googlefonts/noto-fonts/issues/1811",
+"authors": "GitHub, Noto fonts",
+"title":"Issues in Noto Sans Myanmar for Shan Language",
+},
+
+"l": {
+"url":"http://www.unicode.org/L2/L2006/06077-n3043r-myanmar.pdf",
+"authors": "Ireland (NSAI), United Kingdom (BSI), Myanmar Language Commission, Myanmar Unicode and Natural Language Processing Research Center, Myanmar Computer Federation",
+"title":"Proposal to encode seven additional Myanmar characters in the UCS",
+},
+
+"m": {
+"url":"#refs",
+"authors": "Gene Mesher",
+"title":"Burmese for Beginners",
+"isbn":"1-887521-51-8",
+},
+
+"ms": {
+"url":"http://www.microsoft.com/typography/OpenTypeDev/myanmar/intro.htm",
+"authors": "Microsoft Typography",
+"title":"Creating and Supporting OpenType Fonts for Myanmar Script",
+"version":"Dec 2013",
+},
+
+"o": {
+"url":"https://www.omniglot.com/writing/shan.htm",
+"authors": "Omniglot",
+"title":"Shan",
+},
+
+"sb": {
+"url":"http://sealang.net/burmese/",
+"authors": "Sealang Library",
+"title":"Burmese",
+},
+
+"ss": {
+"url":"http://sealang.net/shan/dictionary.htm",
+"authors": "Sealang Library",
+"title":"Shan Dictionary Resources",
+},
+
+"u": {
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch16.pdf",
+"authors": "Unicode Consortium",
+"title":"The Unicode Standard, Version 13.0",
+"sub":"Chapter 16.3: Southeast Asia, Myanmar",
+"pages":"646-652",
+"isbn":"978-1-936213-16-0.",
+},
+
+"ul": {
+"url":"https://www.unicode.org/reports/tr14/",
+"authors": "Unicode Consortium",
+"title":"Unicode Line Breaking Algorithm (UAX#14)",
+"reviewed":true
+},
+
+"wbl": {
+"url":"http://en.wikipedia.org/wiki/Burmese_language",
+"authors": "Wikipedia",
+"title":"Burmese language",
+},
+
+"wbs": {
+"url":"http://en.wikipedia.org/wiki/Burmese_alphabet",
+"authors": "Wikipedia",
+"title":"Burmese alphabet",
+},
+
+"wm": {
+"url":"http://en.wikipedia.org/wiki/MLC_Transcription_System",
+"authors": "Wikipedia",
+"title":"MLC Transcription System",
+},
+
+"wsl": {
+"url":"https://en.wikipedia.org/wiki/Shan_language",
+"authors": "Wikipedia",
+"title":"Shan language",
+},
+
+"wsw": {
+"url":"https://en.wiktionary.org/wiki/Appendix:Kra-Dai_Swadesh_lists",
+"authors": "Wikipedia",
+"title":"Appendix:Kra-Dai Swadesh lists",
+},
+
+
+}
+
+
+
+// <a class="fn">ws,#tones</a>
