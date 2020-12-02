97cca20cbf420e01f1e7dfb6deace3d4827b82c1
r12a <ishida@w3.org>
2020-12-02 19:48:20 +0000

    cans-ike, Move sliding selector before Character Index Add back block-level headings to character block pages Add refs.js file and populate Add refs links to top of index and block files: Change all .fnref spans to tt In intro, change the links etc.

---
+++ cans/refs.js
@@ -0,0 +1,83 @@
+reflist = {
+
+
+"d": {
+"url":"#refs",
+"authors": "Peter T. Daniels and William Bright",
+"title":"The World's Writing Systems",
+"publisher":"Oxford University Press",
+"isbn":"0-19-507993-0",
+},
+
+"i": {
+"url":"http://www.languagegeek.com/inu/inu_syllabarium.html",
+"authors": "Chris Harvey",
+"title":"LanguageGeek, ᐃᓄᒃᑎᑐᑦ – Inuktitut Syllabarium",
+},
+
+"m": {
+"url":"http://www.inuktitutcomputing.ca/Technocrats/ILFT_1.html",
+"authors": "Mick Mallon",
+"title":"Inuktitut Linguistics for Technocrats",
+},
+
+"r": {
+"url":"http://tiro.com/Ross/Canadian_Native_Languages_V1.2.pdf",
+"authors": "Ross Mills",
+"title":"Canadian Native Languages",
+"reviewed":true
+},
+
+"p": {
+"url":"http://std.dkuug.dk/jtc1/sc2/wg2/docs/n3427.pdf",
+"authors": "Michael Everson and Chris Harvey",
+"title":"Proposal to encode 39 Unified Canadian Aboriginal Syllabics in the UCS",
+},
+
+"t": {
+"url":"https://tusaalanga.ca/",
+"authors": "-",
+"title":"Inuktut Tusaalanga",
+"reviewed":true
+},
+
+"u": {
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch20.pdf",
+"authors": "Unicode Consortium",
+"title":"The Unicode Standard, Version 13.0",
+"sub":"Chapter 20.2: Americas, Canadian Aboriginal Syllabics",
+"pages":"790-791",
+"isbn":"978-1-936213-16-0.",
+},
+
+"ul": {
+"url":"https://www.unicode.org/reports/tr14/",
+"authors": "Unicode Consortium",
+"title":"Unicode Line Breaking Algorithm (UAX#14)",
+"reviewed":true
+},
+
+"wcl": {
+"url":"https://en.wikipedia.org/wiki/Plains_Cree",
+"authors": "Wikipedia",
+"title":"Plains Cree",
+},
+
+"wp": {
+"url":"https://en.wikipedia.org/wiki/Inuit_phonology",
+"authors": "Wikipedia",
+"title":"Inuit phonology",
+},
+
+"ws": {
+"url":"https://en.wikipedia.org/wiki/Inuktitut_syllabics",
+"authors": "Wikipedia",
+"title":"Inuktitut syllabics",
+"reviewed":true
+},
+
+
+}
+
+
+
