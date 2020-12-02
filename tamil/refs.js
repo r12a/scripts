12eba9a200319393d6de7d7f572e99c650156f04
r12a <ishida@w3.org>
2020-12-02 19:45:56 +0000

    Move sliding selector before Character Index Add back block-level headings to character block pages Add refs.js file and populate Add refs links to top of index and block files: Change all .fnref spans to tt In intro, change the links etc.

---
+++ tamil/refs.js
@@ -0,0 +1,171 @@
+reflist = {
+
+
+"b": {
+"url":"#refs",
+"authors": "Bernard Comrie",
+"title":"The World's Major Languages",
+"sub":"3rd Edition",
+"publisher":"Routledge",
+"isbn":"97801-315-64493-6",
+},
+
+"c": {
+"url":"https://www.unicode.org/cldr/charts/latest/summary/ta.html",
+"authors": "CLDR",
+"title":"Tamil chart tables",
+},
+
+"d": {
+"url":"#refs",
+"authors": "Peter T. Daniels and William Bright",
+"title":"The World's Writing Systems",
+"publisher":"Oxford University Press",
+"isbn":"0-19-507993-0",
+"pages": "426-30 esp.",
+},
+
+"f": {
+"url":"https://web.archive.org/web/20120311223833/http://www.leonidas.org/rdg/matd/dissertation/FernandoMello_dissertation.pdf",
+"authors": "Fernando de Mello Vargas",
+"title":"Evolution of Tamil typedesign",
+},
+
+"g26": {
+"url":"https://github.com/w3c/iip/issues/26",
+"authors": "Github IIP issue list",
+"title":"Other paragraph features",
+},
+
+"g57": {
+"url":"https://github.com/w3c/iip/issues/57",
+"authors": "Github IIP issue list",
+"title":"Are alphabetic counter styles common in Tamil content?",
+},
+
+"g58": {
+"url":"https://github.com/w3c/iip/issues/58",
+"authors": "Github IIP issue list",
+"title":"Is full inter-character spacing common for narrow columns in Tamil?",
+},
+
+"i": {
+"url":"https://w3c.github.io/ilreq/",
+"authors": "Swaran Lata, Somnath Chandra, Prashant Verma",
+"title":"Indic Layout Requirements",
+},
+
+"l": {
+"url":"#refs",
+"authors": "Anandam Krishnamurthi",
+"title":"Learn Tamil in a Month",
+"publisher":"Readwells",
+"isbn":"9788187782049",
+},
+
+"i": {
+"url":"https://w3c.github.io/ilreq/",
+"authors": "Swaran Lata, Somnath Chandra, Prashant Verma",
+"title":"Indic Layout Requirements",
+},
+
+"m": {
+"url":"#refs",
+"authors": "முத்து நெடுமாறன் (Muthu Neduraman)",
+"title":"Personal correspondence",
+},
+
+"n": {
+"url":"http://unicode.org/faq/tamil.html",
+"authors": "Unicode Consortium",
+"title":"FAQ: Tamil Vowels, Consonants, and Syllables: Alternative Formats",
+},
+
+"o": {
+"url":"http://www.omniglot.com/writing/tamil.htm",
+"authors": "Omniglot",
+"title":"Tamil",
+},
+
+"p": {
+"url":"https://www.academia.edu/671796/Tamil_Hyphenator_P._David_Prabhakar",
+"authors": "P. David Prabhakar",
+"title":"Tamil Hyphenator",
+},
+
+"s": {
+"url":"http://www.unicode.org/L2/L2012/12018-tamil-anusvara-depr.pdf",
+"authors": "Shriramana Sharma",
+"title":"Request to deprecate 0B82 TAMIL SIGN ANUSVARA",
+},
+
+"ss": {
+"url":"http://unicode.org/L2/L2012/12231-tamil-fractions-symbols-proposal.pdf",
+"authors": "Shriramana Sharma",
+"title":"Proposal to encode Tamil fractions and symbols",
+},
+
+"t": {
+"url":"https://w3c.github.io/iip/tamil/",
+"authors": "Ishida, Richard",
+"title":"Tamil Layout Requirements",
+},
+
+"u": {
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch12.pdf",
+"authors": "Unicode Consortium",
+"title":"The Unicode Standard, Version 13.0",
+"sub":"Chapter 12.6: South and Central Asia-I, Tamil",
+"pages":"491-500",
+"isbn":"978-1-936213-16-0.",
+},
+
+"uf": {
+"url":"http://unicode.org/faq/tamil.html",
+"authors": "Unicode Consortium",
+"title":"FAQ: Tamil Language and Script",
+"reviewed":true
+},
+
+"ul": {
+"url":"https://www.unicode.org/reports/tr14/",
+"authors": "Unicode Consortium",
+"title":"Unicode Line Breaking Algorithm (UAX#14)",
+"reviewed":true
+},
+
+"wd": {
+"url":"http://www.wordiq.com/definition/Tamil_language",
+"authors": "Wikipedia",
+"title":"Tamil language - Definition",
+},
+
+"wl": {
+"url":"http://en.wikipedia.org/wiki/Tamil_language",
+"authors": "Wikipedia",
+"title":"Tamil language",
+},
+
+"wp": {
+"url":"https://en.wikipedia.org/wiki/Tamil_phonology",
+"authors": "Wikipedia",
+"title":"Tamil phonology",
+},
+
+"ws": {
+"url":"http://en.wikipedia.org/wiki/Tamil_script",
+"authors": "Wikipedia",
+"title":"Tamil script",
+},
+
+"wss": {
+"url":"https://en.wikipedia.org/wiki/Simplified_Tamil_script",
+"authors": "Wikipedia",
+"title":"Simplified Tamil script",
+},
+
+
+}
+
+
+
