1d1ab6428cb88812e6d8ac054aa28997b530d330
r12a <ishida@w3.org>
2020-12-03 09:47:03 +0000

    osge, Add refs.js file and populate Add refs links to top of index and block files: Change all .fnref spans to tt In intro, change the links etc.

---
+++ osage/refs.js
@@ -0,0 +1,63 @@
+reflist = {
+
+
+"e": {
+"url":"http://std.dkuug.dk/jtc1/sc2/wg2/docs/n4619.pdf",
+"authors": "Michael Everson, Herman Mongrain Lookout, Cameron Pratt",
+"title":"Final proposal to encode the Osage script in the UCS",
+},
+
+"o": {
+"url":"https://www.osagelanguage.com/courses/",
+"authors": "Osage Nation Language Dept.",
+"title":"Osage Language Beginner Lessons",
+"reviewed":true
+},
+
+"q": {
+"url":"https://books.google.co.uk/books?id=wnvaInod5MAC&printsec=frontcover#v=onepage&q&f=false",
+"authors": "Carlyn Quintero",
+"title":"Osage Grammar (preview)",
+"reviewed":true
+},
+
+"s": {
+"url":"https://scriptsource.org/scr/Osge",
+"authors": "ScriptSource",
+"title":"Osage",
+"reviewed":true
+},
+
+"u": {
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch20.pdf",
+"authors": "Unicode Consortium",
+"title":"The Unicode Standard, Version 13.0",
+"sub":"Chapter 20.3: Americas, Osage",
+"pages":"792",
+"isbn":"978-1-936213-16-0.",
+},
+
+"ul": {
+"url":"https://www.unicode.org/reports/tr14/",
+"authors": "Unicode Consortium",
+"title":"Unicode Line Breaking Algorithm (UAX#14)",
+"reviewed":true
+},
+
+"wl": {
+"url":"https://en.wikipedia.org/wiki/Osage_language",
+"authors": "Wikipedia",
+"title":"Osage language",
+},
+
+"ws": {
+"url":"http://en.wikipedia.org/wiki/Osage_script",
+"authors": "Wikipedia",
+"title":"Osage script",
+},
+
+
+}
+
+
+
