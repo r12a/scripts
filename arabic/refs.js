5ef3f0cf58f5788ad711ee4394a206dc17778104
r12a <ishida@w3.org>
2020-12-03 09:07:07 +0000

    arb, Add refs.js file and populate Add refs links to top of index and block files: Change all .fnref spans to tt In intro, change the links etc.

---
+++ arabic/refs.js
@@ -0,0 +1,145 @@
+reflist = {
+
+
+"a": {
+"url":"https://w3c.github.io/alreq/",
+"authors": "Behnam Esfahbod, Mostafa Hajizadeh, Najib Tounsi, Richard Ishida, Shervin Afshar,Titus Nemeth",
+"title":"Text Layout Requirements for the Arabic Script (alreq)",
+},
+
+"c": {
+"url":"https://www.unicode.org/cldr/charts/latest/summary/ar.html",
+"authors": "CLDR",
+"title":"Arabic",
+},
+
+"d": {
+"url":"#refs",
+"authors": "Peter T. Daniels and William Bright",
+"title":"The World's Writing Systems",
+"publisher":"Oxford University Press",
+"isbn":"0-19-507993-0",
+},
+
+"jm": {
+"url":"#refs",
+"authors": "John Mace",
+"title":"Beginner's Arabic Script",
+"publisher":"Hodder & Stoughton Ltd",
+"isbn":"0-340-86016-2",
+},
+
+"ub": {
+"url":"#refs",
+"authors": "Richard Delacy",
+"title":"Beginner's Urdu Script",
+"publisher":"Hodder & Stoughton Ltd",
+"isbn":"0-340-86026-6",
+},
+
+"uu": {
+"url":"#refs",
+"authors": "David Matthews & Mohamed Kasim Dalvi",
+"title":"Urdu",
+"publisher":"Hodder & Stoughton Ltd",
+"isbn":"0-340-86725-6",
+},
+
+"g49": {
+"url":"https://github.com/w3c/alreq/issues/49",
+"authors": "GitHub",
+"title":"Shortcomings of Characters Table",
+},
+
+"pm": {
+"url":"#refs",
+"authors": "Narguess Farzad",
+"title":"Modern Persian",
+"publisher":"Hodder & Stoughton Ltd",
+"isbn":"0-340-80265-0",
+},
+
+"pp": {
+"url":"http://www.unicode.org/L2/L2012/12381-dotless-noon.pdf",
+"authors": "Roozbeh Pournader",
+"title":"Initial and medial forms of Arabic Letter Noon Ghunna",
+"sub":"l2/12-381, 2012-11-03",
+},
+
+"f": {
+"url":"https://www.freearabicdictionary.com/",
+"authors": "FreeArabicDictionary",
+"title":"Arabic Dictionary & Translation",
+},
+
+"j": {
+"url":"#refs",
+"authors": "Jack Smart and Francis Altorfer",
+"title":"Teach Yourself Arabic",
+"publisher":"Oxford University Press",
+"isbn":"978-0-340-86996-3",
+"reviewed":true
+},
+
+"l": {
+"url":"https://www.loc.gov/catdir/cpso/romanization/arabic.pdf",
+"authors": "Library of Congress",
+"title":"Arabic romanization table",
+"reviewed":true
+},
+
+"s": {
+"url":"http://scriptsource.org/cms/scripts/page.php?item_id=script_detail&key=Arab",
+"authors": "ScriptSource",
+"title":"Arabic",
+"reviewed":true
+},
+
+"us": {
+"url":"http://www.unicode.org/L2/L2002/02163-n2413-4-urdu.pdf",
+"authors": "Sarmad Hussain",
+"title":"Proposal to add Marks and Digits in Arabic Code Block (for Urdu)",
+"sub":"L2-02/163",
+"reviewed":true
+},
+
+"u": {
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch09.pdf",
+"authors": "Unicode Consortium",
+"title":"The Unicode Standard, Version 13.0",
+"sub":"Chapter 9.2: Middle East-I, Arabic",
+"pages":"365-389",
+"isbn":"978-1-936213-16-0.",
+},
+
+"ul": {
+"url":"https://www.unicode.org/reports/tr14/",
+"authors": "Unicode Consortium",
+"title":"Unicode Line Breaking Algorithm (UAX#14)",
+"reviewed":true
+},
+
+"wp": {
+"url":"https://en.wikipedia.org/wiki/Arabic_phonology",
+"authors": "Wikipedia",
+"title":"Arabic phonology",
+},
+
+"wa": {
+"url":"https://en.wikipedia.org/wiki/Arabic_alphabet",
+"authors": "Wikipedia",
+"title":"Arabic alphabet",
+},
+
+"ws": {
+"url":"http://en.wikipedia.org/wiki/Arabic_script",
+"authors": "Wikipedia",
+"title":"Arabic script",
+},
+
+
+}
+
+
+
+// <a class="fn">ws,#tones</a>
