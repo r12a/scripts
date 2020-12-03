7f56ccb4377690b098f3047e538d4571edc6210c
r12a <ishida@w3.org>
2020-12-03 09:08:39 +0000

    ethi, Add refs.js file and populate Add refs links to top of index and block files: Change all .fnref spans to tt In intro, change the links etc.

---
+++ ethiopic/refs.js
@@ -0,0 +1,92 @@
+reflist = {
+
+
+"c": {
+"url":"#refs",
+"authors": "Bernard Comrie (ed)",
+"title":"The Major World's Languages",
+"version":"3rd edition",
+"publisher":"Routledge",
+"isbn":"978-1-315-64493-6",
+},
+
+"d": {
+"url":"#refs",
+"authors": "Peter T. Daniels and William Bright",
+"title":"The World's Writing Systems",
+"publisher":"Oxford University Press",
+"isbn":"0-19-507993-0",
+"reviewed":true
+},
+
+"e": {
+"url":"http://w3c.github.io/elreq/",
+"authors": "Daniel Yacob",
+"title":"Ethiopic Layout Requirements",
+},
+
+"g116": {
+"url":"https://github.com/w3c/elreq/issues/116",
+"authors": "GitHub",
+"title":"Does Ethiopic text also get wrapped by word?",
+},
+
+"m": {
+"url":"#refs",
+"authors": "Anandam Krishnamurthi",
+"title":"Learn Ethiopic in a Month",
+"publisher":"Readwells",
+"isbn":"9788187782049",
+},
+
+"o": {
+"url":"http://www.omniglot.com/writing/ethiopic.htm",
+"authors": "Omniglot",
+"title":"Ethiopic",
+},
+
+"u": {
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch19.pdf",
+"authors": "Unicode Consortium",
+"title":"The Unicode Standard, Version 13.0",
+"sub":"Chapter 19.6: Africa, Bamum",
+"pages":"779-780",
+"isbn":"978-1-936213-16-0.",
+},
+
+"ul": {
+"url":"https://www.unicode.org/reports/tr14/",
+"authors": "Unicode Consortium",
+"title":"Unicode Line Breaking Algorithm (UAX#14)",
+"reviewed":true
+},
+
+"ws": {
+"url":"https://en.wikipedia.org/wiki/Ge%CA%BDez_script",
+"authors": "Wikipedia",
+"title":"Geʽez script",
+},
+
+"wa": {
+"url":"https://en.wikipedia.org/wiki/Help:IPA/Amharic",
+"authors": "Wikipedia",
+"title":"Help:IPA/Amharic",
+},
+
+"wg": {
+"url":"https://en.wikipedia.org/wiki/Ge%CA%BDez",
+"authors": "Wikipedia",
+"title":"Geʽez",
+},
+
+"x": {
+"url":"https://cms.education.gov.il/NR/rdonlyres/9D9F5A60-082D-4DDD-9853-A669B5B9C9E5/132239/Safa.PDF",
+"authors": "ዶ/ር አንበሴ ተፈራ",
+"title":"የኢትዮጵያ ብሔረሰቦችና ቋንቋዎቻቸው አጭር ቅኝት ",
+},
+
+
+}
+
+
+
