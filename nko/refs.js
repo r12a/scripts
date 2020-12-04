52904fb3d4e3c1a723f7f41cfef469deace06519
r12a <ishida@w3.org>
2020-12-04 08:23:31 +0000

    nko, Add refs.js file and populate Add refs links to top of index and block files: Change all .fnref spans to tt In intro, change the links etc.

---
+++ nko/refs.js
@@ -0,0 +1,69 @@
+reflist = {
+
+
+"ec": {
+"url":"https://www.unicode.org/L2/L2005/05237-n2977-nko-jona.pdf",
+"authors": "Michael Everson, Mamady Doumbouya, Baba Mamadi Diané, Karamo Kaba Jammeh",
+"title":"Clarification on the identity and use of three N’Ko letters",
+},
+
+"e4": {
+"url":"https://www.unicode.org/L2/L2015/15338-n4706-nko-additions.pdf",
+"authors": "Michael Everson",
+"title":"Proposal to encode four N’Ko characters in the BMP of the UCS",
+},
+
+"ep": {
+"url":"http://std.dkuug.dk/jtc1/sc2/wg2/docs/n2765.pdf",
+"authors": "Michael Everson, Mamady Doumbouya, Baba Mamadi Diané, Karamo Kaba Jammeh",
+"title":"Proposal to add the N’Ko script to the BMP of the UCS",
+},
+
+"g3": {
+"url":"https://github.com/w3c/afrlreq/issues/3",
+"authors": "GitHub, Afrlreq",
+"title":"Direction of slant for italic/oblique RTL text",
+"reviewed":true
+},
+
+"o": {
+"url":"https://www.omniglot.com/writing/bamum.htm",
+"authors": "Omniglot",
+"title":"Bamum syllabary",
+"reviewed":true
+},
+
+"u": {
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch19.pdf",
+"authors": "Unicode Consortium",
+"title":"The Unicode Standard, Version 13.0",
+"sub":"Chapter 19.4: Africa, N’Ko",
+"pages":"772-776",
+"isbn":"978-1-936213-16-0.",
+},
+
+"ul": {
+"url":"https://www.unicode.org/reports/tr14/",
+"authors": "Unicode Consortium",
+"title":"Unicode Line Breaking Algorithm (UAX#14)",
+"reviewed":true
+},
+
+"ws": {
+"url":"https://en.wikipedia.org/wiki/N%27Ko_alphabet",
+"authors": "Wikipedia",
+"title":"N'Ko alphabet",
+},
+
+"ya": {
+"url":"http://std.dkuug.dk/jtc1/sc2/wg2/docs/n2949.pdf",
+"authors": "François Yergeau, Patrick Andries",
+"title":"For a Correct Encoding of N’Ko",
+},
+
+
+}
+
+
+
+// <a class="fn">ws,#tones</a>
