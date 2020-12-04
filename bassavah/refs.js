368c00a04d781454e06b3f89850c09593833af4b
r12a <ishida@w3.org>
2020-12-04 07:15:20 +0000

    bass, Add refs.js file and populate Add refs links to top of index and block files: Change all .fnref spans to tt In intro, change the links etc.

---
+++ bassavah/refs.js
@@ -0,0 +1,81 @@
+reflist = {
+
+
+"fc": {
+"url":"#refs",
+"authors": "Florian Coulmas",
+"title":"The Blackwell Encyclopedia of Writing Systems",
+"publisher":"Oxford University Press",
+"isbn":"0-631-21481-X",
+"reviewed":true
+},
+
+"cb": {
+"url":"https://cefliberia.org/bassa/bassa-dictionary/",
+"authors": "Christian Education Foundation of Liberia",
+"title":"Bassa-English Dictionary",
+},
+
+"e": {
+"url":"https://www.unicode.org/L2/L2010/10382r-n3941r-bassavah.pdf",
+"authors": "Michael Everson, Charles Riley",
+"title":"Final proposal for encoding the Bassa Vah script in the SMP of the UCS",
+},
+
+"o": {
+"url":"https://www.omniglot.com/writing/bassa.htm",
+"authors": "Omniglot",
+"title":"Bassa Vah",
+"reviewed":true
+},
+
+"rc": {
+"url":"http://std.dkuug.dk/JTC1/SC2/wg2/docs/n3991.pdf",
+"authors": "Riley, Charles",
+"title":"Comments on Bassa Vah Comma",
+"reviewed":true
+},
+
+"sr": {
+"url":"#refs",
+"authors": "Tim Slager, Charles Riley",
+"title":"Personal correspondence",
+"reviewed":true
+},
+
+"s": {
+"url":"https://scriptsource.org/cms/scripts/page.php?item_id=script_detail&key=Bass",
+"authors": "ScriptSource",
+"title":"Bassa Vah",
+"reviewed":true
+},
+
+"u": {
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch19.pdf",
+"authors": "Unicode Consortium",
+"title":"The Unicode Standard, Version 13.0",
+"sub":"Chapter 19.7: Africa, Bassa Vah",
+"pages":"781",
+"isbn":"978-1-936213-16-0.",
+},
+
+"ul": {
+"url":"https://www.unicode.org/reports/tr14/",
+"authors": "Unicode Consortium",
+"title":"Unicode Line Breaking Algorithm (UAX#14)",
+"reviewed":true
+},
+
+"ws": {
+"url":"https://en.wikipedia.org/wiki/Bassa_script",
+"authors": "Wikipedia",
+"title":"Bassa script",
+"reviewed":true
+},
+
+
+}
+
+
+
+// <a class="fn">ws,#tones</a>
