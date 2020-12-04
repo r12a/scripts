00b922fc311c8dcffd0e4af10e6a91eda8ade32d
r12a <ishida@w3.org>
2020-12-04 06:19:18 +0000

    thai, Add refs.js file and populate Add refs links to top of index and block files: Change all .fnref spans to tt In intro, change the links etc.

---
+++ thai/refs.js
@@ -0,0 +1,105 @@
+reflist = {
+
+
+"at": {
+"url":"http://pioneer.netserv.chula.ac.th/~awirote/ling/snlp2007-wirote.pdf",
+"authors": "Wirote Aroonmanakun",
+"title":"Thoughts on Word and Sentence Segmentation in Thai",
+},
+
+"b": {
+"url":"#refs",
+"authors": "Marie-Hélène Brown",
+"title":"Reading and Writing Thai",
+"isbn":"974-210-4506",
+"reviewed":true
+},
+
+"cldr": {
+"url":"https://www.unicode.org/cldr/charts/latest/summary/th.html",
+"authors": "CLDR",
+"version":"v36",
+"title":"Locale Data Summary for Thai [th]",
+},
+
+"d": {
+"url":"#refs",
+"authors": "Peter T. Daniels and William Bright",
+"title":"The World's Writing Systems",
+"publisher":"Oxford University Press",
+"isbn":"0-19-507993-0",
+"reviewed":true
+},
+
+"g2455": {
+"url":"https://github.com/w3c/csswg-drafts/issues/2455",
+"authors": "GitHub, CSS",
+"title":"Questionable Thai words",
+"reviewed":true
+},
+
+"g19": {
+"url":"https://github.com/w3c/sealreq/issues/19",
+"authors": "GitHub, Sealreq",
+"title":"Requirements for justification in Thai",
+"reviewed":true
+},
+
+"p": {
+"url":"#refs",
+"authors": "Benjawan Poomsan Becker",
+"title":"Thai for Beginners",
+"isbn":"1-887521-00-3",
+"reviewed":true
+},
+
+"pws": {
+"url":"http://www.thailandqa.com/forum/showthread.php?4830-Special-characters-in-thai-language",
+"authors": "The Packnam Web Forums",
+"title":"Special characters in thai language",
+"reviewed":true
+},
+
+"s": {
+"url":"http://scriptsource.org/scr/Thai",
+"authors": "ScriptSource",
+"title":"Thai",
+"reviewed":true
+},
+
+"u": {
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch16.pdf",
+"authors": "Unicode Consortium",
+"title":"The Unicode Standard, Version 13.0",
+"sub":"Chapter 16.1: Southeast Asia, Thai",
+"pages":"639-642",
+"isbn":"978-1-936213-16-0.",
+},
+
+"ul": {
+"url":"https://www.unicode.org/reports/tr14/",
+"authors": "Unicode Consortium",
+"title":"Unicode Line Breaking Algorithm (UAX#14)",
+"reviewed":true
+},
+
+"wl": {
+"url":"https://en.wikipedia.org/wiki/Thai_language",
+"authors": "Wikipedia",
+"title":"Thai language",
+"reviewed":true
+},
+
+"ws": {
+"url":"https://en.wikipedia.org/wiki/Bamum_script",
+"authors": "Wikipedia",
+"title":"Bamum script",
+"reviewed":true
+},
+
+
+}
+
+
+
+// <a class="fn">ws,#tones</a>
