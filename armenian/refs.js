8a06946dff6a6f5da2e59e4d41842faa6bf7e80c
r12a <ishida@w3.org>
2020-12-07 07:07:30 +0000

    armn, Add refs.js file and populate Add refs links to top of index and block files: Change all .fnref spans to tt In intro, change the links etc.

---
+++ armenian/refs.js
@@ -0,0 +1,63 @@
+reflist = {
+
+
+"a": {
+"url":"https://www.unicode.org/L2/L2020/20143-armenian-ech-yiwn.pdf",
+"authors": "Deborah Anderson",
+"title":"Uppercase of U+0587 ARMENIAN SMALL LIGATURE ECH YIWN",
+"reviewed":true
+},
+
+"d": {
+"url":"#refs",
+"authors": "Peter T. Daniels and William Bright",
+"title":"The World's Writing Systems",
+"publisher":"Oxford University Press",
+"isbn":"0-19-507993-0",
+},
+
+"e": {
+"url":"http://std.dkuug.dk/jtc1/sc2/wg2/docs/n3924.pdf",
+"authors": "Michael Everson",
+"title":"Proposal to encode two symbols for Armenian in the UCS",
+},
+
+"u": {
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch07.pdf",
+"authors": "Unicode Consortium",
+"title":"The Unicode Standard, Version 13.0",
+"sub":"Chapter 7.6: Europe-I, Armenian",
+"pages":"317-318",
+"isbn":"978-1-936213-16-0.",
+},
+
+"ul": {
+"url":"https://www.unicode.org/reports/tr14/",
+"authors": "Unicode Consortium",
+"title":"Unicode Line Breaking Algorithm (UAX#14)",
+"reviewed":true
+},
+
+"wa": {
+"url":"https://en.wikipedia.org/wiki/Armenian_dram_sign",
+"authors": "Wikipedia",
+"title":"Armenian dram sign",
+},
+
+"wl": {
+"url":"https://en.wikipedia.org/wiki/Armenian_language",
+"authors": "Wikipedia",
+"title":"Armenian language",
+},
+
+"ws": {
+"url":"https://en.wikipedia.org/wiki/Armenian_script",
+"authors": "Wikipedia",
+"title":"Armenian script",
+},
+
+
+}
+
+
+
