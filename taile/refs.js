58e70bbe4bd3108e6ffee6877f9d18cdc68eaad4
r12a <ishida@w3.org>
2020-12-07 07:04:47 +0000

    tale, Add refs.js file and populate Add refs links to top of index and block files: Change all .fnref spans to tt In intro, change the links etc.

---
+++ taile/refs.js
@@ -0,0 +1,44 @@
+reflist = {
+
+
+"s": {
+"url":"https://scriptsource.org/scr/Tale",
+"authors": "ScriptSource",
+"title":"Tai Le",
+"reviewed":true
+},
+
+"t": {
+"url":"http://www.seasite.niu.edu/tai/TaiDehong/",
+"authors": "Center for Southeast Asian Studies",
+"title":"Tai Dehong",
+"reviewed":true
+},
+
+"u": {
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch16.pdf",
+"authors": "Unicode Consortium",
+"title":"The Unicode Standard, Version 13.0",
+"sub":"Chapter 16.5: Southeast Asia, Tai Le",
+"pages":"665-666",
+"isbn":"978-1-936213-16-0.",
+},
+
+"ul": {
+"url":"https://www.unicode.org/reports/tr14/",
+"authors": "Unicode Consortium",
+"title":"Unicode Line Breaking Algorithm (UAX#14)",
+"reviewed":true
+},
+
+"ws": {
+"url":"https://en.wikipedia.org/wiki/Tai_Le_script",
+"authors": "Wikipedia",
+"title":"Tai Le script",
+},
+
+
+}
+
+
+
