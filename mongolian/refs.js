5611fadf5753678db34f965b3f0b957df6f1dbf7
r12a <ishida@w3.org>
2020-12-07 07:05:06 +0000

    mong, Add refs.js file and populate Add refs links to top of index and block files: Change all .fnref spans to tt In intro, change the links etc.

---
+++ mongolian/refs.js
@@ -0,0 +1,95 @@
+reflist = {
+
+
+"b": {
+"url":"http://www.cjvlang.com/Writing/writmongol/mongolalpha.html",
+"authors": "Bathrobe's East Asian Writing Systems",
+"title":"Making Sense of the Traditional Mongolian Script",
+},
+
+"d": {
+"url":"#refs",
+"authors": "Peter T. Daniels and William Bright",
+"title":"The World's Writing Systems",
+"publisher":"Oxford University Press",
+"isbn":"0-19-507993-0",
+"reviewed":true
+},
+
+"e": {
+"url":"https://lists.w3.org/Archives/Public/public-i18n-mongolian/2015JulSep/att-0036/DS05_Mongolian_NNBSP_Connected_Suffixes.pdf",
+"authors": "Greg Eck",
+"title":"Mongolian NNBSP - Connected Suffixes",
+},
+
+"g1": {
+"url":"https://github.com/w3c/mlreq/issues/1",
+"authors": "GitHub (mlreq)",
+"title":"Mongolian ruby requirements",
+},
+
+"g5": {
+"url":"https://github.com/w3c/mlreq/issues/5",
+"authors": "GitHub (mlreq)",
+"title":"Non-Mongolian characters in vertical Mongolian text",
+},
+
+"g9": {
+"url":"https://github.com/w3c/mlreq/issues/9",
+"authors": "GitHub (mlreq)",
+"title":"Underline and NNBSP",
+},
+
+"g10": {
+"url":"https://github.com/w3c/mlreq/issues/10",
+"authors": "GitHub (mlreq)",
+"title":"Mongolian text decoration styles",
+},
+
+"n": {
+"url":"https://w3c.github.io/mlreq/",
+"authors": "Nasun-urt, Hu Qitu",
+"title":"Mongolian Layout Requirements",
+},
+
+"r": {
+"url":"https://www.unicode.org/L2/L2007/07023-bamum-report.pdf",
+"authors": "Riley, Charles",
+"title":"L2/07-023: Towards the Encoding of the Bamum Script in the UCS",
+"reviewed":true
+},
+
+"u": {
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch13.pdf",
+"authors": "Unicode Consortium",
+"title":"The Unicode Standard, Version 13.0",
+"sub":"Chapter 13.5: South and Central Asia-II, Mongolian",
+"pages":"537-546",
+"isbn":"978-1-936213-16-0.",
+},
+
+"ul": {
+"url":"https://www.unicode.org/reports/tr14/",
+"authors": "Unicode Consortium",
+"title":"Unicode Line Breaking Algorithm (UAX#14)",
+"reviewed":true
+},
+
+"wl": {
+"url":"https://en.wikipedia.org/wiki/Mongolian_language",
+"authors": "Wikipedia",
+"title":"Mongolian language",
+},
+
+"ws": {
+"url":"http://en.wikipedia.org/wiki/Mongolian_script",
+"authors": "Wikipedia",
+"title":"Mongolian script",
+},
+
+
+}
+
+
+
+// <a class="fn">ws,#tones</a>
