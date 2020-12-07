4e7b84874f7ea553a9924596fc76fa193170a4f6
r12a <ishida@w3.org>
2020-12-07 07:05:58 +0000

    khmr, Add refs.js file and populate Add refs links to top of index and block files: Change all .fnref spans to tt In intro, change the links etc.

---
+++ khmer/refs.js
@@ -0,0 +1,75 @@
+reflist = {
+
+
+"d": {
+"url":"#refs",
+"authors": "Peter T. Daniels and William Bright",
+"title":"The World's Writing Systems",
+"publisher":"Oxford University Press",
+"isbn":"0-19-507993-0",
+"reviewed":true
+},
+
+"h": {
+"url":"http://www.pratyeka.org/csw/hlp-csw.pdf",
+"authors": "Franklin Huffman",
+"title":"Cambodian System of Writing",
+},
+
+"rh": {
+"url":"#refs",
+"authors": "Richard Gilbert & Sovandy Hang",
+"title":"Cambodian for Beginners",
+"publisher":"Paiboon Publishing",
+"isbn":"1-887521-35-6",
+"reviewed":true
+},
+
+"o": {
+"url":"https://www.omniglot.com/writing/bamum.htm",
+"authors": "Omniglot",
+"title":"Bamum syllabary",
+"reviewed":true
+},
+
+"sil": {
+"url":"http://scripts.sil.org/cms/scripts/render_download.php?format=file&media_id=Mondulkiri-5.513-Ortho&filename=Mondulkiri-5.513-Ortho.pdf",
+"authors": "SIL",
+"title":"The order of components in Khmer orthographical syllables",
+"reviewed":true
+},
+
+"u": {
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch16.pdf",
+"authors": "Unicode Consortium",
+"title":"The Unicode Standard, Version 13.0",
+"sub":"Chapter 16.4: Southeast Asia, Khmer",
+"pages":"653-664",
+"isbn":"978-1-936213-16-0.",
+},
+
+"ul": {
+"url":"https://www.unicode.org/reports/tr14/",
+"authors": "Unicode Consortium",
+"title":"Unicode Line Breaking Algorithm (UAX#14)",
+"reviewed":true
+},
+
+"wp": {
+"url":"https://en.wikipedia.org/wiki/Khmer_phonology",
+"authors": "Wikipedia",
+"title":"Khmer phonology",
+},
+
+"ws": {
+"url":"https://en.wikipedia.org/wiki/Khmer_alphabet",
+"authors": "Wikipedia",
+"title":"Khmer alphabet",
+},
+
+
+}
+
+
+
+// <a class="fn">ws,#tones</a>
