fcb3602735f9bd086ce0daaadd9fee15cacc7224
r12a <ishida@w3.org>
2020-12-07 07:07:00 +0000

    bali, Add refs.js file and populate Add refs links to top of index and block files: Change all .fnref spans to tt In intro, change the links etc.

---
+++ balinese/refs.js
@@ -0,0 +1,88 @@
+reflist = {
+
+
+"d": {
+"url":"#refs",
+"authors": "Peter T. Daniels and William Bright",
+"title":"The World's Writing Systems",
+"publisher":"Oxford University Press",
+"isbn":"0-19-507993-0",
+"reviewed":true
+},
+
+"e": {
+"url":"http://std.dkuug.dk/JTC1/sc2/WG2/docs/n2908.pdf",
+"authors": "Michael Everson, I Made Suatjana",
+"title":"Proposal for encoding the Balinese script in the UCS",
+},
+
+"loc": {
+"url":"http://www.loc.gov/catdir/cpso/romanization/balinese.pdf",
+"authors": "Library of Congress",
+"title":"Balinese transcription",
+"reviewed":true
+},
+
+"lb": {
+"url":"https://lindenbergsoftware.com/en/notes/bringing-balinese-to-ios/index.html",
+"authors": "Norbert Lindenberg",
+"title":"Bringing Balinese to iOS",
+"reviewed":true
+},
+
+"o": {
+"url":"http://scriptsource.org/cms/scripts/page.php?item_id=source_detail&uid=nrgqwv9x7a",
+"authors": "Omniglot",
+"title":"Balinese",
+"reviewed":true
+},
+
+"s": {
+"url":"http://scriptsource.org/cms/scripts/page.php?item_id=script_detail&uid=aggc6af8cc",
+"authors": "ScriptSource",
+"title":"Balinese",
+"reviewed":true
+},
+
+"sb": {
+"url":"http://www.babadbali.com/aksarabali/alphabet.htm",
+"authors": "Ida Bagus Adi Sudewa",
+"title":"The Balinese Alphabet",
+"reviewed":true
+},
+
+"u": {
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch17.pdf",
+"authors": "Unicode Consortium",
+"title":"The Unicode Standard, Version 13.0",
+"sub":"Chapter 17.3: Indonesia and Oceania, Balinese",
+"pages":"691-696",
+"isbn":"978-1-936213-16-0.",
+},
+
+"ul": {
+"url":"https://www.unicode.org/reports/tr14/",
+"authors": "Unicode Consortium",
+"title":"Unicode Line Breaking Algorithm (UAX#14)",
+"reviewed":true
+},
+
+"wl": {
+"url":"https://en.wikipedia.org/wiki/Bamum_language",
+"authors": "Wikipedia",
+"title":"Bamum language",
+"reviewed":true
+},
+
+"ws": {
+"url":"http://en.wikipedia.org/wiki/Balinese_script",
+"authors": "Wikipedia",
+"title":"Balinese script",
+},
+
+
+}
+
+
+
+// <a class="fn">ws,#tones</a>
