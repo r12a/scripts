786a59569c33626e3c4f44cd7979875669984908
r12a <ishida@w3.org>
2020-12-07 07:04:28 +0000

    tavt, Add refs.js file and populate Add refs links to top of index and block files: Change all .fnref spans to tt In intro, change the links etc.

---
+++ taiviet/refs.js
@@ -0,0 +1,65 @@
+reflist = {
+
+
+"d": {
+"url":"#refs",
+"authors": "Peter T. Daniels and William Bright",
+"title":"The World's Writing Systems",
+"publisher":"Oxford University Press",
+"isbn":"0-19-507993-0",
+},
+
+"b": {
+"url":"http://std.dkuug.dk/jtc1/sc2/wg2/docs/n3220.pdf",
+"authors": "Jim Brase",
+"title":"Proposal to encode the Tai Viet script in the UCS",
+},
+
+"btd": {
+"url":"https://www.unicode.org/L2/L2008/08217-tai-don.pdf",
+"authors": "Jim Brase",
+"title":"Writing Tai Don",
+},
+
+"s": {
+"url":"https://software.sil.org/taiheritage/wp-content/uploads/sites/44/2017/10/TaiHeritagePro-features.pdf",
+"authors": "SIL",
+"title":"User selectable features in Tai Heritage Pro",
+"reviewed":true
+},
+
+"u": {
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch16.pdf",
+"authors": "Unicode Consortium",
+"title":"The Unicode Standard, Version 13.0",
+"sub":"Chapter 16.8: Southeast Asia, Tai Viet",
+"pages":"673-675",
+"isbn":"978-1-936213-16-0.",
+},
+
+"ul": {
+"url":"https://www.unicode.org/reports/tr14/",
+"authors": "Unicode Consortium",
+"title":"Unicode Line Breaking Algorithm (UAX#14)",
+"reviewed":true
+},
+
+"wl": {
+"url":"https://en.wikipedia.org/wiki/Bamum_language",
+"authors": "Wikipedia",
+"title":"Bamum language",
+"reviewed":true
+},
+
+"ws": {
+"url":"https://en.wikipedia.org/wiki/Bamum_script",
+"authors": "Wikipedia",
+"title":"Bamum script",
+"reviewed":true
+},
+
+
+}
+
+
+
