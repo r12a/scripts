9781168c30a3d244ce7a5fe1f65fd634b4cdeae1
r12a <ishida@w3.org>
2020-12-07 07:04:07 +0000

    tfng, Add refs.js file and populate Add refs links to top of index and block files: Change all .fnref spans to tt In intro, change the links etc.

---
+++ tifinagh/refs.js
@@ -0,0 +1,83 @@
+reflist = {
+
+
+"ae": {
+"url":"https://www.unicode.org/L2/L2010/10278-tifinagh-evolution.pdf",
+"authors": "Paul Anderson",
+"title":"Evolution of the Tifinagh script in Unicode",
+},
+
+"ap": {
+"url":"https://www.unicode.org/L2/L2010/10096-tifinagh-vowels.pdf",
+"authors": "Paul Anderson",
+"title":"Proposal to add two Tifinagh characters for vowels in Tuareg language variants",
+},
+
+"bt": {
+"url":"https://www.academia.edu/16657385/Tifinagh_and_the_IRCAM_Explorations_in_Cursiveness_and_Bicamelarism_in_the_Tifinagh_script",
+"authors": "Juan Luis Blanco",
+"title":"Tifinagh & the IRCAM",
+},
+
+"d": {
+"url":"#refs",
+"authors": "Peter T. Daniels and William Bright",
+"title":"The World's Writing Systems",
+"publisher":"Oxford University Press",
+"isbn":"0-19-507993-0",
+"reviewed":true
+},
+
+"pcd": {
+"url":"http://std.dkuug.dk/JTC1/SC2/WG2/docs/n4069.pdf",
+"authors": "Lorna A. Priest, Jon Coblentz",
+"title":"Documenting a fallback glyph for Tifinagh Consonant Joiner",
+},
+
+"pcs": {
+"url":"http://std.dkuug.dk/jtc1/sc2/wg2/docs/n3482.pdf",
+"authors": "Lorna A. Priest, Jon Coblentz, Andrew Savage",
+"title":"Proposal to encode additional Tifinagh characters",
+},
+
+"s": {
+"url":"http://scriptsource.org/cms/scripts/page.php?item_id=script_detail&key=Tfng",
+"authors": "ScriptSource",
+"title":"Tifinagh (Berber)",
+"reviewed":true
+},
+
+"u": {
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch19.pdf",
+"authors": "Unicode Consortium",
+"title":"The Unicode Standard, Version 13.0",
+"sub":"Chapter 19.3: Africa, Tifinagh",
+"pages":"769-771",
+"isbn":"978-1-936213-16-0.",
+},
+
+"ul": {
+"url":"https://www.unicode.org/reports/tr14/",
+"authors": "Unicode Consortium",
+"title":"Unicode Line Breaking Algorithm (UAX#14)",
+"reviewed":true
+},
+
+"wb": {
+"url":"https://en.wikipedia.org/wiki/Berber_Latin_alphabet",
+"authors": "Wikipedia",
+"title":"Berber Latin alphabet",
+},
+
+"ws": {
+"url":"http://en.wikipedia.org/wiki/Tifinagh",
+"authors": "Wikipedia",
+"title":"Tifinagh",
+},
+
+
+}
+
+
+
+// <a class="fn">ws,#tones</a>
