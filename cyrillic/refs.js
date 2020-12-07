8615ef3da2cbf0f9ec4538492acb0d3ffd57b390
r12a <ishida@w3.org>
2020-12-07 07:06:42 +0000

    cyrl, Add refs.js file and populate Add refs links to top of index and block files: Change all .fnref spans to tt In intro, change the links etc.

---
+++ cyrillic/refs.js
@@ -0,0 +1,78 @@
+reflist = {
+
+
+"cldr": {
+"url":"http://localhost/scripts/cyrillic/#https://unicode-org.github.io/cldr-staging/charts/37/summary/ru.html",
+"authors": "CLDR",
+"title":"Locale Data Summary for Russian [ru]",
+},
+
+"d": {
+"url":"#refs",
+"authors": "Peter T. Daniels and William Bright",
+"title":"The World's Writing Systems",
+"publisher":"Oxford University Press",
+"isbn":"0-19-507993-0",
+"pages":"408-412",
+},
+
+"s": {
+"url":"http://scriptsource.org/cms/scripts/page.php?item_id=entry_detail&uid=k9ew9c6yam",
+"authors": "ScriptSource",
+"title":"Glyph Variant for U+0306",
+"reviewed":true
+},
+
+"u": {
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch07.pdf",
+"authors": "Unicode Consortium",
+"title":"The Unicode Standard, Version 13.0",
+"sub":"Chapter 7.4: Europe-I, Cyrillic",
+"pages":"311-315",
+"isbn":"978-1-936213-16-0.",
+},
+
+"ul": {
+"url":"https://www.unicode.org/reports/tr14/",
+"authors": "Unicode Consortium",
+"title":"Unicode Line Breaking Algorithm (UAX#14)",
+"reviewed":true
+},
+
+"wa": {
+"url":"https://en.wikipedia.org/wiki/Russian_alphabet",
+"authors": "Wikipedia",
+"title":"Russian alphabet",
+},
+
+"wl": {
+"url":"https://en.wikipedia.org/wiki/Bamum_language",
+"authors": "Wikipedia",
+"title":"Bamum language",
+"reviewed":true
+},
+
+"wp": {
+"url":"https://en.wikipedia.org/wiki/Russian_phonology",
+"authors": "Wikipedia",
+"title":"Russian phonology",
+},
+
+"wq": {
+"url":"https://en.wikipedia.org/wiki/Quotation_mark",
+"authors": "Wikipedia",
+"title":"Quotation mark",
+},
+
+"ws": {
+"url":"http://en.wikipedia.org/wiki/Cyrillic_script",
+"authors": "Wikipedia",
+"title":"Cyrillic script",
+"reviewed":true
+},
+
+
+}
+
+
+
