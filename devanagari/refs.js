583c6cea57b1249d84370e685a8169aa70abaca2
r12a <ishida@w3.org>
2020-12-08 15:42:13 +0000

    deva, Add refs.js file and populate Add refs links to top of index and block files: Change all .fnref spans to tt In intro, change the links etc.

---
+++ devanagari/refs.js
@@ -0,0 +1,73 @@
+reflist = {
+
+
+"cldr": {
+"url":"https://www.unicode.org/cldr/charts/latest/summary/hi.html",
+"authors": "CLDR",
+"title":"Hindi",
+},
+
+"d": {
+"url":"#refs",
+"authors": "Peter T. Daniels and William Bright",
+"title":"The World's Writing Systems",
+"publisher":"Oxford University Press",
+"isbn":"0-19-507993-0",
+"pages":"487-497",
+},
+
+"i": {
+"url":"https://www.w3.org/TR/ilreq/",
+"authors": " Swaran Lata, Somnath Chandra, Prashant Verma",
+"title":"Indic Layout Requirements",
+},
+
+"r": {
+"url":"https://www.unicode.org/L2/L2007/07023-bamum-report.pdf",
+"authors": "Riley, Charles",
+"title":"L2/07-023: Towards the Encoding of the Bamum Script in the UCS",
+"reviewed":true
+},
+
+"s": {
+"url":"#refs",
+"authors": "Rupert Snell & Simon Weightman",
+"title":"Teach Yourself Hindi",
+"publisher":"Hodder & Stoughton",
+"isbn":"978-0-340-86687-0",
+},
+
+"u": {
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch12.pdf",
+"authors": "Unicode Consortium",
+"title":"The Unicode Standard, Version 13.0",
+"sub":"Chapter 12.1: South and Central Asia-I, Devanagari",
+"pages":"447-472",
+"isbn":"978-1-936213-16-0.",
+},
+
+"ul": {
+"url":"https://www.unicode.org/reports/tr14/",
+"authors": "Unicode Consortium",
+"title":"Unicode Line Breaking Algorithm (UAX#14)",
+"reviewed":true
+},
+
+"wp": {
+"url":"https://en.wikipedia.org/wiki/Hindustani_phonology",
+"authors": "Wikipedia",
+"title":"Hindustani phonology",
+"reviewed":true
+},
+
+"ws": {
+"url":"https://en.wikipedia.org/wiki/Devanagari",
+"authors": "Wikipedia",
+"title":"Devanagari",
+},
+
+
+}
+
+
+
