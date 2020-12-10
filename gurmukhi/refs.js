c4c6c8369d7c00dfce7609f5ceeb9d54acaf63c5
r12a <ishida@w3.org>
2020-12-10 06:30:02 +0000

    guru, Latest template changes

---
+++ gurmukhi/refs.js
@@ -0,0 +1,64 @@
+reflist = {
+
+
+"b": {
+"url":"https://scholarsarchive.byu.edu/cgi/viewcontent.cgi?article=3982&context=etd",
+"authors": "Andrea Lynn Bowden",
+"title":"Punjabi Tonemics and the Gurmukhi Script: A Preliminary Study",
+},
+
+"d": {
+"url":"#refs",
+"authors": "Peter T. Daniels and William Bright",
+"title":"The World's Writing Systems",
+"publisher":"Oxford University Press",
+"isbn":"0-19-507993-0",
+},
+
+"o": {
+"url":"http://www.omniglot.com/writing/punjabi.htm",
+"authors": "Omniglot",
+"title":"Punjabi",
+"reviewed":true
+},
+
+"s": {
+"url":"http://unicode.org/L2/L2006/06037-yakash.pdf",
+"authors": "Sukhjinder Sidhu",
+"title":"Proposal to encode Gurmukhi Sign Yakash",
+},
+
+"u": {
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch12.pdf",
+"authors": "Unicode Consortium",
+"title":"The Unicode Standard, Version 13.0",
+"sub":"Chapter 12.3: South and Central Asia-I, Gurmukhi",
+"pages":"480-484",
+"isbn":"978-1-936213-16-0.",
+},
+
+"ul": {
+"url":"https://www.unicode.org/reports/tr14/",
+"authors": "Unicode Consortium",
+"title":"Unicode Line Breaking Algorithm (UAX#14)",
+"reviewed":true
+},
+
+"wl": {
+"url":"https://en.wikipedia.org/wiki/Bamum_language",
+"authors": "Wikipedia",
+"title":"Bamum language",
+},
+
+"ws": {
+"url":"http://en.wikipedia.org/wiki/Gurmukhi_script",
+"authors": "Wikipedia",
+"title":"Gurmukhi script",
+},
+
+
+}
+
+
+
+// <a class="fn">ws,#tones</a>
