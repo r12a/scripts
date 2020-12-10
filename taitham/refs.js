30b29e75a4a87d77be49caf49c07f1cdbc8359c9
r12a <ishida@w3.org>
2020-12-10 14:38:38 +0000

    kkh, nod, Add new refs code

---
+++ taitham/refs.js
@@ -0,0 +1,79 @@
+reflist = {
+
+
+"c": {
+"url":"#refs",
+"authors": "Patrick Chew",
+"title":"Various private correspondence",
+},
+
+"d": {
+"url":"#refs",
+"authors": "Peter T. Daniels and William Bright",
+"title":"The World's Writing Systems",
+"publisher":"Oxford University Press",
+"isbn":"0-19-507993-0",
+},
+
+"e": {
+"url":"http://std.dkuug.dk/jtc1/sc2/wg2/docs/n3207.pdf",
+"authors": "Michael Everson, Martin Hosken, Peter Constable",
+"title":"Revised proposal for encoding the Lanna script in the BMP of the UCS",
+},
+
+"h": {
+"url":"#refs",
+"authors": "Martin Hosken",
+"title":"Structuring Tai Tham Unicode",
+},
+
+"o": {
+"url":"https://evols.library.manoa.hawaii.edu/bitstream/10524/52403/09_Owen2017description.pdf",
+"authors": "R Wyn Owen",
+"title":"A description and linguistic analysis of the Tai Khuen writing system",
+"reviewed":true
+},
+
+"r": {
+"url":"#refs",
+"authors": "Richard Wordingham",
+"title":"Personal correspondence",
+"reviewed":true
+},
+
+"u": {
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch16.pdf",
+"authors": "Unicode Consortium",
+"title":"The Unicode Standard, Version 13.0",
+"sub":"Chapter 16.7: Southeast Asia, Tai Tham",
+"pages":"670-672",
+"isbn":"978-1-936213-16-0.",
+},
+
+"ul": {
+"url":"https://www.unicode.org/reports/tr14/",
+"authors": "Unicode Consortium",
+"title":"Unicode Line Breaking Algorithm (UAX#14)",
+"reviewed":true
+},
+
+"w": {
+"url":"http://wrdingham.co.uk/lanna/renderer_test.htm",
+"authors": "Richard Wordingham",
+"title":"Tai Tham Renderer Tests",
+"reviewed":true
+},
+
+"wnl": {
+"url":"https://en.wikipedia.org/wiki/Northern_Thai_language",
+"authors": "Wikipedia",
+"title":"Northern Thai language",
+"reviewed":true
+},
+
+
+}
+
+
+
+// <a class="fn">ws,#tones</a>
