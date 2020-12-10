f5936cac81df96fa3e4269af792cc4a0b6288827
r12a <ishida@w3.org>
2020-12-10 06:30:22 +0000

    mlym, Latest template changes

---
+++ malayalam/refs.js
@@ -0,0 +1,39 @@
+reflist = {
+
+
+"d": {
+"url":"#refs",
+"authors": "Peter T. Daniels and William Bright",
+"title":"The World's Writing Systems",
+"publisher":"Oxford University Press",
+"isbn":"0-19-507993-0",
+"reviewed":true
+},
+
+"u": {
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch12.pdf",
+"authors": "Unicode Consortium",
+"title":"The Unicode Standard, Version 13.0",
+"sub":"Chapter 12.9: South and Central Asia-I, Malayalam",
+"pages":"508-515",
+"isbn":"978-1-936213-16-0.",
+},
+
+"ul": {
+"url":"https://www.unicode.org/reports/tr14/",
+"authors": "Unicode Consortium",
+"title":"Unicode Line Breaking Algorithm (UAX#14)",
+"reviewed":true
+},
+
+"ws": {
+"url":"http://en.wikipedia.org/wiki/Malayalam_script",
+"authors": "Wikipedia",
+"title":"Malayalam script",
+},
+
+
+}
+
+
+
