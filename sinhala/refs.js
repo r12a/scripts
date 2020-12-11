47147437cda2aaeb2a586f00f18a1cd7d4631a8e
r12a <ishida@w3.org>
2020-12-11 10:01:24 +0000

    sinh, Upgrade template etc

---
+++ sinhala/refs.js
@@ -0,0 +1,58 @@
+reflist = {
+
+
+"cldr": {
+"url":"https://www.unicode.org/cldr/charts/latest/summary/si.html",
+"authors": "CLDR",
+"title":"Sinhala",
+"reviewed":true
+},
+
+"d": {
+"url":"#refs",
+"authors": "Peter T. Daniels and William Bright",
+"title":"The World's Writing Systems",
+"publisher":"Oxford University Press",
+"isbn":"0-19-507993-0",
+},
+
+"s": {
+"url":"http://std.dkuug.dk/jtc1/sc2/wg2/docs/n2737.pdf",
+"authors": "Srilankan Sectoral Committee on Information Technology",
+"title":"Sri Lanka Standard, Sinhala Character Code for Information Exchange",
+"reviewed":true
+},
+
+"u": {
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch13.pdf",
+"authors": "Unicode Consortium",
+"title":"The Unicode Standard, Version 13.0",
+"sub":"Chapter 13.2: South and Central Asia-II, Sinhala",
+"pages":"521-522",
+"isbn":"978-1-936213-16-0.",
+},
+
+"ul": {
+"url":"https://www.unicode.org/reports/tr14/",
+"authors": "Unicode Consortium",
+"title":"Unicode Line Breaking Algorithm (UAX#14)",
+"reviewed":true
+},
+
+"wl": {
+"url":"https://en.wikipedia.org/wiki/Sinhala_language",
+"authors": "Wikipedia",
+"title":"Sinhala language",
+},
+
+"ws": {
+"url":"http://en.wikipedia.org/wiki/Sinhalese_alphabet",
+"authors": "Wikipedia",
+"title":"Sinhalese alphabet",
+},
+
+
+}
+
+
+
