66e61c2365d2ff1e5ee2def1be619df24694769f
r12a <ishida@w3.org>
2020-12-17 15:28:49 +0000

    telu, Add all latest template & style updates Change default font to serif + vocab file

---
+++ telugu/refs.js
@@ -0,0 +1,63 @@
+reflist = {
+
+
+"d": {
+"url":"#refs",
+"authors": "Peter T. Daniels and William Bright",
+"title":"The World's Writing Systems",
+"publisher":"Oxford University Press",
+"isbn":"0-19-507993-0",
+"reviewed":true
+},
+
+"cldr": {
+"url":"https://www.unicode.org/cldr/charts/latest/summary/te.html",
+"authors": "CLDR",
+"title":"Telugu",
+},
+
+"s": {
+"url":"https://unicode.org/L2/L2010/10392r2-chandrabindus.pdf",
+"authors": "Shriramana Sharma",
+"title":"Request to encode South Indian CANDRABINDU-s",
+},
+
+"u": {
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch12.pdf",
+"authors": "Unicode Consortium",
+"title":"The Unicode Standard, Version 13.0",
+"sub":"Chapter 12.7: South and Central Asia-I, Telugu",
+"pages":"501-503",
+"isbn":"978-1-936213-16-0.",
+},
+
+"ul": {
+"url":"https://www.unicode.org/reports/tr14/",
+"authors": "Unicode Consortium",
+"title":"Unicode Line Breaking Algorithm (UAX#14)",
+"reviewed":true
+},
+
+"wl": {
+"url":"http://en.wikipedia.org/wiki/Telugu_language",
+"authors": "Wikipedia",
+"title":"Telugu language",
+},
+
+"w": {
+"url":"http://en.wikipedia.org/wiki/Telugu_script",
+"authors": "Wikipedia",
+"title":"Telugu_script",
+},
+
+"wy": {
+"url":"https://en.wiktionary.org/wiki/Wiktionary:Main_Page",
+"authors": "Wikipedia",
+"title":"Wiktionary",
+},
+
+
+}
+
+
+
