b0561f298a56d51f5c1469ab41aeb14872adfbad
r12a <ishida@w3.org>
2020-12-18 12:15:16 +0000

    han, use block examples; add refs

---
+++ han/refs.js
@@ -0,0 +1,45 @@
+reflist = {
+
+
+"c": {
+"url":"https://w3c.github.io/clreq/",
+"authors":"董福興, 陳奕鈞, 刘庆, 陈慧晶, 薛富侨, Richard Ishida",
+"title": "Requirements for Chinese Text Layout 中文排版需求",
+},
+
+"d": {
+"url":"#refs",
+"authors": "Peter T. Daniels and William Bright",
+"title":"The World's Writing Systems",
+"publisher":"Oxford University Press",
+"isbn":"0-19-507993-0",
+"reviewed":true
+},
+
+"u": {
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch18.pdf",
+"authors": "Unicode Consortium",
+"title":"The Unicode Standard, Version 13.0",
+"sub":"Chapter 18.1: East Asia, Han",
+"pages":"712-731",
+"isbn":"978-1-936213-16-0.",
+},
+
+"ul": {
+"url":"https://www.unicode.org/reports/tr14/",
+"authors": "Unicode Consortium",
+"title":"Unicode Line Breaking Algorithm (UAX#14)",
+"reviewed":true
+},
+
+"wp": {
+"url":"https://en.wikipedia.org/wiki/Standard_Chinese_phonology",
+"authors": "Wikipedia",
+"title":"Standard Chinese phonology",
+},
+
+
+}
+
+
+
