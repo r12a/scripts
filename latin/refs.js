874a021eb467762ae221430b2749aff99fdfd3bd
r12a <ishida@w3.org>
2020-12-18 17:52:36 +0000

    latn-ha, update template; use block examples; + vocab lister

---
+++ latin/refs.js
@@ -0,0 +1,61 @@
+reflist = {
+
+
+"c": {
+"url":"#refs",
+"authors": " Bernard Comrie",
+"title":"The World's Major Languages",
+"publisher":"Oxford University Press",
+"isbn":"978-1138184824",
+},
+
+"d": {
+"url":"#refs",
+"authors": "Peter T. Daniels and William Bright",
+"title":"The World's Writing Systems",
+"publisher":"Oxford University Press",
+"isbn":"0-19-507993-0",
+"reviewed":true
+},
+
+"o": {
+"url":"http://www.omniglot.com/writing/hausa.htm",
+"authors": "Omniglot",
+"title":"Hausa (Harshen Hausa / هَرْشَن هَوْسَ)",
+"reviewed":true
+},
+
+"p": {
+"url":"http://www.bisharat.net/wikidoc/pmwiki.php/PanAfrLoc/Hausa",
+"authors": "Pan AfriLoc",
+"title":"Hausa - Haoussa - هوس",
+"reviewed":true
+},
+
+"u": {
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch19.pdf",
+"authors": "Unicode Consortium",
+"title":"The Unicode Standard, Version 13.0",
+"sub":"Chapter 19.6: Africa, Bamum",
+"pages":"779-780",
+"isbn":"978-1-936213-16-0.",
+},
+
+"ul": {
+"url":"https://www.unicode.org/reports/tr14/",
+"authors": "Unicode Consortium",
+"title":"Unicode Line Breaking Algorithm (UAX#14)",
+"reviewed":true
+},
+
+"wha": {
+"url":"https://en.wikipedia.org/wiki/Hausa_language",
+"authors": "Wikipedia",
+"title":"Hausa language",
+},
+
+
+}
+
+
+
