d8a8ae56f93702922f691518a950e93df08916c2
r12a <ishida@w3.org>
2020-12-18 11:44:21 +0000

    jpan, use block examples; add refs

---
+++ jpan/refs.js
@@ -0,0 +1,78 @@
+reflist = {
+
+
+"d": {
+"url":"#refs",
+"authors": "Peter T. Daniels and William Bright",
+"title":"The World's Writing Systems",
+"publisher":"Oxford University Press",
+"isbn":"0-19-507993-0",
+"reviewed":true
+},
+
+"j": {
+"url":"https://w3c.github.io/jlreq/",
+"authors":"千葉 弘幸, 枝本 順三郎, Richard Ishida, 石野 恵一郎, 加藤 誠一, 小林 龍生, 小林 敏, Nathaniel McCully (2nd edition), 小野澤 賢三, Felix Sasaki (1st edition), 下農 淳司 (2nd edition), 塩澤 元 (2nd edition), 薛 富僑 (第２版 2nd edition), 阿南 康宏 (1st edition)",
+"title": "Requirements for Japanese Text Layout 日本語組版処理の要件（日本語版）, 2nd edition",
+},
+
+"s": {
+"url":"http://scriptsource.org/scr/Jpan",
+"authors": "ScriptSource",
+"title":"Japanese",
+},
+
+"uh": {
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch18.pdf",
+"authors": "Unicode Consortium",
+"title":"The Unicode Standard, Version 13.0",
+"sub":"Chapter 18.1: East Asia, Han",
+"pages":"712-731",
+"isbn":"978-1-936213-16-0.",
+},
+
+"uk": {
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch18.pdf",
+"authors": "Unicode Consortium",
+"title":"The Unicode Standard, Version 13.0",
+"sub":"Chapter 18.1: East Asia, Hiragana and Katakana",
+"pages":"739-742",
+"isbn":"978-1-936213-16-0.",
+},
+
+"ul": {
+"url":"https://www.unicode.org/reports/tr14/",
+"authors": "Unicode Consortium",
+"title":"Unicode Line Breaking Algorithm (UAX#14)",
+"reviewed":true
+},
+
+"w": {
+"url":"https://en.wikipedia.org/wiki/Japanese_grammar",
+"authors": "Wikipedia",
+"title":"Japanese grammar",
+},
+
+"wj": {
+"url":"https://en.wikipedia.org/wiki/Jinmeiy%C5%8D_kanji",
+"authors": "Wikipedia",
+"title":"Jinmeiyō kanji",
+},
+
+"wjy": {
+"url":"https://en.wikipedia.org/wiki/J%C5%8Dy%C5%8D_kanji",
+"authors": "Wikipedia",
+"title":"Jōyō kanji",
+},
+
+"wy": {
+"url":"https://en.wikipedia.org/wiki/Yotsugana",
+"authors": "Wikipedia",
+"title":"Yotsugana",
+},
+
+
+}
+
+
+
