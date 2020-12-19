df5e13bb6498b04586ed1c16c0c1d647d9a6abfa
r12a <ishida@w3.org>
2020-12-19 06:12:39 +0000

    dv, update template; use block examples

---
+++ thaana/refs.js
@@ -0,0 +1,44 @@
+reflist = {
+
+
+"d": {
+"url":"#refs",
+"authors": "Peter T. Daniels and William Bright",
+"title":"The World's Writing Systems",
+"publisher":"Oxford University Press",
+"isbn":"0-19-507993-0",
+},
+
+"u": {
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch13.pdf",
+"authors": "Unicode Consortium",
+"title":"The Unicode Standard, Version 13.0",
+"sub":"Chapter 13.1: South and Central Asia-II, Thaana",
+"pages":"519-520",
+"isbn":"978-1-936213-16-0.",
+},
+
+"ul": {
+"url":"https://www.unicode.org/reports/tr14/",
+"authors": "Unicode Consortium",
+"title":"Unicode Line Breaking Algorithm (UAX#14)",
+"reviewed":true
+},
+
+"w": {
+"url":"http://en.wikipedia.org/wiki/Thaana",
+"authors": "Wikipedia",
+"title":"Thaana",
+},
+
+"wp": {
+"url":"https://en.wikipedia.org/wiki/Maldivian_phonology",
+"authors": "Wikipedia",
+"title":"Maldivian phonology",
+},
+
+
+}
+
+
+
