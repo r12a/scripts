a961201bcbb4b037a2b8a7a39a5d817bfe073123
r12a <ishida@w3.org>
2021-03-04 12:14:30 +0000

    oriya: Banking early draft

---
+++ oriya/or-examples.js
@@ -0,0 +1,19 @@
+/*
+@ Source: 
+§ 'Noto Sans Oriya'
+# 200
+~ ltr
+! or
+*/
+
+autoExpandExamples.or = `
+
+କୃମି|worm|krumi
+ମୁଁହ|mouth|mũhɔ
+ଦ୍ୱାର|door|dwara
+ଆମ୍ବ|mango|amba
+ଅକ୍ଷର|character|aksara
+
+
+`
+
---
+++ oriya/refs.js
@@ -0,0 +1,75 @@
+reflist = {
+
+
+"d": {
+"url":"#refs",
+"authors": "Peter T. Daniels and William Bright",
+"title":"The World's Writing Systems",
+"publisher":"Oxford University Press",
+"isbn":"0-19-507993-0",
+"reviewed":true
+},
+
+"e02": {
+"url":"http://std.dkuug.dk/jtc1/sc2/wg2/docs/n2525.pdf",
+"authors": "Michael Everson and Anthony Stone",
+"title":"On Oriya VA and WA, and a proposal to encode one Oriya letter in the UCS",
+},
+
+"o": {
+"url":"https://www.omniglot.com/writing/bamum.htm",
+"authors": "Omniglot",
+"title":"Bamum syllabary",
+"reviewed":true
+},
+
+"r": {
+"url":"https://www.unicode.org/L2/L2007/07023-bamum-report.pdf",
+"authors": "Riley, Charles",
+"title":"L2/07-023: Towards the Encoding of the Bamum Script in the UCS",
+"reviewed":true
+},
+
+"sl2011": {
+"url":"https://www.unicode.org/L2/L2012/12380-odia-naming.pdf",
+"authors": "Swaran Lata",
+"title":"L2/12-380: Letter requesting change of name in Unicode",
+"reviewed":true
+},
+
+"u": {
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch12.pdf",
+"authors": "Unicode Consortium",
+"title":"The Unicode Standard, Version 13.0",
+"sub":"Chapter 12.5: South and Central Asia I, Oriya (Odia)",
+"pages":"487-490",
+"isbn":"978-1-936213-16-0.",
+},
+
+"ul": {
+"url":"https://www.unicode.org/reports/tr14/",
+"authors": "Unicode Consortium",
+"title":"Unicode Line Breaking Algorithm (UAX#14)",
+"reviewed":true
+},
+
+"wl": {
+"url":"https://en.wikipedia.org/wiki/Bamum_language",
+"authors": "Wikipedia",
+"title":"Bamum language",
+"reviewed":true
+},
+
+"ws": {
+"url":"https://en.wikipedia.org/wiki/Bamum_script",
+"authors": "Wikipedia",
+"title":"Bamum script",
+"reviewed":true
+},
+
+
+}
+
+
+
+// <a class="fn">ws,#tones</a>
