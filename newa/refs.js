007c3c6c217023adcb74a70f6482355846772f04
r12a <ishida@w3.org>
2021-03-09 11:13:58 +0000

    newa: Banking early draft

---
+++ newa/refs.js
@@ -0,0 +1,69 @@
+reflist = {
+
+
+"d": {
+"url":"#refs",
+"authors": "Peter T. Daniels and William Bright",
+"title":"The World's Writing Systems",
+"publisher":"Oxford University Press",
+"isbn":"0-19-507993-0",
+"reviewed":true
+},
+
+"m": {
+"url":"https://www.unicode.org/L2/L2012/12349-nepaal-lipi.pdf",
+"authors": "Dev Dass Manandhar, Samir Karmacharya and Bishnu Chitrakar",
+"title":"Proposal for the Nepaalalipi script in ISO/IEC 10646",
+"reviewed":false
+},
+
+"nbl": {
+"url":"https://nepalbhasalearning.wordpress.com/",
+"authors": "Supriya Manandhar, Rukshana Kapali, Roshan Shrestha, Pujan Tuladhar, Rojee Awale, Biju Karmacharya, Labha Ratna Tuladhar",
+"title":"Nepal Bhasa Learning",
+"reviewed":false
+},
+
+"p": {
+"url":"https://www.unicode.org/L2/L2012/12003r-newar.pdf",
+"authors": "Anshuman Pandey",
+"title":"Proposal to Encode the Newar Script in ISO/IEC 10646",
+"reviewed":false
+},
+
+"u": {
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch13.pdf",
+"authors": "Unicode Consortium",
+"title":"The Unicode Standard, Version 13.0",
+"sub":"Chapter 13.3: South and Central Asia II, Newa",
+"pages":"523-525",
+"isbn":"978-1-936213-16-0.",
+},
+
+"ul": {
+"url":"https://www.unicode.org/reports/tr14/",
+"authors": "Unicode Consortium",
+"title":"Unicode Line Breaking Algorithm (UAX#14)",
+"reviewed":true
+},
+
+"wl": {
+"url":"https://en.wikipedia.org/wiki/Newar_language",
+"authors": "Wikipedia",
+"title":"Newar language",
+"reviewed":false
+},
+
+"ws": {
+"url":"https://en.wikipedia.org/wiki/Pracalit_script",
+"authors": "Wikipedia",
+"title":"Pracalit script",
+"reviewed":true
+},
+
+
+}
+
+
+
+// <a class="fn">ws,#tones</a>
