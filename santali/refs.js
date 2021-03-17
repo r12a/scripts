e3f8131643a3c30544495b7e840f9b13b6da7a49
r12a <ishida@w3.org>
2021-03-17 06:16:54 +0000

    Ol Chiki first draft

---
+++ santali/refs.js
@@ -0,0 +1,63 @@
+reflist = {
+
+
+"d": {
+"url":"#refs",
+"authors": "Peter T. Daniels and William Bright",
+"title":"The World's Writing Systems",
+"publisher":"Oxford University Press",
+"pages":"614-615",
+"isbn":"0-19-507993-0",
+"reviewed":true
+},
+
+"fp": {
+"url":"https://www.unicode.org/L2/L2005/05243r-n2984-ol-chiki.pdf",
+"authors": "Everson, Michael",
+"title":"Final proposal to encode the Ol Chiki script in the UCS",
+"reviewed":false
+},
+
+"rp": {
+"url":"https://www.unicode.org/L2/L2002/02422-n2505-olchiki.pdf",
+"authors": "Michael Everson, R.C. Hansdah & N. C. Murmu",
+"title":"Revised proposal for encoding the Ol Chiki script in the UCS",
+"reviewed":false
+},
+
+"u": {
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch13.pdf",
+"authors": "Unicode Consortium",
+"title":"The Unicode Standard, Version 13.0",
+"sub":"Chapter 13.10: South and Central Asia II, Ol Chiki",
+"pages":"555-556",
+"isbn":"978-1-936213-16-0.",
+"reviewed":false
+},
+
+"ul": {
+"url":"https://www.unicode.org/reports/tr14/",
+"authors": "Unicode Consortium",
+"title":"Unicode Line Breaking Algorithm (UAX#14)",
+"reviewed":true
+},
+
+"wl": {
+"url":"https://en.wikipedia.org/wiki/Santali_language",
+"authors": "Wikipedia",
+"title":"Santali language",
+"reviewed":false
+},
+
+"ws": {
+"url":"https://en.wikipedia.org/wiki/Ol_Chiki_script",
+"authors": "Wikipedia",
+"title":"Ol Chiki script",
+"reviewed":false
+},
+
+
+}
+
+
+
---
+++ santali/sat-examples.js
@@ -0,0 +1,112 @@
+/*
+@ Source: 
+§ 'Noto Sans Ol Chiki'
+# 200
+~ ltr
+! sat
+*/
+
+autoExpandExamples.sat = `
+
+ᱤᱧ|I||iñ
+ᱟᱢ|you (singular)||am
+ᱩᱱᱤ|he||uni
+ᱟᱹᱞᱤᱧ|we||ăliñ
+ᱟᱞᱮ|we||ale
+ᱟᱹᱵᱤᱱ|you (plural)||ăbin
+ᱟᱯᱮ|you (plural)|ape
+ᱩᱱᱠᱤᱱ|they|unkin
+ᱩᱱᱠᱩ|they||unku
+ᱱᱚᱣᱟ|this||nôva
+ᱱᱚᱶᱟ|this||nôw̃a
+ᱚᱱᱟ|that||ôna
+ᱱᱚᱰᱮ|here||nôḍe
+ᱦᱟᱸᱰᱮ|there||hãḍe
+ᱚᱠᱚᱭ|who||ôkôy
+ᱪᱮᱫ|what||cet’
+ᱚᱠᱟᱨᱮ|where||ôkare
+ᱛᱤᱨᱮ|when||tire
+ᱛᱤᱥ|when||tis
+ᱪᱮᱫ ᱞᱮᱠᱟᱛᱮ|how||cet’ lekate
+ᱵᱟᱝ|not||baṅ
+ᱡᱚᱛᱚ|all||jôtô
+ᱡᱷᱚᱛᱚ|all|ɟʰɔtɔ|jhôtô
+ᱟᱭᱢᱟ|many||ayma
+ᱟᱫᱚᱢ|some||adôm
+ᱛᱷᱚᱲᱟ|some, few|tʰɔɽa|thôṛa
+ᱮᱴᱟᱜ|other||eṭak’
+ᱢᱤᱫ|one||mit’
+ᱵᱟᱨ|two||bar
+ᱯᱮ|three||pe
+ᱯᱩᱱ|four||pun
+ᱢᱚᱬᱮ|five||môṇe
+ᱛᱩᱨᱩᱭ|six||turuy
+ᱮᱭᱟᱭ|seven||eyay
+ᱤᱨᱟᱹᱞ|eight||irăl
+ᱟᱨᱮ|nine||are
+ᱜᱮᱞ|ten||gel
+ᱢᱟᱨᱟᱝ|big||maraṅ
+ᱡᱤᱞᱤᱧ|long||jiliñ
+ᱚᱥᱟᱨ|wide||ôsar
+ᱤᱵᱤᱞ|thick||ibil
+ᱦᱟᱢᱟᱞ|heavy||hamal
+ᱠᱟᱴᱤᱡ|small||kaṭic’
+ᱦᱩᱰᱤᱧ|small||huḍiñ
+ᱠᱟᱴᱤᱡ|short||kaṭic’
+ᱠᱩᱲᱤ|woman||kuṛi
+ᱠᱚᱲᱟ|man (adult male)||kôṛa
+ᱦᱚᱲ|man (human being)||hôṛ
+ᱜᱤᱫᱨᱟ|child||gidra
+ᱦᱚᱯᱚᱱ|child||hôpôn
+ᱵᱟᱦᱩ|wife||bahu
+ᱮᱨᱚ|wife||erô
+ᱡᱟᱶᱟᱭ|husband||jaw̃ay
+ᱜᱚᱜᱚ|mother||gôgô
+ᱟᱭᱚ|mother||ayô
+ᱮᱝᱜᱚ|mother||eṅgô
+ᱵᱟᱵᱟ|father||baba
+ᱟᱯᱟ|father||apa
+ᱡᱤᱣᱟᱹᱞᱤ|animal||jivăli
+ᱦᱟᱹᱠᱩ|fish||hăku
+ᱪᱮᱬᱮ|bird||ceṇe
+ᱥᱮᱛᱟ|dog||seta
+ᱥᱤ|louse||si
+ᱵᱤᱝ|snake||biṅ
+ᱛᱤᱡᱩ|worm||tiju
+
+ᱦᱮᱸᱫᱮ|black|hẽde
+ᱵᱮᱺᱫᱤ|paddy storage|bə̃di
+ᱡᱭᱼᱭᱤ|soul|jiːyi
+ᱢᱚᱹᱬᱮᱻ ᱢᱚᱸᱻᱦᱟ|five days|mɔɳeː mãːha
+ᱫᱟᱜᱽ|mark|daɡ
+ᱫᱟᱜᱤ|marked|daɡi
+ᱨᱚᱡᱽ|rule|raɟ
+ᱨᱚᱡᱚ|king|raɟa
+ᱜᱤᱫᱽᱨᱟ|child|ɡidra
+ᱜᱤᱫᱤ|vulture|ɡidi
+ᱩᱵᱽ|pour (from a pot)|ub
+ᱵᱤᱨ|forest|bir
+ᱫᱚᱜ|water|dak̚
+ᱨᱚᱜ|cry|rak̚
+ᱨᱮᱡ|snatch|rec̚
+ᱢᱩᱡ|ant|muc̚
+ᱜᱳᱫ|pluck|got̚
+ᱢᱮᱫ|eye|met̚
+ᱩᱵ|hair|up̚
+ᱢᱮᱱᱟᱜᱼᱟ|have|menaka|menak’a
+ᱟᱠᱟᱫᱼᱟ|?|akat|akat’
+ᱦᱤᱡᱩᱜᱼᱟ|come|hijuka|hijuk’a
+ᱥᱩᱱᱭᱚ|zero|sunjɔ
+ᱢᱤᱫ|one|mit’
+ᱵᱟᱨ|two|bar
+ᱯᱮ|three|pe
+ᱯᱩᱱ|four|pun
+ᱢᱚᱬᱮ|five|mɔɳe
+ᱛᱩᱨᱩᱭ|six|turuy
+ᱮᱭᱟᱭ|seven|eyay
+ᱤᱨᱟᱹᱞ|eight|irəl
+ᱟᱨᱮ|nine|are
+
+
+`
+
