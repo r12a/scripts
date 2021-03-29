30e29afc5aef31d8f620c44a827258f4f1668ead
r12a <ishida@w3.org>
2021-03-29 14:27:39 +0100

    kali: Banking early draft

---
+++ kali/refs.js
@@ -0,0 +1,46 @@
+reflist = {
+
+
+"e": {
+"url":"http://std.dkuug.dk/jtc1/sc2/wg2/docs/n3038.pdf",
+"authors": "Michael Everson",
+"title":"Proposal for encoding the Kayah Li script in the BMP of the UCS",
+"reviewed":false
+},
+
+"h": {
+"url":"http://www.karennirefugees.com/uploads/3/0/1/9/3019325/learning_kayah.pdf",
+"authors": "H. Anne Helgerson",
+"title":"Kayah Li (Western Red Karen) Language Lessons",
+"reviewed":false
+},
+
+"u": {
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch16.pdf",
+"authors": "Unicode Consortium",
+"title":"The Unicode Standard, Version 13.0",
+"sub":"Chapter 16.9: Southeast Asia, Kayah Li",
+"pages":"676",
+"isbn":"978-1-936213-16-0.",
+"reviewed": false
+},
+
+"ul": {
+"url":"https://www.unicode.org/reports/tr14/",
+"authors": "Unicode Consortium",
+"title":"Unicode Line Breaking Algorithm (UAX#14)",
+"reviewed":true
+},
+
+"ws": {
+"url":"https://en.wikipedia.org/wiki/Kayah_Li_alphabet",
+"authors": "Wikipedia",
+"title":"Kayah Li alphabet",
+"reviewed":false
+},
+
+
+}
+
+
+
