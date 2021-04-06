3a491b63267fcf022f9d283c0264622fe0d66810
r12a <ishida@w3.org>
2021-04-06 09:10:01 +0100

    lao: - add lines linking to directional & line-breaking properties - change intro to character index - create phonology section - move tones into vowel section - rejig counter styles

--- lao/refs.js
+++ lao/refs.js
@@ -43,6 +43,13 @@ reflist = {
 "reviewed":true
 },
 
+"rmcs": {
+"url":"https://www.w3.org/TR/predefined-counter-styles/#arabic-styles",
+"authors": "Richard Ishida",
+"title":"Ready-made Counter Styles",
+"reviewed":true
+},
+
 "s": {
 "url":"#refs",
 "authors": "Buasawan Simmala, Benjawan Poomsan Becker",
