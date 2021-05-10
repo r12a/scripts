57d630379f553d4c220605247b8603a793874a1f
r12a <ishida@w3.org>
2021-05-10 11:57:01 +0100

    grek: Reformat refs

--- greek/refs.js
+++ greek/refs.js
@@ -2,7 +2,6 @@ reflist = {
 
 
 "d": {
-"url":"#refs",
 "authors": "Peter T. Daniels and William Bright",
 "title":"The World's Writing Systems",
 "publisher":"Oxford University Press",
@@ -11,49 +10,42 @@ reflist = {
 },
 
 "u": {
-"url":"https://www.unicode.org/versions/Unicode13.0.0/ch07.pdf",
 "authors": "Unicode Consortium",
 "title":"The Unicode Standard, Version 13.0",
 "sub":"Chapter 7.2: Europe-I, Greek",
 "pages":"301-307",
 "isbn":"978-1-936213-16-0.",
-},
-
-"ul": {
-"url":"https://www.unicode.org/reports/tr14/",
-"authors": "Unicode Consortium",
-"title":"Unicode Line Breaking Algorithm (UAX#14)",
-"reviewed":true
-},
-
-"wn": {
-"url":"https://en.wikipedia.org/wiki/Greek_numerals",
-"authors": "Wikipedia",
-"title":"Greek numerals",
-},
-
-"wo": {
-"url":"https://en.wikipedia.org/wiki/Greek_orthography",
-"authors": "Wikipedia",
-"title":"Greek orthography",
-},
-
-"wp": {
-"url":"https://en.wikipedia.org/wiki/Modern_Greek_phonology",
-"authors": "Wikipedia",
-"title":"Modern Greek phonology",
-},
-
-"wq": {
-"url":"https://en.wikipedia.org/wiki/Quotation_mark",
-"authors": "Wikipedia",
-"title":"Quotation mark",
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch07.pdf",
 },
 
 "ws": {
-"url":"http://en.wikipedia.org/wiki/Greek_alphabet",
 "authors": "Wikipedia",
 "title":"Greek alphabet",
+"url":"http://en.wikipedia.org/wiki/Greek_alphabet",
+},
+
+"wn": {
+"authors": "Wikipedia",
+"title":"Greek numerals",
+"url":"https://en.wikipedia.org/wiki/Greek_numerals",
+},
+
+"wo": {
+"authors": "Wikipedia",
+"title":"Greek orthography",
+"url":"https://en.wikipedia.org/wiki/Greek_orthography",
+},
+
+"wp": {
+"authors": "Wikipedia",
+"title":"Modern Greek phonology",
+"url":"https://en.wikipedia.org/wiki/Modern_Greek_phonology",
+},
+
+"wq": {
+"authors": "Wikipedia",
+"title":"Quotation mark",
+"url":"https://en.wikipedia.org/wiki/Quotation_mark",
 },
 
 
