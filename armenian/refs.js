21bf1813092778f9142fd55d5bd0bd26008b2a9b
r12a <ishida@w3.org>
2021-05-10 11:55:19 +0100

    armn: Reformat refs

--- armenian/refs.js
+++ armenian/refs.js
@@ -2,66 +2,66 @@ reflist = {
 
 
 "a": {
-"url":"https://www.unicode.org/L2/L2020/20143-armenian-ech-yiwn.pdf",
 "authors": "Deborah Anderson",
 "title":"Uppercase of U+0587 ARMENIAN SMALL LIGATURE ECH YIWN",
+"url":"https://www.unicode.org/L2/L2020/20143-armenian-ech-yiwn.pdf",
 "reviewed":true
 },
 
 "d": {
-"url":"#refs",
 "authors": "Peter T. Daniels and William Bright",
 "title":"The World's Writing Systems",
 "publisher":"Oxford University Press",
 "isbn":"0-19-507993-0",
 },
 
-"e": {
-"url":"http://std.dkuug.dk/jtc1/sc2/wg2/docs/n3924.pdf",
-"authors": "Michael Everson",
-"title":"Proposal to encode two symbols for Armenian in the UCS",
-},
-
 "j": {
-"url":"https://www.google.co.uk/books/edition/Armenian/SGyop5guXDkC?hl=en",
 "authors": "Jasmine Dum-Tragut",
 "title":"Armenian: Modern Eastern Armenian",
 "publisher": "John Benjamins Publishing Co",
-"series": "London Oriental and African Language Library"
+"series": "London Oriental and African Language Library",
+"url":"https://www.google.co.uk/books/edition/Armenian/SGyop5guXDkC?hl=en",
+},
+
+"e": {
+"authors": "Michael Everson",
+"title":"Proposal to encode two symbols for Armenian in the UCS",
+"url":"http://std.dkuug.dk/jtc1/sc2/wg2/docs/n3924.pdf",
+},
+
+"ics": {
+"authors": "Ishida, R",
+"title":"Ready-made Counter Styles",
+"url":"https://www.w3.org/TR/predefined-counter-styles/#arabic-styles",
+"date": "2017",
+"reviewed":true
 },
 
 "u": {
-"url":"https://www.unicode.org/versions/Unicode13.0.0/ch07.pdf",
 "authors": "Unicode Consortium",
 "title":"The Unicode Standard, Version 13.0",
 "sub":"Chapter 7.6: Europe-I, Armenian",
 "pages":"317-318",
 "isbn":"978-1-936213-16-0.",
-},
-
-"ul": {
-"url":"https://www.unicode.org/reports/tr14/",
-"authors": "Unicode Consortium",
-"title":"Unicode Line Breaking Algorithm (UAX#14)",
-"reviewed":true
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch07.pdf",
 },
 
 "wd": {
-"url":"https://en.wikipedia.org/wiki/Armenian_dram_sign",
 "authors": "Wikipedia",
 "title":"Armenian dram sign",
+"url":"https://en.wikipedia.org/wiki/Armenian_dram_sign",
 },
 
 "wl": {
-"url":"https://en.wikipedia.org/wiki/Armenian_language",
 "authors": "Wikipedia",
 "title":"Armenian language",
+"url":"https://en.wikipedia.org/wiki/Armenian_language",
 },
 
 "ws": {
-"url":"https://en.wikipedia.org/wiki/Armenian_script",
 "authors": "Wikipedia",
 "title":"Armenian script",
+"url":"https://en.wikipedia.org/wiki/Armenian_script",
 },
 
 
