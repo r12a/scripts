3e8b1f0df03b32ee735283a66dd38c174360b8e9
r12a <ishida@w3.org>
2021-05-10 11:55:37 +0100

    bass: Reformat refs

--- bassavah/refs.js
+++ bassavah/refs.js
@@ -1,23 +1,28 @@
 reflist = {
 
 "b": {
-"url":"https://cefliberia.org/wp-content/uploads/2020/03/Bertkau-Phonology-of-Bassa.pdf",
 "authors": "Jana Bertkau",
 "title":"A Phonology of Bassa",
 "publisher":"U.S. Peace Corps",
 "pages":"esp. 43ff",
 "date":"1975",
+"url":"https://cefliberia.org/wp-content/uploads/2020/03/Bertkau-Phonology-of-Bassa.pdf",
 },
 
 "b2": {
-"url":"https://cefliberia.org/wp-content/uploads/2020/03/Bertkau-Comparison-of-Three-Bassa-Orthographies.pdf",
 "authors": "Jana S. Bertkau",
 "title":"A Comparison of Three Bassa Orthographies",
 "date":"Feb 1976",
+"url":"https://cefliberia.org/wp-content/uploads/2020/03/Bertkau-Comparison-of-Three-Bassa-Orthographies.pdf",
+},
+
+"cb": {
+"authors": "Christian Education Foundation of Liberia",
+"title":"Bassa-English Dictionary",
+"url":"https://cefliberia.org/bassa/bassa-dictionary/",
 },
 
 "fc": {
-"url":"#refs",
 "authors": "Florian Coulmas",
 "title":"The Blackwell Encyclopedia of Writing Systems",
 "publisher":"Oxford University Press",
@@ -25,66 +30,52 @@ reflist = {
 "reviewed":true
 },
 
-"cb": {
-"url":"https://cefliberia.org/bassa/bassa-dictionary/",
-"authors": "Christian Education Foundation of Liberia",
-"title":"Bassa-English Dictionary",
-},
-
 "e": {
-"url":"https://www.unicode.org/L2/L2010/10382r-n3941r-bassavah.pdf",
 "authors": "Michael Everson, Charles Riley",
 "title":"Final proposal for encoding the Bassa Vah script in the SMP of the UCS",
+"url":"https://www.unicode.org/L2/L2010/10382r-n3941r-bassavah.pdf",
 },
 
 "o": {
-"url":"https://www.omniglot.com/writing/bassa.htm",
 "authors": "Omniglot",
 "title":"Bassa Vah",
+"url":"https://www.omniglot.com/writing/bassa.htm",
 "reviewed":true
 },
 
 "rc": {
-"url":"http://std.dkuug.dk/JTC1/SC2/wg2/docs/n3991.pdf",
 "authors": "Riley, Charles",
 "title":"Comments on Bassa Vah Comma",
+"url":"http://std.dkuug.dk/JTC1/SC2/wg2/docs/n3991.pdf",
+"reviewed":true
+},
+
+"s": {
+"authors": "ScriptSource",
+"title":"Bassa Vah",
+"url":"https://scriptsource.org/cms/scripts/page.php?item_id=script_detail&key=Bass",
 "reviewed":true
 },
 
 "sr": {
-"url":"#refs",
 "authors": "Tim Slager, Charles Riley",
 "title":"Personal correspondence",
 "reviewed":true
 },
 
-"s": {
-"url":"https://scriptsource.org/cms/scripts/page.php?item_id=script_detail&key=Bass",
-"authors": "ScriptSource",
-"title":"Bassa Vah",
-"reviewed":true
-},
-
 "u": {
-"url":"https://www.unicode.org/versions/Unicode13.0.0/ch19.pdf",
 "authors": "Unicode Consortium",
 "title":"The Unicode Standard, Version 13.0",
 "sub":"Chapter 19.7: Africa, Bassa Vah",
 "pages":"781",
 "isbn":"978-1-936213-16-0.",
-},
-
-"ul": {
-"url":"https://www.unicode.org/reports/tr14/",
-"authors": "Unicode Consortium",
-"title":"Unicode Line Breaking Algorithm (UAX#14)",
-"reviewed":true
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch19.pdf",
 },
 
 "ws": {
-"url":"https://en.wikipedia.org/wiki/Bassa_script",
 "authors": "Wikipedia",
 "title":"Bassa script",
+"url":"https://en.wikipedia.org/wiki/Bassa_script",
 "reviewed":true
 },
 
