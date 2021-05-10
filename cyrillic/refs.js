830a1858bcd50c83235ad9242b9218d8c9387e90
r12a <ishida@w3.org>
2021-05-10 11:56:13 +0100

    cyrl: Reformat refs

--- cyrillic/refs.js
+++ cyrillic/refs.js
@@ -1,71 +1,96 @@
 reflist = {
 
 
-"cldr": {
-"url":"http://localhost/scripts/cyrillic/#https://unicode-org.github.io/cldr-staging/charts/37/summary/ru.html",
-"authors": "CLDR",
-"title":"Locale Data Summary for Russian [ru]",
+"ul": {
+"authors": "Christopher Chapman",
+"title":"UAX #14 Unicode Line Breaking Algorithm",
+"date": "2020",
+"publisher": "Unicode Consortium",
+"url":"https://www.unicode.org/reports/tr14/",
+"reviewed":true
 },
 
 "d": {
-"url":"#refs",
 "authors": "Peter T. Daniels and William Bright",
 "title":"The World's Writing Systems",
 "publisher":"Oxford University Press",
 "isbn":"0-19-507993-0",
 "pages":"408-412",
+"date": "1996",
+"url":"#refs",
+},
+
+"ics": {
+"authors": "Ishida, R",
+"title":"Ready-made Counter Styles",
+"url":"https://www.w3.org/TR/predefined-counter-styles/#arabic-styles",
+"date": "2017",
+"reviewed":true
 },
 
 "s": {
-"url":"http://scriptsource.org/cms/scripts/page.php?item_id=entry_detail&uid=k9ew9c6yam",
 "authors": "ScriptSource",
 "title":"Glyph Variant for U+0306",
+"retr": "Apr 2021",
+"url":"http://scriptsource.org/cms/scripts/page.php?item_id=entry_detail&uid=k9ew9c6yam",
 "reviewed":true
 },
 
 "u": {
-"url":"https://www.unicode.org/versions/Unicode13.0.0/ch07.pdf",
 "authors": "Unicode Consortium",
 "title":"The Unicode Standard, Version 13.0",
+"date": "2020",
 "sub":"Chapter 7.4: Europe-I, Cyrillic",
 "pages":"311-315",
 "isbn":"978-1-936213-16-0.",
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch07.pdf",
 },
 
-"ul": {
-"url":"https://www.unicode.org/reports/tr14/",
+"cldr": {
 "authors": "Unicode Consortium",
-"title":"Unicode Line Breaking Algorithm (UAX#14)",
-"reviewed":true
-},
-
-"wa": {
-"url":"https://en.wikipedia.org/wiki/Russian_alphabet",
-"authors": "Wikipedia",
-"title":"Russian alphabet",
-},
-
-"wp": {
-"url":"https://en.wikipedia.org/wiki/Russian_phonology",
-"authors": "Wikipedia",
-"title":"Russian phonology",
-},
-
-"wq": {
-"url":"https://en.wikipedia.org/wiki/Quotation_mark",
-"authors": "Wikipedia",
-"title":"Quotation mark",
+"title":"CLDR: Locale Data Summary for Russian [ru]",
+"url":"http://localhost/scripts/cyrillic/#https://unicode-org.github.io/cldr-staging/charts/37/summary/ru.html",
 },
 
 "ws": {
-"url":"http://en.wikipedia.org/wiki/Cyrillic_script",
 "authors": "Wikipedia",
 "title":"Cyrillic script",
+"retr": "Apr 2021",
+"url":"http://en.wikipedia.org/wiki/Cyrillic_script",
 "reviewed":true
 },
 
+"wq": {
+"authors": "Wikipedia",
+"title":"Quotation mark",
+"retr": "Apr 2021",
+"url":"https://en.wikipedia.org/wiki/Quotation_mark",
+},
 
-// WORD LOOKUP TOOLS
+"wa": {
+"authors": "Wikipedia",
+"title":"Russian alphabet",
+"retr": "Apr 2021",
+"url":"https://en.wikipedia.org/wiki/Russian_alphabet",
+},
+
+"wp": {
+"authors": "Wikipedia",
+"title":"Russian phonology",
+"retr": "Apr 2021",
+"url":"https://en.wikipedia.org/wiki/Russian_phonology",
+},
+
+}
+
+
+
+
+
+
+
+
+terms = {
 
 "x1": {
 "url":"http://shtooka.net/overview.php?lang=rus&page=8",
@@ -80,7 +105,6 @@ reflist = {
 "title":"Russian Wiktionary",
 "notes": "Change the word at the end of the URL to look up IPA.  Remove any accent."
 },
-
 
 }
 
