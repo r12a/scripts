becf28290c788874b8f60f3c6c1bd824ab5daf1f
r12a <ishida@w3.org>
2021-05-10 11:54:01 +0100

    adlam: Reformat refs

--- adlam/refs.js
+++ adlam/refs.js
@@ -1,93 +1,87 @@
 reflist = {
 
+
+"yi": {
+"authors": "Abdoulaye &amp; Ibrahima Barry, Mark Jamara, Neil Patel",
+"title":"Inventing the Adlam Script",
+"sub":"YouTube",
+"url":"https://www.youtube.com/watch?v=PXN4aciv19U",
+},
+
 "b": {
-"url":"https://www.unicode.org/L2/L2018/18282r-adlam.pdf",
 "authors": "Abdoulaye Barry, Ibrahima Barry, Peter Constable, Andrew Glass",
 "title":"Proposal to encode ADLAM NASALIZATION MARK for ADLaM script",
+"url":"https://www.unicode.org/L2/L2018/18282r-adlam.pdf",
+},
+
+"ya": {
+"authors": "Abdoulaye and Ibrahim Barry",
+"title":"The ADLaM Story",
+"sub":"YouTube",
+"url":"https://www.youtube.com/watch?v=NPQ7ufjN2i8",
 },
 
 "e": {
-"url":"http://std.dkuug.dk/jtc1/sc2/wg2/docs/n4628.pdf",
 "authors": "Michael Everson",
 "title":"Proposal for encoding the Adlam script in the SMP of the UCS",
+"url":"http://std.dkuug.dk/jtc1/sc2/wg2/docs/n4628.pdf",
 },
 
 "g": {
-"url":"https://github.com/w3c/afrlreq/issues/3",
-"authors": "Afrlreq GitHub issues list",
+"authors": "GitHub: Afrlreq issue list",
 "title":"Direction of slant for italic/oblique RTL text",
+"url":"https://github.com/w3c/afrlreq/issues/3",
+},
+
+"rmcs": {
+"authors": "Richard Ishida",
+"title":"Ready-made Counter Styles",
+"url":"https://www.w3.org/TR/predefined-counter-styles/#arabic-styles",
+"reviewed":true
+},
+
+"o": {
+"authors": "Omniglot",
+"title":"Adlam",
+"url":"https://www.omniglot.com/writing/adlam.htm",
 },
 
 "n": {
-"url":"#refs",
 "authors": "Neil Patel",
 "title":"Private correspondence",
 },
 
-"o": {
-"url":"https://www.omniglot.com/writing/adlam.htm",
-"authors": "Omniglot",
-"title":"Adlam",
-},
-
 "p": {
-"url":"https://www.unicode.org/L2/L2019/19119r-adlam-font-repl.pdf",
 "authors": "Neil Patel and Mark Jamra (JamraPatel), Craig Cornelius (Google), Ibrahima Barry, Abdoulaye Barry",
 "title":"Replacement of Adlam Reference Font in Codesheet to Updated Design",
-},
-
-"rmcs": {
-"url":"https://www.w3.org/TR/predefined-counter-styles/#arabic-styles",
-"authors": "Richard Ishida",
-"title":"Ready-made Counter Styles",
-"reviewed":true
+"url":"https://www.unicode.org/L2/L2019/19119r-adlam-font-repl.pdf",
 },
 
 "s": {
-"url":"http://scriptsource.org/cms/scripts/page.php?item_id=script_detail&amp;uid=aggc6af8cc",
 "authors": "ScriptSource",
 "title":"Adlam",
+"url":"http://scriptsource.org/cms/scripts/page.php?item_id=script_detail&amp;uid=aggc6af8cc",
 },
 
 "u": {
-"url":"http://www.unicode.org/versions/Unicode11.0.0/ch19.pdf",
 "authors": "Unicode Consortium",
 "title":"The Unicode Standard, Version 13.0",
 "sub":"Chapter 19.9: Africa, Adlam",
 "pages":"784-785",
 "isbn":"978-1-936213-16-0.",
-},
-
-"ul": {
-"url":"https://www.unicode.org/reports/tr14/",
-"authors": "Unicode Consortium",
-"title":"Unicode Line Breaking Algorithm (UAX#14)",
-},
-
-"wl": {
-"url":"https://en.wikipedia.org/wiki/Fula_language",
-"authors": "Wikipedia",
-"title":"Fula language",
+"url":"http://www.unicode.org/versions/Unicode11.0.0/ch19.pdf",
 },
 
 "ws": {
-"url":"http://en.wikipedia.org/wiki/Adlam_script",
 "authors": "Wikipedia",
 "title":"Adlam script",
+"url":"http://en.wikipedia.org/wiki/Adlam_script",
 },
 
-"ya": {
-"url":"https://www.youtube.com/watch?v=NPQ7ufjN2i8",
-"authors": "Abdoulaye and Ibrahim Barry",
-"title":"The ADLaM Story",
-"sub":"YouTube"
-},
-
-"yi": {
-"url":"https://www.youtube.com/watch?v=PXN4aciv19U",
-"authors": "Abdoulaye &amp; Ibrahima Barry, Mark Jamara, Neil Patel",
-"title":"Inventing the Adlam Script",
-"sub":"YouTube"
+"wl": {
+"authors": "Wikipedia",
+"title":"Fula language",
+"url":"https://en.wikipedia.org/wiki/Fula_language",
 },
 
 
