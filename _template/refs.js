5798650711bc9492e8ee415d547a5a6a00b7c9ef
r12a <ishida@w3.org>
2021-05-10 08:24:00 +0100

    template update

--- _template/refs.js
+++ _template/refs.js
@@ -1,71 +1,95 @@
+window.langSet = true
+
+
+
 reflist = {
 
 
-"d": {
-"url":"#refs",
-"authors": "Peter T. Daniels and William Bright",
+"auth_initials": {
+"authors": "Sort by family name of first contributor",
 "title":"The World's Writing Systems",
+"sub": "subtitle",
+"date": "1996",
+"retr": "May 2021",
 "publisher":"Oxford University Press",
 "isbn":"0-19-507993-0",
+"pages": "xxx",
+"url":"refs",
+"lang": new Set(['ru']),
+"reviewed":false
+},
+
+"ul": {
+"authors": "Christopher Chapman",
+"title":"Unicode Line Breaking Algorithm (UAX#14)",
+"date": "1996",
+"publisher": "Unicode Consortium",
+"url":"https://www.unicode.org/reports/tr14/",
+"reviewed":true
+},
+
+"d": {
+"authors": "Peter T. Daniels and William Bright",
+"title":"The World's Writing Systems",
+"date": "1996",
+"publisher":"Oxford University Press",
+"isbn":"0-19-507993-0",
+"url":"#refs",
+"lang": new Set(['ru']),
 "reviewed":false
 },
 
 "e": {
-"url":"https://www.unicode.org/L2/L2008/08350-n3522-bamum.pdf",
 "authors": "Everson, Michael; Riley, Charles; Tuchscherer, Konrad",
 "title":"L2/08-350: Proposal to encode modern Bamum in the BMP",
+"url":"https://www.unicode.org/L2/L2008/08350-n3522-bamum.pdf",
 },
 
 "o": {
-"url":"https://www.omniglot.com/writing/bamum.htm",
 "authors": "Omniglot",
 "title":"Bamum syllabary",
+"url":"https://www.omniglot.com/writing/bamum.htm",
 "reviewed":false
 },
 
 "r": {
-"url":"https://www.unicode.org/L2/L2007/07023-bamum-report.pdf",
 "authors": "Riley, Charles",
 "title":"L2/07-023: Towards the Encoding of the Bamum Script in the UCS",
+"url":"https://www.unicode.org/L2/L2007/07023-bamum-report.pdf",
 "reviewed":false
 },
 
 "rmcs": {
-"url":"https://www.w3.org/TR/predefined-counter-styles/#arabic-styles",
 "authors": "Richard Ishida",
 "title":"Ready-made Counter Styles",
+"url":"https://www.w3.org/TR/predefined-counter-styles/#arabic-styles",
 "reviewed":true
 },
 
 
 "u": {
-"url":"https://www.unicode.org/versions/Unicode13.0.0/ch19.pdf",
 "authors": "Unicode Consortium",
 "title":"The Unicode Standard, Version 13.0",
 "sub":"Chapter 19.6: Africa, Bamum",
 "pages":"779-780",
 "isbn":"978-1-936213-16-0.",
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch19.pdf",
 "reviewed": false
 },
 
-"ul": {
-"url":"https://www.unicode.org/reports/tr14/",
-"authors": "Unicode Consortium",
-"title":"Unicode Line Breaking Algorithm (UAX#14)",
-"reviewed":true
-},
-
 "wl": {
-"url":"https://en.wikipedia.org/wiki/Rohingya_language",
 "authors": "Wikipedia",
 "title":"Rohingya language",
+"retr": "May 2021",
+"url":"https://en.wikipedia.org/wiki/Rohingya_language",
 "reviewed":false
 },
 
 "ws": {
-"url":"https://en.wikipedia.org/wiki/Bamum_script",
 "authors": "Wikipedia",
 "title":"Bamum script",
+"retr": "May 2021",
+"url":"https://en.wikipedia.org/wiki/Bamum_script",
 "reviewed":false
 },
 
@@ -74,4 +98,28 @@ reflist = {
 
 
 
-// <a class="fn">ws,#tones</a>
+
+
+
+terms = {
+
+"i": {
+"url":"https://tal.ircam.ma/tamawalt/alphabet_p.php",
+"authors": "ERCIM",
+"title":"ⵜⴰⵎⴰⵡⴰⵍⵜ ⵏ ⵉⵎⵥⵥⵢⴰⵏⵏ",
+},
+
+"a": {
+"url":"https://amawal.net/about",
+"authors": "?",
+"title":"ⴰⵎⴰⵡⴰⵍ The Berber dictionary",
+"notes":"Latin only",
+},
+
+
+}
+
+
+
+
+
