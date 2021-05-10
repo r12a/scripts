8915038a68f8af93f1e388efb56809746a73d787
r12a <ishida@w3.org>
2021-05-10 11:58:25 +0100

    syriac: Reformat refs

--- syriac/refs.js
+++ syriac/refs.js
@@ -1,91 +1,97 @@
+window.langSet = true
+
+
+
 reflist = {
 
-
 "a": {
-"url":"https://textbook.surayt.com/en/Level%20A/",
 "authors": "Aramaic Online Project (2014-2017) edited by Shabo Talay",
 "title":" ܫܠܳܡܐ ܣܘܪܰܝܬ Šlomo Surayt An Introductory Course to Surayt-Aramaic (Turoyo)",
+"url":"https://textbook.surayt.com/en/Level%20A/",
+"lang": new Set(['tru']),
 },
 
 "a2": {
-"url":"https://textbook.surayt.com/en/Level%20A/2?content-fragment-id=1746",
 "authors": "Aramaic Online Project (2014-2017) edited by Shabo Talay",
 "title":" ܫܠܳܡܐ ܣܘܪܰܝܬ Šlomo Surayt An Introductory Course to Surayt-Aramaic (Turoyo)",
+"url":"https://textbook.surayt.com/en/Level%20A/2?content-fragment-id=1746",
+"lang": new Set(['tru']),
 },
 
 "d": {
-"url":"#refs",
 "authors": "Peter T. Daniels and William Bright",
 "title":"The World's Writing Systems",
 "publisher":"Oxford University Press",
 "isbn":"0-19-507993-0",
+"lang": new Set(['syc','aii']),
 "reviewed":true
 },
 
+"r": {
+"authors": "Madeleine Davis",
+"title":"Assyrian Reading Lessons in Six Days, 2nd ed.",
+"date":"2014",
+"lang": new Set(['aii']),
+},
+
 "mv": {
-"url":"https://www.youtube.com/channel/UConJUIvQYXb48VFWoIFavAQ",
 "authors": "Madeleine Davis Moradkhan",
 "title":"Assyrian Reading Course (videos)",
+"url":"https://www.youtube.com/channel/UConJUIvQYXb48VFWoIFavAQ",
+"lang": new Set(['aii']),
 },
 
 "n": {
-"url":"http://www.unicode.org/L2/L1998/98050-syriac-proposal.pdf",
 "authors": "Paul Nelson, George Anton Kiraz, Sargon Hass",
 "title":"Proposal to Encode Syriac in ISO/IEC 10646",
 "date":"1998",
-},
-
-"r": {
-"url":"#refs",
-"authors": "Madeleine Davis",
-"title":"Assyrian Reading Lessons in Six Days, 2nd ed.",
-"date":"March 2014",
+"url":"http://www.unicode.org/L2/L1998/98050-syriac-proposal.pdf",
+"lang": new Set(['syc','aii']),
 },
 
 "s": {
-"url":"http://www.unicode.org/L2/L2002/02132-N2422-syrian.pdf",
 "authors": "Nicholas Sims-Williams, Michael Everson",
 "title":"Proposal to add six Syriac letters for Sogdian and Persian to the UCS",
+"url":"http://www.unicode.org/L2/L2002/02132-N2422-syrian.pdf",
+"lang": new Set(['syc']),
 },
 
 "u": {
-"url":"https://www.unicode.org/versions/Unicode13.0.0/ch09.pdf",
 "authors": "Unicode Consortium",
 "title":"The Unicode Standard, Version 13.0",
 "sub":"Chapter 9.3: Middle East-I, Syriac",
 "pages":"390-398",
 "isbn":"978-1-936213-16-0.",
-},
-
-"ul": {
-"url":"https://www.unicode.org/reports/tr14/",
-"authors": "Unicode Consortium",
-"title":"Unicode Line Breaking Algorithm (UAX#14)",
-"reviewed":true
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch09.pdf",
+"lang": new Set(['syc','aii']),
 },
 
 "wa": {
-"url":"https://en.wikipedia.org/wiki/Assyrian_Neo-Aramaic",
 "authors": "Wikipedia",
 "title":"Assyrian Neo-Aramaic",
+"url":"https://en.wikipedia.org/wiki/Assyrian_Neo-Aramaic",
+"lang": new Set(['aii']),
 },
 
 "wb": {
-"url":"https://en.wikipedia.org/wiki/Bet_(letter)",
 "authors": "Wikipedia",
 "title":"Bet",
+"url":"https://en.wikipedia.org/wiki/Bet_(letter)",
+"lang": new Set(['syc']),
 },
 
 "wh": {
 "url":"https://en.wikipedia.org/wiki/He_(letter)",
 "authors": "Wikipedia",
 "title":"He",
+"lang": new Set(['syc']),
 },
 
 "w": {
-"url":"httphttps://en.wikipedia.org/wiki/Syriac_alphabet",
 "authors": "Wikipedia",
 "title":"Syriac alphabet",
+"url":"httphttps://en.wikipedia.org/wiki/Syriac_alphabet",
+"lang": new Set(['syc','aii']),
 },
 
 
