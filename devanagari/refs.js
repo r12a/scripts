bd36386fc0fc62d5246e4d18d58cc7752a487e68
r12a <ishida@w3.org>
2021-05-10 11:56:35 +0100

    deva: Reformat refs

--- devanagari/refs.js
+++ devanagari/refs.js
@@ -1,113 +1,121 @@
+window.langSet = true
+
+
+
 reflist = {
 
-
-"cldr": {
-"url":"https://www.unicode.org/cldr/charts/latest/summary/hi.html",
-"authors": "CLDR",
-"title":"Hindi",
-},
-
 "d": {
-"url":"#refs",
 "authors": "Peter T. Daniels and William Bright",
 "title":"The World's Writing Systems",
 "publisher":"Oxford University Press",
 "isbn":"0-19-507993-0",
 "pages":"487-497",
+"lang": new Set(['hi']),
 },
 
 "ep": {
-"url":"https://www.unicode.org/L2/L2008/08250-n3480-kashmiri.pdf",
 "authors": "Michael Everson and Pravin Satpute",
 "title":"Proposal to add four characters for Kashmiri to the BMP of the UCS",
-},
-
-"i": {
-"url":"https://www.w3.org/TR/ilreq/",
-"authors": "Swaran Lata, Somnath Chandra, Prashant Verma",
-"title":"Indic Layout Requirements",
-},
-
-"l": {
-"url":"https://www.unicode.org/L2/L2009/09311-kashmiri-ltr-from-swaran-lata.pdf",
-"authors": "Swaran Lata",
-"title":"Encoding of additional characters in Unicode for representation of Kashmiri",
-},
-
-"mkr": {
-"url":"http://koshur.org/pdf/Let%20Us%20Learn%20Kashmiri.pdf",
-"authors": "M. K. Raina",
-"title":"How to read and write Kashmiri in Devanagari?",
-"reviewed":true
+"url":"https://www.unicode.org/L2/L2008/08250-n3480-kashmiri.pdf",
+"lang": new Set(['ks']),
 },
 
 "rmcs": {
-"url":"https://www.w3.org/TR/predefined-counter-styles/#arabic-styles",
 "authors": "Richard Ishida",
 "title":"Ready-made Counter Styles",
+"url":"https://www.w3.org/TR/predefined-counter-styles/#arabic-styles",
+"lang": new Set(['hi']),
+"reviewed":true
+},
+
+"l": {
+"authors": "Swaran Lata",
+"title":"Encoding of additional characters in Unicode for representation of Kashmiri",
+"url":"https://www.unicode.org/L2/L2009/09311-kashmiri-ltr-from-swaran-lata.pdf",
+"lang": new Set(['ks']),
+},
+
+"i": {
+"authors": "Swaran Lata, Somnath Chandra, Prashant Verma",
+"title":"Indic Layout Requirements",
+"url":"https://www.w3.org/TR/ilreq/",
+"lang": new Set(['hi']),
+},
+
+"mkr": {
+"authors": "M. K. Raina",
+"title":"How to read and write Kashmiri in Devanagari?",
+"url":"http://koshur.org/pdf/Let%20Us%20Learn%20Kashmiri.pdf",
+"lang": new Set(['ks']),
 "reviewed":true
 },
 
 "rt": {
-"url":"http://www.koshur.org/pdf/BasicReader.pdf",
 "authors": "M. K. Raina & Neelam Trakru",
 "title":"Basic Reader for Kashmiri Language",
 "publisher": "Project Zaan",
+"url":"http://www.koshur.org/pdf/BasicReader.pdf",
+"lang": new Set(['ks']),
+},
+
+"sg": {
+"authors": "Akshay Sharma, The Sunday Guardian",
+"title":"Pandits want official status for Kashmiri written in Devanagari script",
+"lang": new Set(['ks']),
+"url":"https://www.sundayguardianlive.com/news/7044-pandits-want-official-status-kashmiri-written-devanagari-script",
 },
 
 "s": {
-"url":"#refs",
 "authors": "Rupert Snell & Simon Weightman",
 "title":"Teach Yourself Hindi",
 "publisher":"Hodder & Stoughton",
 "isbn":"978-0-340-86687-0",
-},
-
-"sg": {
-"url":"https://www.sundayguardianlive.com/news/7044-pandits-want-official-status-kashmiri-written-devanagari-script",
-"authors": "Akshay Sharma, The Sunday Guardian",
-"title":"Pandits want official status for Kashmiri written in Devanagari script",
+"lang": new Set(['hi']),
 },
 
 "st": {
-"url":"#refs",
 "authors": "Santhosh Thottingal",
 "title":"Personal correspondence",
+"lang": new Set(['hi']),
+},
+
+"cldr": {
+"url":"https://www.unicode.org/cldr/charts/latest/summary/hi.html",
+"authors": "Unicode Consortium",
+"title":"CLDR, Hindi",
+"lang": new Set(['hi']),
 },
 
 "u": {
-"url":"https://www.unicode.org/versions/Unicode13.0.0/ch12.pdf",
 "authors": "Unicode Consortium",
 "title":"The Unicode Standard, Version 13.0",
 "sub":"Chapter 12.1: South and Central Asia-I, Devanagari",
 "pages":"447-472",
 "isbn":"978-1-936213-16-0.",
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch12.pdf",
+"lang": new Set(['hi','ks']),
 },
 
-"ul": {
-"url":"https://www.unicode.org/reports/tr14/",
-"authors": "Unicode Consortium",
-"title":"Unicode Line Breaking Algorithm (UAX#14)",
+"ws": {
+"authors": "Wikipedia",
+"title":"Devanagari",
+"url":"https://en.wikipedia.org/wiki/Devanagari",
+"lang": new Set(['hi']),
+},
+
+"wp": {
+"authors": "Wikipedia",
+"title":"Hindustani phonology",
+"url":"https://en.wikipedia.org/wiki/Hindustani_phonology",
+"lang": new Set(['hi']),
 "reviewed":true
 },
 
 "wlk": {
-"url":"https://en.wikipedia.org/wiki/Kashmiri_language",
 "authors": "Wikipedia",
 "title":"Kashmiri language",
-},
-
-"wp": {
-"url":"https://en.wikipedia.org/wiki/Hindustani_phonology",
-"authors": "Wikipedia",
-"title":"Hindustani phonology",
-"reviewed":true
-},
-
-"ws": {
-"url":"https://en.wikipedia.org/wiki/Devanagari",
-"authors": "Wikipedia",
-"title":"Devanagari",
+"url":"https://en.wikipedia.org/wiki/Kashmiri_language",
+"lang": new Set(['ks']),
 },
 
 
