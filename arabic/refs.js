ef24c1400c6f0edd602115779438c93180eda205
r12a <ishida@w3.org>
2021-05-10 11:54:59 +0100

    arabic: Reformat refs

--- arabic/refs.js
+++ arabic/refs.js
@@ -1,312 +1,346 @@
+window.langSet = true
+
+
 reflist = {
 
 
-"a": {
-"url":"https://w3c.github.io/alreq/",
-"authors": "Behnam Esfahbod, Mostafa Hajizadeh, Najib Tounsi, Richard Ishida, Shervin Afshar,Titus Nemeth",
-"title":"Text Layout Requirements for the Arabic Script (alreq)",
+"kau": {
+"authors": "Kamal Abdali",
+"title":"Urdu on the Mac",
+"url":"http://geomete.com/urdumac.html#Hamza",
+"lang": new Set(['ur']),
 },
 
 "ala": {
-"url":"https://www.loc.gov/catdir/cpso/romanization/urdu.pdf",
 "authors": "ALA-LC Romanization Tables",
 "title":"Urdu (in Arabic script)",
-},
-
-"aww": {
-"url":"https://www.academia.edu/13819335/West_African_Scripts_and_Arabic-Script_Orthographies_in_Socio-Political_Context",
-"authors": "Andy Warren-Rothlin",
-"title":"West African Scripts and Arabic-Script Orthographies in Socio-Political Context",
-},
-
-"bc": {
-"url":"#refs",
-"authors": "Bernard Comrie",
-"title":"The World's Major Languages",
-"isbn":"978-1138184824",
-},
-
-"c": {
-"url":"https://www.unicode.org/cldr/charts/latest/summary/ar.html",
-"authors": "CLDR",
-"title":"Arabic",
-},
-
-"d": {
-"url":"#refs",
-"authors": "Peter T. Daniels and William Bright",
-"title":"The World's Writing Systems",
-"publisher":"Oxford University Press",
-"isbn":"0-19-507993-0",
+"url":"https://www.loc.gov/catdir/cpso/romanization/urdu.pdf",
+"lang": new Set(['ur']),
 },
 
 "dbs": {
-"url":"https://www.academia.edu/38873722/Standardisation_Tendencies_in_Kanuri_and_Hausa_Ajami_Writings",
 "authors": "Dmitry Bondarev",
 "title":"Standardisation Tendencies in Kanuri and Hausa Ajami Writings",
+"url":"https://www.academia.edu/38873722/Standardisation_Tendencies_in_Kanuri_and_Hausa_Ajami_Writings",
+"lang": new Set(['ha']),
 },
 
-"dmt": {
-"url":"#refs",
-"authors": "David Matthews & Mohamed Kasim Dalvi",
-"title":"Teach Yourself Urdu",
-"publisher":"Hodder & Shoughton",
-"isbn":"0-340-67027-4",
+"bc": {
+"authors": "Bernard Comrie",
+"title":"The World's Major Languages",
+"isbn":"978-1138184824",
+"lang": new Set(['ha']),
 },
 
-"gop": {
-"url":"http://wwwold.dkuug.dk/jtc1/sc2/wg2/docs/n2413-4.pdf",
-"authors": "Urdu and Regional Language Software Development Forum, Ministry of Science and Technology, Government of Pakistan",
-"title":" Proposal to add Marks and Digits in Arabic Code Block (for Urdu)",
+"hua": {
+"authors": "Hugo Coolens",
+"title":"Hugo's Urdu Alphabet Pages",
+"url":"http://users.skynet.be/hugocoolens/newurdu/newurdu.html",
+"lang": new Set(['ur']),
 },
 
-"jm": {
-"url":"#refs",
-"authors": "John Mace",
-"title":"Beginner's Arabic Script",
-"publisher":"Hodder & Stoughton Ltd",
-"isbn":"0-340-86016-2",
+"huv": {
+"authors": "Hugo Coolens",
+"title":"Urdu Vowels",
+"url":"http://users.skynet.be/hugocoolens/newurdu/vowels.html",
+"lang": new Set(['ur']),
 },
 
-"kau": {
-"url":"http://geomete.com/urdumac.html#Hamza",
-"authors": "Kamal Abdali",
-"title":"Urdu on the Mac",
+"d": {
+"authors": "Peter T. Daniels and William Bright",
+"title":"The World's Writing Systems",
+"date": "1996",
+"publisher":"Oxford University Press",
+"isbn":"0-19-507993-0",
+"lang": new Set(['ar']),
+},
+
+"rdb": {
+"authors": "Richard Delacy",
+"title":"Beginner's Urdu Script",
+"isbn":"0-340-86028-6",
+"lang": new Set(['ur']),
 },
 
 "ub": {
-"url":"#refs",
 "authors": "Richard Delacy",
 "title":"Beginner's Urdu Script",
 "publisher":"Hodder & Stoughton Ltd",
 "isbn":"0-340-86026-6",
+"lang": new Set(['ur']),
 },
 
-"uu": {
-"url":"#refs",
-"authors": "David Matthews & Mohamed Kasim Dalvi",
-"title":"Urdu",
-"publisher":"Hodder & Stoughton Ltd",
-"isbn":"0-340-86725-6",
-},
-
-"g49": {
-"url":"https://github.com/w3c/alreq/issues/49",
-"authors": "GitHub",
-"title":"Shortcomings of Characters Table",
-},
-
-"hua": {
-"url":"http://users.skynet.be/hugocoolens/newurdu/newurdu.html",
-"authors": "Hugo Coolens",
-"title":"Hugo's Urdu Alphabet Pages",
-},
-
-"huv": {
-"url":"http://users.skynet.be/hugocoolens/newurdu/vowels.html",
-"authors": "Hugo Coolens",
-"title":"Urdu Vowels",
-},
-
-"jkp": {
-"url":"http://www.unicode.org/L2/L2001/01425-arabic_marks.pdf",
-"authors": "Jonathan Kew",
-"title":"Proposal to add Arabic-script honorifics and other marks",
-},
-
-"lck": {
-"url":"https://www.loc.gov/catdir/cpso/romanization/kashmiri.pdf",
-"authors": "Library of Congress",
-"title":"Kashmiri (in Perso-Arabic Script) ",
-},
-
-"mki": {
-"url":"http://www.columbia.edu/~mk2580/urdu_section/handouts/izafat.pdf",
-"authors": "Mohamad Khan",
-"title":"Izāfat",
+"a": {
+"authors": "Behnam Esfahbod, Mostafa Hajizadeh, Najib Tounsi, Richard Ishida, Shervin Afshar, Titus Nemeth",
+"title":"Text Layout Requirements for the Arabic Script (alreq)",
+"url":"https://w3c.github.io/alreq/",
+"lang": new Set(['ar','fa']),
 },
 
 "pm": {
-"url":"#refs",
 "authors": "Narguess Farzad",
 "title":"Modern Persian",
 "publisher":"Hodder & Stoughton Ltd",
 "isbn":"0-340-80265-0",
+"lang": new Set(['fa']),
+},
+
+"g49": {
+"authors": "GitHub",
+"title":"Shortcomings of Characters Table",
+"url":"https://github.com/w3c/alreq/issues/49",
+"lang": new Set(['ar','fa']),
+},
+
+"shp": {
+"authors": "Sarmad Hussain",
+"title":"Proposal to add Marks and Digits in Arabic Code Block (for Urdu)",
+"sub":"L2-02/163",
+"url":"http://www.unicode.org/L2/L2002/02163-n2413-4-urdu.pdf",
+"lang": new Set(['ur']),
+"reviewed":true
 },
 
 "rmcs": {
-"url":"https://www.w3.org/TR/predefined-counter-styles/#arabic-styles",
 "authors": "Richard Ishida",
 "title":"Ready-made Counter Styles",
+"url":"https://www.w3.org/TR/predefined-counter-styles/#arabic-styles",
+"lang": new Set(['ar','fa']),
 "reviewed":true
 },
 
-"rpp": {
-"url":"http://www.unicode.org/L2/L2012/12381-dotless-noon.pdf",
-"authors": "Roozbeh Pournader",
-"title":"Initial and medial forms of Arabic Letter Noon Ghunna",
-"sub":"l2/12-381, 2012-11-03",
+"jkp": {
+"authors": "Jonathan Kew",
+"title":"Proposal to add Arabic-script honorifics and other marks",
+"url":"http://www.unicode.org/L2/L2001/01425-arabic_marks.pdf",
+"lang": new Set(['ar']),
 },
 
-"f": {
-"url":"https://www.freearabicdictionary.com/",
-"authors": "FreeArabicDictionary",
-"title":"Arabic Dictionary & Translation",
+"mki": {
+"authors": "Mohamad Khan",
+"title":"Izāfat",
+"url":"http://www.columbia.edu/~mk2580/urdu_section/handouts/izafat.pdf",
+"lang": new Set(['ur']),
 },
 
-"j": {
-"url":"#refs",
-"authors": "Jack Smart and Francis Altorfer",
-"title":"Teach Yourself Arabic",
-"publisher":"Oxford University Press",
-"isbn":"978-0-340-86996-3",
-"reviewed":true
+"sle": {
+"authors": "Swaran Lata",
+"title":"Encoding of additional characters in Unicode for representation of Kashmiri",
+"url":"https://www.unicode.org/L2/L2009/09311-kashmiri-ltr-from-swaran-lata.pdf",
+"lang": new Set(['ks']),
 },
 
 "l": {
-"url":"https://www.loc.gov/catdir/cpso/romanization/arabic.pdf",
 "authors": "Library of Congress",
 "title":"Arabic romanization table",
+"url":"https://www.loc.gov/catdir/cpso/romanization/arabic.pdf",
+"lang": new Set(['ar']),
 "reviewed":true
 },
 
-"lpp": {
-"url":"https://www.unicode.org/L2/L2018/18094-arabic-hausa.pdf",
-"authors": "Lorna Priest Evans, Andy Warren-Rothlin",
-"title":"Proposal to encode additional Arabic script characters for Hausa to the UCS",
-"reviewed":true
+"lck": {
+"authors": "Library of Congress",
+"title":"Kashmiri (in Perso-Arabic Script) ",
+"url":"https://www.loc.gov/catdir/cpso/romanization/kashmiri.pdf",
+"lang": new Set(['ks']),
 },
 
-"lpz": {
-"url":"http://www.unicode.org/L2/L2012/12181r-zain-with-hat.pdf",
-"authors": "Lorna Priest",
-"title":"Proposal to add ARABIC LETTER ZAIN WITH INVERTED V ABOVE",
-"reviewed":true
+"jm": {
+"authors": "John Mace",
+"title":"Beginner's Arabic Script",
+"publisher":"Hodder & Stoughton Ltd",
+"isbn":"0-340-86016-2",
+"url":"#refs",
+"lang": new Set(['ar']),
+},
+
+"uu": {
+"authors": "David Matthews & Mohamed Kasim Dalvi",
+"title":"Urdu",
+"publisher":"Hodder & Stoughton Ltd",
+"isbn":"0-340-86725-6",
+"lang": new Set(['ur']),
+},
+
+"dmt": {
+"authors": "David Matthews & Mohamed Kasim Dalvi",
+"title":"Teach Yourself Urdu",
+"publisher":"Hodder & Shoughton",
+"isbn":"0-340-67027-4",
+"lang": new Set(['ur']),
 },
 
 "oh": {
-"url":"http://www.omniglot.com/writing/hausa.htm",
 "authors": " Omniglot",
 "title":"Hausa (Harshen Hausa / هَرْشَن هَوْسَ)",
-"reviewed":true
-},
-
-"npp": {
-"url":"#refs",
-"authors": "Neil Patel",
-"title":"Private correspondence",
+"url":"http://www.omniglot.com/writing/hausa.htm",
+"lang": new Set(['ha']),
 "reviewed":true
 },
 
 "pah": {
-"url":"http://www.bisharat.net/wikidoc/pmwiki.php/PanAfrLoc/Hausa",
 "authors": "Pan AfriLoc",
 "title":"Hausa - Haoussa - هوس",
+"url":"http://www.bisharat.net/wikidoc/pmwiki.php/PanAfrLoc/Hausa",
+"lang": new Set(['ha']),
 "reviewed":true
 },
 
-"rdb": {
-"url":"#refs",
-"authors": "Richard Delacy",
-"title":"Beginner's Urdu Script",
-"isbn":"0-340-86028-6",
+"npp": {
+"authors": "Neil Patel",
+"title":"Private correspondence",
+"lang": new Set(['ha']),
+"reviewed":true
+},
+
+"rpp": {
+"authors": "Roozbeh Pournader",
+"title":"Initial and medial forms of Arabic Letter Noon Ghunna",
+"sub":"l2/12-381, 2012-11-03",
+"url":"http://www.unicode.org/L2/L2012/12381-dotless-noon.pdf",
+"lang": new Set(['ar','ur']),
+},
+
+"lpz": {
+"authors": "Lorna Priest",
+"title":"Proposal to add ARABIC LETTER ZAIN WITH INVERTED V ABOVE",
+"url":"http://www.unicode.org/L2/L2012/12181r-zain-with-hat.pdf",
+"lang": new Set(['ha']),
+"reviewed":true
+},
+
+"lpp": {
+"authors": "Lorna Priest Evans, Andy Warren-Rothlin",
+"title":"Proposal to encode additional Arabic script characters for Hausa to the UCS",
+"url":"https://www.unicode.org/L2/L2018/18094-arabic-hausa.pdf",
+"lang": new Set(['ha']),
+"reviewed":true
+},
+
+"j": {
+"authors": "Jack Smart and Francis Altorfer",
+"title":"Teach Yourself Arabic",
+"publisher":"Oxford University Press",
+"isbn":"978-0-340-86996-3",
+"lang": new Set(['ar','ur']),
+"reviewed":true
 },
 
 "s": {
-"url":"http://scriptsource.org/cms/scripts/page.php?item_id=script_detail&key=Arab",
 "authors": "ScriptSource",
 "title":"Arabic",
+"url":"http://scriptsource.org/cms/scripts/page.php?item_id=script_detail&key=Arab",
+"lang": new Set(['ar']),
 "reviewed":true
 },
 
-"shp": {
-"url":"http://www.unicode.org/L2/L2002/02163-n2413-4-urdu.pdf",
-"authors": "Sarmad Hussain",
-"title":"Proposal to add Marks and Digits in Arabic Code Block (for Urdu)",
-"sub":"L2-02/163",
-"reviewed":true
-},
-
-"sle": {
-"url":"https://www.unicode.org/L2/L2009/09311-kashmiri-ltr-from-swaran-lata.pdf",
-"authors": "Swaran Lata",
-"title":"Encoding of additional characters in Unicode for representation of Kashmiri",
+"c": {
+"authors": "Unicode Consortium",
+"title":"CLDR, Arabic",
+"url":"https://www.unicode.org/cldr/charts/latest/summary/ar.html",
+"lang": new Set(['ar']),
 },
 
 "u": {
-"url":"https://www.unicode.org/versions/Unicode13.0.0/ch09.pdf",
 "authors": "Unicode Consortium",
 "title":"The Unicode Standard, Version 13.0",
 "sub":"Chapter 9.2: Middle East-I, Arabic",
 "pages":"365-389",
 "isbn":"978-1-936213-16-0.",
-},
-
-"ul": {
-"url":"https://www.unicode.org/reports/tr14/",
-"authors": "Unicode Consortium",
-"title":"Unicode Line Breaking Algorithm (UAX#14)",
-"reviewed":true
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch09.pdf",
+"lang": new Set(['ar','ha']),
 },
 
 "uwa": {
-"url":"https://www.univie.ac.at/Hausa/Ajami/Ajami_Hausa.html",
 "authors": " Universität Wien, adap. from Neil Skinner",
 "title":"Ajami Zeichensatz",
+"url":"https://www.univie.ac.at/Hausa/Ajami/Ajami_Hausa.html",
+"lang": new Set(['ha']),
 "reviewed":true
 },
 
-"wp": {
-"url":"https://en.wikipedia.org/wiki/Arabic_phonology",
-"authors": "Wikipedia",
-"title":"Arabic phonology",
+"gop": {
+"authors": "Urdu and Regional Language Software Development Forum, Ministry of Science and Technology, Government of Pakistan",
+"title":" Proposal to add Marks and Digits in Arabic Code Block (for Urdu)",
+"url":"http://wwwold.dkuug.dk/jtc1/sc2/wg2/docs/n2413-4.pdf",
+"lang": new Set(['ur']),
 },
 
-"wb": {
-"url":"http://en.wikipedia.org/wiki/Basmala",
-"authors": "Wikipedia",
-"title":"Basmala",
-},
-
-"whl": {
-"url":"https://en.wikipedia.org/wiki/Hausa_language",
-"authors": "Wikipedia",
-"title":"Hausa language",
-},
-
-"wkl": {
-"url":"https://en.wikipedia.org/wiki/Kashmiri_language",
-"authors": "Wikipedia",
-"title":"Kashmiri language",
-},
-
-"wul": {
-"url":"https://en.wikipedia.org/wiki/Uyghur_language",
-"authors": "Wikipedia",
-"title":"Uyghur language",
-},
-
-"whp": {
-"url":"https://en.wikipedia.org/wiki/Hindustani_phonology",
-"authors": "Wikipedia",
-"title":"Hindustani phonology",
+"aww": {
+"authors": "Andy Warren-Rothlin",
+"title":"West African Scripts and Arabic-Script Orthographies in Socio-Political Context",
+"url":"https://www.academia.edu/13819335/West_African_Scripts_and_Arabic-Script_Orthographies_in_Socio-Political_Context",
+"lang": new Set(['ha']),
 },
 
 "wa": {
-"url":"https://en.wikipedia.org/wiki/Arabic_alphabet",
 "authors": "Wikipedia",
 "title":"Arabic alphabet",
+"url":"https://en.wikipedia.org/wiki/Arabic_alphabet",
+"lang": new Set(['ar']),
+},
+
+"wp": {
+"authors": "Wikipedia",
+"title":"Arabic phonology",
+"url":"https://en.wikipedia.org/wiki/Arabic_phonology",
+"lang": new Set(['ar']),
 },
 
 "ws": {
-"url":"http://en.wikipedia.org/wiki/Arabic_script",
 "authors": "Wikipedia",
 "title":"Arabic script",
+"url":"http://en.wikipedia.org/wiki/Arabic_script",
+"lang": new Set(['ar']),
 },
 
+"wb": {
+"authors": "Wikipedia",
+"title":"Basmala",
+"url":"http://en.wikipedia.org/wiki/Basmala",
+"lang": new Set(['ar']),
+},
+
+"whl": {
+"authors": "Wikipedia",
+"title":"Hausa language",
+"url":"https://en.wikipedia.org/wiki/Hausa_language",
+"lang": new Set(['ha']),
+},
+
+"whp": {
+"authors": "Wikipedia",
+"title":"Hindustani phonology",
+"url":"https://en.wikipedia.org/wiki/Hindustani_phonology",
+"lang": new Set(['ur']),
+},
+
+"wkl": {
+"authors": "Wikipedia",
+"title":"Kashmiri language",
+"url":"https://en.wikipedia.org/wiki/Kashmiri_language",
+"lang": new Set(['ks']),
+},
+
+"wul": {
+"authors": "Wikipedia",
+"title":"Uyghur language",
+"url":"https://en.wikipedia.org/wiki/Uyghur_language",
+"lang": new Set(['ug']),
+},
 
 }
 
 
 
-// <a class="fn">ws,#tones</a>
+terms = {
+
+"f": {
+"url":"https://www.freearabicdictionary.com/",
+"authors": "FreeArabicDictionary",
+"title":"Arabic Dictionary & Translation",
+"lang": new Set(['ar']),
+},
+
+
+}
