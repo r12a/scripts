c9b4aa516de88d98709a96d785e7f5b9396d9ea8
r12a <ishida@w3.org>
2021-05-10 06:18:52 +0100

    osage: Update character details & template. Add examples & vocab file. Implement new refs approach, showing all refs.

--- tifinagh/refs.js
+++ tifinagh/refs.js
@@ -1,22 +1,29 @@
-reflist = {
+window.langSet = true
 
+reflist = {
 
 "ae": {
 "url":"https://www.unicode.org/L2/L2010/10278-tifinagh-evolution.pdf",
 "authors": "Paul Anderson",
 "title":"Evolution of the Tifinagh script in Unicode",
+"lang": new Set(['zgh']),
+"reviewed":false
 },
 
 "ap": {
 "url":"https://www.unicode.org/L2/L2010/10096-tifinagh-vowels.pdf",
 "authors": "Paul Anderson",
 "title":"Proposal to add two Tifinagh characters for vowels in Tuareg language variants",
+"lang": new Set(['zgh']),
+"reviewed":false
 },
 
 "bt": {
 "url":"https://www.academia.edu/16657385/Tifinagh_and_the_IRCAM_Explorations_in_Cursiveness_and_Bicamelarism_in_the_Tifinagh_script",
 "authors": "Juan Luis Blanco",
 "title":"Tifinagh & the IRCAM",
+"lang": new Set(['zgh']),
+"reviewed":false
 },
 
 "d": {
@@ -25,6 +32,7 @@ reflist = {
 "title":"The World's Writing Systems",
 "publisher":"Oxford University Press",
 "isbn":"0-19-507993-0",
+"lang": new Set(['zgh']),
 "reviewed":true
 },
 
@@ -32,18 +40,23 @@ reflist = {
 "url":"http://std.dkuug.dk/JTC1/SC2/WG2/docs/n4069.pdf",
 "authors": "Lorna A. Priest, Jon Coblentz",
 "title":"Documenting a fallback glyph for Tifinagh Consonant Joiner",
+"lang": new Set(['zgh']),
+"reviewed":false
 },
 
 "pcs": {
 "url":"http://std.dkuug.dk/jtc1/sc2/wg2/docs/n3482.pdf",
 "authors": "Lorna A. Priest, Jon Coblentz, Andrew Savage",
 "title":"Proposal to encode additional Tifinagh characters",
+"lang": new Set(['zgh']),
+"reviewed":false
 },
 
 "s": {
 "url":"http://scriptsource.org/cms/scripts/page.php?item_id=script_detail&key=Tfng",
 "authors": "ScriptSource",
 "title":"Tifinagh (Berber)",
+"lang": new Set(['zgh']),
 "reviewed":true
 },
 
@@ -54,12 +67,15 @@ reflist = {
 "sub":"Chapter 19.3: Africa, Tifinagh",
 "pages":"769-771",
 "isbn":"978-1-936213-16-0.",
+"lang": new Set(['zgh']),
+"reviewed":false
 },
 
 "ul": {
 "url":"https://www.unicode.org/reports/tr14/",
 "authors": "Unicode Consortium",
 "title":"Unicode Line Breaking Algorithm (UAX#14)",
+"lang": new Set(['zgh']),
 "reviewed":true
 },
 
@@ -67,12 +83,48 @@ reflist = {
 "url":"https://en.wikipedia.org/wiki/Berber_Latin_alphabet",
 "authors": "Wikipedia",
 "title":"Berber Latin alphabet",
+"lang": new Set(['zgh']),
+"reviewed":false
+},
+
+"wber": {
+"url":"https://en.wikipedia.org/wiki/Berber_languages",
+"authors": "Wikipedia",
+"title":"Berber languages",
+"lang": new Set(['zgh']),
+"reviewed":false
+},
+
+"wsma": {
+"url":"http://en.wikipedia.org/wiki/Standard_Moroccan_Amazigh",
+"authors": "Wikipedia",
+"title":"Standard Moroccan Amazigh",
+"lang": new Set(['zgh']),
+"reviewed":false
+},
+
+"wsl": {
+"url":"http://en.wikipedia.org/wiki/Shilha_language",
+"authors": "Wikipedia",
+"title":"Shilha language",
+"lang": new Set(['zgh']),
+"reviewed":false
+},
+
+"wcat": {
+"url":"http://en.wikipedia.org/wiki/Central_Atlas_Tamazight",
+"authors": "Wikipedia",
+"title":"Central Atlas Tamazight",
+"lang": new Set(['zgh']),
+"reviewed":false
 },
 
 "ws": {
 "url":"http://en.wikipedia.org/wiki/Tifinagh",
 "authors": "Wikipedia",
 "title":"Tifinagh",
+"lang": new Set(['zgh']),
+"reviewed":false
 },
 
 
@@ -80,4 +132,22 @@ reflist = {
 
 
 
-// <a class="fn">ws,#tones</a>
+terms = {
+
+"i": {
+"url":"https://tal.ircam.ma/tamawalt/alphabet_p.php",
+"authors": "ERCIM",
+"title":"ⵜⴰⵎⴰⵡⴰⵍⵜ ⵏ ⵉⵎⵥⵥⵢⴰⵏⵏ",
+},
+
+"a": {
+"url":"https://amawal.net/about",
+"authors": "?",
+"title":"ⴰⵎⴰⵡⴰⵍ The Berber dictionary",
+"notes":"Latin only",
+},
+
+
+}
+
+
