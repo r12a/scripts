8b6ef8c62e8eed0dbf26aa9fbb1915011849ff69
r12a <ishida@w3.org>
2021-05-10 11:58:48 +0100

    taitham: Reformat refs

--- taitham/refs.js
+++ taitham/refs.js
@@ -1,73 +1,74 @@
+window.langSet = true
+
+
+
 reflist = {
 
-
 "c": {
-"url":"#refs",
 "authors": "Patrick Chew",
 "title":"Various private correspondence",
+"lang": new Set(['kkh']),
 },
 
 "d": {
-"url":"#refs",
 "authors": "Peter T. Daniels and William Bright",
 "title":"The World's Writing Systems",
 "publisher":"Oxford University Press",
 "isbn":"0-19-507993-0",
+"lang": new Set(['kkh']),
 },
 
 "e": {
-"url":"http://std.dkuug.dk/jtc1/sc2/wg2/docs/n3207.pdf",
 "authors": "Michael Everson, Martin Hosken, Peter Constable",
 "title":"Revised proposal for encoding the Lanna script in the BMP of the UCS",
+"url":"http://std.dkuug.dk/jtc1/sc2/wg2/docs/n3207.pdf",
+"lang": new Set(['nod']),
 },
 
 "h": {
-"url":"#refs",
 "authors": "Martin Hosken",
 "title":"Structuring Tai Tham Unicode",
-},
-
-"o": {
-"url":"https://evols.library.manoa.hawaii.edu/bitstream/10524/52403/09_Owen2017description.pdf",
-"authors": "R Wyn Owen",
-"title":"A description and linguistic analysis of the Tai Khuen writing system",
-"reviewed":true
-},
-
-"r": {
-"url":"#refs",
-"authors": "Richard Wordingham",
-"title":"Personal correspondence",
-"reviewed":true
+"lang": new Set(['kkh']),
 },
 
 "u": {
-"url":"https://www.unicode.org/versions/Unicode13.0.0/ch16.pdf",
 "authors": "Unicode Consortium",
 "title":"The Unicode Standard, Version 13.0",
 "sub":"Chapter 16.7: Southeast Asia, Tai Tham",
 "pages":"670-672",
 "isbn":"978-1-936213-16-0.",
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch16.pdf",
+"lang": new Set(['kkh','nod']),
 },
 
-"ul": {
-"url":"https://www.unicode.org/reports/tr14/",
-"authors": "Unicode Consortium",
-"title":"Unicode Line Breaking Algorithm (UAX#14)",
+"wnl": {
+"authors": "Wikipedia",
+"title":"Northern Thai language",
+"url":"https://en.wikipedia.org/wiki/Northern_Thai_language",
+"lang": new Set(['nod']),
+"reviewed":true
+},
+
+"r": {
+"authors": "Richard Wordingham",
+"title":"Personal correspondence",
+"lang": new Set(['kkh']),
 "reviewed":true
 },
 
 "w": {
-"url":"http://wrdingham.co.uk/lanna/renderer_test.htm",
 "authors": "Richard Wordingham",
 "title":"Tai Tham Renderer Tests",
+"url":"http://wrdingham.co.uk/lanna/renderer_test.htm",
+"lang": new Set(['nod']),
 "reviewed":true
 },
 
-"wnl": {
-"url":"https://en.wikipedia.org/wiki/Northern_Thai_language",
-"authors": "Wikipedia",
-"title":"Northern Thai language",
+"o": {
+"authors": "R Wyn Owen",
+"title":"A description and linguistic analysis of the Tai Khuen writing system",
+"url":"https://evols.library.manoa.hawaii.edu/bitstream/10524/52403/09_Owen2017description.pdf",
+"lang": new Set(['kkh','nod']),
 "reviewed":true
 },
 
