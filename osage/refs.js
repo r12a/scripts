980e420497ba751cfcb3c0aa476e1298425f02eb
r12a <ishida@w3.org>
2021-05-10 11:57:48 +0100

    osage: Reformat refs

--- osage/refs.js
+++ osage/refs.js
@@ -2,22 +2,24 @@ reflist = {
 
 
 "e": {
-"url":"http://std.dkuug.dk/jtc1/sc2/wg2/docs/n4619.pdf",
 "authors": "Michael Everson, Herman Mongrain Lookout, Cameron Pratt",
 "title":"Final proposal to encode the Osage script in the UCS",
+"date": "2014",
+"url":"http://std.dkuug.dk/jtc1/sc2/wg2/docs/n4619.pdf",
 },
 
 "o": {
-"url":"https://www.osagelanguage.com/courses/",
 "authors": "Osage Nation Language Dept.",
 "title":"Osage Language Beginner Lessons",
+"url":"https://www.osagelanguage.com/courses/",
 "reviewed":true
 },
 
 "q": {
-"url":"https://books.google.co.uk/books?id=wnvaInod5MAC&printsec=frontcover#v=onepage&q&f=false",
 "authors": "Carlyn Quintero",
 "title":"Osage Grammar (preview)",
+"date": "2004",
+"url":"https://books.google.co.uk/books?id=wnvaInod5MAC&printsec=frontcover#v=onepage&q&f=false",
 "reviewed":true
 },
 
@@ -29,31 +31,25 @@ reflist = {
 },
 
 "u": {
-"url":"https://www.unicode.org/versions/Unicode13.0.0/ch20.pdf",
 "authors": "Unicode Consortium",
 "title":"The Unicode Standard, Version 13.0",
+"date": "2020",
 "sub":"Chapter 20.3: Americas, Osage",
 "pages":"792",
 "isbn":"978-1-936213-16-0.",
-},
-
-"ul": {
-"url":"https://www.unicode.org/reports/tr14/",
-"authors": "Unicode Consortium",
-"title":"Unicode Line Breaking Algorithm (UAX#14)",
-"reviewed":true
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch20.pdf",
 },
 
 "wl": {
-"url":"https://en.wikipedia.org/wiki/Osage_language",
 "authors": "Wikipedia",
 "title":"Osage language",
+"url":"https://en.wikipedia.org/wiki/Osage_language",
 },
 
 "ws": {
-"url":"http://en.wikipedia.org/wiki/Osage_script",
 "authors": "Wikipedia",
 "title":"Osage script",
+"url":"http://en.wikipedia.org/wiki/Osage_script",
 },
 
 
