ddc35a1e083a0d59c21e19d00cfd3a0791aaf3ec
r12a <ishida@w3.org>
2021-05-10 11:58:07 +0100

    santali: Reformat refs

--- santali/refs.js
+++ santali/refs.js
@@ -2,58 +2,56 @@ reflist = {
 
 
 "d": {
-"url":"#refs",
 "authors": "Peter T. Daniels and William Bright",
 "title":"The World's Writing Systems",
+"date": "1996",
 "publisher":"Oxford University Press",
 "pages":"614-615",
 "isbn":"0-19-507993-0",
 "reviewed":true
 },
 
-"fp": {
-"url":"https://www.unicode.org/L2/L2005/05243r-n2984-ol-chiki.pdf",
-"authors": "Everson, Michael",
-"title":"Final proposal to encode the Ol Chiki script in the UCS",
+"rp": {
+"authors": "Michael Everson, R.C. Hansdah & N. C. Murmu",
+"title":"Revised proposal for encoding the Ol Chiki script in the UCS",
+"date": "2002",
+"url":"https://www.unicode.org/L2/L2002/02422-n2505-olchiki.pdf",
 "reviewed":true
 },
 
-"rp": {
-"url":"https://www.unicode.org/L2/L2002/02422-n2505-olchiki.pdf",
-"authors": "Michael Everson, R.C. Hansdah & N. C. Murmu",
-"title":"Revised proposal for encoding the Ol Chiki script in the UCS",
+"fp": {
+"authors": "Everson, Michael",
+"title":"Final proposal to encode the Ol Chiki script in the UCS",
+"date": "2005",
+"url":"https://www.unicode.org/L2/L2005/05243r-n2984-ol-chiki.pdf",
 "reviewed":true
 },
 
 "u": {
-"url":"https://www.unicode.org/versions/Unicode13.0.0/ch13.pdf",
 "authors": "Unicode Consortium",
 "title":"The Unicode Standard, Version 13.0",
+"date": "2020",
 "sub":"Chapter 13.10: South and Central Asia II, Ol Chiki",
 "pages":"555-556",
 "isbn":"978-1-936213-16-0.",
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch13.pdf",
 "reviewed":true
 },
 
-"ul": {
-"url":"https://www.unicode.org/reports/tr14/",
-"authors": "Unicode Consortium",
-"title":"Unicode Line Breaking Algorithm (UAX#14)",
+"ws": {
+"authors": "Wikipedia",
+"title":"Ol Chiki script",
+"retr": "Apr 2021",
+"url":"https://en.wikipedia.org/wiki/Ol_Chiki_script",
 "reviewed":true
 },
 
 "wl": {
-"url":"https://en.wikipedia.org/wiki/Santali_language",
 "authors": "Wikipedia",
 "title":"Santali language",
+"retr": "Apr 2021",
+"url":"https://en.wikipedia.org/wiki/Santali_language",
 "reviewed":false
-},
-
-"ws": {
-"url":"https://en.wikipedia.org/wiki/Ol_Chiki_script",
-"authors": "Wikipedia",
-"title":"Ol Chiki script",
-"reviewed":true
 },
 
 
