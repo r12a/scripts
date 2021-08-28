b92e5d6444ead290ff45ac5dad2bfa0512f05b23
r12a <ishida@w3.org>
2021-08-28 06:06:20 +0100

    scriptdb: more runic files

---
+++ runic/ang-examples.js
@@ -0,0 +1,14 @@
+/*
+@ Source: 
+§ 'Noto Sans Runic'
+# 200
+~ ltr
+! ang
+*/
+
+autoExpandExamples.ang = `
+
+ᨀᨆᨗᨔᨗ|Thursday|kɑmɪːsɪ (kamisi)
+
+`
+
---
+++ runic/refs.js
@@ -0,0 +1,110 @@
+window.langSet = true
+
+
+
+reflist = {
+
+/*
+"auth_initials": {
+"authors": "Sort by family name of first contributor",
+"title":"The World's Writing Systems",
+"sub": "subtitle",
+"date": "1996",
+"retr": "May 2021",
+"publisher":"Oxford University Press",
+"isbn":"0-19-507993-0",
+"pages": "xxx",
+"url":"refs",
+"lang": new Set(['ru']),
+"reviewed":false
+},
+*/
+
+"d": {
+"authors": "Peter T. Daniels and William Bright",
+"title":"The World's Writing Systems",
+"date": "1996",
+"publisher":"Oxford University Press",
+"isbn":"0-19-507993-0",
+"pages":"333-339",
+"lang": new Set(['ang']),
+"reviewed":false
+},
+
+"u": {
+"authors": "Unicode Consortium",
+"title":"The Unicode Standard, Version 14.0",
+"sub":"Chapter 8.6: Europe II, Runic",
+"pages":"346-348",
+"isbn":"978-1-936213-16-0",
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch08.pdf",
+"lang": new Set(['ang']),
+"reviewed": false
+},
+
+"wr": {
+"authors": "Wikipedia",
+"title":"Runes",
+//"retr": "May 2021",
+"url":"https://en.wikipedia.org/wiki/Runes",
+"lang": new Set(['ang']),
+"reviewed": false
+},
+
+"wa": {
+"authors": "Wikipedia",
+"title":"Anglo-Saxon runes",
+"retr": "Aug 2021",
+"url":"https://en.wikipedia.org/wiki/Anglo-Saxon_runes",
+"lang": new Set(['ang']),
+"reviewed":false
+},
+
+"wm": {
+"authors": "Wikipedia",
+"title":"Medieval runes",
+//"retr": "Aug 2021",
+"url":"https://en.wikipedia.org/wiki/Medieval_runes",
+"lang": new Set(['ang']),
+"reviewed": false
+},
+
+"wy": {
+"authors": "Wikipedia",
+"title":"Younger Futhark",
+//"retr": "Aug 2021",
+"url":"https://en.wikipedia.org/wiki/Younger_Futhark",
+"lang": new Set(['ang']),
+"reviewed": false
+},
+
+
+}
+
+
+
+
+
+
+var terms = {
+
+"i": {
+"url":"https://tal.ircam.ma/tamawalt/alphabet_p.php",
+"authors": "ERCIM",
+"title":"ⵜⴰⵎⴰⵡⴰⵍⵜ ⵏ ⵉⵎⵥⵥⵢⴰⵏⵏ",
+},
+
+"a": {
+"url":"https://amawal.net/about",
+"authors": "?",
+"title":"ⴰⵎⴰⵡⴰⵍ The Berber dictionary",
+"notes":"Latin only",
+},
+
+
+}
+
+
+
+
+
