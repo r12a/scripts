2bad9a4e8d408376ef501446c283be03f514568d
r12a <ishida@w3.org>
2021-09-09 07:50:47 +0100

    + cyrillic/mn

---
+++ cyrillic/mn-examples.js
@@ -0,0 +1,375 @@
+/*
+@ Source: https://en.wiktionary.org/wiki/Appendix:Mongolian_Swadesh_list
+@ Source: https://www.youtube.com/watch?v=PnZR2DvmOck
+§ 'Noto Sans Cyrillic'
+# 200
+~ ltr
+! mn
+*/
+
+autoExpandExamples.mn = `
+
+#https://en.wiktionary.org/wiki/Appendix:Mongolian_Swadesh_list
+би|I|bi|bi
+чи|you (singular)|ʧi
+тэр|he, that|terĕ|ter
+бид|we|bidә|bid
+та нар|you (plural)|ta:năr|ta nar
+тэд нар|they||ted nar
+энэ|this|en|ene
+энд|here|eŋdĕ|end
+тэнд|there|tendә|tend
+хэн|who|xen|khen
+юу|what|juː|yuu
+хаана|where|xaːnă|khaana
+хэзээ|when|xeʣeː|khezee
+яаж|how|jaːd͡ʒ|yaaj
+-гүй|not||-güi
+бүх|all|bxĕ|bükh
+олон|many|olon|olon
+зарим|some|ʣȧrim|zarim
+цөөн|few|ʦʊːn]|tsöön
+өөр|other|ʊːr|öör
+нэг|one|neg dexĭ|neg
+хоёр|two|xɔjɔ̆r
+гурав|three|ɡʊrᵊw̜|gurav
+дөрөв|four|dʊrʊw|döröv
+тав|five|taw|tav
+том|big|tom|tom
+урт|long|urtă|urt
+өргөн|wide|өrɡөn|örgön
+зузаан|thick|d͡zud͡zaːn|zuzaan
+хүнд|heavy|xundĕ|khünd
+жижиг|small|ʤiʤiɡ|jijig
+жаал|small|ʤaːɮ|jaal
+богино|short|bȯɡinŏ|bogino
+цухал|narrow|t͡suxal|tsukhal
+нимгэн|thin|nimɡen|nimgen
+эмэгтэй|woman|emexteː|emegtei
+эр хүн|man (adult male)||er khün
+хүн|man (human being)|xun|khün
+хүүхэд|child|xuːxed|khüükhed
+эхнэр|wife||ekhner
+гэргий|wife||gergii
+нөхөр|husband||nökhör
+ээж|mother|eet͡ʃ|eej
+эх|mother||ekh
+аав|father|aːw̜|aav
+эцэг|father||etseg
+амьтан|animal||ȧmĭtan
+загас|fish||zagas
+шувуу|bird||shuvuu
+нохой|dog||nokhoi
+бөөс|louse||böös
+могой|snake||mogoi
+хорхой|worm||khorkhoi
+мод|tree||mod
+ой|forest||oi
+таяг|stick||tayag
+саваа|stick|sawaː|savaa
+жимс|fruit||jims
+үр|seed||ür
+навч|leaf|naft͡ʃĭ|navch
+үндэс|root||ündes
+холтос|bark (of a tree)||kholtos
+цэцэг|flower||tsetseg
+өвс|grass||övs
+олс|rope||ols
+арьс|skin||ȧrĭsᴧ
+мах|meat||makh
+цус|blood||tsus
+яс|bone|jasᵊ|yas
+өөх|fat (noun)||öökh
+эвэр|horn||ever
+сүүл|tail||süül
+өд|feather||öd
+үс|hair||üs
+толгой|head||tolgoi
+чих|ear|t͡ʃixᵊ|chikh
+нүд|eye||nüd
+хамар|nose||khamar
+ам|mouth||am
+шүд|tooth||shüd
+хэл|tongue (organ)||khel
+хумс|fingernail||khums
+хөл|foot||khöl
+хөл|leg||khöl
+өвдөг|knee||övdög
+гар|hand||gar
+далавч|wing|dalaft͡ʃĭ|dalavch
+жигүүр|wing||jigüür
+гэдэс|belly||gedes
+гэдэс|guts||gedes
+хүзүү|neck||khüzüü
+нуруу|back||nuruu
+хөх|breast||khökh
+зүрх|heart||zürkh
+элэг|liver||eleg
+уух|to drink||uukh
+идэх|to eat||idekh
+хазах|to bite||khazakh
+үмхэх|to bite||ümkhekh
+зуух|to bite||zuukh
+сорох|to suck||sorokh
+хаях|to spit||khayakh
+бөөлжих|to vomit||bööljikh
+үлээх|to blow||üleekh
+амьсгалах|to breathe||amisgalakh/ȧmisxal
+инээх|to laugh||ineekh
+үзэх|to see||üzekh
+харах|to see||kharakh
+сонсох|to hear||sonsokh
+мэдэх|to know||medekh
+бодох|to think||bodokh
+үнэрлэх|to smell||ünerlekh
+айх|to fear||aikh
+унтах|to sleep||untakh
+амьдрах|to live||amidrakh/ȧmĭdar
+үхэх|to die||ükhekh
+нас барах|to die||nas barakh
+алах|to kill||alakh
+тэмцэх|to fight||temtsekh
+байлдах|to fight||baildakh
+агнах|to hunt||agnakh
+цохих|to hit||tsokhikh
+хэрчих|to cut||kherchikh
+огтлох|to cut||ogtlokh
+хагарах|to split||khagarakh
+хатгах|to stab||khatgakh
+маажих|to scratch||maajikh
+ухах|to dig||ukhakh
+сэлэх|to swim||selekh
+нисэх|to fly||nisekh
+алхах|to walk||alkhakh
+ирэх|to come||irekh
+хэвтэх|to lie (bed)|xeftĕ|khevtekh
+суух|to sit||suukh
+зогсох|to stand||zogsokh
+эргэх|to turn (intransitive)||ergekh
+унах|to fall||unakh
+өгөх|to give||ögökh
+барих|to hold||barikh
+шахах|to squeeze||shakhakh
+угаах|to wash||ugaakh
+оёх|to sew||oyokh
+хадах|to sew||khadakh
+тоолох|to count||toolokh
+хэлэх|to say||khelekh
+дуулах|to sing||duulakh
+тоглох|to play||toglokh
+нар|sun||nar
+сар|moon||sar
+од|star||od
+ус|water||us
+бороо|rain||boroo
+гол|river||gol
+мөрөн|river||mörön
+нуур|lake||nuur
+далай|sea||dalai
+давс|salt|dawas|davs
+чулуу|stone||chuluu
+элс|sand||els
+дэлхий|earth||delkhii
+үүл|cloud||üül
+манан|fog||manan
+будан|fog|budaŋ|budan
+тэнгэр|sky||tenger
+огторгуй|sky||ogtorgui
+салхи|wind||salkhi
+мөс|ice|mɵs|mös
+утаа|smoke||utaa
+гал|fire||gal
+үнс|ash||üns
+нурам|ash||nuram
+шатаах|to burn||shataakh
+зам|road||zam
+уул|mountain||uul
+улаан|red||ulaan
+ногоон|green||nogoon
+шар|yellow||shar
+цагаан|white||tsagaan
+хар|black||khar
+шөнө|night||shönö
+өдөр|day||ödör
+жил|year||jil
+дулаан|warm||dulaan
+хүйтэн|cold||khüiten
+шин|new||shin
+настай|old||nastai
+хуучин|old||khuuchin
+сайн|good||sain
+муу|bad||muu
+бохир|dirty||bokhir
+шулуун|straight||shuluun
+дугуй|round||duǧui
+хол|far||khol
+зүүн|left||züün
+ба|and||ba
+нэр|name||ner
+
+
+намар|autumn|namᵊr
+дэвтэр|notebook|deftᵊr
+бодол|thought|bodɮ
+зураг|picture|d͡zurg
+өнгө|color|ɵŋg
+амар|to rest|amᵊr
+арав|ten|arᵊw̜
+вагон|train|boɡoːn
+утас|phone|ʊtᵊs
+өндөг|egg|ɵndᵊk
+шинэ|new|ʃin
+мөнгө|money|mɵŋk
+гадна|outside|ɢadn
+амархан|easy|amᵊrxᵊn
+үндэстэн|nationality|undᵊstᵊn
+цэцэрлэ|kindergarten|t͡set͡sᵊrɮⁱk
+хадгалагдах|to be saved|xadɡᵊɮᵊɡdᵊx
+вино|wine|viˈnoː
+машин|car|maˈʃiːn
+
+ээмэр|earings|ˈeːmⁱr
+тийм|yes|tiːm
+лийр|pear|ɮiːr
+доор|under|toːr
+хоосон|empty|ˈxoːsᵊn
+уул|mountain|ʊːɮ
+дуу|song|tʊː
+өөр|different|oːr
+хөөрхөн|pretty|ˈxoːrxᵊn
+үүл|cloud|uːɮ
+сүү|milk|suː
+цас|snow|t͡sas
+цаас|paper|t͡saːs
+дэр|pillow|der
+дээр|on|deːr
+хол|far|xɔɮ
+хоол|food|xɔːɮ
+зах|market|t͡sax
+заах|teach|t͡saːx
+зун|summer|d͡zʊn
+зуун|hundred|d͡zʊːn
+хаашаа|where to|ˈxaːʃa
+туймээс|that's why|ˈtʰiːmes
+уугуул|indigenous|ˈʊːɡʊɮ
+өөрөө|myself|ˈɵːrɵ
+хүүгийн|son's|ˈxuuɡin
+гадаа|outside|ɣaˈtaː
+ширээ|table|ʃiˈreː
+цагдаа|police|t͡saɡˈtaː
+жолооч|driver|t͡ʃɔˈɮɔːt͡ʃʰ
+харандаа|pencil|xarnˈtaː
+заримдаа|sometimes|t͡sarmˈdaː
+Улаанбаатар|Ulaanbataar|ʊɮaːnpatr
+баярлалаа|thank you|bajrɮɮaː
+
+
+гадаад|foreign|ɢadaːt
+халбага|spoon|xaɮbaɡᵊ
+
+# wiktionary terms
+алт|gold|aɮtʰ [aɬt]
+алтан|golden|aɮtʰəŋ [ˈaɬtɐɴ]
+зээр|gazelle|t͡seːr
+монгол|a Mongolian|mɔŋ.ɢɔ̆ɮ
+ном|book, knowledge|nɔm
+төгрөг|tugrug|ˈtʰɵɡrəɡ
+хэл|tongue, language|xeɮ
+цус|blood|t͡sʊs
+шувуу|bird|ʃʊwʊ [ʃo̙ˈw̜o̙]
+
+#wiktionary terms with no pronunciation
+авах|to go, leave
+авга ах|mountain goat
+ард|people|arăd
+ачих|to load, burden|aʧĭ
+баяр жаргал|pleasure
+буга|deer, reindeer|buǧă
+дэлэнч|midge|delenʧĭ
+зөгий|bee|ʣцgi:
+кали|potassium|kȧ:lĭ
+морин зөгий|wasp
+цох|beetle|ʦoxŏ
+шумуул|mosquito|ʃumu:l
+явах|to go, leave|jaw
+янгир|mountain goat|jaŋgir
+
+
+сан|fund, store||saŋ
+чоно|wolf||ʧonŏ
+сандал|chair|sandᵊɮ|sandal
+сонин|news||sɔnin
+говь|Gobi||ǧȯwĭ
+
+
+# Colloquial Russian
+орос|Russian
+хот|town||xotŏ
+таны|your
+амиараа|individually
+хуаран|barracks||xua:raŋ
+ёотон|sugar lump||jo:toŋ
+ширээ юү?|is this a table?|ʃireː juː
+өнөөдөр|today|ɵnɵ:dɵr
+ес|nine|jɵsᵊ
+Cүхбаатар|Sükhbaatar|sʊxbaːtr
+пүүжин|rocket|pʊːd͡ʒiŋ
+фабрик|factory|fabrik
+
+
+
+#nomiin ger
+хэлье|let's say
+өгье|let's give
+морь|horse||mȯrĭ, mȯrꞌ
+хууль|law||xu̇:lĭ, xuil
+хорь|twenty||xorĭ, xȯrꞌ
+явъя|let's go
+уулзъя|let's meet
+бодъё|let's think
+ойлгосон|I understood|ɔilɔksᵊn
+баярлалаа|thank you|bajrɮɮa
+баяртай|goodbye|bajrtɛː
+автобyс|bus|avˈtɔbʊs
+машин|car|maˈʃin
+карт|card|kart
+кофе|coffee|kɔfi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+`
+
--- cyrillic/refs.js
+++ cyrillic/refs.js
@@ -1,5 +1,17 @@
+window.langSet = true
+
+
 reflist = {
 
+
+"bat": {
+"authors": "J Bat-Ireedui, Alan JK Sanders",
+"title":"Colloquial Mongolian",
+"date": "1999",
+"publisher": "Taylor & Francis Ltd",
+"lang": new Set(['mn']),
+"reviewed":false
+},
 
 "ul": {
 "authors": "Christopher Chapman",
@@ -7,6 +19,7 @@ reflist = {
 "date": "2020",
 "publisher": "Unicode Consortium",
 "url":"https://www.unicode.org/reports/tr14/",
+"lang": new Set(['ru']),
 "reviewed":true
 },
 
@@ -17,7 +30,17 @@ reflist = {
 "isbn":"0-19-507993-0",
 "pages":"408-412",
 "date": "1996",
+"lang": new Set(['ru']),
 "url":"#refs",
+},
+
+"gov": {
+"authors": "Gov.uk",
+"title":"Romanization of Mongolian Cyrillic, BGN/PCGN 1964 System",
+"url":"https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/816781/ROMANIZATION_OF_MONGOLIAN.pdf",
+"date": "2019",
+"lang": new Set(['mn']),
+"reviewed":true
 },
 
 "ics": {
@@ -25,7 +48,17 @@ reflist = {
 "title":"Ready-made Counter Styles",
 "url":"https://www.w3.org/TR/predefined-counter-styles/#arabic-styles",
 "date": "2017",
+"lang": new Set(['ru']),
 "reviewed":true
+},
+
+"ng": {
+"url":"https://www.mongolianlanguage.mn/free-lessons/mongolian-cyrillic-alphabet",
+"authors": "номын гэр",
+"title":"The Mongolian Alphabet",
+"retrieved":"Sep 2021",
+"lang": new Set(['mn']),
+"reviewed":false,
 },
 
 "s": {
@@ -33,7 +66,17 @@ reflist = {
 "title":"Glyph Variant for U+0306",
 "retr": "Apr 2021",
 "url":"http://scriptsource.org/cms/scripts/page.php?item_id=entry_detail&uid=k9ew9c6yam",
+"lang": new Set(['ru']),
 "reviewed":true
+},
+
+"d": {
+"authors": "Jan-Olof Svantesson, Anna Tsendina, Anastasia Karlsson, Vivan Franzen",
+"title":"The Phonology of Mongolian",
+"publisher":"Oxford University Press",
+"isbn":"9780199554270",
+"date": "2005",
+"lang": new Set(['mn']),
 },
 
 "u": {
@@ -44,12 +87,14 @@ reflist = {
 "pages":"311-315",
 "isbn":"978-1-936213-16-0.",
 "url":"https://www.unicode.org/versions/Unicode13.0.0/ch07.pdf",
+"lang": new Set(['ru','mn']),
 },
 
 "cldr": {
 "authors": "Unicode Consortium",
 "title":"CLDR: Locale Data Summary for Russian [ru]",
 "url":"http://localhost/scripts/cyrillic/#https://unicode-org.github.io/cldr-staging/charts/37/summary/ru.html",
+"lang": new Set(['ru']),
 },
 
 "ws": {
@@ -57,7 +102,24 @@ reflist = {
 "title":"Cyrillic script",
 "retr": "Apr 2021",
 "url":"http://en.wikipedia.org/wiki/Cyrillic_script",
+"lang": new Set(['ru']),
 "reviewed":true
+},
+
+"wc": {
+"authors": "Wikipedia",
+"title":"Mongolian Cyrillic alphabet",
+"retr": "May 2021",
+"url":"https://en.wikipedia.org/wiki/Mongolian_Cyrillic_alphabet",
+"lang": new Set(['mn']),
+},
+
+"wm": {
+"authors": "Wikipedia",
+"title":"Mongolian Language",
+"retr": "May 2021",
+"url":"https://en.wikipedia.org/wiki/Mongolian_language",
+"lang": new Set(['mn']),
 },
 
 "wq": {
@@ -65,6 +127,7 @@ reflist = {
 "title":"Quotation mark",
 "retr": "Apr 2021",
 "url":"https://en.wikipedia.org/wiki/Quotation_mark",
+"lang": new Set(['ru','mn']),
 },
 
 "wa": {
@@ -72,6 +135,7 @@ reflist = {
 "title":"Russian alphabet",
 "retr": "Apr 2021",
 "url":"https://en.wikipedia.org/wiki/Russian_alphabet",
+"lang": new Set(['ru']),
 },
 
 "wp": {
@@ -79,6 +143,7 @@ reflist = {
 "title":"Russian phonology",
 "retr": "Apr 2021",
 "url":"https://en.wikipedia.org/wiki/Russian_phonology",
+"lang": new Set(['ru']),
 },
 
 }
@@ -92,20 +157,27 @@ reflist = {
 
 terms = {
 
-"x1": {
+"ru1": {
 "url":"http://shtooka.net/overview.php?lang=rus&page=8",
 "authors": "?",
 "title":"Projet Shtooka",
 "notes": "Word lists in alphabetical order"
 },
 
-"x2": {
+"ru2": {
 "url":"https://ru.wiktionary.org/wiki/%D1%81%D1%83%D0%BF%D1%80%D1%83%D0%B3",
 "authors": "Wikipedia",
 "title":"Russian Wiktionary",
 "notes": "Change the word at the end of the URL to look up IPA.  Remove any accent."
 },
 
+
+"mn1": {
+"url":"https://www.mongolianlanguage.mn/free-lessons/mongolian-cyrillic-alphabet",
+"url":"https://www.mongolianlanguage.mn/free-lessons",
+"authors": "Nomiin Ger",
+"title":"Free lessons",
+},
 }
 
 
