8863dfb2f57d44ca5460158e9ae3a1f2db8ebc82
r12a <ishida@w3.org>
2021-09-15 06:54:50 +0100

    maka: Apply v27 (includes updating/adding all files)

---
+++ makasar/refs.js
@@ -0,0 +1,34 @@
+window.langSet = false
+
+
+
+reflist = {
+
+
+"u": {
+"authors": "Unicode Consortium",
+"title":"The Unicode Standard, Version 13.0",
+"sub":"Chapter 18.8: Indonesia & Oceania, Makasar",
+"pages":"706-707",
+"isbn":"978-1-936213-16-0.",
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch18.pdf",
+"reviewed": false
+},
+
+
+}
+
+
+
+
+
+
+terms = {
+
+
+}
+
+
+
+
+
