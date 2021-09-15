1636232a2e5a7f8ab8a678ae8ae2434b9dabb5e6
r12a <ishida@w3.org>
2021-09-15 06:59:18 +0100

    mong: Apply v27 (affects several files).  Add NNBSP to details.

--- mongolian/mn-examples.js
+++ mongolian/mn-examples.js
@@ -1,26 +1,66 @@
 autoExpandExamples.mn = `
-ᠡᠳᠦᠭᠡ|now
-ᠪᠡᠯᠡᠭ|gift
-ᠬᠠᠨ᠎ᠠ|wall of a tent
-ᠬᠠᠨᠠ|outer casing of a vein
-ᠬᠠᠭᠠᠨ|Kahn
-ᠭ᠋ᠠᠯ|fire
-ᠭᠠᠵᠠᠷ|place
-ᠭᠠᠵᠠᠷ ᠠ|to the country
-ᠭᠦᠮᠦᠨ|person
+ᠡᠳᠦᠭᠡ|now||eduge
+ᠪᠡᠯᠡᠭ|gift||beleg
+ᠬᠠᠨ᠎ᠠ|wall of a tent||xan·a
+ᠬᠠᠨᠠ|outer casing of a vein||xana
+ᠭ᠋ᠠᠯ|fire||g¹al
+ᠭᠠᠵᠠᠷ|place||gaǰar
+ᠭᠠᠵᠠᠷ ᠠ|to the country||gaǰar-a
+ᠭᠦᠮᠦᠨ|person||gumun
+ᠰᠠᠶ᠋ᠢᠨ/ᠰᠠᠶᠢᠨ|good||say¹in/sayin
+ᠰᠡᠮ|quietly|səm||sem
+ᠴᠢᠷᠢᠭ᠌|soldier||čirig²
+ᠵᠠᠷᠯᠢᠭ|order||ǰarlig
+ᠵᠣᠷ|good fortune, luck||ǰɔr
+ᠵᠣᠷᠢᠭᠲᠠᠢ|to be brave||ǰɔrigtai
+ᠵᠣᠷᠲᠠᠢ|lucky||ǰɔrtai
+ᠸᠢᠺᠢᠫᠧᠳᠢᠶᠠ|Wikipedia||viḳipədiya
+ᠼᠠᠭᠠᠨ|white||cagan
+ᠨᠠᠢ᠍ᠮᠠ|eight|naima||nai³ma
+ᠺᠢᠨᠣ᠋|theatre (archaic form)|kino|ḳinɔ¹
+
+
 ᠰᠠᠮ|comb|sam
-ᠰᠠᠶ᠋ᠢᠨ/ᠰᠠᠶᠢᠨ|good
-ᠰᠡᠮ|quietly|səm
-ᠴᠢᠷᠢᠭ᠌|soldier
-ᠵᠠᠷᠯᠢᠭ|order
-ᠵᠣᠷ|good fortune, luck
-ᠵᠣᠷᠢᠭᠲᠠᠢ|to be brave
-ᠵᠣᠷᠲᠠᠢ|lucky
-ᠸᠢᠺᠢᠫᠧᠳᠢᠶᠠ|Wikipedia
-ᠼᠠᠭᠠᠨ|white
-ᠨᠠᠢ᠍ᠮᠠ|eight|naima
-ᠺᠢᠨᠣ᠋|theatre (archaic form)|kino
-ᠪᠠᠷᠰ᠋|tigers|bars
+ᠬᠡᠯᠡ|language|xəl|xele
+ᠮᠤᠤᠷ|cat|mʊʊr
+ᠪᠠᠷᠰ|tiger|par|bars
+ᠬᠣᠩᠬᠤ|bell|xɔŋx|xɔŋxʊ
+ᠭᠠᠬᠠᠢ|pig|ɣaxai
+ᠭᠦᠷᠪᠡᠯ|lizard|gurpəl|gurbəl
+ᠲᠢᠩᠬᠢᠮ|living room|tiŋxim
+ᠤᠵᠡᠭᠦᠯᠬᠦ|to show|ud͡ʒuuləx|uǰəgulxu
+ᠱᠣᠷᠤ|pointed|ʃɔr|šɔrʊ
+ᠱᠥᠷᠤ|fence|ʃor
+ᠪᠣᠯᠵᠣᠮᠣᠷ|sparrow|pɔld͡ʒmɔr|bɔlǰɔmɔr
+ᠲᠥᠮᠦᠰᠦ|potato|toms|tomusu
+ᠠᠪᠤ|father|aap|abʊ
+ᠡᠵᠢ|mother|əːd͡ʒ|eǰi
+ᠮᠠᠯᠭᠠᠢ|hat|malɣai
+ᠮᠡᠯᠡᠬᠡᠢ|frog|məlxiː|melexei
+ᠪᠤᠭᠤ|deer|pʊɡ|bʊgʊ
+ᠮᠥᠭᠦ|mushroom|moog|mogu
+ᠢᠮᠠᠭ᠎ᠠ|goat|jamaː|imag·a
+ᠴᠡᠴᠡᠭ|flower|ʧəʧəɡ
+ᠴᠠᠭ|clock|ʧaɡ
+ᠬᠠᠭᠠᠨ|khan|xaan|xagan
+ᠲᠣᠭᠣᠰᠤ|dust|tɔɔs|tɔgɔsʊ
+ᠲᠣᠭᠣᠰ|peacock|tɔyɔs
+ᠨᠡᠷ᠎ᠠ|name|nərə
+ᠨᠠᠷᠠ|sun|nara
+ᠰᠡᠯᠡᠮᠡ|sword|sələm|seleme
+ᠡᠨᠳᠡ|here|əndə
+ᠠᠳᠠ|demon|ada
+ᠠᠲᠠ|gelding|ata
+ᠭᠡᠷᠡᠯᠲᠦᠨ᠎ᠡ|twinkle|ɡərəltənə|gereltun·e
+ᠬᠡᠷᠡᠯᠳᠤᠨ᠎ᠡ|fight|xərəldunə
+ᠲᠡᠦᠬᠡ|history|tuux|teuxe
+ᠤᠳᠬ᠎ᠠ|meaning|utaɡ|ʊdx·a
+ᠶᠠᠪᠤᠬᠤ|to go|japax|yabʊxʊ
+ᠶᠠᠪᠤᠭᠤᠯᠬᠤ|to send|japʊlax|yabʊgʊlxʊ
+ᠦᠵᠡᠬᠦ|to see|ud͡ʒəx|uǰexu
+ᠦᠵᠡᠭᠦᠯᠬᠦ|to show|ud͡ʒuːləx|uǰegulxu
+ᠱᠠᠭᠠᠵᠠᠭᠠᠢ|magpie|ʃd͡zgai|šaɣad͡ʒaɣai
+ᠮᠣᠩᠭᠣᠯ ᠬᠡᠯᠡ|Mongolian language|mɔŋɢɔ̆ɮ xeɮ
 
 
 
--- mongolian/refs.js
+++ mongolian/refs.js
@@ -2,13 +2,13 @@ reflist = {
 
 
 "b": {
-"url":"http://www.cjvlang.com/Writing/writmongol/mongolalpha.html",
-"authors": "Bathrobe's East Asian Writing Systems",
+"authors": "CJVlang",
+"notes": "Bathrobe's East Asian Writing Systems",
 "title":"Making Sense of the Traditional Mongolian Script",
+"url":"http://www.cjvlang.com/Writing/writmongol/mongolalpha.html",
 },
 
 "d": {
-"url":"#refs",
 "authors": "Peter T. Daniels and William Bright",
 "title":"The World's Writing Systems",
 "publisher":"Oxford University Press",
@@ -17,82 +17,67 @@ reflist = {
 },
 
 "e": {
-"url":"https://lists.w3.org/Archives/Public/public-i18n-mongolian/2015JulSep/att-0036/DS05_Mongolian_NNBSP_Connected_Suffixes.pdf",
 "authors": "Greg Eck",
 "title":"Mongolian NNBSP - Connected Suffixes",
+"url":"https://lists.w3.org/Archives/Public/public-i18n-mongolian/2015JulSep/att-0036/DS05_Mongolian_NNBSP_Connected_Suffixes.pdf",
 },
 
 "g1": {
-"url":"https://github.com/w3c/mlreq/issues/1",
 "authors": "GitHub (mlreq)",
 "title":"Mongolian ruby requirements",
+"url":"https://github.com/w3c/mlreq/issues/1",
 },
 
 "g5": {
-"url":"https://github.com/w3c/mlreq/issues/5",
 "authors": "GitHub (mlreq)",
 "title":"Non-Mongolian characters in vertical Mongolian text",
+"url":"https://github.com/w3c/mlreq/issues/5",
 },
 
 "g9": {
-"url":"https://github.com/w3c/mlreq/issues/9",
 "authors": "GitHub (mlreq)",
 "title":"Underline and NNBSP",
+"url":"https://github.com/w3c/mlreq/issues/9",
 },
 
 "g10": {
-"url":"https://github.com/w3c/mlreq/issues/10",
 "authors": "GitHub (mlreq)",
 "title":"Mongolian text decoration styles",
-},
-
-"n": {
-"url":"https://w3c.github.io/mlreq/",
-"authors": "Nasun-urt, Hu Qitu",
-"title":"Mongolian Layout Requirements",
-},
-
-"r": {
-"url":"https://www.unicode.org/L2/L2007/07023-bamum-report.pdf",
-"authors": "Riley, Charles",
-"title":"L2/07-023: Towards the Encoding of the Bamum Script in the UCS",
-"reviewed":true
+"url":"https://github.com/w3c/mlreq/issues/10",
 },
 
 "rmcs": {
-"url":"https://www.w3.org/TR/predefined-counter-styles/#arabic-styles",
 "authors": "Richard Ishida",
 "title":"Ready-made Counter Styles",
+"url":"https://www.w3.org/TR/predefined-counter-styles/#arabic-styles",
 "reviewed":true
 },
 
+"n": {
+"authors": "Nasun-urt, Hu Qitu",
+"title":"Mongolian Layout Requirements",
+"url":"https://w3c.github.io/mlreq/",
+},
 
 "u": {
-"url":"https://www.unicode.org/versions/Unicode13.0.0/ch13.pdf",
 "authors": "Unicode Consortium",
 "title":"The Unicode Standard, Version 13.0",
 "sub":"Chapter 13.5: South and Central Asia-II, Mongolian",
 "pages":"537-546",
 "isbn":"978-1-936213-16-0.",
-},
-
-"ul": {
-"url":"https://www.unicode.org/reports/tr14/",
-"authors": "Unicode Consortium",
-"title":"Unicode Line Breaking Algorithm (UAX#14)",
-"reviewed":true
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch13.pdf",
 },
 
 "wl": {
-"url":"https://en.wikipedia.org/wiki/Mongolian_language",
 "authors": "Wikipedia",
 "title":"Mongolian language",
+"url":"https://en.wikipedia.org/wiki/Mongolian_language",
 },
 
 "ws": {
-"url":"http://en.wikipedia.org/wiki/Mongolian_script",
 "authors": "Wikipedia",
 "title":"Mongolian script",
+"url":"http://en.wikipedia.org/wiki/Mongolian_script",
 },
 
 
@@ -100,4 +85,33 @@ reflist = {
 
 
 
-// <a class="fn">ws,#tones</a>
+
+
+
+terms = {
+
+"i": {
+"url":"http://trans.mglip.com/EnglishT2C.aspx",
+"authors": "Computer College of Inner Mongolia University,Inner Mongolia Mongolian Information Processing Laboratory",
+"title":"Conversion System between Traditional Mongolian and Cyrillic Mongolian",
+},
+
+
+"i2": {
+"url":"http://www.themongolist.com/language-resources/beginner-mongolian/lesson-one-introduction-to-mongolian-cyrillic.html",
+"authors": "The Mongolist",
+"title":"Lesson One: Introduction to Mongolian Cyrillic",
+"notes":"Nice set of example words in table."
+},
+
+
+"i3": {
+"url":"https://mn.wiktionary.org/wiki/%D0%B1%D0%B0%D0%B3%D1%88",
+"authors": "Wiktionary",
+},
+
+
+}
+
+
+
