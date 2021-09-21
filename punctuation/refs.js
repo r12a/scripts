8a7289c4cdc6611ab2e865fd50104cced269aa2c
r12a <ishida@w3.org>
2021-09-21 10:18:44 +0100

    punctuation v28

---
+++ punctuation/refs.js
@@ -0,0 +1,43 @@
+window.langSet = false
+
+
+reflist = {
+
+
+"b": {
+"authors": "Robert Bringhurst",
+"title":"The Elements of Typographic Style, 12th ed",
+"date": "",
+"publisher": "",
+"isbn":"978-0-88179-212-6",
+"reviewed":false
+},
+
+
+"s": {
+"authors": "SIL",
+"title":"The order of components in Khmer orthographical syllables",
+"url":"http://scripts.sil.org/cms/scripts/render_download.php?format=file&media_id=Mondulkiri-5.513-Ortho&filename=Mondulkiri-5.513-Ortho.pdf",
+},
+
+"u": {
+"authors": "Unicode Consortium",
+"title":"The Unicode Standard, Version 14.0",
+"sub":"Chapter 23.2: Special Areas and Format Characters, Layout Controls",
+"pages":"903-914",
+"isbn":"978-1-936213-16-0.",
+"url":"https://www.unicode.org/versions/Unicode14.0.0/ch23.pdf",
+"reviewed": false
+},
+
+
+}
+
+
+
+
+
+
+
+
+
