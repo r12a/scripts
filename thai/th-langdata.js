ae17cce51db301739905db5f489c39f24f16784d
r12a <ishida@w3.org>
2021-10-03 17:29:20 +0100

    th: v20 top-up.

---
+++ thai/th-langdata.js
@@ -0,0 +1,16 @@
+// this should always be updated when app-charuse/langs.js is updated
+
+var langs = {
+
+"th": { name:"Thai (Siamese)", local:"ภาษาไทย", silcode:"tha", 
+source:"85b37d327a5e470abcc40234d6cbfdfa388d6566,cldr_th,udhr_tha", 
+region:"seasia", countries:"Thailand", script:"thai", speakers:"44000000", 
+letter:"ฯๆกขฃคฅฆงจฉชซฌญฎฏฐฑฒณดตถทธนบปผฝพฟภมยรฤลฦวศษสหฬอฮะาๅำเแโใไ", mark:"์็่้๊๋ัิีึืุู", markaux:"ํ", number:"๐๑๒๓๔๕๖๗๘๙", punctuation:"‐–—‘’“”…๏๚๛", symbol:"฿", other:"​⁠", aux:"′″",
+orth:`Thai. &nbsp; <a href="../scripts/thai/" target="_blank">Details</a>.`,
+type:"abugida", cs:"no", gpos:"yes", gsub:"yes", cursive:"no", wordsep:"no/zwsp", baseline:"mid", wrap:"word", hyphenation:"no",  direction:"ltr",  justification:"sp ig", 
+vowels:"inh:2 vs:8 vsl:12 base:อ/O venc:5 comp:37 voc:2", 
+linked:"thai", fonts:"/pickers/thai/", 
+},
+
+
+}
