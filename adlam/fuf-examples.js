d76fa14d1c21c982402f55b1ad0cbab9e53b27a1
r12a <ishida@w3.org>
2021-10-05 18:33:45 +0100

    fuf: v29 further topup.

--- adlam/fuf-examples.js
+++ adlam/fuf-examples.js
@@ -8,233 +8,233 @@
 
 
 autoExpandExamples.fuf = `
-𞤉𞤲𞤫𞥅𞤲|we|ɛnɛːn (Ɛneen)
-𞤞𞤢𞤲𞤼𞤢𞤥𞤢|Gbantama (name of a town)|ɡ͡bantama (Gbantama)
-𞤟𞤭𞤲𞤢𞥄|Zina (name)|zinaː (Zinaa)
-𞤠𞤫𞤤𞤫|Kpelle (ethnic group)|k͡pɛlɛ (Kpele)
+𞤉𞤲𞤫𞥅𞤲|we|ɛnɛːn|Ɛneen
+𞤞𞤢𞤲𞤼𞤢𞤥𞤢|Gbantama (name of a town)|ɡ͡bantama|Gbantama
+𞤟𞤭𞤲𞤢𞥄|Zina (name)|zinaː|Zinaa
+𞤠𞤫𞤤𞤫|Kpelle (ethnic group)|k͡pɛlɛ|Kpele
 𞤢⹁‏ 𞤢𞤣𞤢|thou|a,‏ ada
-𞤢𞤥𞤣𞤫|dance|amdɛ (amde)
-𞤢𞤧𞤢𞤥𞤢𞥄𞤲|sky|asamaːn (asamaan)
-𞤢𞤧𞤣𞤫|dig|asdɛ (asde)
-𞤢𞤪𞤣𞤫|come|ardɛ (arde)
-𞤢𞤲𞤣𞤵𞤣𞤫|know|andudɛ (andude)
+𞤢𞤥𞤣𞤫|dance|amdɛ|amde
+𞤢𞤧𞤢𞤥𞤢𞥄𞤲|sky|asamaːn|asamaan
+𞤢𞤧𞤣𞤫|dig|asdɛ|asde
+𞤢𞤪𞤣𞤫|come|ardɛ|arde
+𞤢𞤲𞤣𞤵𞤣𞤫|know|andudɛ|andude
 𞤢𞤸𞤢𞤣𞤭|agreement|ahadi
-𞤢𞥄𞤬𞤭𞤴𞤢|wellbeing|aːfija (aafiya)
-𞤣𞤢𞤪𞤢𞥄𞤣𞤫|stand|daraːdɛ (daraade)
-𞤣𞤢𞤲𞤫𞥅𞤶𞤮|white|danɛːd͡ʒɔ (daneejo)
+𞤢𞥄𞤬𞤭𞤴𞤢|wellbeing|aːfija|aafiya
+𞤣𞤢𞤪𞤢𞥄𞤣𞤫|stand|daraːdɛ|daraade
+𞤣𞤢𞤲𞤫𞥅𞤶𞤮|white|danɛːd͡ʒɔ|daneejo
 𞤣𞤢𞤲𞤳𞤭|bed|danki
-𞤣𞤢𞥄𞤲𞤢𞥄𞤣𞤫|sleep|daːnaːdɛ (daanaade)
-𞤣𞤢𞥄𞤲𞥋𞤣𞤫|neck|daːⁿdɛ (daande)
-𞤣𞤫𞤥𞤲𞥋𞤺𞤢𞤤|tongue|dɛmᵑɡal (demngal)
-𞤣𞤫𞤦𞥆𞤮|woman|dɛbbɔ (debbo)
+𞤣𞤢𞥄𞤲𞤢𞥄𞤣𞤫|sleep|daːnaːdɛ|daanaade
+𞤣𞤢𞥄𞤲𞥋𞤣𞤫|neck|daːⁿdɛ|daande
+𞤣𞤫𞤥𞤲𞥋𞤺𞤢𞤤|tongue|dɛmᵑɡal|demngal
+𞤣𞤫𞤦𞥆𞤮|woman|dɛbbɔ|debbo
 𞤣𞤭𞤣𞤭|two|didi
-𞤣𞤭𞤱𞤢𞥄𞤣𞤫|fly (v.)|diwaːdɛ (diwaade)
-𞤣𞤭𞥅𞤣𞤮𞤤|characteristic|diːdɔl (diidol)
-𞤣𞤮𞥅|here|dɔː (doo)
-𞤤𞤢𞤲𞥋𞤣𞤢𞤥|salt|laⁿdam (landam)
-𞤤𞤢𞥄𞤩𞤢𞤤|sincerity|laːɓal (laaɓal)
-𞤤𞤢𞥄𞤱𞤮𞤤|road|laːwɔl (laawol)
-𞤤𞤢𞥄𞤷𞤭|tail|laːt͡ʃi (laaci)
-𞤤𞤢𞥄𞤿𞤢𞤪𞤢|hereafter|laːxara (laaxara)
-𞤤𞤫𞤤𞤢𞥄𞤣𞤫|lie|lɛlaːdɛ (lelaade)
-𞤤𞤫𞤨𞥆𞤭|wet|lɛppi (leppi)
-𞤤𞤫𞤱𞤪𞤵|moon|lɛwru (lewru)
-𞤤𞤫𞤳𞥆𞤭|tree|lɛkki (lekki)
-𞤤𞤫𞤴𞤣𞤭|earth|lɛjdi (leydi)
-𞤤𞤫𞤺𞥆𞤢𞤤|stick|lɛɡɡal (leggal)
-𞤤𞤭𞤥𞤣𞤫|count|limdɛ (limde)
-𞤤𞤭𞥅𞤲𞥋𞤺𞤵|fish|liːᵑɡu (liingu)
-𞤤𞤮𞥅𞤼𞤣𞤫|wash|lɔːtdɛ (lootde)
-𞤤𞤵𞤥𞤦𞤢𞥄𞤣𞤫|swim|lumbaːdɛ (lumbaade)
-𞤥𞤢𞤱𞤣𞤮|big|mawdɔ (mawdo)
-𞤥𞤢𞥄𞤪𞤮|rice|maːrɔ (maaro)
-𞤥𞤢𞥄𞤲𞥋𞤣𞤫|mark|maːⁿdɛ (maandɛ)
-𞤥𞤢𞥄𞤴𞤣𞤫|die|maːjdɛ (maayde)
-𞤥𞤢𞥄𞤴𞤮|river|maːjɔ (maayo)
+𞤣𞤭𞤱𞤢𞥄𞤣𞤫|to fly|diwaːdɛ|diwaade
+𞤣𞤭𞥅𞤣𞤮𞤤|characteristic|diːdɔl|diidol
+𞤣𞤮𞥅|here|dɔː|doo
+𞤤𞤢𞤲𞥋𞤣𞤢𞤥|salt|laⁿdam|landam
+𞤤𞤢𞥄𞤩𞤢𞤤|sincerity|laːɓal|laaɓal
+𞤤𞤢𞥄𞤱𞤮𞤤|road|laːwɔl|laawol
+𞤤𞤢𞥄𞤷𞤭|tail|laːt͡ʃi|laaci
+𞤤𞤢𞥄𞤿𞤢𞤪𞤢|hereafter|laːxara|laaxara
+𞤤𞤫𞤤𞤢𞥄𞤣𞤫|lie|lɛlaːdɛ|lelaade
+𞤤𞤫𞤨𞥆𞤭|wet|lɛppi|leppi
+𞤤𞤫𞤱𞤪𞤵|moon|lɛwru|lewru
+𞤤𞤫𞤳𞥆𞤭|tree|lɛkki|lekki
+𞤤𞤫𞤴𞤣𞤭|earth|lɛjdi|leydi
+𞤤𞤫𞤺𞥆𞤢𞤤|stick|lɛɡɡal|leggal
+𞤤𞤭𞤥𞤣𞤫|count|limdɛ|limde
+𞤤𞤭𞥅𞤲𞥋𞤺𞤵|fish|liːᵑɡu|liingu
+𞤤𞤮𞥅𞤼𞤣𞤫|wash|lɔːtdɛ|lootde
+𞤤𞤵𞤥𞤦𞤢𞥄𞤣𞤫|swim|lumbaːdɛ|lumbaade
+𞤥𞤢𞤱𞤣𞤮|big|mawdɔ|mawdo
+𞤥𞤢𞥄𞤪𞤮|rice|maːrɔ|maaro
+𞤥𞤢𞥄𞤲𞥋𞤣𞤫|mark|maːⁿdɛ|maandɛ
+𞤥𞤢𞥄𞤴𞤣𞤫|die|maːjdɛ|maayde
+𞤥𞤢𞥄𞤴𞤮|river|maːjɔ|maayo
 𞤥𞤭⹁‏ 𞤲'𞤦𞤭𞤣𞤢|I|mi,‏ n'bida
-𞤥𞤭𞤲⹁‏ 𞤲𞥋𞤦𞤭𞤣𞤫𞤲⹁‏ 𞤫𞤲⹁‏ 𞤫𞤣𞤫𞤲|we|min,‏ ᵐbidɛn,‏ ɛn,‏ ɛdɛn (min,‏ mbiden,‏ en,‏ eden)
-𞤥𞤮𞤰𞥆𞤭𞥅|good|mɔʔʔʲiː (mɔƴƴiː)
-𞤥𞤵𞤷𞥆𞤵𞤣𞤫|suck|mutt͡ʃudɛ (muccude)
+𞤥𞤭𞤲⹁‏ 𞤲𞥋𞤦𞤭𞤣𞤫𞤲⹁‏ 𞤫𞤲⹁‏ 𞤫𞤣𞤫𞤲|we|min,‏ ᵐbidɛn,‏ ɛn,‏ ɛdɛn|min,‏ mbiden,‏ en,‏ eden
+𞤥𞤮𞤰𞥆𞤭𞥅|good|mɔʔʔʲiː|mɔƴƴiː
+𞤥𞤵𞤷𞥆𞤵𞤣𞤫|suck|mutt͡ʃudɛ|muccude
 𞤦𞤢𞤣𞥆𞤭|near|baddi
-𞤦𞤢𞤤𞤫𞥅𞤶𞤮|black|balɛːd͡ʒɔ (baleejo)
-𞤦𞤢𞥄𞤦𞤢|father|baːba (baaba)
-𞤦𞤢𞥄𞤬𞤢𞤤|door|baːfal (baafal)
-𞤦𞤫⹁‏ 𞤫𞤦𞤫|they|bɛ,‏ ɛbɛ (be,‏ ebe)
-𞤦𞤫𞤪𞤲𞥋𞤣𞤫|heart|bɛrⁿdɛ (bernde)
-𞤦𞤫𞤷𞥆𞤫|back|bɛtt͡ʃɛ (becce)
-𞤦𞤭𞤦𞥆𞤫 𞤤𞤫𞤣𞥆𞤫|fruit|bibbɛ lɛddɛ (bibbe ledde)
-𞤦𞤮𞤣𞤫𞥅𞤶𞤮|red|bɔdɛːd͡ʒɔ (bodeejo)
-𞤦𞤮𞤲𞥋𞤣𞤮|bad|bɔⁿdɔ (bondo)
-𞤦𞤮𞤷𞥆𞤮𞥅𞤲𞥋𞤣𞤫|egg|bɔtt͡ʃɔːⁿdɛ (boccoonde)
-𞤦𞤮𞤺𞥆𞤮𞤤|rope|bɔɡɡɔl (boggol)
-𞤦𞤵𞥅𞤦𞤮𞤤⹁‏ 𞤶𞤢𞤲𞤺𞤮𞤤|cold|buːbɔl,‏ d͡ʒanɡɔl (buubol,‏ jangol)
-𞤦𞤵𞥅𞤼𞤫𞥅𞤣𞤫|swell|buːtɛːdɛ (buuteede)
+𞤦𞤢𞤤𞤫𞥅𞤶𞤮|black|balɛːd͡ʒɔ|baleejo
+𞤦𞤢𞥄𞤦𞤢|father|baːba|baaba
+𞤦𞤢𞥄𞤬𞤢𞤤|door|baːfal|baafal
+𞤦𞤫⹁‏ 𞤫𞤦𞤫|they|bɛ,‏ ɛbɛ|be,‏ ebe
+𞤦𞤫𞤪𞤲𞥋𞤣𞤫|heart|bɛrⁿdɛ|bernde
+𞤦𞤫𞤷𞥆𞤫|back|bɛtt͡ʃɛ|becce
+𞤦𞤭𞤦𞥆𞤫 𞤤𞤫𞤣𞥆𞤫|fruit|bibbɛ lɛddɛ|bibbe ledde
+𞤦𞤮𞤣𞤫𞥅𞤶𞤮|red|bɔdɛːd͡ʒɔ|bodeejo
+𞤦𞤮𞤲𞥋𞤣𞤮|bad|bɔⁿdɔ|bondo
+𞤦𞤮𞤷𞥆𞤮𞥅𞤲𞥋𞤣𞤫|egg|bɔtt͡ʃɔːⁿdɛ|boccoonde
+𞤦𞤮𞤺𞥆𞤮𞤤|rope|bɔɡɡɔl|boggol
+𞤦𞤵𞥅𞤦𞤮𞤤⹁‏ 𞤶𞤢𞤲𞤺𞤮𞤤|cold|buːbɔl,‏ d͡ʒanɡɔl|buubol,‏ jangol
+𞤦𞤵𞥅𞤼𞤫𞥅𞤣𞤫|swell|buːtɛːdɛ|buuteede
 𞤧𞤢𞤦𞤵|because|sabu
 𞤧𞤢𞤳𞤫|warehouse
 𞤧𞤢𞤳𞥆𞤫|leather worker
-𞤧𞤢𞥄𞥋𞤭|hour|saːʔi (saa’i)
-𞤧𞤫𞤺𞤫𞤲𞤫|fingernail|sɛɡɛnɛ (segene)
-𞤧𞤫𞥅𞤣𞤢|few|sɛːda (seeda)
-𞤧𞤭𞤳𞥆𞤵𞤣𞤫⹁‏ 𞤥𞤭𞤶𞥆𞤢𞥄𞤣𞤫|think|sikkudɛ,‏ midd͡ʒaːdɛ (sikkude,‏ mijjaade)
-𞤧𞤮 (𞤼𞤢𞤱𞤭𞥅)|if|sɔ (tawiː) (so (tawii))
-𞤧𞤮𞤲𞤣𞤵|bird|sɔndu (sondu)
-𞤧𞤵𞤥𞤣𞤫|burn|sumdɛ (sumde)
-𞤧𞤵𞤳𞤵𞤲𞥋𞤣𞤵|hair|sukuⁿdu (sukundu)
+𞤧𞤢𞥄𞥋𞤭|hour|saːʔi|saa’i
+𞤧𞤫𞤺𞤫𞤲𞤫|fingernail|sɛɡɛnɛ|segene
+𞤧𞤫𞥅𞤣𞤢|few|sɛːda|seeda
+𞤧𞤭𞤳𞥆𞤵𞤣𞤫⹁‏ 𞤥𞤭𞤶𞥆𞤢𞥄𞤣𞤫|think|sikkudɛ,‏ midd͡ʒaːdɛ|sikkude,‏ mijjaade
+𞤧𞤮 (𞤼𞤢𞤱𞤭𞥅)|if|sɔ (tawiː)|so (tawii)
+𞤧𞤮𞤲𞤣𞤵|bird|sɔndu|sondu
+𞤧𞤵𞤥𞤣𞤫|burn|sumdɛ|sumde
+𞤧𞤵𞤳𞤵𞤲𞥋𞤣𞤵|hair|sukuⁿdu|sukundu
 𞤨𞤢𞤯𞤢𞤤|shoe|paɗal
 𞤨𞤵𞤲𞤣𞤭|dust|pundi
-𞤨𞤵𞤷𞥆𞤵|horse|putt͡ʃu (puccu)
+𞤨𞤵𞤷𞥆𞤵|horse|putt͡ʃu|puccu
 𞤩𞤭𞤪𞤢|milk|ɓira
-𞤩𞤮𞤳𞥆𞤭|baobab|ɓɔkki (ɓokki)
+𞤩𞤮𞤳𞥆𞤭|baobab|ɓɔkki|ɓokki
 𞤪𞤢𞤦𞥆𞤭𞤣𞤭|short|rabbidi
-𞤪𞤢𞤱𞤢𞥄𞤲𞥋𞤣𞤵|dog|rawaːⁿdu (rawaandu)
-𞤪𞤫𞤥𞤣𞤫|grow|rɛmdɛ (remde)
-𞤪𞤫𞥅𞤣𞤵|belly|rɛːdu (reedu)
-𞤪𞤮𞤳𞥆𞤵𞤣𞤫|give|rɔkkudɛ (rokkude)
-𞤫|with|ɛ (e)
-𞤫𞤩𞥆𞤮|measurement|ɛɓɓɔ (eɓɓo)
-𞤫𞤲⹁‏ 𞤫𞤣𞤫𞤲⹁‏ 𞤥𞤭𞤲⹁‏ 𞤥𞤭𞤣𞤫𞤲|we|ɛn,‏ ɛdɛn,‏ min,‏ midɛn (en,‏ eden,‏ min,‏ miden)
-𞤫𞤲𞤣𞤵|breast|ɛndu (endu)
+𞤪𞤢𞤱𞤢𞥄𞤲𞥋𞤣𞤵|dog|rawaːⁿdu|rawaandu
+𞤪𞤫𞤥𞤣𞤫|grow|rɛmdɛ|remde
+𞤪𞤫𞥅𞤣𞤵|belly|rɛːdu|reedu
+𞤪𞤮𞤳𞥆𞤵𞤣𞤫|give|rɔkkudɛ|rokkude
+𞤫|with|ɛ|e
+𞤫𞤩𞥆𞤮|measurement|ɛɓɓɔ|eɓɓo
+𞤫𞤲⹁‏ 𞤫𞤣𞤫𞤲⹁‏ 𞤥𞤭𞤲⹁‏ 𞤥𞤭𞤣𞤫𞤲|we|ɛn,‏ ɛdɛn,‏ min,‏ midɛn|en,‏ eden,‏ min,‏ miden
+𞤫𞤲𞤣𞤵|breast|ɛndu|endu
 𞤬𞤢𞤥𞤣𞤭|small|famdi
-𞤬𞤢𞥄𞤣𞤭|narrow|faːdi (faadi)
-𞤬𞤢𞥄𞤲𞥋𞤣𞤵|gourd|faːⁿdu (faandu)
-𞤬𞤫𞤷𞥆𞤵𞤣𞤫|split|fɛtt͡ʃudɛ (feccude)
-𞤬𞤭𞤲𞤳𞤢𞥄𞤪𞤭|gun|finkaːri (finkaari)
-𞤬𞤭𞤴𞤣𞤫|hit|fijdɛ (fiyde)
-𞤬𞤭𞤶𞤣𞤫|play|fid͡ʒdɛ (fijde)
-𞤬𞤮𞤣𞥆𞤫𞤪𞤫|seed|fɔddɛrɛ (foddere)
-𞤬𞤮𞤨𞥆𞤵𞤣𞤫|wipe|fɔppudɛ (foppude)
-𞤬𞤮𞤬|all|fɔf (fof)
-𞤬𞤮𞥅𞤣𞥆𞤫|pull|fɔːddɛ (foodde)
-𞤬𞤮𞥅𞤬𞤣𞤫|breathe|fɔːfdɛ (foofde)
+𞤬𞤢𞥄𞤣𞤭|narrow|faːdi|faadi
+𞤬𞤢𞥄𞤲𞥋𞤣𞤵|gourd|faːⁿdu|faandu
+𞤬𞤫𞤷𞥆𞤵𞤣𞤫|split|fɛtt͡ʃudɛ|feccude
+𞤬𞤭𞤲𞤳𞤢𞥄𞤪𞤭|gun|finkaːri|finkaari
+𞤬𞤭𞤴𞤣𞤫|hit|fijdɛ|fiyde
+𞤬𞤭𞤶𞤣𞤫|play|fid͡ʒdɛ|fijde
+𞤬𞤮𞤣𞥆𞤫𞤪𞤫|seed|fɔddɛrɛ|foddere
+𞤬𞤮𞤨𞥆𞤵𞤣𞤫|wipe|fɔppudɛ|foppude
+𞤬𞤮𞤬|all|fɔf|fof
+𞤬𞤮𞥅𞤣𞥆𞤫|pull|fɔːddɛ|foodde
+𞤬𞤮𞥅𞤬𞤣𞤫|breathe|fɔːfdɛ|foofde
 𞤭𞤤𞤢𞤥|floodwater|ilam
-𞤭𞤲𞤣𞤫|name|indɛ (inde)
+𞤭𞤲𞤣𞤫|name|indɛ|inde
 𞤭𞤼𞥆𞤭𞤪𞤭|dimension|ittiri
-𞤮⹁‏ 𞤮𞤥𞤮|he|ɔ,‏ ɔmɔ (o,‏ omo)
-𞤮𞤲⹁‏ 𞤮𞤣𞤮𞤲|you|ɔn,‏ ɔdɔn (on,‏ odon)
-𞤮𞤼𞤮|car|ɔtɔ (oto)
-𞤮𞥅𞤤𞤮|yellow|ɔːlɔ (oolo)
-𞤮𞥅𞤤𞤮𞤤|yellow|ɔːlɔl (oolol)
-𞤯𞤢𞥄𞤲𞤢𞥄𞤣𞤫|sleep|ɗaːnaːdɛ (ɗaanaade)
-𞤯𞤢𞥄𞤼𞤭|soft|ɗaːti (ɗaati)
-𞤰𞤫𞥅𞤱𞤣𞤫|look|ʔʲɛːwdɛ (ƴeewde)
-𞤰𞤭𞥅𞤰𞤢𞤲|blood|ʔʲiːʔʲan (ƴiiƴan)
-𞤱𞤢𞤤𞤭𞤴𞤵|friend|waliju (waliyu)
-𞤱𞤢𞤪𞤣𞤫|kill|wardɛ (warde)
-𞤱𞤢𞥄𞤧𞤣𞤫|lose|waːsdɛ (waasde)
-𞤱𞤭𞤴𞤣𞤫|say|wijdɛ (wiyde)
-𞤱𞤮𞤣𞥆𞤭|far|wɔddi (woddi)
-𞤱𞤮𞤲𞤢𞥄|not|wɔnaː (wonaa)
+𞤮⹁‏ 𞤮𞤥𞤮|he|ɔ,‏ ɔmɔ|o,‏ omo
+𞤮𞤲⹁‏ 𞤮𞤣𞤮𞤲|you|ɔn,‏ ɔdɔn|on,‏ odon
+𞤮𞤼𞤮|car|ɔtɔ|oto
+𞤮𞥅𞤤𞤮|yellow|ɔːlɔ|oolo
+𞤮𞥅𞤤𞤮𞤤|yellow|ɔːlɔl|oolol
+𞤯𞤢𞥄𞤲𞤢𞥄𞤣𞤫|sleep|ɗaːnaːdɛ|ɗaanaade
+𞤯𞤢𞥄𞤼𞤭|soft|ɗaːti|ɗaati
+𞤰𞤫𞥅𞤱𞤣𞤫|look|ʔʲɛːwdɛ|ƴeewde
+𞤰𞤭𞥅𞤰𞤢𞤲|blood|ʔʲiːʔʲan|ƴiiƴan
+𞤱𞤢𞤤𞤭𞤴𞤵|friend|waliju|waliyu
+𞤱𞤢𞤪𞤣𞤫|kill|wardɛ|warde
+𞤱𞤢𞥄𞤧𞤣𞤫|lose|waːsdɛ|waasde
+𞤱𞤭𞤴𞤣𞤫|say|wijdɛ|wiyde
+𞤱𞤮𞤣𞥆𞤭|far|wɔddi|woddi
+𞤱𞤮𞤲𞤢𞥄|not|wɔnaː|wonaa
 𞤱𞤵𞤤𞤭|warm|wuli
-𞤱𞤵𞥅𞤪𞤣𞤫|live|wuːrdɛ (wuurde)
-𞤲𞤢𞤲𞤮|left|nanɔ (nano)
-𞤲𞤢𞤲𞥋𞤣𞤫|hear|naⁿdɛ (nande)
-𞤲𞤢𞤲𞥋𞤦𞤢𞤪𞤢|trouble|naᵐbara (nambara)
-𞤲𞤢𞤴𞤫𞥅𞤶𞤮|old|najɛːd͡ʒɔ (nayeejo)
-𞤲𞤢𞤴𞤭|four|naji (nayi)
+𞤱𞤵𞥅𞤪𞤣𞤫|live|wuːrdɛ|wuurde
+𞤲𞤢𞤲𞤮|left|nanɔ|nano
+𞤲𞤢𞤲𞥋𞤣𞤫|hear|naⁿdɛ|nande
+𞤲𞤢𞤲𞥋𞤦𞤢𞤪𞤢|trouble|naᵐbara|nambara
+𞤲𞤢𞤴𞤫𞥅𞤶𞤮|old|najɛːd͡ʒɔ|nayeejo
+𞤲𞤢𞤴𞤭|four|naji|nayi
 𞤲𞤢𞤼𞤢𞤤|photo|natal
-𞤲𞤢𞥄𞤲𞥋𞤺𞤫|sun|naːᵑɡɛ (naange)
-𞤲𞤫𞤣𞥆𞤮|person|nɛddɔ (neddo)
-𞤲𞤮𞤬𞤪𞤵|ear|nɔfru (nofru)
-𞤲𞤴𞤢𞥄𞤳𞤭|bee|njaːki (nyaaki)
-𞤲𞥋𞤣𞤢𞤲𞤳𞤭|bed|ⁿdanki (ndanki)
-𞤲𞥋𞤣𞤫𞤪|in|ⁿdɛr (nder)
-𞤲𞥋𞤣𞤭𞤴𞤢𞤥|water|ⁿdijam (ndiyam)
-𞤲𞥋𞤶𞤢𞤲𞥋𞤣𞤭|iron|ᶮd͡ʒaⁿdi (njandi)
-𞤲𞥋𞤺𞤢𞤪𞤢|indigo|ᵑɡara (ngara)
-𞤲𞥋𞤺𞤵𞤪𞤫|skin|ᵑɡurɛ (ngure)
-𞤳𞤢𞤷𞥆𞤵|lime|katt͡ʃu (kaccu)
-𞤳𞤫𞤧𞤮|new|kɛsɔ (keso)
-𞤳𞤮𞤧𞤢𞤥|milk|kɔsam (kosam)
-𞤳𞤮𞤴𞤲𞥋𞤺𞤢𞤤|foot|kɔjᵑɡal (koyngal)
-𞤳𞤮𞤴𞤲𞥋𞤺𞤢𞤤|leg|kɔjᵑɡal (koyngal)
-𞤴𞤢𞤪𞤣𞤫|drink|jardɛ (yarde)
-𞤴𞤢𞤲𞥋𞤣𞤫|fall|jaⁿdɛ (yande)
-𞤴𞤢𞤸𞤣𞤫|walk|jahdɛ (yahde)
-𞤴𞤢𞥄𞤧𞤭|atmosphere|jaːsi (yaasi)
-𞤴𞤢𞥄𞤶𞤭|wide|jaːd͡ʒi (yaaji)
-𞤴𞤭𞤥𞤣𞤫|sing|jimdɛ (yimde)
-𞤴𞤭𞤴𞤣𞤫|see|jijdɛ (yiyde)
-𞤴𞤭𞤼𞤫𞤪𞤫|eye|jitɛrɛ (yitere)
-𞤴𞤭𞥅𞤴𞤢𞤤|bone|jiːjal (yiiyal)
-𞤴𞤭𞥅𞤴𞤢𞤥|blood|jiːjam (yiiyam)
-𞤴𞤮𞥅𞤪𞤭|dry|jɔːri (yoori)
-𞤴𞤵𞤥𞥆𞤢⹁‏ 𞤲𞤫𞥅𞤲𞤫|mother|jumma,‏ nɛːnɛ (yumma,‏ neene)
-𞤵𞤤𞥆𞤵𞤲𞥋𞤣𞤵|cat|ulluⁿdu (ullundu)
-𞤵𞥅𞤪𞤣𞤫|smell|uːrdɛ (uurde)
-𞤵𞥅𞤴𞤢𞤲𞥋𞤣𞤫|acceleration|uːjaⁿdɛ (uuyande)
-𞤶𞤢𞤤𞤣𞤫|laugh|d͡ʒaldɛ (jalde)
-𞤶𞤢𞤥|peace|d͡ʒam (jam)
-𞤶𞤢𞤥𞥆𞤢|night|d͡ʒamma (jamma)
-𞤶𞤢𞤳𞤢|so|d͡ʒaka (jaka)
-𞤶𞤢𞤺𞥆𞤵𞤣𞤫|hold|d͡ʒaɡɡudɛ (jaggude)
-𞤶𞤮𞤥 𞤧𞤵𞥅𞤣𞤵|wife|d͡ʒɔm suːdu (jom suudu)
-𞤶𞤮𞤥 𞤺𞤢𞤤𞥆𞤫|husband|d͡ʒɔm ɡallɛ (jom galle)
-𞤶𞤮𞤴𞤭|five|d͡ʒɔji (joyi)
-𞤶𞤮𞥅𞤣𞤢𞥄𞤣𞤫|sit|d͡ʒɔːdaːdɛ (joodaade)
-𞤶𞤵𞤲𞤺𞤮|hand|d͡ʒunɡɔ (jungo)
-𞤶𞤵𞥅𞤼𞤭|long|d͡ʒuːti (juuti)
-𞤷𞤫𞤩𞤫|bark|t͡ʃɛɓɛ (ceɓe)
-𞤷𞤵𞤥𞤵|fire|t͡ʃumu (cumu)
-𞤷𞤵𞤪𞤳𞤭|smoke|t͡ʃurki (curki)
-𞤷𞤵𞤳𞤢𞤤𞤫𞤤|child|t͡ʃukalɛl (cukalel)
-𞤷𞤵𞥅𞤪𞤳𞤭|smoke|t͡ʃuːrki (cuurki)
+𞤲𞤢𞥄𞤲𞥋𞤺𞤫|sun|naːᵑɡɛ|naange
+𞤲𞤫𞤣𞥆𞤮|person|nɛddɔ|neddo
+𞤲𞤮𞤬𞤪𞤵|ear|nɔfru|nofru
+𞤲𞤴𞤢𞥄𞤳𞤭|bee|njaːki|nyaaki
+𞤲𞥋𞤣𞤢𞤲𞤳𞤭|bed|ⁿdanki|ndanki
+𞤲𞥋𞤣𞤫𞤪|in|ⁿdɛr|nder
+𞤲𞥋𞤣𞤭𞤴𞤢𞤥|water|ⁿdijam|ndiyam
+𞤲𞥋𞤶𞤢𞤲𞥋𞤣𞤭|iron|ᶮd͡ʒaⁿdi|njandi
+𞤲𞥋𞤺𞤢𞤪𞤢|indigo|ᵑɡara|ngara
+𞤲𞥋𞤺𞤵𞤪𞤫|skin|ᵑɡurɛ|ngure
+𞤳𞤢𞤷𞥆𞤵|lime|katt͡ʃu|kaccu
+𞤳𞤫𞤧𞤮|new|kɛsɔ|keso
+𞤳𞤮𞤧𞤢𞤥|milk|kɔsam|kosam
+𞤳𞤮𞤴𞤲𞥋𞤺𞤢𞤤|foot|kɔjᵑɡal|koyngal
+𞤳𞤮𞤴𞤲𞥋𞤺𞤢𞤤|leg|kɔjᵑɡal|koyngal
+𞤴𞤢𞤪𞤣𞤫|drink|jardɛ|yarde
+𞤴𞤢𞤲𞥋𞤣𞤫|fall|jaⁿdɛ|yande
+𞤴𞤢𞤸𞤣𞤫|walk|jahdɛ|yahde
+𞤴𞤢𞥄𞤧𞤭|atmosphere|jaːsi|yaasi
+𞤴𞤢𞥄𞤶𞤭|wide|jaːd͡ʒi|yaaji
+𞤴𞤭𞤥𞤣𞤫|sing|jimdɛ|yimde
+𞤴𞤭𞤴𞤣𞤫|see|jijdɛ|yiyde
+𞤴𞤭𞤼𞤫𞤪𞤫|eye|jitɛrɛ|yitere
+𞤴𞤭𞥅𞤴𞤢𞤤|bone|jiːjal|yiiyal
+𞤴𞤭𞥅𞤴𞤢𞤥|blood|jiːjam|yiiyam
+𞤴𞤮𞥅𞤪𞤭|dry|jɔːri|yoori
+𞤴𞤵𞤥𞥆𞤢⹁‏ 𞤲𞤫𞥅𞤲𞤫|mother|jumma,‏ nɛːnɛ|yumma,‏ neene
+𞤵𞤤𞥆𞤵𞤲𞥋𞤣𞤵|cat|ulluⁿdu|ullundu
+𞤵𞥅𞤪𞤣𞤫|smell|uːrdɛ|uurde
+𞤵𞥅𞤴𞤢𞤲𞥋𞤣𞤫|acceleration|uːjaⁿdɛ|uuyande
+𞤶𞤢𞤤𞤣𞤫|laugh|d͡ʒaldɛ|jalde
+𞤶𞤢𞤥|peace|d͡ʒam|jam
+𞤶𞤢𞤥𞥆𞤢|night|d͡ʒamma|jamma
+𞤶𞤢𞤳𞤢|so|d͡ʒaka|jaka
+𞤶𞤢𞤺𞥆𞤵𞤣𞤫|hold|d͡ʒaɡɡudɛ|jaggude
+𞤶𞤮𞤥 𞤧𞤵𞥅𞤣𞤵|wife|d͡ʒɔm suːdu|jom suudu
+𞤶𞤮𞤥 𞤺𞤢𞤤𞥆𞤫|husband|d͡ʒɔm ɡallɛ|jom galle
+𞤶𞤮𞤴𞤭|five|d͡ʒɔji|joyi
+𞤶𞤮𞥅𞤣𞤢𞥄𞤣𞤫|sit|d͡ʒɔːdaːdɛ|joodaade
+𞤶𞤵𞤲𞤺𞤮|hand|d͡ʒunɡɔ|jungo
+𞤶𞤵𞥅𞤼𞤭|long|d͡ʒuːti|juuti
+𞤷𞤫𞤩𞤫|bark|t͡ʃɛɓɛ|ceɓe
+𞤷𞤵𞤥𞤵|fire|t͡ʃumu|cumu
+𞤷𞤵𞤪𞤳𞤭|smoke|t͡ʃurki|curki
+𞤷𞤵𞤳𞤢𞤤𞤫𞤤|child|t͡ʃukalɛl|cukalel
+𞤷𞤵𞥅𞤪𞤳𞤭|smoke|t͡ʃuːrki|cuurki
 𞤸𞤢'𞤢𞤤𞤢|language|ha'ala
-𞤸𞤢𞤦𞤣𞤫|fight|habdɛ (habde)
-𞤸𞤢𞤴𞤣𞤢𞤪𞤢|zero|hajdara (haydara)
-𞤸𞤢𞥄𞤪𞤭|full|haːri (haari)
-𞤸𞤢𞥄𞤲𞤵𞤯𞤥|basic|haːnuɗm (haanuɗm)
-𞤸𞤢𞥄𞤳𞤮|leaf|haːkɔ (haako)
-𞤸𞤢𞥄𞤴𞤪𞤫|mountain|haːjrɛ (haayre)
-𞤸𞤢𞥄𞤴𞤪𞤫|stone|haːjrɛ (haayre)
-𞤸𞤫𞤲𞤣𞤵|wind|hɛndu (hendu)
-𞤸𞤫𞥅𞤱𞤭|many|hɛːwi (heewi)
-𞤸𞤫𞥅𞤲|some|hɛːn (heen)
-𞤸𞤭𞤲𞤣𞤵|ancient (hin.du)
-𞤸𞤭𞤲𞤫𞤪𞤫|nose|hinɛrɛ (hinere)
-𞤸𞤭𞤲𞥋𞤣𞤵|it (hi.ndu)
-𞤸𞤭𞤼𞤢𞥄𞤲𞥋𞤣𞤫|year|hitaːⁿdɛ (hitaande)
-𞤸𞤮𞤤𞤥𞤮⹁‏ 𞤸𞤮𞤤𞤦𞤫|who|hɔlmɔ,‏ hɔlbɛ (holmo,‏ holbe)
-𞤸𞤮𞤤𞤲𞤮|how|hɔlnɔ (holno)
-𞤸𞤮𞤤𞤲𞥋𞤣𞤫|when|hɔlⁿdɛ (holnde)
-𞤸𞤮𞤤𞤳𞤮|what|hɔlkɔ (holko)
-𞤸𞤮𞤤𞤼𞤮|where|hɔltɔ (holto)
-𞤸𞤮𞥅𞤣𞤫𞤪𞤫|star|hɔːdɛrɛ (hoodere)
-𞤸𞤮𞥅𞤪𞤫|head|hɔːrɛ (hoore)
-𞤸𞤵𞤣𞤮|grass|hudɔ (hudo)
-𞤸𞤵𞤤𞤣𞤫|fear|huldɛ (hulde)
-𞤸𞤵𞤥𞤣𞤫|tie|humdɛ (humde)
-𞤸𞤵𞤲𞤵𞤳𞤮|mouth|hunukɔ (hunuko)
+𞤸𞤢𞤦𞤣𞤫|fight|habdɛ|habde
+𞤸𞤢𞤴𞤣𞤢𞤪𞤢|zero|hajdara|haydara
+𞤸𞤢𞥄𞤪𞤭|full|haːri|haari
+𞤸𞤢𞥄𞤲𞤵𞤯𞤥|basic|haːnuɗm|haanuɗm
+𞤸𞤢𞥄𞤳𞤮|leaf|haːkɔ|haako
+𞤸𞤢𞥄𞤴𞤪𞤫|mountain|haːjrɛ|haayre
+𞤸𞤢𞥄𞤴𞤪𞤫|stone|haːjrɛ|haayre
+𞤸𞤫𞤲𞤣𞤵|wind|hɛndu|hendu
+𞤸𞤫𞥅𞤱𞤭|many|hɛːwi|heewi
+𞤸𞤫𞥅𞤲|some|hɛːn|heen
+𞤸𞤭𞤲𞤣𞤵|ancient|hin.du
+𞤸𞤭𞤲𞤫𞤪𞤫|nose|hinɛrɛ|hinere
+𞤸𞤭𞤲𞥋𞤣𞤵|it|hi.ndu
+𞤸𞤭𞤼𞤢𞥄𞤲𞥋𞤣𞤫|year|hitaːⁿdɛ|hitaande
+𞤸𞤮𞤤𞤥𞤮⹁‏ 𞤸𞤮𞤤𞤦𞤫|who|hɔlmɔ,‏ hɔlbɛ|holmo,‏ holbe
+𞤸𞤮𞤤𞤲𞤮|how|hɔlnɔ|holno
+𞤸𞤮𞤤𞤲𞥋𞤣𞤫|when|hɔlⁿdɛ|holnde
+𞤸𞤮𞤤𞤳𞤮|what|hɔlkɔ|holko
+𞤸𞤮𞤤𞤼𞤮|where|hɔltɔ|holto
+𞤸𞤮𞥅𞤣𞤫𞤪𞤫|star|hɔːdɛrɛ|hoodere
+𞤸𞤮𞥅𞤪𞤫|head|hɔːrɛ|hoore
+𞤸𞤵𞤣𞤮|grass|hudɔ|hudo
+𞤸𞤵𞤤𞤣𞤫|fear|huldɛ|hulde
+𞤸𞤵𞤥𞤣𞤫|tie|humdɛ|humde
+𞤸𞤵𞤲𞤵𞤳𞤮|mouth|hunukɔ|hunuko
 𞤹𞤭𞤪𞥆𞤢|confession|qirra
-𞤺𞤢𞤴𞤲𞤢𞥄𞤳𞤮|shepherd|ɡajnaːkɔ (gaynaako)
-𞤺𞤢𞤻𞤮|enemy|ɡaɲɔ (gañɔ)
-𞤺𞤫𞥅𞤷|sea|ɡɛːt͡ʃ (geec)
-𞤺𞤮𞤣𞥆𞤮|other|ɡɔddɔ (goddo)
-𞤺𞤮𞤪𞤮|cola nut|ɡɔrɔ (goro)
-𞤺𞤮𞤪𞤳𞤮|man|ɡɔrkɔ (gorko)
-𞤺𞤮𞤼𞤮|one|ɡɔtɔ (goto)
-𞤺𞤮𞥅|one|ɡɔː (goo)
-𞤺𞤮𞥅𞤲𞥋𞤺𞤢|correct|ɡɔːᵑɡa (goonga)
-𞤻𞤢𞤤𞤢𞤱𞤥𞤢|day|ɲalawma (ñalawma)
-𞤻𞤢𞥄𞤥𞤣𞤫|eat|ɲaːmdɛ (ñaamde)
-𞤻𞤢𞥄𞤥𞤮|right|ɲaːmɔ (ñaamo)
-𞤻𞤢𞥄𞤪𞤭𞥅𞤪𞤵|cat|ɲaːriːru (ñaariiru)
-𞤻𞤭𞥅𞤪𞤫|tooth|ɲiːrɛ (ñiire)
-𞤻𞤮𞤤𞤭|rotten|ɲɔli (ñoli)
-𞤻𞤮𞥅𞤼𞤣𞤫|sew|ɲɔːtdɛ (ñootde)
-𞤼𞤢𞤴𞤣𞤫|cut|tajdɛ (tayde)
+𞤺𞤢𞤴𞤲𞤢𞥄𞤳𞤮|shepherd|ɡajnaːkɔ|gaynaako
+𞤺𞤢𞤻𞤮|enemy|ɡaɲɔ|gañɔ
+𞤺𞤫𞥅𞤷|sea|ɡɛːt͡ʃ|geec
+𞤺𞤮𞤣𞥆𞤮|other|ɡɔddɔ|goddo
+𞤺𞤮𞤪𞤮|cola nut|ɡɔrɔ|goro
+𞤺𞤮𞤪𞤳𞤮|man|ɡɔrkɔ|gorko
+𞤺𞤮𞤼𞤮|one|ɡɔtɔ|goto
+𞤺𞤮𞥅|one|ɡɔː|goo
+𞤺𞤮𞥅𞤲𞥋𞤺𞤢|correct|ɡɔːᵑɡa|goonga
+𞤻𞤢𞤤𞤢𞤱𞤥𞤢|day|ɲalawma|ñalawma
+𞤻𞤢𞥄𞤥𞤣𞤫|eat|ɲaːmdɛ|ñaamde
+𞤻𞤢𞥄𞤥𞤮|right|ɲaːmɔ|ñaamo
+𞤻𞤢𞥄𞤪𞤭𞥅𞤪𞤵|cat|ɲaːriːru|ñaariiru
+𞤻𞤭𞥅𞤪𞤫|tooth|ɲiːrɛ|ñiire
+𞤻𞤮𞤤𞤭|rotten|ɲɔli|ñoli
+𞤻𞤮𞥅𞤼𞤣𞤫|sew|ɲɔːtdɛ|ñootde
+𞤼𞤢𞤴𞤣𞤫|cut|tajdɛ|tayde
 𞤼𞤢𞤼𞤭|three|tati
-𞤼𞤢𞥄𞤤𞤭|stories|taːli (taali)
-𞤼𞤫𞤣𞥆𞤭|heavy|tɛddi (teddi)
-𞤼𞤫𞤳𞥆𞤭|thick|tɛkki (tekki)
-𞤼𞤫𞥅𞤱𞤵|meat|tɛːwu (teewu)
-𞤼𞤮𞤦𞤮|rain|tɔbɔ (tobo)
-𞤼𞤮𞤩𞤮|rain|tɔɓɔ (toɓo)
-𞤼𞤮𞥅𞤲|there|tɔːn (toon)
+𞤼𞤢𞥄𞤤𞤭|stories|taːli|taali
+𞤼𞤫𞤣𞥆𞤭|heavy|tɛddi|teddi
+𞤼𞤫𞤳𞥆𞤭|thick|tɛkki|tekki
+𞤼𞤫𞥅𞤱𞤵|meat|tɛːwu|teewu
+𞤼𞤮𞤦𞤮|rain|tɔbɔ|tobo
+𞤼𞤮𞤩𞤮|rain|tɔɓɔ|toɓo
+𞤼𞤮𞥅𞤲|there|tɔːn|toon
 𞤼𞤵𞤲𞤱𞤭|dirty|tunwi
-𞤼𞤵𞥅𞤼𞤣𞤫|vomit|tuːtdɛ (tuutde)
+𞤼𞤵𞥅𞤼𞤣𞤫|vomit|tuːtdɛ|tuutde
 𞤽𞤢𞤪𞤭|beauty|ŋari
-𞤾𞤫𞤤𞤮𞥅|bicycle|vɛlɔː (veloo)
-𞥃𞤢𞤴𞤳𞤵𞤶𞤮|sheikh|ʃajkud͡ʒɔ (shaykujo)
+𞤾𞤫𞤤𞤮𞥅|bicycle|vɛlɔː|veloo
+𞥃𞤢𞤴𞤳𞤵𞤶𞤮|sheikh|ʃajkud͡ʒɔ|shaykujo
 𞥞 𞤢𞤤𞤢𞥄 !|no!
 𞥟 𞤢𞤤𞤢𞥄 ؟|no?
 𞤗𞤵𞤪𞥇𞤢𞤲|Quran|qurʔan
