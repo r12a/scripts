7d049c62f3bafa9790584f1e52027d82c4e60e03
r12a <ishida@w3.org>
2021-10-06 06:37:41 +0100

    bsq: v29 further topup.

---
+++ bassavah/bsq-langdata.js
@@ -0,0 +1,18 @@
+// this should always be updated when app-charuse/langs.js is updated
+
+var langs = {
+
+"bsq-bass": { name:"Bassa", local:"𖫔𖫧𖫳𖫒𖫨𖫰𖫨𖫱 (Ɓǎsɔ́ɔ̀)", silcode:"bsq", 
+source:"feb8d27835761376c21d0c994cb1c128010d1ac0", 
+region:"afr", countries:"Liberia", script:"bass", speakers:"648000", 
+letter:"𖫭𖫪𖫩𖫫𖫬𖫨𖫧𖫥𖫢𖫡𖫗𖫞𖫟𖫙𖫑𖫖𖫘𖫝𖫓𖫣𖫒𖫜𖫚𖫠𖫤𖫔𖫐𖫕𖫛𖫦", mark:"𖫰𖫱𖫲𖫳𖫴", punctuation:"𖫵“”", 
+orth:`[Bassa Vah] &nbsp; Rediscovered in the Americas at the start of the 20th century. Now used mainly by older people. <a href="../scripts/bassavah/">Details</a>.`,
+also:
+["bsq | latn | Primary usage."],
+type:"alphabet", cs:"no", gpos:"yes", gsub:"no", cursive:"no", direction:"ltr", wordsep:"space", baseline:"mid", wrap:"?", hyphenation:"?",  justification:"?",
+vowels:"let:y",
+linked:"bassavah", fonts:"/pickers/bass/", 
+},
+
+
+}
