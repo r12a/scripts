e35a719ef3246f981999286911b13a9676c20348
r12a <ishida@w3.org>
2021-10-08 16:51:19 +0100

    block-functions: Remove console.log line

--- common28/block-functions.js
+++ common28/block-functions.js
@@ -11,7 +11,7 @@ marks = new Set()
 function setMarks () {
     // sets the global variable marks as a set containing all combining marks in the spreadsheet
     for (var char in spreadsheetRows) {
-        console.log(char,spreadsheetRows[char][cols['class']])
+        //console.log(char,spreadsheetRows[char][cols['class']])
         if (spreadsheetRows[char][cols['class']].startsWith('M')) window.marks.add(char)
         }
     return
