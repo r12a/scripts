1b927d2f942bf23e4d5faf3a27e6bd85a6affd2d
r12a <ishida@w3.org>
2021-10-10 17:36:24 +0100

    deva-ks: v29

--- devanagari/ks-examples.js
+++ devanagari/ks-examples.js
@@ -1,5 +1,6 @@
 /*
 @ Source: https://en.wiktionary.org/wiki/Appendix:Indo-Aryan_Swadesh_lists
+@ Source: https://en.wiktionary.org/w/index.php?title=Category:Kashmiri_lemmas&pagefrom=%E0%A4%9C%E0%A4%BC%E0%A5%82%E0%A4%A8%0A%E0%A4%9C%E0%A4%BC%E0%A5%82%E0%A4%A8#mw-pages
 @ Source: http://www.koshur.org/pdf/BasicReader.pdf
 § 'Annapurna SIL'
 # 200
@@ -9,185 +10,425 @@
 
 
 autoExpandExamples.ks = `
-ज़ॊंग|fire|zong
-त्यम्बर|cataract|tʲəmbər
-थऺन्य्|butter|tʰənʲ
-पॖंच़ॗह|twenty-five|pɨnt͡sɨːh
-मुँह|mouth|mũh
 
-बॖ|I|bɨ
-च़ॖ|you|t͡sɨ
-कॉशुर|Kashmiri|kəːʃur
-द्वद्|milk|dɔd
-शुर|child|ʃur
-मोल|father|moːl
-हून्|dog|huːn
-कुल|tree|kul
-ब्रह्मांड|universe
-
-
-अख|one|akʰ
+अंग|limb, organ|əŋɡ|aṅg
+अंब|mango|amb|amb
+अख|one|akʰ|akh
+अख़बार|newspaper|akʰbaːr|akhbār
+अखा||
+अछुर||
+अज़||
+अतलास|satin|atlaːs||** no virama for cluster!
 अथॖ|hand|atʰɨ
-ॳछ|eye|əʧʰ
-ॳड|half|əɖ
-च़ऺर|sparrow|t͡sər
-दऺज़|burned|dəz
-गॖर|energetic|gɨr
-बॖ|I, me|bɨ
-खॖर|slip|kʰɨr
-ॷठ्य|eight times|ɨːʈʰʲ
-ॷठिम|eighth|ɨːʈʰim
-कॗत्य्|how many?|kɨːtʲ
-तॗर|cold|tɨːr
-आश|hope|aːʃ
+अनुन||
+अपोर|other side|apoːr
+अपॖज़||
+अबॊद|stupid, foolish|abod|abod
+अम्ब्|bramble||amb
+अरतल|gold foil|artəl
+अरदाह|eighteen|ardaːh
+असुन||
+अहोक|central pivot of millstone|ahoːk
 आख|mark|aːkʰ
+आगुर|root, source|aːɡuɾ|āgur
+आप्रावुन||
+आब|water|aːb|āb
 आवुर|busy|aːvur
-जान|good|d͡ʒaːn
-वार|day|vaːr
-ताफ|sunny|taːpʰ
-ॴश|enjoyment|əːʃ
-ॴर|plum|əːr
-ॴस|mouth|əːs
-दऻर|window|dəːr
-हऻर|maina|həːr
-लऻर|cucumber|ləːr
+आश|hope|aːʃ
+आसुन||
 इजाज़थ|permission|id͡ʒaːzətʰ
 इशटाम|stamp paper|iʃʈaːm
-दिवता|devta|divtaː
-बितुर|capacity|bitur
-बिचोर|helpless|biʧoːr
-ईमान|faith|iːmaːn
+ईमान|faith|iːmaːn|īmān
 ईश्वर|God|iːʃvər
-सीर|brick|siːr
-शीन|snow|ʃiːn
-तीर|arrow|tiːr
 उजरथ|wages|ud͡ʒrətʰ
 उफतादॖ|ignored|upʰtaːdɨ
 उलफथ|love|ulpʰətʰ
-वुछ|see|vuʧʰ
-करुन|to do|kərun
-हगुर|wheel|həgur
 ऊतरॖ|day before yesterday|uːtrɨ
 ऊर्य् नॖ यूर्य्|not well|uːrʲ nɨ yuːrʲ
-सूर|ash|suːr
-रूद|rain|ruːd
-कून|corner|kuːn
-ज़ून|moon|zuːn
-ऋॆष्य्|rishi|reʂʲ
 ऋग्वेद|Rigveda|rigveːd
-पृतख|divorce|pritəkʰ
-तृष्ना|greed|triʂnaː
-एतिराज़|objection|eːtiraːz
-मेख|nail|meːkʰ
-हलेल|yellow myrobalan|həleːl
-मेठ|head coolie|meːʈʰ
+ऋॆष्य्|rishi|reʂʲ
 ऎहसान|favour|ehsaːn
-रॆह|flame|reh
-हॆलि|when|heli
-दॆह|body|deh
+एतिराज़|objection|eːtiraːz
 ऐब|defect|əĭb
-वैकुंठ|heaven|vəĭkunʈʰ
-हैबुंगॖ|dumb struck|həĭbungɨ
-ओस|was|oːs
-ओम|raw|oːm
-ओलुव|potato|oːluv
-ओबरॏय|respect|oːbrɔy
-गोख|went|goːkʰ
-मोर|peacock|moːr
-अपोर|other side|apoːr
+ऑव्युल||
+ऒक-||
+ऒछ़|frail|ot͡sʰ
+ऒदुर||
 ऒन|blind|on
 ऒर|good|or
-ऒछ़|frail|ot͡sʰ
-गॊछ़|wanted|got͡sʰ
-ख्यॊल|flock|kʰyol
+ऒश||
+ओट|flour|oːʈ
+ओत्रॖ||
+ओबरॏय|respect|oːbrɔy
+ओम|raw|oːm
+ओलुव|potato|oːluv
+ओस|was|oːs
 औज़ार|instrument|əŭzaːr
 औशद|medecine|əŭʃəd
-गौ|went|gəŭ
-चौंखॖरुन|to shrivel up|ʧəŭnkʰɨrun
-अंग|part|ang
-मंज़|in|mənz
-ज़ंग|leg|zəng
-र्‌यथ|month|rʲətʰ
-कॗत्य्|how many|kɨːtʲ
-तीत्य्|so many|tiːtʲ
-गॏर|guru|gɔr
-खॏर|foot|kʰɔr
-मॏल|cost|mɔl
-ॶंह-रारय|to groan|ɨnh-raːrəy
-ॷंच़ॖह-पॗंच़ॖह|make excuses|ɨːnt͡sɨh-pɨ̄nt͡sɨh
-कलुश|kalash|kəluʃ
-कोम|tribe, race|koːm
+कडुन||
+कथ||
+कन||
 कपुर|cloth|kəpur
+कर||
+करुन|to do|kərun
+कलुश|kalash|kəluʃ
+कलॖ||
+काहवऺट|touchstone|kaːhvəʈ
+कुंज़||
+कुल|tree|kul
+कुस||
+कून|corner|kuːn
+केलॖ|banana|keːlɨ|kēlụ
+कॉशुर|Kashmiri|kəːʃur
+कोचॖ|lane|koːt͡ʃɨ
+कोम|tribe, race|koːm
+क्याज़ि|why||kyāzi
+क्रावुत||
+क्रुहुन||
+क्वस||
+कॗत्य्|how many?|kɨːtʲ
 खबर|news|kʰəbər
 खल|threshing floor|kʰəl
-गाटुल|wise|gaːʈul
-प्रागाश|moonshine|praːgaːʃ
-दाग|stain|daːg
-चीर|wring|ʧiːr
-कोचॖ|lane|koːʧɨ
-पाचि|trotters|paːʧi
-छान|carpenter|ʧʰaːn
-बेछुन|to beg|beːʧʰun
-जवान|young|d͡ʒəwaːn
-वॊज|rainwater falling from house eaves|vod͡ʒ
-च़्यड|patience|t͡sʲəɖ
-मच़र|madness|mət͡sər
-च़ोंग|earthen lamp|t͡soːng
-छ़्यतॖ|extinguish|t͡sʰytɨ
-ज़ान|knowledge|zaːn
-बाज़र|market|baːzər
-टमाटर|tomato|ʈəmaːʈər
-टेंग|hillock|ʈeːng
-ओट|flour|oːʈ
-वुठ|lip|wuʈʰ
-ठानॖ|lid|ʈʰaːnɨ
-ठग|cheat|ʈʰəg
-शेठ|sixty|ʃeːʈʰ
-डख|support|ɖəkʰ
-यड|belly|yəɖ
-ताख|odd number|taːkʰ
-तखसीर|fault|təkʰsiːr
-अतलास|satin|atlaːs||** no virama for cluster!
-वथ|way|vətʰ
-राथ|night, yesterday|raːtʰ
-पथर|floor|pətʰər
-दऻर|beard|dəːr
-मदद|help|mədəd
-राद|length of cloth|raːd
-दान|hearth|daːn
-नाबद|candy|naːbəd
-दन|wealth|dən
-पज़र|truth|pəzər
-पोग|destruction|poːg
-रफतार|speed|rəpʰtaːr||** another without virama
-फिरन|turn upside down|pʰirən
-बिसलाय|fishing rod|bislaːy
-रबाब|rabab (musical instrument)|rəbaːb
+खालुन||
+खॊर||
+ख्यावुन|to feed|kʰʲɑːʋun|khyāwun
+ख्यॊन||
+ख्यॊल|flock|kʰyol
+ख्वतॖ||
+ख्वर||
+खॏर|foot|kʰɔr
+खॖर|slip|kʰɨr
+गछ़ुन|to go, leave|ɡat͡sʰun|gaċhun
 गम|grief|gəm
-मॊत|mad|mot
-मस|hair|məs
-येर|wool|yeːr
-मियान|sheath|miyaːn
-यकजा|together|yəkʤaː
-अरदाह|eighteen|ardaːh
-राश|zodiac sign|raːʃ
-प्रऻज|chopped straw|prəːʤ
-द्रोंठ|hardened|droːnʈʰ
+गरॖ|house, home|ɡarɨ|garụ
+गलुन||
+गाज़ॖर||
+गाटुल|wise|gaːʈul
+गाटॖज|wise nom. f. sing.|ɡaːʈɨd͡ʒ|gāṭụj
+गाम||
+गाश||
+गुपुन||
+गुर||
+गुल||
+गॊछ़|wanted|got͡sʰ
+गॊब||
+गोख|went|goːkʰ
+गौ|went|gəŭ
+ग्रंद||
+ग्रकुन||
+गॏर|guru|gɔr
+गॖर|energetic|gɨr
+गॖस|excrement|ɡɨs|gụs
+च़टुन||
+च़लुन||
+च़ऺर|sparrow|t͡sər
+च़ूंठ|apple|t͡sũːʈʰ|ċūṃṭh
+च़ॆ||
+च़ेनुन||
+च़ेर|apricot|t͡seːr|ċēr
+च़ोंग|earthen lamp|t͡soːng
+च़्यड|patience|t͡sʲəɖ
+च़ॖ|you|t͡sɨ
+च़ॗन||
+चावुन||
+चिठ्य|letter||ciṭhy
+चीर|wring|t͡ʃiːr
+चॊन||
+चौंखॖरुन|to shrivel up|t͡ʃəŭnkʰɨrun
+छलुन||
+छ़्यतॖ|extinguish|t͡sʰytɨ
+छान|carpenter|t͡ʃʰaːn
+छॊत||
+जवान|young|d͡ʒəwaːn
+जहाज़||
+ज़ंग|leg|zəng|zaṃg
+ज़ंगॖ||
+ज़गथ||
+ज़मानॖ||
+ज़ल||
+ज़ान|knowledge|zaːn
 ज़ाल|net|zaːl
-लोल|affection|loːl
-अरतल|gold foil|artəl
-वाठ|union|vaːʈʰ
-पश|roof|pəʃ
-वुशनेर|warmth|vuʃneːr
-सोन|our|soːn
-वसवास|perturbation|vəsvaːs
-अहोक|central pivot of millstone|ahoːk
-काहवऺट|touchstone|kaːhvəʈ
-त्रॆ|three|tre
-त्राम|copper|traːm
-यपऻर्य|this side|jəpəːrʲ
-पऻर्यज़ान|knowledge|pəːrʲzaːn
+ज़ालुन||
+ज़ून|moon|zuːn|zūn
+ज़ॊंग|fire|zoŋg
+ज़्यव||
+ज़्यूठ||
+ज़ॖ|two|zɨ|zụ
+जान|good|d͡ʒaːn
+टमाटर|tomato|ʈəmaːʈər
+टर||
+टेंग|hillock|ʈeːng
+ठग|cheat|ʈʰəg
+ठानॖ|lid|ʈʰaːnɨ
+डख|support|ɖəkʰ
+डॊख||
+डॗस||
+तखसीर|fault|təkʰsiːr
+तगुन||
+तरुन||
+ताख|odd number|taːkʰ
+ताफ|sunny|taːpʰ
+ताम||
+तामथ||
+तीत्य्|so many|tiːtʲ
+तीर|arrow|tiːr
+तील|oil||tīl
+तुरुन||
+तुलुन||
+तृष्ना|greed|triʂnaː
+तॊन||
+तोलुन|to weigh|toːlun|tōlun
+त्यम्बर|cataract|tʲəmbər
 त्यम्बॖर|spark|tʲəmbɨr
+त्रठ||
+त्राम|copper|traːm
+त्रावुन||
+त्रॆ|three|tre|tre
+त्रेश||
+तॖ||
+तॗर|cold|tɨːr
+थऺन्य्|butter|tʰənʲ
+थॊद||
+दन|wealth|dən
+दरवाज़ॖ||
+दर्शुन|sight, vision|darʃun|darśun
+दऺज़|burned|dəz
+दऻर|window|dəːr
+दाग|stain|daːg
+दान|hearth|daːn
+दिवता|devta|divtaː
+दॆह|body|deh
+दॊर||
+दॊह|day, daytime|dɔh|doh
+दॊहय|daily, every day|d̪ohaj|dọhay
+दोरुन||
+द्रोंठ|hardened|droːnʈʰ
+द्वद|milk|dɔd|dwad
+द्वद्|milk|dɔd|dwad
+दॖह||
+नच़ुन||
+नवि सरॖ|again, anew||nawi sarụ
+नस|nose|nəs|nas
+नऺर|arm||nạr
+नाबद|candy|naːbəd
+नार||
+नाव||
+नावुत|noun|nɑːwut|nāwut
+नॆंन्दॖर|sleep||neṃndụr
+न्यूल||
+नॖ||
+पंजऻब्य|Punjabi|pand͡ʒəːbʲ|panjạ̄b'
+पकुन||
+पगाह||
+पज़र|truth|pəzər
+पतॖ||
+पथर|floor|pətʰər
+पनुन||
+पम्पोश|lotus|pampoːʃ|pampōś
+परुन||
+परुस||
+पश|roof|pəʃ
+पऻर्यज़ान|knowledge|pəːrʲzaːn
+पाचि|trotters|paːt͡ʃi
+पूथ|book, manuscript|puːt̪ʰ|pūth
+पूथ्य||
+पृतख|divorce|pritəkʰ
+पॆठ||
+पेर्युस||
+पॊंज़||
+पॊत लॊग||
+पोग|destruction|poːg
+पोञु॒||
+पोन्य|water|poːnʲ|pōny
+पोश|flower|poːʃ|pōś
+प्यॊन||
+प्रऻज|chopped straw|prəːʤ
+प्रागाश|moonshine|praːgaːʃ
+प्रावुन||
+पॖंच़ॗह|twenty-five|pɨnt͡sɨːh
+फख||
+फिरन|turn upside down|pʰirən
+फिरुन|to translate, turn over||phirun
+बतॖ||
+बब||
+बलुन||
+बाज़र|market|baːzər
+बादम||
+बादाम||
+बापथ||
+बावुत||
+बिचोर|helpless|bit͡ʃoːr
+बितुर|capacity|bitur
+बिसलाय|fishing rod|bislaːy
+बुथ||
+बॆहुन||
+बेछुन|to beg|beːt͡ʃʰun
+बेत्रि|etc.||bētri
+बॊठ||
+बॊड||
+बोज़ुन|to listen, hear|boːzun|bōzun
+ब्यॊन||
+ब्रह्मांड|universe
+ब्रॊंह लॊग||
+ब्व||
+बॖ|I, me|bɨ|bụ
+मंज़|in|mənz
+मच़र|madness|mət͡sər
+मदद|help|mədəd
+मरुन||
+मरॖद|man|marɨd|marụd
+मस|hair|məs
+मऻज|mother|məːd͡ʒ|mạ̄j
+मऻश|buffalo|məːʃ|mạ̄ś
+मानुन||
+मियान|sheath|miyaːn
+मुँह|mouth|mʊ̃ɦ|mũh
+मूल||
+मॅशीद|mosque||mạśīd
+मॆ||
+मेख|nail|meːkʰ
+मेठ|head coolie|meːʈʰ
+मॊत|mad|mot
+मोज|mother|moːd͡ʒ|mạ̄j
+मोर|peacock|moːr
+मोल|father|moːl|mōl
+म्वल||
+म्वलुल||
+मॏल|cost|mɔl
+यकजा|together|yəkʤaː
+यछ़ुन||
+यड|belly|yəɖ
+यपऻर्य|this side|jəpəːrʲ
+यि तॖ गछ़|coming and going||yi tụ gaċh
+यीरुन|to float, drift||yīrun
+युन||
+येर|wool|yeːr
+रनुन||
+रफतार|speed|rəpʰtaːr||** another without virama
+रबाब|rabab (musical instrument)|rəbaːb
+राज़ॖमाह||
+राथ|night, yesterday|raːtʰ
+राद|length of cloth|raːd
+राश|zodiac sign|raːʃ
+रुकुन||
+रूद|rain|ruːd
+रॆह|flame|reh
+र्‌यथ|month|rʲətʰ
+रॗफ||
+लऻर|cucumber|ləːr
+लूर|stick|luːɾ|lūr
+लॊत||
+लोल|affection|loːl
+ल्वकुट||
+वच़ुन||
+वथ|way|vətʰ
+वदुन||
+वनुत||
+वनुन||
+वर्ताव||
+वसवास|perturbation|vəsvaːs
+वाँगुन|eggplant, brinjal|wãːɡun|wām̐gun
+वाँदुर|monkey|wãːdur|wām̐dur
+वाठ|union|vaːʈʰ
+वार|day|vaːr
+वालुन||
+वीर|hero||wīr
+वुछ|see|vut͡ʃʰ
+वुठ|lip|wuʈʰ
+वुडुन||
+वुफ||
+वुशनेर|warmth|vuʃneːr
+वुशुन||
+वूर||
+वॅथॖर||
+वॅरी|year||wạrī
+वैकुंठ|heaven|vəĭkunʈʰ
+वॊज|rainwater falling from house eaves|vod͡ʒ
+व्रख|tree|ʋrakʰ|wrakh
+शिग|apprehension||śig
+शीन|snow|ʃiːn|śīn
+शुर|child|ʃur
+शेठ|sixty|ʃeːʈʰ
+सावुन||
+सिरी|sun||sirī
+सीर|brick|siːr
+सु||
+सूर|ash|suːr
+सोन|our|soːn
+स्वरॖग||
+हगुर|wheel|həgur
+हलेल|yellow myrobalan|həleːl
+हऻर|maina|həːr
+हाम||
+हाल||
+हून्|dog|huːn
+हॆकुन||
+हॆछुन||
+हॆलि|when|heli
+हैबुंगॖ|dumb struck|həĭbungɨ
+हॊख||
+हॊल||
+ॳंजीर|fig|ənd͡ʒiːr|ạnjīr
+ॳछ|eye|ət͡ʃʰ|ạch
+ॳड|half|əɖ
+ॳमीर|rich||ạmīr
+ॳस्य|we|əsʲ|ạsy
+ॴर|plum|əːr
+ॴश|enjoyment|əːʃ
+ॴस|mouth|əːs|ạ̄s
+ॶंह-रारय|to groan|ɨnh-raːrəy
+ॷंच़ॖह-पॗंच़ॖह|make excuses|ɨːnt͡sɨh-pɨ̄nt͡sɨh
+ॷठिम|eighth|ɨːʈʰim
+ॷठ्य|eight times|ɨːʈʰʲ
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 `
---
+++ devanagari/ks-langdata.js
@@ -0,0 +1,17 @@
+// this should always be updated when app-charuse/langs.js is updated
+
+var langs = {
+
+"ks-deva": { name:"Kashmiri (Koshur)", local:"कॉशुर‎ (Koshur)", silcode:"kas", 
+source:"7f2afe74cc91aa1c3986c9017fac92907f6cd260", 
+region:"sasia", countries:"India, Pakistan", script:"deva", speakers:"5600000", 
+letter:"पफबतथदटठडकखगचछजवसशहमनरलयइईॶॷउऊऎएॳॴऒओॵअआऐऔऋ", mark:"िीॖॗुूॆेऺऻॊोॏाैौृं़्ँ", punctuation:"।", aux:"ज़णञङभधढझघषकज", 
+orth:`Devanagari. &nbsp; Associated with the Hindu Kashmiris. There were orthographic reforms in 1995, 2002, and 2009. This selection of characters reflects the latter. <a href="../scripts/devanagari/kashmiri" target="_blank">Details</a>.`,
+notes:`Need clarity about use or not of Devanagari numbers.`,
+type:"abugida", cs:"no", gpos:"yes", gsub:"yes", cursive:"no", direction:"ltr", wordsep:"space", baseline:"high", wrap:"word", hyphenation:"?",  justification:"sp",
+vowels:"inh:1 ind:17 vs:16 pre:1", clusters:"vir inv stk cnj lig rax", finals:"cm:1", 
+linked:"devanagari/kashmiri", fonts:"/pickers/deva-ks/", 
+},
+
+
+}
