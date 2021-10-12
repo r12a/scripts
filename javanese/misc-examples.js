b0ea05e710218ddfe7a8aefc21f3da32cedd2514
r12a <ishida@w3.org>
2021-10-12 09:17:54 +0100

    + javanese/misc-examples.js

---
+++ javanese/misc-examples.js
@@ -0,0 +1,394 @@
+/*
+@ Source: 
+§ 'Yogyakarta'
+# 200
+~ ltr
+! jv
+*/
+
+autoExpandExamples.misc = `
+
+꧇꧑꧕꧇ꦎꦏ꧀ꦠꦺꦴꦧꦼꦂ꧇꧒꧐꧑꧒꧇|15 october 2012|
+ꦄꦪꦸ|Ayu||ayu|personal name
+ꦄꦫꦶꦱ꧀ꦠꦺꦴꦠꦼꦭꦼꦱ꧀||
+ꦄꦭ꧀ꦭꦃ|Allah||allah
+ꦆꦤ꧀ꦠꦼꦫꦏ꧀ꦱꦶ|interaction||interaksi
+ꦉꦩꦸꦏ꧀ꦗꦸꦁ||
+ꦉꦩꦸꦗꦸꦁ||
+ꦊꦩꦃꦊꦩ꧀ꦧꦸꦠ꧀|gentle||lemah lembut
+ꦊꦩ꧀ꦧꦸ||
+ꦊꦭꦸꦲꦸꦂ||
+ꦏꦁꦩꦱ꧀||
+ꦏꦏꦁ||
+ꦏꦏꦏ꧀||
+ꦏꦏꦶꦄꦩꦺꦴꦁ||
+ꦏꦔꦼꦤ꧀|desirous|ˈka.ŋən
+ꦏꦕ|glass||kaca
+ꦏꦕꦼꦥꦼꦠꦤ꧀||
+ꦏꦚ꧀ꦕ||
+ꦏꦠꦿꦺꦴ||
+ꦏꦤꦱ꧀|pineapple||kanas
+ꦏꦤ꧀ꦛꦶ|by|kanʈi
+ꦏꦤ꧀ꦠꦺꦴꦂ|office||kantor
+ꦏꦧꦸꦥꦠꦺꦤ꧀||
+ꦏꦩꦶ|we, us||kami
+ꦏꦪꦔꦤ꧀||
+ꦏꦭ||
+ꦏꦭꦶꦱ꧀ꦠꦿꦶꦏꦤ꧀||
+ꦏꦭꦺꦴꦁ||
+ꦏꦮꦱ||
+ꦏꦮꦸꦁ||
+ꦏꦶꦩꦶꦲ|chemistry||kimia
+ꦏꦶꦮ|left||kiwa
+ꦏꦸ||
+ꦏꦸꦏꦸꦃ||
+ꦏꦸꦕꦶꦁ||
+ꦏꦸꦛ|town|ku.ʈa|
+ꦏꦸꦩ꧀ꦥꦫꦤ꧀||
+ꦏꦸꦩ꧀ꦧꦁ||
+ꦏꦸꦭꦮꦂꦒ||
+ꦏꦸꦭꦮꦫ꧀ꦒ||
+ꦏꦸꦮꦠ꧀||
+ꦏꦸꦮꦱ||
+ꦏꦺꦏꦃ||
+ꦏꦺꦮꦤ꧀||
+ꦏꦺꦴꦢꦺꦴꦏ꧀||
+ꦏꦼꦤ꧀ꦝꦁ||
+ꦏꦼꦧꦺꦴ||
+ꦏꦼꦧꦿꦤꦁ||
+ꦏꦼꦩꦸꦭ꧀||
+ꦏꦼꦩ꧀ꦧꦁ||
+ꦏꦿꦠꦺꦴꦤ꧀||
+ꦏꦿꦩ||
+ꦏꦿꦩꦱ꧀||
+ꦏꦿꦶꦪ|skill, craft||kriya
+ꦏꦿꦸꦥꦸꦏ꧀|crackers||krupuk
+ꦏꦿꦺꦴꦠꦺꦴ|ant egg|ˈkro.to|kroto
+ꦏ꧀ꦫꦩ|politeness|krɔmɔ
+ꦒꦗꦃ|elephant||gaja
+ꦒꦧꦃ|grain of rice||gabah
+ꦒꦧꦲꦺ|that grain of rice||gabahe
+ꦒꦫꦸꦣ||
+ꦒꦫ꧀ꦣꦸ|guard||gardu
+ꦒꦶꦧꦱ꧀||
+ꦒꦶꦫꦶ||
+ꦒꦸꦤꦸꦁ||
+ꦒꦸꦤ꧀ꦠꦶꦁ||
+ꦒꦼꦠꦶꦃ||
+ꦒꦼꦢꦁ||
+ꦒꦼꦣꦺ||
+ꦒꦼꦤꦶ||
+ꦒꦼꦭꦱ꧀ꦉꦱꦶꦏ꧀|clean glass||gelas resik
+ꦒꦿꦺꦗ||
+ꦒꦿꦺꦴꦗꦺꦴꦒꦤ꧀||
+ꦔꦺꦭꦶꦁꦔꦶ|to consider||ngélingi
+ꦕꦕꦃꦗꦶꦮ||
+ꦕꦸꦧꦿꦶꦪ||
+ꦕꦺꦭꦺꦁ||
+ꦕꦼꦏꦏ꧀||
+ꦕꦼꦩꦼꦁ|black||cemeng
+ꦕꦼꦩꦼꦔꦚ꧀|dry rice paddy|ˈt͡ʃə.mə.ŋan|cemengan
+ꦗꦔꦤꦤ꧀||
+ꦗꦚ꧀ꦕꦸꦏ꧀||
+ꦗꦤ꧀ꦩ|human, people||jalma
+ꦗꦩꦱ꧀||
+ꦗꦩ꧀ꦧꦸ|rose apple|ˈjam.bu/ˈjɔm.bu
+ꦗꦫꦤ꧀||
+ꦗꦮ|Java||jawa
+ꦗꦲꦺ|ginger||jahe
+ꦗ꦳ꦏꦠ꧀|||zakat
+ꦗꦸꦫꦁ||
+ꦗꦸꦮꦮꦸꦠ꧀||
+ꦗꦼꦮꦮꦸꦠ꧀||
+ꦚꦠ|true||nyata
+ꦚꦮ|life, soul||nyawa
+ꦠꦁꦒꦭ꧀||
+ꦠꦔꦤ꧀|hand|ˈta.ŋan/ˈtɔ.ŋɔn
+ꦠꦤ꧀ꦢꦸꦫꦤ꧀||
+ꦠꦩꦤ꧀|garden||taman
+ꦠꦭꦶ||
+ꦠꦱꦶꦏ꧀||
+ꦠꦶꦂꦠ|holy water|tirtɔ
+ꦠꦶꦊꦩ꧀||
+ꦠꦶꦤꦸꦭꦶꦱ꧀||
+ꦠꦸꦫꦸ||
+ꦠꦺꦴꦏꦺꦴ||
+ꦠꦺꦴꦪ||
+ꦠꦼꦏ|to arrive, come||têka
+ꦠꦼꦠꦤ꧀ꦢꦸꦫꦤ꧀||
+ꦠꦼꦣꦤ꧀||
+ꦠꦽꦱ꧀ꦤ||
+ꦢꦏ꧀ꦱꦶꦤ||
+ꦢꦒꦁ||
+ꦢꦫ||
+ꦢꦫꦺꦱ꧀||
+ꦢꦭꦸ||
+ꦢꦲꦂ|eat||dahar
+ꦢꦲꦤ|fire||dahana
+ꦢꦲꦫ|dahara||dhr
+ꦢꦶꦤ||
+ꦢꦶꦤ꧀ꦠꦼꦤ꧀||
+ꦢꦸꦥ|incense||dupa
+ꦢꦸꦫꦸꦔꦤ꧀||
+ꦢꦸꦫꦺꦤ꧀||
+ꦢꦺꦴꦔꦺꦁ||
+ꦢꦺꦴꦩ꧀||
+ꦣꦂꦩ|Dharma||dharma
+ꦣꦲꦂ|to eat (hon.)||dhahar
+ꦣꦲꦂꦫꦤ꧀|food (hon.)||dhaharan
+ꦣꦸꦮꦶꦠ꧀|money||dhuwit
+ꦤꦒꦫꦶ-​ꦧꦁꦱ||
+ꦤꦸꦱꦤ꧀ꦠꦫ||
+ꦤꦼꦣ|to eat (polite)||nedha
+ꦤꦼꦤꦼꦂ||
+ꦥꦔꦤ꧀ꦤꦤ꧀||
+ꦥꦔꦺꦫꦤ꧀||
+ꦥꦔꦿꦸꦱꦏ꧀||
+ꦥꦚ꧀ꦗꦶ||
+ꦥꦝ||
+ꦥꦠꦶꦊꦩ꧀ꦩꦤ꧀||
+ꦥꦠꦸꦫꦺꦴꦤ꧀||
+ꦥꦤꦱ꧀||
+ꦥꦥꦠ꧀||
+ꦥꦫꦩꦱꦱ꧀ꦠꦿ||
+ꦥꦱꦫꦺꦪꦤ꧀||
+ꦥꦱꦶꦏ꧀||
+ꦥꦱꦺꦏ꧀||
+ꦥꦶꦁꦒꦤ꧀||
+ꦥꦶꦫꦤ꧀ꦠꦶ ꦲꦭꦸꦱ꧀|software||piranti alus
+ꦥꦶꦫꦤ꧀ꦠꦶ||
+ꦥꦸꦗꦁꦒ||
+ꦥꦸꦠꦿꦶ||
+ꦥꦸꦤꦶꦏ||
+ꦥꦸꦭꦺꦴ|island|pulo
+ꦥꦺ||
+ꦥꦼꦂꦏꦏꦱ꧀||
+ꦥꦼꦂꦠꦤꦶꦲꦤ꧀|agriculture||pertanian
+ꦥꦼꦔꦶꦭꦺꦴꦤ꧀||
+ꦥꦼꦠꦼꦁ|dark||peteng
+ꦥꦼꦢꦸꦤꦸꦁ||
+ꦥꦼꦤ꧀ꦣꦶꦣꦶꦏꦤ꧀||
+ꦥꦼꦫꦢꦔꦤ꧀||
+ꦥꦼꦫꦲꦸꦭꦪꦂ|sailboat||perahu layar
+ꦥꦿꦧꦸ||
+ꦥꦿꦩꦸꦏ|scout||pramuka
+ꦥꦿꦮꦶꦫ||
+ꦦꦑꦸꦨꦸꦮꦟ|Pakubuwana (name)|
+ꦧꦁꦱ||
+ꦧꦏꦸ||
+ꦧꦗꦶꦁꦔꦤ꧀||
+ꦧꦚꦸ||
+ꦧꦛꦁ||
+ꦧꦛꦶꦏ꧀|batik clothing|ˈba.ʈɪʔ|bathik
+ꦧꦤ꧀ꦢꦼꦁ||
+ꦧꦤ꧀ꦣ|wealth|bɔnɖ̥ɔ
+ꦧꦥꦏ꧀||
+ꦧꦥꦏ꧀ꦏꦸꦮꦭꦺꦴꦤ꧀||
+ꦧꦧꦶ||
+ꦧꦭ||
+ꦧꦭꦸꦁ||
+ꦧꦭꦺ||
+ꦧꦱ||
+ꦧꦲꦸꦱꦱ꧀ꦠꦿ|dictionary|bausastra|bausastra
+ꦧꦶꦁꦧꦶꦁ||
+ꦧꦶꦏꦏ꧀||
+ꦧꦶꦕꦫ||
+ꦧꦶꦱ꧀ꦠꦿꦤ꧀​ꦱ꧀ꦗꦏꦂꦠ|Transjakarta bus||bis transjakarta
+ꦧꦸꦏꦏ꧀||
+ꦧꦸꦏꦸꧏ|books||buku-buku
+ꦧꦸꦛꦏ꧀||
+ꦧꦸꦠ|giant|butɔ|buta
+ꦧꦸꦥꦠꦶ||
+ꦧꦸꦭ꧀ꦧꦸꦭ꧀||
+ꦧꦺꦢ|difference||béda
+ꦧꦺꦴꦧꦿꦺꦴꦏ꧀||
+ꦧꦼꦧꦪ||
+ꦧꦿꦤꦁ||
+ꦧꦿꦩ||
+ꦧ꧀ꦊꦏꦺꦴꦏ꧀||
+ꦧ꧀ꦊꦢꦸꦒ꧀|dust|bləd̪uɡ|bledug
+ꦧ꧀ꦊꦣꦸꦒ꧀|mud|bləɖuɡ|bledhug
+ꦨꦶꦩ|Bima (name)|
+ꦩꦁꦒꦶꦱ꧀||
+ꦩꦁꦱꦶ||
+ꦩꦔꦤ꧀||
+ꦩꦕ|ink||maca
+ꦩꦠ||
+ꦩꦠꦼꦤꦶ||
+ꦩꦢꦸ||
+ꦩꦤꦮ||
+ꦩꦤꦸꦁꦱ||
+ꦩꦤꦸꦏ꧀||
+ꦩꦥꦒ꧀||
+ꦩꦧꦸꦫꦸ|hunting|
+ꦩꦪꦁ||
+ꦩꦭꦲꦺꦏꦠ꧀||
+ꦩꦱꦾꦫꦏꦠ꧀||
+ꦩꦲꦺꦴꦱ꧀|to read (polite)||maos
+ꦩꦶꦠꦿ||
+ꦩꦸꦏ||
+ꦩꦸꦝ|young||mudha
+ꦩꦺꦫꦸ||
+ꦩꦼꦚꦮꦏ꧀||
+ꦪꦠꦿ|money (polite)||yatra
+ꦫꦠ|flat|ˈra.ta/ˈrɔ.tɔ
+ꦫꦠꦔ꧀ꦒ|cart|ˈrɔ.tɔŋ.ɡɔ
+ꦫꦠ꧀ꦤ|diamond||ratna
+ꦫꦩ||
+ꦫꦩꦏꦸꦮꦭꦺꦴꦤ꧀||
+ꦫꦸꦩ꧀ꦥꦸꦠ꧀||
+ꦫꦺꦏꦪꦱ||
+ꦫ꧈ꦩ꧈ꦯꦸꦭꦂꦠ|R. M. Sularta|
+ꦭꦠꦲꦤ꧀ꦚꦚꦶ|singing practice||latihan nyanyi
+ꦭꦠꦸ||
+ꦭꦪꦁꦥꦥꦒ꧀||
+ꦭꦪꦺꦴꦤ꧀||
+ꦭꦫ|sick|lɔrɔ|lara
+ꦭꦶꦤꦸꦮꦶꦃ||
+ꦭꦶꦫꦁ||
+ꦭꦺꦠꦺꦂ||
+ꦭꦺꦴꦩ꧀ꦧꦺꦴꦏ꧀||
+ꦭꦼꦏ꧀ꦱ||
+ꦮꦁꦮꦸꦁ||
+ꦮꦁꦱ||
+ꦮꦂꦠ||
+ꦮꦢꦺꦴꦤ꧀||
+ꦮꦤ||
+ꦮꦤꦶꦠ||
+ꦮꦭꦶꦫꦁ||
+ꦮꦶꦕꦤ꧀ꦠꦼꦤ꧀||
+ꦮꦶꦕꦫ||
+ꦮꦶꦤꦶꦭꦶꦱ꧀||
+ꦮꦶꦲꦫ|monastery||wihara
+ꦮꦸꦏꦸ||
+ꦮꦸꦭꦤ꧀||
+ꦮꦺꦢꦁ||
+ꦮꦺꦴꦁꦮꦢꦺꦴꦤ꧀||
+ꦮꦺꦴꦭꦸ|eight||wolu
+ꦮꦼꦔꦶ||
+ꦮꦼꦭꦶꦫꦁ||
+ꦮꦼꦱꦶ||
+ꦮ꧀ꦭꦶꦫꦁ|sulfur||wlirang
+ꦱꦒꦫ||
+ꦱꦩꦶ||
+ꦱꦩ꧀ꦥꦸꦤ꧀|already||sampun
+ꦱꦫꦶꦫ||
+ꦱꦫꦺ||
+ꦱꦭ꧀ꦗꦸ||
+ꦱꦱꦶ||
+ꦱꦶꦔ||
+ꦱꦶꦢ||
+ꦱꦶꦱꦾ||
+ꦱꦶꦱ꧀ꦮ|student||siswa
+ꦱꦸꦏꦼꦠ꧀||
+ꦱꦸꦤꦂ||
+ꦱꦸꦩꦸꦂ||
+ꦱꦸꦱꦶꦭ||
+ꦱꦺꦭ||
+ꦱꦼꦏꦺꦴꦭꦃ|school||sekolah
+ꦱꦼꦩꦔ꧀ꦏ||
+ꦱꦼꦩꦢꦶ||
+ꦱꦼꦩ꧀ꦥꦿꦺꦴꦁ||
+ꦱꦼꦭꦱ꧇꧑꧙꧇ꦩꦉꦠ꧀꧇꧒꧐꧑꧓꧇|Tuesday, 19 March 2013||selasa 19 maret 2013
+ꦱꦽꦔꦺꦔꦺ||
+ꦱꦿꦁꦒ||
+ꦲꦁꦏ꧀ꦭꦸꦁ|type of musical instrument||angklung
+ꦲꦁꦫꦁ||
+ꦲꦂꦠ||
+ꦲꦏꦸ|I, me||aku
+ꦲꦏꦺꦫꦠ꧀||
+ꦲꦏꦿꦧ꧀||
+ꦲꦏ꧀ꦱꦫ|alphabet||aksara
+ꦲꦒꦩ|religion||agama
+ꦲꦒꦼꦁ|big (polite)||ageng
+ꦲꦒ꧀ꦤꦶ||
+ꦲꦔꦶꦤ꧀|wind||angin
+ꦲꦔꦺꦭ꧀|difficult|ˈaŋɛl|angel
+ꦲꦕꦫ|programme||acara
+ꦲꦠꦮ||
+ꦲꦢꦠ꧀|tradition, custom||adat
+ꦲꦤꦕꦫꦏ|alphabet||hanacaraka
+ꦲꦤ꧀ꦝꦲꦂ||
+ꦲꦤ꧀ꦝꦸꦏ꧀||
+ꦲꦧꦁ|red|ˈa.baŋ/ˈɔ.bɔŋ
+ꦲꦩ꧀ꦧꦿꦤꦁ|turn fiery red||ambranaŋ
+ꦲꦪꦤ꧀||
+ꦲꦪꦸ|graceful||ayu
+ꦲꦭꦱ꧀||
+ꦲꦭꦺꦴꦤ꧀ꦲꦭꦺꦴꦤ꧀||
+ꦲꦱꦸ||
+ꦲꦱ꧀ꦠꦶ||
+ꦲꦶꦒ||
+ꦲꦶꦗꦺꦴ||
+ꦲꦶꦪꦤ꧀||
+ꦲꦶꦪꦸ||
+ꦲꦶꦭꦠ꧀||
+ꦲꦸꦏꦫ||
+ꦲꦸꦠꦮ||
+ꦲꦸꦠꦮꦶ||
+ꦲꦸꦥꦧꦁꦱ||
+ꦲꦸꦧꦶꦉꦧꦸꦱ꧀|boiled tubers||ubi rebus
+ꦲꦸꦪꦃ||
+ꦲꦸꦫꦁ||
+ꦲꦸꦱꦢ||
+ꦲꦺꦥꦺꦴꦭꦸꦱꦶ||
+ꦲꦺꦭꦶꦁ||
+ꦲꦺꦱ꧀ꦠꦿꦶ||
+ꦲꦺꦴꦪꦺꦴꦢ꧀||
+ꦲꦼꦩ꧀ꦥꦿꦶꦠ꧀||
+꧈ꦢꦺ꧈ꦲ꧈ꦌꦭ꧀꧈|DHL|
+
+
+
+
+ꦎꦏ꧀ꦠꦺꦴꦧꦼꦂ|October|
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+`
+
