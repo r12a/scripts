e80f908493428f4bf0da3eb102cf9e367df43c8a
r12a <ishida@w3.org>
2021-10-15 18:02:33 +0100

    bug: Add examples and html vocab file

--- buginese/bug-examples.js
+++ buginese/bug-examples.js
@@ -2,21 +2,63 @@
 
 autoExpandExamples.bug = `
 
-ᨀᨆᨗᨔᨗ|Thursday|kɑmɪːsɪ (kamisi)
-ᨄᨉᨚᨕᨂᨛ|prayers|(paddoangeng)
-ᨅᨔ ᨕᨘᨁᨗ|Buginese language|(basa ugi)
-ᨅᨗᨌᨑ|language|(bicara)
-ᨆᨌ|clever|(maca)
-ᨍᨘᨆ|Friday|(juma)
-ᨒᨄ|lava|(lapa)
-ᨓᨕᨙ|water|(wae)
-ᨔᨈᨘ|Saturday|(satung)
+
+ᨀᨆᨗᨔᨗ|Thursday|kɑmɪːsɪ|kamisi
+ᨄᨉᨚᨕᨂᨛ|prayers||paddoangeng
+ᨄᨒᨚᨈᨑ|lontara expert|paʔlontaraʔ|
+ᨄᨗᨈᨘ|seven|
+ᨅᨔ ᨕᨘᨁᨗ|Buginese language||basa ugi|
+ᨅᨗᨌᨑ|language||bicara
+ᨆᨌ|clever, tiger||maca
+ᨆᨑᨙᨉᨀ|free|
+ᨉᨘᨓ|two|dʊa|dua
+ᨍᨘᨆ|Friday|juma
+ᨒᨄ|lava||lapa
+ᨒᨚᨈᨑ|lontara|lontaraʔ|lontara'
+ᨓᨕᨙ|water||wae
+ᨔᨈᨘ|Saturday|sɑtuŋ|satung
 ᨔᨒᨔ|Tuesday|sɑlɑːsɑ
-ᨕᨆᨑᨙᨉᨀᨂᨛ|democracy|(amaradekangəng)
-ᨕᨑᨅ|Wednesday|(araba)
-ᨕᨔᨘ|dog|(asu)
-ᨕᨙᨔᨙᨊᨙ|Monday|eseneŋ (eseneng)
-ᨕᨛᨔᨚ ᨕᨖᨕᨗ|Sunday|(əso ahai)
+ᨕᨆᨑᨙᨉᨀᨂᨛ|democracy|amaradekaŋəŋ|amaradekangəng
+ᨕᨑᨅ|Wednesday|ɑrɑːbɑ|araba
+ᨕᨔᨘ|dog||asu
+ᨕᨔᨙ|rice plant||ase
+ᨕᨗᨊ|mother|ina|ina
+ᨕᨙᨔᨙᨊᨙ|Monday|eseneŋ|eseneng
+ᨕᨛᨔᨚ ᨕᨖᨕᨗ|Sunday||əso ahai
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 `
 
