3af20310e2b4592a93b728496f57cd268eea2847
r12a <ishida@w3.org>
2021-10-16 10:18:48 +0100

    bali: Final changes to new draft

---
+++ balinese/ban-langdata.js
@@ -0,0 +1,19 @@
+// this should always be updated when app-charuse/langs.js is updated
+
+var langs = {
+
+"ban-bali": { name:"Bali (Balinese)", local:"ᬪᬵᬱᬩᬮᬶ, ᬩᬲᬩᬮᬶ /baʃa bali/", silcode:"ban", 
+source:"ff844379ad4006570a536dd0572148858532d87b", 
+region:"seasia", countries:"Indonesia", script:"bali", speakers:"3300000", 
+letter:"ᬅᬆᬇᬈᬉᬊᬋᬌᬍᬎᬏᬐᬑᬒᬓᬔᬕᬖᬗᬘᬙᬚᬛᬜᬝᬞᬟᬠᬡᬢᬣᬤᬥᬦᬧᬨᬩᬪᬫᬬᬭᬮᬯᬰᬱᬲᬳ", mark:"ᬂᬃᬄ᬴ᬵᬶᬷᬸᬹᬺᬻᬼᬽᬾᬿᭀᭁᭂᭃ᭄", number:"᭐᭑᭒᭓᭔᭕᭖᭗᭘᭙", punctuation:"᭞᭟᭚᭛᭜᭝᭠", aux:"ᬀᬁ", other:"\u200B\u200C", 
+orth:`Balinese. &nbsp; <a href="../scripts/balinese" target="_blank">Details</a>.`,
+also:
+["ban | latn | Used since early 20th century.",
+"ban-java | java | No longer in use."],
+type:"abugida", cs:"no", gpos:"yes", gsub:"yes", cursive:"no", direction:"ltr", wordsep:"no/zwsp", baseline:"mid", wrap:"syllable", hyphenation:"(yes)", hyphen:"᭠", justification:"none",
+vowels:"inh:1 ind:10 vs:11 base:ᬳ/HA pre:2 circ:6 voc:4", clusters:"inv stk cnj", medials:"sj:3", finals:"cm:3 vk:᭄", 
+linked:"balinese", 
+},
+
+
+}
