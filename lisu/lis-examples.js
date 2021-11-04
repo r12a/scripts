fd733ec44e1d2ff7bf731f7280315366f4f51e0d
r12a <ishida@w3.org>
2021-11-04 07:15:59 +0000

    lis: Add examples files.

--- lisu/lis-examples.js
+++ lisu/lis-examples.js
@@ -1,5 +1,59 @@
 autoExpandExamples.lis = `
 
+ꓑꓺ ꓦꓯꓹ|bamboo rat||pa h̃æ̌
+ꓔꓲꓹ|to dye||tǐ
+ꓞꓽ ꓐꓳꓺ|salt||tsʰà bo
+ꓠꓴꓻ|you||nu
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 `
 
