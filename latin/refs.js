1b05fad09efb02d9cd2a199e6ffeb415f59bae2f
r12a <ishida@w3.org>
2021-11-14 11:54:39 +0000

    + latn-bm

--- latin/refs.js
+++ latin/refs.js
@@ -1,4 +1,10 @@
+window.langSet = true
+
+
+
+
 reflist = {
+
 
 
 "c": {
@@ -7,7 +13,44 @@ reflist = {
 "title":"The World's Major Languages",
 "publisher":"Oxford University Press",
 "isbn":"978-1138184824",
+"lang": new Set(['ha']),
+"reviewed":false
 },
+
+"cd": {
+"authors": "Donaldson, Coleman Dupont",
+"title":"Clear Language: Script, Register And The N’ko Movement Of Manding-Speaking West Africa",
+"date": "2017",
+"retr": "Nov 2021",
+"publisher":"Publicly Accessible Penn Dissertations",
+"url":"https://repository.upenn.edu/cgi/viewcontent.cgi?article=4537&context=edissertations",
+"lang": new Set(['bm']),
+"reviewed":false
+},
+
+"cd": {
+"authors": "Donaldson, Coleman Dupont",
+"title":"The An ka taa Bambara/Dioula Dictionary",
+"retr": "Nov 2021",
+"url":"https://dictionary.ankataa.com/about.php",
+"lang": new Set(['bm']),
+"reviewed":false
+},
+
+
+"ckj": {
+"authors": "Kutsch Lojenga, Constance",
+"title":"Orthography and tone: A tone-system typology and its implications for orthography development",
+"in":"Developing orthographies for unwritten languages",
+"by":"Michael Cahill and Keren Rice (eds.)",
+"pages": "49-72",
+"date": "2013",
+"publisher":"SIL International",
+"url":"refs",
+"lang": new Set(['bm']),
+"reviewed":false
+},
+
 
 "d": {
 "url":"#refs",
@@ -15,6 +58,7 @@ reflist = {
 "title":"The World's Writing Systems",
 "publisher":"Oxford University Press",
 "isbn":"0-19-507993-0",
+"lang": new Set(['ha']),
 "reviewed":true
 },
 
@@ -22,36 +66,42 @@ reflist = {
 "url":"http://www.omniglot.com/writing/hausa.htm",
 "authors": "Omniglot",
 "title":"Hausa (Harshen Hausa / هَرْشَن هَوْسَ)",
-"reviewed":true
+"lang": new Set(['ha']),
+"reviewed":false
 },
 
 "p": {
 "url":"http://www.bisharat.net/wikidoc/pmwiki.php/PanAfrLoc/Hausa",
 "authors": "Pan AfriLoc",
 "title":"Hausa - Haoussa - هوس",
+"lang": new Set(['ha']),
 "reviewed":true
 },
 
-"u": {
-"url":"https://www.unicode.org/versions/Unicode13.0.0/ch19.pdf",
-"authors": "Unicode Consortium",
-"title":"The Unicode Standard, Version 13.0",
-"sub":"Chapter 19.6: Africa, Bamum",
-"pages":"779-780",
-"isbn":"978-1-936213-16-0.",
-},
-
-"ul": {
-"url":"https://www.unicode.org/reports/tr14/",
-"authors": "Unicode Consortium",
-"title":"Unicode Line Breaking Algorithm (UAX#14)",
-"reviewed":true
+"vv": {
+"authors": "Valentin Vydrin",
+"title":"Tonal inflection in Mande languages: The cases of Bamana and Dan-Gw taa",
+"date": "2016",
+"retr": "Nov 2021",
+"publisher":"HAL Archives Ouvertes",
+"url":"https://halshs.archives-ouvertes.fr/halshs-01287458/document",
+"lang": new Set(['bm']),
+"reviewed":false
 },
 
 "wha": {
 "url":"https://en.wikipedia.org/wiki/Hausa_language",
 "authors": "Wikipedia",
 "title":"Hausa language",
+"lang": new Set(['ha']),
+},
+
+"wbm": {
+"url":"https://en.wikipedia.org/wiki/Bambara_language",
+"authors": "Wikipedia",
+"title":"Bambara language",
+"lang": new Set(['bm']),
+"retr": "Nov 2021",
 },
 
 
