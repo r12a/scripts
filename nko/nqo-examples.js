76929928c90aca5d7faae6c256d2aac9df11fd8e
r12a <ishida@w3.org>
2021-11-19 19:45:03 +0000

    nqo: Add examples, and brush up text.

--- nko/nqo-examples.js
+++ nko/nqo-examples.js
@@ -10,7 +10,7 @@ autoExpandExamples.nqo = `
 ߛߌ ߺ ߌ ߺ ߛߌ|anyone||si _ i _ si
 ߛߌߘߐ|gram||sidɔ
 ߜߟߊ߬ߗߡߍ߬ߝߘߎ߬ߓߍ߲|cm²||gᵇla²ʧmɛ²fdu²bɛ˜
-ߜߟߊ߬ߥߊ߰ߘߋ߲|millimetre||gᵇla²wa⁶de˜
+ߜߟߊ߬ߥߊ߰ߘߋ߲|millimetre||gbalàwàadẽ||x
 ߞߎߘߍ|kilometer||kudɛ
 ߡߐ߰ ߺ ߐ ߺ ߡߐ߰|everyone||mɔ⁶ _ ɔ _ mɔ⁶
 ߁߂߃߲|123rd||1̣2̣3̣˜
@@ -23,23 +23,23 @@ autoExpandExamples.nqo = `
 
 
 ߓߊ߬ߓߊ߫|father, dad||ba²ba¹
-ߓߙߋ|stick||bre
+ߓߙߋ|stick||bere
 ߓߍ߬ߙߍ|gravel||bɛ²rɛ
 ߛߌ߭|life, lifespan||si³
-ߓߟߏ|arm||blo
+ߓߟߏ|arm||bolo||x
 ߓߐ߬ߟߐ|stake, picket||bɔ²lɔ
-ߡߎ߬ߙߎ|knife||mu²ru
+ߡߎ߬ߙߎ|knife||mùru||x
 ߓߊ|river||ba
 ߗߍ߭|man||ʧɛ³
 ߘߊ߬ߓߊ|hand hoe||da²ba
 ߝߊ߬ߟߌ|donkey||fa²li
 ߜߊ߭|kitchen, hearth||gᵇa³
-ߤߣߍ|peace||hrɛ
+ߤߣߍ|peace||hɛrɛ||x
 ߖߋ߭|melon||ʤe³
 ߞߏ|affair, matter||ko
 ߟߍ߭|pig||lɛ³
 ߡߊ߬ߟߏ|rice||ma²lo
-ߣߐߣߐ|milk||nɔnɔ
+ߣߐߣߐ|milk||nɔnɔ||x
 ߔߊߕߌߛߊߞߣߊ߫|good heavens!||patisakna¹
 ߢߐ߭|millet||ɲɔ³
 ߓߊ߯ߙߊ|work||ba⁵ra
@@ -51,7 +51,7 @@ autoExpandExamples.nqo = `
 ߒ|I||ŋ̩
 ߒߞߏ|N’Ko||ŋ̩ko
 
-ߝ߭ߍߙߌ|glass|véri||extremely marginal phoneme from French (cdl)|x
+ߝ߭ߍߙߌ|glass||véri|extremely marginal phoneme from French (cdl)|x
 ߜߍ߫|white||ɡ͡bɛ||x
 ߜߍ߬ߟߍ߲߬|hard||ɡ͡bɛ̀lɛ̃||x
 ߜߏ߬ߟߏ|skin||ɡ͡bòló||x
@@ -66,6 +66,48 @@ autoExpandExamples.nqo = `
 ߛߑߔߌߣߏߖ߭ߊ߫|Spinoza||spinozá||x
 
 
+
+ߊߡߌ߯ߣߊ߫|amen||amíiná
+ߓߊ߬ߣߊ|rich, wealthy||bàna|
+ߓߊ߯ߙߊ߫|work||báará
+
+
+ߡߊ߬ߡߛߏ߬ߣߍ߲|sister||màmàsònɛ̃
+ߓߊ߲߬ߓߊ|crocodile||bã̀ba||x
+ߓߊ߬ߣߊ߲߬ߞߎ|cassava||bànà̃ku
+ߓߊߙߌߞߊߓߐ|thanks||barikabɔ
+ߓߊ|river||ba
+ߓߊ߭|goat||bǎ||x
+ߓߌߢߍ|liver||biɲɛ
+ߞߌ߬ߢߍ߬ߞߌߢߍ|sand||kìɲɛ̀kiɲɛ
+ߟߋߟߋ|absent||lele||x
+ߝߘߊ߬ߝߌ߲߬ߠߊ߫|Africa||fadàfì̃lá||x
+ߜߐ߱|widow||gbòó||x
+ߘߎ߱|city||dùú||x
+ߓߎ߱|wigwam||bùú||x
+ߞߏ߬ߝߊ߮|bath||kòfáà||x
+ߖߌ߰ߖߊ߮|because||jìjáà||x
+ߢߌ߲ߞߏߛߍ߮|toothpaste||ɲĩkosɛ́ɛ̀||x
+ߢߊߟߐ߲ߓߊ߮|expert||ɲalɔ̃báà||x
+ߞߎ߬ߙߊ߲߬ߓߊ߮|person||kùrã̀báà||x
+ߡߌ߲߭|asteroid|mǐ̃||x
+ߔߊ߬ߔߘߊ|menu||pàpada||x
+ߕߊߕߍߟߊ߲|brazier||tatɛlã||x
+ߘߊߘߐߞߍ|ambiguity||dadɔkɛ||x
+ߞߊߞߊߞߊ߫|to laugh||kakaká||x
+ߜߊߜߊߟߌ|incision||gbagbali||x
+ߖߛߎ߬ߘߐ߬ߗߍ߫|whisper||jùsùdɔ̀cɛ́||x
+ߝߊߝߊ|cave||fafa||x
+ߛߎߛߋߟߡߊ|greyish||suselma||x
+ߤߊߤߊߣߍ߲|panting||hahanɛ̃||x
+ߛߌߣߊߣߍ߲|gazelle||sinanɛ̃||x
+ߢߐ߯ߡߍ|camel||ɲɔ́ɔmɛ||x
+ߥߏ߲ߥߏ߲ߠߌ|bark||wõwõli||x
+ߥߋ߬ߟߋ߲߬ߥߋߟߋ߲|acrobatics||wèlẽ̀welẽ||x
+ߒߞߏ|N'ko||n'ko||x
+ߡߊ߰ߒߠߊ|contact||màn'la||x
+ߓߏ߲߬ߧߊ|honour||bõ̀ɲa||x
+ߘߏ߲߬ߠߌ߲|access||dõ̀lĩ||x
 
 
 
