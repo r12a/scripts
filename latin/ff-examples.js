487fbf84e4d9ee8d83dfb43e1aa8ae33a6e8d2df
r12a <ishida@w3.org>
2021-11-26 08:28:30 +0000

    + Fulah

---
+++ latin/ff-examples.js
@@ -0,0 +1,503 @@
+
+autoExpandExamples.ff = `
+
+
+-ɓe||
+-ɗe||
+-ɗi||
+Boɗaaɗo||
+Fulfulde|Fulfulde|fulfulde||Pular, Pulaar; 𞤊𞤵𞤤𞤬𞤵𞤤𞤣𞤫
+Haalpulaar||
+Motti||
+Pulaar||
+Pular||
+Pullo||
+Teenengu||
+aad-||
+aada||
+aaden||
+aadi||
+aafiya||
+aahiijo||
+aala||
+aali||
+aanyeere||
+aan||
+aaw-||
+aawdi|seed|aːwdi||Fulfulde, Pulaar, Pular
+aawoowo||
+aawugol||
+abadan|never|abadan||Pulaar
+abada|never|abada||Western Niger Fulfulde
+acaari||
+acc-||
+accaari||
+accitugol||
+adadu|total, sum|adadu
+adala||
+adugol||
+adunaaru||
+alaadu||
+alaa||
+aljam||
+am||
+annabi||
+an||
+arma||
+arɗo||
+asama||
+asangal||
+awdi|seed|awdi||Pular
+awotooɗo||
+awru||
+a||
+aɗa||
+baaba|father|baːba|||x
+baafata||
+baagol||
+baaso||
+baharu||
+baka||
+bakkaatu||
+balanngaaru||
+ballal||
+bappoore||
+bataake||
+bayillo||
+ba||
+bee||
+bifi||
+bimmbi||
+binndi||
+binndo||
+bolol||
+bolondaa||
+bufenri||
+buguuru|grass hut|buguːru
+bukkaaru||
+bullal||
+buuru|bread, hyena|buːru
+buuruuje|hyenas|buːruːɟe
+carmalal|twig for fire|carmalal||Pular
+cellal|health, wellbeing|cellal||Pular
+coggal||
+coggu|price|coggu|||x
+collaari||
+conndi||
+cukalel||
+cuuram||
+daande||
+dakkere||
+dankali||
+dankata||
+danki|bed|danki|||x
+dannaajo||
+debbo||
+deftere||
+dewtere||
+diidol|design, line|diːdol||Pular
+diisagol||
+diiwal||
+dolokke||
+donsanke||
+duroowo||
+enen||
+en||
+esiraawo|in-law|esiraːwo|||x
+eɓe||
+eɗen||
+fattorde|refuge|fattorde||Pular
+fayande|cooking pot|fajaⁿde|||x
+feccere|part|feccere||Fulfulde, Pulaar, Pular
+feere||
+feewugol||
+fii honɗum||
+fiɲaare|type of fish|fiɲaːre||Maasina Fulfulde
+follere||
+fow||
+ful-||
+gaanin||
+gatal|prayer mat|gatal|||x
+gawri||
+geɓal||
+giggol||
+gimɗol||
+gimɗo||
+goʼo|one|ɡoʔo||Pular
+go'o|one|ɡoʔo||Pular
+golle||
+golloowo||
+goofoondi||
+goonga|truth|ɡoːŋɡa||Pular
+gooto||
+gootun||
+gorko|husband, man|gorko||Pular
+goro||
+haako||
+haal-||
+haala||
+haande||
+haa||
+hakkundeejo||
+hakkunde||
+halfeere||
+hanki|yesterday|hanki|||x
+haray||
+harfeere||
+harfu||
+hayre||
+heew-||
+hello||
+himo||
+himɓe||
+hinaa||
+hiɓe||
+hiɗa||
+hiɗen||
+hommbo||
+honto||
+hooreejo||
+hoore||
+hoorugol||
+huccere||
+hunnduko||
+huntere||
+huutoral||
+innde|name|inⁿde|||x
+jahdodinal|agreement|jahdodinal||Pular
+jalbugol||
+jannde||
+janngirde||
+janngo||
+jappaajo|leather worker, cobbler|ɟappaːɟo
+jawdi||
+jawleeru||
+jaŋgurdu||
+jeegom||
+jeenayi||
+jeetati||
+jeeɗiɗi||
+jetti||
+jeɗɗi||
+joyi||
+jukka||
+junngo|hand, arm|ɟuŋɡo
+juŋŋgo||
+kaaɗo||
+kahi||
+kala||
+kammu|sky|kammu||Pular
+kamɓe||
+kaybonru||
+kaŋko||
+keerol|border, frontier|keːrol||Pular
+kemmbu||
+kewal||
+kiikiiɗe||
+ki||
+kono|but, however|kono||Pular
+kooho|lord, chief|koːho
+koohooɓe|lords, chiefs|koːhoːɓe
+koosaajo||
+kosam|milk|kosam||Fulfulde, Pulaar, Pular...
+ko||
+koŋŋgol|word|koŋŋgol||Pular
+kudaku||
+kunnawal||
+kuuran||
+la'aaje||
+laalawal||
+laamɗo||
+laawol||
+labbo||
+lacceeri|sorghum|lacceːri||Nigerian Fulfulde
+lacciri||
+lamm-||
+lammbe||
+lamɗam||
+lanɗam||
+law||
+laɓi||
+laɓndal||
+leembol||
+leggal||
+lekki|tree|lekki
+lesdi||
+lewru||
+leydi||
+liinga||
+liingu||
+lingu||
+loot-||
+lowre||
+maabo||
+maaro|rice|maːro||Pular
+mahoowo||
+majjere||
+maleyka||
+marke||
+marɗo||
+masaku||
+maw-||
+mawniraawo||
+mawniraaɗo||
+mawɗo|old person, adult|mawɗo||Fulfulde, Pulaar, Pular...
+mbaroodi||
+mbayeere||
+mbayla||
+mbedda||
+mbeewa|goat|ᵐbeːwa||Fulfulde, Pulaar, Pular...
+mbelndi||
+mbolo||
+mboɗ-||
+mbusiri||
+menen||
+men||
+meɗen||
+miin||
+minen||
+min||
+mi||
+miñiraaɗo||
+miɗo||
+moƴƴere|goodness, happiness|moʔʔʲere||Pular
+musel||
+naange||
+nagge||
+nantinoore|translation, interpretation|nantinoore||Pular
+nantinoowo|translator, interpreter|nantinowo||Pular
+nayeejo||
+nayi||
+nayo||
+nder||
+ndiyam||
+ndi||
+ndowru|mouse|ⁿdowɾu|||x
+nduggu||
+nduungu||
+ndu||
+nebbam|oil, fat|nebbam
+neesila||
+neɗɗo||
+ngalu||
+ngam||
+ngayka||
+ngaynaako||
+ngayuuri|lion|ᵑɡajuːri|||x
+ngelooba||
+ngel||
+ngesa||
+nge||
+ngol||
+nguufo||
+nguurndam||
+ngu||
+njaatigi|friend|ⁿd͡ʒaːtigi|||x
+njanndi||
+njoyo|five|ⁿd͡ʒojo
+nofuru||
+no||
+nyayiiri||
+omo||
+onon||
+oɗon||
+o’owooye|open|oʔowoːje|||x
+paggiri||
+peewal|integrity, honour|peːwal||Pular
+piindi||
+piɓol||
+poofirgel||
+puumtugol|pronunciation|puːmtugol||Pular
+puyɗo|lazy person|pujɗo|||x
+qe||
+reedu|stomach|reːdu||𞤪𞤫𞥅𞤣𞤵|x
+rew-||
+roogo||
+ruuhu||
+saare|village, town|saːre
+sababu||
+sagg-||
+saggitorde||
+salli||
+sappo||
+sawru||
+saññoowo||
+seremmbe||
+sogg-||
+sondu||
+soora||
+soɗo||
+suudu-depiteeɓe||
+suudu||
+suuperefee||
+suur-||
+suuɗ-||
+tabalde||
+tafugol||
+tagudi||
+takko||
+takuure||
+tan||
+tasakuyee||
+taskagol|to research, analyse|taskagol||Pular
+tati|three|tati||Fulfulde, Pular, Pulaar
+tato||
+tawsaajo||
+teen-||
+teewu|meat|||𞤼𞤫𞥅𞤱𞤵
+tijjagol|to count on, hope for|tiɟɟagol||Pular
+tinndol||
+ton||
+toyoori||
+to||
+toɓɓere||
+toɗɗagol||
+tuggordu||
+tulde||
+tuma|when|tuma||Pular
+tun||
+unirgal|pestle|unirgal|x
+waande||
+waandu||
+waare|hay bale, beard|waːre
+waawugol|to defeat, conquer|waːwugol||Pular
+waañoowo|hunter|waːñoːwo||Pular
+wahabiyyaajo||
+wal-||
+walaa||
+wall-||
+wallif-||
+warrugol||
+waɓɓugo|cheek|waɓɓugo||Pular
+winnd-||
+wonaa||
+wonande||
+wonnde||
+woodugol||
+wor-||
+woyndu|well|wojⁿdu|||x
+woɗ-||
+wuumre||
+wuur-||
+wuɓere||
+yamiroore||
+yar-||
+yarirko||
+yeeso|face|yeːso|||x
+ye||
+yiite||
+yimɓe||
+yitere||
+ñaakii||
+ñaalal||
+ñaam-||
+ñaari|cat|ɲaːri|||x
+ɲaari|cat|ɲaːri|||x
+nyaari|cat|ɲaːri|||x
+ñalaande||
+ñallal||
+ñawu||
+ñayko||
+ŋari|beauty|ŋari
+ƴanngal||
+ƴi'al|bone|jiʔal|
+ƴiire||
+ƴiiƴam|blood|j̰iːj̰am
+ƴiʼal|bone|jiʔal|||ƴi'al
+ƴulɓere||
+ɓaawo||
+ɓalal|foundation, base|ɓalal||Pular
+ɓanndu||
+ɓantaare||
+ɓaŋagol|to leave, go out|ɓaŋagol||Pular
+ɓernde||
+ɓeyngu|wife, motherhood|ɓejᵑɡu
+ɓe||
+ɓii||
+ɓiraaɗan||
+ɓiɗɗo|child, offspring|ɓiɗɗo||Pular
+ɓoggol||
+ɓokki||
+ɓooldi||
+ɓuteeru||
+ɗaaleehi||
+ɗaa||
+ɗam||
+ɗatal|path, route|ɗatal
+ɗaɗol|root, nerve, artery|ɗaɗol||Pular
+ɗemŋgal|language, tongue|ɗemᵑɡal||Fulfulde, Pulaar, Pular
+ɗi||
+ɗiɗi|two|ɗiɗi||Fulfulde, Pular, Pulaar
+ɗiɗo||
+ɗo||
+ɗuuɗ-||
+
+
+
+
+
+jango|tomorrow|jaŋgo||Sene-gambian Fula|x
+jaango|cold|jaːŋgo||Sene-gambian Fula|x
+
+tuttogol|to spit|tuttogol|||x
+tutogol|to plant a tree|tutogol|||x
+
+qalamu|digit, number|qalamu||Pular (Arab)|x
+qaburu|tomb|qaburu||Pular (Arab)|x
+xayru|charm, beauty|xajru||Pular (Arab)|x
+xalwa|pious retreat|xalwa||Pular (Arab)|x
+zawju|husband|zawɟu/ɟawɟu||Pular (Arab)|x
+zawjatu|wife|zawɟatu||Pular (Arab)|x
+zuwerjo|(football) player|zuwerɟo||Pular|x
+zeneral|general (Mil)|zeneral/seneral||Pular #seneral#|x
+seneral|general (Mil)|seneral||Pular|x
+shahaada|muslim expression of faith|ʃahaːda||Pular (Arab)|x
+shahiidi|martyr|ʃahiːdi||Pular (Arab)|x
+shari'a|Sharia|ʃariʔa||Pular (Arab), #sariya#|x
+sariya|Sharia|ʃariʔa||Pular (Arab)|x
+shukru|thanks|ʃukru||Pular (Arab)|x
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+`
+
