b516a3640ec8b33af5e717c0de5e1e2e65437dfc
r12a <ishida@w3.org>
2021-11-28 11:09:34 +0000

    latin: Reinstate missing refs

--- latin/refs.js
+++ latin/refs.js
@@ -1,4 +1,4 @@
-window.langSet = false
+window.langSet = true
 
 
 
@@ -6,12 +6,24 @@ window.langSet = false
 reflist = {
 
 
-"fsi": {
-"authors": "Foreign Service Institute",
-"title":"Fula Basic Course",
+"c": {
+"url":"#refs",
+"authors": " Bernard Comrie",
+"title":"The World's Major Languages",
+"publisher":"Oxford University Press",
+"isbn":"978-1138184824",
+"lang": new Set(['ha']),
+"reviewed":false
+},
+
+"cd": {
+"authors": "Donaldson, Coleman Dupont",
+"title":"Clear Language: Script, Register And The N’ko Movement Of Manding-Speaking West Africa",
+"date": "2017",
 "retr": "Nov 2021",
-"publisher":"Live Lingua",
-"url":"https://www.livelingua.com/course/fsi/fula_basic_course",
+"publisher":"Publicly Accessible Penn Dissertations",
+"url":"https://repository.upenn.edu/cgi/viewcontent.cgi?article=4537&context=edissertations",
+"lang": new Set(['bm']),
 "reviewed":false
 },
 
@@ -22,16 +34,112 @@ reflist = {
 "date": "2000",
 "retr": "Nov 2021",
 "url":"http://www.ibamba.net/pular/manual.pdf",
+"lang": new Set(['ff']),
+"reviewed":false
+},
+
+"cdl": {
+"authors": "Donaldson, Coleman",
+"title":"Linguistic and Civic Refinement in the N'ko Movement of Manding-Speaking West Africa",
+"date": "2019",
+"retr": "Nov 2021",
+"publisher":"University of Chicago Press Journals",
+"url":"https://www.journals.uchicago.edu/doi/10.1086/702554",
+"lang": new Set(['bm']),
+"reviewed":false
+},
+
+"cdc": {
+"authors": "Donaldson, Coleman Dupont",
+"title":"The An ka taa Bambara/Dioula Dictionary",
+"retr": "Nov 2021",
+"url":"https://dictionary.ankataa.com/about.php",
+"lang": new Set(['bm']),
 "reviewed":false
 },
 
 
+"d": {
+"url":"#refs",
+"authors": "Peter T. Daniels and William Bright",
+"title":"The World's Writing Systems",
+"publisher":"Oxford University Press",
+"isbn":"0-19-507993-0",
+"lang": new Set(['ha']),
+"reviewed":true
+},
+
+
+"fsi": {
+"authors": "Foreign Service Institute",
+"title":"Fula Basic Course",
+"retr": "Nov 2021",
+"publisher":"Live Lingua",
+"url":"https://www.livelingua.com/course/fsi/fula_basic_course",
+"lang": new Set(['ff']),
+"reviewed":false
+},
+
+"gd": {
+"authors": "Dumestre, Gérard",
+"title":"Grammaire fondamental du Bambara",
+"date": "2003",
+"publisher":"Éditions Karthala",
+"isbn":"978-2-84586402-3",
+"lang": new Set(['bm']),
+"reviewed":false
+},
+
+
+"ckj": {
+"authors": "Kutsch Lojenga, Constance",
+"title":"Orthography and tone: A tone-system typology and its implications for orthography development",
+"in":"Developing orthographies for unwritten languages",
+"by":"Michael Cahill and Keren Rice (eds.)",
+"pages": "49-72",
+"date": "2013",
+"publisher":"SIL International",
+"url":"refs",
+"lang": new Set(['bm']),
+"reviewed":false
+},
+
+
+"o": {
+"url":"http://www.omniglot.com/writing/hausa.htm",
+"authors": "Omniglot",
+"title":"Hausa (Harshen Hausa / هَرْشَن هَوْسَ)",
+"lang": new Set(['ha']),
+"reviewed":false
+},
+
 "p": {
+"url":"http://www.bisharat.net/wikidoc/pmwiki.php/PanAfrLoc/Hausa",
+"authors": "Pan AfriLoc",
+"title":"Hausa - Haoussa - هوس",
+"lang": new Set(['ha']),
+"reviewed":true
+},
+
+"paf": {
 "url":"http://www.bisharat.net/wikidoc/pmwiki.php/PanAfrLoc/Fula",
 "authors": "Pan AfriLoc",
 "title":"Fula",
 "sub": "Fula - Peul - Fulfulde - Pulaar - Pular",
+"lang": new Set(['ff']),
 "reviewed":true
+},
+
+
+"vv": {
+"authors": "Valentin Vydrin",
+"title":"Tonal inflection in Mande languages: The cases of Bamana and Dan-Gw taa",
+"date": "2016",
+"retr": "Nov 2021",
+"publisher":"HAL Archives Ouvertes",
+"url":"https://halshs.archives-ouvertes.fr/halshs-01287458/document",
+"lang": new Set(['bm']),
+"reviewed":false
 },
 
 "wfa": {
@@ -39,7 +147,15 @@ reflist = {
 "title":"Fula alphabets",
 "retr": "Nov 2021",
 "url":"https://en.wikipedia.org/wiki/Fula_alphabets",
+"lang": new Set(['ff']),
 "reviewed":false
+},
+
+"wha": {
+"url":"https://en.wikipedia.org/wiki/Hausa_language",
+"authors": "Wikipedia",
+"title":"Hausa language",
+"lang": new Set(['ha']),
 },
 
 "wfl": {
@@ -47,11 +163,21 @@ reflist = {
 "title":"Fula language",
 "retr": "Nov 2021",
 "url":"https://en.wikipedia.org/wiki/Fula_language",
+"lang": new Set(['ff']),
 "reviewed":false
+},
+
+"wbm": {
+"url":"https://en.wikipedia.org/wiki/Bambara_language",
+"authors": "Wikipedia",
+"title":"Bambara language",
+"lang": new Set(['bm']),
+"retr": "Nov 2021",
 },
 
 
 }
+
 
 
 /*
@@ -69,6 +195,5 @@ reflist = {
 "reviewed":false
 },
 */
-
 
 
