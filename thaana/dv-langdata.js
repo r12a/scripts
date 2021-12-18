f55331cd9562e9de8ffd4ef89f3629aad2dd6591
r12a <ishida@w3.org>
2021-12-18 16:06:33 +0000

    dv: Apply latest template #96.  Add term base.

---
+++ thaana/dv-langdata.js
@@ -0,0 +1,18 @@
+// this should always be updated when app-charuse/langs.js is updated
+
+var langs = {
+
+"dv": { name:"Dhivehi (Maldivian, Divehi)", local:"ދިވެހި [d̪iˈʋehi], ދިވެހިބަސް (dhivehi-bas)", silcode:"div", rtl:true, 
+source:"udhr_div,e9188c572c9c158255cede2459f076ecb3b50b9f", 
+region:"sasia", countries:"Maldives", script:"thaa", speakers:"340000", 
+letter:"ޑސމބރގއދޖލހޢނފކށވޙޤތޕޓޔޝޞޅޚޣޒޠޗޏޘޛޟޜޡޥޱ", mark:"ިެްަީުާޮޭޫޯ", punctuation:"،؛", 
+orth:`[Thaana] &nbsp;  <a href="../scripts/thaana/" target="_blank">Details</a>.`,
+also:
+["dv-latn | latn | ‘Maldivian Latin’."],
+type:"alphabet", cs:"no", gpos:"no", gsub:"no", cursive:"no", direction:"rtl", wordsep:"space", baseline:"mid", wrap:"word", hyphenation:"?",  justification:"sp",
+vowels:"cm:10 base:އ/A", clusters:"mrk",
+linked:"thaana", fonts:"/pickers/thaa/", 
+},
+
+
+}
