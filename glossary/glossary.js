0a6805e4f8e84a8eeaa44f9b3ef10fb8ed1c0fe2
r12a <ishida@w3.org>
2022-04-14 12:19:09 +0100

    glossary: +CSS +typographic unit

--- glossary/glossary.js
+++ glossary/glossary.js
@@ -3,23 +3,25 @@ defList={
 
 "circumgraph": 'When a single vowel-sign code point produces glyphs on more than one side of the consonant base, it is referred to as a circumgraph.',
 
-"compositevowel": 'A <a class="termref" target="_blank" href="../glossary/#compositevowel">composite vowel</a> is a single vowel sound or diphthong that is represented by more than one code point from the set of vowel-signs, repurposed consonants, and diacritics available.',
+"compositevowel": 'A <a class="termref" target="_blank" href="../glossary/index.html#compositevowel">composite vowel</a> is a single vowel sound or diphthong that is represented by more than one code point from the set of vowel-signs, repurposed consonants, and diacritics available.',
 
-"conjunct": 'A <a class="termref" target="_blank" href="../glossary/#conjunct">conjunct</a> is a consonant cluster where the lack of intervening vowels is indicated by one or more of stacking, changing and merging the shapes of the constituent letter forms (usually in abugidas). Not all consonant clusters are displayed as conjuncts.',
+"conjunct": 'A <a class="termref" target="_blank" href="../glossary/index.html#conjunct">conjunct</a> is a consonant cluster where the lack of intervening vowels is indicated by one or more of stacking, changing and merging the shapes of the constituent letter forms (usually in abugidas). Not all consonant clusters are displayed as conjuncts.',
 
 "consonantcluster": 'A consonant cluster is a sequence of consonants with no intervening vowels.',
 
 "contourtonelanguage": 'A contour tone language has patterns where the pitch moves up and down over the course of a syllable. (Compare with register tone languages.)',
 
-"grapheme": `A <a class="termref" target="_blank" href="../glossary/#grapheme">grapheme</a> is a user-perceived unit of text. Text operations that use graphemes as a unit of text include line-breaking, forwards deletion, cursor movement & selection, character counts, text spacing, text insertion, justification, case conversions, and sorting.  The Unicode Standard uses generalised rules to define \'<a class="termref" target="_blank" href="../glossary/#graphemecluster">grapheme clusters</a>\', which approximate the likely grapheme boundaries in a writing system, however they don't work well with many complex scripts.`,
+"ccs": `A <a class="termref" target="_blank" href="../glossary/index.html#ccs">CSS (combining character sequence)</a> includes a base (consonant, vowel, or digit) plus all the combining marks plus ZWJ and ZWNJ that are attached to it. When a CCS contains certain characters (such as viramas), multiple CCSs may combine to make a <a class="termref" target="_blank" href="../glossary/index.html#typographicunit">typographic unit</a>, which is a unit of text that is not split during a particular text operation. To simplify usage, this document includes a base with no following characters in the definition of CSS.`,
+
+"grapheme": `A <a class="termref" target="_blank" href="../glossary/index.html#grapheme">grapheme</a> is a user-perceived unit of text. Text operations that use graphemes as a unit of text include line-breaking, forwards deletion, cursor movement & selection, character counts, text spacing, text insertion, justification, case conversions, and sorting.  The Unicode Standard uses generalised rules to define \'<a class="termref" target="_blank" href="../glossary/index.html#graphemecluster">grapheme clusters</a>\', which approximate the likely grapheme boundaries in a writing system, however they don't work well with many complex scripts.`,
 
 "hyphenation": '\'Hyphenation\' here refers to an extra set of rules applied after the basic line-break algorithm to split words at syllable or morphological boundaries in order to improve the layout of a paragraph. Hyphenation may or may not be indicated using a visual marker at the end or start of a line, however it is commonly marked by a hyphen or other glyph.',
 
-"ijam": 'An <a class="termref" target="_blank" href="../glossary/#ijam">ijam</a> is a diacritical mark considered to be part of a basic letter form. Unicode encodes letter+ijam combinations as separate, atomic characters, which are never given decompositions in the standard. Compare with <a class="termref" target="_blank" href="../glossary/#tashkil">tashkil</a>.',
+"ijam": 'An <a class="termref" target="_blank" href="../glossary/index.html#ijam">ijam</a> is a diacritical mark considered to be part of a basic letter form. Unicode encodes letter+ijam combinations as separate, atomic characters, which are never given decompositions in the standard. Compare with <a class="termref" target="_blank" href="../glossary/index.html#tashkil">tashkil</a>.',
 
-"independentvowel": 'An <a class="termref" target="_blank" href="../glossary/#inherentvowel">inherent vowel</a> is a vowel sound that is automatically pronounced after a consonant letter, unless specifically suppressed.',
+"independentvowel": 'An <a class="termref" target="_blank" href="../glossary/index.html#inherentvowel">inherent vowel</a> is a vowel sound that is automatically pronounced after a consonant letter, unless specifically suppressed.',
 
-"inherentvowel": 'An <a class="termref" target="_blank" href="../glossary/#inherentvowel">inherent vowel</a> is a vowel sound that is automatically pronounced after a consonant letter, unless specifically suppressed.',
+"inherentvowel": 'An <a class="termref" target="_blank" href="../glossary/index.html#inherentvowel">inherent vowel</a> is a vowel sound that is automatically pronounced after a consonant letter, unless specifically suppressed.',
 
 "justification": 'The essence of justification (unlike letter-spacing) is that text is arranged to fit within a given distance, usually a line width.',
 
@@ -31,13 +33,15 @@ defList={
 
 "standalonevowel": 'Standalone vowels are not preceded by a consonant sound, and may appear at the beginning or in the middle of a word.',
 
-"tashkil": 'A <a class="termref" target="_blank" href="../glossary/#tashkil">tashkil</a> is a mark that indicates vocalization of text, as well as other types of phonetic guides to correct pronunciation. They are separately encoded as combining marks. A basic Arabic letter plus any of these types of marks is never encoded as a separate, precomposed character, but must always be represented as a sequence of letter plus combining mark. Compare with <a class="termref" target="_blank" href="../glossary/#ijam">ijam</a>.',
+"tashkil": 'A <a class="termref" target="_blank" href="../glossary/index.html#tashkil">tashkil</a> is a mark that indicates vocalization of text, as well as other types of phonetic guides to correct pronunciation. They are separately encoded as combining marks. A basic Arabic letter plus any of these types of marks is never encoded as a separate, precomposed character, but must always be represented as a sequence of letter plus combining mark. Compare with <a class="termref" target="_blank" href="../glossary/index.html#ijam">ijam</a>.',
+
+"typographicunit": `A <a class="termref" target="_blank" href="../glossary/index.html#typographicunit">typographic unit</a> is a unit of text that is not normally split during a particular text operation. The actual constituents of a typographic unit vary according to the process being applied to the text. For example, in complex scripts cursor movement may use smaller typographic units than hyphenation for the same text.`,
 
 "unchecked": '\'Checked\' means ending in the sound <span class="ipa">-p</span>, <span class="ipa">-t</span>, <span class="ipa">-k</span>, or <span class="ipa">-ʔ</span>. \'Unchecked\' means ending in  sonorants <span class="ipa">m, n, ŋ, w, j</span> or vowels.</p>',
 
 "vocalic": 'Vocalics are letters derived from Sanskrit that generally behave like vowels, but represent r/l followed by a vowel. They are often available both as vowel-signs and independent vowel letters.',
 
-"vowelsign": '<a class="termref" target="_blank" href="../glossary/#vowelsign">Vowel-signs</a> are shapes that appear alongside a consonant to express a following (non-inherent) vowel sound.  These are known as <span class="name">\'matras\'</span> in Sanskrit. Sometimes multiple shapes are used, on various sides of the base consonant (circumgraphs). ',
+"vowelsign": '<a class="termref" target="_blank" href="../glossary/index.html#vowelsign">Vowel-signs</a> are shapes that appear alongside a consonant to express a following (non-inherent) vowel sound.  These are known as <span class="name">\'matras\'</span> in Sanskrit. Sometimes multiple shapes are used, on various sides of the base consonant (circumgraphs). ',
 
 "wordboundary": 'The concept of \'word\' is difficult to define in any language (see <a href="https://www.w3.org/International/articles/typography/linebreak.en#whatisword">What is a word?</a>). Here, a word is a vaguely-defined, but recognisable  semantic unit that is typically smaller than a phrase and may comprise one or more syllables.',
 }
