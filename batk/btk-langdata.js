3ca461acb871af28bc897ee375065ddc6a670add
r12a <ishida@w3.org>
2022-05-06 08:22:25 +0100

    + batk

---
+++ batk/btk-langdata.js
@@ -0,0 +1,16 @@
+// this should always be updated when app-charuse/langs.js is updated
+
+var langs = {
+
+"btk": { name:"Batak (gen)", local:"ᯘᯮᯒᯖ᯲ ᯅᯖᯂ᯲", 
+region:"seasia", countries:"Indonesia", script:"batk", speakers:"5,665,000", 
+letter:"ᯂᯃᯄᯅᯆᯇᯈᯉᯋᯌᯍᯎᯏᯐᯑᯒᯓᯔᯕᯖᯗᯘᯙᯚᯛᯜᯝᯞᯟᯠᯡᯢᯣᯀᯁᯤᯥ", mark:"ᯧᯨᯩᯪᯫᯬᯭᯮᯯᯰᯱ᯦᯲᯳", punctuation:"᯼᯽᯾᯿", 
+orth:`[Batak] Covers a superset of usage for Mandailing (btm), Simalungun (bts), Toba (bbc), Karo (btx), & Pakpak (Dairi) (btd). Individual languages each use a subset of the characters listed here.`,
+notes:`btk represents a collection of languages.`,
+type:"abugida", cs:"no", gpos:"yes", gsub:"yes", cursive:"no", wordsep:"no/zwsp", baseline:"mid", wrap:"syllable", hyphenation:"?",  direction:"ltr",  justification:"?", 
+vowels:"vs:9 ind:3 base:A",  finals:"cm:2", 
+linked:"batk", fonts:"batk", 
+},
+
+
+}
---
+++ batk/refs.js
@@ -0,0 +1,113 @@
+window.langSet = false
+
+
+
+reflist = {
+
+
+/*"auth_initials": {
+"authors": "Sort_by_family_name_of_first_contributor",
+"title":"The_Worlds_Writing_Systems",
+"sub": "subtitle",
+"date": "1996",
+"retr": "May 2021",
+"publisher":"Oxford_University_Press",
+"isbn":"0-19-507993-0",
+"pages": "xxx",
+"url":"refs",
+"lang": new Set(['ru']),
+"reviewed":false
+},
+*/
+
+"ab": {
+"authors": "",
+"title":"Batak Surat",
+"retr": "Apr 2022",
+"publisher": "",
+"url":"https://aksara-batak.com/",
+"reviewed":true
+},
+
+"d": {
+"authors": "Peter T. Daniels and William Bright",
+"title":"The World's Writing Systems",
+"date": "1996",
+"publisher":"Oxford University Press",
+"isbn":"0-19-507993-0",
+"url":"#refs",
+"reviewed":false
+},
+
+"ek": {
+"authors": "Michael Everson and Uli Kozok",
+"title":"Proposal for encoding the Batak script in the UCS",
+"date": "2008",
+"url":"http://std.dkuug.dk/jtc1/sc2/wg2/docs/n3320.pdf",
+"reviewed":false
+},
+
+"nl": {
+"authors": "Norbert Lindenbert",
+"title":"Constructing fonts for the Batak script",
+"date": "2020",
+"url":"https://lindenbergsoftware.com/en/notes/constructing-fonts-for-batak/",
+"reviewed":false
+},
+
+"u": {
+"authors": "Unicode Consortium",
+"title":"The Unicode Standard, Version 14.0",
+"sub":"Chapter 17.6: Indonesia and Oceania",
+"pages":"718-719",
+"isbn":"978-1-936213-29-0",
+"url":"https://www.unicode.org/versions/Unicode14.0.0/ch17.pdf",
+"reviewed": true
+},
+
+"uk": {
+"authors": "Uli Kozok",
+"title":"Sejarah Aksara Batak",
+"url":"https://ulikozok.com/aksara-batak/sejarah-aksara-batak/",
+"reviewed":false
+},
+
+"wsi": {
+"authors": "Wikipedia",
+"title":"Surat Batak",
+"retr": "May 2022",
+"url":"https://id.wikipedia.org/wiki/Surat_Batak",
+"reviewed":true
+},
+
+"ws": {
+"authors": "Wikipedia",
+"title":"Batak script",
+"retr": "Apr 2022",
+"url":"https://en.wikipedia.org/wiki/Batak_script",
+"reviewed":true
+},
+
+
+}
+
+
+
+
+scriptLanguageList = {
+akb:{name:"Angkola"},
+btd:{name:"Dairi"},
+btx:{name:"Karo", url:"index"},
+btm:{name:"Mandailing", url:"index"},
+bts:{name:"Simalungun", url:"index"},
+bbc:{name:"Toba", url:"index"},
+}
+
+
+
+
+
+
+
+
+
