4f44cfdbfc74e4be32ba63e454052969b449abd5
r12a <ishida@w3.org>
2023-01-02 17:05:26 +0000

    2nd bank of lana v29

---
+++ lana/block-globals.js
@@ -0,0 +1,9 @@
+window.version = 'v29'
+window.scriptName = 'Tai Tham'
+window.ghDirectory = 'lana'
+window.blockDirectory = 'lana'
+window.languageList = ['kkh','nod'] // lang tags for each orthography handled
+window.detailsfileList = ['kkh','nod'] // first part of xx-details for each orthography
+window.scriptISO = 'lana'
+window.webfonts = ["Hariphunchai WF", "Lamphun WF", "Noto Serif Tai Tham WF", "Lanna Alif WF", "A Tai Tham KH New WF"]
+
---
+++ lana/nod-globals.js
@@ -0,0 +1,67 @@
+langTag='nod' // BCP47 language tag for this orthography
+scriptTag='lana' // BCP47 script tag for this orthography
+notesLangtag='nod'
+blockLangtag = 'nod' // same as langTag; used in block.html
+blockDirection=''  // ltr/rtl or '' for ltr; used by block.html
+
+blockDirectoryName='lana'  // folder name where block file is found, no slash
+orthogFilePath='lana/nod'  // path below /scripts, including filename, but no extension, eg. lana/nod
+pickerDir='lana-nod'  // directory name for picker, no slash at end
+scriptSummaryTableName='nod'  // id used for langs record in xx-langdata.js; used to make summary table
+charUsageBCP='nod'  // bcp tag to find the orthography in the charuse app; used in makeIndexIntro only
+orthogName = 'Lanna' // natural language name for the orthography; used only in makeIndexIntro
+languageName = 'Northern Thai' // natural language name of language; used in various places, esp block
+scriptIsIndic = true  // true if there are indic syllabic categories; used only in makeIndexIntro
+
+
+defaultSize = '24px'  // used by block.html to set size of font, including in font mgt panel
+letterClassName = 'nod'  // classname for a given orthography in the block page
+
+
+
+
+
+
+// the following don't seem to be used
+pageWebfonts = ["Hariphunchai WF", "Noto Serif Tai Tham WF", "Lamphun WF", "Lanna Alif WF", "A Tai Tham KH New WF"]  //  doesn't seem to be used
+fontSwitchLangs = 'nod,kkh'
+lang = 'nod'
+direction = ''
+
+
+
+/*
+langTag='nod'
+scriptTag='lana'
+notesLangtag='nod'
+
+blockDirectoryName='lana'  // folder name where block file is found, no slash
+orthogFilePath='lana/nod'  // path below /scripts, including filename, but no extension, eg. lana/nod
+pickerDir='lana-nod'  // directory name for picker, no slash at end
+scriptSummaryTableName='nod'  // id used for langs record in xx-langdata.js; used to make summary table
+charUsageBCP='nod'  // bcp tag to find the orthography in the charuse app; used in makeIndexIntro only
+orthogName = 'Lanna' // natural language name for the orthography; used only in makeIndexIntro
+scriptIsIndic = true  // true if there are indic syllabic categories; used only in makeIndexIntro
+
+
+
+blockDir='lana/'
+
+pageWebfonts = ["Noto Serif Tai Tham WF", "Lamphun WF", "Lanna Alif WF", "A Tai Tham KH New WF"]
+defaultSize = '24px'
+fontSwitchLangs = 'nod,kkh'
+
+
+autoIndex = true
+
+
+
+
+// block settings
+
+languageName = 'Northern Thai'
+letterClassName = 'nod'
+lang = 'nod'
+direction = ''
+
+*/
