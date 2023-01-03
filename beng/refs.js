2d497374f72181fa38feacfaa1af1d04f143ae6a
r12a <ishida@w3.org>
2023-01-03 19:42:51 +0000

    Stub for new beng directory

---
+++ beng/refs.js
@@ -0,0 +1,144 @@
+reflist = {
+
+
+"b": {
+"url":"#refs",
+"authors": "Tanmoy Bhattacharya",
+"title":"Private correspondence, July 2004",
+"reviewed":true
+},
+
+"d": {
+"url":"#refs",
+"authors": "Peter T. Daniels and William Bright",
+"title":"The World's Writing Systems",
+"publisher":"Oxford University Press",
+"isbn":"0-19-507993-0",
+},
+
+"cs": {
+"url":"http://www.w3.org/International/docs/counter-styles/",
+"authors": "Richard Ishida",
+"title":"Ready-made Counter Styles",
+"reviewed":true
+},
+
+"p": {
+"url":"http://std.dkuug.dk/JTC1/SC2/wg2/docs/n3311.pdf",
+"authors": "Anshuman Pandey",
+"title":"Proposal to Encode the Ganda Currency Mark for Bengali in ISO/IEC 10646",
+"reviewed":true
+},
+
+"r": {
+"url":"#refs",
+"authors": "William Radice",
+"title":"Teach Yourself Bengali",
+"publisher":"Hodder & Stoughton",
+"isbn":"0-340-86029-4",
+},
+
+"rmcs": {
+"url":"https://www.w3.org/TR/predefined-counter-styles/#arabic-styles",
+"authors": "Richard Ishida",
+"title":"Ready-made Counter Styles",
+"reviewed":true
+},
+
+"st": {
+"url":"#refs",
+"authors": "Santhosh Thottingal",
+"title":"Personal correspondence",
+},
+
+"u": {
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch12.pdf",
+"authors": "Unicode Consortium",
+"title":"The Unicode Standard, Version 13.0",
+"sub":"Chapter 12.5: South and Central Asia-I, Bengali (Bangla)",
+"pages":"473-479",
+"isbn":"978-1-936213-16-0.",
+},
+
+"ul": {
+"url":"https://www.unicode.org/reports/tr14/",
+"authors": "Unicode Consortium",
+"title":"Unicode Line Breaking Algorithm (UAX#14)",
+"reviewed":true
+},
+
+"wl": {
+"url":"http://en.wikipedia.org/wiki/Bengali_language",
+"authors": "Wikipedia",
+"title":"Bengali language",
+},
+
+"wp": {
+"url":"http://en.wikipedia.org/wiki/Bengali_phonology",
+"authors": "Wikipedia",
+"title":"Bengali phonology",
+},
+
+"wa": {
+"url":"https://en.wikipedia.org/wiki/Bengali_alphabet",
+"authors": "Wikipedia",
+"title":"Bengali alphabet",
+},
+
+"ws": {
+"url":"https://en.wikipedia.org/wiki/Bengali–Assamese_script",
+"authors": "Wikipedia",
+"title":"Bengali–Assamese script",
+},
+
+
+}
+
+
+
+
+
+
+scriptLanguageList = {
+as:{name:"Assamese"},
+bn:{name:"Bengali"},
+bpy:{name:"Bishnupriya"},
+brx:{name:"Bodo (India)"},
+ccp:{name:"Chakma"},
+cdf:{name:"Chiru"},
+ctg:{name:"Chittagonian"},
+nct:{name:"Naga, Chothe"},
+der:{name:"Deori"},
+dis:{name:"Dimasa"},
+cfm:{name:"Chin, Falam"},
+grt:{name:"Garo"},
+haj:{name:"Hajong"},
+kha:{name:"Khasi"},
+kdq:{name:"Koch"},
+cdz:{name:"Koda"},
+trp:{name:"Kok Borok"},
+kyw:{name:"Kudmali"},
+lus:{name:"Lushai"},
+mni:{name:"Manipuri"},
+nng:{name:"Naga, Maring"},
+mrg:{name:"Mising"},
+nmo:{name:"Naga, Moyon"},
+unx:{name:"Munda"},
+unr:{name:"Mundari"},
+njz:{name:"Nyishi"},
+sdr:{name:"Sadri, Oraon"},
+tdb:{name:"Panchpargania"},
+suv:{name:"Puroik"},
+rah:{name:"Rabha"},
+rkt:{name:"Rangpuri"},
+sck:{name:"Sadri"},
+sat:{name:"Santali"},
+mjt:{name:"Sauria Paharia"},
+syl:{name:"Sylheti"},
+nki:{name:"Naga, Thangal"},
+tpe:{name:"Tippera"},
+lax:{name:"Tiwa"},
+txo:{name:"Toto"},
+usi:{name:"Usui"},
+}
+
