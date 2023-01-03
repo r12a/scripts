9ecbd82c2ac8bf6ad87cb83646fe611f8cba7977
r12a <ishida@w3.org>
2023-01-03 19:45:31 +0000

    Stub for new gujr directory

---
+++ gujr/refs.js
@@ -0,0 +1,100 @@
+reflist = {
+
+
+"cldr": {
+"url":"https://www.unicode.org/cldr/charts/latest/summary/ta.html",
+"authors": "CLDR",
+"title":"Gujarati",
+"reviewed":true
+},
+
+"d": {
+"url":"#refs",
+"authors": "Peter T. Daniels and William Bright",
+"title":"The World's Writing Systems",
+"publisher":"Oxford University Press",
+"isbn":"0-19-507993-0",
+"reviewed":true
+},
+
+"rmcs": {
+"url":"https://www.w3.org/TR/predefined-counter-styles/#arabic-styles",
+"authors": "Richard Ishida",
+"title":"Ready-made Counter Styles",
+"reviewed":true
+},
+
+"s": {
+"url":"http://scriptsource.org/cms/scripts/page.php?item_id=script_detail&uid=aggc6af8cc",
+"authors": "ScriptSource",
+"title":"Gujarati",
+"reviewed":true
+},
+
+"u": {
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch12.pdf",
+"authors": "Unicode Consortium",
+"title":"The Unicode Standard, Version 13.0",
+"sub":"Chapter 12.4: South and Central Asia-I, Gujarati",
+"pages":"485-486",
+"isbn":"978-1-936213-16-0.",
+},
+
+"ul": {
+"url":"https://www.unicode.org/reports/tr14/",
+"authors": "Unicode Consortium",
+"title":"Unicode Line Breaking Algorithm (UAX#14)",
+"reviewed":true
+},
+
+"wp": {
+"url":"https://en.wikipedia.org/wiki/Gujarati_phonology",
+"authors": "Wikipedia",
+"title":"Gujarati phonology",
+},
+
+"ws": {
+"url":"http://en.wikipedia.org/wiki/Gujarati_script",
+"authors": "Wikipedia",
+"title":"Gujarati script",
+},
+
+"z": {
+"url":"http://std.dkuug.dk/jtc1/sc2/wg2/docs/n4473.pdf",
+"authors": "Vinodh Rajan",
+"title":"Proposal to encode Gujarati Letter ZHA",
+},
+
+
+}
+
+
+
+
+scriptLanguageList = {
+gas:{name:"Garasia, Adiwasi"},
+ave:{name:"Avestan"},
+bhb:{name:"Bhili"},
+cdi:{name:"Chodri"},
+dhn:{name:"Dhanki"},
+dub:{name:"Dubli"},
+duh:{name:"Dungra Bhil"},
+gbl:{name:"Gamit"},
+guj:{name:"Gujarati", url:"gu.html"},
+kfr:{name:"Kachhi"},
+gjk:{name:"Koli, Kachi"},
+kex:{name:"Kukna"},
+gra:{name:"Garasia, Rajput"},
+vav:{name:"Varli"},
+vas:{name:"Vasavi"},
+kxp:{name:"Koli, Wadiyara"},
+}
+
+
+
+
+
+
+
+
+
