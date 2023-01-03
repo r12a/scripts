d883c178631efab610d0de55a9b5c5bb9b9197b2
r12a <ishida@w3.org>
2023-01-03 15:48:19 +0000

    tdd: Apply https://github.com/r12a/scripts/issues/123 and port to tale

---
+++ tale/block-globals.js
@@ -0,0 +1,10 @@
+window.version = 'v29'
+window.scriptName = 'Tai Le'
+window.ghDirectory = 'taile'
+window.blockDirectory = 'taile'
+window.languageList = ['tdd'] // lang tags for each orthography handled
+window.detailsfileList = ['tdd'] // first part of xx-details for each orthography
+window.scriptISO = 'tale'
+window.webfonts = ["Noto Sans Tai Le WF"]
+window.defaultSize = '24px'
+
---
+++ tale/tdd-globals.js
@@ -0,0 +1,19 @@
+langTag='tdd' // BCP47 language tag for this orthography
+scriptTag='tale' // BCP47 script tag for this orthography
+notesLangtag='tdd'
+blockLangtag = 'tdd' // same as langTag; used in block.html
+blockDirection=''  // ltr/rtl or '' for ltr; used by block.html
+
+blockDirectoryName='tale'  // folder name where block file is found, no slash
+orthogFilePath='tale/tdd'  // path below /scripts, including filename, but no extension, eg. lana/nod
+pickerDir='tale-tdd'  // directory name for picker, no slash at end
+scriptSummaryTableName='tdd'  // id used for langs record in xx-langdata.js; used to make summary table
+charUsageBCP='tdd'  // bcp tag to find the orthography in the charuse app; used in makeIndexIntro only
+orthogName = 'Tai Le' // natural language name for the orthography; used only in makeIndexIntro
+languageName = 'Tai Nüe' // natural language name of language; used in various places, esp block
+scriptIsIndic = true  // true if there are indic syllabic categories; used only in makeIndexIntro
+
+
+defaultSize = '24px'  // used by block.html to set size of font, including in font mgt panel
+letterClassName = 'tdd'  // classname for a given orthography in the block page
+
