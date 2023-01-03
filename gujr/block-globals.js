99835c7a1fd83a6a254e0294ba2bf9ccbcb98437
r12a <ishida@w3.org>
2023-01-03 22:20:43 +0000

    gujr: 1st draft of v29

---
+++ gujr/block-globals.js
@@ -0,0 +1,10 @@
+window.version = 'v29'
+window.scriptName = 'Gujarati'
+window.ghDirectory = 'gujarati'
+window.blockDirectory = 'gujarati'
+window.languageList = ['gu'] // lang tags for each orthography handled
+window.detailsfileList = ['gu'] // first part of xx-details for each orthography
+window.scriptISO = 'gujr'
+window.webfonts = ["Noto Serif Gujarati WF", "Noto Sans Gujarati WF"]
+window.defaultSize = '24px'
+
---
+++ gujr/gu-globals.js
@@ -0,0 +1,19 @@
+
+langTag='gu' // BCP47 language tag for this orthography
+scriptTag='gujr' // BCP47 script tag for this orthography
+notesLangtag='gu'
+blockLangtag = 'gu' // same as langTag; used in block.html
+blockDirection=''  // ltr/rtl or '' for ltr; used by block.html
+
+blockDirectoryName='gujr'  // folder name where block file is found, no slash
+orthogFilePath='gujr/gu'  // path below /scripts, including filename, but no extension, eg. lana/nod
+pickerDir='gujr'  // directory name for picker, no slash at end
+scriptSummaryTableName='gu'  // id used for langs record in xx-langdata.js; used to make summary table
+charUsageBCP='gu'  // bcp tag to find the orthography in the charuse app; used in makeIndexIntro only
+orthogName = 'Gujarati' // natural language name for the orthography; used only in makeIndexIntro
+languageName = 'Gujarati' // natural language name of language; used in various places, esp block
+scriptIsIndic = true  // true if there are indic syllabic categories; used only in makeIndexIntro
+
+
+defaultSize = '24px'  // used by block.html to set size of font, including in font mgt panel
+letterClassName = 'gu'  // classname for a given orthography in the block page
