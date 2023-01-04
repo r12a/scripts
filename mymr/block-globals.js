f381569ab7ffebc58c8a39ec02a39e34793bd6dc
r12a <ishida@w3.org>
2023-01-04 14:13:41 +0000

    mymr: 1st draft of v29

---
+++ mymr/block-globals.js
@@ -0,0 +1,10 @@
+
+window.version = 'v28'
+window.ghDirectory = 'myanmar'
+window.blockDirectory = 'myanmar'
+window.languageList = ['my','shn'] // lang tags for each orthography handled
+window.detailsfileList = ['my','shn'] // first part of xx-details for each orthography
+window.scriptISO = 'mymr'
+window.webfonts = ["Noto Serif Myanmar WF", "Myanmar3 WF", "Noto Sans Myanmar WF"]
+window.defaultSize = '24px'
+
---
+++ mymr/my-globals.js
@@ -0,0 +1,20 @@
+
+
+langTag='my' // BCP47 language tag for this orthography
+scriptTag='mymr' // BCP47 script tag for this orthography
+notesLangtag='my'
+blockLangtag = 'my' // same as langTag; used in block.html
+blockDirection=''  // ltr/rtl or '' for ltr; used by block.html
+
+blockDirectoryName='mymr'  // folder name where block file is found, no slash
+orthogFilePath='mymr/my'  // path below /scripts, including filename, but no extension, eg. lana/nod
+pickerDir='mymr-my'  // directory name for picker, no slash at end
+scriptSummaryTableName='my'  // id used for langs record in xx-langdata.js; used to make summary table
+charUsageBCP='my'  // bcp tag to find the orthography in the charuse app; used in makeIndexIntro only
+orthogName = 'Myanmar' // natural language name for the orthography; used only in makeIndexIntro
+languageName = 'Burmese' // natural language name of language; used in various places, esp block
+scriptIsIndic = true  // true if there are indic syllabic categories; used only in makeIndexIntro
+
+
+defaultSize = '24px'  // used by block.html to set size of font, including in font mgt panel
+letterClassName = 'my'  // classname for a given orthography in the block page
