f6bfe357ceda2b85b4bfefce2f5922398927d17b
r12a <ishida@w3.org>
2023-01-04 17:20:58 +0000

    Stub for new talu directory

---
+++ talu/refs.js
@@ -0,0 +1,61 @@
+reflist = {
+
+
+"d": {
+"url":"#refs",
+"authors": "Peter T. Daniels and William Bright",
+"title":"The World's Writing Systems",
+"publisher":"Oxford University Press",
+"isbn":"0-19-507993-0",
+"reviewed":true
+},
+
+"u": {
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch16.pdf",
+"authors": "Unicode Consortium",
+"title":"The Unicode Standard, Version 13.0",
+"sub":"Chapter 16.6: Southeast Asia, New Tai Lue",
+"pages":"667-669",
+"isbn":"978-1-936213-16-0.",
+},
+
+"ul": {
+"url":"https://www.unicode.org/reports/tr14/",
+"authors": "Unicode Consortium",
+"title":"Unicode Line Breaking Algorithm (UAX#14)",
+"reviewed":true
+},
+
+"wl": {
+"url":"https://en.wikipedia.org/wiki/Tai_Lue_language",
+"authors": "Wikipedia",
+"title":"Tai Lue language",
+},
+
+"ws": {
+"url":"https://en.wikipedia.org/wiki/New_Tai_Lue_alphabet",
+"authors": "Wikipedia",
+"title":"New Tai Lue alphabet",
+},
+
+"l": {
+"url":"https://www.webonary.org/dailu/overview/introduction/?lang=en",
+"authors": "SIL International",
+"title":"Dai Lue Dictionary ᦈᦹᧈ ᦶᦑᧃ ᦺᦑ ᦟᦹᧉ 傣泐词典",
+},
+
+
+}
+
+
+
+
+scriptLanguageList = {
+khb:{name:"Lü", url:"khb.html"},
+}
+
+
+
+
+
+
