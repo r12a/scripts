4e1a060d298ac22d8e4b253cec80ab39f9f7fe82
r12a <ishida@w3.org>
2023-01-04 11:41:42 +0000

    telu: 1st draft of v29

---
+++ telu/block-globals.js
@@ -0,0 +1,11 @@
+
+window.version = 'v29'
+window.scriptName = 'Telugu'
+window.ghDirectory = 'telugu'
+window.blockDirectory = 'telugu'
+window.languageList = ['te'] // lang tags for each orthography handled
+window.detailsfileList = ['te'] // first part of xx-details for each orthography
+window.scriptISO = 'telu'
+window.webfonts = ["Noto Sans Telugu WF", "Noto Serif Telugu WF"]
+window.defaultSize = '24px'
+
---
+++ telu/te-globals.js
@@ -0,0 +1,21 @@
+
+
+
+langTag='te' // BCP47 language tag for this orthography
+scriptTag='telu' // BCP47 script tag for this orthography
+notesLangtag='te'
+blockLangtag = 'te' // same as langTag; used in block.html
+blockDirection=''  // ltr/rtl or '' for ltr; used by block.html
+
+blockDirectoryName='telu'  // folder name where block file is found, no slash
+orthogFilePath='telu/te'  // path below /scripts, including filename, but no extension, eg. lana/nod
+pickerDir='telu'  // directory name for picker, no slash at end
+scriptSummaryTableName='te'  // id used for langs record in xx-langdata.js; used to make summary table
+charUsageBCP='te'  // bcp tag to find the orthography in the charuse app; used in makeIndexIntro only
+orthogName = 'Telugu' // natural language name for the orthography; used only in makeIndexIntro
+languageName = 'Telugu' // natural language name of language; used in various places, esp block
+scriptIsIndic = true  // true if there are indic syllabic categories; used only in makeIndexIntro
+
+
+defaultSize = '24px'  // used by block.html to set size of font, including in font mgt panel
+letterClassName = 'te'  // classname for a given orthography in the block page
