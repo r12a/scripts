db9c9c5fd863fe57e81615ac226bc582caaec398
r12a <ishida@w3.org>
2023-01-04 13:13:33 +0000

    btk: 1st draft of v29

---
+++ batk/block-globals.js
@@ -0,0 +1,11 @@
+
+window.version = 'v28'
+window.scriptName = 'Batak'
+window.ghDirectory = 'batk'
+window.blockDirectory = 'batk'
+window.languageList = ['btk'] // lang tags for each orthography handled
+window.detailsfileList = ['btk'] // first part of xx-details for each orthography
+window.scriptISO = 'batk'
+window.webfonts = ["Noto Sans Batak WF"]
+window.defaultSize = '24px'
+
---
+++ batk/btk-globals.js
@@ -0,0 +1,20 @@
+
+
+langTag='btk' // BCP47 language tag for this orthography
+scriptTag='batk' // BCP47 script tag for this orthography
+notesLangtag='btk'
+blockLangtag = 'btk' // same as langTag; used in block.html
+blockDirection=''  // ltr/rtl or '' for ltr; used by block.html
+
+blockDirectoryName='batk'  // folder name where block file is found, no slash
+orthogFilePath='batk/btk'  // path below /scripts, including filename, but no extension, eg. lana/nod
+pickerDir='batk'  // directory name for picker, no slash at end
+scriptSummaryTableName='btk'  // id used for langs record in xx-langdata.js; used to make summary table
+charUsageBCP='btk'  // bcp tag to find the orthography in the charuse app; used in makeIndexIntro only
+orthogName = 'Batak' // natural language name for the orthography; used only in makeIndexIntro
+languageName = 'Batak' // natural language name of language; used in various places, esp block
+scriptIsIndic = true  // true if there are indic syllabic categories; used only in makeIndexIntro
+
+
+defaultSize = '24px'  // used by block.html to set size of font, including in font mgt panel
+letterClassName = 'btk'  // classname for a given orthography in the block page
