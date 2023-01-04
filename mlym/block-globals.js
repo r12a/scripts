11cd8ebc6460d62676f7cafa63def1540ce8e784
r12a <ishida@w3.org>
2023-01-04 09:28:04 +0000

    mlym: 1st draft of v29

---
+++ mlym/block-globals.js
@@ -0,0 +1,12 @@
+
+
+window.version = 'v29'
+window.scriptName = 'Malayalam'
+window.ghDirectory = 'malayalam'
+window.blockDirectory = 'malayalam'
+window.languageList = ['ml'] // lang tags for each orthography handled
+window.detailsfileList = ['ml'] // first part of xx-details for each orthography
+window.scriptISO = 'mlym'
+window.webfonts = ["Noto Serif Malayalam WF", "Noto Sans Malayalam WF"]
+window.defaultSize = '24px'
+
---
+++ mlym/ml-globals.js
@@ -0,0 +1,20 @@
+
+
+langTag='ml' // BCP47 language tag for this orthography
+scriptTag='mlym' // BCP47 script tag for this orthography
+notesLangtag='ml'
+blockLangtag = 'ml' // same as langTag; used in block.html
+blockDirection=''  // ltr/rtl or '' for ltr; used by block.html
+
+blockDirectoryName='mlym'  // folder name where block file is found, no slash
+orthogFilePath='mlym/ml'  // path below /scripts, including filename, but no extension, eg. lana/nod
+pickerDir='mlym'  // directory name for picker, no slash at end
+scriptSummaryTableName='ml'  // id used for langs record in xx-langdata.js; used to make summary table
+charUsageBCP='ml'  // bcp tag to find the orthography in the charuse app; used in makeIndexIntro only
+orthogName = 'Malayalam' // natural language name for the orthography; used only in makeIndexIntro
+languageName = 'Malayalam' // natural language name of language; used in various places, esp block
+scriptIsIndic = true  // true if there are indic syllabic categories; used only in makeIndexIntro
+
+
+defaultSize = '24px'  // used by block.html to set size of font, including in font mgt panel
+letterClassName = 'ml'  // classname for a given orthography in the block page
