088d179452d2e4b0490329436d338f5f19c2eb50
r12a <ishida@w3.org>
2023-01-04 18:44:09 +0000

    Stub for new syrc directory

---
+++ syrc/refs.js
@@ -0,0 +1,158 @@
+window.langSet = true
+
+
+
+reflist = {
+
+"a": {
+"authors": "Aramaic Online Project (2014-2017) edited by Shabo Talay",
+"title":" ܫܠܳܡܐ ܣܘܪܰܝܬ Šlomo Surayt An Introductory Course to Surayt-Aramaic (Turoyo) (consonants)",
+"url":"https://textbook.surayt.com/en/Level%20A/2.1?content-fragment-id=1747",
+"lang": new Set(['tru']),
+},
+
+"a2": {
+"authors": "Aramaic Online Project (2014-2017) edited by Shabo Talay",
+"title":" ܫܠܳܡܐ ܣܘܪܰܝܬ Šlomo Surayt An Introductory Course to Surayt-Aramaic (Turoyo) (vowels)",
+"url":"https://textbook.surayt.com/en/Level%20A/2?content-fragment-id=1746",
+"lang": new Set(['tru']),
+},
+
+"d": {
+"authors": "Peter T. Daniels and William Bright",
+"title":"The World's Writing Systems",
+"publisher":"Oxford University Press",
+"isbn":"0-19-507993-0",
+"lang": new Set(['syr','aii']),
+"reviewed":true
+},
+
+"r": {
+"authors": "Madeleine Davis",
+"title":"Assyrian Reading Lessons in Six Days, 2nd ed.",
+"date":"2014",
+"lang": new Set(['aii']),
+},
+
+"mv": {
+"authors": "Madeleine Davis Moradkhan",
+"title":"Assyrian Reading Course (videos)",
+"url":"https://www.youtube.com/channel/UConJUIvQYXb48VFWoIFavAQ",
+"lang": new Set(['aii']),
+},
+
+"n": {
+"authors": "Paul Nelson, George Anton Kiraz, Sargon Hass",
+"title":"Proposal to Encode Syriac in ISO/IEC 10646",
+"date":"1998",
+"url":"http://www.unicode.org/L2/L1998/98050-syriac-proposal.pdf",
+"lang": new Set(['syr','aii']),
+},
+
+"s": {
+"authors": "Nicholas Sims-Williams, Michael Everson",
+"title":"Proposal to add six Syriac letters for Sogdian and Persian to the UCS",
+"url":"http://www.unicode.org/L2/L2002/02132-N2422-syrian.pdf",
+"lang": new Set(['syr']),
+},
+
+"so": {
+"authors": "Erasmus+ Aramaic-Online project (2014–2017)",
+"title":"Surayt Orthography",
+"url":"https://userblogs.fu-berlin.de/wp-includes/ms-files.php?path=/aramaic-ol/&file=2017/08/Surayt-Orthography.pdf",
+"lang": new Set(['tru']),
+},
+
+"ss": {
+"authors": "Shabo Talay (ed)",
+"title":"Šlomo Suryat – An introductory course to Surayt-Aramaic (Turoyo)",
+"url":"https://zenodo.org/record/2682272",
+"date":"2017",
+"lang": new Set(['tru']),
+},
+
+"u": {
+"authors": "Unicode Consortium",
+"title":"The Unicode Standard, Version 13.0",
+"sub":"Chapter 9.3: Middle East-I, Syriac",
+"pages":"390-398",
+"isbn":"978-1-936213-16-0.",
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch09.pdf",
+"lang": new Set(['syr','aii', 'tru']),
+},
+
+"wa": {
+"authors": "Wikipedia",
+"title":"Assyrian Neo-Aramaic",
+"url":"https://en.wikipedia.org/wiki/Assyrian_Neo-Aramaic",
+"lang": new Set(['aii']),
+},
+
+"wb": {
+"authors": "Wikipedia",
+"title":"Bet",
+"url":"https://en.wikipedia.org/wiki/Bet_(letter)",
+"lang": new Set(['syr','aii']),
+},
+
+"wh": {
+"url":"https://en.wikipedia.org/wiki/He_(letter)",
+"authors": "Wikipedia",
+"title":"He",
+"lang": new Set(['syr']),
+},
+
+"ws": {
+"authors": "Wikipedia",
+"title":"Suret language",
+"url":"https://en.wikipedia.org/wiki/Suret_language",
+"lang": new Set(['aii']),
+},
+
+"wt": {
+"authors": "Wikipedia",
+"title":"Turoyo language",
+"url":"https://en.wikipedia.org/wiki/Turoyo_language",
+"lang": new Set(['tru']),
+},
+
+"w": {
+"authors": "Wikipedia",
+"title":"Syriac alphabet",
+"url":"https://en.wikipedia.org/wiki/Syriac_alphabet",
+"lang": new Set(['syr','aii']),
+},
+
+
+}
+
+
+
+
+
+
+scriptLanguageList = {
+ara:{name:"Arabic"},
+aii:{name:"Neo-Aramaic, Assyrian", url:"aii.html"},
+bjf:{name:"Neo-Aramaic, Barzani Jewish"},
+bhn:{name:"Neo-Aramaic, Bohtan"},
+cld:{name:"Neo-Aramaic, Chaldean"},
+syc:{name:"Syriac, Classical"},
+hrt:{name:"Hértevin"},
+tmr:{name:"Aramaic, Jewish Babylonian (ca. 200-1200 CE)"},
+kqd:{name:"Koy Sanjaq Surat"},
+lhs:{name:"Mlahsö"},
+arc:{name:"Aramaic, Official (700-300 BCE)"},
+oar:{name:"Aramaic, Old (up to 700 BCE)"},
+sam:{name:"Aramaic, Samaritan"},
+syn:{name:"Senaya"},
+sog:{name:"Sogdian"},
+syr:{name:"Syriac", url:"syr.html"},
+tru:{name:"Turoyo", url:"tru.html"},
+}
+
+
+
+
+
+
---
+++ syrc/syr-langdata.js
@@ -0,0 +1,15 @@
+// this should always be updated when app-charuse/langs.js is updated
+
+var langs = {
+
+"syc": { name:"Syriac", local:" ܠܫܢܐ ܣܘܪܝܝܐ‎ (Leššānā Suryāyā)", rtl:true, 
+source:"a9dcf48a64fd2fb12d3014d2f0295b003130db65", 
+region:"wasia", countries:"Turkey, Iraq, Syria", script:"syrc", speakers:"0", 
+letter:"ܐܝܘܦܒܬܛܕܟܓܩܥܣܤܨܫܙܚܗܡܢܪܠـ", mark:"ّܼܸܹܻܾܷܱܴ݂̥̣݄̤݈̱̭̮ܿܲܵܺܽܶܰܳ݁̊݀̇݃̈݇̄݉݊", punctuation:"،؛.؟܀܁܂܃܄܅܆܇܈܉܊܋܌܍", symbol:"♰♱", other:"܏", aux:"ܞܧ", 
+type:"abjad", cs:"no", gpos:"yes", gsub:"yes", cursive:"yes", direction:"rtl", wordsep:"space", baseline:"mid", wrap:"word", hyphenation:"?",  justification:"?",
+vowels:"ml:3 hcm:6-11",
+linked:"syriac", 
+},
+
+
+}
