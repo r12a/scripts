7115fb5ff8234709963c4c74962e249ffbb2707f
r12a <ishida@w3.org>
2023-01-04 10:26:31 +0000

    orya: 1st draft of v29

---
+++ orya/block-globals.js
@@ -0,0 +1,12 @@
+
+
+window.version = 'v29'
+window.scriptName = 'Odia (Oriya)'
+window.ghDirectory = 'oriya'
+window.blockDirectory = 'oriya'
+window.languageList = ['or'] // lang tags for each orthography handled
+window.detailsfileList = ['or'] // first part of xx-details for each orthography
+window.scriptISO = 'orya'
+window.webfonts = ["Noto Sans Oriya WF"]
+window.defaultSize = '24px'
+
---
+++ orya/or-globals.js
@@ -0,0 +1,20 @@
+
+
+langTag='or' // BCP47 language tag for this orthography
+scriptTag='orya' // BCP47 script tag for this orthography
+notesLangtag='or'
+blockLangtag = 'or' // same as langTag; used in block.html
+blockDirection=''  // ltr/rtl or '' for ltr; used by block.html
+
+blockDirectoryName='orya'  // folder name where block file is found, no slash
+orthogFilePath='orya/or'  // path below /scripts, including filename, but no extension, eg. lana/nod
+pickerDir='orya'  // directory name for picker, no slash at end
+scriptSummaryTableName='ory'  // id used for langs record in xx-langdata.js; used to make summary table
+charUsageBCP='ory'  // bcp tag to find the orthography in the charuse app; used in makeIndexIntro only
+orthogName = 'Oriya' // natural language name for the orthography; used only in makeIndexIntro
+languageName = 'Odia' // natural language name of language; used in various places, esp block
+scriptIsIndic = true  // true if there are indic syllabic categories; used only in makeIndexIntro
+
+
+defaultSize = '24px'  // used by block.html to set size of font, including in font mgt panel
+letterClassName = 'or'  // classname for a given orthography in the block page
