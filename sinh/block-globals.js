e85daa467a6e25c7b880217a30c695a43d5d386d
r12a <ishida@w3.org>
2023-01-04 11:05:40 +0000

    sinh: 1st draft of v29

---
+++ sinh/block-globals.js
@@ -0,0 +1,11 @@
+
+window.version = 'v29'
+window.scriptName = 'Sinhala'
+window.ghDirectory = 'sinhala'
+window.blockDirectory = 'sinhala'
+window.languageList = ['si'] // lang tags for each orthography handled
+window.detailsfileList = ['si'] // first part of xx-details for each orthography
+window.scriptISO = 'sinh'
+window.webfonts = ["Noto Serif Sinhala WF"]
+window.defaultSize = '24px'
+
---
+++ sinh/si-globals.js
@@ -0,0 +1,21 @@
+
+
+
+langTag='si' // BCP47 language tag for this orthography
+scriptTag='sinh' // BCP47 script tag for this orthography
+notesLangtag='si'
+blockLangtag = 'si' // same as langTag; used in block.html
+blockDirection=''  // ltr/rtl or '' for ltr; used by block.html
+
+blockDirectoryName='sinh'  // folder name where block file is found, no slash
+orthogFilePath='sinh/si'  // path below /scripts, including filename, but no extension, eg. lana/nod
+pickerDir='sinh'  // directory name for picker, no slash at end
+scriptSummaryTableName='si'  // id used for langs record in xx-langdata.js; used to make summary table
+charUsageBCP='si'  // bcp tag to find the orthography in the charuse app; used in makeIndexIntro only
+orthogName = 'Sinhala' // natural language name for the orthography; used only in makeIndexIntro
+languageName = 'Sinhala' // natural language name of language; used in various places, esp block
+scriptIsIndic = true  // true if there are indic syllabic categories; used only in makeIndexIntro
+
+
+defaultSize = '24px'  // used by block.html to set size of font, including in font mgt panel
+letterClassName = 'si'  // classname for a given orthography in the block page
