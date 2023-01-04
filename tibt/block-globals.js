9d8d901bddc5aa53018b769dafcb8276c01ee3ee
r12a <ishida@w3.org>
2023-01-04 11:56:21 +0000

    tibt: 1st draft of v29

---
+++ tibt/block-globals.js
@@ -0,0 +1,11 @@
+
+window.version = 'v29'
+window.scriptName = 'Tibetan'
+window.ghDirectory = 'tibetan'
+window.blockDirectory = 'tibetan'
+window.languageList = ['bo'] // lang tags for each orthography handled
+window.detailsfileList = ['bo'] // first part of xx-details for each orthography
+window.scriptISO = 'tibt'
+window.webfonts = ["Noto Sans Tibetan WF"]
+window.defaultSize = '24px'
+
---
+++ tibt/bo-globals.js
@@ -0,0 +1,20 @@
+
+
+langTag='bo' // BCP47 language tag for this orthography
+scriptTag='tibt' // BCP47 script tag for this orthography
+notesLangtag='bo'
+blockLangtag = 'bo' // same as langTag; used in block.html
+blockDirection=''  // ltr/rtl or '' for ltr; used by block.html
+
+blockDirectoryName='tibt'  // folder name where block file is found, no slash
+orthogFilePath='tibt/bo'  // path below /scripts, including filename, but no extension, eg. lana/nod
+pickerDir='tibt'  // directory name for picker, no slash at end
+scriptSummaryTableName='bo'  // id used for langs record in xx-langdata.js; used to make summary table
+charUsageBCP='bo'  // bcp tag to find the orthography in the charuse app; used in makeIndexIntro only
+orthogName = 'Tibetan' // natural language name for the orthography; used only in makeIndexIntro
+languageName = 'Tibetan' // natural language name of language; used in various places, esp block
+scriptIsIndic = true  // true if there are indic syllabic categories; used only in makeIndexIntro
+
+
+defaultSize = '24px'  // used by block.html to set size of font, including in font mgt panel
+letterClassName = 'bo'  // classname for a given orthography in the block page
