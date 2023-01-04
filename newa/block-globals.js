d76edd646386195cf9ee06eb3c3bacb05b36919d
r12a <ishida@w3.org>
2023-01-04 09:43:14 +0000

    newa: 1st draft of v29

---
+++ newa/block-globals.js
@@ -0,0 +1,12 @@
+
+
+window.version = 'v29'
+window.scriptName = 'Newa'
+window.ghDirectory = 'newa'
+window.blockDirectory = 'newa'
+window.languageList = ['new'] // lang tags for each orthography handled
+window.detailsfileList = ['new'] // first part of xx-details for each orthography
+window.scriptISO = 'newa'
+window.webfonts = ["Noto Sans Newa WF"]
+window.defaultSize = '24px'
+
---
+++ newa/new-globals.js
@@ -0,0 +1,20 @@
+
+
+langTag='new' // BCP47 language tag for this orthography
+scriptTag='newa' // BCP47 script tag for this orthography
+notesLangtag='new'
+blockLangtag = 'new' // same as langTag; used in block.html
+blockDirection=''  // ltr/rtl or '' for ltr; used by block.html
+
+blockDirectoryName='newa'  // folder name where block file is found, no slash
+orthogFilePath='newa/new'  // path below /scripts, including filename, but no extension, eg. lana/nod
+pickerDir='newa'  // directory name for picker, no slash at end
+scriptSummaryTableName='new'  // id used for langs record in xx-langdata.js; used to make summary table
+charUsageBCP='new'  // bcp tag to find the orthography in the charuse app; used in makeIndexIntro only
+orthogName = 'Newa' // natural language name for the orthography; used only in makeIndexIntro
+languageName = 'Newar' // natural language name of language; used in various places, esp block
+scriptIsIndic = true  // true if there are indic syllabic categories; used only in makeIndexIntro
+
+
+defaultSize = '24px'  // used by block.html to set size of font, including in font mgt panel
+letterClassName = 'new'  // classname for a given orthography in the block page
