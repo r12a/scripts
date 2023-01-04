82edd9631351393ea0160897de5bd7e808baca1a
r12a <ishida@w3.org>
2023-01-04 17:15:29 +0000

    lisu: 1st draft of v29

---
+++ lisu/block-globals.js
@@ -0,0 +1,11 @@
+
+window.version = 'v28'
+window.scriptName = 'Lisu'
+window.ghDirectory = 'lisu'
+window.blockDirectory = 'lisu'
+window.languageList = ['lis'] // lang tags for each orthography handled
+window.detailsfileList = ['lis'] // first part of xx-details for each orthography
+window.scriptISO = 'lisu'
+window.webfonts = ["Noto Lisu WF"]
+window.defaultSize = '24px'
+
---
+++ lisu/lis-globals.js
@@ -0,0 +1,20 @@
+
+
+langTag='lis' // BCP47 language tag for this orthography
+scriptTag='lisu' // BCP47 script tag for this orthography
+notesLangtag='lis'
+blockLangtag = 'lis' // same as langTag; used in block.html
+blockDirection=''  // ltr/rtl or '' for ltr; used by block.html
+
+blockDirectoryName='lisu'  // folder name where block file is found, no slash
+orthogFilePath='lisu/lis'  // path below /scripts, including filename, but no extension, eg. lana/nod
+pickerDir='lisu'  // directory name for picker, no slash at end
+scriptSummaryTableName='lis'  // id used for langs record in xx-langdata.js; used to make summary table
+charUsageBCP='lis'  // bcp tag to find the orthography in the charuse app; used in makeIndexIntro only
+orthogName = 'Lisu' // natural language name for the orthography; used only in makeIndexIntro
+languageName = 'Lisu' // natural language name of language; used in various places, esp block
+scriptIsIndic = true  // true if there are indic syllabic categories; used only in makeIndexIntro
+
+
+defaultSize = '24px'  // used by block.html to set size of font, including in font mgt panel
+letterClassName = 'lis'  // classname for a given orthography in the block page
