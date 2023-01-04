e17efd147222055f665cec3b93125a8be62a43f9
r12a <ishida@w3.org>
2023-01-04 11:27:02 +0000

    taml: 1st draft of v29

---
+++ taml/block-globals.js
@@ -0,0 +1,11 @@
+
+window.version = 'v29'
+window.scriptName = 'Tamil'
+window.ghDirectory = 'tamil'
+window.blockDirectory = 'tamil'
+window.languageList = ['ta'] // lang tags for each orthography handled
+window.detailsfileList = ['ta'] // first part of xx-details for each orthography
+window.scriptISO = 'taml'
+window.webfonts = ["Noto Serif Tamil WF"]
+window.defaultSize = '24px'
+
---
+++ taml/ta-globals.js
@@ -0,0 +1,21 @@
+
+
+
+langTag='ta' // BCP47 language tag for this orthography
+scriptTag='taml' // BCP47 script tag for this orthography
+notesLangtag='ta'
+blockLangtag = 'ta' // same as langTag; used in block.html
+blockDirection=''  // ltr/rtl or '' for ltr; used by block.html
+
+blockDirectoryName='taml'  // folder name where block file is found, no slash
+orthogFilePath='taml/ta'  // path below /scripts, including filename, but no extension, eg. lana/nod
+pickerDir='taml'  // directory name for picker, no slash at end
+scriptSummaryTableName='ta'  // id used for langs record in xx-langdata.js; used to make summary table
+charUsageBCP='ta'  // bcp tag to find the orthography in the charuse app; used in makeIndexIntro only
+orthogName = 'Tamil' // natural language name for the orthography; used only in makeIndexIntro
+languageName = 'Tamil' // natural language name of language; used in various places, esp block
+scriptIsIndic = true  // true if there are indic syllabic categories; used only in makeIndexIntro
+
+
+defaultSize = '24px'  // used by block.html to set size of font, including in font mgt panel
+letterClassName = 'ta'  // classname for a given orthography in the block page
