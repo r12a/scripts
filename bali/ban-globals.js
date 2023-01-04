a1dcf62ae45ac0095abb62ebbe5bfe4c7da04254
r12a <ishida@w3.org>
2023-01-04 12:16:13 +0000

    bali: 1st draft of v29

---
+++ bali/ban-globals.js
@@ -0,0 +1,21 @@
+
+
+
+langTag='ban' // BCP47 language tag for this orthography
+scriptTag='bali' // BCP47 script tag for this orthography
+notesLangtag='ban'
+blockLangtag = 'ban' // same as langTag; used in block.html
+blockDirection=''  // ltr/rtl or '' for ltr; used by block.html
+
+blockDirectoryName='bali'  // folder name where block file is found, no slash
+orthogFilePath='bali/ban'  // path below /scripts, including filename, but no extension, eg. lana/nod
+pickerDir='bali'  // directory name for picker, no slash at end
+scriptSummaryTableName='ban-bali'  // id used for langs record in xx-langdata.js; used to make summary table
+charUsageBCP='ban-bali'  // bcp tag to find the orthography in the charuse app; used in makeIndexIntro only
+orthogName = 'Balinese' // natural language name for the orthography; used only in makeIndexIntro
+languageName = 'Balinese' // natural language name of language; used in various places, esp block
+scriptIsIndic = true  // true if there are indic syllabic categories; used only in makeIndexIntro
+
+
+defaultSize = '24px'  // used by block.html to set size of font, including in font mgt panel
+letterClassName = 'ban'  // classname for a given orthography in the block page
---
+++ bali/block-globals.js
@@ -0,0 +1,11 @@
+
+window.version = 'v29'
+window.scriptName = 'Balinese'
+window.ghDirectory = 'balinese'
+window.blockDirectory = 'balinese'
+window.languageList = ['ban'] // lang tags for each orthography handled
+window.detailsfileList = ['ban'] // first part of xx-details for each orthography
+window.scriptISO = 'bali'
+window.webfonts = ["Vimala", "Noto Serif Balinese WF", "Aksara Bali WF"]
+window.defaultSize = '24px'
+
