7ee0c89bab0a80267cd9195b2b8a5f4e73f4f61a
r12a <ishida@w3.org>
2023-01-04 17:17:41 +0000

    khmr: 1st draft of v29

---
+++ khmr/block-globals.js
@@ -0,0 +1,11 @@
+
+window.version = 'v28'
+window.scriptName = 'Khmer'
+window.ghDirectory = 'khmer'
+window.blockDirectory = 'khmer'
+window.languageList = ['km'] // lang tags for each orthography handled
+window.detailsfileList = ['km'] // first part of xx-details for each orthography
+window.scriptISO = 'khmr'
+window.webfonts = ["Khmer OS Battambang WF", "Noto Serif Khmer WF"]
+window.defaultSize = '24px'
+
---
+++ khmr/km-globals.js
@@ -0,0 +1,20 @@
+
+
+langTag='km' // BCP47 language tag for this orthography
+scriptTag='khmr' // BCP47 script tag for this orthography
+notesLangtag='km'
+blockLangtag = 'km' // same as langTag; used in block.html
+blockDirection=''  // ltr/rtl or '' for ltr; used by block.html
+
+blockDirectoryName='khmr'  // folder name where block file is found, no slash
+orthogFilePath='khmr/km'  // path below /scripts, including filename, but no extension, eg. lana/nod
+pickerDir='khmr'  // directory name for picker, no slash at end
+scriptSummaryTableName='km'  // id used for langs record in xx-langdata.js; used to make summary table
+charUsageBCP='km'  // bcp tag to find the orthography in the charuse app; used in makeIndexIntro only
+orthogName = 'Khmer' // natural language name for the orthography; used only in makeIndexIntro
+languageName = 'Khmer' // natural language name of language; used in various places, esp block
+scriptIsIndic = true  // true if there are indic syllabic categories; used only in makeIndexIntro
+
+
+defaultSize = '24px'  // used by block.html to set size of font, including in font mgt panel
+letterClassName = 'km'  // classname for a given orthography in the block page
