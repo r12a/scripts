18885056adda43ba8c94cefd0f678c45b6009083
r12a <ishida@w3.org>
2023-01-04 14:36:48 +0000

    java: 1st draft of v29

---
+++ java/block-globals.js
@@ -0,0 +1,12 @@
+
+
+window.version = 'v29'
+window.scriptName = 'Javanese'
+window.ghDirectory = 'javanese'
+window.blockDirectory = 'javanese'
+window.languageList = ['jv'] // lang tags for each orthography handled
+window.detailsfileList = ['jv'] // first part of xx-details for each orthography
+window.scriptISO = 'java'
+window.webfonts = ["Tuladha Jejeg WF", "Noto Sans Javanese WF"]
+window.defaultSize = '24px'
+
