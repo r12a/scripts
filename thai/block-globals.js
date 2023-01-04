443c54f354b4b3d581199fe2dbccf27e937c7e00
r12a <ishida@w3.org>
2023-01-04 18:06:35 +0000

    thai: 1st draft of v29

---
+++ thai/block-globals.js
@@ -0,0 +1,11 @@
+
+window.version = 'v28'
+window.scriptName = 'Thai'
+window.ghDirectory = 'thai'
+window.blockDirectory = 'thai'
+window.languageList = ['th'] // lang tags for each orthography handled
+window.detailsfileList = ['th'] // first part of xx-details for each orthography
+window.scriptISO = 'thai'
+window.webfonts = ["Noto Serif Thai WF"]
+window.defaultSize = '24px'
+
