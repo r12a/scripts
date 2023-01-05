c70c5055e5fd9733be212b8be7fef8064c738d1b
r12a <ishida@w3.org>
2023-01-05 10:50:11 +0000

    cher: 1st draft of v29

---
+++ cher/block-globals.js
@@ -0,0 +1,11 @@
+
+window.version = 'v28'
+window.scriptName = 'Cherokee'
+window.ghDirectory = 'cherokee'
+window.blockDirectory = 'cherokee'
+window.languageList = ['chr'] // lang tags for each orthography handled
+window.detailsfileList = ['chr'] // first part of xx-details for each orthography
+window.scriptISO = 'cher'
+window.webfonts = ["Noto Sans Cherokee WF"]
+window.defaultSize = '24px'
+
---
+++ cher/chr-globals.js
@@ -0,0 +1,21 @@
+langTag='chr' // BCP47 language tag for this orthography
+scriptTag='cher' // BCP47 script tag for this orthography
+notesLangtag='chr'
+blockLangtag = 'chr' // same as langTag; used in block.html
+blockDirection=''  // ltr/rtl or '' for ltr; used by block.html
+
+blockDirectoryName='cher'  // folder name where block file is found, no slash
+orthogFilePath='cher/chr'  // path below /scripts, including filename, but no extension, eg. lana/nod
+pickerDir='cher'  // directory name for picker, no slash at end
+scriptSummaryTableName='chr'  // id used for langs record in xx-langdata.js; used to make summary table
+charUsageBCP='chr'  // bcp tag to find the orthography in the charuse app; used in makeIndexIntro only
+orthogName = 'Cherokee' // natural language name for the orthography; used only in makeIndexIntro
+languageName = 'Cherokee' // natural language name of language; used in various places, esp block
+scriptIsIndic = false  // true if there are indic syllabic categories; used only in makeIndexIntro
+
+
+defaultSize = '24px'  // used by block.html to set size of font, including in font mgt panel
+letterClassName = 'chr'  // classname for a given orthography in the block page
+
+
+
