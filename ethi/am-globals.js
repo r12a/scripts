0116a1702e76c72606c83a15960387376c514ea1
r12a <ishida@w3.org>
2023-01-05 10:07:58 +0000

    ethi: 1st draft of v29

---
+++ ethi/am-globals.js
@@ -0,0 +1,19 @@
+
+langTag='am' // BCP47 language tag for this orthography
+scriptTag='ethi' // BCP47 script tag for this orthography
+notesLangtag='am'
+blockLangtag = 'am' // same as langTag; used in block.html
+blockDirection=''  // ltr/rtl or '' for ltr; used by block.html
+
+blockDirectoryName='ethi'  // folder name where block file is found, no slash
+orthogFilePath='ethi/am'  // path below /scripts, including filename, but no extension, eg. lana/nod
+pickerDir='ethi'  // directory name for picker, no slash at end
+scriptSummaryTableName='am'  // id used for langs record in xx-langdata.js; used to make summary table
+charUsageBCP='am'  // bcp tag to find the orthography in the charuse app; used in makeIndexIntro only
+orthogName = 'Ethiopic' // natural language name for the orthography; used only in makeIndexIntro
+languageName = 'Amharic' // natural language name of language; used in various places, esp block
+scriptIsIndic = true  // true if there are indic syllabic categories; used only in makeIndexIntro
+
+
+defaultSize = '24px'  // used by block.html to set size of font, including in font mgt panel
+letterClassName = 'am'  // classname for a given orthography in the block page
---
+++ ethi/block-globals.js
@@ -0,0 +1,11 @@
+
+window.version = 'v29'
+window.scriptName = 'Ethiopic'
+window.ghDirectory = 'ethiopic'
+window.blockDirectory = 'ethiopic'
+window.languageList = ['am'] // lang tags for each orthography handled
+window.detailsfileList = ['am'] // first part of xx-details for each orthography
+window.scriptISO = 'ethi'
+window.webfonts = ["Noto Serif Ethiopic WF"]
+window.defaultSize = '24px'
+
