7bc00c844f673c15c01abd3d62c4bff32b9802fd
r12a <ishida@w3.org>
2023-01-05 10:58:22 +0000

    vaii: 1st draft of v29

---
+++ vaii/block-globals.js
@@ -0,0 +1,11 @@
+
+window.version = 'v27'
+window.scriptName = 'Vai'
+window.ghDirectory = 'vai'
+window.blockDirectory = 'vai'
+window.languageList = ['vai'] // lang tags for each orthography handled
+window.detailsfileList = ['vai'] // first part of xx-details for each orthography
+window.scriptISO = 'vaii'
+window.webfonts = ["Noto Sans Vai WF"]
+window.defaultSize = '24px'
+
---
+++ vaii/vai-globals.js
@@ -0,0 +1,19 @@
+langTag='vai' // BCP47 language tag for this orthography
+scriptTag='vaii' // BCP47 script tag for this orthography
+notesLangtag='vai'
+blockLangtag = 'vai' // same as langTag; used in block.html
+blockDirection=''  // ltr/rtl or '' for ltr; used by block.html
+
+blockDirectoryName='vaii'  // folder name where block file is found, no slash
+orthogFilePath='vaii/vai'  // path below /scripts, including filename, but no extension, eg. lana/nod
+pickerDir='vai'  // directory name for picker, no slash at end
+scriptSummaryTableName='vai'  // id used for langs record in xx-langdata.js; used to make summary table
+charUsageBCP='vai'  // bcp tag to find the orthography in the charuse app; used in makeIndexIntro only
+orthogName = 'Vai' // natural language name for the orthography; used only in makeIndexIntro
+languageName = 'Vai' // natural language name of language; used in various places, esp block
+scriptIsIndic = false  // true if there are indic syllabic categories; used only in makeIndexIntro
+
+
+defaultSize = '24px'  // used by block.html to set size of font, including in font mgt panel
+letterClassName = 'vai'  // classname for a given orthography in the block page
+
