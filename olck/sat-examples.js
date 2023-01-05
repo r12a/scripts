a35de8755e4ebc0d2a748b4f2d9b8ecc3eaa82ff
r12a <ishida@w3.org>
2023-01-05 17:21:51 +0000

    Stub for new olck directory

---
+++ olck/sat-examples.js
@@ -0,0 +1,141 @@
+/*
+@ Source: 
+@ Source: https://en.wiktionary.org/wiki/Category:Santali_terms_with_IPA_pronunciation
+§ 'Noto Sans Ol Chiki'
+# 200
+~ ltr
+! sat
+*/
+
+autoExpandExamples.sat = `
+
+ᱤᱧ|I||iñ
+ᱟᱢ|you (singular)||am
+ᱩᱱᱤ|he||uni
+ᱟᱹᱞᱤᱧ|we||ăliñ
+ᱟᱞᱮ|we||ale
+ᱟᱹᱵᱤᱱ|you (plural)||ăbin
+ᱟᱯᱮ|you (plural)|ape
+ᱩᱱᱠᱤᱱ|they|unkin
+ᱩᱱᱠᱩ|they||unku
+ᱱᱚᱣᱟ|this|nɔwa|nôva
+ᱚᱱᱟ|that||ôna
+ᱱᱚᱰᱮ|here|nɔɖe|nôḍe
+ᱦᱟᱸᱰᱮ|there||hãḍe
+ᱚᱠᱚᱭ|who||ôkôy
+ᱪᱮᱫ|what||cet’
+ᱚᱠᱟᱨᱮ|where||ôkare
+ᱛᱤᱨᱮ|when||tire
+ᱛᱤᱥ|when||tis
+ᱪᱮᱫ ᱞᱮᱠᱟᱛᱮ|how||cet’ lekate
+ᱵᱟᱝ|not||baṅ
+ᱡᱚᱛᱚ|all|ӡoto|jôtô
+ᱡᱷᱚᱛᱚ|all|ɟʰɔtɔ|jhôtô
+ᱟᱭᱢᱟ|many||ayma
+ᱟᱫᱚᱢ|some||adôm
+ᱛᱷᱚᱲᱟ|some, few|tʰɔɽa|thôṛa
+ᱮᱴᱟᱜ|other|eʈak̚|eṭak’
+ᱢᱤᱫ|one||mit’
+ᱵᱟᱨ|two||bar
+ᱯᱮ|three||pe
+ᱢᱚᱬᱮ|five|mɔɳe|môṇe
+ᱛᱩᱨᱩᱭ|six|turuj|turuy
+ᱮᱭᱟᱭ|seven|ejaj|eyay
+ᱤᱨᱟᱹᱞ|eight|irəl|irăl
+ᱟᱨᱮ|nine||are
+ᱜᱮᱞ|ten||gel
+ᱢᱟᱨᱟᱝ|big|maraŋ|maraṅ
+ᱡᱤᱞᱤᱧ|long|ɟiliɲ|jiliñ
+ᱚᱥᱟᱨ|wide||ôsar
+ᱤᱵᱤᱞ|thick||ibil
+ᱦᱟᱢᱟᱞ|heavy||hamal
+ᱠᱟᱴᱤᱡ|small||kaṭic’
+ᱦᱩᱰᱤᱧ|small||huḍiñ
+ᱠᱟᱴᱤᱡ|short||kaṭic’
+ᱠᱩᱲᱤ|woman|kuɽi|kuṛi
+ᱠᱚᱲᱟ|man (adult male)|kɔɽa|kôṛa
+ᱦᱚᱲ|man (human being)||hôṛ
+ᱜᱤᱫᱨᱟ|child||gidra
+ᱦᱚᱯᱚᱱ|child||hôpôn
+ᱵᱟᱦᱩ|wife||bahu
+ᱮᱨᱚ|wife||erô
+ᱡᱟᱶᱟᱭ|husband|ɟaw̃aj|jaw̃ay
+ᱜᱚᱜᱚ|mother||gôgô
+ᱟᱭᱚ|mother||ayô
+ᱮᱝᱜᱚ|mother|eŋɡɔ|eṅgô
+ᱵᱟᱵᱟ|father||baba
+ᱟᱯᱟ|father|apa|apa
+ᱡᱤᱣᱟᱹᱞᱤ|animal||jivăli
+ᱦᱟᱹᱠᱩ|fish||hăku
+ᱪᱮᱬᱮ|bird|ceɳe|ceṇe
+ᱥᱮᱛᱟ|dog|seta|seta
+ᱥᱤ|louse||si
+ᱵᱤᱝ|snake||biṅ
+ᱛᱤᱡᱩ|worm||tiju
+
+ᱦᱮᱸᱫᱮ|black|hẽde
+ᱵᱮᱺᱫᱤ|paddy storage|bə̃di
+ᱡᱭᱻᱭᱤ|soul|jiːyi
+ᱢᱚᱹᱬᱮᱻ ᱢᱚᱸᱻᱦᱟ|five days|mɔɳeː mãːha
+ᱫᱟᱜᱽ|mark|daɡ
+ᱫᱟᱜᱤ|marked|daɡi
+ᱨᱚᱡᱽ|rule|raɟ
+ᱨᱚᱡᱚ|king|raɟa
+ᱜᱤᱫᱽᱨᱟ|child|ɡidra
+ᱜᱤᱫᱤ|vulture|ɡidi
+ᱩᱵᱽ|pour (from a pot)|ub
+ᱵᱤᱨ|forest|bir
+ᱫᱚᱜ|water|dak̚
+ᱨᱚᱜ|cry|rak̚
+ᱨᱮᱡ|snatch|rec̚
+ᱢᱩᱡ|ant|muc̚
+ᱜᱳᱫ|pluck|got̚
+ᱢᱮᱫ|eye|met̚
+ᱩᱵ|hair|up̚
+ᱢᱮᱱᱟᱜᱼᱟ|have|menaka|menak’a
+ᱟᱠᱟᱫᱼᱟ|?|akat|akat’
+ᱦᱤᱡᱩᱜᱼᱟ|come|hijuka|hijuk’a
+ᱥᱩᱱᱭᱚ|zero|sunjɔ
+
+ᱚᱡᱚ|grandfather|ɔd͡ʒɔ
+ᱟᱛᱳ|village|ɔto
+ᱡᱟᱝ|bone|jaŋ/ɖʒaŋ/d͡ʒa(ː)ŋ/ɟaŋ
+ᱥᱮᱝᱜᱮᱞ|fire|seŋɡel/sɛŋɡɛl
+ᱯᱳᱱ|four|poːn
+ᱳᱞ ᱪᱮᱢᱮᱫ|Ol Cemet|ol cemetˈ
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+`
+
---
+++ olck/sat-langdata.js
@@ -0,0 +1,16 @@
+// this should always be updated when app-charuse/langs.js is updated
+
+var langs = {
+
+"sat": { name:"Santali (Santhali)", local:"ᱥᱟᱱᱛᱟᱲᱤ", 
+source:"7e9f0d5d372afc701b3e3a5bc5b45a1d8375eeca", 
+region:"sasia", countries:"India, Bangladesh, Nepal", script:"olck", speakers:"7340000", 
+letter:"ᱯᱵᱛᱫᱪᱰᱴᱡᱠᱜᱥᱦᱢᱱᱧᱬᱝᱣᱶᱨᱲᱞᱭᱤᱩᱮᱳᱚᱟᱹᱸᱺᱻᱽᱼᱷ", number:"᱐᱑᱒᱓᱔᱕᱖᱗᱘᱙", punctuation:"᱾᱿“”‘’", 
+orth:`Ol Chiki. &nbsp; <a href="../scripts/santali/" target="_blank">Details</a>.`,
+type:"alphabet", cs:"no", gpos:"no", gsub:"no", cursive:"no", direction:"ltr", wordsep:"space", baseline:"mid", wrap:"word", hyphenation:"?",  justification:"sp",
+vowels:"let:6 comp:3",
+linked:"santali", fonts:"/pickers/olck/", 
+},
+
+
+}
