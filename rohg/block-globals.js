1bf1f50015b8c5eba7686f6a3a4d44612ddaab4e
r12a <ishida@w3.org>
2023-01-05 18:53:46 +0000

    rohg: 1st draft of v29

---
+++ rohg/block-globals.js
@@ -0,0 +1,11 @@
+
+window.version = 'v28'
+window.scriptName = 'Hanifi Rohingya'
+window.ghDirectory = 'rohg'
+window.blockDirectory = 'rogh'
+window.languageList = ['rhg'] // lang tags for each orthography handled
+window.detailsfileList = ['rhg'] // first part of xx-details for each orthography
+window.scriptISO = 'rohg'
+window.webfonts = ["Noto Sans Hanifi Rohingya WF"]
+window.defaultSize = '24px'
+
---
+++ rohg/rhg-globals.js
@@ -0,0 +1,23 @@
+
+langTag='rhg' // BCP47 language tag for this orthography
+scriptTag='rohg' // BCP47 script tag for this orthography
+notesLangtag='rhg'
+blockLangtag = 'rhg' // same as langTag; used in block.html
+blockDirection='rtl'  // ltr/rtl or '' for ltr; used by block.html
+
+blockDirectoryName='rohg'  // folder name where block file is found, no slash
+orthogFilePath='rohg/rhg'  // path below /scripts, including filename, but no extension, eg. lana/nod
+pickerDir='rohg'  // directory name for picker, no slash at end
+scriptSummaryTableName='rhg'  // id used for langs record in xx-langdata.js; used to make summary table
+charUsageBCP='rhg'  // bcp tag to find the orthography in the charuse app; used in makeIndexIntro only
+orthogName = 'Hanifi Rohingya' // natural language name for the orthography; used only in makeIndexIntro
+languageName = 'Rohingya' // natural language name of language; used in various places, esp block
+scriptIsIndic = false  // true if there are indic syllabic categories; used only in makeIndexIntro
+
+
+defaultSize = '24px'  // used by block.html to set size of font, including in font mgt panel
+letterClassName = 'rhg'  // classname for a given orthography in the block page
+
+
+
+
