ba46a56de573a270fdad2ac2a6959482b6dfe2bd
r12a <ishida@w3.org>
2023-01-05 17:09:28 +0000

    grek: 1st draft of v29

---
+++ grek/block-globals.js
@@ -0,0 +1,11 @@
+
+window.version = 'v28'
+window.scriptName = 'Greek'
+window.ghDirectory = 'greek'
+window.blockDirectory = 'greek'
+window.languageList = ['el'] // lang tags for each orthography handled
+window.detailsfileList = ['el'] // first part of xx-details for each orthography
+window.scriptISO = 'grek'
+window.webfonts = ["Gentium Plus WF"]
+window.defaultSize = '24px'
+
---
+++ grek/el-globals.js
@@ -0,0 +1,23 @@
+
+langTag='el' // BCP47 language tag for this orthography
+scriptTag='grek' // BCP47 script tag for this orthography
+notesLangtag='el'
+blockLangtag = 'el' // same as langTag; used in block.html
+blockDirection=''  // ltr/rtl or '' for ltr; used by block.html
+
+blockDirectoryName='grek'  // folder name where block file is found, no slash
+orthogFilePath='grek/el'  // path below /scripts, including filename, but no extension, eg. lana/nod
+pickerDir='grek'  // directory name for picker, no slash at end
+scriptSummaryTableName='el'  // id used for langs record in xx-langdata.js; used to make summary table
+charUsageBCP='el'  // bcp tag to find the orthography in the charuse app; used in makeIndexIntro only
+orthogName = 'Greek' // natural language name for the orthography; used only in makeIndexIntro
+languageName = 'Modern Greek' // natural language name of language; used in various places, esp block
+scriptIsIndic = false  // true if there are indic syllabic categories; used only in makeIndexIntro
+
+
+defaultSize = '24px'  // used by block.html to set size of font, including in font mgt panel
+letterClassName = 'el'  // classname for a given orthography in the block page
+
+
+
+
