2de6cf5276f0fffd7e3f86d2e1cbd448c36737bf
r12a <ishida@w3.org>
2023-01-05 16:59:22 +0000

    Stub for new grek directory

---
+++ grek/el-examples.js
@@ -0,0 +1,293 @@
+/*
+@ Source: https://en.wiktionary.org/wiki/Appendix:Greek_Swadesh_list
+§ 'Gentium Plus'
+# 200
+~ ltr
+! el
+*/
+
+autoExpandExamples.el = `
+Έλληνας|Greek|ˈelinas
+ευφυΐα|intelligence|efiˈia
+νεράιδα|fairy|neˈɾai̯.ða
+
+εγώ|I|eˈɣo|egó
+εσύ|you (singular)|eˈsi|esý
+εσείς|you (singular) (formal)|eˈsis|eseís
+αυτός|he|afˈtos|aftós
+εμείς|we|eˈmis|emeís
+εσείς|you (plural)|eˈsis|eseís
+αυτοί|they|afˈti|aftoí
+αυτός|this|aˈftos|aftós
+εκείνος|that|eˈcino|ekeínos
+εδώ|here|eˈðo|edó
+εκεί|there|eˈci|ekeí
+ποιος|who|pços|poios
+τι|what|ti|ti
+πού|where|pu|poú
+πότε|when|ˈpote|póte
+πώς|how|pos|pós
+όλοι|all|ˈoli|óloi
+πολλοί|many|poˈli|polloí
+μερικοί|some|meriˈci|merikoí
+λίγοι|few|ˈliʝi|lígoi
+άλλοι|other|ˈali|álloi
+ένα|one|ˈena|éna
+δύο|two|ˈðio|dýo
+τρία|three|ˈtria|tría
+τέσσερα|four|ˈtesera|téssera
+πέντε|five|ˈpende|pénte
+μεγάλος|big|meˈɣalos|megálos
+μακρύς|long|maˈkris|makrýs
+πλατύς|wide|plaˈtis|platýs
+φαρδύς|wide|farˈðis|fardýs
+παχύς|thick|paˈçis|pachýs
+χοντρός|thick|xonˈdros|chontrós
+βαρύς|heavy|vaˈris|varýs
+μικρός|small|miˈkros|mikrós
+κοντός|short|koˈndos|kontós
+στενός|narrow|steˈnos|stenós
+λεπτός|thin|leˈptos|leptós
+γυναίκα|woman|ʝiˈneka|gynaíka
+άνδρας|man (male)|ˈandras|ándras
+άνθρωπος|man (human)|ˈan̪θropos|ánthropos
+παιδί|child|peˈði|paidí
+σύζυγος|wife|ˈsiziɣos|sýzygos
+γυναίκα|wife|ʝiˈneka|gynaíka
+σύζυγος|husband|ˈsiziɣos|sýzygos
+άντρας|husband|ˈandras|ántras
+μητέρα|mother|mitéra|miˈtera
+μάνα|mother|ˈmana|mána
+πατέρας|father|paˈteras|patéras
+ζώο|animal|ˈzoo|zóo
+ψάρι|fish|ˈpsari|psári
+πουλί|bird|puˈli|poulí
+σκύλος|dog|ˈscilos|skýlos
+ψείρα|louse|ˈpsira|pseíra
+φίδι|snake|ˈfiði|fídi
+σκουλήκι|worm|skuˈlici|skoulíki
+δέντρο|tree|ˈðendro|déntro
+δάσος|forest|ˈðasos|dásos
+ραβδί|stick|raˈvði|ravdí
+βέργα|stick|ˈverɣa|vérga
+φρούτο|fruit|ˈfruto|froúto
+σπόρος|seed|ˈsporos|spóros
+φύλλο|leaf|ˈfilo|fýllo
+ρίζα|root|ˈriza|ríza
+φλοιός|bark (of a tree)|fliˈos|floiós
+λουλούδι|flower|luˈluði|louloúdi
+χορτάρι|grass|xorˈtari|chortári
+σχοινί|rope|sçiˈni|schoiní
+δέρμα|skin|ˈðerma|dérma
+κρέας|meat|ˈkreas|kréas
+αίμα|blood|ˈema|aíma
+κόκαλο|bone|ˈkokalo|kókalo
+λίπος|fat (noun)|ˈlipos|lípos
+αυγό|egg|avˈɣo|avgó
+κέρατο|horn|ˈcerato|kérato
+ουρά|tail|uˈra|ourá
+πούπουλο|feather|ˈpupulo|poúpoulo
+φτερό|feather|fteˈro|fteró
+τρίχα|hair|ˈtrixa|trícha 
+μαλλιά|hair|maˈʎa|malliá 
+κεφάλι|head|ceˈfali|kefáli
+αυτί|ear|afˈti|aftí
+μάτι|eye|ˈmati|máti
+μύτη|nose|ˈmiti|mýti
+στόμα|mouth|ˈstoma|stóma
+δόντι|tooth|ˈðondi|dónti
+γλώσσα|tongue|ˈɣlosa|glóssa
+νύχι|fingernail|ˈniçi|nýchi
+πόδι|foot|ˈpoði|pódi
+πόδι|leg|ˈpoði|pódi
+γόνατο|knee|ˈɣonato|gónato
+χέρι|hand|ˈçeri|chéri
+φτερό|wing|fteˈro|fteró
+κοιλιά|belly|ciˈʎa|koiliá
+σπλάχνα|guts|ˈsplaxna|spláchna
+εντόσθια|guts|eˈndosθia|entósthia
+λαιμός|neck|leˈmos|laimós
+πλάτη|back|ˈplati|pláti
+στήθος|breast|ˈstiθos|stíthos
+καρδιά|heart|karˈðʝa|kardiá
+συκώτι|liver|siˈkoti|sykóti
+πίνω|to drink|ˈpino|píno
+τρώ[γ]ω|to eat|ˈtro[ɣ]o
+δαγκώνω|to bite|ðaˈŋɡono|dagkóno
+ρουφώ|to suck|ruˈfo|roufó
+φτύνω|to spit|ˈftino|ftýno
+ξερνώ|to vomit|kserˈno|xernó
+φυσάω|to blow|fiˈsao|fysáo
+αναπνέω|to breathe|anaˈpneo|anapnéo
+ανασαίνω|to breathe|anaˈseno|anasaíno
+γελάω|to laugh|ʝeˈlao|geláo
+βλέπω|to see|ˈvlepo|vlépo
+ακούω|to hear|aˈkuo|akoúo
+ξέρω|to know|ˈksero|xéro
+γνωρίζω|to know|ɣnoˈrizo|gnorízo
+σκέφτομαι|to think|ˈsceftome|skéftomai
+νομίζω|to think|ˈsceftome|nomízo
+μυρίζω|to smell|miˈrizo|myrízo
+φοβάμαι|to fear|foˈvame|fovámai
+κοιμάμαι|to sleep|ciˈmame|koimámai
+ζω|to live|zo|zo
+πεθαίνω|to die|peˈθeno|pethaíno
+σκοτώνω|to kill|skoˈtono|skotóno
+μαλώνω|to fight|maˈlono|malóno
+κυνηγώ|to hunt|ciniˈɣo|kynigó
+χτυπώ|to hit|xtiˈpo|chtypó
+κόβω|to cut|ˈkovo|kóvo
+χωρίζω|to split|xoˈrizo|chorízo
+μαχαιρώνω|to stab|maçeˈrono|machairóno
+ξύνω|to scratch|ˈksino|xýno
+σκάβω|to dig|ˈskavo|skávo
+κολυμπώ|to swim|kolimˈbo|kolympó
+πετάω|to fly|peˈtao|petáo
+περπατώ|to walk|perpaˈto|perpató
+έρχομαι|to come|ˈerxome|érchomai
+ξαπλώνω|to lie (as in a bed)|ksaˈplono|xaplóno
+πλαγιάζω|to lie (as in a bed)|plaˈʝazo|plagiázo
+κάθομαι|to sit|ˈkaθome|káthomai
+στέκομαι|to stand|ˈstekome|stékomai
+γυρίζω|to turn (intransitive)|ʝiˈrizo|gyrízo
+πέφτω|to fall|ˈpefto|péfto
+δίνω|to give|ˈðino|díno
+κρατάω|to hold|kraˈtao|kratáo
+ζουλάω|to squeeze|zuˈlao|zouláo
+τρίβω|to rub|ˈtrivo|trívo
+πλένω|to wash|ˈpleno|pléno
+σκουπίζω|to wipe|skuˈpizo|skoupízo
+τραβώ|to pull|traˈvo|travó
+σπρώχνω|to push|ˈzbroxno|spróchno
+ρίχνω|to throw|ˈrixno|ríchno
+πετώ|to throw|ˈrixno|petó
+δένω|to tie|ˈðeno|déno
+ράβω|to sew|ˈravo|rávo
+μετράω|to count|meˈtrao|metráo
+λέω|to say|ˈleo|léo
+τραγουδώ|to sing|traɣuˈðo|tragoudó
+παίζω|to play|ˈpezo|paízo
+πλέω|to float|ˈpleo|pléo
+επιπλέω|to float|epiˈpleo|epipléo
+τρέχω|to flow|ˈtrexo|trécho
+παγώνω|to freeze|paˈɣono|pagóno
+φουσκώνω|to swell|fuˈskono|fouskóno
+πρήζομαι|to swell|ˈprizome|prízomai
+ήλιος|sun|ˈiʎos|ílios
+φεγγάρι|moon|feˈŋɡari|fengári
+Σελήνη|moon|seˈlini|Selíni
+αστέρι|star|aˈsteri|astéri
+άστρο|star|aˈsteri|ástro
+νερό|water|neˈro|neró
+βροχή|rain|vroˈçi|vrochí
+ποτάμι|river|poˈtami|potámi
+λίμνη|lake|ˈlimni|límni
+θάλασσα|sea|ˈθalasa|thálassa
+αλάτι|salt|aˈlati|aláti
+πέτρα|stone|ˈpetra|pétra
+άμμος|sand|ˈamos|ámmos
+σκόνη|dust|ˈskoni|skóni
+γη|earth|ʝi|gi
+σύννεφο|cloud|ˈsinefo|sýnnefo
+ομίχλη|fog|oˈmixli|omíchli
+ουρανός|sky|uraˈnos|ouranós
+αέρας|wind|aˈeras|aéras
+χιόνι|snow|ˈçoni|chióni
+πάγος|ice|ˈpaɣos|págos
+καπνός|smoke|kaˈpnos|kapnós
+φωτιά|fire|foˈtça|fotiá
+στάχτη|ash|ˈstaxti|stáchti
+καίω|to burn|ˈceo|kaío
+δρόμος|road|ˈðromos|drómos
+βουνό|mountain|vuˈno|vounó
+κόκκινος|red|ˈkocinos|kókkinos
+πράσινος|green|ˈprasinos|prásinos
+κίτρινος|yellow|ˈcitrinos|kítrinos
+άσπρος|white|ˈaspros|áspros
+μαύρος|black|ˈmavros|mávros
+νύχτα|night|ˈnixta|nýchta
+μέρα|day|ˈmera|méra
+χρόνος|year|xroˈnos|chrónos
+ζεστός|warm|zeˈstos|zestós
+κρύος|cold|ˈkrios|krýos
+γεμάτος|full|ʝeˈmatos|gemátos
+καινούργιος|new|ceˈnurʝos|kainoúrgios
+παλιός|old|paˈʎos|paliós
+καλός|good|kaˈlos|kalós
+κακός|bad|kaˈkos|kakós
+σάπιος|rotten|ˈsapços|sápios
+βρόμικος|dirty|ˈvromikos|vrómikos
+ίσιος|straight|ˈisços|ísios
+στρογγυλός|round|stroɲɟiˈlos|strongylós
+κοφτερός|sharp (as a knife)|kofteˈros|kofterós
+στομωμένος|dull (as a knife)|stomoˈmenos|stomoménos
+λείος|smooth|ˈlios|leíos
+βρεγμένος|wet|vreɣˈmenos|vregménos
+υγρός|wet|iˈɣros|ygrós
+στεγνός|dry|steˈɣnos|stegnós
+ξερός|dry|kseˈros|xerós
+σωστός|correct|soˈstos|sostós
+κοντά|near|koˈnda|kontá
+μακριά|far|makriˈa|makriá
+δεξιά|right|ðeksiˈa|dexiá
+αριστερά|left|aristeˈra|aristerá
+σε|at|se|se
+μέσα|in|ˈmesa se|mésa
+με|with|me|me
+και|and|ce|kai
+εάν|if|eˈan|eán
+αν|if|an|an
+επειδή|because|epiˈði|epeidí
+γιατί|because|ʝaˈti|giatí
+όνομα|name|ˈonoma|ónoma
+
+μια|a|mɲa
+βια|violence|ˈvʝa
+κοροϊδεύω|to mock|kɔɾɔiˈðɛvɔ
+μποϊκοτάρω|I boycott|bojkoˈtaro
+αυλός|flute|aˈvlos
+Ταΰγετος|Mount Taygetus
+μπορώ|can|boˈɾo
+δεν πάω|I am not going|ðen 
+μπύρα|beer|bira
+φάση|phase|ˈfasi
+βάση|base|ˈvasi
+μόνος|alone|ˈmonos
+νόμος|law|ˈnomos
+τείνω|I tend|ˈtino
+ντύνω|I dress|ˈdino
+θέμα|topic|ˈθema
+δέμα|parcel|ˈðema
+σώα|safe|ˈsoa
+ζώα|animals|ˈzoa
+ρήμα|verb|ˈrima
+λίμα|nail file|ˈlima
+κόμμα|comma|ˈkoma
+χώμα|soil|ˈxoma
+γόμα|eraser|ˈɣoma
+γκάμα|range|ˈɡama
+μπροστά|in the front|bɾoˈsta
+ντοκιμαντέρ|documentary
+νταντά|nanny|daˈda
+ντους|shower|ˈdus
+φαίνομαι|look|ˈfenome
+λατομείο|quarry|latoˈmio
+νεράιδα|fairy|neˈɾai̯ða
+εφηύρα|I invented|efˈivra
+καϊμάν|caiman|kaiˈman
+Ναΐτες|Templars|ˈnaites
+ή|or|ˈi
+πώς;|how?|ˈpos
+πού;|where?|ˈpu
+αδελφοί|brothers|aðelˈfi
+Φίλιππος Βʹ|Philip II (of Macedon)
+
+
+
+
+
+
+
+
+`
+
