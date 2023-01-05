a4b18c74d208f20236d913e9517f5223b678c7ab
r12a <ishida@w3.org>
2023-01-05 08:21:28 +0000

    kali: 1st draft of v29

---
+++ kali/block-globals.js
@@ -0,0 +1,11 @@
+
+window.version = 'v28'
+window.scriptName = 'Kayah Li'
+window.ghDirectory = 'kali'
+window.blockDirectory = 'kali'
+window.languageList = ['kyu'] // lang tags for each orthography handled
+window.detailsfileList = ['kyu'] // first part of xx-details for each orthography
+window.scriptISO = 'kali'
+window.webfonts = ["Noto Sans Kayah Li WF"]
+window.defaultSize = '24px'
+
---
+++ kali/kyu-globals.js
@@ -0,0 +1,21 @@
+
+
+
+langTag='kyu' // BCP47 language tag for this orthography
+scriptTag='kali' // BCP47 script tag for this orthography
+notesLangtag='kyu'
+blockLangtag = 'kyu' // same as langTag; used in block.html
+blockDirection=''  // ltr/rtl or '' for ltr; used by block.html
+
+blockDirectoryName='kali'  // folder name where block file is found, no slash
+orthogFilePath='kali/kyu'  // path below /scripts, including filename, but no extension, eg. lana/nod
+pickerDir='kali'  // directory name for picker, no slash at end
+scriptSummaryTableName='kyu'  // id used for langs record in xx-langdata.js; used to make summary table
+charUsageBCP='kyu'  // bcp tag to find the orthography in the charuse app; used in makeIndexIntro only
+orthogName = 'Kayah Li' // natural language name for the orthography; used only in makeIndexIntro
+languageName = 'Western Kayah' // natural language name of language; used in various places, esp block
+scriptIsIndic = true  // true if there are indic syllabic categories; used only in makeIndexIntro
+
+
+defaultSize = '24px'  // used by block.html to set size of font, including in font mgt panel
+letterClassName = 'kyu'  // classname for a given orthography in the block page
