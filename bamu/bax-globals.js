c7123bae464763fff617d1a42e94656ea93bf096
r12a <ishida@w3.org>
2023-01-05 10:41:26 +0000

    bamu: 1st draft of v29

---
+++ bamu/bax-globals.js
@@ -0,0 +1,19 @@
+langTag='bax' // BCP47 language tag for this orthography
+scriptTag='bamu' // BCP47 script tag for this orthography
+notesLangtag='bax'
+blockLangtag = 'bax' // same as langTag; used in block.html
+blockDirection=''  // ltr/rtl or '' for ltr; used by block.html
+
+blockDirectoryName='bamu'  // folder name where block file is found, no slash
+orthogFilePath='bamu/bax'  // path below /scripts, including filename, but no extension, eg. lana/nod
+pickerDir='bamu'  // directory name for picker, no slash at end
+scriptSummaryTableName='bax-bamu'  // id used for langs record in xx-langdata.js; used to make summary table
+charUsageBCP='bax-bamu'  // bcp tag to find the orthography in the charuse app; used in makeIndexIntro only
+orthogName = 'Bamum' // natural language name for the orthography; used only in makeIndexIntro
+languageName = 'Bamun' // natural language name of language; used in various places, esp block
+scriptIsIndic = false  // true if there are indic syllabic categories; used only in makeIndexIntro
+
+
+defaultSize = '24px'  // used by block.html to set size of font, including in font mgt panel
+letterClassName = 'bax'  // classname for a given orthography in the block page
+
---
+++ bamu/block-globals.js
@@ -0,0 +1,11 @@
+
+window.version = 'v28'
+window.scriptName = 'Bamum'
+window.ghDirectory = 'bamum'
+window.blockDirectory = 'bamum'
+window.languageList = ['bax'] // lang tags for each orthography handled
+window.detailsfileList = ['bax'] // first part of xx-details for each orthography
+window.scriptISO = 'bamu'
+window.webfonts = ["Noto Sans Bamum WF"]
+window.defaultSize = '24px'
+
