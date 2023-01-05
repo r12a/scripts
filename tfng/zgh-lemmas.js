f957a10ddd89f8b6924f150a38e06c7c1b0a6fd8
r12a <ishida@w3.org>
2023-01-05 17:10:53 +0000

    Stub for new tfng directory

---
+++ tfng/zgh-lemmas.js
@@ -0,0 +1,133 @@
+/*
+@ Source: https://en.wiktionary.org/wiki/Category:Moroccan_Amazigh_lemmas
+§ 'Noto Sans Tifinagh'
+# 200
+~ ltr
+! zgh
+*/
+
+
+autoExpandExamples.zgh = `
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴰ#Moroccan_Amazigh">ⴰⴱⴰ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴰⴷⴰ#Moroccan_Amazigh">ⴰⴱⴰⴷⴰ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴰⴷⴷⵏ#Moroccan_Amazigh">ⴰⴱⴰⴷⴷⵏ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴰⴷⴷⵓⵙ#Moroccan_Amazigh">ⴰⴱⴰⴷⴷⵓⵙ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴰⴷⴷⵔ#Moroccan_Amazigh">ⴰⴱⴰⴷⴷⵔ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴰⴷⵉⵔ#Moroccan_Amazigh">ⴰⴱⴰⴷⵉⵔ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴰⴷⵓ#Moroccan_Amazigh">ⴰⴱⴰⴷⵓ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴰⴹⴰⵕ#Moroccan_Amazigh">ⴰⴱⴰⴹⴰⵕ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴰⴹⵅⴰⵅ#Moroccan_Amazigh">ⴰⴱⴰⴹⵅⴰⵅ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴰⴹⵛ#Moroccan_Amazigh">ⴰⴱⴰⴹⵛ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴰⴽⴽⵓ#Moroccan_Amazigh">ⴰⴱⴰⴽⴽⵓ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴰⴽⵛⵛⵓ#Moroccan_Amazigh">ⴰⴱⴰⴽⵛⵛⵓ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴰⵄⵓⵊ#Moroccan_Amazigh">ⴰⴱⴰⵄⵓⵊ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴰⵄⵕⵓⵊ#Moroccan_Amazigh">ⴰⴱⴰⵄⵕⵓⵊ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴰⵅⵅⵓ#Moroccan_Amazigh">ⴰⴱⴰⵅⵅⵓ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴰⵇⵇⴰⵔ#Moroccan_Amazigh">ⴰⴱⴰⵇⵇⴰⵔ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴰⵇⵇⴰⵢ#Moroccan_Amazigh">ⴰⴱⴰⵇⵇⴰⵢ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴰⵇⵇⵉⵙ#Moroccan_Amazigh">ⴰⴱⴰⵇⵇⵉⵙ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴰⵇⵍⵓ#Moroccan_Amazigh">ⴰⴱⴰⵇⵍⵓ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴰⵇⵜⵉⵛ#Moroccan_Amazigh">ⴰⴱⴰⵇⵜⵉⵛ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴰⵍⴰⵍ#Moroccan_Amazigh">ⴰⴱⴰⵍⴰⵍ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴰⵍⴱⴱⴰ#Moroccan_Amazigh">ⴰⴱⴰⵍⴱⴱⴰ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴰⵍⵍⴰⵖ#Moroccan_Amazigh">ⴰⴱⴰⵍⵍⴰⵖ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴰⵍⵖⴰ#Moroccan_Amazigh">ⴰⴱⴰⵍⵖⴰ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴰⵏⴷⵓ#Moroccan_Amazigh">ⴰⴱⴰⵏⴷⵓ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴰⵏⴹⵕⵉⵡ#Moroccan_Amazigh">ⴰⴱⴰⵏⴹⵕⵉⵡ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴰⵏⵓⵇ#Moroccan_Amazigh">ⴰⴱⴰⵏⵓⵇ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴰⵔⴰⴳ#Moroccan_Amazigh">ⴰⴱⴰⵔⴰⴳ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴰⵔⴰⵏ#Moroccan_Amazigh">ⴰⴱⴰⵔⴰⵏ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴰⵔⴰⵔ#Moroccan_Amazigh">ⴰⴱⴰⵔⴰⵔ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴰⵔⴰⵡ#Moroccan_Amazigh">ⴰⴱⴰⵔⴰⵡ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴰⵔⴰⵣ#Moroccan_Amazigh">ⴰⴱⴰⵔⴰⵣ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴰⵔⵉⵇ#Moroccan_Amazigh">ⴰⴱⴰⵔⵉⵇ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴰⵕⵕⴰⵢ#Moroccan_Amazigh">ⴰⴱⴰⵕⵕⴰⵢ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴰⵖⵓⵔ#Moroccan_Amazigh">ⴰⴱⴰⵖⵓⵔ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴰⵖⵓⵖ#Moroccan_Amazigh">ⴰⴱⴰⵖⵓⵖ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴰⵖⵓⵙ#Moroccan_Amazigh">ⴰⴱⴰⵖⵓⵙ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴰⵖⵔⴰⵔ#Moroccan_Amazigh">ⴰⴱⴰⵖⵔⴰⵔ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴰⵙⴰⵍ#Moroccan_Amazigh">ⴰⴱⴰⵙⴰⵍ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴰⵙⵓⵔ#Moroccan_Amazigh">ⴰⴱⴰⵙⵓⵔ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴰⵚⴰⴹ#Moroccan_Amazigh">ⴰⴱⴰⵚⴰⴹ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴰⵚⵚⴰ#Moroccan_Amazigh">ⴰⴱⴰⵚⵚⴰ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴰⵛⵉⵍ#Moroccan_Amazigh">ⴰⴱⴰⵛⵉⵍ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴰⵛⵛⴰⴹ#Moroccan_Amazigh">ⴰⴱⴰⵛⵛⴰⴹ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴰⵛⵛⵉⵅ#Moroccan_Amazigh">ⴰⴱⴰⵛⵛⵉⵅ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴰⵛⵛⵔ#Moroccan_Amazigh">ⴰⴱⴰⵛⵛⵔ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴰⵜⵓⵍ#Moroccan_Amazigh">ⴰⴱⴰⵜⵓⵍ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴰⵟⵟⴰⵃ#Moroccan_Amazigh">ⴰⴱⴰⵟⵟⴰⵃ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴰⵡ#Moroccan_Amazigh">ⴰⴱⴰⵡ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴰⵡⴱⴰⵡ#Moroccan_Amazigh">ⴰⴱⴰⵡⴱⴰⵡ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴰⵡⵊ#Moroccan_Amazigh">ⴰⴱⴰⵡⵊ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴰⵢⵏⵓ#Moroccan_Amazigh">ⴰⴱⴰⵢⵏⵓ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴰⵢⵓⵖ#Moroccan_Amazigh">ⴰⴱⴰⵢⵓⵖ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴰⵢⵕⵕⵓ#Moroccan_Amazigh">ⴰⴱⴰⵢⵕⵕⵓ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴰⵢⵢⵓⵕ#Moroccan_Amazigh">ⴰⴱⴰⵢⵢⵓⵕ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴱⴰⵙⴽⴰⵔ#Moroccan_Amazigh">ⴰⴱⴱⴰⵙⴽⴰⵔ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴱⵃⵍⵓⵍ#Moroccan_Amazigh">ⴰⴱⴱⵃⵍⵓⵍ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴱⵄⵓⵢ#Moroccan_Amazigh">ⴰⴱⴱⵄⵓⵢ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴱⵅⵜⵓⵜⵉ#Moroccan_Amazigh">ⴰⴱⴱⵅⵜⵓⵜⵉ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴱⵉⵛ#Moroccan_Amazigh">ⴰⴱⴱⵉⵛ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴱⵓⵀⵍⵍⵓ#Moroccan_Amazigh">ⴰⴱⴱⵓⵀⵍⵍⵓ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴱⵓⵄ#Moroccan_Amazigh">ⴰⴱⴱⵓⵄ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴱⵓⵖ#Moroccan_Amazigh">ⴰⴱⴱⵓⵖ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴱⵓⵖⵣⵍ#Moroccan_Amazigh">ⴰⴱⴱⵓⵖⵣⵍ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴱⵛⵜⵔ#Moroccan_Amazigh">ⴰⴱⴱⵛⵜⵔ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴳⵓ#Moroccan_Amazigh">ⴰⴱⴳⵓ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴳⵓⵔ#Moroccan_Amazigh">ⴰⴱⴳⵓⵔ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴱⴷⴰ#Moroccan_Amazigh">ⴰⴱⴷⴰ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⴽⵯⴼⴰⵢ#Moroccan_Amazigh">ⴰⴽⵯⴼⴰⵢ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⵖⵖⵓ#Moroccan_Amazigh">ⴰⵖⵖⵓ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⵛⴼⴼⴰⵢ#Moroccan_Amazigh">ⴰⵛⴼⴼⴰⵢ</a>
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⴰⵡⴰⵍ#Moroccan_Amazigh">ⴰⵡⴰⵍ</a>
+
+
+<a target="_blank" href="https://en.wiktionary.org/wiki/ⵡⵉⵏⵓ#Moroccan_Amazigh">ⵡⵉⵏⵓ</a>
+
+`
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
