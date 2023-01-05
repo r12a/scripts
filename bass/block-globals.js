90218c26aafdfb6bf8e15b35d02332261ea4372c
r12a <ishida@w3.org>
2023-01-05 16:36:12 +0000

    bass: 1st draft of v29

---
+++ bass/block-globals.js
@@ -0,0 +1,11 @@
+
+window.version = 'v28'
+window.scriptName = 'Bassa Vah'
+window.ghDirectory = 'bassavah'
+window.blockDirectory = 'bassavah'
+window.languageList = ['bsq'] // lang tags for each orthography handled
+window.detailsfileList = ['bsq'] // first part of xx-details for each orthography
+window.scriptISO = 'bass'
+window.webfonts = ["Noto Sans Bassah Vah WF"]
+window.defaultSize = '24px'
+
---
+++ bass/bsq-globals.js
@@ -0,0 +1,23 @@
+
+langTag='bsq' // BCP47 language tag for this orthography
+scriptTag='bass' // BCP47 script tag for this orthography
+notesLangtag='bsq'
+blockLangtag = 'bsq' // same as langTag; used in block.html
+blockDirection=''  // ltr/rtl or '' for ltr; used by block.html
+
+blockDirectoryName='bass'  // folder name where block file is found, no slash
+orthogFilePath='bass/bsq'  // path below /scripts, including filename, but no extension, eg. lana/nod
+pickerDir='bass'  // directory name for picker, no slash at end
+scriptSummaryTableName='bsq-bass'  // id used for langs record in xx-langdata.js; used to make summary table
+charUsageBCP='bsq'  // bcp tag to find the orthography in the charuse app; used in makeIndexIntro only
+orthogName = 'Bassa Vah' // natural language name for the orthography; used only in makeIndexIntro
+languageName = 'Bassa' // natural language name of language; used in various places, esp block
+scriptIsIndic = false  // true if there are indic syllabic categories; used only in makeIndexIntro
+
+
+defaultSize = '24px'  // used by block.html to set size of font, including in font mgt panel
+letterClassName = 'bsq'  // classname for a given orthography in the block page
+
+
+
+
