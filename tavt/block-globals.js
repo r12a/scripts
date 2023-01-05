d5cb9d349e665623fc6d34f69bfc9e8b8b9d5563
r12a <ishida@w3.org>
2023-01-05 09:45:42 +0000

    tavt: 1st draft of v29

---
+++ tavt/block-globals.js
@@ -0,0 +1,11 @@
+
+window.version = 'v29'
+window.scriptName = 'Tai Viet'
+window.ghDirectory = 'taiviet'
+window.blockDirectory = 'taiviet'
+window.languageList = ['blt'] // lang tags for each orthography handled
+window.detailsfileList = ['blt'] // first part of xx-details for each orthography
+window.scriptISO = 'tavt'
+window.webfonts = ["Tai Heritage WF", "Noto Sans Tai Viet WF"]
+window.defaultSize = '24px'
+
---
+++ tavt/blt-globals.js
@@ -0,0 +1,20 @@
+
+
+langTag='blt' // BCP47 language tag for this orthography
+scriptTag='tavt' // BCP47 script tag for this orthography
+notesLangtag='blt'
+blockLangtag = 'blt' // same as langTag; used in block.html
+blockDirection=''  // ltr/rtl or '' for ltr; used by block.html
+
+blockDirectoryName='tavt'  // folder name where block file is found, no slash
+orthogFilePath='tavt/blt'  // path below /scripts, including filename, but no extension, eg. lana/nod
+pickerDir='tavt'  // directory name for picker, no slash at end
+scriptSummaryTableName='blt'  // id used for langs record in xx-langdata.js; used to make summary table
+charUsageBCP='blt'  // bcp tag to find the orthography in the charuse app; used in makeIndexIntro only
+orthogName = 'Tai Viet' // natural language name for the orthography; used only in makeIndexIntro
+languageName = 'Tai Dam' // natural language name of language; used in various places, esp block
+scriptIsIndic = true  // true if there are indic syllabic categories; used only in makeIndexIntro
+
+
+defaultSize = '24px'  // used by block.html to set size of font, including in font mgt panel
+letterClassName = 'blt'  // classname for a given orthography in the block page
