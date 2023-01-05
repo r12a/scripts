7a9c6056a15f38824f530cb00fec4fdedf6791cc
r12a <ishida@w3.org>
2023-01-05 17:41:31 +0000

    nkoo: 1st draft of v29

---
+++ nkoo/block-globals.js
@@ -0,0 +1,11 @@
+
+window.version = 'v28'
+window.scriptName = 'N’Ko'
+window.ghDirectory = 'nko'
+window.blockDirectory = 'nko'
+window.languageList = ['nqo'] // lang tags for each orthography handled
+window.detailsfileList = ['nqo'] // first part of xx-details for each orthography
+window.scriptISO = 'nkoo'
+window.webfonts = ["Noto Sans NKo WF"]
+window.defaultSize = '24px'
+
---
+++ nkoo/nqo-globals.js
@@ -0,0 +1,23 @@
+
+langTag='nqo' // BCP47 language tag for this orthography
+scriptTag='nkoo' // BCP47 script tag for this orthography
+notesLangtag='nqo'
+blockLangtag = 'nqo' // same as langTag; used in block.html
+blockDirection='rtl'  // ltr/rtl or '' for ltr; used by block.html
+
+blockDirectoryName='nkoo'  // folder name where block file is found, no slash
+orthogFilePath='nkoo/nqo'  // path below /scripts, including filename, but no extension, eg. lana/nod
+pickerDir='nko'  // directory name for picker, no slash at end
+scriptSummaryTableName='nqo'  // id used for langs record in xx-langdata.js; used to make summary table
+charUsageBCP='nqo'  // bcp tag to find the orthography in the charuse app; used in makeIndexIntro only
+orthogName = 'N’Ko' // natural language name for the orthography; used only in makeIndexIntro
+languageName = 'Kangbe' // natural language name of language; used in various places, esp block
+scriptIsIndic = false  // true if there are indic syllabic categories; used only in makeIndexIntro
+
+
+defaultSize = '24px'  // used by block.html to set size of font, including in font mgt panel
+letterClassName = 'nqo'  // classname for a given orthography in the block page
+
+
+
+
