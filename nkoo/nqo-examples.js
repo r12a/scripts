f7996e59e3c6f3d7663651ff9abc171d8f378bac
r12a <ishida@w3.org>
2023-01-05 17:33:20 +0000

    Stub for new nkoo directory

---
+++ nkoo/nqo-examples.js
@@ -0,0 +1,166 @@
+
+autoExpandExamples.nqo = `
+
+߁߭|first||1̣³
+߂߲|second||2̣˜
+ߊ߲ ߧߴߌ ߝߏ߬ ߟߊ߫ ߸ ߞߵߌ ߞߎߟߎ߲ߖߋ߲|We give you thanks.|ã ɲi fò lá,  ki kulũd͡ʒẽ|ã y’i fò lá,  k’i kulũjẽ||x
+ߓߐߗ߭ߎߙ. ߓߌߢߍ߲ ߝ߭ߋߣߎ߳.|Bonjour. Bien venue.|bɔʒur. bi‌ɲɛ̃ veny|bɔʧ³ur. biɲɛ˜ f³enuˑ.||x
+ߕߏ߲ߜߊ|litre||to˜gᵇa
+ߘߋ߲ߣߍ߲ ߠߎ߬ ߟߊ߫ ߓߋߟߋ߲ߠߊ|a speech from the children|dẽnɛ̃ łu la belẽła|
+ߛߌ ߺ ߌ ߺ ߛߌ|anyone||si _ i _ si
+ߛߌߘߐ|gram||sidɔ
+ߜߟߊ߬ߗߡߍ߬ߝߘߎ߬ߓߍ߲|cm²||gbàlàcɛ̀mɛ̀fùdùbɛ̃
+ߜߟߊ߬ߥߊ߰ߘߋ߲|millimetre||gbalàwàadẽ||x
+ߞߎߘߍ|kilometer||kudɛ
+ߡߐ߰ ߺ ߐ ߺ ߡߐ߰|everyone||mɔ⁶ _ ɔ _ mɔ⁶
+߁߂߃߲|123rd||1̣2̣3̣˜
+
+ߟߊߡߌ߲߬|drink||lami˜²
+ߞߊߛߌߦߊ|acacia||kasiya
+ߛߌߟߊ|air||sila
+ߛߊ߲ߖߟߎ|rope||sa˜ʤlu
+ߞߵߊ߬ ߟߴߊ߬ ߞߊ߲߬|however||k⁹a² l⁸a² ka˜²
+
+
+ߓߊ߬ߓߊ߫|father, dad||ba²ba¹
+ߓߙߋ|stick||bere
+ߓߍ߬ߙߍ|gravel||bɛ²rɛ
+ߛߌ߭|life, lifespan||si³
+ߓߟߏ|arm||bolo||x
+ߓߐ߬ߟߐ|stake, picket||bɔ²lɔ
+ߡߎ߬ߙߎ|knife||mùru||x
+ߓߊ|river||ba
+ߗߍ߭|man||ʧɛ³
+ߘߊ߬ߓߊ|hand hoe||da²ba
+ߝߊ߬ߟߌ|donkey||fa²li
+ߜߊ߭|kitchen, hearth||gᵇa³
+ߤߣߍ|peace||hɛrɛ||x
+ߖߋ߭|melon||ʤe³
+ߞߏ|affair, matter||ko
+ߟߍ߭|pig||lɛ³
+ߡߊ߬ߟߏ|rice||ma²lo
+ߣߐߣߐ|milk||nɔnɔ||x
+ߔߊߕߌߛߊߞߣߊ߫|good heavens!||patisakna¹
+ߢߐ߭|millet||ɲɔ³
+ߓߊ߯ߙߊ|work||ba⁵ra
+ߝߚߎ߫|for a long time||fṙu¹
+ߛߏ|house, home||so
+ߕߏ߫|leave, abandon||to¹
+ߥߊߙߌ|money||wari
+ߦߊ߲߬|here||ya˜²
+ߒ|I|n̩|ń
+
+ߝ߭ߍߙߌ|glass||véri|extremely marginal phoneme from French (cdl)|x
+ߜߍ߫|white||ɡ͡bɛ||x
+ߜߍ߬ߟߍ߲߬|hard||ɡ͡bɛ̀lɛ̃||x
+ߜߏ߬ߟߏ|skin||ɡ͡bòló||x
+ߓߊ߭ߞߐ߬ߦߐ|herbaceous plant|bǎkɔ̀jɔ́|bǎkɔ̀yɔ́||x
+ߌ ߓߴߊ߬ ߝߐ߫|you say it|í b(ɛ́)à fɔ́|í bʼà fɔ́||x
+ߌ ߞߵߊ߬ ߝߐ߫|you said it|í k(à)à fɔ́|í kʼà fɔ́||x
+ߞߏ߲|do first, precede||kɔ̌n||x
+ߛߓߊ߬|three||sábá||x
+ߍߛߑߞߌߡߏߞߊ|eskimo||ɛskimoka||x
+ߍߛߑߕߎߥߊߣߌߞߊ|Estonian||ɛstuwanika||x
+ߦߎߣߍߛߑߞߏ|UNESCO||yunɛsko||x
+ߛߑߔߌߣߏߖ߭ߊ߫|Spinoza||spinozá||x
+
+
+
+ߊߡߌ߯ߣߊ߫|amen||amíiná
+ߓߊ߬ߣߊ|rich, wealthy||bàna|
+ߓߊ߯ߙߊ߫|work||báará
+
+
+ߡߊ߬ߡߛߏ߬ߣߍ߲|sister||màmàsònɛ̃
+ߓߊ߲߬ߓߊ|crocodile||bã̀ba||x
+ߓߊ߬ߣߊ߲߬ߞߎ|cassava||bànà̃ku
+ߓߊߙߌߞߊߓߐ|thanks||barikabɔ
+ߓߊ|river||ba
+ߓߊ߭|goat||bǎ||x
+ߓߌߢߍ|liver||biɲɛ
+ߞߌ߬ߢߍ߬ߞߌߢߍ|sand||kìɲɛ̀kiɲɛ
+ߟߋߟߋ|absent||lele||x
+ߝߘߊ߬ߝߌ߲߬ߠߊ߫|Africa||fadàfĩ̀ná||x
+ߜߐ߱|widow||gbòó||x
+ߘߎ߱|city||dùú||x
+ߓߎ߱|wigwam||bùú||x
+ߞߏ߬ߝߊ߮|bath||kòfáà||x
+ߖߌ߰ߖߊ߮|because||jìjáà||x
+ߢߌ߲ߞߏߛߍ߮|toothpaste||ɲĩkosɛ́ɛ̀||x
+ߢߊߟߐ߲ߓߊ߮|expert||ɲalɔ̃báà||x
+ߞߎ߬ߙߊ߲߬ߓߊ߮|person||kùrã̀báà||x
+ߡߌ߲߭|asteroid|mǐ̃||x
+ߔߊ߬ߔߘߊ|menu||pàpada||x
+ߕߊߕߍߟߊ߲|brazier||tatɛlã||x
+ߘߊߘߐߞߍ|ambiguity||dadɔkɛ||x
+ߞߊߞߊߞߊ߫|to laugh||kakaká||x
+ߜߊߜߊߟߌ|incision||gbagbali||x
+ߖߛߎ߬ߘߐ߬ߗߍ߫|whisper||jùsùdɔ̀cɛ́||x
+ߝߊߝߊ|cave||fafa||x
+ߛߎߛߋߟߡߊ|greyish||suselma||x
+ߤߊߤߊߣߍ߲|panting||hahanɛ̃||x
+ߛߌߣߊߣߍ߲|gazelle||sinanɛ̃||x
+ߢߐ߯ߡߍ|camel||ɲɔ́ɔmɛ||x
+ߥߏ߲ߥߏ߲ߠߌ|bark||wõwõni||x
+ߥߋ߬ߟߋ߲߬ߥߋߟߋ߲|acrobatics||wèlẽ̀welẽ||x
+ߒߞߏ|N’Ko||ńko
+ߡߊ߰ߒߠߊ|contact||màńna||x
+ߓߏ߲߬ߧߊ|honour||bõ̀ya||x
+ߘߏ߲߬ߠߌ߲|access||dõ̀nĩ||x
+ߝߚߎ߫|completely||fúr(ú)rú||x
+ߛߐ߬ߛߐ|bean||sɔ̀sɔ́||x
+
+
+
+ߓߟߊ|xylophone||bála||x
+ߓߊ߬ߟߊ|porcupine||bàlá||x
+ߓߊߛߌ|evil||bási||x
+ߓߊ߬ߛߌ|couscous||bàsí||x
+ߓߊ߲߫|finish||bán||x
+ߓߊ߲߬|refuse||bàn||x
+ߞߣߎ߫|wake up||kúnu||x
+ߞߣߎ߬|swallow||kùnu||x
+ߦߙߌ|tree||yíri||x
+
+
+߾߅،߿߉߅|5 dorome and 95 taman (đ5.ŧ95)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+`
+
