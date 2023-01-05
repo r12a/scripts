a48a79ae2ff71ba8d83a56669716c5bd57f0ac23
r12a <ishida@w3.org>
2023-01-05 19:03:13 +0000

    osge: 1st draft of v29

---
+++ osge/block-globals.js
@@ -0,0 +1,11 @@
+
+window.version = 'v28'
+window.scriptName = 'Osage'
+window.ghDirectory = 'osage'
+window.blockDirectory = 'osage'
+window.languageList = ['osa'] // lang tags for each orthography handled
+window.detailsfileList = ['osa'] // first part of xx-details for each orthography
+window.scriptISO = 'osge'
+window.webfonts = ["Noto Sans Osage WF"]
+window.defaultSize = '24px'
+
---
+++ osge/osa-globals.js
@@ -0,0 +1,21 @@
+
+langTag='osa' // BCP47 language tag for this orthography
+scriptTag='osge' // BCP47 script tag for this orthography
+notesLangtag='osa'
+blockLangtag = 'osa' // same as langTag; used in block.html
+blockDirection=''  // ltr/rtl or '' for ltr; used by block.html
+
+blockDirectoryName='osge'  // folder name where block file is found, no slash
+orthogFilePath='osge/osa'  // path below /scripts, including filename, but no extension, eg. lana/nod
+pickerDir='osge'  // directory name for picker, no slash at end
+scriptSummaryTableName='osa'  // id used for langs record in xx-langdata.js; used to make summary table
+charUsageBCP='osa'  // bcp tag to find the orthography in the charuse app; used in makeIndexIntro only
+orthogName = 'Osage' // natural language name for the orthography; used only in makeIndexIntro
+languageName = 'Osage' // natural language name of language; used in various places, esp block
+scriptIsIndic = false  // true if there are indic syllabic categories; used only in makeIndexIntro
+
+
+defaultSize = '24px'  // used by block.html to set size of font, including in font mgt panel
+letterClassName = 'osa'  // classname for a given orthography in the block page
+
+
