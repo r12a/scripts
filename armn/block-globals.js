62ad577bbbbfe2695c5092baf9ab3f12a243af5f
r12a <ishida@w3.org>
2023-01-05 15:55:25 +0000

    armn: 1st draft of v29

---
+++ armn/block-globals.js
@@ -0,0 +1,11 @@
+
+window.version = 'v28'
+window.scriptName = 'Armenian'
+window.ghDirectory = 'armenian'
+window.blockDirectory = 'armenian'
+window.languageList = ['hy'] // lang tags for each orthography handled
+window.detailsfileList = ['hy'] // first part of xx-details for each orthography
+window.scriptISO = 'armn'
+window.webfonts = ["Arian AMU WF", "Noto Sans Armenian WF"]
+window.defaultSize = '24px'
+
