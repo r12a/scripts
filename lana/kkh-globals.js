47470b1bc65133914ad64d06e83b2de0f6e93f7a
r12a <ishida@w3.org>
2023-01-05 09:55:30 +0000

    lana-kkh: 1st draft of v29

--- lana/kkh-globals.js
+++ lana/kkh-globals.js
@@ -1,3 +1,5 @@
+
+
 langTag='kkh' // BCP47 language tag for this orthography
 scriptTag='lana' // BCP47 script tag for this orthography
 notesLangtag='kkh'
@@ -9,56 +11,11 @@ orthogFilePath='lana/kkh'  // path below /scripts, including filename, but no ex
 pickerDir='lana-kkh'  // directory name for picker, no slash at end
 scriptSummaryTableName='kkh'  // id used for langs record in xx-langdata.js; used to make summary table
 charUsageBCP='kkh'  // bcp tag to find the orthography in the charuse app; used in makeIndexIntro only
-orthogName = 'Lanna' // natural language name for the orthography; used only in makeIndexIntro
+orthogName = 'Tai Tham' // natural language name for the orthography; used only in makeIndexIntro
 languageName = 'Tai Khün' // natural language name of language; used in various places, esp block
 scriptIsIndic = true  // true if there are indic syllabic categories; used only in makeIndexIntro
-
-
-
-blockDir='lana/'
 
 
 defaultSize = '24px'  // used by block.html to set size of font, including in font mgt panel
 letterClassName = 'kkh'  // classname for a given orthography in the block page
 
-
-
-
-
-
-// the following don't seem to be used
-pageWebfonts = ["Hariphunchai WF", "Noto Serif Tai Tham WF", "Lamphun WF", "Lanna Alif WF", "A Tai Tham KH New WF"]  //  doesn't seem to be used
-fontSwitchLangs = 'nod,kkh'
-lang = 'nod'
-direction = ''
-
-
-/* 
-langTag='kkh'
-scriptTag='lana'
-notesLangtag='kkh'
-
-explainerDir='taitham/'
-blockDir='taitham/'
-pickerDir='lana-kkh/'
-scriptSummaryTableName='kkh'
-charUsageBCP='kkh'
-orthogName = 'Lanna'
-indic = true
-
-pageWebfonts = ["Lamphun WF", "Noto Serif Tai Tham WF", "Lanna Alif WF", "A Tai Tham KH New WF"]
-defaultSize = '24px'
-
-
-
-autoIndex = true
-
-
-// block settings
-
-languageName = 'Tai Khün'
-letterClassName = 'kkh'
-lang = 'kkh'
-direction = ''
-
-*/
