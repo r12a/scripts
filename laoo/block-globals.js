8f56c1861b56c95a6129d4face39900fcc69067e
r12a <ishida@w3.org>
2023-01-05 08:52:40 +0000

    laoo: 1st draft of v29

---
+++ laoo/block-globals.js
@@ -0,0 +1,11 @@
+
+window.version = 'v28'
+window.scriptName = 'Lao'
+window.ghDirectory = 'lao'
+window.blockDirectory = 'lao'
+window.languageList = ['lo'] // lang tags for each orthography handled
+window.detailsfileList = ['lo'] // first part of xx-details for each orthography
+window.scriptISO = 'laoo'
+window.webfonts = ["Noto Serif Lao WF"]
+window.defaultSize = '24px'
+
---
+++ laoo/lo-globals.js
@@ -0,0 +1,20 @@
+
+
+langTag='lo' // BCP47 language tag for this orthography
+scriptTag='laoo' // BCP47 script tag for this orthography
+notesLangtag='lo'
+blockLangtag = 'lo' // same as langTag; used in block.html
+blockDirection=''  // ltr/rtl or '' for ltr; used by block.html
+
+blockDirectoryName='laoo'  // folder name where block file is found, no slash
+orthogFilePath='laoo/lo'  // path below /scripts, including filename, but no extension, eg. lana/nod
+pickerDir='lao'  // directory name for picker, no slash at end
+scriptSummaryTableName='lo'  // id used for langs record in xx-langdata.js; used to make summary table
+charUsageBCP='lo'  // bcp tag to find the orthography in the charuse app; used in makeIndexIntro only
+orthogName = 'Lao' // natural language name for the orthography; used only in makeIndexIntro
+languageName = 'Lao' // natural language name of language; used in various places, esp block
+scriptIsIndic = true  // true if there are indic syllabic categories; used only in makeIndexIntro
+
+
+defaultSize = '24px'  // used by block.html to set size of font, including in font mgt panel
+letterClassName = 'lo'  // classname for a given orthography in the block page
