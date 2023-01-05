a4c9f1eac746bf98cf3df478d1fe3a42ba875fa4
r12a <ishida@w3.org>
2023-01-05 10:33:06 +0000

    Stub for new bamu directory

---
+++ bamu/refs.js
@@ -0,0 +1,68 @@
+reflist = {
+
+
+"d": {
+"url":"#refs",
+"authors": "Peter T. Daniels and William Bright",
+"title":"The World's Writing Systems",
+"publisher":"Oxford University Press",
+"isbn":"0-19-507993-0",
+"reviewed":true
+},
+
+"e": {
+"url":"https://www.unicode.org/L2/L2008/08350-n3522-bamum.pdf",
+"authors": "Everson, Michael; Riley, Charles; Tuchscherer, Konrad",
+"title":"L2/08-350: Proposal to encode modern Bamum in the BMP",
+},
+
+"o": {
+"url":"https://www.omniglot.com/writing/bamum.htm",
+"authors": "Omniglot",
+"title":"Bamum syllabary",
+"reviewed":true
+},
+
+"r": {
+"url":"https://www.unicode.org/L2/L2007/07023-bamum-report.pdf",
+"authors": "Riley, Charles",
+"title":"L2/07-023: Towards the Encoding of the Bamum Script in the UCS",
+"reviewed":true
+},
+
+"u": {
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch19.pdf",
+"authors": "Unicode Consortium",
+"title":"The Unicode Standard, Version 13.0",
+"sub":"Chapter 19.6: Africa, Bamum",
+"pages":"779-780",
+"isbn":"978-1-936213-16-0.",
+},
+
+"ul": {
+"url":"https://www.unicode.org/reports/tr14/",
+"authors": "Unicode Consortium",
+"title":"Unicode Line Breaking Algorithm (UAX#14)",
+"reviewed":true
+},
+
+"wl": {
+"url":"https://en.wikipedia.org/wiki/Bamum_language",
+"authors": "Wikipedia",
+"title":"Bamum language",
+"reviewed":true
+},
+
+"ws": {
+"url":"https://en.wikipedia.org/wiki/Bamum_script",
+"authors": "Wikipedia",
+"title":"Bamum script",
+"reviewed":true
+},
+
+
+}
+
+
+
+// <a class="fn">ws,#tones</a>
