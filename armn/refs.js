092d34756099846b3f47e33a75fed74087a0535c
r12a <ishida@w3.org>
2023-01-05 15:43:06 +0000

    Stub for new armn directory

---
+++ armn/refs.js
@@ -0,0 +1,89 @@
+reflist = {
+
+
+"a": {
+"authors": "Deborah Anderson",
+"title":"Uppercase of U+0587 ARMENIAN SMALL LIGATURE ECH YIWN",
+"url":"https://www.unicode.org/L2/L2020/20143-armenian-ech-yiwn.pdf",
+"reviewed":true
+},
+
+"d": {
+"authors": "Peter T. Daniels and William Bright",
+"title":"The World's Writing Systems",
+"publisher":"Oxford University Press",
+"isbn":"0-19-507993-0",
+},
+
+"j": {
+"authors": "Jasmine Dum-Tragut",
+"title":"Armenian: Modern Eastern Armenian",
+"publisher": "John Benjamins Publishing Co",
+"series": "London Oriental and African Language Library",
+"url":"https://www.google.co.uk/books/edition/Armenian/SGyop5guXDkC?hl=en",
+},
+
+"e": {
+"authors": "Michael Everson",
+"title":"Proposal to encode two symbols for Armenian in the UCS",
+"url":"http://std.dkuug.dk/jtc1/sc2/wg2/docs/n3924.pdf",
+},
+
+"ics": {
+"authors": "Ishida, R",
+"title":"Ready-made Counter Styles",
+"url":"https://www.w3.org/TR/predefined-counter-styles/#arabic-styles",
+"date": "2017",
+"reviewed":true
+},
+
+"u": {
+"authors": "Unicode Consortium",
+"title":"The Unicode Standard, Version 13.0",
+"sub":"Chapter 7.6: Europe-I, Armenian",
+"pages":"317-318",
+"isbn":"978-1-936213-16-0.",
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch07.pdf",
+},
+
+"wd": {
+"authors": "Wikipedia",
+"title":"Armenian dram sign",
+"url":"https://en.wikipedia.org/wiki/Armenian_dram_sign",
+},
+
+"wl": {
+"authors": "Wikipedia",
+"title":"Armenian language",
+"url":"https://en.wikipedia.org/wiki/Armenian_language",
+},
+
+"ws": {
+"authors": "Wikipedia",
+"title":"Armenian script",
+"url":"https://en.wikipedia.org/wiki/Armenian_script",
+},
+
+
+}
+
+
+
+
+
+scriptLanguageList = {
+hy:{name:"Armenian", url:"hy.html"},
+kur:{name:"Kurdish"},
+kmr:{name:"Kurdish, Northern"},
+hyw:{name:"Armenian, Western"},
+}
+
+
+
+
+
+
+
+
+
+
