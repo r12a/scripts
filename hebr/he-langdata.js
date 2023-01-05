c257f71ffa85f3edc9e9828d53c1a9295232c23b
r12a <ishida@w3.org>
2023-01-05 07:22:35 +0000

    Stub for new hebr directory

---
+++ hebr/he-langdata.js
@@ -0,0 +1,16 @@
+// this should always be updated when app-charuse/langs.js is updated
+
+var langs = {
+
+"he": { name:"Hebrew", local:" עברית‎ (Ivrit), עברית חדשה‎ (ivrít ḥadašá[h])", silcode:"heb", rtl:true, 
+source:"0a5ae89a710c3b0c9e880e33bdaaf863175ea62a,cldr_he,udhr_heb", 
+region:"wasia", countries:"Israel, etc.", script:"hebr", speakers:"9000000", 
+letter:"אבגדהוזחטיכךלמםנןסעפףצץקרשת", punctuation:"׳״־’”‐–—", punctuationaux:"„", symbol:"₪", other:"\u200F\u200E\u2067\u2066\u2069\u2068\u202B\u202A\u202C", markaux:"ְֱֲֳִֵֶַָֹֻּׁׂ", 
+orth:`Hebrew. &nbsp;  <a href="../scripts/hebrew" target="_blank">Details</a>.`,
+type:"abjad", cs:"no", gpos:"yes", gsub:"no", cursive:"no", direction:"rtl", wordsep:"space", baseline:"mid", wrap:"word", hyphenation:"yes", hyphen:"-",  justification:"sp",
+vowels:"ml:4 hcm:11",
+linked:"hebrew", fonts:"/pickers/hebr/", 
+},
+
+
+}
