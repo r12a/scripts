fb9655b63050b4da04bb478f346e7caa6b5009aa
r12a <ishida@w3.org>
2023-01-05 09:59:21 +0000

    Stub for new ethi directory

---
+++ ethi/refs.js
@@ -0,0 +1,156 @@
+reflist = {
+
+
+"c": {
+"url":"#refs",
+"authors": "Bernard Comrie (ed)",
+"title":"The Major World's Languages",
+"version":"3rd edition",
+"publisher":"Routledge",
+"isbn":"978-1-315-64493-6",
+},
+
+"d": {
+"url":"#refs",
+"authors": "Peter T. Daniels and William Bright",
+"title":"The World's Writing Systems",
+"publisher":"Oxford University Press",
+"isbn":"0-19-507993-0",
+"reviewed":true
+},
+
+"e": {
+"url":"http://w3c.github.io/elreq/",
+"authors": "Daniel Yacob",
+"title":"Ethiopic Layout Requirements",
+},
+
+"g116": {
+"url":"https://github.com/w3c/elreq/issues/116",
+"authors": "GitHub",
+"title":"Does Ethiopic text also get wrapped by word?",
+},
+
+"m": {
+"url":"#refs",
+"authors": "Anandam Krishnamurthi",
+"title":"Learn Ethiopic in a Month",
+"publisher":"Readwells",
+"isbn":"9788187782049",
+},
+
+"o": {
+"url":"http://www.omniglot.com/writing/ethiopic.htm",
+"authors": "Omniglot",
+"title":"Ethiopic",
+},
+
+"u": {
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch19.pdf",
+"authors": "Unicode Consortium",
+"title":"The Unicode Standard, Version 13.0",
+"sub":"Chapter 19.6: Africa, Bamum",
+"pages":"779-780",
+"isbn":"978-1-936213-16-0.",
+},
+
+"ul": {
+"url":"https://www.unicode.org/reports/tr14/",
+"authors": "Unicode Consortium",
+"title":"Unicode Line Breaking Algorithm (UAX#14)",
+"reviewed":true
+},
+
+"ws": {
+"url":"https://en.wikipedia.org/wiki/Ge%CA%BDez_script",
+"authors": "Wikipedia",
+"title":"Geʽez script",
+},
+
+"wam": {
+"url":"https://en.wikipedia.org/wiki/Amharic",
+"authors": "Wikipedia",
+"title":"Amharic",
+},
+
+"wa": {
+"url":"https://en.wikipedia.org/wiki/Help:IPA/Amharic",
+"authors": "Wikipedia",
+"title":"Help:IPA/Amharic",
+},
+
+"wg": {
+"url":"https://en.wikipedia.org/wiki/Ge%CA%BDez",
+"authors": "Wikipedia",
+"title":"Geʽez",
+},
+
+"x": {
+"url":"https://cms.education.gov.il/NR/rdonlyres/9D9F5A60-082D-4DDD-9853-A669B5B9C9E5/132239/Safa.PDF",
+"authors": "ዶ/ር አንበሴ ተፈራ",
+"title":"የኢትዮጵያ ብሔረሰቦችና ቋንቋዎቻቸው አጭር ቅኝት ",
+},
+
+
+}
+
+
+
+
+scriptLanguageList = {
+aiw:{name:"Aari"},
+aar:{name:"Afar"},
+alw:{name:"Alaba-K’abeena"},
+amh:{name:"Amharic", url:"am.html"},
+anu:{name:"Anuak"},
+agj:{name:"Argobba"},
+awn:{name:"Awngi"},
+bst:{name:"Basketo"},
+bcq:{name:"Bench"},
+byn:{name:"Bilin"},
+gax:{name:"Oromo, Borana-Arsi-Guji"},
+bwo:{name:"Boro (Ethiopia)"},
+bji:{name:"Burji"},
+dwr:{name:"Dawro"},
+gdl:{name:"Dirasha"},
+mdx:{name:"Dizin"},
+gmv:{name:"Gamo"},
+drs:{name:"Gedeo"},
+gez:{name:"Geez"},
+gof:{name:"Gofa"},
+guk:{name:"Gumuz"},
+hdy:{name:"Hadiyya"},
+amf:{name:"Hamer-Banna"},
+har:{name:"Harari"},
+kbr:{name:"Kafa"},
+ktb:{name:"Kambaata"},
+gru:{name:"Kistane"},
+kxc:{name:"Konso"},
+kqy:{name:"Koorete"},
+mpe:{name:"Majang"},
+mdy:{name:"Male (Ethiopia)"},
+mym:{name:"Me'en"},
+muz:{name:"Mursi"},
+orm:{name:"Oromo"},
+ahg:{name:"Qimant"},
+ssy:{name:"Saho"},
+sgw:{name:"Sebat Bet Gurage"},
+sid:{name:"Sidamo"},
+stv:{name:"Silt'e"},
+suq:{name:"Suri"},
+tig:{name:"Tigre"},
+tir:{name:"Tigrinya"},
+tir:{name:"Tigrinya"},
+gaz:{name:"Oromo, West Central"},
+wal:{name:"Wolaytta"},
+xan:{name:"Xamtanga"},
+zay:{name:"Zaysete"},
+}
+
+
+
+
+
+
+
+
