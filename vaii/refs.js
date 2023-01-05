8dd0d1e9c021effdbf6c17bdda677ff9b2e8cabb
r12a <ishida@w3.org>
2023-01-05 10:51:25 +0000

    Stub for new vaii directory

---
+++ vaii/refs.js
@@ -0,0 +1,136 @@
+reflist = {
+
+/*
+"auth_initials": {
+"authors": "Sort by family name of first contributor",
+"title":"The World's Writing Systems",
+"sub": "subtitle",
+"date": "1996",
+"retr": "May 2021",
+"publisher":"Oxford University Press",
+"isbn":"0-19-507993-0",
+"pages": "xxx",
+"url":"refs",
+"lang": new Set(['ru']),
+"reviewed":false
+},
+*/
+
+
+"d": {
+"url":"#refs",
+"authors": "John Victor Singler, eds.Peter T. Daniels and William Bright",
+"title":"The World's Writing Systems",
+"sub": "Vai script",
+"publisher":"Oxford University Press",
+"pages": "593-597",
+"isbn":"0-19-507993-0",
+"reviewed":false
+},
+
+"esf": {
+"authors": "Eberhard, David M, Gary F. Simons, and Charles D Fennig (eds)",
+"title":"Ethnologue: Languages of the World, 24th ed.",
+"sub": "Vai",
+"date": "2021",
+"retr": "Dec 2021",
+"publisher":"SIL International, Dallas, Texas",
+"url":"https://www.ethnologue.com/language/vai",
+"reviewed":true
+},
+
+"ev": {
+"authors": "Michael Everson",
+"title":"Proposal to add additional historic syllables for Vai to the UCS",
+"date": "2007",
+"url":"http://std.dkuug.dk/jtc1/sc2/wg2/docs/n3243.pdf",
+"reviewed":true
+},
+
+"enrs": {
+"authors": "Michael Everson, Mohamed Nyei, Charles Riley, Tombekai Sherman",
+"title":"Proposal for addition of Vai characters to the UCS",
+"date": "2006",
+"url":"https://www.unicode.org/L2/L2006/06120r-n3081r-vai-additions.pdf",
+"reviewed":true
+},
+
+"err": {
+"authors": "Michael Everson, Charles Riley, José Rivera",
+"title":"Proposal to add the Vai script to the BMP of the UCS",
+"date": "2005",
+"url":"http://std.dkuug.dk/jtc1/sc2/wg2/docs/n2948.pdf",
+"reviewed":true
+},
+
+"rmr": {
+"authors": "Andrij Rovenchak, Ján Mačutek, Charles Riley",
+"title":"Distribution of complexities in the Vai script",
+"date": "unknown",
+"url":"https://www.academia.edu/379791/Distribution_of_complexities_in_the_Vai_script",
+"reviewed":false
+},
+
+"s": {
+"url":"http://scriptsource.org/cms/scripts/page.php?item_id=script_detail&key=Vaii",
+"authors": "ScriptSource",
+"title":"Vai",
+},
+
+"u": {
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch19.pdf",
+"authors": "Unicode Consortium",
+"title":"The Unicode Standard, Version 13.0",
+"sub":"Chapter 19.5: Africa, Vai",
+"pages":"777-778",
+"isbn":"978-1-936213-16-0.",
+},
+
+"ul": {
+"url":"https://www.unicode.org/reports/tr14/",
+"authors": "Unicode Consortium",
+"title":"Unicode Line Breaking Algorithm (UAX#14)",
+"reviewed":true
+},
+
+"w": {
+"authors": "Wm E Welmers",
+"title":"A Grammar of Vai",
+"date": "1976",
+"publisher":"University of California Publications",
+"isbn":"0-520-09555-3",
+"url":"https://www.google.co.uk/books/edition/A_Grammar_of_Vai/yXTdbqvhnaEC?hl=en&gbpv=0",
+"reviewed":false
+},
+
+"wl": {
+"url":"http://en.wikipedia.org/wiki/Vai_language",
+"authors": "Wikipedia",
+"title":"Vai language",
+},
+
+"ws": {
+"url":"http://en.wikipedia.org/wiki/Vai_syllabary",
+"authors": "Wikipedia",
+"title":"Vai syllabary",
+"reviewed":true
+},
+
+
+}
+
+
+
+
+
+scriptLanguageList = {
+vai:{name:"Vai", url:"vai.html"},
+}
+
+
+
+
+
+
+
+
