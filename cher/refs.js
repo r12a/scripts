f513addf6157aa82cb7b534ea983903cdb064e26
r12a <ishida@w3.org>
2023-01-05 10:42:59 +0000

    Stub for new cher directory

---
+++ cher/refs.js
@@ -0,0 +1,98 @@
+reflist = {
+
+
+"d": {
+"url":"#refs",
+"authors": "Peter T. Daniels and William Bright",
+"title":"The World's Writing Systems",
+"publisher":"Oxford University Press",
+"isbn":"0-19-507993-0",
+},
+
+"e": {
+"url":"http://www.unicode.org/L2/L2014/14064r-n4537r-cherokee.pdf",
+"authors": "Michael Everson & Durbin Feeling",
+"title":"Revised proposal for the addition of Cherokee characters to the UCS",
+},
+
+"ma": {
+"authors": "Brad Montgomery-Anderson",
+"title":"A reference grammar of Oklahoma Cherokee",
+"date": "2008",
+"publisher":"PhD dissertation",
+"url":"https://kuscholarworks.ku.edu/bitstream/handle/1808/4212/umi-ku-2613_1.pdf",
+"reviewed":false
+},
+
+"o": {
+"url":"http://www.omniglot.com/writing/cherokee.htm",
+"authors": "Omniglot",
+"title":"Cherokee",
+},
+
+"pr": {
+"authors": "Margaret Peake Raymond",
+"title":"The Cherokee Nation and its Language",
+"sub": "tsalagi ayeli ale uniwonishisdi",
+"date": "2008",
+"retr": "Dec 2021",
+"url":"https://carla.umn.edu/conferences/past/immersion2008/documents/Peter_L_CherokeeNation.pdf",
+"reviewed":false
+},
+
+"s": {
+"url":"http://scriptsource.org/scr/Cher",
+"authors": "ScriptSource",
+"title":"Cherokee",
+},
+
+"u": {
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch20.pdf",
+"authors": "Unicode Consortium",
+"title":"The Unicode Standard, Version 13.0",
+"sub":"Chapter 20.1: Americas, Cherokee",
+"pages":"788-789",
+"isbn":"978-1-936213-16-0.",
+},
+
+
+"uh": {
+"authors": "Uchihara, Hiroto",
+"title":"Cherokee Phonology and Verb Morphology",
+"date": "2007",
+"retr": "Dec 2021",
+"publisher":"University of Tokyo, MA Thesis",
+"url":"https://www.academia.edu/3884316/Cherokee_Phonology_and_Verb_Morphology",
+"reviewed":false
+},
+
+
+"ul": {
+"url":"https://www.unicode.org/reports/tr14/",
+"authors": "Unicode Consortium",
+"title":"Unicode Line Breaking Algorithm (UAX#14)",
+"reviewed":true
+},
+
+"ws": {
+"url":"https://en.wikipedia.org/wiki/Cherokee_syllabary",
+"authors": "Wikipedia",
+"title":"Cherokee syllabary",
+},
+
+
+}
+
+
+
+
+
+
+scriptLanguageList = {
+chr:{name:"Cherokee", url:"chr.html"},
+}
+
+
+
+
+
