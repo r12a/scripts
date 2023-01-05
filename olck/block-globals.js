7b7a4eb4b0ee0271372ff74ffabad790a84e97cf
r12a <ishida@w3.org>
2023-01-05 17:31:26 +0000

    olck: 1st draft of v29

---
+++ olck/block-globals.js
@@ -0,0 +1,11 @@
+
+window.version = 'v28'
+window.scriptName = 'Ol Chiki'
+window.ghDirectory = 'santali'
+window.blockDirectory = 'santali'
+window.languageList = ['sat'] // lang tags for each orthography handled
+window.detailsfileList = ['sat'] // first part of xx-details for each orthography
+window.scriptISO = 'olck'
+window.webfonts = ["Noto Sans Ol Chiki WF"]
+window.defaultSize = '24px'
+
---
+++ olck/sat-globals.js
@@ -0,0 +1,23 @@
+
+langTag='sat' // BCP47 language tag for this orthography
+scriptTag='olck' // BCP47 script tag for this orthography
+notesLangtag='sat'
+blockLangtag = 'sat' // same as langTag; used in block.html
+blockDirection=''  // ltr/rtl or '' for ltr; used by block.html
+
+blockDirectoryName='olck'  // folder name where block file is found, no slash
+orthogFilePath='olck/sat'  // path below /scripts, including filename, but no extension, eg. lana/nod
+pickerDir='olck'  // directory name for picker, no slash at end
+scriptSummaryTableName='sat'  // id used for langs record in xx-langdata.js; used to make summary table
+charUsageBCP='sat'  // bcp tag to find the orthography in the charuse app; used in makeIndexIntro only
+orthogName = 'Ol Chiki' // natural language name for the orthography; used only in makeIndexIntro
+languageName = 'Santali' // natural language name of language; used in various places, esp block
+scriptIsIndic = false  // true if there are indic syllabic categories; used only in makeIndexIntro
+
+
+defaultSize = '24px'  // used by block.html to set size of font, including in font mgt panel
+letterClassName = 'sat'  // classname for a given orthography in the block page
+
+
+
+
