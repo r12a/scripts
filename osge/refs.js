68143fc2bd8f2a035212645249c95639f70ca414
r12a <ishida@w3.org>
2023-01-05 18:55:46 +0000

    Stub for new nkoo directory

---
+++ osge/refs.js
@@ -0,0 +1,66 @@
+reflist = {
+
+
+"e": {
+"authors": "Michael Everson, Herman Mongrain Lookout, Cameron Pratt",
+"title":"Final proposal to encode the Osage script in the UCS",
+"date": "2014",
+"url":"http://std.dkuug.dk/jtc1/sc2/wg2/docs/n4619.pdf",
+},
+
+"o": {
+"authors": "Osage Nation Language Dept.",
+"title":"Osage Language Beginner Lessons",
+"url":"https://www.osagelanguage.com/courses/",
+"reviewed":true
+},
+
+"q": {
+"authors": "Carlyn Quintero",
+"title":"Osage Grammar (preview)",
+"date": "2004",
+"url":"https://books.google.co.uk/books?id=wnvaInod5MAC&printsec=frontcover#v=onepage&q&f=false",
+"reviewed":true
+},
+
+"s": {
+"url":"https://scriptsource.org/scr/Osge",
+"authors": "ScriptSource",
+"title":"Osage",
+"reviewed":true
+},
+
+"u": {
+"authors": "Unicode Consortium",
+"title":"The Unicode Standard, Version 13.0",
+"date": "2020",
+"sub":"Chapter 20.3: Americas, Osage",
+"pages":"792",
+"isbn":"978-1-936213-16-0.",
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch20.pdf",
+},
+
+"wl": {
+"authors": "Wikipedia",
+"title":"Osage language",
+"url":"https://en.wikipedia.org/wiki/Osage_language",
+},
+
+"ws": {
+"authors": "Wikipedia",
+"title":"Osage script",
+"url":"http://en.wikipedia.org/wiki/Osage_script",
+},
+
+
+}
+
+
+
+
+scriptLanguageList = {
+osa:{name:"Osage", url:"osa.html"},
+}
+
+
+
