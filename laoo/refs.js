7b8d5afbb1dfd7b093e0163d6288f054ea47bbb6
r12a <ishida@w3.org>
2023-01-05 08:22:52 +0000

    Stub for new laoo directory

---
+++ laoo/refs.js
@@ -0,0 +1,136 @@
+reflist = {
+
+
+"cldr": {
+"url":"https://www.unicode.org/cldr/charts/latest/summary/lo.html",
+"authors": "CLDR",
+"version":"v36",
+"title":"Locale Data Summary for Lao [lo]",
+},
+
+"d": {
+"url":"#refs",
+"authors": "Peter T. Daniels and William Bright",
+"title":"The World's Writing Systems",
+"publisher":"Oxford University Press",
+"isbn":"0-19-507993-0",
+"reviewed":true
+},
+
+"e": {
+"url":"https://www.unicode.org/L2/L2008/08350-n3522-bamum.pdf",
+"authors": "Everson, Michael; Riley, Charles; Tuchscherer, Konrad",
+"title":"L2/08-350: Proposal to encode modern Bamum in the BMP",
+},
+
+"g3": {
+"url":"https://github.com/w3c/sealreq/issues/3",
+"authors": "GitHub",
+"title":"How is line-breaking handled on the Web for Lao?",
+},
+
+"l": {
+"url":"https://lo.wikipedia.org/wiki/%E0%BA%AD%E0%BA%B1%E0%BA%81%E0%BA%AA%E0%BA%AD%E0%BA%99%E0%BA%A5%E0%BA%B2%E0%BA%A7",
+"authors": "Wikipedia",
+"title":"Lao Wikipedia",
+"reviewed":true
+},
+
+"lga": {
+"url":"https://w3c.github.io/sealreq/gap-analysis/laoo-gap.html",
+"authors": "W3C Southeast Asian task force",
+"title":"Lao Gap Analysis",
+"reviewed":true
+},
+
+"rmcs": {
+"url":"https://www.w3.org/TR/predefined-counter-styles/#arabic-styles",
+"authors": "Richard Ishida",
+"title":"Ready-made Counter Styles",
+"reviewed":true
+},
+
+"s": {
+"url":"#refs",
+"authors": "Buasawan Simmala, Benjawan Poomsan Becker",
+"title":"Lao for Beginners",
+"publisher":"Paiboon Publishing",
+"isbn":"1-887521-28-34",
+"reviewed":true
+},
+
+"u": {
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch16.pdf",
+"authors": "Unicode Consortium",
+"title":"The Unicode Standard, Version 13.0",
+"sub":"Chapter 16.2: Southeast Asia, Lao",
+"pages":"643-645",
+"isbn":"978-1-936213-16-0.",
+},
+
+"ul": {
+"url":"https://www.unicode.org/reports/tr14/",
+"authors": "Unicode Consortium",
+"title":"Unicode Line Breaking Algorithm (UAX#14)",
+"reviewed":true
+},
+
+"wl": {
+"url":"https://en.wikipedia.org/wiki/Lao_language",
+"authors": "Wikipedia",
+"title":"Lao language",
+"reviewed":true
+},
+
+"wi": {
+"url":"https://en.wikipedia.org/wiki/Help:IPA/Thai_and_Lao",
+"authors": "Wikipedia",
+"title":"IPA/Thai and Lao",
+"reviewed":true
+},
+
+
+}
+
+
+
+
+scriptLanguageList = {
+bru:{name:"Bru, Eastern"},
+mww:{name:"Hmong Daw"},
+ium:{name:"Mien, Iu"},
+jeh:{name:"Jeh"},
+kgd:{name:"Kataang"},
+kjg:{name:"Khmu"},
+ngt:{name:"Kriang"},
+kdt:{name:"Kuy"},
+lbn:{name:"Lamet"},
+lao:{name:"Lao", url:"lo.html"},
+brb:{name:"Lave"},
+tto:{name:"Ta'oih, Lower"},
+ncq:{name:"Katang, Northern"},
+pac:{name:"Pacoh"},
+pho:{name:"Phunoi"},
+sss:{name:"Sô"},
+blt:{name:"Tai Dam"},
+tth:{name:"Ta'oih, Upper"},
+brv:{name:"Bru, Western"},
+kuf:{name:"Katu, Western"},
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
