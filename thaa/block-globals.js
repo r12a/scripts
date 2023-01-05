58a1ddd3d5084413a3da2e9964ff56b49b775f3d
r12a <ishida@w3.org>
2023-01-05 07:46:34 +0000

    thaa: 1st draft of v29

---
+++ thaa/block-globals.js
@@ -0,0 +1,11 @@
+
+window.version = 'v28'
+window.scriptName = 'Thaana'
+window.ghDirectory = 'thaana'
+window.blockDirectory = 'thaana'
+window.languageList = ['dv'] // lang tags for each orthography handled
+window.detailsfileList = ['dv'] // first part of xx-details for each orthography
+window.scriptISO = 'thaa'
+window.webfonts = ["Noto Sans Thaana WF"]
+window.defaultSize = '24px'
+
---
+++ thaa/dv-globals.js
@@ -0,0 +1,21 @@
+
+
+
+langTag='dv' // BCP47 language tag for this orthography
+scriptTag='thaa' // BCP47 script tag for this orthography
+notesLangtag='dv'
+blockLangtag = 'dv' // same as langTag; used in block.html
+blockDirection='rtl'  // ltr/rtl or '' for ltr; used by block.html
+
+blockDirectoryName='thaa'  // folder name where block file is found, no slash
+orthogFilePath='thaa/dv'  // path below /scripts, including filename, but no extension, eg. lana/nod
+pickerDir='thaa'  // directory name for picker, no slash at end
+scriptSummaryTableName='dv'  // id used for langs record in xx-langdata.js; used to make summary table
+charUsageBCP='dv'  // bcp tag to find the orthography in the charuse app; used in makeIndexIntro only
+orthogName = 'Thaana' // natural language name for the orthography; used only in makeIndexIntro
+languageName = 'Dhivehi' // natural language name of language; used in various places, esp block
+scriptIsIndic = true  // true if there are indic syllabic categories; used only in makeIndexIntro
+
+
+defaultSize = '24px'  // used by block.html to set size of font, including in font mgt panel
+letterClassName = 'dv'  // classname for a given orthography in the block page
