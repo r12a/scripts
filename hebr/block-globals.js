aa482149ba9e8e0bc9eb5d27808b64e0331d4127
r12a <ishida@w3.org>
2023-01-05 07:35:02 +0000

    hebr: 1st draft of v29

---
+++ hebr/block-globals.js
@@ -0,0 +1,11 @@
+
+window.version = 'v29'
+window.scriptName = 'Hebrew'
+window.ghDirectory = 'hebrew'
+window.blockDirectory = 'hebrew'
+window.languageList = ['he'] // lang tags for each orthography handled
+window.detailsfileList = ['he'] // first part of xx-details for each orthography
+window.scriptISO = 'hebr'
+window.webfonts = ["Noto Serif Hebrew"]
+window.defaultSize = '24px'
+
