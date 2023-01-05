a372f7142307388c96d4ab1b1136c4f9f27401b0
r12a <ishida@w3.org>
2023-01-05 16:37:27 +0000

    Stub for new cyrl directory

---
+++ cyrl/refs.js
@@ -0,0 +1,356 @@
+window.langSet = true
+
+
+reflist = {
+
+
+"bat": {
+"authors": "J Bat-Ireedui, Alan JK Sanders",
+"title":"Colloquial Mongolian",
+"date": "1999",
+"publisher": "Taylor & Francis Ltd",
+"lang": new Set(['mn']),
+"reviewed":false
+},
+
+"ul": {
+"authors": "Christopher Chapman",
+"title":"UAX #14 Unicode Line Breaking Algorithm",
+"date": "2020",
+"publisher": "Unicode Consortium",
+"url":"https://www.unicode.org/reports/tr14/",
+"lang": new Set(['ru']),
+"reviewed":true
+},
+
+"d": {
+"authors": "Peter T. Daniels and William Bright",
+"title":"The World's Writing Systems",
+"publisher":"Oxford University Press",
+"isbn":"0-19-507993-0",
+"pages":"408-412",
+"date": "1996",
+"lang": new Set(['ru']),
+"url":"#refs",
+},
+
+"gov": {
+"authors": "Gov.uk",
+"title":"Romanization of Mongolian Cyrillic, BGN/PCGN 1964 System",
+"url":"https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/816781/ROMANIZATION_OF_MONGOLIAN.pdf",
+"date": "2019",
+"lang": new Set(['mn']),
+"reviewed":true
+},
+
+"ics": {
+"authors": "Ishida, R",
+"title":"Ready-made Counter Styles",
+"url":"https://www.w3.org/TR/predefined-counter-styles/#arabic-styles",
+"date": "2017",
+"lang": new Set(['ru']),
+"reviewed":true
+},
+
+"ng": {
+"url":"https://www.mongolianlanguage.mn/free-lessons/mongolian-cyrillic-alphabet",
+"authors": "номын гэр",
+"title":"The Mongolian Alphabet",
+"retrieved":"Sep 2021",
+"lang": new Set(['mn']),
+"reviewed":false,
+},
+
+"s": {
+"authors": "ScriptSource",
+"title":"Glyph Variant for U+0306",
+"retr": "Apr 2021",
+"url":"http://scriptsource.org/cms/scripts/page.php?item_id=entry_detail&uid=k9ew9c6yam",
+"lang": new Set(['ru','mn']),
+"reviewed":true
+},
+
+"d": {
+"authors": "Jan-Olof Svantesson, Anna Tsendina, Anastasia Karlsson, Vivan Franzen",
+"title":"The Phonology of Mongolian",
+"publisher":"Oxford University Press",
+"isbn":"9780199554270",
+"date": "2005",
+"lang": new Set(['mn']),
+},
+
+"u": {
+"authors": "Unicode Consortium",
+"title":"The Unicode Standard, Version 13.0",
+"date": "2020",
+"sub":"Chapter 7.4: Europe-I, Cyrillic",
+"pages":"311-315",
+"isbn":"978-1-936213-16-0.",
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch07.pdf",
+"lang": new Set(['ru','mn']),
+},
+
+"cldr": {
+"authors": "Unicode Consortium",
+"title":"CLDR: Locale Data Summary for Russian [ru]",
+"url":"http://localhost/scripts/cyrillic/#https://unicode-org.github.io/cldr-staging/charts/37/summary/ru.html",
+"lang": new Set(['ru']),
+},
+
+"ws": {
+"authors": "Wikipedia",
+"title":"Cyrillic script",
+"retr": "Apr 2021",
+"url":"http://en.wikipedia.org/wiki/Cyrillic_script",
+"lang": new Set(['ru']),
+"reviewed":true
+},
+
+"wc": {
+"authors": "Wikipedia",
+"title":"Mongolian Cyrillic alphabet",
+"retr": "May 2021",
+"url":"https://en.wikipedia.org/wiki/Mongolian_Cyrillic_alphabet",
+"lang": new Set(['mn']),
+},
+
+"wm": {
+"authors": "Wikipedia",
+"title":"Mongolian Language",
+"retr": "May 2021",
+"url":"https://en.wikipedia.org/wiki/Mongolian_language",
+"lang": new Set(['mn']),
+},
+
+"wul": {
+"authors": "Wikipedia",
+"title":"Ukrainian Language",
+"retr": "Mar 2022",
+"url":"https://en.wikipedia.org/wiki/Ukrainian_language",
+"lang": new Set(['uk']),
+},
+
+"wq": {
+"authors": "Wikipedia",
+"title":"Quotation mark",
+"retr": "Apr 2021",
+"url":"https://en.wikipedia.org/wiki/Quotation_mark",
+"lang": new Set(['ru','mn','uk']),
+},
+
+"wa": {
+"authors": "Wikipedia",
+"title":"Russian alphabet",
+"retr": "Apr 2021",
+"url":"https://en.wikipedia.org/wiki/Russian_alphabet",
+"lang": new Set(['ru']),
+},
+
+"wua": {
+"authors": "Wikipedia",
+"title":"Ukrainian alphabet",
+"retr": "Mar 2022",
+"url":"https://en.wikipedia.org/wiki/Ukrainian_alphabet",
+"lang": new Set(['uk']),
+},
+
+"wp": {
+"authors": "Wikipedia",
+"title":"Russian phonology",
+"retr": "Apr 2021",
+"url":"https://en.wikipedia.org/wiki/Russian_phonology",
+"lang": new Set(['ru']),
+},
+
+"wup": {
+"authors": "Wikipedia",
+"title":"Ukrainian phonology",
+"retr": "Mar 2022",
+"url":"https://en.wikipedia.org/wiki/Ukrainian_phonology",
+"lang": new Set(['uk']),
+},
+
+}
+
+
+
+
+
+
+scriptLanguageList = {
+
+abq:{name:"Abaza"},
+abk:{name:"Abkhazian"},
+ady:{name:"Adyghe"},
+agx:{name:"Aghul"},
+akv:{name:"Akhvakh"},
+sia:{name:"Sami, Akkala"},
+alr:{name:"Alutor"},
+ani:{name:"Andi"},
+aqc:{name:"Archi"},
+xrn:{name:"Arin"},
+xrm:{name:"Armazic"},
+xss:{name:"Assan"},
+aii:{name:"Neo-Aramaic, Assyrian"},
+ava:{name:"Avaric"},
+aze:{name:"Azerbaijani"},
+kva:{name:"Bagvalal"},
+rmn:{name:"Romani, Balkan"},
+rml:{name:"Romani, Baltic"},
+bak:{name:"Bashkir"},
+bel:{name:"Belarusian"},
+kap:{name:"Bezhta"},
+bos:{name:"Bosnian"},
+bph:{name:"Botlikh"},
+bhh:{name:"Bukharic"},
+bul:{name:"Bulgarian"},
+bua:{name:"Buriat"},
+ess:{name:"Yupik, Central Siberian"},
+cji:{name:"Chamalal"},
+che:{name:"Chechen"},
+ckt:{name:"Chukot"},
+clw:{name:"Chulym"},
+chu:{name:"Slavonic, Church"},
+chv:{name:"Chuvash"},
+crh:{name:"Tatar, Crimean"},
+dar:{name:"Dargwa"},
+dta:{name:"Daur"},
+ddo:{name:"Dido"},
+dlg:{name:"Dolgan"},
+dng:{name:"Dungan"},
+mhr:{name:"Mari, Eastern"},
+myv:{name:"Erzya"},
+eve:{name:"Even"},
+evn:{name:"Evenki"},
+enf:{name:"Enets, Forest"},
+gag:{name:"Gagauz"},
+gdo:{name:"Ghodoberi"},
+niv:{name:"Gilyak"},
+khk:{name:"Mongolian, Halh"},
+gin:{name:"Hinukh"},
+huz:{name:"Hunzib"},
+inh:{name:"Ingush"},
+itl:{name:"Itelmen"},
+jdt:{name:"Judeo-Tat"},
+kbd:{name:"Kabardian"},
+xal:{name:"Kalmyk"},
+xas:{name:"Kamas"},
+kaa:{name:"Kara-Kalpak"},
+krc:{name:"Karachay-Balkar"},
+kim:{name:"Karagas"},
+kdr:{name:"Karaim"},
+kpt:{name:"Karata"},
+krl:{name:"Karelian"},
+kaz:{name:"Kazakh"},
+krk:{name:"Kerek"},
+ket:{name:"Ket"},
+kjh:{name:"Khakas"},
+kca:{name:"Khanty"},
+zkz:{name:"Khazar"},
+khv:{name:"Khvarshi"},
+sjd:{name:"Sami, Kildin"},
+zkb:{name:"Koibal"},
+kom:{name:"Komi"},
+koi:{name:"Komi-Permyak"},
+kpv:{name:"Komi-Zyrian"},
+kpy:{name:"Koryak"},
+zko:{name:"Kott"},
+jct:{name:"Krymchak"},
+qwm:{name:"Kuman (Russia)"},
+kum:{name:"Kumyk"},
+kur:{name:"Kurdish"},
+kir:{name:"Kyrgyz"},
+lad:{name:"Ladino"},
+lbe:{name:"Lak"},
+lez:{name:"Lezghian"},
+lfn:{name:"Lingua Franca Nova"},
+mkd:{name:"Macedonian"},
+mns:{name:"Mansi"},
+chm:{name:"Mari (Russia)"},
+mtm:{name:"Mator"},
+mud:{name:"Aleut, Mednyj"},
+ell:{name:"Greek, Modern (1453-)"},
+mdf:{name:"Moksha"},
+bxm:{name:"Buriat, Mongolia"},
+mon:{name:"Mongolian", url:"mn.html"},
+cnr:{name:"Montenegrin"},
+ttt:{name:"Tat, Muslim"},
+ndf:{name:"Nadruvian"},
+gld:{name:"Nanai"},
+ynk:{name:"Yupik, Naukan"},
+neg:{name:"Negidal"},
+yrk:{name:"Nenets"},
+nio:{name:"Nganasan"},
+nog:{name:"Nogai"},
+azj:{name:"Azerbaijani, North"},
+atv:{name:"Altai, Northern"},
+kmr:{name:"Kurdish, Northern"},
+sme:{name:"Sami, Northern"},
+uzn:{name:"Uzbek, Northern"},
+ykg:{name:"Yukaghir, Northern"},
+orv:{name:"Russian, Old"},
+omk:{name:"Omok"},
+oac:{name:"Oroch"},
+oaa:{name:"Orok"},
+oss:{name:"Ossetic"},
+ems:{name:"Yupik, Pacific Gulf"},
+pnt:{name:"Pontic"},
+xpm:{name:"Pumpokol"},
+ron:{name:"Romanian"},
+rom:{name:"Romany"},
+bxr:{name:"Buriat, Russia"},
+rus:{name:"Russian", url:"ru.html"},
+rue:{name:"Rusyn"},
+rut:{name:"Rutul"},
+sel:{name:"Selkup"},
+srp:{name:"Serbian"},
+cjs:{name:"Shor"},
+sgh:{name:"Shughni"},
+sty:{name:"Tatar, Siberian"},
+ysr:{name:"Yupik, Sirenik"},
+alt:{name:"Altai, Southern"},
+yux:{name:"Yukaghir, Southern"},
+sva:{name:"Svan"},
+tab:{name:"Tabassaran"},
+tgk:{name:"Tajik"},
+tly:{name:"Talysh"},
+tat:{name:"Tatar"},
+sjt:{name:"Sami, Ter"},
+tin:{name:"Tindi"},
+tli:{name:"Tlingit"},
+tkr:{name:"Tsakhur"},
+enh:{name:"Enets, Tundra"},
+tur:{name:"Turkish"},
+tuk:{name:"Turkmen"},
+tyv:{name:"Tuvinian"},
+udi:{name:"Udi"},
+ude:{name:"Udihe"},
+udm:{name:"Udmurt"},
+ukr:{name:"Ukrainian"},
+ulc:{name:"Ulch"},
+uum:{name:"Urum"},
+uig:{name:"Uyghur"},
+uzb:{name:"Uzbek"},
+rmy:{name:"Romani, Vlax"},
+wbl:{name:"Wakhi"},
+bgn:{name:"Balochi, Western"},
+mrj:{name:"Mari, Western"},
+xwo:{name:"Oirat, Written"},
+sjo:{name:"Xibe"},
+yai:{name:"Yagnobi"},
+sah:{name:"Yakut"},
+yah:{name:"Yazgulyam"},
+yug:{name:"Yug"},
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ cyrl/uk-langdata.js
@@ -0,0 +1,14 @@
+// this should always be updated when app-charuse/langs.js is updated
+
+var langs = {
+
+"uk": { name:"Ukrainian", silcode:"ukr", source:"cldr_uk,udhr_ukr", region:"eur", countries:"Ukraine, Brazil, Moldova, Romania", script:"cyrl", speakers:"45000000", local:"українська мова‎ (Ukrayins’ka mova)", 
+letter:"ʼЄІЇАБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЮЯабвгдежзийклмнопрстуфхцчшщьюяєіїҐґ", mark:"̆̈́", punctuation:"«»„“—‐–…", symbol:"№", aux:"§",
+orth:`Cyrillic. &nbsp; <a href="../scripts/cyrillic/ks.html" target="_blank">Details</a>.`,
+type:"alphabet", cs:"yes", gpos:"no", gsub:"no", cursive:"no", direction:"ltr", wordsep:"space", baseline:"mid", wrap:"word", hyphenation:"yes", hyphen:"-",  justification:"sp",
+vowels:"let:10",
+linked:"cyrillic", fonts:"/pickers/cyrl-uk/", 
+},
+
+
+}
