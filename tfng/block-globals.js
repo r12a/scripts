945a13ed1778177b23e7fc84da5fd2179b8fde2f
r12a <ishida@w3.org>
2023-01-05 17:20:11 +0000

    tfng: 1st draft of v29

---
+++ tfng/block-globals.js
@@ -0,0 +1,11 @@
+
+window.version = 'v28'
+window.scriptName = 'Tifinagh'
+window.ghDirectory = 'tifinagh'
+window.blockDirectory = 'tifinagh'
+window.languageList = ['ber'] // lang tags for each orthography handled
+window.detailsfileList = ['zgh'] // first part of xx-details for each orthography
+window.scriptISO = 'tfng'
+window.webfonts = ["Noto Sans Tifinagh WF"]
+window.defaultSize = '24px'
+
---
+++ tfng/zgh-globals.js
@@ -0,0 +1,20 @@
+
+langTag='zgh' // BCP47 language tag for this orthography
+scriptTag='tfng' // BCP47 script tag for this orthography
+notesLangtag='zgh'
+blockLangtag = 'zgh' // same as langTag; used in block.html
+blockDirection=''  // ltr/rtl or '' for ltr; used by block.html
+
+blockDirectoryName='tfng'  // folder name where block file is found, no slash
+orthogFilePath='tfng/zgh'  // path below /scripts, including filename, but no extension, eg. lana/nod
+pickerDir='tfng-ber'  // directory name for picker, no slash at end
+scriptSummaryTableName='zgh'  // id used for langs record in xx-langdata.js; used to make summary table
+charUsageBCP='zgh'  // bcp tag to find the orthography in the charuse app; used in makeIndexIntro only
+orthogName = 'Neo-Tifinagh' // natural language name for the orthography; used only in makeIndexIntro
+languageName = 'Tamazight' // natural language name of language; used in various places, esp block
+scriptIsIndic = false  // true if there are indic syllabic categories; used only in makeIndexIntro
+
+
+defaultSize = '24px'  // used by block.html to set size of font, including in font mgt panel
+letterClassName = 'zgh'  // classname for a given orthography in the block page
+
