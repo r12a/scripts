213f0a2038a1c15ffbd1e590d5b648bdd5afed94
r12a <ishida@w3.org>
2023-01-05 08:10:11 +0000

    mand: 1st draft of v29

---
+++ mand/block-globals.js
@@ -0,0 +1,11 @@
+
+window.version = 'v28'
+window.scriptName = 'Mandaic'
+window.ghDirectory = 'mandaic'
+window.blockDirectory = 'mandaic'
+window.languageList = ['mid'] // lang tags for each orthography handled
+window.detailsfileList = ['mid'] // first part of xx-details for each orthography
+window.scriptISO = 'mand'
+window.webfonts = ["Noto Sans Mandaic WF"]
+window.defaultSize = '24px'
+
