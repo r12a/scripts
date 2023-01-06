6cfad6bf35856c89d6ddde9f1ccfa23b90ae90bb
r12a <ishida@w3.org>
2023-01-06 11:06:11 +0000

    Stub for new latn directory

---
+++ latn/ang-examples.js
@@ -0,0 +1,244 @@
+/*
+@ Source: https://en.wiktionary.org/wiki/Appendix:Old_English_Swadesh_list
+§ 'Gentium Plus'
+# 200
+~ ltr
+! ang
+*/
+
+autoExpandExamples.ang = `
+
+iċ|I|it͡ʃ
+þū|you (singular)|θuː
+hē|he|heː
+wē|we|weː
+ġē|you (plural)|jeː
+hīe|they|hiːy̯
+þēs m|this|θeːs
+þēos f|this|θeːo̯s
+þis n|this|θis
+sē m|that|seː
+sēo f|that|seːo̯
+þæt n|that|θæt
+hēr|here|heːr
+þǣr|there|θæːr
+hwā|who|ʍɑː
+hwæt|what|ʍæt
+hwǣr|where|ʍæːr
+hwænne|when|ˈʍæn.ne
+hū|how|huː
+ne|not|ne
+eall|all|æɑ̯ɫː
+maniġ|many|ˈmɒnij
+fela|many|ˈfelɑ
+sum|some|sum
+fēaw|few|fæːɑ̯w
+lȳt|few|lyːt
+ōþer|other|ˈoːðer
+ān|one|ɑːn
+twēġen|two|ˈtweːjen
+þrī|three|θriː
+fēower|four|ˈfeːo̯wer
+fīf|five|fiːf
+miċel|big|ˈmit͡ʃel
+lang|long|lɒŋɡ
+wīd|wide|wiːd
+brād|wide|brɑːd
+þicce|thick|ˈθik.ke
+hefiġ|heavy|ˈhevij
+lȳtel|small|ˈlyːtel
+sċort|short|ʃorˠt
+nearu|narrow|ˈnæɑ̯ru
+þynne|thin|ˈθyn.ne
+wīf n|woman|wiːf
+wer m|man|wer
+mann m|person|mɒnː
+ċild n|child|t͡ʃild
+bearn n|child|bæɑ̯rˠn
+wīf f|wife|wiːf
+wer m|husband|wer
+ċeorl m|husband|ˈt͡ʃeorˠl̩
+mōdor f|mother|ˈmoːdor
+fæder m|father|ˈfæder
+nīeten n|animal|ˈniːy̯ten
+fisċ m|fish|fiʃ
+fugol m|bird|ˈfuɣol
+hund m|dog|hund
+lūs f|louse|luːs
+nǣdre f|snake|ˈnæːdre
+wyrm f|worm|wyrˠm
+trēow n|tree|treːo̯w
+wudu m|forest|ˈwudu
+sticca m|stick|ˈstik.kɑ
+wæstm m|fruit|ˈwæstm̩
+sǣd n|seed|sæːd
+lēaf n|leaf|læːɑ̯f
+wyrtruma m|root|wyrˠtrumɑ
+rind f|bark (of a tree)|rind
+blōstma m|flower|ˈbloːstmɑ
+gærs n|grass|ɣærˠs
+rāp m|rope|rɑːp
+hȳd f|skin|hyːd
+flǣsċ n|meat|flæːʃ
+blōd n|blood|bloːd
+bān n|bone|bɑːn
+rysel m|fat (noun)|ˈryzel
+ǣġ n|egg|æːj
+horn m|horn|horˠn
+tæġl m|tail|ˈtæjl̩
+steort m|tail|ˈsteo̯rˠt
+feþer f|feather|ˈfeðer
+hǣr n|hair|hæːr
+hēafod n|head|ˈhæːɑ̯vod
+ēare n|ear|æːɑ̯re
+ēage n|eye|ˈæːɑ̯ɣe
+nosu f|nose|ˈnozu
+mūþ m|mouth|muːθ
+tōþ m|tooth|toːθ
+tunge f|tongue (organ)|ˈtuŋɡe
+fingernæġl m|fingernail|ˈfiŋɡerˠnæjl̩
+fōt m|foot|foːt
+sċanca m|leg|ˈʃɒŋkɑ
+cnēow n|knee|kneːo̯w
+hand f|hand|hɒnd
+fiþere n|wing|ˈfiðere
+innoþ m|belly|ˈin.noθ
+wamb f|belly|wɒmb
+þearmas m pl|guts|ˈθæɑ̯rˠmɑs
+sweora m|neck|ˈsweo̯rɑ
+hryċġ m|back|hr̥ydd͡ʒ
+brēost n|breast|breːo̯st
+heorte f|heart|ˈheo̯rˠte
+lifer f|liver|ˈliver
+drincan|to drink|ˈdriŋkɑn
+etan|to eat|ˈetɑn
+bītan|to bite|ˈbiːtɑn
+sūcan|to suck|ˈsuːkɑn
+spǣtan|to spit|ˈspæːtɑn
+spīwan|to vomit|ˈspiːwɑn
+blāwan|to blow|ˈblɑːwɑn
+orþian|to breathe|ˈorðiɑn
+hliehhan|to laugh|ˈhl̥iy̯ç.çɑn
+ġesēon|to see|jeˈseːo̯n
+ġehīeran|to hear|jeˈhiːy̯rɑn
+witan|to know|ˈwitɑn|be aware of
+cunnan|to know|ˈkun.nɑn|be familiar with
+þenċan|to think|ˈθent͡ʃɑn
+ġestincan|to smell|jeˈstiŋkɑn|sense with the nose
+stincan|to smell|ˈstiŋkɑn|give off a scent
+ondrǣdan|to fear|ondˈræːdɑn
+slǣpan|to sleep|ˈslæːpɑn
+libban|to live|ˈlib.bɑn
+sweltan|to die|ˈsweltɑn
+ofslēan|to kill|ofˈslæːɑ̯n
+feohtan|to fight|ˈfeo̯xtɑn
+huntian|to hunt|ˈhuntiɑn
+slēan|to hit|slæːɑ̯n
+snīþan|to cut|ˈsniːðɑn
+tōclēofan|to split|toːˈkleːo̯vɑn
+stingan|to stab|stiŋɡɑn
+stician|to stab|ˈstikiɑn
+clawian|to scratch|ˈklɑwiɑn
+delfan|to dig|ˈdelvɑn
+swimman|to swim|ˈswim.mɑn
+flēogan|to fly|ˈfleːo̯ɣɑn
+gān|to walk|ɣɑːn
+cuman|to come|ˈkumɑn
+liċġan|to lie (as in a bed)|ˈlid.d͡ʒɑn
+sittan|to sit|ˈsit.tɑn
+standan|to stand|ˈstɒndɑn
+wendan|to turn (intransitive)|ˈwendɑn
+feallan|to fall|ˈfæɑ̯ɫ.ɫɑn
+sellan|to give|ˈsel.lɑn
+ġiefan|to give|ˈjiy̯vɑn
+healdan|to hold|ˈhæɑ̯ɫdɑn
+twenġan|to squeeze|ˈtwend͡ʒɑn
+gnīdan|to rub|ˈɣniːdɑn
+þwēan|to wash|θwæːɑ̯n
+wascan|to wash|ˈwɑskɑn
+wīpian|to wipe|ˈwiːpiɑn
+tēon|to pull|ˈteːo̯n
+sċūfan|to push|ˈʃuːvɑn
+weorpan|to throw|ˈweo̯rˠpɑn
+tīeġan|to tie|ˈtiːy̯jɑn
+sīwian|to sew|ˈsiːwiɑn
+tellan|to count|ˈtel.lɑn
+cweþan|to say|ˈkweðɑn
+seċġan|to say|ˈsed.d͡ʒɑn
+singan|to sing|ˈsiŋɡɑn
+plegian|to play|ˈpleɣiɑn
+flēotan|to float|ˈfleːo̯tɑn
+flōwan|to flow|ˈfloːwɑn
+frēosan|to freeze|ˈfreːo̯zɑn
+swellan|to swell|ˈswel.lɑn
+sunne f|sun|ˈsun.ne
+mōna m|moon|ˈmoːnɑ
+steorra m|star|ˈsteo̯rˠ.rˠɑ
+wæter n|water|ˈwæter
+reġn m|rain|ˈrejn̩
+ēa f|river|æːɑ̯
+lacu f|lake|ˈlɑku
+sǣ f|sea|sæː
+sealt n|salt|sæɑ̯ɫt
+stān m|stone|stɑːn
+sand n|sand|sɒnd
+dust n|dust|dust
+eorþe f|earth|eo̯rˠðe
+wolcn n|cloud|ˈwoɫkn̩
+mist m|fog|mist
+heofon m|sky|ˈheo̯von
+wind m|wind|wind
+snāw m|snow|snɑːw
+īs n|ice|iːs
+smīeċ m|smoke|smiːy̯t͡ʃ
+fȳr n|fire|fyːr
+æsċe f|ash|ˈæʃ.ʃe
+birnan|to burn|ˈbirˠ.nɑn|intransitive
+bærnan|to burn|ˈbærˠ.nɑn|transitive
+weġ m|road|wej
+strǣt f|road|stræːt
+beorg m|mountain|beo̯rˠɣ
+dūn f|mountain|duːn
+munt m|mountain|munt
+rēad|red|ræːɑ̯d
+grēne|green|ˈɣreːne
+ġeolu|yellow|ˈjeo̯ɫu
+hwīt|white|ʍiːt
+sweart|black|swæɑ̯rˠt
+blæc|black|blæk
+niht f|night|niçt
+dæġ m|day|dæj
+ġēar n|year|jæːɑ̯r
+wearm|warm|wæɑ̯rˠm
+ċeald|cold|t͡ʃæɑ̯ɫd
+full|full|fuɫː
+nīwe|new|ˈniːwe
+eald|old|æɑ̯ɫd
+gōd|good|ɣoːd
+yfel|bad|ˈyvel
+fūl|rotten|fuːl
+forrotod|rotten|forˠˈrˠotod
+horiġ|dirty|ˈhorij
+riht|straight|riçt
+sinewealt|round|ˈsinewæɑ̯ɫt
+sċearp|sharp (as a knife)|ʃæɑ̯rˠp
+stump|dull (as a knife)|stump
+smēþe|smooth|ˈsmeːðe
+wǣt|wet|wæːt
+drȳġe|dry|ˈdryːje
+riht|correct|riçt
+nēah|near|næːɑ̯x
+feorr|far|feo̯rˠː
+swīþra|right|ˈswiːðrɑ
+winestra|left|ˈwinestrɑ
+æt|at|æt
+on|in|on
+mid|with|mid
+and|and|ɒnd
+ġif|if|jif
+for þon þe|because|for θon θe|because
+for|because|for|because of
+nama|name|ˈnɒmɑ
+
+`
+
