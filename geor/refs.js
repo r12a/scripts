929fa58abcdaaf8ae470c11142213b673d6fcc62
r12a <ishida@w3.org>
2023-01-06 09:12:56 +0000

    Stub for new geor directory

---
+++ geor/refs.js
@@ -0,0 +1,80 @@
+reflist = {
+
+
+"d": {
+"url":"#refs",
+"authors": "Peter T. Daniels and William Bright",
+"title":"The World's Writing Systems",
+"publisher":"Oxford University Press",
+"isbn":"0-19-507993-0",
+},
+
+"l": {
+"url":"https://www.loc.gov/catdir/cpso/romanization/georgian.pdf",
+"authors": "Library of Congress",
+"title":"Georgian",
+},
+
+"m": {
+"url":"https://medium.com/@akakirazmadze/mtavruli-letters-5665d0aec09c",
+"authors": "Akaki Razmadze",
+"title":"Mtavruli letters",
+},
+
+"r": {
+"url":"https://www.unicode.org/wg2/docs/n4712-georgian.pdf",
+"authors": "Michael Everson, Nika Gujejiani, & Akaki Razmadze",
+"title":" Revised proposal for the addition of Georgian characters to the UCS",
+},
+
+"u": {
+"url":"https://www.unicode.org/versions/Unicode13.0.0/ch07.pdf",
+"authors": "Unicode Consortium",
+"title":"The Unicode Standard, Version 13.0",
+"sub":"Chapter 7.7: Europe-I, Georgian",
+"pages":"319-320",
+"isbn":"978-1-936213-16-0.",
+},
+
+"ul": {
+"url":"https://www.unicode.org/reports/tr14/",
+"authors": "Unicode Consortium",
+"title":"Unicode Line Breaking Algorithm (UAX#14)",
+"reviewed":true
+},
+
+"w": {
+"url":"http://en.wikipedia.org/wiki/Georgian_scripts",
+"authors": "Wikipedia",
+"title":"Georgian scripts",
+},
+
+"wl": {
+"url":"http://en.wikipedia.org/wiki/Georgian_language",
+"authors": "Wikipedia",
+"title":"Georgian language",
+},
+
+
+}
+
+
+
+
+
+scriptLanguageList = {
+abk:{name:"Abkhazian"},
+ka:{name:"Georgian", url:"ka.html"},
+lzz:{name:"Laz"},
+xmf:{name:"Mingrelian"},
+oav:{name:"Avar, Old"},
+oss:{name:"Ossetic"},
+sva:{name:"Svan"},
+}
+
+
+
+
+
+
+
