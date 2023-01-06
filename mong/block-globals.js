5a7d47bbaf6b01bc31c2d8ab0a1e181113855244
r12a <ishida@w3.org>
2023-01-06 09:11:06 +0000

    mong: 1st draft of v29

---
+++ mong/block-globals.js
@@ -0,0 +1,11 @@
+
+window.version = 'v27'
+window.scriptName = 'Mongolian'
+window.ghDirectory = 'mongolian'
+window.blockDirectory = 'mongolian'
+window.languageList = ['mn'] // lang tags for each orthography handled
+window.detailsfileList = ['mn'] // first part of xx-details for each orthography
+window.scriptISO = 'mong'
+window.webfonts = ["Noto Sans Mongolian WF"]
+window.defaultSize = '24px'
+
---
+++ mong/mn-globals.js
@@ -0,0 +1,24 @@
+
+
+langTag='mn' // BCP47 language tag for this orthography
+scriptTag='mong' // BCP47 script tag for this orthography
+notesLangtag='mn'
+blockLangtag = 'mn' // same as langTag; used in block.html
+blockDirection=''  // ltr/rtl or '' for ltr; used by block.html
+
+blockDirectoryName='mong'  // folder name where block file is found, no slash
+orthogFilePath='mong/mn'  // path below /scripts, including filename, but no extension, eg. lana/nod
+pickerDir='mong-mn'  // directory name for picker, no slash at end
+scriptSummaryTableName='khk-mong'  // id used for langs record in xx-langdata.js; used to make summary table
+charUsageBCP='khk-mong'  // bcp tag to find the orthography in the charuse app; used in makeIndexIntro only
+orthogName = 'Mongolian' // natural language name for the orthography; used only in makeIndexIntro
+languageName = 'Halh Mongolian' // natural language name of language; used in various places, esp block
+scriptIsIndic = false  // true if there are indic syllabic categories; used only in makeIndexIntro
+
+
+defaultSize = '24px'  // used by block.html to set size of font, including in font mgt panel
+letterClassName = 'mn'  // classname for a given orthography in the block page
+
+
+
+
