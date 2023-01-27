360d4b7098683b314211b85c2c065d8b2d5d543d
r12a <ishida@w3.org>
2023-01-27 08:08:20 +0000

    template update

---
+++ _template/block-globals.js
@@ -0,0 +1,11 @@
+
+window.version = 'v29'
+window.scriptName = 'Punctuation'
+window.ghDirectory = 'punctuation'
+window.blockDirectory = 'punctuation'
+window.languageList = ['en'] // lang tags for each orthography handled
+window.detailsfileList = ['en'] // first part of xx-details for each orthography
+window.scriptISO = 'latn'
+window.webfonts = ["Noto Sans WF"]
+window.defaultSize = '24px'
+
