e02a6a49ddeca8b21c52362f0f96b26abe715ef7
r12a <ishida@w3.org>
2023-01-29 08:45:28 +0000

    ike examples: Fill in transcriptions etc

--- cans/ike-examples.js
+++ cans/ike-examples.js
@@ -6,256 +6,252 @@ ikiaqqivik|website|ikiaqqivik
 
 autoExpandExamples.ike = `
 
--ᕗᑦ||
-ᐁᕕᖃ||
-ᐃᐦᐃᑉᕆᐅᖅᑐᖅ||
-ᐃᐱᕋᐅᑕᖅ||
-ᐃᑉᐸᒃᓴᖅ||
-ᐃᑎᖅ||
-ᐃᒃᑦᓱᐊᕐᐳᒃ||
-ᐃᒃᓯᕙᐅᑕᖅ/ᐃᑦᓯᕙᐅᑕᖅ|chair|iksivautaq/itsivautaq|normal/SBaffin & Nunavik
-ᐃᒡᓕᐊᑉ ᐊᑦᑐᖏᖓ||
-ᐃᒡᓕᖅ||
-ᐃᒡᓗ|house|iglu
-ᐃᒡᓗᕐᔪᐊᖅ|big house|igluʁjuaq (iglurjuaq)
-ᐃᒥᖅ||
-ᐃᒥᖅᑐᖅ||
-ᐃᒪᖅ||
-ᐃᒻᒧᒃ||
-ᐃᒻᒪᖄ||
-ᐃᓄᐃᑦ ᖃᐅᔨᒪᔭᑐᖃᖏᑦ||
-ᐃᓄᐃᑦ|people|inuit
-ᐃᓄᒃ|person|inuk
-ᐃᓄᒃᑎᑐᑦ||
-ᐃᓄᒃᑎᑐᑦ‎|Inuktitut|inuktitut‎
-ᐃᓄᒃᑎᑑᓲᖑᕕᑦ?|Do you speak Inuktitut?|inuktituːsuːŋuvit? (inuktituusuunguvit?)
-ᐃᓄᒃᓱᒃ||
-ᐃᓄᖅ||
-ᐃᓄᖕᒧᑦ|to a person|inuŋmut (inungmut)
-ᐃᓅᒃ|two people|inuːk (inuuk)
-ᐃᓕᓴᐃᔨ|teacher|ilisaiji
-ᐃᓕᔅᓯ||
-ᐃᓗ||
-ᐃᓪᓗ||
-ᐃᓯᐱᕋᓐᑐ||
-ᐃᓴᕈᖅ||
-ᐃᔨ||
-ᐃᕐᒥᐅᑦ||
-ᐃᕐᓂᕋ|my son|iʁniʁa (irnira)
-ᐃᕐᓂᖅ|son|iʁniq|irniq
-ᐃᕝᕕᑦ||
-ᐃᖃᓗᐃᑦ||
-ᐃᖃᓗᒃ||
-ᐃᖏᑦᑐᒪᔪᖓ|I want to sit down|iŋittumajuŋa (ingittumajunga)
-ᐄ||
-ᐅᐃᒃ||
-ᐅᐃᕖᑎᑐᓪᓗ||
-ᐅᐱᕐᓐᖄᖅ||
-ᐅᑎᕈᒪᔪᖓ|I want to return|utiʁumajuŋa (utirumajunga)
-ᐅᑭᐅᖅ||
-ᐅᒃᐱᒃ||
-ᐅᒥᐊᑦ|boats|umiat
-ᐅᒥᐊᒃ|two boats|umiak
-ᐅᒥᐊᕐᔪᐊᕌᓗᒃ|big ship|umiaʁjuaʁaːluk (umiarjuaraaluk)
-ᐅᒥᐊᕐᔪᐊᖅ|big boat, ship|umiaʁjuaq (umiarjuaq)
-ᐅᒥᐊᖅ|boat|umiaq
-ᐅᒥᐊᖓ|his boat|umiaŋa (umianga)
-ᐅᒥᐊᙳᐊᖅ|toy boat|umiaŋŋuaq (umiannguaq)
-ᐅᒥᒃ||
-ᐅᓈᖅ||
-ᐅᓐᓄᖅ||
-ᐅᓗ||
-ᐅᓪᓗᒥ||
-ᐅᓪᓗᕆᐊᖅ||
-ᐅᓱᒃ||
-ᐅᓵᐃᐅ||
-ᐅᕕᐅᖅ||
-ᐅᕙᒍᑦ||
-ᐅᕙᒍᒃ||
-ᐅᕙᖓ||
-ᐅᖃᐅᓯᖅ||
-ᐅᖃᖅᑐᖅ|she speaks|uqaqtuq
-ᐅᖅᓱᖅ||
-ᐆᒪ|this one's|uːma (uuma)
-ᐆᒪᔪᖅ||
-ᐆᒻᒪᑎ||
-ᐊᐃᕕᖅ|walrus|aiviq
-ᐊᐅᒃ||
-ᐊᐅᓪᓚᓛᖅ ᑐᑦ|they will depart|aullalaːq tut (aullalaaq tut)
-ᐊᐅᔪᐃᑦᑐᖅ||
-ᐊᐅᔭ||
-ᐊᐱᓯᒪᔪᖅ||
-ᐊᐳᑦ||
-ᐊᑎᖅᐳᖅ||
-ᐊᑖᑕ|father|ataːta (ataata)
-ᐊᑖᑕᒐ|my father|ataːtaga (ataataga)
-ᐊᑖᓂ||
-ᐊᑦᑐᓈᖅ|rope|attunaːq (attunaaq)|South Qikiqtaaluk
-ᐊᑭᓯᖅ||
-ᐊᒃᓱᓈᖅ|rope|akhunaːq (aksunaaq)|Inuinnaqtun
-ᐊᒃᖢᓈᖅ|rope|akɬunaːq (akɫunaaq)|North Qikiqtaaluk
-ᐊᒡᒐᒃ||
-ᐊᒡᓘᒃᑲᖅ||
-ᐊᒥᐃᐅᓕᐅᕈ||
-ᐊᒪᕈᖅ||
-ᐊᓂᔪᒪᔪᖓ|I want to go out|anijumajuŋa (anijumajunga)
-ᐊᓄᕆ||
-ᐊᓇᐅᓪᓚᐅᑦ||
-ᐊᓈᓇ||
-ᐊᓗᐃᖅᑲᓇᖅ||
-ᐊᓘᑦ||
-ᐊᓪᓕᖅ||
-ᐊᕐᓇᖅ||
-ᐊᕐᕕᒃ||
-ᐊᕕᙵᖅ||
-ᐊᕕᙵᙳᐊᖅ|computer mouse|aviŋŋaŋŋuaq (avinngannguaq)
-ᐊᕙᑎᑦᑎᓐᓂᒃ ᑲᒪᑦᓯᐊᕐᓂᖅ||
-ᐊᖅᐱᒃ||
-ᐊᖏᖅᑲᖅᑐᖅ||
-ᐊᖑᑦ||
-ᐊᖑᓇᓱᒃᑐᖅ||
-ᐊᖓᔪᖅ||
-ᐋᒃᑲ||
-ᐋᓯᕙᖅ||
-ᐯᑉᐹᖅ||
-ᐱ||
-ᐱᓱᒃ||
-ᐱᓱᒡᕕᒃ|walkway|pisugvik
-ᐱᕈᖅᑐᓴᔭᖅ||
-ᐳᐊᓘᒃ||
-ᐴᐹᖅ||
-ᐸᓂᒃ|daughter|panik
-ᐸᓂᒐ|my daughter|paniga
-ᐸᓂᖅ||
-ᐸᕉ ᕿᑭᖅᑕᑦ||
-ᐹᓯᓂᖅ||
-ᑎᑎᕋᐅᑦ||
-ᑎᑭᖅ||
-ᑎᒥ||
-ᑎᕆᒐᓂᐊᖅ||
-ᑎᕆᒐᓐᓂᐊᖅ||
-ᑎᖕᒥᐊᖅ||
-ᑎᖕᒥᓲᖅ||
-ᑏ||
-ᑐᐊᐸᑦ||
-ᑐᐱᖅ|tent|tupiq
-ᑐᐲᒃ|two tents|tupiːk (tupiik)
-ᑐᑐ||
-ᑐᑭᓯᔪᖓ|I understand|tukisijuŋa (tukisijunga)
-ᑐᑭᓯᕕᑦ?|do you understand?|tukisivit?
-ᑐᒃᑐ|caribou|tuktu
-ᑐᒃᑐᐃᑦ|caribou pl.|tuktuit
-ᑐᕙᖅ||
-ᑐᙵᓱᒋᑦ|Welcome!|tuŋŋasugit (tunngasugit)
-ᑑᒑᓕᒃ||
-ᑑᒑᖅ||
-ᑕᐃᓇ|that one|taina
-ᑕᑯᒐᑉᓯ/ᑕᑯᒐᑦᓯ|because you (pl) see|takugapsi/takugatsi|normal/SBaffin & Nunavik
-ᑕᒃᑐᒃ||
-ᑕᓪᓗ||
-ᑕᓪᓗᕈᑎᐅᑉ ᐃᒪᖓ||
-ᑕᓯᕐᔪᐊᖅ|big lake|tasiʁjuaq (tasirjuaq)
-ᑕᓯᖅ||
-ᑕᕆᐅᖅ||
-ᑕᕝᕙ||
-ᑭᐊᑎ||
-ᑭᐊᒃᑎᓯᒪᔪᖅ||
-ᑭᒍᑕᖕᒋᕐᓇᖅ|blueberry|kigutaŋgiʁnaq (kigutanggirnaq) 
-ᑭᓇ||
-ᑭᓴᐅᑦ|anchor|kisaut|South Qikiqtaaluk
-ᑭᓴᖅ|anchor|kihaq|Paallirmiutut
-ᑭᖓᖅ||
-ᑮᓇᐅᔭᖅ||
-ᑮᓇᖅ||
-ᑯᒡᔪᐊᖅ|big river|kugjuaq
-ᑰᒃ||
-ᑲᑎᕝᕕᒃ/ᑲᑎᕕᒃ|meeting-place|kativvik/kativik|(latter in Nunavik & S/E Baffin)
-ᑲᑭᓃᑦ||
-ᑲᒥᒃ||
-ᑲᓇᑕ||
-ᑲᖏᖅᑐᒃ|inlet, fjord|kaŋiqtuk
-ᑳᐱ||
-ᑳᒃᑐᖅ||
-ᒥᓗᑦᓱᑳᒐᖅ||
-ᒪ'ᓇ|thank you|maʔna|Paallirmiutut
-ᒪᐅᔭ||
-ᒪᒻᒪᓛᑎᒃ||
-ᓂᐅ||
-ᓂᐅᒃ||
-ᓂᐅᕕᕐᕕᒃ|store|niuviʁvik (niuvirvik)
-ᓂᐅᕕᖅᑎ|trader|niuviqti
-ᓂᐊᖁᖅ||
-ᓂᐊᖂᔮᖅ||
-ᓂᔾᔭᔪᑦ|music|nid͡ʒajut|nijjajut
-ᓂᕆᔪᖅ||
-ᓂᕿ||
-ᓃᖁᓗᒃᑐᖅ||
-ᓄᑕᕋᒃᓴᐅᑉ ᐃᒪᖓ||
-ᓄᑲᖅ||
-ᓄᓇ|land|nuna
-ᓄᓇᑐᑲᕗᑦ||
-ᓄᓇᑦᓯᐊᕗᑦ||
-ᓄᓇᒥ|on the land|nunami
-ᓄᓇᒧᑦ|to the land|nunamut
-ᓄᓇᕕᒃ||
-ᓄᓇᕕᒻᒥᐅᑦ||
-ᓄᓇᕗᑦ||
-ᓄᓇᕗᒻᒥᐅᖅ||
-ᓄᓕᐊᖅ||
-ᓄᔭᑦ||
-ᓄᖅᑲᕆᑦ||
-ᓇᑦᓯᖅ|seal|natsiq
-ᓇᑯᕐᒦᒃ|thank you|nakuʁmiːk (nakurmiik)| South Qikiqtaaluk
-ᓇᓂ||
-ᓇᓄᒃ||
-ᓇᓄᖅ||
-ᓈᓚᒃᐱᑦ?|are you listening?|naːlakpit? (naalakpit?)
-ᓈᓚᒃᑐᖓ|I listen|naːlaktuŋa (naalaktunga)
-ᓕᐅᓇ||
-ᓯᐅᑦ||
-ᓯᐱᓂᖅ||
-ᓯᓂᒍᒪᔪᖓ|I want to sleep|sinigumajuŋa (sinigumajunga)
-ᓯᓈ||
-ᓯᓚ||
-ᓯᓚᒥ||
-ᓯᕿᓂᖅ||
-ᓯᖏᕐᓂᖅ||
-ᓱᓇᒐᖅ||
-ᓲᓴᓐ||
-ᓴᐃᒧ||
-ᓴᑯ||
-ᓴᕕᒃ||
-ᔪᐊᑕᓐ||
-ᔭᓐᓄᐊᓕ||
-ᕼᐊᓐᑕ||
-ᕼᐋᑭ||
-ᕿᒃᑐᕆᐊᖅ||
-ᕿᒃᑐᕆᐊᖅᑕᐅᓂᖅ||
-ᕿᒻᒥᒃ||
-ᕿᒻᒥᖅ|dog|qimmiq
-ᕿᒻᒦᑦ|dogs|qimmiːt (qimmiit)
-ᕿᒻᒧᒃᓯᑦ||
-ᕿᓚᓗᒐᖅ||
-ᕿᓯᒃ||
-ᕿᖓᖅ||
-ᖁᑭᐅᑦ||
-ᖁᓕᖏᓗᐊᖅᑐᑦ||
-ᖁᔭᓐᓇᒦᒃ||
-ᖃᐅᒃᐸᑦ||
-ᖃᐅᓯᖅᑐᖅ||
-ᖃᐅᔨᒪᓂᑐᖃᖏᑦ||
-ᖃᐅᔨᒪᔭᑐᖃᖏᑦ||
-ᖃᑉᓗ|eyebrow|qablu|Inuinnaqtun
-ᖃᒧᑏᒃ||
-ᖃᓂᖅ||
-ᖃᓪᓗ|eyebrow|qallu|South Qikiqtaaluk
-ᖃᓪᓗᓈᖅ||
-ᖃᔭᖅ|kayak|qajaq
-ᖃᖅᑲᖅ||
-ᖃᖓ||
-ᖃᖓᑕ||
-ᖃᖓᑕᓲᖅ||
-ᖄᓐᕿᔭᐅᑦ||
+-ᕗᑦ|we, our suffix||-vut
+ᐁᕕᖃ|walrus||aiviq
+ᐃᐦᐃᑉᕆᐅᖅᑐᖅ|to explore|i§h§i§p§ɢi§u§q§tu§q|ihipqiuqtuq
+ᐃᐱᕋᐅᑕᖅ|whip||ipirautaq
+ᐃᑉᐸᒃᓴᖅ|yesterday||ippaksaq
+ᐃᒃᑦᓱᐊᕐᐳᒃ|to go outside often to see if someone is coming||iktsuarpok
+ᐃᒃᓯᕙᐅᑕᖅ|chair|i§k§si§va§u§ta§q§||normal|x
+ᐃᑦᓯᕙᐅᑕᖅ|chair|i§t§si§va§u§ta§q||SBaffin & Nunavik|x
+ᐃᒡᓕᐊᑉ ᐊᑦᑐᖏᖓ|fallopian tube||igliap attunginga
+ᐃᒡᓕᖅ|bed||igliq
+ᐃᒡᓗ|house|i§g§lu
+ᐃᒡᓗᕐᔪᐊᖅ|big house|i§g§lu§ʁ§ju§a§q||iglurjuaq
+ᐃᒥᖅ|drinking water||imiq
+ᐃᒥᖅᑐᖅ|to drink||imiqtuq
+ᐃᒪᖅ|water||imaq
+ᐃᒻᒧᒃ|milk||immuk
+ᐃᒻᒪᖄ|perhaps||immaqaa
+ᐃᓄᐃᑦ ᖃᐅᔨᒪᔭᑐᖃᖏᑦ|traditional inuit knowledge||inuit qaujimajatuqangit
+ᐃᓄᐃᑦ|people|i§nu§i§t
+ᐃᓄᒃ|person, man|ˈi.§nu§k|inuq
+ᐃᓄᒃᑎᑐᑦ|Inuktitut|i§nu§k§ti§tu§t‎
+ᐃᓄᒃᑎᑑᓲᖑᕕᑦ?|Do you speak Inuktitut?|i§nu§k§ti§tuː§suː§ŋu§vi§t§?|inuktituusuunguvit?
+ᐃᓄᒃᓱᒃ|to act as human||inuksuk
+ᐃᓄᖅ|person, man||inuq|alt. for inuk
+ᐃᓄᖕᒧᑦ|to a person|i§nu§ŋ§mu§t|inungmut||x
+ᐃᓅᒃ|two people|i§nuː§k|inuuk||x
+ᐃᓕᓴᐃᔨ|teacher|i§li§sa§i§ji|||x
+ᐃᓕᔅᓯ|you all||ilissi
+ᐃᓗ|cave||ilu
+ᐃᓪᓗ|house||illu
+ᐃᓯᐱᕋᓐᑐ|Esperanto||isipirantu
+ᐃᓴᕈᖅ|wing||isaruq
+ᐃᔨ|eye||iji
+ᐃᕐᒥᐅᑦ|soap||irmiut
+ᐃᕐᓂᕋ|my son|i§ʁ§ni§ʁa|irnira||x
+ᐃᕐᓂᖅ|son|i§ʁ§ni§q|irniq
+ᐃᕝᕕᑦ|you||ivvit
+ᐃᖃᓗᐃᑦ|Iqaluit||iqaluit
+ᐃᖃᓗᒃ|fish; salmon, trout||iqaluk
+ᐃᖏᑦᑐᒪᔪᖓ|I want to sit down|i§ŋi§t§tu§ma§ju§ŋa|ingittumajunga||x
+ᐄ|yes||ii
+ᐅᐃᒃ|husband||uik
+ᐅᐃᕖᑎᑐᓪᓗ|French (language)||uiviititullu
+ᐅᐱᕐᓐᖄᖅ|spring, early summer||upirnqaaq
+ᐅᑎᕈᒪᔪᖓ|I want to return|u§ti§ʁu§ma§ju§ŋa|utirumajunga||x
+ᐅᑭᐅᖅ|winter||ukiuq
+ᐅᒃᐱᒃ|snowy owl||ookpik
+ᐅᒥᐊᑦ|boats||umiat||x
+ᐅᒥᐊᒃ|two boats||umiak||x
+ᐅᒥᐊᕐᔪᐊᕌᓗᒃ|big ship|u§mi§a§ʁ§ju§a§ʁaː§lu§k|umiarjuaraaluk||x
+ᐅᒥᐊᕐᔪᐊᖅ|big boat, ship|u§mi§a§ʁ§ju§a§q|umiarjuaq||x
+ᐅᒥᐊᖅ|boat|u§mi§a§q|umiaq||x
+ᐅᒥᐊᖓ|his boat|u§mi§a§ŋa|umianga||x
+ᐅᒥᐊᙳᐊᖅ|toy boat|u§mi§a§ŋ§ŋu§a§q|umiannguaq||x
+ᐅᒥᒃ|beard||umik
+ᐅᓈᖅ|harpoon||unaaq
+ᐅᓐᓄᖅ|night, evening||unnuq
+ᐅᓗ|woman's knife||ulu
+ᐅᓪᓗᒥ|today||ullumi
+ᐅᓪᓗᕆᐊᖅ|star|u§l.§lu.§ʁi§ˈa§q|ulluriaq
+ᐅᓵᐃᐅ|Ohio||usaaiu
+ᐅᕙᒍᑦ|we, us (pl.)||uvagut
+ᐅᕙᒍᒃ|we, us (dual)||uvaguk
+ᐅᕙᖓ|me, mine||uvanga
+ᐅᖃᐅᓯᖅ|word||uqausiq
+ᐅᖃᖅᑐᖅ|she speaks|u§qa§q§tu§q|||x
+ᐅᖅᓱᖅ|oil, blubber||uqsuq
+ᐆᒪ|this one's|uː§ma|uuma||x
+ᐆᒪᔪᖅ|animal||uumajuq
+ᐆᒻᒪᑎ|heart||uummati
+ᐊᐃᕕᖅ|walrus|a§i§vi§q|aiviq||x
+ᐊᐅᒃ|blood||aik
+ᐊᐅᓪᓚᓛᖅ ᑐᑦ|they will depart|a§u§l§la§laː§q§ §tu§t|aullalaaq tut||x
+ᐊᐅᔪᐃᑦᑐᖅ|permafrost||aujuittuq
+ᐊᐅᔭ|summer||auja
+ᐊᐱᓯᒪᔪᖅ|snow-covered||apisimajuq
+ᐊᐳᑦ|snow||aput
+ᐊᑎᖅᐳᖅ|bear||atiqpuq
+ᐊᑖᑕ|father|a§taː§ta|ataata
+ᐊᑖᑕᒐ|my father|a§taː§ta§ga|ataataga
+ᐊᑖᓂ|underneath||ataani
+ᐊᑦᑐᓈᖅ|rope|a§t§tu§naː§q|attunaaq|South Qikiqtaaluk|x
+ᐊᑭᓯᖅ|pillow||akisiq
+ᐊᒃᓱᓈᖅ|rope|a§k§hu§naː§q|aksunaaq|Inuinnaqtun|x
+ᐊᒃᖢᓈᖅ|rope|a§k§ɬu§naː§q|akɫunaaq|North Qikiqtaaluk|x
+ᐊᒡᒐᒃ|hand||aggak
+ᐊᒡᓘᒃᑲᖅ|Agluukkaq (name)||agluukkaq
+ᐊᒥᐃᐅᓕᐅᕈ|||amiiuliuru
+ᐊᒪᕈᖅ|wolf||amaruq
+ᐊᓂᔪᒪᔪᖓ|I want to go out|a§ni§ju§ma§ju§ŋa|anijumajunga||x
+ᐊᓄᕆ|wind||anuri
+ᐊᓇᐅᓪᓚᐅᑦ|bat (sport)||anaullaut
+ᐊᓈᓇ|mother||anaana
+ᐊᓗᐃᖅᑲᓇᖅ|avalanche||aluiqqanaq
+ᐊᓘᑦ|spoon||aluut
+ᐊᓪᓕᖅ|underneath||alliq
+ᐊᕐᓇᖅ|woman||arnaq
+ᐊᕐᕕᒃ|whale||arvik
+ᐊᕕᙵᖅ|lemming||avinngaq
+ᐊᕕᙵᙳᐊᖅ|computer mouse|a§vi§ŋŋa§ŋŋu§a§q|avinngannguaq||x
+ᐊᕙᑎᑦᑎᓐᓂᒃ ᑲᒪᑦᓯᐊᕐᓂᖅ|environmental stewardship||avatittinnik kamatsiarniq
+ᐊᖅᐱᒃ|cloud berry||aqpik
+ᐊᖏᖅᑲᖅᑐᖅ|to go home||angiqqaqtuq
+ᐊᖑᑦ|man||angut
+ᐊᖑᓇᓱᒃᑐᖅ|to hunt||angunasuktuq
+ᐊᖓᔪᖅ|older sibling||angajuq
+ᐋᒃᑲ|no||aakka
+ᐋᓯᕙᖅ|spider||aasivaq
+ᐯᑉᐹᖅ|paper||paippaaq
+ᐱ|thing||pi
+ᐱᓱᒃ|to be walking||pisuk
+ᐱᓱᒡᕕᒃ|walkway|pi§su§g§vi§k|pisugvik||x
+ᐱᕈᖅᑐᓴᔭᖅ|flower||piruqtusajaq
+ᐳᐊᓘᒃ|pair of mitts||pualuuk
+ᐴᐹᖅ|amniotic sac||puupaaq
+ᐸᓂᒃ|daughter|pa§ni§k|panik||x
+ᐸᓂᒐ|my daughter|pa§ni§ga|paniga||x
+ᐸᓂᖅ|daughter||paniq
+ᐸᕉ ᕿᑭᖅᑕᑦ|Faroe Islands||Paruu Qikiqtat
+ᑎᑎᕋᐅᑦ|pen, pencil||titiraut
+ᑎᑭᖅ|finger||tikiq
+ᑎᒥ|body||timi
+ᑎᕆᒐᓂᐊᖅ|arctic fox||tiriganiaq
+ᑎᕆᒐᓐᓂᐊᖅ|fox||tiriganniaq
+ᑎᖕᒥᐊᖅ|bird||tingmiaq
+ᑎᖕᒥᓲᖅ|airplane||tingmisuuq
+ᑏ|tea||tii
+ᑐᐊᐸᑦ|gravel||tuapat
+ᑐᐱᖅ|tent|tu§pi§q|tupiq||x
+ᑐᐲᒃ|two tents|tu§piː§k|tupiik||x
+ᑐᑐ|Tootoo (name)||tutu
+ᑐᑭᓯᔪᖓ|I understand|tu§ki§si§ju§ŋa|tukisijunga||x
+ᑐᑭᓯᕕᑦ?|do you understand?|tu§ki§si§vi§t§?|tukisivit?||x
+ᑐᒃᑐ|caribou|tu§k§tu|tuktu
+ᑐᒃᑐᐃᑦ|caribou pl.|tu§k§tu§i§t|tuktuit||x
+ᑐᕙᖅ|sea ice||tuvaq
+ᑐᙵᓱᒋᑦ|Welcome!|tu§ŋŋa§su§gi§t|tunngasugit||x
+ᑑᒑᓕᒃ|narwhal||tuugaalik
+ᑑᒑᖅ|ivory tusk|tuugaaq
+ᑕᐃᓇ|that one|ta§i§na|taina||x
+ᑕᑯᒐᑉᓯ|because you (pl) see|ta§ku§ga§p§si||normal|x
+ᑕᑯᒐᑦᓯ|because you (pl) see|ta§ku§ga§t§si||SBaffin & Nunavik|x
+ᑕᒃᑐᒃ|fog||taktuk
+ᑕᓪᓗ|chin||tallu
+ᑕᓪᓗᕈᑎᐅᑉ ᐃᒪᖓ|Lancaster Sound||tallurutiup imanga
+ᑕᓯᕐᔪᐊᖅ|big lake|ta§si§ʁ§ju§a§q|tasirjuaq||x
+ᑕᓯᖅ|lake||tasiq
+ᑕᕆᐅᖅ|sea||tariuq
+ᑕᕝᕙ|here||tavva
+ᑭᐊᑎ|trunk (anat.)||kiati
+ᑭᐊᒃᑎᓯᒪᔪᖅ|sweat||kiaktisimajuq
+ᑭᒍᑕᖕᒋᕐᓇᖅ|blueberry|kigutaŋgiʁnaq|kigutanggirnaq||x
+ᑭᓇ|who?||kina
+ᑭᓴᐅᑦ|anchor|ki§sa§u§t|kisaut|South Qikiqtaaluk|x
+ᑭᓴᖅ|anchor|ki§ha§q|kihaq|Paallirmiutut|x
+ᑭᖓᖅ|hill||kingaq
+ᑮᓇᐅᔭᖅ|money||kiinaujaq
+ᑮᓇᖅ|face||kiinaq
+ᑯᒡᔪᐊᖅ|big river|ku§g§ju§a§q|kugjuaq||x
+ᑰᒃ|river||kuuk
+ᑲᑎᕝᕕᒃ|meeting-place|ka§ti§v§vi§k|kativvik||x
+ᑲᑎᕕᒃ|meeting-place|ka§ti§vi§k|kativik|Nunavik & S/E Baffin|x
+ᑲᑭᓃᑦ|tattoo||kakiniit
+ᑲᒥᒃ|boot||kamik
+ᑲᓇᑕ|Canada||Kanata
+ᑲᖏᖅᑐᒃ|inlet, fjord|ka§ŋi§q§tu§k|kaŋiqtuk||x
+ᑳᐱ|coffee||kaapi
+ᑳᒃᑐᖅ|hungry||kaaktuq
+ᒥᓗᑦᓱᑳᒐᖅ|tomato||milutsukaagaq
+ᒪ'ᓇ|thank you|ma§ʔ§na|maʔna|Paallirmiutut||x
+ᒪᐅᔭ|deep, soft snow||mauja
+ᒪᒻᒪᓛᑎᒃ|mammal||mammalaatik
+ᓂᐅ|leg||niu
+ᓂᐅᒃ|the leg|niuq
+ᓂᐅᕕᕐᕕᒃ|store|ni§u§vi§ʁ§vi§k|niuvirvik||x
+ᓂᐅᕕᖅᑎ|trader|ni§u§vi§q§ti|niuviqti||x
+ᓂᐊᖁᖅ|head||niaquq
+ᓂᐊᖂᔮᖅ|bread||niaquujaaq
+ᓂᔾᔭᔪᑦ|music|ni§d§d͡ʒa§ju§t|nijjajut||x
+ᓂᕆᔪᖅ|to eat||nirijuq
+ᓂᕿ|food||niqi
+ᓃᖁᓗᒃᑐᖅ|bruxism||niiquluktuq
+ᓄᑕᕋᒃᓴᐅᑉ ᐃᒪᖓ|amniotic fluid||nutaraksaup imanga
+ᓄᑲᖅ|younger sibling||nukaq
+ᓄᓇ|land|nu§na|nuna
+ᓄᓇᑐᑲᕗᑦ|our ancient land||nunatukavut
+ᓄᓇᑦᓯᐊᕗᑦ|our beautiful land||nunatsiavut
+ᓄᓇᒥ|on the land|nu§na§mi|nunami||x
+ᓄᓇᒧᑦ|to the land|nu§na§mu§t|nunamut||x
+ᓄᓇᕕᒃ|Nunavik||Nunavik
+ᓄᓇᕕᒻᒥᐅᑦ|person from Nunavik||nunavimmiut
+ᓄᓇᕗᑦ|Nunavut||Nunavut
+ᓄᓇᕗᒻᒥᐅᖅ|person from Nunavut||nunavummiuq
+ᓄᓕᐊᖅ|wife||nuliaq
+ᓄᔭᑦ|head||nujat
+ᓄᖅᑲᕆᑦ|to stop||nuqqarit
+ᓇᑦᓯᖅ|seal|na§t§si§q|natsiq||x
+ᓇᑯᕐᒦᒃ|thank you|na§ku§ʁ§miː§k|nakurmiik|South Qikiqtaaluk|x
+ᓇᓂ|where?||nani
+ᓇᓄᒃ|polar bear||nanuk
+ᓇᓄᖅ|polar bear||nanuq
+ᓈᓚᒃᐱᑦ?|are you listening?|naː§la§k§pi§t§?|naalakpit?||x
+ᓈᓚᒃᑐᖓ|I listen|naː§la§k§tu§ŋa|naalaktunga||x
+ᓕᐅᓇ|Liuna (name)||liuna
+ᓯᐅᑦ|ear||siut
+ᓯᓂᒍᒪᔪᖓ|I want to sleep|si§ni§gu§ma§ju§ŋa|sinigumajunga||x
+ᓯᓈ|floe edge||sinaa
+ᓯᓚ|weather||sila
+ᓯᓚᒥ|outside||silami
+ᓯᕿᓂᖅ|sun||siqiniq
+ᓯᖏᕐᓂᖅ|ankle||singirniq
+ᓱᓇᒐᖅ|bullet|su.§na§ˈɣa§ɡ|sunagaq
+ᓲᓴᓐ|Susan||suusan
+ᓴᐃᒧ|goodbye||saimu
+ᓴᑯ|head of harpoon||saku
+ᓴᕕᒃ|knife||savik
+ᔪᐊᑕᓐ|Jordan (name)||juatan
+ᔭᓐᓄᐊᓕ|January||jannuali
+ᕼᐋᑭ|ice hockey||haaki
+ᕿᒃᑐᕆᐊᖅ|mosquito||qikturiaq
+ᕿᒃᑐᕆᐊᖅᑕᐅᓂᖅ|mosquito bite||qikturiaqtauniq
+ᕿᒻᒥᒃ|dog||qimmik
+ᕿᒻᒥᖅ|dog|qi§m§mi§q|qimmiq
+ᕿᒻᒦᑦ|dogs|qi§m§miː§t|qimmiit||x
+ᕿᒻᒧᒃᓯᑦ|husky dog team||qimmuksit
+ᕿᓚᓗᒐᖅ|beluqa whale||qilalugaq
+ᕿᓯᒃ|sealskin||qisik
+ᕿᖓᖅ|nose||qingaq
+ᖁᑭᐅᑦ|gun, rifle||qukiut
+ᖁᓕᖏᓗᐊᖅᑐᑦ|nine||qulingiluaqtut
+ᖁᔭᓐᓇᒦᒃ|thank you||qujannamiik
+ᖃᐅᒃᐸᑦ|tomorrow||qaukpat
+ᖃᐅᓯᖅᑐᖅ|wet||qausiqtuq
+ᖃᐅᔨᒪᓂᑐᖃᖏᑦ|traditional knowledge||qaujimanituqangit
+ᖃᐅᔨᒪᔭᑐᖃᖏᑦ|traditional knowledge||qaujimajatuqangit
+ᖃᑉᓗ|eyebrow|qa§b§lu|qablu|Inuinnaqtun|x
+ᖃᒧᑏᒃ|sled||qamutiik
+ᖃᓂᖅ|mouth||qaniq
+ᖃᓪᓗ|eyebrow|qa§l§lu|qallu|South Qikiqtaaluk|x
+ᖃᓪᓗᓈᖅ|non-Inuit||qallunaaq
+ᖃᔭᖅ|kayak|qa§ˈjaʁ§k|qajaq
+ᖃᖅᑲᖅ|Qaqqaq (name)||qaqkaq
+ᖃᖓ|when||qanga
+ᖃᖓᑕ|to raise in the air||qangata
+ᖃᖓᑕᓲᖅ|airplane||qangatasuuq
+ᖄᓐᕿᔭᐅᑦ|pairing knife||qaanqijaut
 
 
 
