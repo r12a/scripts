692874350f2cbc3e7677a5efe8a2f2f78aaaf934
r12a <ishida@w3.org>
2023-01-30 08:12:33 +0000

    cans/crk-examples: Standardise top

--- cans/crk-examples.js
+++ cans/crk-examples.js
@@ -1,12 +1,25 @@
 /*
-Sources: 
-Mostly Wikipedia:Plains Cree article
+Sources: Wikipedia:Plains Cree article
 Wiktionary https://en.wiktionary.org/wiki/Category:Plains_Cree_lemmas
-§ 'Noto Sans Canadian Aboriginal'
-# 200
-~ ltr
-! crk
+
+native | meaning | IPA① | transcription | other transcriptions② & notes | wiktionary③
+
+
+① separate each text unit (consonant+combining_mark(s)) with §, separate phonetic syllables with .
+   use ‹ and › for a sound that spans 2 text units
+   use – for a text unit that doesn't produce sound
+   syllable boundary markers go inside previous unit, and emphasis markers begin emphasised unit
+
+② the previous transcription field is always Latin. 
+   if there is another key orthography (eg. Mongolian cyrillic) it goes here.
+   vowelled/unvowelled alternatives also go here
+
+③ x means that no link to Wiktionary should be added
+   if Wiktionary spells the word differently, add the spelling here
 */
+
+
+
 autoExpandExamples.crk = `
 
 
