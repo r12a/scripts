1c688854db94a0791917e67713af93e5e010c025
r12a <ishida@w3.org>
2023-02-18 17:54:04 +0000

    mid: Apply https://github.com/r12a/scripts/issues/123

--- mand/mid-examples.js
+++ mand/mid-examples.js
@@ -3,78 +3,78 @@
 autoExpandExamples.mid = `
 
 
--ࡖ||
-ࡀࡃࡀࡌ ࡓࡁࡀ||
-ࡀࡃࡀࡌ||
-ࡀࡃࡅࡍࡀࡉ||
-ࡀࡃࡌ||
-ࡀࡋࡀࡄࡀ||
-ࡀࡋࡌࡀ ࡖࡄࡔࡅࡊࡀ||
-ࡀࡋࡌࡀ ࡖࡍࡄࡅࡓࡀ||
-ࡀࡋࡌࡀ||
-ࡀࡌࡓࡀ||
-ࡀࡍࡀࡔࡀ||
-ࡀࡍࡅࡔ||
-ࡀࡐࡀ||
-ࡀࡐࡀࡓࡀ||
-ࡁࡀࡁࡉࡋ||
-ࡁࡀࡂࡀࡋ||
-ࡁࡉࡋ||
-ࡁࡉࡋࡀࡅࡓ||
-ࡁࡉࡕ ࡌࡉࡒࡃࡀࡔࡅࡀ||
-ࡁࡉࡕ||
-ࡁࡓ||
-ࡁࡓࡀࡄࡉࡌ||
-ࡂࡉࡍࡆࡀ||
-ࡃࡓࡀࡔࡀ||
-ࡄࡀࡁࡔࡀࡁࡀ||
-ࡄࡉࡁࡅࡋ||
-ࡄࡉࡉࡀ ࡒࡀࡃࡌࡀࡉࡉࡀ||
-ࡄࡉࡉࡀ ࡓࡁࡉࡀ||
-ࡄࡉࡉࡀ||
-ࡆࡀࡓࡅࡀࡍ||
-ࡆࡉࡃࡒࡀ||
-ࡉࡀࡄࡁࡀ||
-ࡉࡀࡄࡅࡃࡉࡋࡀࡏࡉࡋ||
-ࡉࡅࡔࡀࡌࡉࡍ||
-ࡊࡀࡄࡍࡀ||
-ࡊࡋࡀࡕࡀ||
-ࡋࡉࡁ࡛ࡀ|heart|lib˖ā
-ࡌࡀࡋࡊࡀ ࡖࡍࡄࡅࡓࡀ||
-ࡌࡀࡋࡊࡀ||
-ࡌࡀࡍࡀ ࡓࡁࡀ||
-ࡌࡀࡍࡀ||
+-ࡖ|of||ḏ-
+ࡀࡃࡀࡌ ࡓࡁࡀ|soul, mind||adām rba
+ࡀࡃࡀࡌ|Adam||adām
+ࡀࡃࡅࡍࡀࡉ|title for the sun||adunay
+ࡀࡃࡌ|Adam||adam
+ࡀࡋࡀࡄࡀ|God||alaha
+ࡀࡋࡌࡀ ࡖࡄࡔࡅࡊࡀ|World of Darkness||alma d-hšuka
+ࡀࡋࡌࡀ ࡖࡍࡄࡅࡓࡀ|World of Light||alma d-nhura
+ࡀࡋࡌࡀ|world||alma
+ࡀࡌࡓࡀ|Amram||amrā
+ࡀࡍࡀࡔࡀ|person, human||anaša
+ࡀࡍࡅࡔ|Enoch||anōš
+ࡀࡐࡀ|father, ancestor||aba
+ࡀࡐࡀࡓࡀ|lead (metal)||abara
+ࡁࡀࡁࡉࡋ|Babylon||bābīl
+ࡁࡀࡂࡀࡋ|mule||baglal
+ࡁࡉࡋ|Bel; Jupiter||bīl
+ࡁࡉࡋࡀࡅࡓ|crystal||bīlawr
+ࡁࡉࡕ ࡌࡉࡒࡃࡀࡔࡅࡀ|Jerusalem Temple||bīt mīqdāšia
+ࡁࡉࡕ|house||bīt
+ࡁࡓ|son||b'r
+ࡁࡓࡀࡄࡉࡌ|Abraham||brahim
+ࡂࡉࡍࡆࡀ|treasure||ginza
+ࡃࡓࡀࡔࡀ|sermon||draša
+ࡄࡀࡁࡔࡀࡁࡀ|Sunday||habšaba
+ࡄࡉࡁࡅࡋ|Abel||hibil
+ࡄࡉࡉࡀ ࡒࡀࡃࡌࡀࡉࡉࡀ|First Life||haiyi qadmaiyi
+ࡄࡉࡉࡀ ࡓࡁࡉࡀ|Great Life||haije rabia
+ࡄࡉࡉࡀ|life||haije
+ࡆࡀࡓࡅࡀࡍ|Ahura Mazda||zarvan
+ࡆࡉࡃࡒࡀ|righteousness||zidqa
+ࡉࡀࡄࡁࡀ|Judaism||yahba
+ࡉࡀࡄࡅࡃࡉࡋࡀࡏࡉࡋ|Judea||yahwdyla'yl
+ࡉࡅࡔࡀࡌࡉࡍ|Yoshamin||Yōšamin
+ࡊࡀࡄࡍࡀ|Jewish priest||kahna
+ࡊࡋࡀࡕࡀ|three|k§l§ɒː§θ§ɒ|klɔ̄ṯɔ
+ࡋࡉࡁ࡛ࡀ|heart||lib˖ā
+ࡌࡀࡋࡊࡀ ࡖࡍࡄࡅࡓࡀ|King of Light||malka d-nhura
+ࡌࡀࡋࡊࡀ|king||malka
+ࡌࡀࡍࡀ ࡓࡁࡀ|Great Mind||mana rba
+ࡌࡀࡍࡀ|mind||mana
 ࡌࡀࡍࡃࡀ ࡖࡄࡉࡉࡀ||
-ࡌࡀࡍࡃࡀ||
-ࡌࡀࡍࡃࡉࡉࡀࡍࡀ||
-ࡌࡀࡑࡁࡅࡕࡀ||
-ࡌࡀࡓࡀ ࡖࡄࡔࡅࡊࡀ||
-ࡌࡀࡓࡀ ࡖࡓࡀࡁࡅࡕࡀ||
-ࡌࡀࡓࡀ||
-ࡌࡅࡒࡃࡀࡎ||
-ࡌࡉࡀ||
-ࡌࡉࡓࡉࡀࡉ||
-ࡌࡉࡔࡀ||
-ࡍࡀࡑࡅࡓࡀࡉࡀ||
-ࡍࡅ||
-ࡍࡉࡎࡓࡀࡕ||
-ࡍࡉࡔࡉࡌࡕࡀ||
-ࡎࡉࡍࡀࡉ||
-ࡏࡅࡓࡀࡔࡋࡀࡌ||
-ࡏࡅࡕࡓࡀ||
-ࡏࡔࡅ||
-ࡐࡀࡂࡓࡀ||
-ࡒࡀࡃࡅࡔ||
-ࡒࡀࡃࡅࡔࡀࡍ||
-ࡓࡁࡉࡀ||
+ࡌࡀࡍࡃࡀ|knowledge||manda
+ࡌࡀࡍࡃࡉࡉࡀࡍࡀ|Mandaeism||mandeyānā
+ࡌࡀࡑࡁࡅࡕࡀ|baptism||maṣwetta
+ࡌࡀࡓࡀ ࡖࡄࡔࡅࡊࡀ|Lord of Darkness||mara d-hšuka
+ࡌࡀࡓࡀ ࡖࡓࡀࡁࡅࡕࡀ|Lord of Greatness||mara d-rabuta
+ࡌࡀࡓࡀ|lord||mara
+ࡌࡅࡒࡃࡀࡎ|Jerusalem||muqdas
+ࡌࡉࡀ|water||mya
+ࡌࡉࡓࡉࡀࡉ|Mary||miryai
+ࡌࡉࡔࡀ|Moses||mīšā
+ࡍࡀࡑࡅࡓࡀࡉࡀ|initiate||naṣuraia
+ࡍࡅ|Noah||nū
+ࡍࡉࡎࡓࡀࡕ|Nazareth||nisrat
+ࡍࡉࡔࡉࡌࡕࡀ|soul||nišimta
+ࡎࡉࡍࡀࡉ|Sinai||sinai
+ࡏࡅࡓࡀࡔࡋࡀࡌ|Jerusalem||urašlām
+ࡏࡅࡕࡓࡀ|angel||uthra
+ࡏࡔࡅ|Jesus||’šu/īšū
+ࡐࡀࡂࡓࡀ|body||pagra
+ࡒࡀࡃࡅࡔ|Ephitet of Adonai||qādūš
+ࡒࡀࡃࡅࡔࡀࡍ|angel||qādūšān
+ࡓࡁࡉࡀ|master||rabia
 ࡔࡀࡃࡀࡉ||
-ࡔࡀࡃࡀࡍࡀ||
-ࡔࡅࡁࡀ||
-ࡔࡅࡌ||
-ࡕࡀࡓࡌࡉࡃࡀ||
-ࡕࡉࡁࡉࡋ||
-ࡕࡓࡉࡎࡀࡓ||
-ࡖ||
+ࡔࡀࡃࡀࡍࡀ|Jewish name for God||šidana
+ࡔࡅࡁࡀ|seven||šuba
+ࡔࡅࡌ|Shem||šum
+ࡕࡀࡓࡌࡉࡃࡀ|disciple, priest||tarmida
+ࡕࡉࡁࡉࡋ|world||tibil
+ࡕࡓࡉࡎࡀࡓ|twelve||trisar
+
 
 
 
