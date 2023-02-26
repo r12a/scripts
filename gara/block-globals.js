cd7c3ef029bd983b6d40e45466e1501f3d857eb9
r12a <ishida@w3.org>
2023-02-26 18:54:28 +0000

    garay: Block. Direction fixes. Complete sample.

---
+++ gara/block-globals.js
@@ -0,0 +1,11 @@
+
+window.version = 'v29'
+window.scriptName = 'Garay'
+window.ghDirectory = 'garay'
+window.blockDirectory = 'garay'
+window.languageList = ['wo'] // lang tags for each orthography handled
+window.detailsfileList = ['wo'] // first part of xx-details for each orthography
+window.scriptISO = 'gara'
+window.webfonts = ["Wolof Gara WF"]
+window.defaultSize = '24px'
+
