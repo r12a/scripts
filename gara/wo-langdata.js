9d32f5699c1aef284a78eac09c58ae92b81922aa
r12a <ishida@w3.org>
2023-02-27 06:55:30 +0000

    gara: Fix e in sample. Add missing characters to index. Create wo-langdata.

--- gara/wo-langdata.js
+++ gara/wo-langdata.js
@@ -3,16 +3,17 @@
 var langs = {
 
 "wo": { name:"Wolof", local:"Wolof", silcode:"wol", 
-source:"cldr_wo,udhr_wol", 
+source:"", 
 region:"afr", countries:"Senegal, Mauritania, The Gambia",
-script:"latn", speakers:"5500000", 
-letter:"ëñàéóËÑÀÉÓŋŊ", mark:"̈̃̀́", punctuation:"‰", 
-orth:`Latin. &nbsp; Primary usage.`,
+script:"tbd", speakers:"5500000", 
+letter:"\u{10D71}\u{10D72}\u{10D73}\u{10D74}\u{10D75}\u{10D76}\u{10D77}\u{10D78}\u{10D79}\u{10D7A}\u{10D7B}\u{10D7C}\u{10D7D}\u{10D7E}\u{10D7F}\u{10D80}\u{10D81}\u{10D82}\u{10D51}\u{10D52}\u{10D53}\u{10D54}\u{10D55}\u{10D56}\u{10D57}\u{10D58}\u{10D59}\u{10D5A}\u{10D5B}\u{10D5C}\u{10D5D}\u{10D5E}\u{10D5F}\u{10D60}\u{10D61}\u{10D62}\u{10D51}\u{10D52}\u{10D53}\u{10D54}\u{10D55}\u{10D56}\u{10D57}\u{10D58}\u{10D59}\u{10D5A}\u{10D5B}\u{10D5C}\u{10D5D}\u{10D5E}\u{10D5F}\u{10D60}\u{10D61}\u{10D62}\u{10D50}", mark:"𐵩𐵪𐵫𐵬𐵭", punctuation:"‘’“”𐵮", number:"𐵀𐵁𐵂𐵃𐵄𐵅𐵆𐵇𐵈𐵉", symbol:"𐵯", other:"\u{200C}\u{200D}\u{2067}\u{202B}\u{2066}\u{202A}\u{2068}\u{2069}\u{202C}\u{200F}\u{200E}\u{61C}\u{34F}", 
+orth:`Garay.`,
 also:
-['wo-arab | arab | Naskh style.  <a href="../scripts/latn/wo.html#variants" target="_blank">Details.</a>'],
-type:"alphabet", cs:"yes", gpos:"no", gsub:"no", cursive:"no", direction:"ltr", wordsep:"space", baseline:"mid", wrap:"word", hyphenation:"?",  justification:"?",
-vowels:"let:9",
-linked:"latn/wo.html", fonts:"/pickers/latn-wo/index.html", 
+['wo-latn | latn | Primary usage.  <a href="../scripts/latn/wo.html#variants" target="_blank">Details.</a>',
+'wo-arab | arab | Naskh style.'],
+type:"alphabet", cs:"yes", gpos:"yes", gsub:"no", cursive:"no", direction:"rtl", wordsep:"space", baseline:"mid", wrap:"word", hyphenation:"yes 𐵮",  justification:"?",
+vowels:"let:8",
+linked:"gara/wo.html", fonts:"/pickers/gara-wo/index.html", 
 },
 
 
