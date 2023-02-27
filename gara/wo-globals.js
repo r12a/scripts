5f3fc10908cec6e0f90807b03cfe4d2e5beb0b67
r12a <ishida@w3.org>
2023-02-27 07:08:25 +0000

    wo-globals: Created

--- gara/wo-globals.js
+++ gara/wo-globals.js
@@ -1,16 +1,16 @@
 
 langTag='wo' // BCP47 language tag for this orthography
-scriptTag='latn' // BCP47 script tag for this orthography
+scriptTag='gara' // BCP47 script tag for this orthography
 notesLangtag='wo'
 blockLangtag = 'wo' // same as langTag; used in block.html
-blockDirection=''  // ltr/rtl or '' for ltr; used by block.html
+blockDirection='rtl'  // ltr/rtl or '' for ltr; used by block.html
 
-blockDirectoryName='latn'  // folder name where block file is found, no slash
-orthogFilePath='latn/wo'  // path below /scripts, including filename, but no extension, eg. lana/nod
-pickerDir='latn-wo'  // directory name for picker, no slash at end
+blockDirectoryName='gara'  // folder name where block file is found, no slash
+orthogFilePath='gara/wo'  // path below /scripts, including filename, but no extension, eg. lana/nod
+pickerDir='gara-wo'  // directory name for picker, no slash at end
 scriptSummaryTableName='wo'  // id used for langs record in xx-langdata.js; used to make summary table
 charUsageBCP='wo'  // bcp tag to find the orthography in the charuse app; used in makeIndexIntro only
-orthogName = 'Latin' // natural language name for the orthography; used only in makeIndexIntro
+orthogName = 'Garay' // natural language name for the orthography; used only in makeIndexIntro
 languageName = 'Wolof' // natural language name of language; used in various places, esp block
 scriptIsIndic = false  // true if there are indic syllabic categories; used only in makeIndexIntro
 
