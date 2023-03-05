c962088b9ce4ff62cad6055a5ec1d48dd70b2235
r12a <ishida@w3.org>
2023-03-05 05:51:58 +0000

    arb block: Add ff-arab

---
+++ arab/ff-globals.js
@@ -0,0 +1,24 @@
+
+langTag='ff'
+scriptTag='arab'
+notesLangtag='ff'
+
+explainerDir=''
+blockDir='arabic/'
+pickerDir='arab-all/'
+scriptSummaryTableName=''
+charUsageBCP=''
+orthogName = 'Arabic'
+indic = false
+
+pageWebfonts = []
+defaultSize = '24px'
+
+
+
+// block settings
+
+languageName = 'Fulfulde'
+letterClassName = 'ff'
+lang = 'ff'
+direction = 'rtl'
