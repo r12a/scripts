ca5fb923811b9b4fc70d3bdb448f4623bc130623
r12a <ishida@w3.org>
2023-03-05 05:42:43 +0000

    el: Finish segmenting ipa

--- grek/el-examples.js
+++ grek/el-examples.js
@@ -24,166 +24,165 @@ autoExpandExamples.el = `
 ευφυΐα|intelligence|e§‹§f§–§ˈi§a
 νεράιδα|fairy|n§e§ˈɾ§a§i̯.§ð§a
 
-εγώ|I|eˈɣo|egó
-εσύ|you (singular)|eˈsi|esý
-εσείς|you (singular) (formal)|eˈsis|eseís
+εγώ|I|e§ˈɣ§o|egó
+εσύ|you (singular)|e§ˈs§i|esý
 αυτός|he|a§f§ˈt§o§s|aftós
-εμείς|we|eˈmis|emeís
-εσείς|you (plural)|eˈsis|eseís
-αυτοί|they|afˈti|aftoí
-εκείνος|that|eˈcino|ekeínos
-εδώ|here|eˈðo|edó
-εκεί|there|eˈci|ekeí
+εμείς|we|e§ˈm§›§i§s|emeís
+εσείς|you (plural)|e§ˈs§›§i§s|eseís
+αυτοί|they|a§f§ˈt§›§i|aftoí
+εκείνος|that|e§ˈ§c§›§i§n§o|ekeínos
+εδώ|here|e§ˈð§o|edó
+εκεί|there|e§ˈc§›§i|ekeí
 ποιος|who|pços|poios
-τι|what|ti|ti
-πού|where|pu|poú
-πότε|when|ˈpote|póte
-πώς|how|pos|pós
+τι|what|t§i|ti
+πού|where|p§›§u|poú
+πότε|when|ˈp§o§t§e|póte
+πώς|how|p§o§s|pós
 όλοι|all|ˈo§l§i§‹|óloi
-πολλοί|many|poˈli|polloí
-μερικοί|some|meriˈci|merikoí
+πολλοί|many|p§o§›§ˈl§›§i|polloí
+μερικοί|some|m§e§r§i§ˈc§›§i|merikoí
 λίγοι|few|ˈl§i§ʝ§i§‹|lígoi
 άλλοι|other|ˈa§l§‹§i§‹|álloi
 ένα|one|ˈe̞.§n§a|éna
 δύο|two|ˈð§i.§o|dýo
 τρία|three|ˈt§ɾ§i.§a|tría
 τέσσερα|four|ˈt§e̞.§s§‹§e̞.§ɾ§a|téssera
-πέντε|five|ˈpende|pénte
-μεγάλος|big|meˈɣalos|megálos
-μακρύς|long|maˈkris|makrýs
-πλατύς|wide|plaˈtis|platýs
-φαρδύς|wide|farˈðis|fardýs
-παχύς|thick|paˈçis|pachýs
-χοντρός|thick|xonˈdros|chontrós
-βαρύς|heavy|vaˈris|varýs
+πέντε|five|ˈp§e§n§d§e|pénte
+μεγάλος|big|m§e§ˈɣ§a§l§o§s|megálos
+μακρύς|long|m§a§ˈk§r§i§s|makrýs
+πλατύς|wide|p§l§a§ˈt§i§s|platýs
+φαρδύς|wide|f§a§r§ˈð§i§s|fardýs
+παχύς|thick|p§a§ˈç§i§s|pachýs
+χοντρός|thick|x§o§n§ˈd§r§o§s|chontrós
+βαρύς|heavy|v§a§ˈr§i§s|varýs
 μικρός|small|m§i§ˈk§r§o̞§s|mikrós
-κοντός|short|koˈndos|kontós
-στενός|narrow|steˈnos|stenós
-λεπτός|thin|leˈptos|leptós
-γυναίκα|woman|ʝiˈneka|gynaíka
-άνδρας|man (male)|ˈandras|ándras
+κοντός|short|k§o§ˈn§d§o§s|kontós
+στενός|narrow|s§t§e§ˈn§o§s|stenós
+λεπτός|thin|l§e§ˈp§t§o§s|leptós
+γυναίκα|woman|ʝ§i§ˈn§›§e§k§a|gynaíka
+άνδρας|man (male)|ˈa§n§d§r§a§s|ándras
 άνθρωπος|man (human)|ˈa§n̪§θ§r§o§p§o§s|ánthropos
 παιδί|child|p§e§‹§ˈð§i|paidí
-σύζυγος|wife|ˈsiziɣos|sýzygos
-γυναίκα|wife|ʝiˈneka|gynaíka
-σύζυγος|husband|ˈsiziɣos|sýzygos
-άντρας|husband|ˈandras|ántras
-μητέρα|mother|mitéra|miˈtera
-μάνα|mother|ˈmana|mána
-πατέρας|father|paˈteras|patéras
+σύζυγος|wife|ˈs§i§z§i§ɣ§o§s|sýzygos
+γυναίκα|wife|ʝ§i§ˈn§›§e§k§a|gynaíka
+σύζυγος|husband|ˈs§i§z§i§ɣ§o§s|sýzygos
+άντρας|husband|ˈa§n§d§r§a§s|ántras
+μητέρα|mother|m§i§t§é§r§a|miˈtera
+μάνα|mother|ˈm§a§n§a|mána
+πατέρας|father|p§a§ˈt§e§r§a§s|patéras
 ζώο|animal|ˈz§o.§o|zóo
 ψάρι|fish|ˈps§a.§ɾ§i|psári
-πουλί|bird|puˈli|poulí
-σκύλος|dog|ˈscilos|skýlos
+πουλί|bird|p§u§ˈl§i|poulí
+σκύλος|dog|ˈs§c§i§l§o§s|skýlos
 ψείρα|louse|ˈps§i.§‹§ɾ§a|pseíra
 φίδι|snake|ˈf§i§ð§i|fídi
-σκουλήκι|worm|skuˈlici|skoulíki
+σκουλήκι|worm|s§k§›§u§ˈl§i§c§i|skoulíki
 δέντρο|tree|ˈð§e§n.§d§r§o|déntro
-δάσος|forest|ˈðasos|dásos
-ραβδί|stick|raˈvði|ravdí
-βέργα|stick|ˈverɣa|vérga
-φρούτο|fruit|ˈfruto|froúto
-σπόρος|seed|ˈsporos|spóros
+δάσος|forest|ˈð§a§s§o§s|dásos
+ραβδί|stick|r§a§ˈv§ð§i|ravdí
+βέργα|stick|ˈv§e§r§ɣ§a|vérga
+φρούτο|fruit|ˈf§r§›§u§t§o|froúto
+σπόρος|seed|ˈs§p§o§r§o§s|spóros
 φύλλο|leaf|ˈf§i§l§‹§o|fýllo
 ρίζα|root|ˈɾ§i.§z§a|ríza
-φλοιός|bark (of a tree)|fliˈos|floiós
+φλοιός|bark (of a tree)|f§l§›§i§ˈo§s|floiós
 λουλούδι|flower|l§u§‹§ˈl§‹§u.§ð§i|louloúdi
-χορτάρι|grass|xorˈtari|chortári
-σχοινί|rope|sçiˈni|schoiní
-δέρμα|skin|ˈðerma|dérma
-κρέας|meat|ˈkreas|kréas
+χορτάρι|grass|x§o§r§ˈt§a§r§i|chortári
+σχοινί|rope|s§ç§›§i§ˈn§i|schoiní
+δέρμα|skin|ˈð§e§r§m§a|dérma
+κρέας|meat|ˈk§r§e§a§s|kréas
 αίμα|blood|ˈe.§‹§m§a|aíma
 κόκαλο|bone|ˈk§o§k§a§l§o|kókalo
-λίπος|fat (noun)|ˈlipos|lípos
+λίπος|fat (noun)|ˈl§i§p§o§s|lípos
 αυγό|egg|a§ˈv§ɣ§o|avgó
-κέρατο|horn|ˈcerato|kérato
-ουρά|tail|uˈra|ourá
+κέρατο|horn|ˈc§e§r§a§t§o|kérato
+ουρά|tail|›§u§ˈr§a|ourá
 πούπουλο|feather|ˈp§u.§‹§p§u.§‹§l§o|poúpoulo
-φτερό|feather|fteˈro|fteró
-τρίχα|hair|ˈtrixa|trícha 
-μαλλιά|hair|maˈʎa|malliá 
-κεφάλι|head|ceˈfali|kefáli
-αυτί|ear|afˈti|aftí
-μάτι|eye|ˈmati|máti
-μύτη|nose|ˈmiti|mýti
+φτερό|feather|f§t§e§ˈr§o|fteró
+τρίχα|hair|ˈt§r§i§x§a|trícha 
+μαλλιά|hair|m§a§›§ˈʎ§a|malliá 
+κεφάλι|head|c§e§ˈf§a§l§i|kefáli
+αυτί|ear|a§f§ˈt§i|aftí
+μάτι|eye|ˈm§a§t§i|máti
+μύτη|nose|ˈm§i§t§i|mýti
 στόμα|mouth|ˈs§t§o.§m§a|stóma
-δόντι|tooth|ˈðondi|dónti
+δόντι|tooth|ˈð§o§n§d§i|dónti
 γλώσσα|tongue|ˈɣ§l§o.§s§‹§a|glóssa
-νύχι|fingernail|ˈniçi|nýchi
+νύχι|fingernail|ˈn§i§ç§i|nýchi
 πόδι|leg, foot|ˈp§o.§ð§i|pódi
-γόνατο|knee|ˈɣonato|gónato
+γόνατο|knee|ˈɣ§o§n§a§t§o|gónato
 χέρι|hand|ˈç§e.§ɾ§i|chéri
-φτερό|wing|fteˈro|fteró
-κοιλιά|belly|ciˈʎa|koiliá
-σπλάχνα|guts|ˈsplaxna|spláchna
-εντόσθια|guts|eˈndosθia|entósthia
+φτερό|wing|f§t§e§ˈr§o|fteró
+κοιλιά|belly|c§›§i§ˈʎ§›§a|koiliá
+σπλάχνα|guts|ˈs§p§l§a§x§n§a|spláchna
+εντόσθια|guts|e§ˈn§d§o§s§θ§i§a|entósthia
 λαιμός|neck|l§e§‹§ˈm§o§s|laimós
-πλάτη|back|ˈplati|pláti
-στήθος|breast|ˈstiθos|stíthos
+πλάτη|back|ˈp§l§a§t§i|pláti
+στήθος|breast|ˈs§t§i§θ§o§s|stíthos
 καρδιά|heart|k§a§r§ˈð§ʝ§a|kardiá
-συκώτι|liver|siˈkoti|sykóti
-πίνω|to drink|ˈpino|píno
-τρώ[γ]ω|to eat|ˈtro[ɣ]o
+συκώτι|liver|s§i§ˈk§o§t§i|sykóti
+πίνω|to drink|ˈp§i§n§o|píno
+τρώ[γ]ω|to eat|ˈt§r§o§[ɣ]§o
 δαγκώνω|to bite|ð§a§ŋ§ˈɡ§o§n§o|dagkóno
-ρουφώ|to suck|ruˈfo|roufó
-φτύνω|to spit|ˈftino|ftýno
-ξερνώ|to vomit|kserˈno|xernó
-φυσάω|to blow|fiˈsao|fysáo
-αναπνέω|to breathe|anaˈpneo|anapnéo
-ανασαίνω|to breathe|anaˈseno|anasaíno
-γελάω|to laugh|ʝeˈlao|geláo
-βλέπω|to see|ˈvlepo|vlépo
-ακούω|to hear|aˈkuo|akoúo
-ξέρω|to know|ˈksero|xéro
-γνωρίζω|to know|ɣnoˈrizo|gnorízo
-σκέφτομαι|to think|ˈsceftome|skéftomai
-νομίζω|to think|ˈsceftome|nomízo
+ρουφώ|to suck|r§›§u§ˈf§o|roufó
+φτύνω|to spit|ˈf§t§i§n§o|ftýno
+ξερνώ|to vomit|ks§e§r§ˈn§o|xernó
+φυσάω|to blow|f§i§ˈs§a§o|fysáo
+αναπνέω|to breathe|a§n§a§ˈp§n§e§o|anapnéo
+ανασαίνω|to breathe|a§n§a§ˈs§›§e§n§o|anasaíno
+γελάω|to laugh|ʝ§e§ˈl§a§o|geláo
+βλέπω|to see|ˈv§l§e§p§o|vlépo
+ακούω|to hear|a§ˈk§›§u§o|akoúo
+ξέρω|to know|ˈks§e§r§o|xéro
+γνωρίζω|to know|ɣ§n§o§ˈr§i§z§o|gnorízo
+σκέφτομαι|to think|ˈs§c§e§f§t§o§m§›§e|skéftomai
+νομίζω|to think|n§o§ˈm§i§z§o|nomízo
 μυρίζω|to smell|m§i§ˈr§i§z§o|myrízo
-φοβάμαι|to fear|foˈvame|fovámai
-κοιμάμαι|to sleep|ciˈmame|koimámai
-ζω|to live|zo|zo
-πεθαίνω|to die|peˈθeno|pethaíno
+φοβάμαι|to fear|f§o§ˈv§a§m§›§e|fovámai
+κοιμάμαι|to sleep|c§›§iˈ§m§a§m§›§e|koimámai
+ζω|to live|z§o|zo
+πεθαίνω|to die|p§e§ˈθ§›§e§n§o|pethaíno
 σκοτώνω|to kill|s§k§o§ˈt§o.§n§o|skotóno
-μαλώνω|to fight|maˈlono|malóno
-κυνηγώ|to hunt|ciniˈɣo|kynigó
-χτυπώ|to hit|xtiˈpo|chtypó
-κόβω|to cut|ˈkovo|kóvo
-χωρίζω|to split|xoˈrizo|chorízo
-μαχαιρώνω|to stab|maçeˈrono|machairóno
-ξύνω|to scratch|ˈksino|xýno
-σκάβω|to dig|ˈskavo|skávo
+μαλώνω|to fight|m§a§ˈl§o§n§o|malóno
+κυνηγώ|to hunt|c§i§n§i§ˈɣ§o|kynigó
+χτυπώ|to hit|x§t§i§ˈp§o|chtypó
+κόβω|to cut|ˈk§o§v§o|kóvo
+χωρίζω|to split|x§o§ˈr§i§z§o|chorízo
+μαχαιρώνω|to stab|m§a§ç§›§e§ˈr§o§n§o|machairóno
+ξύνω|to scratch|ˈks§i§n§o|xýno
+σκάβω|to dig|ˈs§k§a§v§o|skávo
 κολυμπώ|to swim|k§o.§l§i§m§ˈb§o|kolympó
-πετάω|to fly|peˈtao|petáo
-περπατώ|to walk|perpaˈto|perpató
-έρχομαι|to come|ˈerxome|érchomai
-ξαπλώνω|to lie (as in a bed)|ksaˈplono|xaplóno
-πλαγιάζω|to lie (as in a bed)|plaˈʝazo|plagiázo
-κάθομαι|to sit|ˈkaθome|káthomai
-στέκομαι|to stand|ˈstekome|stékomai
-γυρίζω|to turn (intransitive)|ʝiˈrizo|gyrízo
-πέφτω|to fall|ˈpefto|péfto
-δίνω|to give|ˈðino|díno
-κρατάω|to hold|kraˈtao|kratáo
-ζουλάω|to squeeze|zuˈlao|zouláo
-τρίβω|to rub|ˈtrivo|trívo
-πλένω|to wash|ˈpleno|pléno
-σκουπίζω|to wipe|skuˈpizo|skoupízo
-τραβώ|to pull|traˈvo|travó
+πετάω|to fly|p§e§ˈt§a§o|petáo
+περπατώ|to walk|p§e§r§p§a§ˈt§o|perpató
+έρχομαι|to come|ˈe§r§x§o§m§e§‹|érchomai
+ξαπλώνω|to lie (as in a bed)|ks§a§ˈp§l§o§n§o|xaplóno
+πλαγιάζω|to lie (as in a bed)|p§l§a§ˈʝ§a§z§o|plagiázo
+κάθομαι|to sit|ˈk§a§θ§o§m§e§‹|káthomai
+στέκομαι|to stand|ˈs§t§e§k§o§m§e§‹|stékomai
+γυρίζω|to turn (intransitive)|ʝ§i§ˈr§i§z§o|gyrízo
+πέφτω|to fall|ˈp§e§f§t§o|péfto
+δίνω|to give|ˈð§i§n§o|díno
+κρατάω|to hold|k§r§a§ˈt§a§o|kratáo
+ζουλάω|to squeeze|z§u§‹§ˈl§a§o|zouláo
+τρίβω|to rub|ˈt§r§i§v§o|trívo
+πλένω|to wash|ˈp§l§e§n§o|pléno
+σκουπίζω|to wipe|s§k§u§‹§ˈp§i§z§o|skoupízo
+τραβώ|to pull|t§r§a§ˈv§o|travó
 σπρώχνω|to push|ˈz§b§ɾ§o.§x§n§o|spróchno
-ρίχνω|to throw|ˈrixno|ríchno
+ρίχνω|to throw|ˈr§i§x§n§o|ríchno
 πετώ|to throw|ˈrixno|petó
-δένω|to tie|ˈðeno|déno
-ράβω|to sew|ˈravo|rávo
-μετράω|to count|meˈtrao|metráo
-λέω|to say|ˈleo|léo
-τραγουδώ|to sing|traɣuˈðo|tragoudó
-παίζω|to play|ˈpezo|paízo
-πλέω|to float|ˈpleo|pléo
-επιπλέω|to float|epiˈpleo|epipléo
-τρέχω|to flow|ˈtrexo|trécho
-παγώνω|to freeze|paˈɣono|pagóno
-φουσκώνω|to swell|fuˈskono|fouskóno
-πρήζομαι|to swell|ˈprizome|prízomai
+δένω|to tie|ˈð§e§n§o|déno
+ράβω|to sew|ˈr§a§v§o|rávo
+μετράω|to count|m§e§ˈt§r§a§o|metráo
+λέω|to say|ˈl§e§o|léo
+τραγουδώ|to sing|t§r§a§ɣ§u§‹§ˈð§o|tragoudó
+παίζω|to play|ˈp§e§‹§z§o|paízo
+πλέω|to float|ˈp§l§e§o|pléo
+επιπλέω|to float|e§p§i§ˈp§l§e§o|epipléo
+τρέχω|to flow|ˈt§r§e§x§o|trécho
+παγώνω|to freeze|p§a§ˈɣ§o§n§o|pagóno
+φουσκώνω|to swell|f§u§‹§ˈs§k§o§n§o|fouskóno
+πρήζομαι|to swell|ˈp§r§i§z§o§m§e|prízomai
 ήλιος|sun|ˈi.§ʎ§‹§o§s§/ˈi.li.os|ílios
 φεγγάρι|moon|f§e§ŋ§ˈɡ§a.§ɾ§i|fengári
 Σελήνη|moon|s§e§ˈl§i§n§i|Selíni
@@ -216,79 +215,79 @@ autoExpandExamples.el = `
 κίτρινος|yellow|ˈc§i§t§r§i§n§o§s|kítrinos
 άσπρος|white|ˈa§s§p§r§o§s|áspros
 μαύρος|black|ˈm§a§v§r§o§s|mávros
-νύχτα|night|ˈnixta|nýchta
-μέρα|day|ˈmera|méra
+νύχτα|night|ˈn§i§x§t§a|nýchta
+μέρα|day|ˈm§e§r§a|méra
 χρόνος|year|ˈx§ɾ§o.§n§o§s|chrónos
-ζεστός|warm|zeˈstos|zestós
-κρύος|cold|ˈkrios|krýos
-γεμάτος|full|ʝeˈmatos|gemátos
-καινούργιος|new|ceˈnurʝos|kainoúrgios
-παλιός|old|paˈʎos|paliós
+ζεστός|warm|z§e§ˈs§t§o§s|zestós
+κρύος|cold|ˈk§r§i§o§s|krýos
+γεμάτος|full|ʝ§e§ˈm§a§t§o§s|gemátos
+καινούργιος|new|c§e§‹§ˈn§u§‹§r§ʝ§o§s|kainoúrgios
+παλιός|old|p§a§ˈʎ§o§s|paliós
 καλός|good|k§a§ˈl§o§s|kalós
-κακός|bad|kaˈkos|kakós
-σάπιος|rotten|ˈsapços|sápios
-βρόμικος|dirty|ˈvromikos|vrómikos
-ίσιος|straight|ˈisços|ísios
+κακός|bad|k§a§ˈk§o§s|kakós
+σάπιος|rotten|ˈs§a§p§ç§o§s|sápios
+βρόμικος|dirty|ˈv§r§o§m§i§k§o§s|vrómikos
+ίσιος|straight|ˈi§s§ç§o§s|ísios
 στρογγυλός|round|s§t§r§o§ɲ§ɟ§i§ˈl§o§s|strongylós
-κοφτερός|sharp (as a knife)|kofteˈros|kofterós
-στομωμένος|dull (as a knife)|stomoˈmenos|stomoménos
-λείος|smooth|ˈlios|leíos
-βρεγμένος|wet|vreɣˈmenos|vregménos
-υγρός|wet|iˈɣros|ygrós
-στεγνός|dry|steˈɣnos|stegnós
+κοφτερός|sharp (as a knife)|k§o§f§t§e§ˈr§o§s|kofterós
+στομωμένος|dull (as a knife)|s§t§o§m§o§ˈm§e§n§o§s|stomoménos
+λείος|smooth|ˈl§i§‹§o§s|leíos
+βρεγμένος|wet|v§r§e§ɣ§ˈm§e§n§o§s|vregménos
+υγρός|wet|i§ˈɣ§r§o§s|ygrós
+στεγνός|dry|s§t§e§ˈɣ§n§o§s|stegnós
 ξερός|dry|ks§e§ˈr§o§s|xerós
-σωστός|correct|soˈstos|sostós
-κοντά|near|koˈnda|kontá
-μακριά|far|makriˈa|makriá
-δεξιά|right|ðeksiˈa|dexiá
-αριστερά|left|aristeˈra|aristerá
-σε|at|se|se
-μέσα|in|ˈmesa se|mésa
-με|with|me|me
-και|and|ce|kai
-εάν|if|eˈan|eán
-αν|if|an|an
+σωστός|correct|s§o§ˈs§t§o§s|sostós
+κοντά|near|k§o§ˈn§d§a|kontá
+μακριά|far|m§a§k§r§i§ˈa|makriá
+δεξιά|right|ð§e§ks§i§ˈa|dexiá
+αριστερά|left|a§r§i§s§t§e§ˈr§a|aristerá
+σε|at|s§e|se
+μέσα|in|ˈm§e§s§a se|mésa
+με|with|m§e|me
+και|and|c§e§‹|kai
+εάν|if|e§ˈa§n|eán
+αν|if|a§n|an
 επειδή|because|e§p§i§‹§ˈð§i|epeidí
-γιατί|because|ʝaˈti|giatí
+γιατί|because|ʝ§a§‹§ˈt§i|giatí
 όνομα|name|ˈo.§n§o.§m§a|ónoma
 
 μια|a|m§ɲ§a|mia
-βια|violence|ˈvʝa
+βια|violence|ˈv§ʝ§a
 κοροϊδεύω|to mock|k§o.§ɾ§o§i̯ˈ§ð§e.§v§o|koroïdévo
 μποϊκοτάρω|I boycott|–§b§o.§i.§k§o§ˈt§a.§ɾ§o|boïkotáro
-αυλός|flute|aˈvlos
+αυλός|flute|a§ˈv§l§o§s
 Ταΰγετος|Mount Taygetus|t§aː.§ý.§ɡ§e.§t§o§s|Tāǘgetos
 μπορώ|to be able to|–§b§o§ˈɾ§o
-δεν πάω|I am not going|ðen 
+δεν πάω|I am not going|ð§e§n§ 
 μπύρα|beer|–§ˈb§i§r§a|býra
 φάση|phase|ˈf§a§s§i|||x
-βάση|base|ˈvasi
-μόνος|alone|ˈmonos
+βάση|base|ˈv§a§s§i
+μόνος|alone|ˈm§o§n§o§s
 νόμος|law|ˈn§o§m§o§s|
-τείνω|I tend|ˈtino
-ντύνω|I dress|ˈdino
-θέμα|topic|ˈθema
-δέμα|parcel|ˈðema
-σώα|safe|ˈsoa
-ζώα|animals|ˈzoa
-ρήμα|verb|ˈrima
-λίμα|nail file|ˈlima
-κόμμα|comma|ˈkoma
+τείνω|I tend|ˈt§i§‹§n§o
+ντύνω|I dress|ˈd§‹§i§n§o
+θέμα|topic|ˈθ§e§m§a
+δέμα|parcel|ˈð§e§m§a
+σώα|safe|ˈs§o§a
+ζώα|animals|ˈz§o§a
+ρήμα|verb|ˈr§i§m§a
+λίμα|nail file|ˈl§i§m§a
+κόμμα|comma|ˈk§o§m§‹§a
 χώμα|soil|ˈx§o.§m§a|chóma
 γόμα|eraser|ˈɣ§o§m§a|góma
 γκάμα|range|ˈɡ§‹§a§m§a|||x
-μπροστά|in the front|bɾoˈsta
+μπροστά|in the front|b§‹§ɾ§o§ˈs§t§a
 ντοκιμαντέρ|documentary
-νταντά|nanny|daˈda
+νταντά|nanny|d§‹§a§ˈ§‹§d§a
 ντους|shower|–§ˈd§u§‹§s
-φαίνομαι|look|ˈfenome
-λατομείο|quarry|latoˈmio
-εφηύρα|I invented|efˈivra
+φαίνομαι|look|ˈf§e§‹§n§o§m§e§‹
+λατομείο|quarry|l§a§t§o§ˈm§i§o
+εφηύρα|I invented|e§f§ˈi§v§r§a
 καϊμάν|caiman|k§a§i§ˈm§a§n|káiman
-Ναΐτες|Templars|ˈnaites
+Ναΐτες|Templars|ˈn§a§i§t§e§s
 ή|or|ˈi
-πώς;|how?|ˈpos
-πού;|where?|ˈpu
+πώς;|how?|ˈp§o§s§?
+πού;|where?|ˈp§u§‹§?
 αδελφοί|brothers|a§ð§e§lˈ§f§i§‹|adelfoí
 Φίλιππος Βʹ|Philip II (of Macedon)|
 
