e372bdb1b1f5ea5398d06a377500f6d4f9ed0b13
r12a <ishida@w3.org>
2023-03-06 09:22:23 +0000

    sunu: First draft

--- sunu/suz-examples.js
+++ sunu/suz-examples.js
@@ -1,6 +1,7 @@
 /*
 @ Source: https://en.wiktionary.org/wiki/Category:Sunwar_lemmas (2 items)
 @ Source: https://www.unicode.org/L2/L2021/21157-sunuwar.pdf
+@ Source: https://www.devkumarsunuwar.com.np/iwsadmin/imagine-filemanager/uploads/media/Second_Hamso_bhadra-August-September%202078.pdf
 
 native|meaning|IPA¹|transcription|notes|wiktionary²
 
@@ -16,22 +17,69 @@ use – for a CCS that doesn't produce sound
 autoExpandExamples.suz = `
 
 
-𑯇𑯃𑯜|woman|miʃ
-𑯓𑯖̃𑯆𑯅|water|vā̃ku
+𑯇𑯃𑯜|woman|m§i§ʃ
+𑯓𑯖̃𑯆𑯅|water|v§ãː§k§u
 
-𑯒𑯅𑯇|zero|sum
-𑯆𑯖|one|kaː
-𑯍𑯃𑯠𑯒𑯃|two|niʔsi
-𑯒𑯖̐|three|sã
-𑯐𑯂|four|lɛ
-𑯚𑯈|five|ŋə
-𑯄𑯈𑯆𑯅|six|rəku
-𑯌𑯈𑯍𑯃|seven|t͡ʃəni
-𑯒𑯈𑯒𑯃|eight|səsi
-𑯔𑯖̐|nine|yã
-𑯊𑯈𑯅|ten|gəu
-𑯆𑯑̃𑯃𑯌 𑯎𑯄𑯂𑯒𑯂|Sunuwar script|kõit͡ʃ brese
+𑯒𑯅𑯇|zero|s§u§m
+𑯆𑯖|one|k§aː
+𑯍𑯃𑯠𑯒𑯃|two|n§i§ʔ§s§i
+𑯒𑯖̐|three|s§ã
+𑯐𑯂|four|l§ɛ
+𑯚𑯈|five|ŋ§ə
+𑯄𑯈𑯆𑯅|six|r§ə§k§u
+𑯌𑯈𑯍𑯃|seven|t͡ʃ§ə§n§i
+𑯒𑯈𑯒𑯃|eight|s§ə§s§i
+𑯔𑯖̐|nine|y§ã
+𑯊𑯈𑯅|ten|g§ə§u
+𑯆𑯑̃𑯃𑯌 𑯎𑯄𑯂𑯒𑯂|Sunuwar script|k§õ§i§t͡ʃ§ §b§r§e§s§e
 
+𑯔𑯖̍𑯁|rice mill|j§a§t
+𑯒𑯂́𑯌𑯖|carding machine|s§e§t͡ʃ§aː
+𑯀𑯂𑯔𑯃|god|d§e§j§i
+𑯁𑯖𑯒𑯐𑯖|moon|t§a§s§l§a
+𑯂𑯆𑯑|thunderbolt|e§k§o
+𑯄𑯂𑯅|rain|r§e§u
+𑯅𑯁𑯘𑯃|top|u§t§tʰ§i
+𑯆𑯃𑯆𑯃|grandpa|k§i§k§i
+𑯇𑯖𑯇𑯖|mother|m§a§m§a
+𑯖𑯉𑯙𑯑|father|a§p§pʰ§o
+𑯉𑯃𑯉𑯃|emperor|p§i§p§i
+𑯊𑯖𑯒|cloud|g§aː§s
+𑯋𑯖𑯇𑯒𑯑|light|h§aː§m§s§o
+𑯌𑯖𑯄𑯇𑯃|young calf|t͡ʃ§aː§r§m§i
+𑯍𑯖|sun|n§aː
+𑯓𑯖𑯄𑯌𑯃|friend|w§a§r§t͡ʃ§i
+𑯖𑯐|infant|aː§l
+𑯗𑯑̃:𑯊𑯖|deep pit|ɖ§õː§g§aː
+𑯘𑯖𑯄𑯃|boarder|tʰ§aː§r§i
+𑯙𑯖𑯄|fur|pʰ§aː§r
+𑯛𑯐𑯖|axe|kʰlaː
+𑯜𑯂𑯄|rice|ʃ§e§r
+𑯜𑯖:𑯎|snake|ʃ§aː§b
+𑯞𑯂̃𑯞𑯅|tadpole|ʈ§ẽ§ʈ§u
+𑯟𑯂𑯐𑯂|fireball|ʈʰ§e§l§e
+𑯠𑯐𑯑𑯠𑯑|throat|ʔ§l§o§ʔ§o
+𑯖̃:|he|ãː§‹
+𑯒𑯖̃:|three|s§ãː§‹
+𑯜𑯖̃:|three|ʃ§ãː§‹
+𑯀𑯂|or|d§e
+𑯀𑯂̃:|snail|d§ẽː§‹
+𑯍𑯖̀|before|n§a
+𑯍𑯖́|sun|n§a
+𑯛𑯖:|decide|kʰ§aː
+𑯛𑯖̃:|guess|kʰ§ãː
+𑯊𑯂̍|nail|g§e
+𑯊𑯂̀|work|g§e
+𑯊𑯂́|side|g§e
+𑯊𑯂|you|g§e
+𑯍𑯂́|name|n§e
+𑯍𑯂|nose|n§e
+𑯍𑯂:|leech|n§eː§‹
+𑯍𑯂̀|what?; fame|n§e
+𑯁𑯂𑯇𑯌𑯖|to serve|t§e§m§c§a
+𑯁𑯂̍𑯇𑯌𑯖|to add|t§e§m§c§a
+𑯘𑯃̍𑯌𑯌𑯖|to meet|ˈtʰ§i§c§c§a
+𑯘𑯃𑯌𑯌𑯖̍|to fall down|tʰ§i§c§ˈc§a
 
 
 
