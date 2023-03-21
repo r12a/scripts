e67d38c864487971e7490b9711e969b225ce8af4
r12a <ishida@w3.org>
2023-03-21 09:55:40 +0000

    nagm/unr-examples, unr_vocab

---
+++ nagm/unr-examples.js
@@ -0,0 +1,49 @@
+/*
+@ Source: https://en.wiktionary.org/wiki/Category:Mundari_lemmas
+native|meaning|IPA¹|transcription|notes|wiktionary²
+
+[₁] separate grapheme clusters with §, separate phonetic syllables with .
+use ‹ and › for a sound that spans 2 CCSes
+use – for a CCS that doesn't produce sound
+
+[₂] x means that no link to Wiktionary should be added
+*/
+
+
+
+autoExpandExamples.unr = `
+
+𞓰|No terms available yet|
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+`
+
