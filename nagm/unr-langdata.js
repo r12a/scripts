fbd5603bb0e6640e29908a55531d54f0c3b05f49
r12a <ishida@w3.org>
2023-04-09 09:36:15 +0100

    nagm/unr-langdata.js

---
+++ nagm/unr-langdata.js
@@ -0,0 +1,19 @@
+// this should always be updated when app-charuse/langs.js is updated
+
+var langs = {
+
+"unr": { name:"Mundari", local:"", silcode:"unr", 
+source:"", 
+region:"sasia", countries:"Bangladesh",
+script:"nagm", speakers:"1100000", 
+letter:"𞓙𞓗𞓠𞓡𞓜𞓦𞓞𞓖𞓢𞓒𞓧𞓨𞓘𞓔𞓥𞓑𞓣𞓪𞓛𞓝𞓩𞓓𞓕𞓤𞓚𞓐𞓟𞓫", mark:"𞓮𞓯𞓬𞓭", punctuation:"–‘’“”…", number:"𞓰𞓱𞓲𞓳𞓴𞓵𞓶𞓷𞓸𞓹", 
+orth:`Nag Mundari (Mundari Bani).  <a href="../scripts/nagm/unr.html" target="_blank">Details.</a>`,
+also:
+['unr-deva | deva.'],
+type:"alphabet", cs:"no", gpos:"yes", gsub:"no", cursive:"no", direction:"ltr", wordsep:"space", baseline:"mid", wrap:"word", hyphenation:"?",  justification:"word",
+vowels:"l:5",
+linked:"nagm/unr.html", fonts:"/pickers/nagm-unr/index.html", 
+},
+
+
+}
