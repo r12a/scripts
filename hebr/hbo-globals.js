b05a7da84da709e0abaef4e346125b04d98e3851
r12a <ishida@w3.org>
2023-04-16 13:00:48 +0100

    + hbo-details for notes related to biblical text

---
+++ hebr/hbo-globals.js
@@ -0,0 +1,20 @@
+
+
+langTag='hbo' // BCP47 language tag for this orthography
+scriptTag='hebr' // BCP47 script tag for this orthography
+notesLangtag='hbo'
+blockLangtag = 'hbo' // same as langTag; used in block.html
+blockDirection='rtl'  // ltr/rtl or '' for ltr; used by block.html
+
+blockDirectoryName='hebr'  // folder name where block file is found, no slash
+orthogFilePath='hebr/hbo'  // path below /scripts, including filename, but no extension, eg. lana/nod
+pickerDir='hebr'  // directory name for picker, no slash at end
+scriptSummaryTableName='hbo'  // id used for langs record in xx-langdata.js; used to make summary table
+charUsageBCP='hbo'  // bcp tag to find the orthography in the charuse app; used in makeIndexIntro only
+orthogName = 'Hebrew' // natural language name for the orthography; used only in makeIndexIntro
+languageName = 'Biblical Hebrew' // natural language name of language; used in various places, esp block
+scriptIsIndic = true  // true if there are indic syllabic categories; used only in makeIndexIntro
+
+
+defaultSize = '24px'  // used by block.html to set size of font, including in font mgt panel
+letterClassName = 'hbo'  // classname for a given orthography in the block page
