8caa45e219d224385a15a969d395f3cdc16cb6d0
r12a <ishida@w3.org>
2023-05-04 22:02:44 +0100

    ethi/am-langdata: Synch with changes to character usage

--- ethi/am-langdata.js
+++ ethi/am-langdata.js
@@ -5,11 +5,11 @@ var langs = {
 "am": { name:"Amharic", local:"አማርኛ [amarɨɲːa]", silcode:"amh", 
 source:"cldr_am,udhr_amh", 
 region:"afr", countries:"Ethiopia", script:"ethi", speakers:"21634396", 
-letter:"ሀሁሂሃሄህሆለሉሊላሌልሎሏሐሑሒሓሔሕሖሗመሙሚማሜምሞሟሠሡሢሣሤሥሦሧረሩሪራሬርሮሯሰሱሲሳሴስሶሷሸሹሺሻሼሽሾሿቀቁቂቃቄቅቆቈቊቋቌቍበቡቢባቤብቦቧቨቩቪቫቬቭቮቯተቱቲታቴትቶቷቸቹቺቻቼችቾቿኀኁኂኃኄኅኆኈኊኋኌኍነኑኒናኔንኖኗኘኙኚኛኜኝኞኟአኡኢኣኤእኦኧከኩኪካኬክኮኰኲኳኴኵኸኹኺኻኼኽኾወዉዊዋዌውዎዐዑዒዓዔዕዖዘዙዚዛዜዝዞዟዠዡዢዣዤዥዦዧየዩዪያዬይዮደዱዲዳዴድዶዷጀጁጂጃጄጅጆጇገጉጊጋጌግጎጐጒጓጔጕጠጡጢጣጤጥጦጧጨጩጪጫጬጭጮጯጰጱጲጳጴጵጶጷጸጹጺጻጼጽጾጿፀፁፂፃፄፅፆፈፉፊፋፌፍፎፏፐፑፒፓፔፕፖፗ", number:"፩፪፫፬፭፮፯፰፱፲፳፴", punctuation:"፡፣፤፥፦።‐–‹›«»", 
+letter:"ሀሁሂሃሄህሆለሉሊላሌልሎሏሐሑሒሓሔሕሖሗመሙሚማሜምሞሟሠሡሢሣሤሥሦሧረሩሪራሬርሮሯሰሱሲሳሴስሶሷሸሹሺሻሼሽሾሿቀቁቂቃቄቅቆቈቊቋቌቍበቡቢባቤብቦቧቨቩቪቫቬቭቮቯተቱቲታቴትቶቷቸቹቺቻቼችቾቿኀኁኂኃኄኅኆኈኊኋኌኍነኑኒናኔንኖኗኘኙኚኛኜኝኞኟአኡኢኣኤእኦኧከኩኪካኬክኮኰኲኳኴኵኸኹኺኻኼኽኾወዉዊዋዌውዎዐዑዒዓዔዕዖዘዙዚዛዜዝዞዟዠዡዢዣዤዥዦዧየዩዪያዬይዮደዱዲዳዴድዶዷጀጁጂጃጄጅጆጇገጉጊጋጌግጎጐጒጓጔጕጠጡጢጣጤጥጦጧጨጩጪጫጬጭጮጯጰጱጲጳጴጵጶጷጸጹጺጻጼጽጾጿፀፁፂፃፄፅፆፈፉፊፋፌፍፎፏፐፑፒፓፔፕፖፗ", mark:"፞፟፝", number:"፩፪፫፬፭፮፯፰፱፲፳፴፵፶፷፸፹፺፻፼", punctuation:"¡«»፠፡።፣፤፥፦፧፨–‘’“”‹›", 
 orth:`Ethiopic. &nbsp; <a href="../scripts/ethiopic" target="_blank">Details</a>.`,
-type:"syllabary", cs:"no", gpos:"no", gsub:"no", cursive:"no", direction:"ltr", justification:"sp", wordsep:"space/wb", wordsepChar:"፡", baseline:"mid", wrap:"word/char", hyphenation:"?", 
-vowels:"syl:y",
-linked:"ethiopic", fonts:"/pickers/ethi/", 
+type:"syllabary", cs:"no", gpos:"no", gsub:"no", cursive:"no", direction:"ltr", justification:"sp", wordsep:"space/wb", wordsepChar:"፡", baseline:"romn", wrap:"word/char", hyphenation:"?", 
+vowels:"syl:15",
+linked:"ethi/am", fonts:"/pickers/ethi/", 
 },
 
 
