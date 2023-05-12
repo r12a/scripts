e71d0b55bb408b3ad167d8165f24287a6c9d3bbd
r12a <ishida@w3.org>
2023-05-12 17:02:18 +0100

    dv:  Update to match orthography chart

--- thaa/dv-langdata.js
+++ thaa/dv-langdata.js
@@ -5,13 +5,13 @@ var langs = {
 "dv": { name:"Dhivehi (Maldivian, Divehi)", local:"ދިވެހި [d̪iˈʋehi], ދިވެހިބަސް (dhivehi-bas)", silcode:"div", rtl:true, 
 source:"udhr_div,e9188c572c9c158255cede2459f076ecb3b50b9f", 
 region:"sasia", countries:"Maldives", script:"thaa", speakers:"340000", 
-letter:"ޑސމބރގއދޖލހޢނފކށވޙޤތޕޓޔޝޞޅޚޣޒޠޗޏޘޛޟޜޡޥޱ", mark:"ިެްަީުާޮޭޫޯ", punctuation:"،؛", 
+letter:"ހށނރބޅކއވމފދތލގޏސޑޒޓޔޕޖޗޘޙޚޛޜޝޞޟޠޡޢޣޤޥ", mark:"ަާިީުޫެޭޮޯް", number:"", punctuation:"،؛", symbol:"", other:"", 
 orth:`[Thaana] &nbsp;  <a href="../scripts/thaana/" target="_blank">Details</a>.`,
 also:
 ["dv-latn | latn | ‘Maldivian Latin’."],
-type:"alphabet", cs:"no", gpos:"no", gsub:"no", cursive:"no", direction:"rtl", wordsep:"space", baseline:"mid", wrap:"word", hyphenation:"?",  justification:"sp",
+type:"alphabet", cs:"no", gpos:"no", gsub:"no", cursive:"no", direction:"rtl", wordsep:"space", baseline:"romn", wrap:"word", hyphenation:"?",  justification:"sp",
 vowels:"cm:10 base:އ/A", clusters:"mrk",
-linked:"thaana", fonts:"/pickers/thaa/", 
+linked:"thaa/dv", fonts:"/pickers/thaa/", 
 },
 
 
