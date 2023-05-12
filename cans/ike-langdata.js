e8e497a4e648d4e03f2666dddded04fb489b3697
r12a <ishida@w3.org>
2023-05-12 17:26:35 +0100

    ike:  Update to match orthography chart

--- cans/ike-langdata.js
+++ cans/ike-langdata.js
@@ -5,13 +5,13 @@ var langs = {
 "ike": { name:"Eastern Canadian Inuktitut", local:"ᐃᓄᒃᑎᑐᑦ‎ (Inuktitut)", 
 source:"3cc72e78bdfa007c9612513a501835f81aa20ff2,udhr_ike", 
 region:"nam", countries:"Canada", script:"cans", speakers:"34000", 
-letter:"ᐁᐃᐄᐅᐆᐊᐋᐯᐱᐲᐳᐴᐸᐹᑉᑌᑎᑏᑐᑑᑕᑖᑦᑫᑭᑮᑯᑰᑲᑳᒃᒉᒋᒌᒍᒎᒐᒑᒡᒣᒥᒦᒧᒨᒪᒫᒻᓀᓂᓃᓄᓅᓇᓈᓐᓓᓕᓖᓗᓘᓚᓛᓪᓭᓯᓰᓱᓲᓴᓵᔅᔦᔨᔩᔪᔫᔭᔮᔾᕃᕆᕇᕈᕉᕋᕌᕐᕓᕕᕖᕗᕘᕙᕚᕝᕼᕿᖀᖁᖂᖃᖄᖅᖏᖐᖑᖒᖓᖔᖕᖠᖡᖢᖣᖤᖥᖦᖯᙯᙰᙱᙲᙳᙴᙵᙶ", aux:"ᕵᕶᕷᕸᕹᕺᕴᕻ", 
+letter:"ᐁᐃᐄᐅᐆᐊᐋᐯᐱᐲᐳᐴᐸᐹᑉᑌᑎᑏᑐᑑᑕᑖᑦᑫᑭᑮᑯᑰᑲᑳᒃᒉᒋᒌᒍᒎᒐᒑᒡᒣᒥᒦᒧᒨᒪᒫᒻᓀᓂᓃᓄᓅᓇᓈᓐᓓᓕᓖᓗᓘᓚᓛᓪᓭᓯᓰᓱᓲᓴᓵᔅᔦᔨᔩᔪᔫᔭᔮᔾᕃᕆᕇᕈᕉᕋᕌᕐᕓᕕᕖᕗᕘᕙᕚᕝᕼᕿᖀᖁᖂᖃᖄᖅᖏᖐᖑᖒᖓᖔᖕᖠᖡᖢᖣᖤᖥᖦᖯᙯᙰᙱᙲᙳᙴᙵᙶ", letteraux:"ᕴᕵᕶᕷᕸᕹᕺᕻ", mark:"", number:"", punctuation:"‘’“”", symbol:"", other:"", 
 orth:`[UCAS] &nbsp; Standardised for Inuktitut in 1976, however application varies across dialects. <a href="../scripts/cans/" target="_blank">Details</a>`,
 also:
 ["ike-latn | latn | Also standardised for Inuktitut in 1976."],
-type:"syllabary", cs:"no", gpos:"no", gsub:"no", cursive:"no", direction:"ltr", wordsep:"space", baseline:"mid", wrap:"word", hyphenation:"yes", hyphen:"-",  justification:"sp",
+type:"syllabary", cs:"no", gpos:"no", gsub:"no", cursive:"no", direction:"ltr", wordsep:"space", baseline:"romn", wrap:"word", hyphenation:"yes", hyphen:"-",  justification:"sp",
 vowels:"syl:7", clusters:"ss", finals:"ss:y", 
-linked:"cans", fonts:"/pickers/cans-ike/", 
+linked:"cans/ike", fonts:"/pickers/cans-ike/", 
 },
 
 
