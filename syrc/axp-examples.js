6be21cd025eb0afb9f387e99d42fdd152395046c
r12a <ishida@w3.org>
2023-05-13 11:45:16 +0100

    NEW syrc/axp-examples; syrc/axp_vocab

---
+++ syrc/axp-examples.js
@@ -0,0 +1,68 @@
+/*
+@ Source: Proposal for a New Unicode Script: Christian Palestinian Aramaic (CPA)
+
+native|meaning|IPA¹|transcription|notes|wiktionary²
+
+[₁] separate CCS with §, separate phonetic syllables with .
+use ‹ and › for a sound that spans 2 CCSes
+use – for a CCS that doesn't produce sound
+
+[₂] x means that no link to Wiktionary should be added
+*/
+
+autoExpandExamples.axp = `
+
+
+ܘܗܘܐ||
+ܒܩܠܐ||
+ܓܘܝܬ݂ܐ||
+ܓܘ[ܝ]ܬܐ||
+ܥ[ܘܒ]ܖܐ ܖܝ||
+ܥ[ܘܒ]ܖܐ ܕܝ||
+ܗ[ܖܢ ܃] ܗܘܐ||
+ܘܡܠܐ ܗܢܝܐ||
+ܙ[ܥܘ]ܪܐ||
+ܚܓ̈ܠܘ||
+ܛܘܒܢܐ||
+ܥ[ܘܒ]ܖܐ ܖܝ||
+ܥܘ̈ܠܘ ܒܟܝܪ||
+ܝܬܟ||
+ܠܡܣܟܝ̈ܢܝܐ||
+ܘܬܘܒܢ||
+ܠܡܣܟܝ̈ܢܝܐ||
+ܠܡܫܡܘܥ||
+ܝ]ܨܦܐ||
+ܧܣܝܐܬܝܢܗ||
+[ܡܬܪܚ]ܩ ܖܝ||
+ܒܣܪܬ||
+ܒܚܡܝ̈ܫܝܢ||
+ܝܬܗܘܢ ܃܁||
+ܘܫܘܐ̇||
+ܛܘܒܢܐ܁||
+ܠܘܬܗ ܃||
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+`
+
