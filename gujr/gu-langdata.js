a8cf19a3aa5d97f4a7053308145f073a78921cb9
r12a <ishida@w3.org>
2023-05-14 19:11:58 +0100

    gu:  Update to match orthography chart

--- gujr/gu-langdata.js
+++ gujr/gu-langdata.js
@@ -5,14 +5,14 @@ var langs = {
 "gu": { name:"Gujarati", local:"ગુજરાત‎ (Gujarātī)", silcode:"guj", 
 source:"cldr_gu,udhr_guj,cd18536eac759a61da40080d717664d64a29b397", 
 region:"sasia", countries:"India, Tanzania, Kenya, Bahrain, Zambia, Pakistan", script:"gujr", speakers:"49000000", 
-letter:"ૐઅઆઇઈઉઊઋૠઍએઐઑઓઔકખગઘઙચછજઝઞટઠડઢણતથદધનપફબભમયરલવશષસહળઽ", mark:"઼ંઁઃાિીુૂૃૄૅેૈૉોૌ્", number:"૧૨૩૪૫૬૭૮૯૦", punctuation:"§‐–—…‘’“”†‡′″", symbol:"૱", other:"‍‌", aux:"૰।॥", 
+letter:"અઆઇઈઉઊઋઍએઐઑઓઔકખગઘઙચછજઝઞટઠડઢણતથદધનપફબભમયરલળવશષસહૐૠ", mark:"ંઃ઼ાિીુૂૃૄૅેૈૉોૌ્", number:"૦૧૨૩૪૫૬૭૮૯", punctuation:"।॥૰‘’“”", symbol:"૱", other:"\u200C\u200D", 
 orth:`[Gujurati] &nbsp; Primary usage. 
 <a href="../scripts/gujarati" target="_blank">Detailed summary</a>.`,
 also:
 ["gu-khoj | khoj | "],
-type:"abugida", cs:"no", gpos:"yes", gsub:"yes", cursive:"no", direction:"ltr", wordsep:"space", baseline:"high", wrap:"word", hyphenation:"?",  justification:"sp",
-vowels:"inh:1 ind:12 vs:11 pre:1 voc:2", clusters:"inv stk cnj lig tcg rax", finals:"cm:2", 
-linked:"gujarati", fonts:"/pickers/gujr/", 
+type:"abugida", cs:"no", gpos:"yes", gsub:"yes", cursive:"no", direction:"ltr", wordsep:"space", baseline:"hang", wrap:"word", hyphenation:"?",  justification:"sp",
+vowels:"inh:1 ind:12 vs:11 pre:1 voc:2", clusters:"vir stk cnj lig tcg rax", finals:"cm:2", 
+linked:"gujr/gu", fonts:"/pickers/gujr/", 
 },
 
 
