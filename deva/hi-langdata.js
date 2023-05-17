1728378da5c604e8e549016ca8348442a8120d41
r12a <ishida@w3.org>
2023-05-17 10:26:23 +0100

    hi:  Update to match orthography chart

--- deva/hi-langdata.js
+++ deva/hi-langdata.js
@@ -3,16 +3,16 @@
 var langs = {
 
 "hi": { name:"Hindi", local:" मानक हिन्दी‎ (Mānak Hindī), हिन्दी‎ (Hindī)", silcode:"hin", 
-source:"cldr_hi,udhr_hin,9d8e188be96400153901b55006feaaf9fd4bf3b1", 
+source:"21f8184138e858d28a8478313d2b5b59a1f650f6,cldr_hi,udhr_hin", 
 region:"sasia", countries:"India, Nepal, Singapore, South Africa", script:"deva", speakers:"260000000", 
-letter:"अआइईउऊऋएऐओऔकखगघङचछजझञटठडढणतथदधनपफबभमयरलवशषसहक़फ़ज़ख़ग़ड़ढ़", letteraux:"ऍऑऽॐ", mark:"ँंः़ािीुूृेैोौ्", markaux:"ॅॉ", number:"०१२३४५६७८९", punctuation:"।॥॰‘’“”—", symbol:"₹", other:"‌‍", 
+letter:"अआइईउऊऋऍएऐऑओऔकखगघङचछजझञटठडढणतथदधनपफबभमयरलवशषसहऽॐ", letteraux:"क़ख़ग़ज़ड़ढ़फ़", mark:"ँंः़ािीुूृॅेैॉोौ्", number:"०१२३४५६७८९", punctuation:"।॥॰‘’“”", symbol:"₹", other:"\u200C\u200D", 
 orth:`[Devanagari] &nbsp;  
 <a href="../scripts/devanagari" target="_blank">Detailed summary</a>.`,
 also:
 ["hi-newa | newa | Archaic, no longer in use."],
-type:"abugida", cs:"no", gpos:"yes", gsub:"yes", cursive:"no", wordsep:"space", baseline:"high", wrap:"word", hyphenation:"(yes)", hyphen:"-",  direction:"ltr",  justification:"sp", 
-vowels:"inh:1 vs:11 ind:12 pre:1 voc:1",  clusters:"inv stk cnj lig rax",  finals:"cm:2 let:y", 
-linked:"devanagari", fonts:"/pickers/deva-hi/", 
+type:"abugida", cs:"no", gpos:"yes", gsub:"yes", cursive:"no", wordsep:"space", baseline:"hang", wrap:"word", hyphenation:"(yes)", hyphen:"-",  direction:"ltr",  justification:"sp", 
+vowels:"inh:1 vs:11 ind:12 pre:1 voc:1",  clusters:"vir stk cnj lig rax",  finals:"cm:2", 
+linked:"deva/hi", fonts:"/pickers/deva-hi/", 
 },
 
 
