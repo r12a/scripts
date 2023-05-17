24b43bd0b985b074332a164c71a1b05bd3baafad
r12a <ishida@w3.org>
2023-05-17 19:15:10 +0100

    ks-deva:  Update to match orthography chart

--- deva/ks-langdata.js
+++ deva/ks-langdata.js
@@ -5,12 +5,12 @@ var langs = {
 "ks-deva": { name:"Kashmiri (Koshur)", local:"कॉशुर‎ (Koshur)", silcode:"kas", 
 source:"7f2afe74cc91aa1c3986c9017fac92907f6cd260", 
 region:"sasia", countries:"India, Pakistan", script:"deva", speakers:"5600000", 
-letter:"पफबतथदटठडकखगचछजवसशहमनरलयइईॶॷउऊऎएॳॴऒओॵअआऐऔऋ", mark:"िीॖॗुूॆेऺऻॊोॏाैौृं़्ँ", punctuation:"।", aux:"ज़णञङभधढझघषकज", 
+letter:"अआइईउऊऋऎएऐऒओऔकखगचछजटठडतथदनपफबमयरलवशसहज़ॳॴॵॶॷ", letteraux:"घङझञढणधभष", mark:"ँंऺऻ़ािीुूृॆेैॊोौ्ॏॖॗ", number:"", punctuation:"।॥", symbol:"", other:"\u200C\u200D", 
 orth:`Devanagari. &nbsp; Associated with the Hindu Kashmiris. There were orthographic reforms in 1995, 2002, and 2009. This selection of characters reflects the latter. <a href="../scripts/devanagari/kashmiri" target="_blank">Details</a>.`,
 notes:`Need clarity about use or not of Devanagari numbers.`,
-type:"abugida", cs:"no", gpos:"yes", gsub:"yes", cursive:"no", direction:"ltr", wordsep:"space", baseline:"high", wrap:"word", hyphenation:"?",  justification:"sp",
-vowels:"inh:1 ind:17 vs:16 pre:1", clusters:"vir inv stk cnj lig rax", finals:"cm:1", 
-linked:"devanagari/kashmiri", fonts:"/pickers/deva-ks/", 
+type:"abugida", cs:"no", gpos:"yes", gsub:"yes", cursive:"no", direction:"ltr", wordsep:"space", baseline:"hang", wrap:"word", hyphenation:"?",  justification:"sp",
+vowels:"inh:1 ind:17 vs:16 pre:1", clusters:"vir stk cnj lig rax", finals:"cm:1", 
+linked:"deva/ks", fonts:"/pickers/deva-ks/", 
 },
 
 
