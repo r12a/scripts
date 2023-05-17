cf8d8cc1ffdf962b9c1cb13e3064436e0331482f
r12a <ishida@w3.org>
2023-05-17 10:28:27 +0100

    he:  Update to match orthography chart

--- hebr/he-langdata.js
+++ hebr/he-langdata.js
@@ -5,11 +5,11 @@ var langs = {
 "he": { name:"Hebrew", local:" עברית‎ (Ivrit), עברית חדשה‎ (ivrít ḥadašá[h])", silcode:"heb", rtl:true, 
 source:"0a5ae89a710c3b0c9e880e33bdaaf863175ea62a,cldr_he,udhr_heb", 
 region:"wasia", countries:"Israel, etc.", script:"hebr", speakers:"9000000", 
-letter:"אבגדהוזחטיכךלמםנןסעפףצץקרשת", punctuation:"׳״־’”‐–—", punctuationaux:"„", symbol:"₪", other:"\u200F\u200E\u2067\u2066\u2069\u2068\u202B\u202A\u202C", markaux:"ְֱֲֳִֵֶַָֹֻּׁׂ", 
+letter:"אבגדהוזחטיךכלםמןנסעףפץצקרשת", mark:"ְֱֲֳִֵֶַָׇֹֺֻּׁׂ", number:"", punctuation:"־׳״’”„", symbol:"₪", other:"\u200E\u200F\u202A\u202B\u202C\u2066\u2067\u2068\u2069", 
 orth:`Hebrew. &nbsp;  <a href="../scripts/hebrew" target="_blank">Details</a>.`,
-type:"abjad", cs:"no", gpos:"yes", gsub:"no", cursive:"no", direction:"rtl", wordsep:"space", baseline:"mid", wrap:"word", hyphenation:"yes", hyphen:"-",  justification:"sp",
-vowels:"ml:4 hcm:11",
-linked:"hebrew", fonts:"/pickers/hebr/", 
+type:"abjad", cs:"no", gpos:"yes", gsub:"no", cursive:"no", direction:"rtl", wordsep:"space", baseline:"romn", wrap:"word", hyphenation:"yes", hyphen:"-",  justification:"sp",
+vowels:"ml:4 cm:11 hcm:11 other:4",
+linked:"hebr/he", fonts:"/pickers/hebr/", 
 },
 
 
