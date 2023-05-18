1aa06ad342efa81d1b046bfc9fde01d56d7910ef
r12a <ishida@w3.org>
2023-05-18 19:52:32 +0100

    lep:  Update to match orthography chart

--- lepc/lep-langdata.js
+++ lepc/lep-langdata.js
@@ -2,17 +2,16 @@
 
 var langs = {
 
-"lep": { name:"Lepcha", local:"", silcode:"lep", 
-source:"", 
-region:"sasia", countries:"Nepal, Sikkim",
-script:"lepc", speakers:"400000", 
-letter:"ᰀᰁᰂᰃᰄᰅᰆᰇᰈᰉᰊᰋᰌᰍᰎᰏᰐᰑᰒᰓᰔᰕᰖᰗᰘᰙᰚᰛᰜᰝᰞᰟᰠᰡᰢᰣᱍᱎᱏ", mark:"ᰤᰥᰭᰮᰯᰰᰱᰲᰳᰴᰵᰦᰧᰨᰩᰪᰫᰬᰶ᰷", punctuation:"᰻᰼᰽᰾᰿‘’“”", number:"᱀᱁᱂᱃᱄᱅᱆᱇᱈᱉", symbol:"᥀", 
-orth:`Lepcha.  <a href="../scripts/lepc/lep.html" target="_blank">Details.</a>`,
+"lep": { name:"Lepcha", local:"ᰛᰩᰵᰛᰧᰵᰶ", 
+source:"b1367290d9c75505221c288a0382c33f2b3070b9", 
+region:"sasia", countries:"India", script:"lepc", speakers:"53000", 
+letter:"ᰀᰁᰂᰃᰄᰅᰆᰇᰈᰉᰊᰋᰌᰍᰎᰏᰐᰑᰒᰓᰔᰕᰖᰗᰘᰙᰚᰛᰜᰝᰞᰟᰠᰡᰢᰣᱍᱎᱏ", mark:"ᰤᰥᰦᰧᰨᰩᰪᰫᰬᰭᰮᰯᰰᰱᰲᰳᰴᰵᰶ᰷", number:"᱀᱁᱂᱃᱄᱅᱆᱇᱈᱉", punctuation:"᰻᰼᰽᰾‘’“”", punctuationaux:"᰿", symbol:"", other:"", 
+orth:`[Lepcha] <a href='../scripts/lepc/lep.html' target='_blank'>Details</a>.`,
 also:
 ['lep-deva | deva.'],
-type:"abugida", cs:"no", gpos:"yes", gsub:"yes", cursive:"no", direction:"ltr", wordsep:"space", baseline:"mid", wrap:"word", hyphenation:"?",  justification:"word",
-vowels:"vs:7",
-linked:"lepc/lep.html", fonts:"/pickers/lepc-lep/index.html", 
+type:"abugida", cs:"no", gpos:"yes", gsub:"yes", cursive:"no", wordsep:"space", baseline:"romn", wrap:"word", hyphenation:"?",  direction:"ltr",  justification:"sp", 
+vowels:"inh:1 cm:8 vs:7 base:ᰣ/A pre:3",  medials:"cm:2 let:7",  finals:"cm:9", 
+linked:"lepc/lep", fonts:"lepc-lep", 
 },
 
 
