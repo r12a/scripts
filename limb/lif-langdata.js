68e21d7d8f8175ccd6efbf00d9ef74ea6bf39232
r12a <ishida@w3.org>
2023-05-18 22:07:15 +0100

    lif:  Update to match orthography chart

--- limb/lif-langdata.js
+++ limb/lif-langdata.js
@@ -6,13 +6,13 @@ var langs = {
 source:"", 
 region:"sasia", countries:"Nepal, Sikkim",
 script:"limb", speakers:"400000", 
-letter:"ᤝᤞᤒᤓᤆᤇᤍᤎᤑᤃᤄᤜᤈᤁᤂᤗᤔᤏᤅᤐᤖᤛᤙᤋᤌᤘᤕᤉᤊᤚᤀ", mark:"ᤤᤦᤰᤸᤶᤵᤷᤳᤫᤪᤣᤥ᤹ᤲᤠᤡᤢ᤻᤺", punctuation:"॥᥅᥄‘’“”", number:"᥇᥈᥉᥊᥋᥌᥍᥎᥏᥆", symbol:"᥀", 
+letter:"ᤀᤁᤂᤃᤄᤅᤆᤇᤈᤋᤌᤍᤎᤏᤐᤑᤒᤓᤔᤕᤖᤗᤘᤙᤛᤜ", mark:"ᤠᤡᤢᤣᤤᤥᤦᤧᤨᤩᤪᤫᤰᤱᤳᤴᤵᤶᤷᤸ᤻᤹᤺", number:"᥆᥇᥈᥉᥊᥋᥌᥍᥎᥏", punctuation:"॥᥄᥅“”", symbol:"", other:"", 
 orth:`Limbu.  <a href="../scripts/limb/lif.html#variants" target="_blank">Details.</a>`,
 also:
 ['lif-deva | deva | Primary usage.'],
 type:"abugida", cs:"no", gpos:"yes", gsub:"no", cursive:"no", direction:"ltr", wordsep:"space", baseline:"mid", wrap:"word", hyphenation:"yes",  justification:"word",
-vowels:"vs:9",
-linked:"limb/lif.html", fonts:"/pickers/limb-lif/index.html", 
+vowels:"inh:1 cm:9 vs:9 circ:2 base:ᤀ/ɔ",
+linked:"limb/lif", fonts:"/pickers/limb-lif", 
 },
 
 
