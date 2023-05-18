48a3ca90099a6c8917de6b0f3bb4a896c4fc2a74
r12a <ishida@w3.org>
2023-05-18 07:14:03 +0100

    kyu:  Update to match orthography chart

--- khmr/km-langdata.js
+++ khmr/km-langdata.js
@@ -5,11 +5,11 @@ var langs = {
 "km": { name:"Khmer (Cambodian)", local:"ខ្មែរ‎ (Khmer)", silcode:"khm", 
 source:"cldr_km,udhr_khm,7f422cd6f0781ddbcb488b785f4f0d3a5883c93b", 
 region:"seasia", countries:"Cambodia, Vietnam", script:"khmr", speakers:"16000000", 
-letter:"ឥឦឪឧឩឯឰឱឳឲឫឬឭឮកខគឃងចឆជឈញដឋឌឍណតថទធនបផពភមយរឡលវសហអៗ", mark:"ៈាិីឹឺុូួើឿៀេែៃោៅំះ៉៊់៍័្", number:"០១២៣៤៥៦៧៨៩", punctuation:"៖។៕៙៚‘’“”", symbol:"៛", markaux:"៑៌៝៎៏", 
+letter:"កខគឃងចឆជឈញដឋណតថទធនបផពភមយរលវសហឡអឥឦឧឩឪឫឭឯឰឱឲឳៗ", letteraux:"ឌឍឬឮ", mark:"ាិីឹឺុូួើឿៀេែៃោៅំះៈ៉៊់៌៍៎៏័៑្៝", number:"០១២៣៤៥៦៧៨៩", punctuation:"។៕៖៙៚‘’“”", symbol:"៛", other:"\u200B\u200C\u200D", 
 orth:`Khmer. &nbsp; <a href="../scripts/khmer" target="_blank">Details</a>.`,
-type:"abugida", cs:"no", gpos:"yes", gsub:"yes", cursive:"no", wordsep:"no/zwsp", baseline:"mid", wrap:"word", hyphenation:"no",  direction:"ltr",  justification:"ig", 
-vowels:"inh:2 vs:19 vsl:4 ind:10 base:អ/QA pre:3 circ:5 comp:15 voc:4",  clusters:"inv stk", 
-linked:"khmer", fonts:"/pickers/khmr/", 
+type:"abugida", cs:"no", gpos:"yes", gsub:"yes", cursive:"no", wordsep:"no/zwsp", baseline:"romn", wrap:"word", hyphenation:"no",  direction:"ltr",  justification:"ig", 
+vowels:"inh:2 vs:19 cm:2 vsl:4 other:2 ind:10 base:អ/QA pre:3 circ:5 comp:15 voc:4",  clusters:"inv stk", 
+linked:"khmr/km", fonts:"/pickers/khmr/", 
 },
 
 
