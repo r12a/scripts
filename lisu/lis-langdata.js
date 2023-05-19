bad1cf07f5eee001a0bfa1a4c9ae18ab1b59d2c2
r12a <ishida@w3.org>
2023-05-19 07:01:13 +0100

    lis:  Update to match orthography chart

--- lisu/lis-langdata.js
+++ lisu/lis-langdata.js
@@ -5,13 +5,13 @@ var langs = {
 "lis": { name:"Lisu", local:"ꓡꓲ-ꓢꓴ (Li-su), Lisu", 
 source:"d6a5076cb77188fd52722d4a57b621ac1390c714", 
 region:"easia", countries:"China, Myanmar, Thailand", script:"lisu", speakers:"940000", 
-letter:"ꓐꓑꓒꓓꓔꓕꓖꓗꓘꓙꓚꓛꓜꓝꓞꓟꓠꓡꓢꓣꓤꓥꓦꓧꓨꓩꓪꓫꓬꓭꓮꓯꓰꓱꓲꓳꓴꓵꓶꓷꓸꓹꓺꓻꓼꓽʼˍ", punctuation:"《》…꓾꓿", 
+letter:"ˍꓐꓑꓒꓓꓔꓕꓖꓗꓘꓙꓚꓛꓜꓝꓞꓟꓠꓡꓢꓣꓤꓥꓦꓧꓨꓩꓪꓫꓬꓭꓮꓯꓰꓱꓲꓳꓴꓵꓶꓷꓸꓹꓺꓻꓼꓽ꓿", mark:"", number:"", punctuation:"…《》꓾", symbol:"", other:"", 
 orth:`Lisu (Fraser script). &nbsp; Since 1918, primary usage. <a href="../scripts/lisu" target="_blank">Details</a>.`,
 also:
 ["lis-latn | latn | 1915-1918, 1958-1966, and from 1981."],
-type:"abugida", cs:"no", gpos:"no", gsub:"no", cursive:"no", direction:"ltr", wordsep:"syllable", baseline:"mid", wrap:"syllable", hyphenation:"no",  justification:"sp",
-vowels:"let:10",
-linked:"lisu", fonts:"/pickers/lisu/", 
+type:"abugida", cs:"no", gpos:"no", gsub:"no", cursive:"no", direction:"ltr", wordsep:"syllable", baseline:"romn", wrap:"syllable", hyphenation:"no",  justification:"sp",
+vowels:"inh:1 let:10 other:4",
+linked:"lisu/lis", fonts:"/pickers/lisu/", 
 },
 
 
