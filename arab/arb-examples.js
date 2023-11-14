281a78cd54194c26a40e9cbe2fbd869a2fd1f809
r12a <ishida@w3.org>
2023-11-14 19:01:11 +0000

    arb-examples: Update top of page

--- arab/arb-examples.js
+++ arab/arb-examples.js
@@ -1,11 +1,28 @@
 /*
 @ Source: https:en.wiktionary.orgwikiAppendix:Arabic_Swadesh_list
 @ Source: https://en.wiktionary.org/w/index.php?title=Category:Arabic_terms_with_IPA_pronunciation&from=آ
-§ 'Scheherazade New'
-# 200
-~ rtl
-! ar
+
+native | meaning | IPA① | transcription | other transcriptions② | notes | wiktionary③ | rawIPA
+
+
+① separate each text unit (consonant+combining_mark(s)) with §, separate phonetic syllables with .
+   use ‹ and › for a sound that spans 2 text units
+   use – for a text unit that doesn't produce sound
+   syllable boundary markers go inside previous unit, and emphasis markers begin emphasised unit
+
+② the previous transcription field is always Latin. 
+   if there is another key orthography (eg. Mongolian cyrillic) it goes here.
+   vowelled/unvowelled alternatives also go here
+
+③ x means that no link to Wiktionary should be added
+   if Wiktionary spells the word differently, add the spelling here
 */
+
+
+otherTranscription = true   // kicks in the latest order of items
+
+
+
 
 
 autoExpandExamples.ar = `
