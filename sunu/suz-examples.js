d7ade1f7d780d55fe17384a5306f33aaf5d69029
r12a <ishida@w3.org>
2023-11-14 17:06:07 +0000

    suz-examples: General overhaul, and apply latest template.

--- sunu/suz-examples.js
+++ sunu/suz-examples.js
@@ -3,114 +3,125 @@
 @ Source: https://www.unicode.org/L2/L2021/21157-sunuwar.pdf
 @ Source: https://www.devkumarsunuwar.com.np/iwsadmin/imagine-filemanager/uploads/media/Second_Hamso_bhadra-August-September%202078.pdf
 
-native|meaning|IPA¹|transcription|notes|wiktionary²
+native | meaning | IPA① | transcription | other transcriptions② | notes | wiktionary③ | rawIPA
 
-[₁] separate grapheme clusters with §, separate phonetic syllables with .
-use ‹ and › for a sound that spans 2 CCSes
-use – for a CCS that doesn't produce sound
 
-[₂] x means that no link to Wiktionary should be added
+① separate each text unit (consonant+combining_mark(s)) with §, separate phonetic syllables with .
+   use ‹ and › for a sound that spans 2 text units
+   use – for a text unit that doesn't produce sound
+   syllable boundary markers go inside previous unit, and emphasis markers begin emphasised unit
+
+② the previous transcription field is always Latin. 
+   if there is another key orthography (eg. Mongolian cyrillic) it goes here.
+   vowelled/unvowelled alternatives also go here
+
+③ x means that no link to Wiktionary should be added
+   if Wiktionary spells the word differently, add the spelling here
 */
+
+
+
+otherTranscription = true
+
 
 
 
 autoExpandExamples.suz = `
 
+𑯇𑯃𑯜|woman|m§i§ʃ||||x
+𑯓𑯖̃𑯆𑯅|water|v§ãː§k§u||||x
 
-𑯇𑯃𑯜|woman|m§i§ʃ
-𑯓𑯖̃𑯆𑯅|water|v§ãː§k§u
+𑯒𑯅𑯇|zero|s§u§m||||x
+𑯆𑯖|one|k§aː||का||x|
+𑯍𑯃𑯠𑯒𑯃|two|n§i§ʔ§s§i||निक्शी||x|
+𑯒𑯖̃|three|s§ãː||सां||x
+𑯐𑯂|four|l§ẽː||लें||x|
+𑯚𑯈|five|ŋ§ə||ङो||x|
+𑯄𑯈𑯆𑯅|six|r§ə§k§u||रकु||x|
+𑯌𑯈𑯍𑯃|seven|t͡ʃ§ə§n§i||चनी||x|
+𑯒𑯈𑯒𑯃|eight|s§ə§s§i||सस्सी||x|
+𑯔𑯖̃|nine|y§ãː||यां||x|
+𑯊𑯈𑯅|ten|g§ə§u||गौ||x|
+𑯆𑯑𑯃̃𑯌 𑯎𑯄𑯂𑯒𑯂|Sunuwar script|k§o§ĩ§t͡ʃ§ §b§r§e§s§e||||x
+𑯆𑯑𑯃̐𑯌 𑯎̭𑯂𑯒𑯂|Sunuwar script|k§o§ĩ§t͡ʃ§ §b§r§e§s§e||||x
 
-𑯒𑯅𑯇|zero|s§u§m
-𑯆𑯖|one|k§aː
-𑯍𑯃𑯠𑯒𑯃|two|n§i§ʔ§s§i
-𑯒𑯖̃|three|s§ã
-𑯐𑯂|four|l§ɛ
-𑯚𑯈|five|ŋ§ə
-𑯄𑯈𑯆𑯅|six|r§ə§k§u
-𑯌𑯈𑯍𑯃|seven|t͡ʃ§ə§n§i
-𑯒𑯈𑯒𑯃|eight|s§ə§s§i
-𑯔𑯖̃|nine|y§ã
-𑯊𑯈𑯅|ten|g§ə§u
-𑯆𑯑𑯃̃𑯌 𑯎𑯄𑯂𑯒𑯂|Sunuwar script|k§o§ĩ§t͡ʃ§ §b§r§e§s§e
-𑯆𑯑𑯃̐𑯌 𑯎̭𑯂𑯒𑯂|Sunuwar script|k§o§ĩ§t͡ʃ§ §b§r§e§s§e
-
-𑯔𑯖̍𑯁|rice mill|j§a§t
-𑯒𑯂́𑯌𑯖|carding machine|s§e§t͡ʃ§aː
-𑯀𑯂𑯔𑯃|god|d§e§j§i
-𑯁𑯖𑯒𑯐𑯖|moon|t§a§s§l§a
-𑯂𑯆𑯑|thunderbolt|e§k§o
-𑯄𑯂𑯅|rain|r§e§u
-𑯅𑯁𑯘𑯃|top|u§t§tʰ§i
-𑯆𑯃𑯆𑯃|grandpa|k§i§k§i
-𑯇𑯖𑯇𑯖|mother|m§a§m§a
-𑯖𑯉𑯙𑯑|father|a§p§pʰ§o
-𑯉𑯃𑯉𑯃|emperor|p§i§p§i
-𑯊𑯖𑯒|cloud|g§a§s
-𑯋𑯖𑯇𑯒𑯑|light|h§a§m§s§o
-𑯌𑯖𑯄𑯇𑯃|young calf|t͡ʃ§a§r§m§i
-𑯍𑯖|sun|n§a
-𑯓𑯖𑯄𑯌𑯃|friend|w§a§r§t͡ʃ§i
-𑯖𑯐|infant|a§l
-𑯗𑯑̃:𑯊𑯖|deep pit|ɖ§õː§‹§g§aː
-𑯘𑯖𑯄𑯃|boarder|tʰ§a§r§i
-𑯙𑯖𑯄|fur|pʰ§a§r
-𑯛𑯐𑯖|axe|kʰ§l§a
-𑯜𑯂𑯄|rice|ʃ§e§r
-𑯜𑯖:𑯎|snake|ʃ§aː§‹§b
-𑯞𑯂̃𑯞𑯅|tadpole|ʈ§ẽ§ʈ§u
-𑯟𑯂𑯐𑯂|fireball|ʈʰ§e§l§e
-𑯠𑯐𑯑𑯠𑯑|throat|ʔ§l§o§ʔ§o
-𑯖̃:|he|ãː§‹
-𑯒𑯖̃:|three|s§ãː§‹
-𑯜𑯖̃:|what?|ʃ§ãː§‹
-𑯀𑯂|or|d§e
-𑯀𑯂̃:|snail|d§ẽː§‹
-𑯍𑯖̀|before|n§a
-𑯍𑯖́|sun|n§a
-𑯛𑯖:|decide|kʰ§aː§‹
-𑯛𑯖̃:|guess|kʰ§ãː§‹
-𑯊𑯂̍|nail|g§e
-𑯊𑯂̀|work|g§e
-𑯊𑯂́|side|g§e
-𑯊𑯂|you|g§e
-𑯍𑯂́|name|n§e
-𑯍𑯂|nose|n§e
-𑯍𑯂:|leech|n§eː§‹
-𑯍𑯂̀|what?; fame|n§e
-𑯁𑯂𑯇𑯌𑯖|to serve|t§e§m§c§a
-𑯁𑯂̍𑯇𑯌𑯖|to add|ˈt§e§m§c§a
-𑯘𑯃̍𑯌𑯌𑯖|to meet|ˈtʰ§i§c§c§a
-𑯘𑯃𑯌𑯌𑯖̍|to fall down|tʰ§i§c§ˈc§a
-𑯍𑯖̀𑯃𑯌𑯌𑯖|to rest|n§a§i§t͡ʃ§t͡ʃ§a
-𑯍𑯖̀𑯃𑯌𑯌𑯖|to pinch|n§a§i§t͡ʃ§t͡ʃ§a
-𑯍𑯖𑯃𑯌𑯌𑯖|to realise|n§a§i§t͡ʃ§t͡ʃ§a
-𑯍𑯖̍𑯃𑯌𑯌𑯖|to burn|n§a§i§t͡ʃ§t͡ʃ§a
-𑯎𑯐𑯂𑯌𑯖|to fall|b§l§e§t͡ʃ§a
-𑯎𑯐𑯂̀𑯠𑯌𑯖|to knock|b§l§e§ʔ§t͡ʃ§a
-𑯎𑯐𑯂́𑯠𑯌𑯖|to write|b§l§e§ʔ§t͡ʃ§a
-𑯎𑯐𑯂𑯠𑯌𑯖|to collect|b§l§e§ʔ§t͡ʃ§a
-𑯒𑯖̀𑯇𑯌𑯖|to store|s§a§m§t͡ʃ§a
-𑯒𑯖́𑯇𑯌𑯖|to lose|s§a§m§t͡ʃ§a
-𑯜𑯖𑯇𑯌𑯖|to abort|s§a§m§t͡ʃ§a
-𑯉𑯅𑯠𑯌𑯖|to measure|p§u§ʔ§t͡ʃ§a
-𑯉𑯅̀𑯠𑯌𑯖|to blast|p§u§ʔ§t͡ʃ§a
-𑯉𑯅́𑯠𑯌𑯖|to pile up|p§u§ʔ§t͡ʃ§a
-𑯉𑯅:𑯌𑯖|to gather|p§uː§‹§t͡ʃ§a
-𑯎𑯅̀𑯠𑯌𑯖|to burst|b§u§ʔ§t͡ʃ§a
-𑯎𑯅́𑯠𑯌𑯖|to accumulate|b§u§ʔ§t͡ʃ§a
-𑯉𑯅𑯌𑯖|to dry|p§uː§‹§t͡ʃ§a
-𑯖̃|my|a§n
-𑯏𑯂̃|surname|d͡ʒẽ
-𑯍𑯂𑯁𑯖|address|neta
-𑯁𑯖𑯓𑯅|son|tawu
-𑯁𑯖𑯇𑯃|daughter|tami
-𑯆𑯑𑯃̐𑯌|Sunuwar|k§oĩ§‹§t͡ʃ
-𑯆𑯑𑯃̃𑯌|Sunuwar|k§oĩ§‹§t͡ʃ
-𑯆𑯑̍𑯃̃𑯌|Sunuwar|k§oĩ§‹§t͡ʃ
+𑯔𑯖̍𑯁|rice mill|j§a§t||||x
+𑯒𑯂́𑯌𑯖|carding machine|s§e§t͡ʃ§aː||||x
+𑯀𑯂𑯔𑯃|god|d§e§j§i||देयि||x
+𑯁𑯖𑯒𑯐𑯖|moon|t§aː§s§l§a||तास्ला||x
+𑯂𑯆𑯑|thunderbolt|e§k§o||||x
+𑯄𑯂𑯅|rain|r§e§u||रेउ||x|
+𑯅𑯁𑯘𑯃|top|u§t§tʰ§i||||x
+𑯆𑯃𑯆𑯃|grandpa|k§i§k§i||||x
+𑯇𑯖𑯇𑯖|mother|m§a§m§a||||x
+𑯖𑯉𑯙𑯑|father|aː§p§pʰ§o||||x
+𑯉𑯃𑯉𑯃|emperor|p§i§p§||||x
+𑯊𑯖𑯒|cloud|g§aː§s||गोस्स||x
+𑯋𑯖𑯇𑯒𑯑|light|h§a§m§s§o||||x
+𑯌𑯖𑯄𑯇𑯃|young calf|t͡ʃ§a§r§m§i||||x
+𑯍𑯖|sun|n§aː||नां||x
+𑯓𑯖𑯄𑯌𑯃|friend|w§a§r§t͡ʃ§i||||x
+𑯖𑯐|infant|aː§l||गोस्स||x
+𑯗𑯑̃:𑯊𑯖|deep pit|ɖ§õː§‹§g§aː||||x
+𑯘𑯖𑯄𑯃|boarder|tʰ§a§r§i||||x
+𑯙𑯖𑯄|fur|pʰ§a§r||||x
+𑯛𑯐𑯖|axe|kʰ§l§a||||x
+𑯜𑯂𑯄|rice|ʃ§e§r||सयेर||x|
+𑯜𑯖:𑯎|snake|ʃ§aː§‹§b||सर्ब||x
+𑯞𑯂̃𑯞𑯅|tadpole|ʈ§ẽ§ʈ§u||||x
+𑯟𑯂𑯐𑯂|fireball|ʈʰ§e§l§e||||x
+𑯠𑯐𑯑𑯠𑯑|throat|ʔ§l§o§ʔ§o||क्रोक्क्रो||x
+𑯖̃:|he|ãː§‹||||x
+𑯒𑯖̃:|three|s§ãː§‹||सां||x
+𑯜𑯖̃:|what?|ʃ§ãː§‹||||x
+𑯀𑯂|or|d§e||||x
+𑯀𑯂̃:|snail|d§ẽː§‹||दें||x
+𑯍𑯖̀|before|n§aː||ना||x
+𑯍𑯖́|sun|n§aː||नां||x
+𑯛𑯖:|decide|kʰ§aː§‹||||x
+𑯛𑯖̃:|guess|kʰ§ãː§‹||||x
+𑯊𑯂̍|nail|g§e||ग्ये||x
+𑯊𑯂̀|work|g§e||ग्ये||x
+𑯊𑯂́|side|g§e||||x
+𑯊𑯂|you|g§e||गे||x
+𑯍𑯂́|name|n§e||||x
+𑯍𑯂|nose|n§e||||x
+𑯍𑯂:|leech|n§eː§‹||||x
+𑯍𑯂̀|what?; fame|n§e||||x
+𑯁𑯂𑯇𑯌𑯖|to serve|t§e§m§c§a||||x
+𑯁𑯂̍𑯇𑯌𑯖|to add|ˈt§e§m§c§a||तेम्चा||x
+𑯘𑯃̍𑯌𑯌𑯖|to meet|ˈtʰ§i§c§c§a||||x
+𑯘𑯃𑯌𑯌𑯖̍|to fall down|tʰ§i§c§ˈc§a||||x
+𑯍𑯖̀𑯃𑯌𑯌𑯖|to rest|n§a§i§t͡ʃ§t͡ʃ§a||नांइसीचा||x
+𑯍𑯖̀𑯃𑯌𑯌𑯖|to pinch|n§a§i§t͡ʃ§t͡ʃ§a||||x
+𑯍𑯖𑯃𑯌𑯌𑯖|to realise|n§a§i§t͡ʃ§t͡ʃ§a||||x
+𑯍𑯖̍𑯃𑯌𑯌𑯖|to burn|n§a§i§t͡ʃ§t͡ʃ§a||||x
+𑯎𑯐𑯂𑯌𑯖|to fall|b§l§e§t͡ʃ§a||||x
+𑯎𑯐𑯂̀𑯠𑯌𑯖|to knock|b§l§e§ʔ§t͡ʃ§a||||x
+𑯎𑯐𑯂́𑯠𑯌𑯖|to write|b§l§e§ʔ§t͡ʃ§a||||x
+𑯎𑯐𑯂𑯠𑯌𑯖|to collect|b§l§e§ʔ§t͡ʃ§a||||x
+𑯒𑯖̀𑯇𑯌𑯖|to store|s§a§m§t͡ʃ§a||||x
+𑯒𑯖́𑯇𑯌𑯖|to lose|s§a§m§t͡ʃ§a||||x
+𑯜𑯖𑯇𑯌𑯖|to abort|s§a§m§t͡ʃ§a||||x
+𑯉𑯅𑯠𑯌𑯖|to measure|p§u§ʔ§t͡ʃ§a||||x
+𑯉𑯅̀𑯠𑯌𑯖|to blast|p§u§ʔ§t͡ʃ§a||||x
+𑯉𑯅́𑯠𑯌𑯖|to pile up|p§u§ʔ§t͡ʃ§a||||x
+𑯉𑯅:𑯌𑯖|to gather|p§uː§‹§t͡ʃ§a||||x
+𑯎𑯅̀𑯠𑯌𑯖|to burst|b§u§ʔ§t͡ʃ§a||||x
+𑯎𑯅́𑯠𑯌𑯖|to accumulate|b§u§ʔ§t͡ʃ§a||||x
+𑯉𑯅𑯌𑯖|to dry|p§uː§‹§t͡ʃ§a||||x
+𑯖̃|my|a§n||||x
+𑯏𑯂̃|surname|d͡ʒ§ẽ||||x
+𑯍𑯂𑯁𑯖|address|n§e§t§a||||x
+𑯁𑯖𑯓𑯅|son|t§a§w§u||||x
+𑯁𑯖𑯇𑯃|daughter|t§a§m§i||तमी||x
+𑯆𑯑𑯃̐𑯌|Sunuwar|k§oĩ§‹§t͡ʃ||||x
+𑯆𑯑𑯃̃𑯌|Sunuwar|k§oĩ§‹§t͡ʃ||||x
+𑯆𑯑̍𑯃̃𑯌|Sunuwar|k§oĩ§‹§t͡ʃ||||x
 
 
-𑯎̭𑯂𑯒𑯂|script|br§e§s§e
-𑯎𑯄𑯂𑯒𑯂|script|br§e§s§e
+𑯎̭𑯂𑯒𑯂|script|br§e§s§e||||x
+𑯎𑯄𑯂𑯒𑯂|script|br§e§s§e||||x
 
 
 
