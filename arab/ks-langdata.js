e6f3c6b36a9a2e18047f7add6e81ee75388634f1
r12a <ishida@w3.org>
2023-11-18 21:40:08 +0000

    arab-ks: Add honorifics section

--- arab/ks-langdata.js
+++ arab/ks-langdata.js
@@ -2,15 +2,17 @@
 
 var langs = {
 
-"ks": { name:"Kashmiri (Koshur)", local:"کٲشُر‎ (Koshur)", silcode:"kas", rtl:true, 
+"ks": { name:"Kashmiri (Koshur)", local:"کٲشُر‎", localtrans:"(koshur)", silcode:"kas", rtl:true, 
 source:"cldr_ks,57237835618d4daaf27d149d55e14be159182c33", 
 region:"sasia", countries:"India, Pakistan", script:"arab", speakers:"5600000", 
-letter:"ؠآأؤإابتثجحخدذرزسشصضطظعغفقلمنوٲٹپچڈڑژکگںھہۂۄیے", letteraux:"ـ", mark:"͏َُِّْٕٖٟٔٗٚ", markaux:"ٓ", number:"۰۱۲۳۴۵۶۷۸۹", punctuation:"،؛؟٪۔“”", symbol:"", other:"\u061C\u200C\u200D\u200E\u200F\u202A\u202B\u202C\u2066\u2067\u2068\u2069", 
+letter:"ؠآأؤإابتثجحخدذرزسشصضطظعغفقلمنوٲٹپچڈڑژکگںھہۂۄیے", letteraux:"ـ", mark:"͏َُِّْٕٖٟؔٔٗٚ", markaux:"ٓ", number:"۰۱۲۳۴۵۶۷۸۹", punctuation:"،؛؟٪۔“”", symbol:"", other:"\u061C\u200C\u200D\u200E\u200F\u202A\u202B\u202C\u2066\u2067\u2068\u2069", aux:".[]§«»ʼۓ​‑–—‘’†‡…‰′″‹›₹",
 orth:`Arabic. &nbsp; Nastaliq style ([Aran]) is primary usage, but naskh style also used. Associated with the Muslim Kashmiris.
 Kashmiri is one of the languages that indicates all vowel sounds when using Arabic script. <a href="../scripts/arabic/kashmiri" target="_blank">Details</a>`,
-type:"alphabet", cs:"no", gpos:"yes", gsub:"yes", cursive:"yes", direction:"rtl", wordsep:"space", baseline:"romn", wrap:"word", hyphenation:"?",  justification:"?",
-vowels:"let:7 cm:10 other:2 comp:18(3)", clusters:"mrk",
-linked:"arab/ks", fonts:"/pickers/arab-ks/", 
+also:
+["ks-deva | deva | Used by the Hindi community in Kashmir."],
+type:"alphabet", cs:"no", gpos:"yes", gsub:"yes", cursive:"yes", wordsep:"space", wrap:"word", hyphenation:"?",  direction:"rtl",  justification:"?", 
+vowels:"let:7 cm:10 comp:18(3)",  clusters:"mrk", 
+linked:"arab/ks", picker:"arab-ks", font:"'Noto Nastaliq Urdu', 'Urdu Typesetting'", 
 },
 
 
