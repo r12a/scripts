51a9b7c093c775b08382cbb8c550c942cbc9e0f0
r12a <ishida@w3.org>
2023-12-11 17:33:54 +0000

    gara/wo: Sort out formatting character usage

--- gara/wo-langdata.js
+++ gara/wo-langdata.js
@@ -6,7 +6,7 @@ var langs = {
 source:"", 
 region:"afr", countries:"Senegal, Mauritania, The Gambia",
 script:"tbd", speakers:"5500000", 
-letter:"\u{10D71}\u{10D72}\u{10D73}\u{10D74}\u{10D75}\u{10D76}\u{10D77}\u{10D78}\u{10D79}\u{10D7A}\u{10D7B}\u{10D7C}\u{10D7D}\u{10D7E}\u{10D7F}\u{10D80}\u{10D81}\u{10D82}\u{10D51}\u{10D52}\u{10D53}\u{10D54}\u{10D55}\u{10D56}\u{10D57}\u{10D58}\u{10D59}\u{10D5A}\u{10D5B}\u{10D5C}\u{10D5D}\u{10D5E}\u{10D5F}\u{10D60}\u{10D61}\u{10D62}\u{10D51}\u{10D52}\u{10D53}\u{10D54}\u{10D55}\u{10D56}\u{10D57}\u{10D58}\u{10D59}\u{10D5A}\u{10D5B}\u{10D5C}\u{10D5D}\u{10D5E}\u{10D5F}\u{10D60}\u{10D61}\u{10D62}\u{10D50}", mark:"𐵩𐵪𐵫𐵬𐵭", punctuation:"‘’“”𐵮", number:"𐵀𐵁𐵂𐵃𐵄𐵅𐵆𐵇𐵈𐵉", symbol:"𐵯", other:"\u{200C}\u{200D}\u{2067}\u{202B}\u{2066}\u{202A}\u{2068}\u{2069}\u{202C}\u{200F}\u{200E}\u{61C}\u{34F}", 
+letter:"𐵊𐵋𐵌𐵍𐵎𐵐𐵑𐵒𐵓𐵔𐵕𐵖𐵗𐵘𐵙𐵚𐵛𐵜𐵝𐵞𐵟𐵠𐵡𐵢𐵣𐵰𐵱𐵲𐵳𐵴𐵵𐵶𐵷𐵸𐵹𐵺𐵻𐵼𐵽𐵾𐵿𐶀𐶁𐶂𐶃", mark:"𐵩𐵪𐵫𐵬𐵭", number:"𐵀𐵁𐵂𐵃𐵄𐵅𐵆𐵇𐵈𐵉", punctuation:"،؛؟‘’“”…𐵮", symbol:"𐵯", other:"\u200E\u200F\u202A\u202B\u202C\u2066\u2067\u2068\u2069",	
 orth:`Garay.`,
 also:
 ['wo-latn | latn | Primary usage.  <a href="../scripts/latn/wo.html#variants" target="_blank">Details.</a>',
