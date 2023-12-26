8facccbcbcff50625f83cb76e2358598dc2c2634
r12a <ishida@w3.org>
2023-12-26 09:26:37 +0000

    armn: Rework Basic Features. Add exclusions to Vowel, Consonant, & Shaping sections. Make summary tables h4. Fix picker link. Add more headings to consonant summary. Standardise summary & map table intros.

--- armn/hy-langdata.js
+++ armn/hy-langdata.js
@@ -2,17 +2,17 @@
 
 var langs = {
 
-"hy": { name:"Armenian", local:"հայերէն/հայերեն‎ (hayeren)", silcode:"hye", 
+"hy": { name:"Armenian", local:"հայերէն/հայերեն‎", localtrans:"(hayeren)", silcode:"hye", 
 source:"ac321e266f0a5b1374f2b3889d418fc3399daf34,cldr_hy,udhr_hye", 
 region:"wasia", countries:"Armenia, Russia, Azerbaijan, Iran", script:"armn", speakers:"10000000", 
-letter:"աբգդեզէըթժիլխծկհձղճմյնշոչպջռսվտրցւփքևօֆԱԲԳԴԵԶԷԸԹԺԻԼԽԾԿՀՁՂՃՄՅՆՇՈՉՊՋՌՍՎՏՐՑՒՓՔՕՖ", punctuation:"֊՝՜՞՛։․«»‐’", symbolaux:"֍֎֏", deprecated:"ՙ՚", 
+letter:"ԱԲԳԴԵԶԷԸԹԺԻԼԽԾԿՀՁՂՃՄՅՆՇՈՉՊՋՌՍՎՏՐՑՒՓՔՕՖաբգդեզէըթժիլխծկհձղճմյնշոչպջռսվտրցւփքօֆև", mark:"", number:"", punctuation:"«»՛՜՝՞։֊’․…", symbol:"֍֎֏", other:"", 
+aux:"[]§ʼ‌‍‑–—‘“”†‡‰′″‹›★",
 orth:`Armenian. &nbsp;
 <a href="../scripts/armenian" target="_blank">Detailed summary</a>.`,
 related:`See also Western Armenian [hyw].`,
-type:"alphabet", cs:"yes", gpos:"no", gsub:"no", cursive:"no", direction:"ltr", wordsep:"space", baseline:"mid", wrap:"word", hyphenation:"yes", hyphen:"֊*",  justification:"sp",
+type:"alphabet", cs:"yes", gpos:"no", gsub:"no", cursive:"no", direction:"ltr", wordsep:"space", baseline:"romn", wrap:"word", hyphenation:"yes", hyphen:"֊*",  justification:"sp",
 vowels:"let:8",
-linked:"armenian", fonts:"/pickers/armn/", 
+linked:"armn/hy", fonts:"/pickers/armn/", picker:"armn", 
 },
-
 
 }
