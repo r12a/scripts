231e9e82dc98acb474aa288499f3afebbee34bae
r12a <ishida@w3.org>
2023-12-27 12:36:39 +0000

    bn: Rework Basic Features. Add exclusions to Vowel, Consonant, & Shaping sections. Make summary tables h4. Fix picker link. Add more headings to consonant summary. Standardise summary & map table intros.

--- bugi/bug-langdata.js
+++ bugi/bug-langdata.js
@@ -2,16 +2,17 @@
 
 var langs = {
 
-"bug-bugi": { name:"Buginese", local:"ᨅᨔ ᨕᨘᨁᨗ (basa ugi)", silcode:"bug", 
+"bug-bugi": { name:"Buginese", local:"ᨅᨔ ᨕᨘᨁᨗ", localtrans:"(basa ugi)", silcode:"bug", 
 source:"d997476f6f5d5a7525a2cdf2d0790e944bfa83f2", 
 region:"seasia", countries:"Indonesia, Malaysia", script:"bugi", speakers:"5000000", 
-letter:"ᨀᨁᨂᨃᨄᨅᨆᨇᨈᨉᨊᨋᨌᨍᨎᨏᨐᨑᨒᨓᨔᨕᨖꧏ", mark:"ᨘᨗᨙᨚᨛ", number:"", punctuation:"᨞᨟", symbol:"", other:"\u200D", 
+letter:"ᨀᨁᨂᨃᨄᨅᨆᨇᨈᨉᨊᨋᨌᨍᨎᨏᨐᨑᨒᨓᨔᨕᨖꧏ", mark:"ᨘᨗᨙᨚᨛ", number:"", punctuation:"᨞᨟", symbol:"", other:"\u200D",
+aux:"​‌",
 orth:`[Buginese] &nbsp; No longer in significant use, but found on signage. 
 <a href="../scripts/buginese" target="_blank">Detailed summary</a>.`,
 also:
 ["bug | latn | "],
 type:"abugida", cs:"no", gpos:"yes", gsub:"yes", cursive:"no", direction:"ltr", wordsep:"space", baseline:"romn", wrap:"?", hyphenation:"no",  justification:"?",
-vowels:"inh:1 cm:5 vs:5 base:ᨕ/A pre:1",
+vowels:"inh:1 cm:5 vs:5 base:ᨕ/ɑ pre:1",
 linked:"bugi/bug", 
 },
 
