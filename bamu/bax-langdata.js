c8d0a9979729e01b95c1d5325f234172ae724f4a
r12a <ishida@w3.org>
2023-12-27 06:40:52 +0000

    bax: Add extra lists for CV(C) sections. Rework Basic Features. Add exclusions to Vowel, Consonant, & Shaping sections. Make summary tables h4. Fix picker link. Add more headings to consonant summary. Standardise summary & map table intros.

--- bamu/bax-langdata.js
+++ bamu/bax-langdata.js
@@ -2,31 +2,16 @@
 
 var langs = {
 
-"bax": { name:"Bamun (Bamum, Bamoun)", source:"udhr_bax", region:"afr", countries:"Cameroon", script:"latn", speakers:"420000", letter:"úéêüûâôîáèùàÚÉÊÜÛÂÔÎÁÈÙÀṅṄ", mark:"́̂̈̀̇", punctuation:"‘’", local:"Shü Pamom [ʃŷpǎˑmə̀m]", 
-orth:`[Latin] &nbsp; `,
-also:
-['bax-bamu | bamu | Taught in schools until 1930s.']
-},
-"bax": { name:"Bamun (Bamum, Bamoun)", local:"Shü Pamom [ʃŷpǎˑmə̀m]", 
-source:"udhr_bax", 
-region:"afr", countries:"Cameroon", script:"latn", speakers:"420000", 
-letter:"úéêüûâôîáèùàÚÉÊÜÛÂÔÎÁÈÙÀṅṄ", mark:"́̂̈̀̇", punctuation:"‘’", 
-orth:`[Latin] &nbsp; `,
-also:
-["bax-bamu | bamu | Taught in schools until 1930s."],
-},
-
-"bax-bamu": { name:"Bamun (Bamum, Bamoun)", local:"Shü Pamom [ʃŷpǎˑmə̀m]", silcode:"bax", 
+"bax-bamu": { name:"Bamun (Bamum, Bamoun)", local:"Shü Pamom", localtrans:"[ʃŷpǎˑmə̀m]", silcode:"bax", 
 source:"820f9f09ce28a3610548365c402e624a9d6e7b32", 
 region:"afr", countries:"Cameroon", script:"bamu", speakers:"420000", 
-letter:"ꚠꚡꚢꚣꚤꚥꚦꚧꚨꚩꚪꚫꚬꚭꚮꚯꚰꚱꚲꚳꚴꚵꚶꚷꚸꚹꚺꚻꚼꚽꚾꚿꛀꛁꛂꛃꛄꛅꛆꛇꛈꛉꛊꛋꛌꛍꛎꛏꛐꛑꛒꛓꛔꛕꛖꛗꛘꛙꛚꛛꛜꛝꛞꛟꛠꛡꛢꛣꛤꛥꛦꛧꛨꛩꛪꛫꛬꛭꛮꛯ", mark:"꛰꛱", punctuation:"꛲꛳꛴꛵꛶꛷", 
+letter:"ꚠꚡꚢꚣꚤꚥꚦꚧꚨꚩꚪꚫꚬꚭꚮꚯꚰꚱꚲꚳꚴꚵꚶꚷꚸꚹꚺꚻꚼꚽꚾꚿꛀꛁꛂꛃꛄꛅꛆꛇꛈꛉꛊꛋꛌꛍꛎꛏꛐꛑꛒꛓꛔꛕꛖꛗꛘꛙꛚꛛꛜꛝꛞꛟꛠꛡꛢꛣꛤꛥ", mark:"꛰꛱", number:"ꛦꛧꛨꛩꛪꛫꛬꛭꛮꛯ", punctuation:"꛲꛳꛴꛵꛶꛷", symbol:"", other:"", 
 orth:`[Bamum] &nbsp; Taught in schools until 1930s.`,
 also:
 ["bax | latn | "],
-type:"syllabary", cs:"no", gpos:"yes", gsub:"no", cursive:"no", direction:"ltr", wordsep:"space", baseline:"mid", wrap:"word", hyphenation:"?", justification:"sp",
-vowels:"syl:5", 
-linked:"bamu", 
+type:"syllabary", cs:"no", gpos:"yes", gsub:"no", cursive:"no", direction:"ltr", wordsep:"space", baseline:"romn", wrap:"word", hyphenation:"?", justification:"sp",
+vowels:"syl:6 cm:2", 
+linked:"bamu/bax", 
 },
-
 
 }
