3e294ee295b4b31ce9d7a1122eeb85ec23bb573b
r12a <ishida@w3.org>
2023-12-27 06:40:03 +0000

    bsq: Rework Basic Features. Add exclusions to Vowel, Consonant, & Shaping sections. Make summary tables h4. Fix picker link. Add more headings to consonant summary. Standardise summary & map table intros.

--- bass/bsq-langdata.js
+++ bass/bsq-langdata.js
@@ -2,17 +2,17 @@
 
 var langs = {
 
-"bsq-bass": { name:"Bassa", local:"𖫔𖫧𖫳𖫒𖫨𖫰𖫨𖫱 (Ɓǎsɔ́ɔ̀)", silcode:"bsq", 
+"bsq-bass": { name:"Bassa", local:"𖫔𖫧𖫳𖫒𖫨𖫰𖫨𖫱", localtrans:"(Ɓǎsɔ́ɔ̀)", silcode:"bsq", 
 source:"feb8d27835761376c21d0c994cb1c128010d1ac0", 
 region:"afr", countries:"Liberia", script:"bass", speakers:"648000", 
-letter:"𖫭𖫪𖫩𖫫𖫬𖫨𖫧𖫥𖫢𖫡𖫗𖫞𖫟𖫙𖫑𖫖𖫘𖫝𖫓𖫣𖫒𖫜𖫚𖫠𖫤𖫔𖫐𖫕𖫛𖫦", mark:"𖫰𖫱𖫲𖫳𖫴", punctuation:"𖫵“”", 
+letter:"𖫐𖫑𖫒𖫓𖫔𖫕𖫖𖫗𖫘𖫙𖫚𖫛𖫜𖫝𖫞𖫟𖫠𖫡𖫢𖫣𖫤𖫥𖫧𖫨𖫩𖫪𖫫𖫬𖫭", letteraux:"𖫦", mark:"𖫰𖫱𖫲𖫳𖫴", number:"", punctuation:"“”𖫵", symbol:"", other:"",
+aux:"!():;§«»ʼ‐‑–—‘’†‡…′″",
 orth:`[Bassa Vah] &nbsp; Rediscovered in the Americas at the start of the 20th century. Now used mainly by older people. <a href="../scripts/bassavah/">Details</a>.`,
 also:
 ["bsq | latn | Primary usage."],
-type:"alphabet", cs:"no", gpos:"yes", gsub:"no", cursive:"no", direction:"ltr", wordsep:"space", baseline:"mid", wrap:"?", hyphenation:"?",  justification:"?",
-vowels:"let:y",
-linked:"bassavah", fonts:"/pickers/bass/", 
+type:"alphabet", cs:"no", gpos:"yes", gsub:"no", cursive:"no", direction:"ltr", wordsep:"space", baseline:"romn", wrap:"?", hyphenation:"?",  justification:"?",
+vowels:"let:7",
+linked:"bass/bsq", fonts:"/pickers/bass/", picker:"bass", 
 },
-
 
 }
