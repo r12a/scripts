9556a3bb7ba447b3ba3a800b6dc8cecdefb57dea
r12a <ishida@w3.org>
2024-01-08 06:11:05 +0000

    latn/kmr: Final polish of first draft

--- latn/kmr-langdata.js
+++ latn/kmr-langdata.js
@@ -2,21 +2,25 @@
 
 var langs = {
 
-"kmr": { name:"Northern Kurdish (Kurmanji)", source:"udhr_kmr", region:"wasia", countries:"Turkey, Iraq, Syria, Iran, Lebanon, Armenia", script:"latn", speakers:"15000000", 
-letter:"çêîûňş", mark:"", number:"", punctuation:"‑–—‘’“”…‰", symbol:"", other:"", 
-local:"Kurmancî", 
-related:`Macrolanguage is Kurdish [ku]. Legacy applications often use ku rather kmr.`,
+"kmr": { name:"Northern Kurdish (Kurmanji)", local:"Kurmancî", 
+source:"udhr_kmr", 
+region:"wasia", countries:"Turkey, Iraq, Syria, Iran, Lebanon, Armenia", script:"latn", speakers:"15000000", 
+letter:"ÇÊÎÛçêîûňŞş", letteraux:"ŇḦḧẌẍ", mark:"", markaux:"̧̂", number:"", punctuation:"‑–—‘’“”…‰", symbol:"", other:"", 
 orth:`[Latin] &nbsp; Hawar alphabet, from 1932 in Syria and (fully) from 2013 in Turkey.`,
 also:
-['kmr-arab | arab | Naskh style, used in Iran, Iraq, Syria, Lebanon.',
-'kmr-armn | armn | 1921-1929 in Armenia.',
-'kmr-x-yanalif | latn | Yañalif-like alphabet, replaces Armenian alphabet during Latinisation phase (probably 1930s?).',
-'kmr-x-1928latn | latn | 1928 in Soviet Union, adds some cyrillic characters into a Latin set.',
-'kmr-x-1929latn | latn | 1929 in Soviet Union.',
-'kmr-cyrl | cyrl | From 1946, in Soviet Union &amp; Armenia.',
-'kmr-x-hawar2 | latn | Reduced Hawar alphabet, used in Turkey until 2013 (missing Q,W,X).',
-'kmr-x-uka | latn | Proposed Universal Kurdish alphabet (adds Ł, Ň, Ř and Ü).',
-'kmr-x-yekgirtu | latn | Yekgirtú (Kurdish Unified Alphabet) promoted by the Kurdish Academy of Language for all Kurdish dialects.',]
+["kmr-arab | arab | Naskh style, used in Iran, Iraq, Syria, Lebanon.",
+"kmr-armn | armn | 1921-1929 in Armenia.",
+"kmr-x-yanalif | latn | Yañalif-like alphabet, replaces Armenian alphabet during Latinisation phase (probably 1930s?).",
+"kmr-x-1928latn | latn | 1928 in Soviet Union, adds some cyrillic characters into a Latin set.",
+"kmr-x-1929latn | latn | 1929 in Soviet Union.",
+"kmr-cyrl | cyrl | From 1946, in Soviet Union &amp; Armenia.",
+"kmr-x-hawar2 | latn | Reduced Hawar alphabet, used in Turkey until 2013 (missing Q,W,X).",
+"kmr-x-uka | latn | Proposed Universal Kurdish alphabet (adds Ł, Ň, Ř and Ü).",
+"kmr-x-yekgirtu | latn | Yekgirtú (Kurdish Unified Alphabet) promoted by the Kurdish Academy of Language for all Kurdish dialects."],
+related:`Macrolanguage is Kurdish [ku]. Legacy applications often use ku rather kmr.`,
+type:"alphabet", cs:"yes", gpos:"no", gsub:"no", cursive:"no", wordsep:"space", baseline:"mid", wrap:"word", hyphenation:"?",  direction:"ltr",  justification:"sp", 
+vowels:"let:8", 
+linked:"latn/kmr", fonts:"latn-kmr", 
 },
 
 }
