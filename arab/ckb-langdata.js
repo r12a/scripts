a788fb1e0471fb98e38bd7a707e27aba5af7034a
r12a <ishida@w3.org>
2024-01-08 09:30:08 +0000

    ckb, kmr: Char usage fixes

--- arab/ckb-langdata.js
+++ arab/ckb-langdata.js
@@ -5,7 +5,7 @@ var langs = {
 "ckb": { name:"Central Kurdish (Sorani)", local:"کوردیی ناوەندی / سۆرانی", localtrans:"(Kurdîy nawendî / Soranî)", rtl:true, 
 source:"cldr_ckb,fa1e79cbcb9ca9906ebecd0c8e47c135d5a380c1", 
 region:"wasia", countries:"Iraq, Iran", script:"arab", speakers:"7250000", 
-letter:"ئابتجحخدرزسشعغفقلمنهوپچڕژڤکگڵھۆیێە", letteraux:"يۊ", mark:"͏", markaux:"̧ٔ", number:"", punctuation:"،؛؟“”…", symbol:"", other:"\u061C\u200C\u200D\u200E\u200F\u202A\u202B\u202C\u2066\u2067\u2068\u2069", 
+letter:"ئابتجحخدرزسشعغفقلمنهوپچڕژڤکگڵھۆیێە", letteraux:"يۊ", mark:"", markaux:"ٔ", number:"", punctuation:"،؛؟“”…", symbol:"", other:"\u200C\u200D\u200E\u200F\u202A\u202B\u202C\u2066\u2067\u2068\u2069", 
 orth:`[Arabic] &nbsp; Alphabetised naskh style, from 1920s, mostly used by Kurds in Iraq and Iran.`,
 also:
 ["ckb-latn | latn | Proposed Universal Kurdish alphabet (adds Ł, Ň, Ř and Ü).",
