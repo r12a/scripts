198d8dae609a44991b4f6b27e6effb3ec608509c
r12a <ishida@w3.org>
2024-01-15 19:43:41 +0000

    Add xx-sets.js files.  Tidy .css files and use for webfont settings in xx_vocab files.

---
+++ bass/bsq-sets.js
@@ -0,0 +1,12 @@
+
+
+// Sets that can be used for searching text in term lists and in pickers.																	
+// Keys are drawn from the following set of characters:																	
+// ⒶⒷⒸⒹⒺⒻⒼⒽⒾⒿⓀⓁⓂⓃⓄⓅⓆⓇⓈⓉⓊⓋⓌⓍⓎⓏ																	
+// ⓐⓑⓒⓓⓔⓕⓖⓗⓘⓙⓚⓛⓜⓝⓞⓟⓠⓡⓢⓣⓤⓥⓦⓧⓨⓩ⊙																	
+																	
+var collections = [
+{ symbol:'Ⓥ', desc:'Vowels', chars:'(𖫭|𖫪|𖫫|𖫩|𖫬|𖫨|𖫧)' },
+{ symbol:'Ⓒ', desc:'Consonants', chars:'(𖫥|𖫢|𖫡|𖫗|𖫞|𖫟|𖫙|𖫑|𖫖|𖫘|𖫝|𖫓|𖫣|𖫒|𖫜|𖫚|𖫠|𖫤|𖫔|𖫐|𖫕|𖫛|𖫦)' },
+{ symbol:'Ⓣ', desc:'Tones', chars:'(𖫰|𖫱|𖫲|𖫳|𖫴)' },
+]
---
+++ sunu/suz-sets.js
@@ -0,0 +1,12 @@
+
+// ⒶⒷⒸⒹⒺⒻⒼⒽⒾⒿⓀⓁⓂⓃⓄⓅⓆⓇⓈⓉⓊⓋⓌⓍⓎⓏ
+// ⓐⓑⓒⓓⓔⓕⓖⓗⓘⓙⓚⓛⓜⓝⓞⓟⓠⓡⓢⓣⓤⓥⓦⓧⓨⓩ⊙
+// 🅐🅑🅒🅓🅔🅕🅖🅗🅘🅙🅚🅛🅜🅝🅞🅟🅠🅡🅢🅣🅤🅥🅦🅧🅨🅩
+// 🅰🅱🅲🅳🅴🅵🅶🅷🅸🅹🅺🅻🅼🅽🅾🅿🆀🆁🆂🆃🆄🆅🆆🆇🆈🆉
+
+
+
+var collections = [
+{ symbol:'Ⓥ', desc:'Vowels', chars:'(\u{11BC2}|\u{11BC8}|\u{11BC3}|\u{11BD1}|\u{11BC5})' },
+{ symbol:'Ⓒ', desc:'Consonants', chars:'(\u{11BCE}|\u{11BD5}|\u{11BCC}|\u{11BDD}|\u{11BC0}|\u{11BD7}|\u{11BCA}|\u{11BCB}|\u{11BCF}|\u{11BC6}|\u{11BDB}|\u{11BD0}|\u{11BC7}|\u{11BCD}|\u{11BDA}|\u{11BC9}|\u{11BD9}|\u{11BC4}|\u{11BD2}|\u{11BDC}|\u{11BC1}|\u{11BD8}|\u{11BDE}|\u{11BDF}|\u{11BD3}|\u{11BD4})' },
+]
