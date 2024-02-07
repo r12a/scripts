d64273fcc3aa6ec71e02aeb64f7aef2e7c79c5a6
r12a <ishida@w3.org>
2024-02-07 07:57:21 +0000

    plrd/block-globals: Straggler

---
+++ plrd/block-globals.js
@@ -0,0 +1,11 @@
+
+window.version = 'v29'
+window.scriptName = 'Miao'
+window.ghDirectory = 'plrd'
+window.blockDirectory = 'plrd'
+window.languageList = ['hmd','hmdd'] // lang tags for each orthography handled
+window.detailsfileList = ['hmd'] // first part of xx-details for each orthography
+window.scriptISO = 'plrd'
+window.webfonts = ["Noto Sans Miao WF","Shimenkan Guifan WF"]
+window.defaultSize = '28px'
+
