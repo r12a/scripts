05427e6e9a4eead4958fbd492972e7e099fdb2b7
r12a <ishida@w3.org>
2024-02-08 06:58:32 +0000

    plrd/hmd-langdata: Add bits

--- plrd/hmd-langdata.js
+++ plrd/hmd-langdata.js
@@ -2,11 +2,11 @@
 
 var langs = {
 
-"hmd": { name:"A-Hmao", 
-source:"r12a", 
+"hmd": { name:"A-Hmao (Large Flowery Miao, Northeast Yunnan Miao)", 
+source:"https://github.com/r12a/app-charuse/commit/e3f689e49515f744239669fdf085c9ee8c9f71b9", 
 region:"easia", countries:"China", script:"plrd", speakers:"300,000", 
 letter:"𖼀𖼁𖼄𖼅𖼇𖼈𖼊𖼋𖼎𖼏𖼐𖼑𖼔𖼕𖼖𖼘𖼚𖼛𖼜𖼝𖼞𖼟𖼡𖼢𖼣𖼦𖼨𖼩𖼪𖼫𖼬𖼭𖼮𖼯𖼳𖼴𖼵𖼷𖼸𖼺𖼻𖼽𖽀𖽂𖽃𖽐𖾓𖾔𖾕𖾖𖾗𖾘𖾙", mark:"𖾏𖾐𖾑𖽔𖽗𖽘𖽙𖽜𖽝𖽞𖽟𖽠𖽡𖽢𖽣𖽤𖽦𖽨𖽪𖽫𖽬𖽭𖽱𖽳𖽵𖽷𖽹𖽺𖽻𖽾𖽑", punctuation:"!()-.:;?", 
-orth:`[Miao Pollard script] Primary usage. <a href="../scripts/plrd/' target='_blank'>Details</a>`,
+orth:`[Miao-Pollard] &nbsp; Primary usage. This actually covers both the 1936 (traditional, and most current) and the 1998 orthographies, as described by Enwall. <a href="../scripts/plrd/hmd.html" target="_blank">Details</a>.`,
 also:
 ["hmd-latn | latn | Several alternative variants."],
 type:"alphabet", cs:"no", gpos:"yes", gsub:"no", cursive:"no", wordsep:"syllable", baseline:"mid", wrap:"syllable", hyphenation:"no",  direction:"ltr",  justification:"?", 
