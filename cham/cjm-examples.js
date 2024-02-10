abcaac9d04d630b0349febc1a45d77d194cc75ce
r12a <ishida@w3.org>
2024-02-10 07:42:38 +0000

    cjm: Banking first draft

---
+++ cham/cjm-examples.js
@@ -0,0 +1,123 @@
+/*
+native | meaning | IPA① | transcription | other transcriptions② | notes | wiktionary③
+
+
+① separate each text unit (consonant+combining_mark(s)) with §, separate phonetic syllables with .
+   use ‹ and › for a sound that spans 2 text units
+   use – for a text unit that doesn't produce sound
+   syllable boundary markers go inside previous unit, and emphasis markers begin emphasised unit
+
+② the previous transcription field is always Latin. 
+   if there is another key orthography (eg. Mongolian cyrillic) it goes here.
+   vowelled/unvowelled alternatives also go here
+
+③ if Wiktionary spells the word differently, add the spelling here
+*/
+
+
+
+
+
+autoExpandExamples.cjm = `
+
+@ https://en.wiktionary.org/wiki/Category:Eastern_Cham_lemmas
+
+ꨀꨆꨯꨱꩀ|head|ʔa.§kɔ§ʔ|akaok
+ꨀꨇꩉ|letter|ʔa.§kʰa§r|akhar
+ꨀꨋꨪꩆ|wind||angin
+ꨀꨓꨭꨥ|soul|ʔa.§ta§ʊ|atuw
+ꨀꨗꨲꩀ|child|ʔa.§nɨː§ʔ|anâk
+ꨀꨗꩀ|child|ʔa.§nɨː§ʔ|anâk
+ꨀꨧꨮꨭ|dog|ʔa.§saʊ|asau
+ꨀꨳꨩ|water|ʔiaː|aia
+ꨁꨗꨤꩃ|dictionary|ʔi.§nə.§laːŋ|inalang
+ꨂꨣꩃ|person|ʔu.§raːŋ|urang
+ꨆꨝꨥ|water buffalo|ˈka§ba§ʊ|kabaw
+ꨆꨢꨮꨭ|wood, stick|ka.§jauː|kayau
+ꨆꨭꨝꨥ|water buffalo|ku.§ba§ʊ|kubaw
+ꨆꨭꩉ|round, circular|ku§r|kur
+ꨆꨴꨩ|monkey|kraː|kra
+ꨆꨴꨯꨱꩃ|river|krɔːŋ|kraong
+ꨆꨵꨮꨭ|three|klaʊ|klau
+ꨈꨤꨪꨟꩃ|spider|ɡ͡ɣa.§li.§mɨŋ|galimâng
+ꨌꨭꩌ|to kiss, smell|cum|cum
+ꨌꨮꩀ|mountain|cə§ʔ|cek
+ꨌꨰꩈ|tea|cɛː§‹|caiy
+ꨌꨳꨪꩌ|bird|ciːm|ciim
+ꨌꩌ|Cham|cam|cam
+ꨎꨣꨭꩌ|needle|d͡ʑa.§rum|jarum
+ꨎꨥꨩ|Java|d͡ʑa.§waː|Jawa
+ꨓꨊꨪꩆ|hand, arm|ta.§ŋɪ§n|tangin
+ꨓꨎꨭꩍ|seven|ta.§d͡ʑuh|tajuh
+ꨓꨭꩀ|hour||tuk
+ꨔꨮꨭ|to know, understand|tʰaʊ|thau
+ꨔꨯꨱꩍ|zero|tʰɔh|thaoh
+ꨔꩆꨕꨩ|post, stake|tʰa§n.§d̪aː|thanda
+ꨔꩍ|to sharpen||thah
+ꨕꨣꨶꨰ|thorn|d̪a.§rʊoɪ|daruai
+ꨕꨣꩍ|blood|d̪a.§rah|darah
+ꨕꨤꨚꩆ|eight|d̪a.§la.§pʌ§n|dalapan
+ꨕꨤꩌ|deep|d̪a.§lʌm|dalam
+ꨕꨯꨱꩃ|to help|d̪ɔŋ|daong
+ꨕꨶ|two|ˈdwa|dua
+ꨖꨬ|forehead|d̪ʱeɪ|dhei
+ꨘꨮꩌ|six|nəm|nem
+ꨘꩌ|six|nʌm|nam
+ꨚꨈꨯꨮꨩ|morning|pa.§ɡ͡ɣɛː|pagé
+ꨚꨝꩆ|table||paban
+ꨚꨣꨳꩀ|silver|pa.§ria§ʔ|pariak
+ꨚꨤꨬ|village|pa.§leɪ|palei
+ꨚꨧꨬ|iron|pa.§seɪ|pasei
+ꨚꨴꨯꨱꩃ|big|prɔŋ|praong
+ꨚꨵꨭꩍ|ten|pluh|pluh
+ꨚꨶꨮꩄ|to speak, say|pʊəɪ§ʔ|puec
+ꨝꨧꨬ|iron|ba.§seɪ|basei
+ꨝꨪꨧꨬ|iron|bi.§seɪ|bisei
+ꨝꨯꨱꩍ|fruit|bɔh|baoh
+ꨝꨵꨬ|to buy|bleɪ|blei
+ꨟꨐꩀ|oil, lamp|mə.§ɲɨː§ʔ|manyâk
+ꨟꨓꨩ|eye|mə.§taː|mata
+ꨟꨢꨥ|cat|mə.§ja§ʊ|mayaw
+ꨟꨣꨳꩍ|red|mə.§riah|mariah
+ꨟꨤꨪꩍ|jasmine|mə.§lih|malih
+ꨡꨵꩃ|courtyard, field|ɓlaːŋ|mblang
+ꨣꨓꨭꩍ|hundred|ra.§tuh|ratuh
+ꨣꨪꨓꨭꩍ|hundred|ri.§tuh|rituh
+ꨤꨗꨪꩂ|worm|la.§niː§ŋ|laning
+ꨤꨟꨩ|five|la.§mɨː|lamâ
+ꨤꨪꨟꨩ|five|li.§mɨː|limâ
+ꨦꨤꩌ|hello||salam
+ꨧ|one|saː|sa
+ꨧꨤꨚꩆ|nine|sa.§la.§pʌ§n|salapan
+ꨧꨤꨪꨚꨮꩆ|nine|sa.§li.§pən|salipen
+ꨧꨯꨱꩍ|zero|sɔh|saoh
+ꨧꩃ|house, home|saːŋ|sang
+ꨨꨣꨫ|to recite|ha.§riː|hari
+ꨨꨤꨭꩆ|slave, servant||halun
+ꨨꨵꨭꨥ|pestle||hluw
+ꨨꨶꩀ|to eat (rice)|hʊa§ʔ|huak
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+`
+
---
+++ cham/cjm-globals.js
@@ -0,0 +1,20 @@
+
+
+langTag='cjm' // BCP47 language tag for this orthography
+scriptTag='cham' // BCP47 script tag for this orthography
+notesLangtag='cjm'
+blockLangtag = 'cjm' // same as langTag; used in block.html
+blockDirection=''  // ltr/rtl or '' for ltr; used by block.html
+
+blockDirectoryName='cham'  // folder name where block file is found, no slash
+orthogFilePath='cham/cjm'  // path below /scripts, including filename, but no extension, eg. lana/nod
+pickerDir='cham-cjm'  // directory name for picker, no slash at end
+scriptSummaryTableName='cjm'  // id used for langs record in xx-langdata.js; used to make summary table
+charUsageBCP='cjm'  // bcp tag to find the orthography in the charuse app; used in makeIndexIntro only
+orthogName = 'Cham' // natural language name for the orthography; used only in makeIndexIntro
+languageName = 'Cham' // natural language name of language; used in various places, esp block
+scriptIsIndic = true  // true if there are indic syllabic categories; used only in makeIndexIntro
+
+
+defaultSize = '24px'  // used by block.html to set size of font, including in font mgt panel
+letterClassName = 'cjm'  // classname for a given orthography in the block page
