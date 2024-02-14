25c8ddc8154d7ef2d6c8397cef851daffcf834f4
r12a <ishida@w3.org>
2024-02-14 17:44:42 +0000

    rej: Add summary & langdata

--- rjng/rej-langdata.js
+++ rjng/rej-langdata.js
@@ -2,17 +2,14 @@
 
 var langs = {
 
-"hi": { name:"Hindi", local:" मानक हिन्दी‎ (Mānak Hindī), हिन्दी‎ (Hindī)", silcode:"hin", 
-source:"cldr_hi,udhr_hin,9d8e188be96400153901b55006feaaf9fd4bf3b1", 
-region:"sasia", countries:"India, Nepal, Singapore, South Africa", script:"deva", speakers:"260000000", 
-letter:"अआइईउऊऋएऐओऔकखगघङचछजझञटठडढणतथदधनपफबभमयरलवशषसहक़फ़ज़ख़ग़ड़ढ़", letteraux:"ऍऑऽॐ", mark:"ँंः़ािीुूृेैोौ्", markaux:"ॅॉ", number:"०१२३४५६७८९", punctuation:"।॥॰‘’“”—", symbol:"₹", other:"‌‍", 
-orth:`[Devanagari] &nbsp;  
-<a href="../scripts/devanagari" target="_blank">Detailed summary</a>.`,
-also:
-["hi-newa | newa | Archaic, no longer in use."],
-type:"abugida", cs:"no", gpos:"yes", gsub:"yes", cursive:"no", wordsep:"space", baseline:"high", wrap:"word", hyphenation:"(yes)", hyphen:"-",  direction:"ltr",  justification:"sp", 
-vowels:"inh:1 vs:11 ind:12 pre:1 voc:1",  clusters:"inv stk cnj lig rax",  finals:"cm:2 let:y", 
-linked:"devanagari", fonts:"/pickers/deva-hi/", 
+"rej": { name:"Rejang", 
+source:"r12a", 
+region:"seasia", countries:"Indonesia", script:"rjng", speakers:"350,000", 
+letter:"ꤰꤱꤲꤳꤴꤵꤶꤷꤸꤹꤺꤻꤼꤽꤾꤿꥀꥁꥂꥃꥄꥅꥆ", mark:"ꥇꥈꥉꥊꥋꥌꥍꥎꥏꥐꥑꥒ꥓", punctuation:"꥟", 
+orth:`[Rejang script] Endangered. <a href="../scripts/rjng/' target='_blank'>Details</a>.`,
+type:"abugida", cs:"no", gpos:"yes", gsub:"no", cursive:"no", wordsep:"space", baseline:"mid", wrap:"?", hyphenation:"?",  direction:"ltr",  justification:"?", 
+vowels:"cm:8 inh:a ind:1 base:A pre:3 circ:1",  clusters:"vir",  finals:"cm:4", 
+linked:"rjng/rej", fonts:"rjng-rej", font:"Noto Sans Rejang", 
 },
 
 }
