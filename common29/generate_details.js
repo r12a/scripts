5af9f8c790532f1a861fe968b697cecf6c5a67ae
r12a <ishida@w3.org>
2024-03-08 05:07:45 +0000

    common29/generate_details: Significantly increase data pulled from spreadsheet

--- common29/generate_details.js
+++ common29/generate_details.js
@@ -88,13 +88,30 @@ function makeListFromChars (chars) {
 
         out += "'\\u{"+hex+"}': `\n"
         out += '<p class="insertTranscription">&#x'+hex+';</p>\n\n'
-        
+
+        // add status info to top of entry
+        if (spreadsheetRows[charList[x]][cols['status']] || spreadsheetRows[charList[x]][cols['statusLoc']]) {
+            out += `<p>`
+            if (spreadsheetRows[charList[x]][cols['statusLoc']]) {
+                out += ' '+ spreadsheetRows[charList[x]][cols['statusLoc']]
+                }
+            if (spreadsheetRows[charList[x]][cols['status']]) {
+                switch (spreadsheetRows[charList[x]][cols['status']]) {
+                    case 'l': out += 'Used for non-native sounds in loan words.'; break
+                    case 'i': out += 'Infrequent.'; break
+                    case 'a': out += 'Archaic.'; break
+                    }
+                }
+            out += `</p>\n`
+            }
+
         // make code for each phone
         if (spreadsheetRows[charList[x]] && spreadsheetRows[charList[x]][cols['ipaLoc']]) {
             ipas = spreadsheetRows[charList[x]][cols['ipaLoc']].split(' ')
-            
+
+
             for (i=0;i<ipas.length;i++) {
-                out +=
+               out +=
             `<p><span class="ipa h">${ ipas[i] }</span> ${ spreadsheetRows[charList[x]][cols['typeLoc']] }`
                 if ( spreadsheetRows[charList[x]][cols['ipaPlus']] ) out += ` with inherent vowel <span class="ipa">${ spreadsheetRows[charList[x]][cols['ipaPlus']] }</span>`
                 out +=
@@ -111,96 +128,41 @@ function makeListFromChars (chars) {
                 for (i=0;i<ipao.length;i++) {
                     out +=
                 `<p><span class="ipa h">${ ipao[i] }</span>`
-                    out += `</p>\n<p><span class="eg" lang="${ lang }"></span></p>\n<p><span class="eg" lang="${ lang }"></span></p>\n<p><span class="eg" lang="${ lang }"></span></p>\n`
+                    out += `</p>\n<p><span class="eg" lang="${ lang }"></span></p>\n<p><span class="eg" lang="${ lang }"></span></p>\n<p><span class="eg" lang="${ lang }"></span></p>`
                     }              
                 }
-            
-            // check for combinations
-            combinations = ''
-            for (comb in spreadsheetRows) {
-                if ([...comb].length > 1 && comb.includes(charList[x]) && spreadsheetRows[comb][cols['ipaLoc']]) {
-                    console.log(comb,'>>',spreadsheetRows[comb][cols['ipaLoc']])
-                    combinations += `\n<p><span class="ipa h">${ spreadsheetRows[comb][cols['ipaLoc']] }</span> is <span class="ch">${ comb }</span></p>\n`
-                    combinations += `<p><span class="eg" lang="${ lang }"></span></p>\n`
+            }
+        // check for combinations
+        combinations = ''
+        for (comb in spreadsheetRows) {
+            if ([...comb].length > 1 && comb.includes(charList[x]) && spreadsheetRows[comb][cols['ipaLoc']]) {
+                console.log(comb,'>>',spreadsheetRows[comb][cols['ipaLoc']])
+                if (spreadsheetRows[comb][cols['transLoc']]) {
+                    combinations += `\n<p class="insertTranscription">${ comb }</p>`
                     }
-                }
-            if (combinations) out += `\n<p class="leadin">Combinations</p>${ combinations }`
-            }
-        out += '\n`,\n\n\n\n\n\n'
-		}
-	return out
-	}
-
-
-
-
-
-
-
-function makeListFromCharsXX (chars) {
-    // generate the code for the xx-details
-    
-	var charList = chars.split(' ')
-    var items, hex, ipas, combinations
-    
-    if (document.getElementById('sort').checked) charList = charList.sort()
-    
-	var out = ''
-	
-    for (var x=0; x<charList.length; x++) {
-        if (charList[x] == '') continue
-        items = charList[x].split('\t')
-        
-        // get code point value, converting escapes if necessary
-        if (items[0].includes('&#x')) {
-            items[0] = items[0].replace('&#x','').replace(';','')
-            i = parseInt(items[0],16)
-            }
-		else i = items[0].codePointAt(0)
-		//console.log('i',i, 'items',items)
-
-        hex = i.toString(16).toUpperCase()
-        while (hex.length < 4) hex = '0' + hex
-        //console.log(hex)
-
-        out += "'\\u{"+hex+"}': `\n"
-        out += '<p class="insertTranscription">&#x'+hex+';</p>\n'
-        
-        // make code for each phone
-        if (spreadsheetRows[charList[x]] && spreadsheetRows[charList[x]][cols['ipaLoc']]) {
-            ipas = spreadsheetRows[charList[x]][cols['ipaLoc']].split(' ')
-            
-            for (i=0;i<ipas.length;i++) {
-                
-            
-//                out +=
-//            `\n<p><span class="ipa h">${ ipas[i] }</span> ${ spreadsheetRows[charList[x]][9] }.</p>
-//<p><span class="eg" lang="${ lang }"></span></p>
-//<p><span class="eg" lang="${ lang }"></span></p>
-//<p><span class="eg" lang="${ lang }"></span></p>`
-
-                out +=
-            `\n<p><span class="ipa h">${ ipas[i] }</span> ${ spreadsheetRows[charList[x]][cols['typeLoc']] }`
-                if ( spreadsheetRows[charList[x]][cols['ipaPlus']] ) out += ` with inherent vowel <span class="ipa">${ spreadsheetRows[charList[x]][cols['ipaPlus']] }</span>`
-                out +=
-            `.</p>
-<p><span class="eg" lang="${ lang }"></span></p>
-<p><span class="eg" lang="${ lang }"></span></p>
-<p><span class="eg" lang="${ lang }"></span></p>`
-              
-                // check for combinations
-                combinations = ''
-                for (comb in spreadsheetRows) {
-                    if ([...comb].length > 1 && comb.includes(charList[x]) && spreadsheetRows[comb][cols['ipaLoc']]) {
-                        console.log(comb,'>>',spreadsheetRows[comb][cols['ipaLoc']])
-                        combinations += `\n<p><span class="ipa h">${ spreadsheetRows[comb][cols['ipaLoc']] }</span> is <span class="ch">${ comb }</span></p>\n`
-                        combinations += `<p><span class="eg" lang="${ lang }"></span></p>\n`
+                combinations += `\n<p><span class="ipa h">${ spreadsheetRows[comb][cols['ipaLoc']] }</span> is <span class="ch">${ comb }</span>`
+                if (spreadsheetRows[comb][cols['status']]) {
+                    switch (spreadsheetRows[comb][cols['status']]) {
+                        case 'l': combinations += ' (used for non-native sounds in loan words)'; break
+                        case 'i': combinations += ' (infrequent)'; break
+                        case 'a': combinations += ' (archaic)'; break
                         }
                     }
-                if (combinations) out += `\n\n<p class="leadin">Combinations</p>${ combinations }`
+                if (spreadsheetRows[comb][cols['statusLoc']]) {
+                    combinations += ' '+ spreadsheetRows[comb][cols['statusLoc']]
+                    }
+                combinations += `</p>\n`
+                combinations += `<p><span class="eg" lang="${ lang }"></span></p>\n`
                 }
             }
+        if (combinations) out += `\n<p class="leadin">Combinations</p>${ combinations }`
         out += '\n`,\n\n\n\n\n\n'
 		}
 	return out
 	}
+
+
+
+
+
+
