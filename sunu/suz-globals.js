4ca0fbd0d5769c40284742fe7ba0e48ca15a4af7
r12a <ishida@w3.org>
2024-03-15 07:13:13 +0000

    suz: Feb template update

--- sunu/suz-globals.js
+++ sunu/suz-globals.js
@@ -17,7 +17,7 @@ scriptIsIndic = true  // true if there are indic syllabic categories; used only 
 
 defaultSize = '24px'  // used by block.html to set size of font, including in font mgt panel
 letterClassName = 'suz'  // classname for a given orthography in the block page
-
+hideBlockName = 'SUNUWAR' // if a value is given, that text is removed from code point names
 
 
 
