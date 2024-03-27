7ac3f7cec67e9ff5a7186e324f6e39a69137ae65
r12a <ishida@w3.org>
2024-03-27 11:44:36 +0000

    kore: Feb template update & substantial rewrite of jamos, etc.

--- kore/ko-examples.js
+++ kore/ko-examples.js
@@ -1,4 +1,54 @@
+/*
+native | meaning | IPA① | transcription | other transcriptions② | notes | wiktionary③
+
+
+① separate each text unit (consonant+combining_mark(s)) with §, separate phonetic syllables with .
+   use ‹ and › for a sound that spans 2 text units
+   use – for a text unit that doesn't produce sound
+   syllable boundary markers go inside previous unit, and emphasis markers begin emphasised unit
+
+② the previous transcription field is always Latin. 
+   if there is another key orthography (eg. Mongolian cyrillic) it goes here.
+   vowelled/unvowelled alternatives also go here
+
+③ if Wiktionary spells the word differently, add the spelling here
+*/
+
+
+
+
+
 autoExpandExamples.ko = `
+
+@ Source
+가톨릭교회|Catholic church|ka̠§tʰo̞ʎ§ʎik̚ §kjo(ː)§ɦø̞|Gatollikgyohoe
+ᄀ​ᅡ​ᄐ​ᅩ​ᆯ​ᄅ​ᅵ​ᆨ​ᄀ​ᅭ​ᄒ​ᅬ|Catholic church|k§§a̠§§tʰ§§o̞§§ʎ§§ʎ§§i§§k̚ §§k§§jo(ː)§§ɦ§§ø̞|Gatollikgyohoe
+
+
+
+@ other
+다국어|multilingual|ta§kuk̚§ʌ|dagukeo
+다국어|multilingual|t§a§k§u§k̚§–§ʌ|dagukeo
+
+
+
 
 `
 
+
+
+
+
+
+
+
+
+termLists = [
+
+{
+"title":"Wiktionary, Korean terms with IPA pronunciation",
+"url":"https://en.wiktionary.org/wiki/Category:Korean_terms_with_IPA_pronunciation",
+},
+
+
+]
