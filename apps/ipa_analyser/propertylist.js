cc746da37ec753a7d92d5231fa45b513c9c3bf3f
r12a <ishida@w3.org>
2024-04-01 11:26:21 +0100

    + IPA Analyser app

---
+++ apps/ipa_analyser/propertylist.js
@@ -0,0 +1,259 @@
+    var symbols = {
+    'p':'Voiceless bilabial plosive',
+    'b':'Voiced bilabial plosive',
+    't':'Voiceless alveolar plosive',
+    'd':'Voiced alveolar plosive',
+    'ʈ':'Voiceless retroflex plosive',
+    'ɖ':'Voiced retroflex plosive',
+    'c':'Voiceless palatal plosive',
+    'ɟ':'Voiced palatal plosive',
+    'k':'Voiceless velar plosive',
+    'ɡ':'Voiced velar plosive',
+    'q':'Voiceless uvular plosive',
+    'ɢ':'Voiced uvular plosive',
+    'ʡ':'Epiglottal plosive',
+    'ʔ':'Glottal stop',
+
+    'ɓ':'Voiced bilabial implosive',
+    'ɗ':'Voiced alveolar implosive',
+    'ᶑ':'Voiced retroflex implosive',   // add to IPA picker
+    'ʄ':'Voiced palatal implosive',
+    'ɠ':'Voiced velar implosive',
+    'ʛ':'Voiced uvular implosive',
+
+    'ɸ':'Voiceless bilabial fricative',
+    'β':'Voiced bilabial fricative',
+    'f':'Voiceless labiodental fricative',
+    'v':'Voiced labiodental fricative',
+    'θ':'Voiceless dental fricative',
+    'ð':'Voiced dental fricative',
+    's':'Voiceless alveolar fricative',
+    'z':'Voiced alveolar fricative',
+    'ʃ':'Voiceless postalveolar fricative',
+    'ʒ':'Voiced postalveolar fricative',
+    'ʂ':'Voiceless retroflex fricative',
+    'ʐ':'Voiced retroflex fricative',
+    'ɕ':'Voiceless alveolo-palatal fricative',
+    'ʑ':'Voiced alveolo-palatal fricative',
+    'ç':'Voiceless palatal fricative',
+    'ʝ':'Voiced palatal fricative',
+    'x':'Voiceless velar fricative',
+    'ɣ':'Voiced velar fricative',
+    'χ':'Voiceless uvular fricative',
+    'ʁ':'Voiced uvular fricative',
+    'ħ':'Voiceless pharyngeal fricative',
+    'ʕ':'Voiced pharyngeal fricative',
+    'h':'Voiced glottal fricative',
+    'ɦ':'Voiced glottal fricative',
+
+
+    'm':'Voiced bilabial nasal',
+    'ɱ':'Voiced labiodental nasal',
+    'n':'Voiced alveolar nasal',
+    'ɳ':'Voiced retroflex nasal',
+    'ɲ':'Voiced palatal nasal',
+    'ŋ':'Voiced velar nasal',
+    'ɴ':'Voiced uvular nasal',
+    
+    'ʍ':'Voiceless labiovelar approximant',
+    'w':'Voiced labiovelar approximant',
+    'ɥ':'Voiced labial-palatal approximant',
+    'w':'Voiced labiovelar approximant',
+    'ʋ':'Voiced labiodental approximant',
+    'ɹ':'Voiced alveolar approximant',
+    'ɻ':'Voiced retroflex approximant',
+    'j':'Voiced palatal approximant',
+    'ɰ':'Voiced velar approximant',
+   
+    'ⱱ':'Voiced labiodental flap',
+    'ɾ':'Voiced alveolar flap',
+    'ɽ':'Voiced retroflex flap',
+
+    'ʙ':'Voiced bilabial trill',
+    'r':'Voiced alveolar trill',
+    'ʀ':'Voiced uvular trill',
+    'ʜ':'Voiceless epiglottal trill',
+    'ʢ':'Voiced epiglottal trill',
+    
+    'ɬ':'Voiceless alveolar lateral fricative',
+    'ɮ':'Voiced alveolar lateral fricative',
+    'ꞎ':'Voiceless retroflex lateral fricative', // add
+    '𝼅':'Voiced retroflex lateral fricative',  // add
+    '𝼄':'Voiceless velar lateral fricative',  // add
+    
+    'l':'Voiced alveolar lateral approximant',
+    'ɭ':'Voiced retroflex lateral approximant',
+    'ʎ':'Voiced palatal lateral approximant', 
+    'ʟ':'Voiced velar lateral approximant',  // add
+
+    'ɺ':'Voiced alveolar lateral flap',
+    '𝼈':'Voiced retroflex lateral flap',
+
+    'ʘ':'Bilabial click',
+    'ǀ':'Dental click',
+    'ǃ':'Alveolar click',
+    'ǂ':'Palatal click',
+    'ʘ':'Bilabial click',
+    'ǁ':'Lateral click',
+    
+    
+    'ʼ':'Ejective consonant',
+
+    't͡s':'Voiceless dental affricate',
+    'd͡z':'Voiced dental affricate',
+    't͡s':'Voiceless alveolar affricate',
+    'd͡z':'Voiced alveolar affricate',
+    't͡ʃ':'Voiceless postalveolar affricate',
+    'd͡ʒ':'Voiced postalveolar affricate',
+    'ʈ͡ʂ':'Voiceless retroflex affricate',
+    'ɖ͡ʐ':'Voiced retroflex affricate',
+    't͡ɕ':'Voiceless alveolo-palatal affricate',
+    'd͡ʑ':'Voiced alveolo-palatal affricate',
+
+    't͡pʼ':'Labial–alveolar ejective stop',
+    'd͡b':'Voiceless dental affricate',
+    'ɧ':'Sj-sound',
+    'k͡p':'Voiceless labial–velar plosive',
+    'ɡ͡b':'Voiced labial–velar plosive',
+
+    'ɫ':'Velarized voiced alveolar approximant',
+
+
+
+
+    'i':'Close front unrounded vowel',
+    'y':'Close front rounded vowel',
+    'ɨ':'Close central unrounded vowel',
+    'ʉ':'Close central rounded vowel',
+    'ɯ':'Close back unrounded vowel',
+    'u':'Close back rounded vowel',
+    
+    'ɪ':'Near-close near-front unrounded vowel',
+    'ʏ':'Near-close near-front rounded vowel',
+    'ʊ':'Near-close near-back rounded vowel',
+    
+    'e':'Close-mid front unrounded vowel',
+    'ø':'Close-mid front rounded vowel',
+    'ɘ':'Close-mid central unrounded vowel',
+    'ɵ':'Close-mid central rounded vowel',
+    'ɤ':'Close-mid back unrounded vowel',
+    'o':'Close-mid back rounded vowel',
+    
+    'ə':'Mid central vowel',
+    
+    'ɛ':'Open-mid front unrounded vowel',
+    'œ':'Open-mid front rounded vowel',
+    'ɜ':'Open-mid central unrounded vowel',
+    'ɞ':'Open-mid central rounded vowel',
+    'ʌ':'Open-mid back unrounded vowel',
+    'ɔ':'Open-mid back rounded vowel',
+    
+    'æ':'Near-open front unrounded vowel',
+    'ɐ':'Near-open central vowel',
+    
+    'a':'Open front unrounded vowel',
+    'ɶ':'Open front rounded vowel',
+    'ä':'Open central unrounded vowel',
+    'ɑ':'Open back unrounded vowel',
+    'ɒ':'Open back rounded vowel',
+    
+    
+    
+    'ɚ':'Rhoticised schwa§R-colored_vowel',
+    'ɝ':'Rhoticised lower mid central vowel§R-colored_vowel',
+
+    
+    
+    
+    '\u030A':'Voicelessness',
+    '\u0325':'Voicelessness',
+    '\u0329':'Syllabic consonant',
+    '\u030D':'Syllabic consonant',
+    '\u207F':'Nasal release',
+    '\u02B0':'Aspirated consonant',
+    '\u032F':'Semivowel',
+    '\u0311':'Semivowel',
+    '\u031A':'No audible release',
+    '\u02E1':'Lateral release',
+    '\u032C':'Voiced',
+    '\u0324':'Breathy voice',
+    '\u0330':'Creaky voice',
+    '\u032A':'Dental consonant',
+    '\u033C':'Linguolabial consonant',
+    '\u033A':'Linguolabial consonant',
+    '\u033A':'Apical consonant',
+    '\u033B':'Laminal consonant',
+    '\u031F':'Advanced§Relative_articulation#Advanced_and_retracted',
+    '\u0320':'Retracted§Relative_articulation#Advanced_and_retracted',
+    '\u02D6':'Advanced§Relative_articulation#Advanced_and_retracted',
+    '\u02D7':'Retracted§Relative_articulation#Advanced_and_retracted',
+    '\u031D':'Raised§Relative_articulation#Raised_and_lowered',
+    '\u031E':'Lowered§Relative_articulation#Raised_and_lowered',
+    '\u02D4':'Raised§Relative_articulation#Raised_and_lowered',
+    '\u02D5':'Lowered§Relative_articulation#Raised_and_lowered',
+    '\u0308':'Centralised§Relative_articulation#Centralized',
+    '\u033D':'Mid-centralised§Relative_articulation#Centralized',
+    '\u0339':'More rounded§Relative_articulation#More_and_less_rounded',
+    '\u031C':'Less rounded§Relative_articulation#More_and_less_rounded',
+    'ʷ':'Labialization',
+    'ʲ':'Palatalization',
+    'ˠ':'Velarization/pharyngealization§Velarization',
+    'ˤ':'Pharyngealization§Velarization',
+    '\u0334':'Velarization',
+    '\u0318':'Advanced tongue root§Advanced and retracted tongue root',
+    '\u0319':'Retracted tongue root§Advanced and retracted tongue root',
+    '\u0303':'Nasalization',
+    '\u02DE':'Rhoticity§R-colored vowel',
+    '\u0348':'Faucalized voice',
+    '\u0327':'Fricative§Voiceless_palatal_fricative',   // add to IPA picker
+    
+    
+    
+    
+    
+    
+    'ˈ':'Primary stress§Stress_(linguistics)',
+    'ˌ':'Secondary stress',
+    'ː':'Long§Length_(phonetics)',
+    'ˑ':'Half-long§Length_(phonetics)',
+    '.':'Syllable break§Syllable',
+    '‿':'Linking§Connected speech',
+    '|':'Minor (foot) break§Prosodic unit',
+    '‖':'Major (intonation) break§Prosodic unit',
+    '\u8599':'Global rise§Intonation_(linguistics)',
+    '\u8600':'Global fall§Intonation_(linguistics)',
+    'ꜛ':'Upstep',
+    'ꜜ':'Downstep',
+    
+    
+    
+    
+    '\u030B':'Extra high pitch',
+    '\u0301':'High pitch',
+    '\u0304':'Mid pitch',
+    '\u0300':'Low pitch',
+    '\u030F':'Extra low pitch',
+    '\u030C':'Rising contour',
+    '\u0302':'Falling contour',
+    '\u1DC8':'Peaking (rising-falling) contour',
+    '\u1DC9':'Dipping (falling-rising) contour',
+    '\u1DC4':'Mid-rising contour',
+    '\u1DC5':'Low-rising contour',
+    '\u1DC7':'High-falling contour',
+    '\u1DC6':'Mid-falling contour',
+    
+    '˥':'High pitch§Tone_letter#Chao_tone_letters_(IPA)',
+    '˦':'Half high pitch§Tone_letter#Chao_tone_letters_(IPA)',
+    '˧':'Mid pitch§Tone_letter#Chao_tone_letters_(IPA)',
+    '˨':'Half low pitch§Tone_letter#Chao_tone_letters_(IPA)',
+    '˩':'Low pitch§Tone_letter#Chao_tone_letters_(IPA)',
+    
+    /*
+    '\u030F':'Voicelessness',
+    '\u030F':'Voicelessness',
+    '\u030F':'Voicelessness',
+    '\u030F':'Voicelessness',
+    '\u030F':'Voicelessness',
+    */
+    }
+
---
+++ apps/ipa_analyser/script.js
@@ -0,0 +1,41 @@
+
+
+
+function showIPAPhone (str) { 
+	// look for a Wikipedia page that describes the phoneme input
+        
+    searchitem = str.normalize("NFD")
+    
+    //console.log('Looking up '+searchitem)
+    founditems = []
+    
+    var out = ''
+            
+    out += `<table id="ipaAnalyseTable">`
+    
+    var searchArray = [... searchitem]
+    for (i=0;i<searchArray.length;i++) {
+        if (symbols[searchArray[i]]) {
+            var names = symbols[searchArray[i]].split('§')
+            var name = names[0]
+            if (typeof names[1] !== 'undefined') var webpage = names[1]
+            else webpage = names[0]
+            out += `<tr><th>${ searchArray[i] }</th><td><a href="https://en.wikipedia.org/wiki/${ webpage.replace(/ /g,'_') }" target=_blank>${ name }</a></td></tr>`
+            }
+        }
+    
+    out += `</table>`
+    
+    
+    document.getElementById('out').innerHTML = out
+	}
+
+
+
+
+function openUniViewWindow () {
+	var chars =  document.getElementById('in').value
+	var analyse = window.open('../../../uniview/index.html?charlist='+	encodeURIComponent(chars), 'analyse') 
+	analyse.focus()
+	}
+
