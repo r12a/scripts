3a61720895820dfabb58d304f0f9bc57c318c237
r12a <ishida@w3.org>
2024-04-09 19:24:57 +0100

    sinh-si: Feb template update.  Add maps.

--- sinh/block-globals.js
+++ sinh/block-globals.js
@@ -8,4 +8,5 @@ window.detailsfileList = ['si'] // first part of xx-details for each orthography
 window.scriptISO = 'sinh'
 window.webfonts = ["Noto Serif Sinhala WF"]
 window.defaultSize = '24px'
+hideBlockName = 'SINHALA'
 
--- sinh/si-examples.js
+++ sinh/si-examples.js
@@ -15,10 +15,6 @@ native | meaning | IPA① | transcription | other transcriptions② | notes | wi
 */
 
 
-otherTranscription = true   // kicks in the latest order of items: indicates that this uses a format that includes other transcriptions in the position shown above – this affects the assignments for the code, and i think also affects lookup for script notes that show both unvocalised and vocalised
-termListType = 3			// picks up sources automatically
-
-
 
 
 
@@ -26,83 +22,130 @@ autoExpandExamples.si = `
 
 @ https://en.wiktionary.org/wiki/Index:Sinhala
 
+
+-ය||␣||
 අංක||
 අක්මාව|liver||akmāwa
 අක්‍කා||
-අකුර||
+අකුර|letter (alph.)|a.§ku.§ra|akura
+අගය|value, price|a.§ɡə.§jə|agaya|
 අඟ|horn|a§ⁿga
 අට||
+අටුව||␣||
 අඩුව පවතිනවා||
 අණ්ඩ|egg|a§ɳ§ɖa
 අණ්ඩය|egg||aṇḍaya
 අණුව||
-අඬ|sound|a§ⁿɖa
+අඬ|sound|a§ⁿɖa|aⁿḍa
 අත|hand|a§ta
 අතන|there (emphatic)|a§ta§na
 අත්අඩංගුවට||
+අතුරනවා||␣||
 අතුල්ලනවා|to rub|atullanawā
 අදිනවා|to pull||adinawā
 අන්ත්‍රාව||
+අනුරාධපුර||␣||
+අඳුර|dusk, gloom|a§ⁿdu§rə|aⁿdura|
+අප්පා||␣||
 අප්පොච්චි|father||appocci
 අපි|we (1pl)|api
 අපිරිසිදු|dirty||apirisidu
 අම්මා|mother||ammā
 අම්ලකර||
+අඹ|mango|a.§ᵐbə|aᵐba|
 අය්‍යා|older brother||ayyā
 අයිස්||
 අයු කල්||
 අයු||
 අර|that (distal, gender neutral)|a§ra
+අරනවා||␣||
 අරයා|he, she, it (3sg)(distal, gender neutral)||arayā
 අරයාලා|they (3pl)(distal, gender neutral)||arayālā
 අරහේ|there||arahē
-අර්‍තාපල්||
+අර්‍තාපල්|potato|a§r§§t̪aː§pa§l|artāpal
 අල්ලනවා|to hold||allanawā
 අලියා||
-අලුත්|new|a§lu§t
+අලුත්|new|a§lu§t|alut
+අවට|a surrounding|a.§wə.§ʈə|awaṭa|
 අවියක්||
 අවුරුද්ද|year||awurudda
-අවුරුද්‍ද||
+අවුරුද්‍ද||␣
 අශ්වය||
 අසල|near||asala
 අස්ථි||
+අහනවා||␣||
 අහස|sky||ahasa
-අළු||
+අළු||␣
+ආජන්ටිනාව|Argentina|aː.§d͡ʒə§n̪.§ʈi.§n̪aː.§wə|ājanṭināwa|
+ආදෙස|command, order|aː.§de.§sə|ādesa|
 ආන්ද්ර ප්රදේශ්||
 ආයුබෝවන්||
 ආරෝග්‍යශාලා||
 ආලෝකය||
 ආවරණය||
 ඇඟිල්‍ල||
+ඇඟිලි|finger, toe|æ§ᵑɡi§li|æⁿgili|
 ඇතැම්|other, some|æ§tæ§m
+ඇදුරු|teacher, instructor|æ.§du.§ru|æduru|
+ඇඳ|bed|æ.§ⁿdə|æⁿda|
 ඇපල්||
 ඇවිදිනවා|to walk||æwidinawā
 ඇස|eye||æsa
 ඇහෙනවා|to hear||æhenawā
+ඈ|she|æː|ǣ|
 ඉංග්‍රීසි|English||iṁgrīsi
 ඉගිලෙනවා|to fly||igilenawā
+ඉඟුරු|ginger|i§ᵑɡu§ru|iⁿguru|
+ඉඩම|place, situation|i§ɖə§mə|iḍama|
 ඉදිමෙනවා|to swell||idimenawā
 ඉදිරිය||
 ඉන්දියාව||
 ඉඳගන්නවා|to sit||iⁿdagannawā
+ඉඳිනවා|to plant|i.§ⁿdi.§nə.§ʋaː|iⁿdinawā|
 ඉර||
+ඉරනවා||␣||
 ඉරු|sun|i§ru
 ඉස|head|i§sa
+ඉස්සර|before, previous|i§s.§sə.§rə|issara|
+ඊජිප්තුව|Egypt|iː§d͡ʒi§pə§t̪u§wə|ījiptuwa|
+ඊයේ|yesterday|iː§jeː|īyē|
 උකුණා|snake, louse||ukuṇā
+උකුණු|bug|u.§ku.§nu|ukuṇu|
+උගන්නවා||␣||
+උණු||␣||
 උණුසුම්|warm||uṇusum
+උතුරනවා||␣||
+උතුරු පළාත||␣||
+උතුරු මැද පළාත||␣||
+උතුරු|north|u§t̪u§ru|uturu|
+උදුන||␣||
+උඳුන|kiln|u.§ⁿdu.§nə|uⁿduna|
+උපන්|born|u.§pə§n|upan|
+උපුරනවා||␣||
 උඹ|you (2sg)(informal)|u§ᵐba
 උඹලා|you (2pl)(informal)||uᵐbalā
 උරනවා|to suck||uranawā
+උළු||␣||
+ඌ|it|uː|ū|
+ඌව පළාත|Uwa Province|uː§ʋə§‿§pa§ɭaː§tə|ūwa paḷāta|
+ඌව|Uwa (province)|uː§ʋa|ūwa|
 එක|one||eka
+එකතු|united|e.§kə.§tu|ekatu|
+එක්වෙනවා||␣||
+එක්සත් ජනපදය||␣||
+එක්සත් රාජධානිය||␣||
+එකානූව|ninety-one|e.§kaː.§nuː.§wə|ekānūwa|
 එතන|there (emphatic)||etana
+එදිරිය|emnity|e§d̪i§ri§jə|ediriya|
 එනවා|to come||enawā
-එයා|he, she, it (3sg)(anaphoric, gender neutral)||eyā
-එයාලා|they (3pl)(distal/anaphoric, gender neutral)|eyālā
+එයා|he, she, it||eyā
+එයාලා|they|eyālā
 එල||
 එහේ|there||ehē
-ඒ|that(distal/anaphoric, gender neutral)||ē
+ඒ|that||ē
 ඔක්කොම|all||okkoma
 ඔටුවා||
+ඔද|nectar|u.§də|oda|
 ඔබ|you (2sg)(formal)||oba
 ඔබතුමා|you (2sg)(high-level honorific)||obatumā
 ඔබතුමාලා|you (2pl)(high-level honorific)||obatumālā
@@ -114,97 +157,139 @@ autoExpandExamples.si = `
 ඔව්හු|they (3pl)||owhu
 ඔවුන්|they (3pl)||owun
 ඔවුහු||
+ඔසවනවා|to lift up|o.§sə.§wə.§nə.§waː|osawanawā|
 ඔහු|he, she, it (3sg)||ohu
 ඔහේ|you (2sg)(dial. neutral)||ohē
 ඔහේලා|you (2pl)(dial. neutral)||ohēlā
-ඕස්ට්‍රේලියාව||
-කකුල|foot, leg||kakula
+ඕස්ට්‍රේලියාව|Australia||ōsṭrēliyāwa
+කකුල|foot, leg|␣|kakula
+කකුළුවා|crab|ka§ku§ɭu§waː|kakuḷuwā|
 කට|mouth||kaṭa
 කටු|bone||kaṭu
+කටුපිල||␣||
+කඩ||␣||
+කඩනවා||␣||
 කණිනවා|to dig||kaṇinawā
 කතා කරනවා|to say||katā karanawā
 කතුර||
 කන|ear|ka§na
 කනවා|to eat||kanawā
 කන්ද|mountain|ka§n§da
-කපනවා|to cut||kapanawā
+කනිනවා||␣||
+කපනවා|to cut|␣|kapanawā
+කපු||␣||
+කම||␣||
 කඹය|rope||kaᵐbaya
+කඹුරා|artisan|ka.§ᵐbu.§raː|kaᵐburā|
 කරත්තය||
 කරනවා||
+කරවළ||␣||
 කල්මීකියා||
 කවදා|when||kawadā 
 කවුරු|who||kawuru
 කහ|yellow||kaha
+කහිනවා||␣||
 කළු|black||kaḷu
 ක්‍ය්‍ර|Kyra|
 ක්‍රිමියාව|worm||krimiyāwa
 ක්‍රොඒෂියාව|Croatia||kroēṣiyāwa
 කාමරංගා||
 කාමරය||
+කාර||␣||
 කාර්‍ය්‍යාලය|the office|
 කාලගුණ||
 කැටි කරනවා|to freeze||kæṭi karanawā
+කැනඩාව||␣||
 කැන්බරා||
+කැඳනවා||␣||
 කැරකැවෙනවා|to turn (intransitive)||kærakæwenawā
+කිණු||␣||
 කියනවා|to say||kiyanawā
+කිරනවා||␣||
 කිරි||
 කීයට|when||kīyaṭa 
 කුඩා|small||kuḍā
+කුණ්ඩලිය|paragraph mark|ku§n§§ɖə§li§jə|kuṇḍaliya|
+කුඹලා||␣||
+කුඹුරුවෙල්||␣||
+කුරුණෑගල|Kurunegala|ku§ru§næː§ɡa§lə|kuruṇǣgala|
 කුරුල්ලා|bird|ku§ru§l§laː|kurullā
 කුරුල්‍ලා|bird|ku§ru§l§laː|kurullā|with ZWJ
+කුලි||␣||
 කුලුණ||
+කුළු|spicy hot|ku.§ɭu|kuḷu|
+කෘෂ්ණ|black; Krishna|kri§s§ɳa|kr̥ṣṇa|
 කෙටි|short||keṭi
 කෙස්|hair||kes
 කෙසෙල්||
 කෙළගහනවා|to spit||keḷagahanawā
+කෙළිනවා||␣||
 කෙළින්|straight||keḷin
 කේරළ||
+කොකුම්||␣||
+කොටනවා||␣||
 කොණ්ඩය|hair||koṇḍaya
 කොතන|where (emphatic)||kotana
+කොතඹුරු||␣||
+කොර|lame, limping|ko.§rə|kora|
 කොරියාව||
 කොස්ස||
 කොහේ|where||kohē
 කොහොම|how||kohoma
 කොහොමද||
 කොළ|green||koḷa
-කොළඹ||
+කොළඹ||␣
 කොළේ|leaf||koḷē
 කෝටුව|stick||kōṭuwa
 කෝපි||
+කෝවිල|hindu temple|koː§wi§lə|kōwila|
 ගඟ|river||gaⁿga
-ගණිනවා|to count||gaṇinawā
+ගඩුව||␣||
+ගණිනවා|to count|␣|gaṇinawā
 ගන|thick||gana
+ගන්නවා||␣||
 ගම||
 ගමරාළ||
 ගයනවා|to sing||gayanawā
 ගල|stone||gala
 ගලනවා|to flow||galanawā
-ගස|tree||gasa
+ගස|tree|␣|gasa
 ගස්||
-ගහනවා|to hit||gahanawā
+ගහනවා|to hit|␣|gahanawā
+ගළපනවා||␣||
+ග්‍රන්ථ|poetical stanza|ɡra§n.§tə|grantha|
 ග්‍රහලෝක||
-ගාල්ල||
+ගාල්ල||␣
 ගැට ගසනවා|to tie||gæṭa gasanawā
+ගැටය||␣||
+ගැළ||␣||
 ගෑනි|woman||gǣni
+ගිගුරනවා||␣||
+ගිගුරුම|thunder|ɡi.§ɡu.§ru.§mə|giguruma|
 ගිටාරය||
-ගිතෙල්||
+ගිතෙල්||␣
 ගින්දර||
 ගින්‍න||
 ගිනි අවියක්||
 ගිනි|fire||gini
 ගිරි|mountain||giri
+ගුමුව||␣||
 ගුවන් යානයක්||
-ගූ||
-ගූථ||
+ගුළිය|ball, pill|ɡu.§ɭi.§jə|guḷiya|
+ගූ||␣
+ගූථ||␣
 ගෙන්දගම්||
+ගෙනෙනවා||␣||
 ගෙය||
 ගොටුකොල||
 ගොඩක්|many||goḍak
+ගොතනවා||␣||
+ගෝණිය|sack, cloth bag|ɡoː.§n̪i.§jə|gōṇiya|
 චතුරිකා||
 චර්‍මය|skin, hide||carmaya
 චරිතය||
 චීන||
-චීනය||
+චීනය|China|t͡ʃiː§n̪ə§jə|cīnaya
 ජනේලය||
 ජලය||
 ජීවත් වෙනවා|to live||jīwat wenawā
@@ -212,58 +297,79 @@ autoExpandExamples.si = `
 ජීවිතය||
 ඤාණ|Jnana||ɲāna
 ඥාන|knowledge||ᵑǰāṅa
+ටකනවා|to heed|ʈa.§kə.§nə.§waː|ṭakanawā|
 ටිකක්|few||ṭikak
-ඪාකා||
+ඪාකා|Dhaka||ḍhākā
+තණ||␣||
 තණකොළ|grass||taṇakoḷa
 තත්පරය||
+තන||␣||
 තනේ|breast||tanē
 තප්පුලනවා|to blow||tappulanawā
 තමුසේ|you (2sg)(informal)||tamusē
 තමුසේලා|you (2pl)(informal)||tamusēlā
+තඹර||␣||
 තරු|star||taru
 තරුණ||
-තරුව||
+තරුව|||
 තල්ලුව|to push||talluwa
-තාත්තා|father||tāttā
+ත්‍රිකුණාමලය||␣||
+තාච්චිය|frying pan|taː§t̚.§t͡ʃi.§jə|tācciya|
+තාත්තා|father|t̪aː§t̪ə§t̪aː|tāttā
 තැතිගන්නවා|to fear||tætigannawā
 තියුණු|sharp (as a knife)||tiyuṇu
 තිරුවනංතපුරම්||
 තුන|three||tuna
+තුර්කිය||␣||
+තුවක්කුව||␣||
+තුවරා||␣||
 තුළ|in||tuḷa
 තෙත්|wet||tet
 තේ||
 තොපි|you (2pl)(derogatory)||topi
 තෝ|you (2sg)(derogatory)||tō
 දකිනවා|to see||dakinawā
-දකුණු|right||dakuṇu
+දකුණු පළාත||␣||
+දකුණු|right|␣|dakuṇu
+දඩය||␣||
 දඩයම් කරනවා|to hunt||daḍayam karanawā
 දණ||
 දණහිස||
-දත|tooth||data
+දඬු|stick, staff|da.§ᶯɖu|daⁿḍu|
+දත|tooth|␣|data
 දන හිස|knee||dana hisa
-දන්නවා|to know||dannawā
+දන්නවා|to know|␣|dannawā
+දමනවා||␣||
+දවනවා||␣||
+දවල||␣||
 දවස|day||dawasa
 දහය||
 දහස||
 දා|day||dā
-දැන්||
+දැන්|now|dæ§n|dæn
 දැව||
-දිග|long||diga
-දිල්ලිය||
+දිග|long|di.ɡə|diga
+දිය||␣||
+දිල්ලිය||␣
 දිව|tongue (organ)||diwa
 දිවයින||
 දිවුල්||
-දුම|smoke||duma
+දුනු||␣||
+දුම|smoke|␣|duma
 දුර|far||dura
+දුරු||␣||
 දුව||
+දුවනවා||␣||
 දූවිල්ල||
 දෙක|two||deka
-දෙනවා|to give||denawā
+දෙනවා|to give|de.§nə.§waː|denawā
 දෙවියා||
 දේවදූතයා||
 දොර||
+දොළ|desire|do.§ɭə|doḷa|
 නං|if||naṁ
 නංගි||
+නගනවා||␣||
 නගරය||
 නදිය||
 නම|name||nama
@@ -271,10 +377,13 @@ autoExpandExamples.si = `
 නම්|if||nam
 නරක|bad||naraka
 නව දිල්ලිය||
+නව සීලන්තය||␣||
 නවය||
 නහය|nose||nahaya
+නැගෙනහිර පළාත||␣||
+නැගෙනහිර||␣||
 නැහැ|not||næhæ
-නිදනවා|to sleep||nidanawā
+නිදනවා|to sleep|ni.§ⁿdə.§nə.§waː|nidanawā
 නිදා ගන්නවා|to sleep||nidā gannawā
 නියපොතු|fingernail||niyapotu
 නිල්||
@@ -283,76 +392,131 @@ autoExpandExamples.si = `
 නිළි||
 නුඹ|you (2sg)(honorific)||nuᵐba
 නුඹලා|you (2pl)(honorific)||nuᵐbalā
+නුවර||␣||
+නෙරනවා||␣||
 නො-|not||no-
 නෝනා|wife||nōnā
+පටන්|first, beginning|pə.§ʈən|paṭan|
 පටුයි|narrow||paṭuyi
-පණුවා|worm|| paṇuwā
+පණුවා|worm|pa.§nu.§waː|paṇuwā
+පන||␣||
+පනාව||␣||
 පය|foot||paya
 පරණ|old||paraṇa
+පරිප්පු|dhal, pulse|pə§ri§pə§pu|parippu|
 පලතුරු|fruit||palaturu
+පල්ල||␣||
+පල්ලල|pond|pa§l.§la.§la|pallala|
 පවසනවා|to say||pawasanawā
+පවුර||␣||
 පස|dust||pasa
 පස්ස|back||passa
 පහ|five||paha
 පළවෙනි|first||paḷaweni
+පළාත||␣||
+ප්රේත|ghost|p§reː.§tə|prēta|
 ප්‍රභාසංශ්ලේෂණය||
+ප්‍රාචීරය|fence, wall|p§§raː§t͡ʃiː§rə§jə|prācīraya|
+පාකිස්ථානය|Pakistan|paː.§ki§s.§t̪aː.§n̪ə.§jə|pākisthānaya|
 පාන්||
 පාර|road||pāra
 පාවෙනවා|to float||pāwenawā
+පැණි|syrup, honey|pæ.§ni|pæṇi|
 පැය||
 පැරිස්||
 පෑන||
-පිට|guts||piṭa
+පිට|guts|␣|piṭa
+පිටි|flour|pi.§ʈi|piṭi|
 පිය|father||piya
 පියඹනවා|to fly||piyaᵐbanawā
 පියයුරුව|breast||piyayuruwa
 පියාපත්|wing||piyāpat
 පිරිමියා|man (adult male)||pirimiyā
+පිලි||␣||
 පිසනවා|to wipe||pisanawā
 පිහාටු|feather||pihāṭu
 පිහිනනවා|to swim||pihinanawā
 පිහියකින් අනිනවා|to stab||pihiyakin aninawā
+පීනනවා||␣||
 පුංචි|small|||puṁci
 පුක||
 පුච්චනවා|to burn||puccanawā
 පුතා||
 පුර|full||pura
+පුරුදු|accustomed, practised|pu.§ru.§du|purudu|
 පුළුල්|wide||puḷul
 පූති|rotten||pūti
-පූසා||
+පූසා|cat|puː§saː|pūsā
+පෙඳ|bird's tail|pe.§ⁿdə|peⁿda|
+පෙර||␣||
+පේ||␣||
 පොකුණ||
 පොත||
 පොතු|bark (of a tree)||potu
 පොළොව|earth||poḷowa
+පෝච්චිය||␣||
+බංගලාදේශය|Bangladesh|bəm̃.§ɡə.§laː.§d̪eː.§ʃə.§jə|baṁgalādēśaya|
 බටහිර||
+බටු||␣||
 බඩ|belly||baḍa
 බණ්ඩක්කා||
+බත||␣||
 බත්||
-බර|heavy||bara
+බදනවා||␣||
+බදුල්ල||␣||
+බඳිනවා|to tie, bind|bə.§ⁿdi.§nə.§waː|baⁿdinawā|
+බප||␣||
+බබලනවා|to shine, glitter|ba.§bə.§lə.§nə.§waː|babalanawā|
+බඹ||␣||
+බර|heavy|ba.§rə|bara
+බල||␣||
 බලනවා|to see||balanawā
 බලය||
 බල්ලා|dog||ballā
+බස්නාහිර පළාත||␣||
+බස්නාහිර|western, west|ba§s§naː§hi§ra|basnāhira|
 බළලා||
-බැංකුව||
+බ්‍රසීලය||␣||
+බානවා||␣||
+බැංකුව|bank (fin.)||bæṁkuwa
+බැහැර|outer, exterior|bæ.§hæ.§rə|bæhæra|
 බිත්තරය|egg||bittaraya
 බිම|earth||bima
+බිරි||␣||
+බිහිරි|deaf|bi.§hi.§ri|bihiri|
 බීජ|seed||bīja
 බීර||
+බුද්ධි|wisdom, understanding|bud.§di|buddhi|
+බෙක|frog|be.§kə|beka|
 බෙදා ගන්නවා|to split||bedā gannawā
+බෙරය||␣||
 බෙල්ල|neck||bella
 බෙල්ලා||
 බෙලි||
-බොනවා|to drink||bonawā
+බොනවා|to drink|bo.§nə.§waː|bonawā
+බොබ්||␣||
 බොහොම|many||bohoma
+බෝක්කුව|arch|boː§k̚.§ku.§wə|bōkkuwa|
 බෝට්‍ටුව||
 බෝතලය||
-භාරත්||
+භාරත්|India||bhārat
 භාෂාව||
+භූමිය|the Earth|buː§mi§jə|bhūmiya|
+ම||␣||
 මං|I (1sg)||maṁ
 මඟ|road||maⁿga
+මඩ|mud, mire|ma.§ɖə|maḍa|
+මඩකලපුව||␣||
+මඩිනවා||␣||
+මඩුල්ල||␣||
+මත්ත||␣||
+මධ්‍යම පළාත||␣||
+මධ්‍යම|middle|ma§d§§ja§ma|madhyama|
 මධු||
+මන||␣||
 මම|I (1sg)||mama
 මමම||
+මර||␣||
 මරණ||
 මරණය||
 මරනවා|to kill||maranawā
@@ -360,28 +524,39 @@ autoExpandExamples.si = `
 මල්‍ලි||
 මව|mother||mawa
 මස්|meat||mas
+මහත||␣||
 මහත්තයා|husband||mahattayā
 මහත්මයා||
 මහනවා|to sew||mahanawā
-මහනුවර||
+මහනුවර රාජධානිය||␣||
+මහනුවර||␣
 මානෙල්||
 මාර්ග|ash||mārga
 මාලුව|fish||māluwa
 මාසය||
+මැටි||␣||
 මැරෙනවා|to die||mærenawā
+මිටිය||␣||
+මිතුරු||␣||
+මිදුල||␣||
 මිනිහා|man (human being)||minihā
 මිය යනවා|to die||miya yanawā
 මියනවා|to die||miyanawā
 මිරිකනවා|to squeeze||mirikanawā
 මිලියනය||
+මිශ්‍ර||␣||
+මීගමුව||␣||
 මීදුම|fog||mīduma
 මීයා||
+මුගුර||␣||
 මුදල්||
+මුරුංගා|drumstick|mu§rum§̃ɡaː|muruṁgā|
 මුල|root||mula
 මුහුණ||
 මුහුද||
 මුහුදු|sea||muhudu
 මූණ||
+මෙක්සිකෝව||␣||
 මෙතන|here (emphatic)||metana
 මෙයා|he, she, it (3sg)(proximal, gender neutral)||meyā
 මෙයාලා|they (3pl)(proximal, gender neutral)||meyālā
@@ -394,76 +569,125 @@ autoExpandExamples.si = `
 මොනවා|what||monawā
 මොස්කව්||
 මෝටර රිය||
+යට||␣||
 යතුර||
 යතුරු පැදි||
+යම||␣||
+යහන|bed, couch|ja.§hə.§nə|yahana|
+යාපනය රාජධානිය||␣||
+යාපනය||␣||
 යුද්ධය||
 යෙරෙවාන්||
+යෙළ||␣||
+රකුසා||␣||
 රජ||
-රට||
+රට||␣
+රත්නපුර||␣||
 රතු|red||ratu
 රන්||
-රෑ|night||rǣ
+රළු|rough, cruel|ra.§ɭu|raḷu|
+රාජධානිය|kingdom|raː.§d͡ʒə.§daː.§ni.§jə|rājadhāniya|
+රාජපක්ෂ||␣||
+රැලි||␣||
+රෑ|night|ræː|rǣ
 රිදවනවා|to bite||ridawanawā
+රිදී|silver|ri.§diː|ridī|
+රිදෙනවා|lpainful, aching|ri.§de.§nə.§waː|ridenawā|
+රියඟ|ruddy goose|ri.§jə.§ᵑɡə|riyaⁿga|
+රියන්||␣||
+රුපියල||␣||
+රුව|beauty|ruː§ʋə|ruwa|
+රුවල්||␣||
 රුසියානු||
 රුසියාව||
 ලංකා||
-ලක්ෂය|lakh|ˈla§k§sə§jə
+ලක්ෂය|lakh|ˈla§k§sə§jə|lakṣaya
+ලනවා||␣||
+ලබනවා||␣||
+ලියනවා||␣||
 ලුණු|salt||luṇu
 ලූණු||
 ලූනු||
-ලේ|blood||lē
+ලේ|blood|leː|lē
 ලොකු මීයා||
 ලොකු|big||loku
 ලෝකය|world||lōkaya
 වචනය||
+වට||␣||
 වටය|round||waṭaya
+වණ්ෂ|race|va§n.§sə|waṇṣa|
 වතුර|water||watura
+වදනවා|to bear, bring forth|wa.§də.§nə.§waː|wadanawā|
 වන|forest||wana
 වම|left||wama
 වමනෙ යනවා|to vomit||wamane yanawā
+වයඹ පළාත||␣||
+වයඹ|northwest|ʋa§ja§ᵐba|wayaᵐba|
 වයින්||
+වර||␣||
+වර්ග|class, tribe|ʋə§r.§ɡə|warga|
 වර්ණ||
+වරෙණ්ය|chief, principal|ʋa.§ɾe.§n§jə|wareṇya|
 වලසා||
 වලිගය|tail||waligaya
+වළ||␣||
 ව්‍යාජ|fake|||vyāja
 වාතය||
 වැටෙනවා|to fall||wæṭenawā
+වැඩ||␣||
 වැතිරෙනවා|to lie (as in a bed)||wætirenawā
 වැලි|cloud, sand||wæli
 වැස්ස|rain||wæssa
 විකිපීඩියා||
+විදුලිය|lightning|vi.§du.§li.§jə|widuliya|
 වියළි|dry||wiyaḷi
 විල|lake||wila
 විල්||
 විශාල||
+විෂ්ණු|Vishnu|ʋi§s§ɳu|wiṣṇu|
 විස්ස||
+වී|paddy|wiː|wī|
 වීසි කරනවා|to throw||wīsi karanawā
+වෙනවා|to become|ʋe.§nə.§waː|wenawā|
 වේදනාව||
-ශ්‍රී ලංකා|Sri Lanka||śrī laṁkā
+ශ්වාස|air, wind|s§waː.§sə|śwāsa|
+ශ්‍රී ජයවර්ධනපුර කෝට්ටේ||␣||
+ශ්‍රී ලංකා|Sri Lanka|ʃ§riː§ §ləm̃§kaː|śrī laṁkā
 ශ්‍රී ලංකාව||
+ශ්‍රී|sir, lord|s§riː|śrī|
 ශාක||
 ශීතකරණය||
+ශුද්ධ|clean, pure|su§d̚§də|śuddha|
 සටන් කරන්න|to fight||saṭan karanna
 සත්‍තු||
 සතා|animal||satā
 සතිය||
+සදනවා||␣||
 සන්දේශය||
 සඳ||
 සබන්||
+සබරගමුව පළාත||␣||
+සබරගමුව||␣||
 සමග|with||samaga
 සමහර||
 සමාජ||
 සයුර|sea||sayura
 සර්පයා||
-සල්ෆර්||
+සලනවා||␣||
+සල්ෆර්|sulfur||salfar
+සවුදි අරාබියාව||␣||
 සහ||
 සාගරය||
 සාමය||
-සිංහල|Sinhala||siŋhala
+සාහිත්‍යය||␣||
+සිංහය|lion|siŋ§hə§jaː|siṁhayā|
+සිංහයා|lion|siŋ§hə§jaː|siṁhayā|
+සිංහල|Sinhala|siŋ.§hə.§lə|siŋhala
 සිකුරාදා||
-සිඛු||
+සිඛු|disciple||sikhu
 සිගරෙට්ටුව||
 සිටිනවා|to live, stand||siṭinawā
+සිත||␣||
 සිනා||
 සිනිඳු|smooth||siniⁿdu
 සියඹලා||
@@ -473,31 +697,46 @@ autoExpandExamples.si = `
 සීනි||
 සීනුව||
 සුදු|white||sudu
+සුබ|good fortune|su.§bə|suba|
+සුරුට්ටුව|cheroot|s̪u§ɾu§ʈ§ʈu§ʋə|suruṭṭuwa|
+සුලුප්පුව||␣||
 සුළඟ|wind||suḷaⁿga
 සූරනවා|to scratch||sūranawā
+සෙය||␣||
+සෙරෙප්පුව||␣||
 සෙල්ලම් කරනවා|to play||sellam karanawā
+සොබන||␣||
 සෝදනවා|to wash||sōdanawā
+හකුරු||␣||
 හත||
 හතර|four||hatara
 හදවත|heart||hadawata
 හඳ|moon||haⁿda
+හපනවා||␣||
 හමනවා|to breathe||hamanawā
 හම්|skin||ham
+හම්බන්තොට||␣||
 හය||
 හල්මිල්ල||
+හලාවත||␣||
 හා|and||hā
+හානවා|to plough|haː.§nə.§waː|hānawā|
 හැන්ද||
 හැම|all||hæma
+හිටිනවා||␣||
 හිත|heart||hita
 හිතනවා|to think||hitanawā
 හිනාවෙනවා|to laugh||hināwenawā
 හිම|snow||hima
 හිමකැට|ice||himakæṭa
 හීන් ගොටුකොල||
+හුළු||␣||
+හෙණ||␣||
+හෙළනවා||␣||
 හොඳ|good||hoⁿda
+හොවිනවා||␣||
 ළමයා|child||ḷamayā
 ළය|heart||ḷaya
-
 
 
 
