93c5e5ad52d6963eb5088c67350bc44ba6437d8f
r12a <ishida@w3.org>
2024-04-26 08:32:05 +0100

    nod: Enlarge term list

--- lana/nod-examples.js
+++ lana/nod-examples.js
@@ -15,116 +15,203 @@ native | meaning | IPA① | transcription | other transcriptions② | notes | wi
 */
 
 
-otherTranscription = true   // kicks in the latest order of items: indicates that this uses a format that includes other transcriptions in the position shown above – this affects the assignments for the code, and i think also affects lookup for script notes that show both unvocalised and vocalised
-termListType = 3			// picks up sources automatically
-
 
 
 
 autoExpandExamples.nod = `
 
 @ https://en.wikipedia.org/wiki/Northern_Thai_language
+@ https://en.wiktionary.org/w/index.php?title=Category:Northern_Thai_terms_with_IPA_pronunciation
 
 ᨠᩋᩯ᩠ᨶ|fingerroot|
 ᨠᩕᩣ᩠ᨷ|to prostrate oneself|kʰàː§p§/xàːp|||x
+ᨠᩖᩣ᩶|brave|ka᷇ː
 ᨠ᩠ᩅᩣ᩠ᨦ|deer|kwaːŋ|kwaang
-ᨠ᩠ᩅᩣ᩠᩶ᨦ|wide, spacious|kwaːŋ˥˧|kwa᷇ang||ᨠ᩠ᩅ᩶ᩣ᩠ᨦ
+ᨠ᩠ᩅᩣ᩠᩶ᨦ|wide, spacious|kwaːŋ˥˧|kwa᷇ang||ᨠ᩠ᩅ᩶ᩣ᩠ᨦ
+ᨠ᩠ᩅ᩶ᩣ᩠ᨦ||␣||
 ᨠᩡᩋᩯ᩠ᨶ|fingerroot|
 ᨠᩢᩬᩃᨼ᩺|golf|kɔ§l§f
+ᨠᩣ||␣||
+ᨠᩣᨠᩯ||␣||
 ᨠᩣᨼᩯ|coffee|kaːfɛ
 ᨠᩣ᩠ᨯ|market|kaːt³
 ᨠᩣ᩠ᩁ|work|ka§n¹
+ᨠᩣᩴ᩵ᩃᩢ᩠ᨦ|currently|ka.§lāŋ|||x
+ᨠᩣᩴ᩵ᩃᩢ᩠ᨦᩉᩡ|currently|ka.§lāŋ.§hà|||x
 ᨠᩥ᩠ᨶ|eat|ki§n¹
+ᨠᩥ᩠ᨶᨴᩮᩥᩬᩡ|please eat|kǐn tɤ᷇ʔ|||x
+ᨠᩥ᩠ᨶᩃᩯ᩵|eat!|kǐn§lɛ̂ː|||x
 ᨠᩨ|a hundred million|kɯː
+ᨠᩫ᩠ᨷ||␣||
+ᨠᩯᩡ|sheep|kɛ̌ʔ
+ᨠᩯ᩵|to be old|kɛ̀ː
 ᨠᩱ᩵|chicken|kaj˨˨
+ᨡᩣ᩶|I, me|xa᷇ː
+ᨡᩣ᩶ᨻᩕᨧᩮᩢᩣ᩶|I, me (v formal)|xa᷇ː.§pʰà.§t͡ɕa᷇w|||x
+ᨡᩣ᩶ᩉᩥ᩠ᩅ|i'm hungry|xa᷇ː§hǐw|||x
+ᨡᩧ᩠᩶ᨶ|to go up|xɯ᷇n
+ᨡᩬᨦᨠᩥ᩠ᨶ|food|xɔ̌ː§ŋ.§kǐ§n
+ᨡᩬᩴᨠᩥ᩠ᨶᨧᩥ᩠᩵ᨾ|may i please eat?|xɔ̌ː§kǐ§n§t͡ɕì§m|||x
+ᨡᩮᩢᩣ|they, them|xǎw|||x
 ᨡᩮᩢᩣ᩶|rice|kʰaw⁵
 ᨡᩮᩢᩣ᩶ᨡᩬᨦ|belongings|xo²§xɔ§ŋ|||x
-ᨡᩮᩢᩬᩣ᩠᩶᩻ᨦ|belongings|xo²xɔ§ŋ|||x
 ᨡᩮᩢᩣ᩶ᨧᩲ|understand||khaw chai||ᨡᩮᩢ᩶ᩣᨧᩲ
+ᨡᩮᩢᩬᩣ᩠᩶᩻ᨦ|belongings|xo²xɔ§ŋ|||x
+ᨡᩮᩢ᩶ᩣ||␣||
 ᨡᩱ᩵|egg||khai
-ᨡᩣ᩶|I, me|xa᷇ː
 ᨢ᩠ᩅᨯ|bottle||khwad
+ᨣᩕᩢ᩠ᨷ|xap|male polite particle|||x
+ᨣᩕᩪᩉᩣ᩶ᨤᩫ᩠ᨶ|five teachers||||x
+ᨣᩕᩲ᩵|want, desire|xâj
+ᨣᩕᩲ᩵ᩉ᩠ᩅᩫ|laugh|xâj.§h§ǔa
 ᨣ᩠ᩅᩣ᩠ᨿ|buffalo|k§waː§j²
+ᨣᩢ᩠᩶ᨶ|to try|ká§n
+ᨣᩤᩴᨾᩮᩨᩬᨦ|Northern Thai language|kam˧.§mɯːə§ŋ˧
+ᨣᩤ᩵|value|kâː
+ᨣᩧ᩠ᨯ|think|kɯ́t
 ᨣᩮᩢᩤ|eldest|ko⁵
 ᨣᩴ|then|kɔː
+ᨣᩴ᩵|question particle|kɔ̀ː
+ᨤ᩠ᩅᩁᨧᩢ|should|x§ūa§n.§t͡ɕa|||x
 ᨤ᩠ᩅᩣ᩠ᨿ|water buffalo|xwaːj˦
-ᨤᩢ᩠ᨶᨤᩣ᩠ᨠ|toad||khankhak
+ᨤᩢ᩠ᨶᨤᩣ᩠ᨠ|toad|␣|khankhak
 ᨤᩥ᩠ᨦ|you|xī§ŋ
+ᨤᩨ᩠ᨶ|to return|xɯ̄ːn
 ᨤᩫ᩠ᨶ|human, person|xon˧˧|khon
 ᨥᩁᩣᩅᩤᩈ|lay person||khalawasa
+ᨥᩰᩇᨱᩣ|advert|xôːt.§sa.§nāː
 ᨦᩪ|snake|ŋuː˦|ngu
+ᨦᩬ᩵ᩁ|nape|ŋɔ̂n|||x
+ᨦᩮᩥ᩠ᨶ|silver|ŋɤ̄n
 ᨧ᩠ᨷᩪ|kiss|cuː§p³
+ᨧᩥ᩠᩵ᨾ|imperative particle|t͡ɕìm|||x
+ᨧᩩ|lie|t͡ɕúʔ|||x
+ᨧᩫ᩠ᨶ|to be poor|t͡ɕǒn
+ᨧᩮᩢ᩠ᨯ|seven|t͡ɕě§t|||x
+ᨧᩮᩢᩣ᩶|master, lord|t͡ɕa᷇w|||x
+ᨧᩮᩢ᩶ᩣ||␣||
+ᨧᩰᩫ᩠ᩁ|thief|t͡ɕǒːn
 ᨧᩲ|heart|t͡ɕaj|jai
+ᨩ᩠ᨿᨦ||␣||
+ᨩ᩠ᨿᨦᩁᩣ᩠ᨿ|Chiang Rai|t͡ɕia§ŋ.§hāː§j|chiang-rai
 ᨩ᩠ᨿᨦᩉ᩠ᨾᩲ᩵|Chiang Mai|t͡ɕīəŋ.màj|chiang-mài
+ᨩᩣ᩠᩶ᨦ|elephant|t͡ɕaːŋ|cang||ᨩ᩶ᩣ᩠ᨦ
+ᨩᩥ᩠᩶ᨶ|meat|t͡ɕín|||x
 ᨩᩨ᩵|to be named|cɯː⁴
 ᨩᩬᨦ|bed|tɕɔŋ
+ᨩᩬ᩶ᩁ|spoon|t͡ɕɔ́ː§n|||x
 ᨩᩲ᩶|use|t͡ɕáj
-ᨩᩣ᩠᩶ᨦ|elephant|t͡ɕaːŋ|cang||ᨩ᩶ᩣ᩠ᨦ
+ᨪᩣ᩠ᩅ|twenty|sāːw|||x
+ᨪᩰᩬᩡ|to search|sɔ́ʔ
 ᨬᩥ᩠ᨦ|woman||nying
+ᨯᩢ᩠ᨦ|nose|dāŋ|||x
+ᨯᩢᩬ|flower|dɔːk
+ᨯᩢᩬᩡ|flower|dɔːk³
 ᨯᩣ᩠ᩅ|star|daː§w˧˧
 ᨯᩥ᩠ᨶ|earth, dirt|din˧˧|din
 ᨯᩦ|good||di
-ᨯᩢᩬ|flower|dɔːk
-ᨯᩢᩬᩡ|flower|dɔːk³
+ᨯᩬᨠᨾᩱ᩶||␣||
 ᨯᩮᩬᩥᩁ|moon|
-ᨯᩱ᩶|did|daj⁵
+ᨯᩰ᩠ᨶ|passive particle|dōː§n||x
+ᨯᩱ᩶|did £££££ past marker|daj⁵ £££££ da᷇j
+ᨲ᩠ᩅᩫᨾᩮᩥᩬᨦ|Lanna script|t§ǔa.§mɯ̄a§ŋ
 ᨲᩣ᩠ᩃ|palmyra||dtaan
-ᨲᩪ|we, us||tǔu
-ᨲᩮᩢᩣ᩵|turtle, tortoise||tao
 ᨲᩣ᩠᩵ᨦ|to be different|taː§ŋ
 ᨲᩣ᩠᩵᩻ᨦ|different (in my view)|taːŋtaː§ŋ
+ᨲᩥ|to criticize|tǐʔ
+ᨲᩦ|to cut|tǐː
+ᨲᩪ|we, us||tǔu
+ᨲᩫ᩠ᩅ|you|tǔa|||x
+ᨲᩫ᩠ᩅᨠᩮᩢᩣ᩵|oneself|tǔa.kàw|||x
+ᨲᩬ᩶ᨦ|must|tɔ᷇ː§ŋ
+ᨲᩮᩢᩣ᩵|turtle, tortoise||tao
 ᨳ᩠ᨶᩫ᩻ᩁ|path|tʰa§no§n|||x
 ᨳᩣ᩶|wait|tʰâː
 ᨳᩣ᩶ᩴ|cave|tʰam˦˦ʔ
+ᨳ᩶ᩣᩴ||␣||
 ᨴ᩠ᨿᩁ|candle||tian
+ᨴ᩠ᨿᩅ|walk|tīa§w|||x
 ᨴ᩠ᩅ᩶ᨦ|to move, stir|
 ᨴᩢ᩠ᨦ|all|taŋ²
 ᨴᩤ|to smear, paint|taː˦|ta
+ᨴᩤ᩠ᨦ|way|tāːŋ|||x
+ᨴᩤ᩠᩵ᨶ|he, she, you|tâː§n|||x
+ᨴᩩᨠᪧ|every|tu§k⁶§tuk⁶|||x
+ᨴᩩᨧᩮᩢᩣ᩶|monk|tu᷇.§t͡ɕa᷇w|||x
+ᨴᩪᨲ||␣||
 ᨴᩮᩅ|god|
+ᨴᩮᩥᩬᩡ|imperative particle|tɤ᷇ʔ
 ᨴᩱ᩠ᨿ|Thailand|tai§‹
+ᨵᨾ᩠ᨾ᩺|dharma|tʰā§m§–|||x
+ᨶᩢ᩠ᨠ|much, many|na᷇§k|||x
 ᨶᩣᩴ|lead|nam
-ᨶᩫ᩠ᨠ|bird|nok˦˥|nok
 ᨶᩣ᩶ᩴ|water||nam||ᨶ᩶ᩣᩴ
+ᨶᩧ᩠᩵ᨦ|one|nɯ̂ŋ|||x
+ᨶᩫ᩠ᨠ|bird|nok˦˥|nok
+ᨷᩕᨴᩮ᩠ᩆ|country|pʰa.§têː§t|||x
+ᨷᩕᩤᩈᩣ᩠ᨴ|palace|pʰǎː.§sàːt|||x
 ᨷᩢᨱ᩠ᨻᨷᩩᩁᩩᩇ|disciple||||x
 ᨷᩤ᩠ᨷ|sin, evil|bàap
 ᨷᩤ᩠ᨸ|sin, evil|bàa§p
+ᨷᩤ᩠᩶ᨶ|village, home|ba᷇ːn|||x
 ᨷᩴ᩵|no|bɔ̀ː
 ᨸᩖᩣ|fish|pa
+ᨸᩣ᩠᩶ᨿ|sign|pa᷇ːj
 ᨸᩦ|year|piː
 ᨸᩦ᩵ᨩᩩᨾ|flute ensemble|piːʨum
 ᨸᩮ᩠ᨶ|to be|pě§n
 ᨹᩫ᩠ᨾ|hair||phom
+ᨹᩬᩴ᩵|to look|pʰɔ̀ː
+ᨺᩢ᩠ᨶ|to dream|fǎ§n
+ᨺᩣ᩠ᨶ|to slice|fǎːn
 ᨻᩕᩥ᩠ᨠ|chilli||||x
+ᨻᩖᩪ||␣||
 ᨻ᩠ᩅᩢ|group|puak⁴§‹|||x
-ᨻᩬᩴ᩵ᨾᩯ᩵|parents|pɔː⁴mɛː⁴|||x
+ᨻᩣ᩠᩵ᨿ|to lose|pâːj
 ᨻᩬᩴ᩵|father|pɔ̂ː|||x
+ᨻᩬᩴ᩵ᨾᩯ᩵|parents|pɔː⁴mɛː⁴|||x
 ᨻᩮᩥ᩠᩵ᨶ|he, she|pɤn˥˩|poen
+ᨻᩮᩥ᩠᩵ᨶᨠᩥ᩠ᨶᩓ᩠ᩅ|he has eaten|pɤ̂n§kǐ§n§lɛ́ːw|||x
+ᨻᩮᩥ᩠᩵ᨶᨯᩱ᩶ᨠᩥ᩠ᨶ|he ate|pɤ̂§n§da᷇j§kǐ§n|||x
 ᨻᩮᩥᩬ᩵ᩁ|friend||puean||ᨻᩮᩬᩥ᩵ᩁ
 ᨻᩯ᩠ᨦ|beloved|pɛːŋ
 ᨻᩱ|to go|pāj|pai
 ᨼᩢ᩠ᨶ|tooth|fa§n|fan
+ᨼᩣ᩶|sky; lime||faa||ᨼ᩶ᩣ
 ᨼᩥᩃ᩠ᨾ᩺|film|fi§–§m²
 ᨼᩱ|fire||fai
 ᨼᩱᨼᩣ᩶|electricity||||ᨼᩱᨼ᩶ᩣ
-ᨼᩣ᩶|sky; lime||faa||ᨼ᩶ᩣ
+ᨼ᩶ᩣ||␣||
 ᨽᩣᩇᩣ|language|pʰāː.§sǎː||alternative form
 ᨽᩣᩈᩣ|language|pʰāː.§sǎː
+ᨾ᩠ᩅ᩵ᩁ|funny, amusing|ᨾ᩠ᩅ᩵ᩁ|||x
+ᨾ᩠ᩅ᩵ᩁᨣᩤ|it's fun, right?|||x
 ᨾᩢ᩠ᨠ|to like|ma᷇§k
+ᨾᩢ᩠ᨶ|he, she, it|mān|||x
 ᨾᩢᩣ|a lot|maːk⁴
 ᨾᩣ|to come|maː˧˧|ma
+ᨾᩣ᩠ᨯ||␣||
+ᨾᩣ᩶|horse|maː˦˥|ma||ᨾ᩶ᩣ
 ᨾᩦ|to have|mīː
 ᨾᩦ᩠ᨯ|knife|mîː§t
 ᨾᩧ᩠ᨦ|you|mɯ̄§ŋ
+ᨾᩮᩥ᩠ᨶ|to delay|mɤː̄n
 ᨾᩮᩥᩬᨦ|city, country|mɯa§ŋ˧˧|myyang
-ᨾᩮᩬᩥᨦ|city, country|mɯa§ŋ˧˧|myyang
 ᨾᩮᩥᩬ᩵ᩋ|time|mɯa˥˩|mûea||ᨾᩮᩬᩥ᩵ᩋ
+ᨾᩮᩬᩥᨦ|city, country|mɯa§ŋ˧˧|myyang
+ᨾᩮᩬᩥ᩵ᩋ||␣||
 ᨾᩯ᩠ᩅ|cat||maew
 ᨾᩯ᩵|mother|mɛ̂ː
 ᨾᩳ|drunk|mɔː
-ᨾᩣ᩶|horse|maː˦˥|ma||ᨾ᩶ᩣ
-ᨿᩡ|do|ɲa᷇ʔ
+ᨾ᩶ᩣ||␣||
 ᨿ᩠ᨿᩮᩡ|do|ɲiaʔ
+ᨿᩡ|do|ɲa᷇ʔ
+ᨿᩣ᩠ᨠ|difficult|ɲâː§k|||x
+ᨿᩣ᩠ᩅ|long|ɲāːw|||x
 ᨿᩩᨦ|mosquito|ɲū§ŋ
+ᩀᩣ|medecine|jāː|yaa
+ᩀᩣ᩠ᨠ|desire|jàːk|||x
+ᩀᩣ᩠᩵ᨦ|manner, way|jàːŋ|||x
 ᩁᨭᩛᨷᩣ᩠ᩃ|government
 ᩁ᩠ᩅᩣ᩠ᨿᨲᩕᩦ|night||lwaithi
 ᩁᩢ|love|hak⁶
@@ -132,166 +219,90 @@ autoExpandExamples.nod = `
 ᩁᩣ|mold|hāː|ha
 ᩁᩪ᩶|to know|húː
 ᩁᩬ᩶ᩁ|hot|hɔ́ː§n|||x
+ᩁᩮᩢᩣ|we, us|hāw|||x
 ᩁᩮᩥᩬᩋ|boat, ship|
+ᩃᩡᩋᩬ᩵ᩁ|child|la.§ʔɔ̀ːn
+ᩃᩡᩋᩬ᩵ᩁᪧ|children|la.§ʔɔ̀ː§n§la.ʔɔ̀ːn
 ᩃᩢ᩠ᨠ|steal|la᷇k
+ᩃᩢᩪ|offspring|luː§k⁴
 ᩃᩣ|donkey|laː˦
 ᩃᩣ᩠ᨠ|drag|lâːk
+ᩃᩣ᩠᩶ᨶᨶᩣ|Lanna|láːn.naː|láan-naa||ᩃ᩶ᩣ᩠ᨶᨶᩣ
 ᩃᩪᨠ|offspring|luː§k⁴
-ᩃᩢᩪ|offspring|luː§k⁴
 ᩃᩫ᩠ᨾ|wind|lom˧˧|lom
 ᩃᩫ᩠᩵ᨶ|run|lô§n
+ᩃᩬᨦ|to try|lɔ̄ːŋ
 ᩃᩮᩢᩣ|beautiful|lāw
 ᩃᩮᩢᩣ᩵|tell (story)|lâw
 ᩃᩮᩢᩣ᩶|coop, pen|láw
 ᩃᩮᩥᩢ᩠ᨠ|deep|lɜ§k
 ᩃᩮᩥᩬᨠ|to choose|
-ᩃᩣ᩠᩶ᨶᨶᩣ|Lanna|láːn.naː|láan-naa||ᩃ᩶ᩣ᩠ᨶᨶᩣ
+ᩃᩮᩬᩥᨠ||␣||
+ᩃᩯ᩵|imperative particle|lɛ̂ː|||x
+ᩃ᩶ᩣ᩠ᨶᨶᩣ||␣||
+ᩅᨯ᩠ᨰᨶᨵᨾ᩠ᨾ᩺|culture|wa᷇§t.§tʰa.§na᷇ʔ.§tʰā§m§–|||x
 ᩅ᩠ᨿᨦ|walled city||wiang
-ᩅᩬᩴ᩶|rabies|
+ᩅᩬᩴ᩶|rabies|␣
 ᩈᨱᩛᩣ᩠ᨶ|shape
 ᩈᨳᨷᩢ᩠ᨶ‍ᩁᩣᨩᨽᩢ᩠ᨮ‍ᨩ᩠ᩀᨦ‍ᩉ᩠ᨾᩱ᩵|Chiang Mai College of Further Education
 ᩈᨻᩛ|omniscience||sabba
 ᩈ᩠ᩅ᩠ᨿ|beautiful|s§ua§i
+ᩈ᩠ᩅᩢᩁ᩠ᨣ᩺||␣||
 ᩈᩣ᩠ᨾ|three|saː§m¹
 ᩈᩣᩴᨶ᩠ᨿᨦ|accent|
+ᩈᩥ᩠ᨷ|ten|sǐ§p|||x
+ᩈᩩᨯ|rearmost|sǔt
+ᩈᩪᨡᩮᩢᩣ|you|sǔː.xǎw|||x
+ᩈᩪᨧᩮᩢᩣ᩶|you|sǔː.t͡ɕa᷇w|||x
+ᩈᩪᨯ|to inhale|sùːt
 ᩈᩮᩓ᩠ᩅ᩶|already|seː¹§lɛː§w⁶
 ᩈᩮᩥᩬ᩵ᩋ|mat||||ᩈᩮᩬᩥ᩵ᩋ
 ᩉᩖᩋᩯ᩠ᨶ|fingerroot|
 ᩉᩖᩡᩋᩯ᩠ᨶ|fingerroot|
 ᩉᩖᩢᨠ|post|lǎ§k
 ᩉᩖᩣ᩠ᨠ|differ from others|làː§k
+ᩉᩖᩮᩢᩣ|sharpen|lǎw
+ᩉᩖᩮᩢᩣ᩵|forest|làw
+ᩉᩖᩮᩢᩣ᩶|alcohol £££££ liquor|la᷇w|||x
+ᩉ᩠ᨶᩣ᩶ᨧᩢ|likely to|na᷇ː.§t͡ɕa|||x
 ᩉ᩠ᨶᩧ᩵ᨦ|one|›§nɯ§ɲ³
-ᨶᩧ᩠᩵ᨦ|one|nɯ̂ŋ|||x
 ᩉ᩠ᨾᩣ|dog|maː˨˦|maa
-ᩉ᩠ᨾᩲ᩵|new|maj˨˩|mài
 ᩉ᩠ᨾᩣᩴ᩵|eat|mam|||x
+ᩉ᩠ᨾᩪ᩵ᨹᩫ᩠ᨾ|we|mùː§pʰǒm|||x
+ᩉ᩠ᨾᩪ᩵ᩁᩮᩢᩣ|emphasised we|mùː§hāw
+ᩉ᩠ᨾᩪ᩵ᩉ᩠ᨾᩣ|the dogs|mùː§mǎː|||x
+ᩉ᩠ᨾᩲ᩵|new|maj˨˩|mài
 ᩉ᩠ᩃᩋᩯ᩠ᨶ|fingerroot|
 ᩉ᩠ᩃᩡᩋᩯ᩠ᨶ|fingerroot|
 ᩉ᩠ᩅᩫ|head||hua
 ᩉᩢ᩠ᨶ|to see|han˨˦
+ᩉᩣ᩶|five|ha᷇ː|||x
 ᩉᩨ᩶|give|hɯ᷇ː
-ᩉᩩᨾ|to like||hum
-ᩉᩖᩮᩢᩣ|sharpen|lǎw
-ᩉᩖᩮᩢᩣ᩵|forest|làw
-ᩉᩖᩮᩢᩣ᩶|liquor|la᷇w
+ᩉᩩᨾ|to like|␣|hum
+ᩉᩪ||␣||
+ᩉᩫ᩠ᨠ|six|hǒk|||x
+ᩉᩮ᩠ᨿ|imperative particle|hǐa|||x
 ᩉᩯ᩵ᩉᩯ᩠ᨶ|to walk in a procession|
 ᩉᩱ᩵|hit|hai
 ᩋᨾᩛ|mango
+ᩋᩉ᩠ᨿᩢᨦ|what|ʔa.§–§ɲǎ§ŋ|||x
+ᩋᩣᩉᩣ᩠ᩁ|food|ʔāː.§hǎː§n|||x
+ᩋᩣ᩠ᨧᨧᩢ|might, may|ʔàːt.§t͡ɕa|||x
+ᩋᩣ᩠᩶ᨿ|older brother|ʔa᷇ːj|||x
 ᩋᩧ᩠ᨷ|starve|ɯ§p²
 ᩋᩪ᩶|to speak|ʔu᷇ː|ʼuu
 ᩋᩬᨠᩋᩬᩴ|brain||||ᩋᩬᨠᩋᩴᩬ
-ᩋᩯ᩠᩵ᩅ|travel|ʔɛ̀ː§w
-ᩌᩣᩴ|sprinkle|ham|||x
-ᩑᨠ|one|eː§k³
-ᩓ|and||lae
-᪓᩠ᨴ|three times|saːm§tiː
-ᨣᩤᩴᨾᩮᩨᩬᨦ|Northern Thai language|kam˧.§mɯːə§ŋ˧
-ᨲ᩠ᩅᩫᨾᩮᩥᩬᨦ|Lanna script|t§ǔa.§mɯ̄a§ŋ
-
-
-
-ᨺᩣ᩠ᨶ|to slice|fǎːn
-ᨺᩢ᩠ᨶ|to dream|fǎ§n
-ᨲᩦ|to cut|tǐː
-ᨲᩥ|to criticize|tǐʔ
-ᩈᩪᨯ|to inhale|sùːt
-ᩈᩩᨯ|rearmost|sǔt
 ᩋᩮ᩠ᨶ|to recline|ʔēːn
 ᩋᩮᩢ᩠ᨶ|tendon|ʔēn
-ᨠᩯ᩵|to be old|kɛ̀ː
-ᨠᩯᩡ|sheep|kɛ̌ʔ
-ᨤᩨ᩠ᨶ|to return|xɯ̄ːn
-ᨡᩧ᩠᩶ᨶ|to go up|xɯ᷇n
-ᨾᩮᩥ᩠ᨶ|to delay|mɤː̄n
-ᨦᩮᩥ᩠ᨶ|silver|ŋɤ̄n
-ᨧᩰᩫ᩠ᩁ|thief|t͡ɕǒːn
-ᨧᩫ᩠ᨶ|to be poor|t͡ɕǒn
-ᩃᩬᨦ|to try|lɔ̄ːŋ
-ᨪᩰᩬᩡ|to search|sɔ́ʔ
-ᨴᩮᩥᩬᩡ|imperative particle|tɤ᷇ʔ
-ᩃᩡᩋᩬ᩵ᩁ|child|la.§ʔɔ̀ːn
-ᩃᩡᩋᩬ᩵ᩁᪧ|children|la.§ʔɔ̀ː§n§la.ʔɔ̀ːn
-ᩉ᩠ᨾᩪ᩵ᩁᩮᩢᩣ|emphasised we|mùː§hāw
-ᨣᩕᩪᩉᩣ᩶ᨤᩫ᩠ᨶ|five teachers||||x
-ᨡᩬᨦᨠᩥ᩠ᨶ|food|xɔ̌ː§ŋ.§kǐ§n
-ᩋᩣᩉᩣ᩠ᩁ|food|ʔāː.§hǎː§n|||x
-ᨻᩣ᩠᩵ᨿ|to lose|pâːj
-ᨸᩣ᩠᩶ᨿ|sign|pa᷇ːj
-ᨹᩬᩴ᩵|to look|pʰɔ̀ː
-ᨷᩕᨴᩮ᩠ᩆ|country|pʰa.§têː§t|||x
-ᨷᩕᩤᩈᩣ᩠ᨴ|palace|pʰǎː.§sàːt|||x
-ᨥᩰᩇᨱᩣ|advert|xôːt.§sa.§nāː
-ᩅᨯ᩠ᨰᨶᨵᨾ᩠ᨾ᩺|culture|wa᷇§t.§tʰa.§na᷇ʔ.§tʰā§m§–|||x
-ᨵᨾ᩠ᨾ᩺|dharma|tʰā§m§–|||x
-ᨣᩕᩢ᩠ᨷ|xap|male polite particle|||x
-ᨲᩬ᩶ᨦ|must|tɔ᷇ː§ŋ
-ᨲᩫ᩠ᩅᨠᩮᩢᩣ᩵|oneself|tǔa.kàw|||x
-ᨠᩖᩣ᩶|brave|ka᷇ː
-ᨣᩤ᩵|value|kâː
-ᨣᩕᩲ᩵ᩉ᩠ᩅᩫ|laugh|xâj.§h§ǔa
-ᨣᩧ᩠ᨯ|think|kɯ́t
-ᨣᩕᩲ᩵|want, desire|xâj
-ᨣᩢ᩠᩶ᨶ|to try|ká§n
-ᨣᩴ᩵|question particle|kɔ̀ː
-ᩋᩉ᩠ᨿᩢᨦ|what|ʔa.§–§ɲǎ§ŋ|||x
-ᨿᩣ᩠ᨠ|difficult|ɲâː§k|||x
-ᨿᩣ᩠ᩅ|long|ɲāːw|||x
-ᩀᩣ|medecine|jāː|yaa
-ᩀᩣ᩠ᨠ|desire|jàːk|||x
-ᩀᩣ᩠᩵ᨦ|manner, way|jàːŋ|||x
-ᨷᩤ᩠᩶ᨶ|village, home|ba᷇ːn|||x
-ᩉᩣ᩶|five|ha᷇ː|||x
-ᨧᩮᩢᩣ᩶|master, lord|t͡ɕa᷇w|||x
-ᩉᩖᩮᩢᩣ᩶|alcohol|la᷇w|||x
-ᨪᩣ᩠ᩅ|twenty|sāːw|||x
-ᩋᩣ᩠᩶ᨿ|older brother|ʔa᷇ːj|||x
-ᨦᩬ᩵ᩁ|nape|ŋɔ̂n|||x
-ᨯᩢ᩠ᨦ|nose|dāŋ|||x
-ᩋᩯ᩠᩵ᩅ|visit, travel|ʔɛ̀ːw|||x
-ᨩᩥ᩠᩶ᨶ|meat|t͡ɕín|||x
-ᨶᩢ᩠ᨠ|much, many|na᷇§k|||x
-ᨴ᩠ᨿᩅ|walk|tīa§w|||x
-ᨾ᩠ᩅ᩵ᩁ|funny, amusing|ᨾ᩠ᩅ᩵ᩁ|||x
-ᨧᩩ|lie|t͡ɕúʔ|||x
-ᨴᩩᨧᩮᩢᩣ᩶|monk|tu᷇.§t͡ɕa᷇w|||x
-ᩉᩫ᩠ᨠ|six|hǒk|||x
-ᨧᩮᩢ᩠ᨯ|seven|t͡ɕě§t|||x
-ᩈᩥ᩠ᨷ|ten|sǐ§p|||x
-ᨩ᩠ᨿᨦᩁᩣ᩠ᨿ|Chiang Rai|t͡ɕia§ŋ.§hāː§j|chiang-rai
-ᨩᩬ᩶ᩁ|spoon|t͡ɕɔ́ː§n|||x
-ᨴᩤ᩠ᨦ|way|tāːŋ|||x
-ᨡᩣ᩶ᨻᩕᨧᩮᩢᩣ᩶|I, me (v formal)|xa᷇ː.§pʰà.§t͡ɕa᷇w|||x
-ᩁᩮᩢᩣ|we, us|hāw|||x
-ᨲᩫ᩠ᩅ|you|tǔa|||x
-ᩈᩪᨡᩮᩢᩣ|you|sǔː.xǎw|||x
-ᩈᩪᨧᩮᩢᩣ᩶|you|sǔː.t͡ɕa᷇w|||x
-ᨾᩢ᩠ᨶ|he, she, it|mān|||x
-ᨡᩮᩢᩣ|they, them|xǎw|||x
-ᨴᩤ᩠᩵ᨶ|he, she, you|tâː§n|||x
-ᩉ᩠ᨾᩪ᩵ᨹᩫ᩠ᨾ|we|mùː§pʰǒm|||x
-ᩉ᩠ᨾᩪ᩵ᩉ᩠ᨾᩣ|the dogs|mùː§mǎː|||x
-ᨴᩮᩥᩬᩡ|imperative particle|tɤ᷇ʔ|||x
-ᨠᩥ᩠ᨶᨴᩮᩥᩬᩡ|please eat|kǐn tɤ᷇ʔ|||x
-ᩉᩮ᩠ᨿ|imperative particle|hǐa|||x
-ᨧᩥ᩠᩵ᨾ|imperative particle|t͡ɕìm|||x
-ᨡᩬᩴᨠᩥ᩠ᨶᨧᩥ᩠᩵ᨾ|may i please eat?|xɔ̌ː§kǐ§n§t͡ɕì§m|||x
-ᩃᩯ᩵|imperative particle|lɛ̂ː|||x
-ᨠᩥ᩠ᨶᩃᩯ᩵|eat!|kǐn§lɛ̂ː|||x
-ᨾ᩠ᩅ᩵ᩁᨣᩤ|it's fun, right?|||x
-ᨤ᩠ᩅᩁᨧᩢ|should|x§ūa§n.§t͡ɕa|||x
-ᩉ᩠ᨶᩣ᩶ᨧᩢ|likely to|na᷇ː.§t͡ɕa|||x
-ᩋᩣ᩠ᨧᨧᩢ|might, may|ʔàːt.§t͡ɕa|||x
-ᨯᩱ᩶|past marker|da᷇j|||x
-ᨻᩮᩥ᩠᩵ᨶᨯᩱ᩶ᨠᩥ᩠ᨶ|he ate|pɤ̂§n§da᷇j§kǐ§n|||x
+ᩋᩯ᩠᩵ᩅ|travel|ʔɛ̀ː§w
+ᩋᩯ᩠᩵ᩅ|visit, travel|ʔɛ̀ːw|||x
+ᩌᩣᩴ|sprinkle|ham|||x
+ᩌᩦᨷᩕᩪ||␣||
+ᩑᨠ|one|eː§k³
+ᩓ|and||lae
 ᩓ᩠ᩅ|already|lɛ́ːw|||x
-ᨻᩮᩥ᩠᩵ᨶᨠᩥ᩠ᨶᩓ᩠ᩅ|he has eaten|pɤ̂n§kǐ§n§lɛ́ːw|||x
-ᨠᩣᩴ᩵ᩃᩢ᩠ᨦ|currently|ka.§lāŋ|||x
-ᨠᩣᩴ᩵ᩃᩢ᩠ᨦᩉᩡ|currently|ka.§lāŋ.§hà|||x
-ᨯᩰ᩠ᨶ|passive particle|dōː§n||x
-ᨡᩣ᩶ᩉᩥ᩠ᩅ|i'm hungry|xa᷇ː§hǐw|||x
+᪓᩠ᨴ|three times|saːm§tiː
 ᪭ᩣ|elephant|t͡ɕaːŋ|cang||ᨩ᩶ᩣ᩠ᨦ
-ᨴᩩᨠᪧ|every|tu§k⁶§tuk⁶|||x
-
 
 
 
