ab785ebbd80acb6e9032c7ce9a9d83da5f0c382d
r12a <ishida@w3.org>
2024-05-04 17:45:03 +0100

    template update

--- template/block-globals.js
+++ template/block-globals.js
@@ -8,4 +8,4 @@ window.detailsfileList = ['en'] // first part of xx-details for each orthography
 window.scriptISO = 'latn'
 window.webfonts = ["Noto Sans WF"]
 window.defaultSize = '24px'
-
+hideBlockName = "XXXXX"
