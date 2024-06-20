d09b5d9fce678b781ed1f7a94292eac49e0d9df9
r12a <ishida@w3.org>
2024-06-20 08:56:11 +0100

    sd: Banking draft text

--- arab/sd-globals.js
+++ arab/sd-globals.js
@@ -6,10 +6,10 @@ blockLangtag = 'sd' // same as langTag; used in block.html
 blockDirection='rtl'  // ltr/rtl or '' for ltr; used by block.html
 
 blockDirectoryName='arab'  // folder name where block file is found, no slash
-orthogFilePath=''  // path below /scripts, including filename, but no extension, eg. lana/nod
-pickerDir='arab-all'  // directory name for picker, no slash at end
-scriptSummaryTableName=''  // id used for langs record in xx-langdata.js; used to make summary table
-charUsageBCP=''  // bcp tag to find the orthography in the charuse app; used in makeIndexIntro only
+orthogFilePath='arab/sd'  // path below /scripts, including filename, but no extension, eg. lana/nod
+pickerDir='arab-sd'  // directory name for picker, no slash at end
+scriptSummaryTableName='sd'  // id used for langs record in xx-langdata.js; used to make summary table
+charUsageBCP='sd'  // bcp tag to find the orthography in the charuse app; used in makeIndexIntro only
 orthogName = 'Arabic' // natural language name for the orthography; used only in makeIndexIntro
 languageName = 'Sindhi' // natural language name of language; used in various places, esp block
 scriptIsIndic = false  // true if there are indic syllabic categories; used only in makeIndexIntro
@@ -17,6 +17,6 @@ scriptIsIndic = false  // true if there are indic syllabic categories; used only
 
 defaultSize = '24px'  // used by block.html to set size of font, including in font mgt panel
 letterClassName = 'sd'  // classname for a given orthography in the block page
-
+hideBlockName = "ARABIC"
 
 
---
+++ arab/sd-langdata.js
@@ -0,0 +1,13 @@
+// this should always be updated when app-charuse/langs.js is updated
+
+var langs = {
+
+"sd": { name:"Sindhi", silcode:"snd", source:"cldr_sd", region:"sasia", countries:"Pakistan, India", script:"arab", speakers:"25000000", letter:"آابٻپڀتثٺٽٿجھڃڄچڇحخدذڊڌڍڏرزڙسشصضطظعغفڦقکڪگڱڳلمنڻهوي", mark:"ٓ", punctuation:"‰", aux:"ئٔ", local:"سنڌي‎", localtrans:"(sindhi)", 
+orth:`Arabic. &nbsp; Naskh style.`,
+also:
+['sd-deva | deva | In India.',
+'sd-guru | guru | In India.']
+},
+
+
+}
