d55f70b73f5c7bcfd8be7927358eab8c850d64c6
r12a <ishida@w3.org>
2024-06-22 06:05:38 +0100

    az, Experiment to create az-characters page (one of the brief additional locales). Too much work for not much of a result.  Abandoned plan to rework others like this.

---
+++ arab/az-examples.js
@@ -0,0 +1,52 @@
+/*
+native | meaning | IPA① | transcription | other transcriptions② | notes | wiktionary③
+
+
+① separate each text unit (consonant+combining_mark(s)) with §, separate phonetic syllables with .
+   use ‹ and › for a sound that spans 2 text units
+   use – for a text unit that doesn't produce sound
+   syllable boundary markers go inside previous unit, and emphasis markers begin emphasised unit
+
+② the previous transcription field is always Latin. 
+   if there is another key orthography (eg. Mongolian cyrillic) it goes here.
+   vowelled/unvowelled alternatives also go here
+
+③ if Wiktionary spells the word differently, add the spelling here
+*/
+
+
+
+
+
+autoExpandExamples.az = `
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+`
+
--- arab/az-globals.js
+++ arab/az-globals.js
@@ -1,24 +1,21 @@
 
-langTag='az'
-scriptTag='arab'
+
+langTag='az' // BCP47 language tag for this orthography
+scriptTag='arab' // BCP47 script tag for this orthography
 notesLangtag='az'
+blockLangtag = 'az' // same as langTag; used in block.html
+blockDirection='rtl'  // ltr/rtl or '' for ltr; used by block.html
 
-explainerDir=''
-blockDir='arabic/'
-pickerDir='arab-all/'
-scriptSummaryTableName=''
-charUsageBCP=''
-orthogName = 'Arabic'
-indic = false
-
-pageWebfonts = []
-defaultSize = '24px'
+blockDirectoryName='arab'  // folder name where block file is found, no slash
+orthogFilePath='arab/az'  // path below /scripts, including filename, but no extension, eg. lana/nod
+pickerDir='arab-az'  // directory name for picker, no slash at end
+scriptSummaryTableName='az'  // id used for langs record in xx-langdata.js; used to make summary table
+charUsageBCP='az'  // bcp tag to find the orthography in the charuse app; used in makeIndexIntro only
+orthogName = 'Arabic' // natural language name for the orthography; used only in makeIndexIntro
+languageName = 'Azerbaijani' // natural language name of language; used in various places, esp block
+scriptIsIndic = false  // true if there are indic syllabic categories; used only in makeIndexIntro
 
 
-
-// block settings
-
-languageName = 'Azerbaijani'
-letterClassName = 'az'
-lang = 'az'
-direction = 'rtl'
+defaultSize = '24px'  // used by block.html to set size of font, including in font mgt panel
+letterClassName = 'az'  // classname for a given orthography in the block page
+hideBlockName = "ARABIC"
