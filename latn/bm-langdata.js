595c1bfadc4e71320564bfe495d1acec95eeb670
r12a <ishida@w3.org>
2024-07-02 17:14:42 +0100

    latn/bm-langdata update

--- latn/bm-langdata.js
+++ latn/bm-langdata.js
@@ -5,8 +5,7 @@ var langs = {
 "bm": { name:"Bamanan (Bambara, Bamanankan)", local:"bamanankan", silcode:"bam", 
 source:"61711e9f6eb3f930b12ed40b27bf0d8c2c6ee101,cldr_bm,udhr_bam", 
 region:"afr", countries:"Mali", script:"latn", speakers:"4100000", 
-letter:"àáèéìíòóùúěŊŋƆƐƝǎǐǒǔɔɛɲʼ", mark:"̀́", markaux:"̂̌", number:"", punctuation:"«»‘’“”…", symbol:"", other:"",
-aux:"[]§‑–—†‡‰′″‹›",
+letter:"ŊŋƆƐƝɔɛɲʼ", letteraux:"àáèéìíòóùúěǎǐǒǔ", mark:"̀́", markaux:"̂̌", number:"", punctuation:"«»‘’“”…", symbol:"", other:"",
 orth:`[Latin] &nbsp; <a href="../scripts/latin/bm" target="_blank">Details</a>.`,
 also:
 ["nqo | nkoo | Written koiné used by speakers of Maninka, Bambara, Dyula and related dialects."],
