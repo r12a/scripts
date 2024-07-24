210d748260c66a5e825b86ad7c77f1677e97344e
r12a <ishida@w3.org>
2024-07-24 06:37:30 +0100

    ps   Add langdata & index

--- arab/ps-langdata.js
+++ arab/ps-langdata.js
@@ -2,17 +2,47 @@
 
 var langs = {
 
-"ps": { name:"Pashto", local:"سنڌي‎", localtrans:"(sindhi)", silcode:"snd", rtl:true, 
-source:"cldr_sd", 
-region:"sasia", countries:"Pakistan, India", script:"arab", speakers:"25000000", 
-letter:"آابتثجحخدذرزسشصضطظعغفقلمنهويٺٻٽپٿڀڃڄچڇڊڌڍڏڙڦکڪگڱڳڻھہ", mark:"͏", markaux:"َُِٓٔ", number:"۰۱۲۳۴۵۶۷۸۹", punctuation:"؟⁏⹁", symbol:"۽۾", other:"\u061C\u200C\u200D\u200E\u200F\u202A\u202B\u202C\u2066\u2067\u2068\u2069", 
-orth:`Arabic. &nbsp; Naskh style.`,
-also:
-["sd-deva | deva | In India.",
-"sd-guru | guru | In India."],
+"ps": { name:"Pashto (Pushtu, Pushto)", local:"پښتو", localtrans:"Pax̌tó", silcode:"pus", redirect:true, 
+source:"r12a", 
+region:"casia", countries:"Afghanistan, Pakistan", script:"macrolanguage", speakers:"~50000000", 
+letter:"آئابتثجحخدذرزسشصضطظعغفقلمنهويټپځڅچډړږژښکګگڼۀیۍېے", letteraux:"كںە", mark:"͏ً", markaux:"َُِٓٔٙ", number:"۰۱۲۳۴۵۶۷۸۹", punctuation:"«»،؛؟٪٬‘’“”…", symbol:"", other:"\u061C\u200C\u200D\u200E\u200F\u202A\u202B\u202C\u2066\u2067\u2068\u2069", 
+orth:`[Arabic] This entry reflects characters recommended by both Afghan and Peshawar authorities. There exist a small number of regional differences. <a href='../scripts/arab/ps.html' target='_blank'>Details</a>.`,
+related:`ps is a macrolanguage that encompasses the following more specific primary language subtags:<br/>Southern Pashto [pbt]<br/>Northern Pashto [pbu]<br/>Central Pashto [pst].
+Legacy applications often use ps rather than [pbu].
+`,
 type:"abjad", cs:"no", gpos:"yes", gsub:"yes", cursive:"yes", wordsep:"space", baseline:"mid", wrap:"word", hyphenation:"?",  direction:"rtl",  justification:"?", 
-vowels:"ml:4 hcm:3", 
-linked:"arab/sd", fonts:"arab-sd", font:"Scheherazade New", 
+vowels:"let:y ml:y hcm:y", 
+linked:"arab/ps", fonts:"arab-ps", font:"Scheherazade", 
 },
+
+
+"pbt": { name:"Southern Pashto (Pushtu, Pushto)", local:"پښتو", localtrans:"Pax̌tó", silcode:"pus",
+source:"r12a", 
+region:"casia", countries:"Afghanistan", script:"arab", speakers:"~50000000", 
+letter:"آئابتثجحخدذرزسشصضطظعغفقلمنهويټپځڅچډړږژښکگڼیۍې", letteraux:"", mark:"َُِٙ", markaux:"͏ًٓٔ", number:"۰۱۲۳۴۵۶۷۸۹", punctuation:"«»،؛؟٪٬‘’“”…", other:"\u061C\u200C\u200D\u200E\u200F\u202A\u202B\u202C\u2066\u2067\u2068\u2069", 
+orth:`[Arabic] Characters typically used in Afghanistan. <a href='../scripts/arab/ps.html' target='_blank'>Details</a>. `,
+related:`ps is a macrolanguage that encompasses the following more specific primary language subtags:<br/>Southern Pashto [pbt]<br/>Northern Pashto [pbu]<br/>Central Pashto [pst].
+Legacy applications often use ps rather than [pbu].
+`,
+type:"abjad", cs:"no", gpos:"yes", gsub:"yes", cursive:"yes", wordsep:"space", baseline:"mid", wrap:"word", hyphenation:"?",  direction:"rtl",  justification:"?", 
+vowels:"ml:y hcm:y", 
+linked:"arab/ps", fonts:"arab-ps", font:"Scheherazade", 
+},
+
+"pbu": { name:"Northern Pashto (Pushtu, Pushto)", local:"پښتو", localtrans:"Pax̌tó", silcode:"pbu",  rtl:true,
+source:"r12a,cldr_ps", 
+region:"casia", countries:"Pakistan, Afghanistan, UAE", script:"arab", speakers:"20850900", 
+letter:"آئابتثجحخدذرزسشصضطظعغفقلمنهويټپځڅچډړږژښکګڼۀیۍېے", letteraux:"ە", mark:"َُِٙ", markaux:"͏ًٓٔ", number:"۰۱۲۳۴۵۶۷۸۹", punctuation:"«»،؛؟٪٬‘’“”…", other:"\u061C\u200C\u200D\u200E\u200F\u202A\u202B\u202C\u2066\u2067\u2068\u2069", 
+orth:`[Arabic]  &nbsp; Characters typically used in the Peshawar region. Primary usage is naskh style, but nastaliq ([Aran]) also used. <a href='../scripts/arab/ps.html' target='_blank'>Details</a>. `,
+related:`ps is a macrolanguage that encompasses the following more specific primary language subtags:<br/>Southern Pashto [pbt]<br/>Northern Pashto [pbu]<br/>Central Pashto [pst].
+Legacy applications often use ps rather than [pbu].
+`,
+type:"abjad", cs:"no", gpos:"yes", gsub:"yes", cursive:"yes", wordsep:"space", baseline:"mid", wrap:"word", hyphenation:"?",  direction:"rtl",  justification:"?", 
+vowels:"ml:y hcm:y", 
+linked:"arab/ps", fonts:"arab-ps", font:"Scheherazade", 
+},
+
+
+
 
 }
