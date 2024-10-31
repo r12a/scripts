33b96d7a2fd9703d74e1410195f458d3e3aef7bd
r12a <ishida@w3.org>
2024-10-31 07:17:25 +0000

    + Kurukh terms list

---
+++ tols/kru-examples.js
@@ -0,0 +1,154 @@
+/*
+native | meaning | IPA① | transcription | other transcriptions② | notes | wiktionary③
+
+
+① separate each text unit (consonant+combining_mark(s)) with §, separate phonetic syllables with .
+   use ‹ and › for a sound that spans 2 text units
+   use – for a text unit that doesn't produce sound
+   syllable boundary markers go inside previous unit, and emphasis markers begin emphasised unit
+
+② the previous transcription field is always Latin. 
+   if there is another key orthography (eg. Mongolian cyrillic) it goes here.
+   vowelled/unvowelled alternatives also go here
+
+③ if Wiktionary spells the word differently, add the spelling here
+*/
+
+
+
+
+
+autoExpandExamples.kru = `
+
+
+
+@ The Kurukh Language https://www.google.co.uk/books/edition/The_Kurux_Language/v9BCDwAAQBAJ?hl=en&kptab=getbook&gbpv=1
+
+𑶶𑶱𑷑𑷑𑶳|woman|pel.lo
+𑷖𑶵𑶶𑶵|to graze|xaːpaː
+𑶾𑶴𑶶|shutting|ɖʰap
+𑶻𑶲𑷊𑷊𑶵|to push|tuk.kaː
+𑷖𑶱𑷙𑶻𑶵|leaf cup|xeː.taː
+𑶺𑶱𑷙𑶻|husband|meːt
+𑷀𑶵𑷀𑶵|to lick|ʈaːʈaː
+𑷑𑶴𑷀𑶶𑶴𑷀|sticky|laʈ.paʈ
+𑷅𑶰𑷅𑷅|fire|cicc
+𑶵𑷅𑶵|thin|aː.caː
+𑷊𑶲𑷊𑷊|head|kukk
+𑶱𑷙𑷊𑶵|to walk|eː.kaː
+𑶰𑷔𑶻𑷑𑶱𑷙𑷊|from here|ist.leːk
+𑶷𑶴𑷔𑶰𑷏𑶵𑷐|falsehood|pʰa.si.yaːr
+𑷅𑶲𑶷𑶱𑶺|secretly|cu.pʰem
+𑶺𑶵𑶷|pardon|maːpʰ
+𑶼𑶴𑷐𑶰𑷏𑶵|brass plate|tʰa.ri.yaː
+𑶱𑷙𑶼𑷐ʼ𑶵|to be visible|eːtʰr.ʔaː
+𑷑𑶵𑶼|kick|laːtʰ
+𑷁𑶱𑷊𑷐ʼ𑶵|to be stopped|ʈʰekr.ʔaː
+𑷑𑶲𑷁𑶰𑷙|half-burned wood|lu.ʈʰiː
+𑶶𑶱𑷙𑷁|market|peːʈʰ
+𑷆𑶰𑷐𑶰𑷏𑶵𑷐ʼ𑶵|to be dispersed|t͡ʃʰi.ri.yaː.ʔaː
+𑷊𑶴.𑷆𑶲𑷙𑷐|slough|ka.cʰuːr
+𑶶𑶳𑷐𑶳𑷆|half-cooked|po.rocʰ
+𑷋𑶱𑶶𑶶𑶵|village|kʰep.paː
+𑷖𑶴𑷋𑷐ʼ𑶵|to be available|xakʰr.ʔaː
+𑶸𑶳𑷋𑷋|locust|bokkʰ
+𑶸𑶳𑷎𑷌𑷌𑶵|to run away|boŋɡ.ɡaː
+𑷃𑶰𑶸𑶵|money|ɖʰi.baː
+𑶰𑷐𑶰𑶸|two (persons)|i.rib
+𑶽𑶰𑷌𑷕𑶵|long|dig.haː
+𑶶𑶵𑶽𑶵|root|paː.daː
+𑷖𑶴𑶽𑶽|child|xadd
+𑷂𑶵̃𑷗𑶵|branch|ɖãː.ɽaː
+𑶽𑶳𑷂𑶳𑷐-𑶸𑶳𑷂𑶳𑷐|dirty|so.ɖor-bo.ɖor
+𑷖𑶱𑷂𑷂|food|xeɖɖ
+𑷇𑶳𑷊𑷊|a few|d͡ʒokk
+𑷖𑶳𑷙𑷇𑶵|to collect|xoː.d͡ʒaː
+𑷖𑶴𑷇𑷇|dirt|xajj
+𑷌𑶳𑷑𑷑𑶴𑷔|landlord|ɡol.las
+𑷅𑶰𑷌𑶵|seedling|ci.ɡaː
+𑶿𑶱𑷙𑷌|rite|neːɡ
+𑶹𑶴𑶰𑷏𑶳𑷔|brother|bʰai.jos
+𑶹𑶵𑶹𑶿𑶵|conjecture|bʰaːbʰ.naː
+𑷑𑶴𑶹-𑷑𑶴𑶹|voraciously|labʰ-labʰ
+𑶾𑶴𑷐ʼ𑶵|to hold|dʰar.ʔaː
+𑷑𑶳𑶾𑶴𑷐|name of a tribe|lo.dʰar
+𑶸𑶲𑶾|Wednesday|budʰ
+𑷃𑶰̇𑷊𑶰𑷙|pounding machine|ɖʰiŋ.kiː
+𑷂𑶰𑷃𑷌𑶴𑷐|intrepid|ɖiɖʰ.ɡar
+𑷂𑶰𑷃|courage|ɖiɖʰ
+𑷈𑶴𑷐𑶿𑶵|spring|d͡ʒʰar.naː
+𑶺𑶲̃𑷙𑷈𑶲𑷐ʼ𑶵|to wash one's face|mũː.d͡ʒʰur.ʔaː
+𑷐𑶰𑷈|joy|rid͡ʒʰ
+𑷍𑶵̃𑷔𑶰𑷙|grass|ɡʰaː.siː
+𑶺𑶵𑷍𑶱𑷙|name of a month|maː.ɡʰeː
+𑷈𑶳𑷎𑷍|pitfall|d͡ʒʰ§o§ŋ§ɡʰ
+𑶶𑶴̃𑷏𑷏𑶵|cold|p§a§ɲ.§ɲ§aː
+𑶿𑶰𑷙𑶿|you|n§iː§‹§n
+𑶳𑶿-𑶵|to drink|o§n§-§aː
+𑶺𑶳𑷙𑷖𑶵|to eat|m§oː.§‹§x§aː
+𑷖𑶴𑶺𑶰𑷙|spear-grass|x§a.§m§iː§‹
+𑶱𑷙𑶺|we|eː§‹§m
+𑷀𑶲𑷉𑶵̃|small parrot|ʈ§u.§ɲ§ãː
+𑷀𑶳̇𑷊𑶵|openfield|ʈ§oŋ.§k§aː
+𑷕𑶳𑶻𑶴𑷎𑷌|it seems|h§o.§t§a§ŋ§ɡ
+𑷐𑶱𑶻ʼ𑶵|to file|r§e§t.§ʔ§aː
+𑶴𑷐𑶰𑷙|small saw|a.§r§iː
+𑷖𑶱𑷙𑷐|chicken|x§eː§‹§r
+𑶵𑷗𑶰𑷙|ridge|aː.§ɽ§iː§‹
+𑶲𑷎𑷌𑶲𑷗|a while|u§ŋ.§ɡ§u§ɽ
+𑶿𑶳𑷙𑷘𑶳Rʼ𑶵|to wash oneself|n§oː.§‹§ɽʰ§or.§ʔ§aː
+𑷌𑶴𑷘|fort|ɡ§a§ɽʰ
+𑷑𑶵𑷑𑶱|saliva|l§aː.§l§e
+𑷅𑶵𑷑|voice|t͡ʃ§aː§l
+𑷏𑶴𑶰𑶽|memory|j§a§i§d
+𑷖𑶵𑷏𑶵|to dry|x§aː.§y§aː
+𑷊𑶰𑷏𑷏𑶵|below|k§i§j.§j§aː
+𑶶𑶱𑷐𑷒𑶵̃|pigeon|p§e§r.§w§ãː
+𑷀𑶴𑷕𑷀𑶴𑷕𑶴𑷒𑷒𑶵|very bright|ʈ§a§h.§ʈ§a.§h§a§w.§w§aː
+𑷔𑶴𑶳̃𑷔𑶱𑷙|whole|s§a§õ.§s§eː
+𑷊𑶴𑷔𑷔𑶵|scurf|k§a§s.§s§aː
+𑶵𑷔|he|aː§s
+𑶺𑶱𑷙𑷖𑶵|to call|m§eː.§‹§x§aː
+𑶿𑶵𑷖|four|n§aː§x
+𑷕𑶴ʼ𑶵|yes|h§a.§ʔ§aː
+𑶻𑶰𑷕𑶵|inclination|t§i.§h§aː
+𑷅𑶵𑷕|tea|t͡ʃ§aː§h
+
+
+
+@ Wikipedia, Kurukh language https://en.wikipedia.org/wiki/Kurukh_language
+
+𑷔𑶴𑷐𑶴𑷕 𑶻𑶳𑷙𑷗|vowels|s§a§r§a§h§ §t§oː§‹§ɽ
+𑷕𑶴𑷐𑶴𑷕 𑶻𑶳𑷙𑷗|consonants|h§a§r§a§h§ §t§oː§‹§ɽ
+𑷛|auspicious symbol|ũɡɡu
+𑶵̆𑶶𑶰𑷔|office|ɔ§f§i§s
+𑶾𑶴𑶿𑶽𑷕𑶵|astonishment|dʱ§a§n§d.§h§a:
+𑶾𑶴𑶿𑶾𑶵|exertion|dʱ§a§n.§dʱ§a:
+𑶺𑶴𑷈𑷕𑶰𑷙|middle|m§a§dʒʱ.§h§i:§‹
+𑶺𑶴𑷈𑶰𑷔|zamindar's agent|m§a.§dʒʱ§i§s
+𑷊𑶲̃𑷗𑶲𑷖|Kurukh language|kũɽux
+
+
+
+
+
+
+
+
+
+`
+
+
+
+
+// automatically creates Online Resources section, if markup is uncommented in xx.html
+
+termLists = [
+
+{
+"title":"Kudukh Times",
+"url":"https://tolongsiki.com/sites/default/files/2022-04/Kudukh%20times-min_merged.pdf",
+},
+
+]
+
