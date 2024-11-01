1803b85d1fe8d8e48c01cf729aa124804f16ec02
r12a <ishida@w3.org>
2024-11-01 19:42:30 +0000

    + Meroitic termslist

---
+++ mero/xmr-examples.js
@@ -0,0 +1,104 @@
+/*
+native | meaning | IPA① | transcription | other transcriptions② | notes | wiktionary③
+
+
+① separate each text unit (consonant+combining_mark(s)) with §, separate phonetic syllables with .
+   use ‹ and › for a sound that spans 2 text units
+   use – for a text unit that doesn't produce sound
+   syllable boundary markers go inside previous unit, and emphasis markers begin emphasised unit
+
+② the previous transcription field is always Latin. 
+   if there is another key orthography (eg. Mongolian cyrillic) it goes here.
+   vowelled/unvowelled alternatives also go here
+
+③ if Wiktionary spells the word differently, add the spelling here
+*/
+
+
+
+
+
+autoExpandExamples.xmr = `
+
+
+@ https://en.wiktionary.org/wiki/Category:Meroitic_lemmas
+
+𐦠𐦦𐦫|man|a§ba§ra|abr
+𐦠𐦧𐦡𐦷𐦡𐦨𐦲|Apedemak (Kushite warrior god)|a§p§ə§ɾ§ə§ma§ka|apedemk
+𐦠𐦧𐦣𐦵|messenger|a§p§u§t(ə)|apote
+𐦠𐦨𐦩𐦢|Amun|a§ma§n§i|amni
+𐦠𐦨𐦩𐦧|god Amun of Luxor|a§ma§na§pa|amnp
+𐦠𐦩𐦴|priest|a§nna§ta|ant
+𐦠𐦫|Horus|a§ra|ar
+𐦠𐦫𐦣𐦨𐦡|Rome, Italy|a§r§u§m§ə|arome
+𐦠𐦯𐦣𐦫𐦢|Osiris|a§s§u§r§i|asori
+𐦠𐦴|bread|a§ta|at
+𐦠𐦴𐦫𐦢|Hathor|a§ta§r§i|atri
+𐦠𐦶|water|a§tt§u|ato
+𐦤𐦡𐦫|milk|›§ə§ra|yer
+𐦤𐦡𐦶|water|›§ə§tt§u|yeto
+𐦥𐦢𐦷𐦡|brother|w§i§ɾ§ə|wide
+𐦥𐦣𐦯|Isis|w§u§sa|wos
+𐦥𐦬𐦡|dog|wal|wle
+𐦧𐦡𐦫𐦢𐦵|a title|p§ə§r§i§t§–|perite
+𐦧𐦡𐦬𐦨𐦣𐦯|general|p§ə§la§m§u§sa|pelmos
+𐦧𐦡𐦱𐦶|viceroy|p§ə§sə§n§tu|peseto
+𐦧𐦢𐦬𐦡𐦳𐦡|Philae|p§i§l§ə§q§–|pileqe
+𐦧𐦬𐦯𐦩|a title|pa§la§sa§na|plsn
+𐦧𐦮𐦣𐦨𐦡|Egyptian given name|pa§ẖ§u§m§–|pẖome
+𐦧𐦳𐦫|prince|paqara|pqr
+𐦨𐦡𐦷𐦡𐦥𐦢|Meroe|m§ə§ɾ§ə§w§i|medewi
+𐦨𐦬𐦣|good|ma§l§u|mlu
+𐦨𐦯|sun god|ma§sa|ms
+𐦨𐦲|god|ma§ka|mk
+𐦨𐦲𐦷𐦢|goddess|ma§ka§ɾ§i|mkdi
+𐦨𐦴|goddess Mut?|ma§ta|mt
+𐦩𐦣𐦦|slave, Nubian|n§u§ba|nob
+𐦬𐦭|great, big|la§xa|lḫ
+𐦭𐦯|Khonsu?|xan§sa|ḫs
+𐦯𐦢𐦧𐦡𐦯𐦢𐦤𐦡|Egyptian female given name|s§i§p§–§s§i§y§ə|sipesiye
+𐦯𐦴|foot|sa§ta|st
+𐦯𐦵|tutor, mother|sa§tə|ste
+𐦱𐦨|wife|sə§ma|sem
+𐦲𐦢𐦯𐦫𐦢|Caesar|kisari|kisri
+𐦲𐦷𐦢|woman|ka§ɾ§i|kdi
+𐦲𐦷𐦢𐦵|sister|ka§ɾ§i§t§–|kdite
+𐦲𐦷𐦲𐦡|candace, queen|kan§da§k§ə|kdke
+𐦳𐦣𐦫𐦡|ruler, sovereign|q§u§r§ə|qore
+𐦴𐦤𐦡𐦯𐦢|Egyptian female given name|ta§y§ə§s§i|tyesi
+𐦵𐦥𐦢𐦱𐦴𐦢|proscynema|tə§w§i§sə§t§i|tewiseti
+
+
+
+
+
+
+
+`
+
+
+
+
+// automatically creates Online Resources section, if markup is uncommented in xx.html
+
+termLists = [
+
+{
+"title":"Wiktionary, Eastern Cham lemmas",
+"url":"https://en.wiktionary.org/wiki/Category:Eastern_Cham_lemmas",
+},
+
+{
+"title":"Austronesian Basic Vocabulary Database, Phan Rang Cham (Eastern Cham)",
+"url":"https://abvd.eva.mpg.de/austronesian/language.php?id=371&sort=item",
+"notes":"Latin only",
+},
+
+{
+"title":"Living Dictionaries",
+"url":"https://livingdictionaries.app/sora/entries/list",
+"notes":"No native script.",
+},
+
+]
+
