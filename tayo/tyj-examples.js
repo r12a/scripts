e1b03474d86db00f5d2e6a9128f4c37c0f571e1d
r12a <ishida@w3.org>
2024-11-10 07:22:47 +0000

    + Tayo

---
+++ tayo/tyj-examples.js
@@ -0,0 +1,71 @@
+/*
+native | meaning | IPA① | transcription | other transcriptions② | notes | wiktionary③
+
+
+① separate each text unit (consonant+combining_mark(s)) with §, separate phonetic syllables with .
+   use ‹ and › for a sound that spans 2 text units
+   use – for a text unit that doesn't produce sound
+   syllable boundary markers go inside previous unit, and emphasis markers begin emphasised unit
+
+② the previous transcription field is always Latin. 
+   if there is another key orthography (eg. Mongolian cyrillic) it goes here.
+   vowelled/unvowelled alternatives also go here
+
+③ if Wiktionary spells the word differently, add the spelling here
+*/
+
+
+
+
+
+autoExpandExamples.tyj = `
+
+
+
+@ https://www.unicode.org/L2/L2022/22152-yo-lai-tay-script.pdf
+
+𞛆𞛠𞛑|snake skin|t͡ʃaːp|
+𞛛𞛠𞛀|root|haːk|
+𞛖𞛠𞛋|birthmark|maːt
+𞛏𞛠𞛖|thorn|naːm
+𞛆𞛴|perched|t͡ʃap|
+𞛛𞛲|love|hak|
+𞛖𞛳|vermin|mat
+𞛏𞛱|thorn|naːm
+𞛖𞛣|hand|mɨ|
+𞛆𞛣|character|t͡ʃɨ|
+𞛑𞛣𞛏|arrow|pɨn
+𞛔𞛣𞛏|firewood|fɨn
+𞛾|village, country|mɨəŋ|
+
+
+
+
+`
+
+
+
+
+// automatically creates Online Resources section, if markup is uncommented in xx.html
+
+termLists = [
+
+{
+"title":"Wiktionary, Eastern Cham lemmas",
+"url":"https://en.wiktionary.org/wiki/Category:Eastern_Cham_lemmas",
+},
+
+{
+"title":"Austronesian Basic Vocabulary Database, Phan Rang Cham (Eastern Cham)",
+"url":"https://abvd.eva.mpg.de/austronesian/language.php?id=371&sort=item",
+"notes":"Latin only",
+},
+
+{
+"title":"Living Dictionaries",
+"url":"https://livingdictionaries.app/sora/entries/list",
+"notes":"No native script.",
+},
+
+]
+
