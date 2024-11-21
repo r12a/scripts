c4e788ff3a073a868f34f14c14c11ef206d8c9db
r12a <ishida@w3.org>
2024-11-21 08:50:50 +0000

    adlm/fuf-langdata   Add script data

--- adlm/fuf-langdata.js
+++ adlm/fuf-langdata.js
@@ -1,19 +1,36 @@
 // this should always be updated when app-charuse/langs.js is updated
 
+    const Y = true
+    const N = false
+    const P = 'partial'
+    const U = '?'
+
+
+
 var langs = {
 
-"fuf-adlm": { name:"Pular", rtl:true, 
+"fuf-adlm": { name:"Pular", local:"", localtrans:"", silcode:"", rtl:true, 
 source:"9ee024aca4e4745c9b779ef3ee0697c42700d2ca,udhr_fuf_adlm", 
-region:"afr", script:"adlm", speakers:"3000000", 
-letter:"ـ𞤀𞤁𞤂𞤃𞤄𞤅𞤆𞤇𞤈𞤉𞤊𞤋𞤌𞤍𞤎𞤏𞤐𞤑𞤒𞤓𞤔𞤕𞤖𞤗𞤘𞤙𞤚𞤛𞤜𞤝𞤞𞤟𞤠𞤡𞤢𞤣𞤤𞤥𞤦𞤧𞤨𞤩𞤪𞤫𞤬𞤭𞤮𞤯𞤰𞤱𞤲𞤳𞤴𞤵𞤶𞤷𞤸𞤹𞤺𞤻𞤼𞤽𞤾𞤿𞥀𞥁𞥂𞥃𞥋", mark:"𞥊𞥄𞥅𞥆𞥇𞥈𞥉", number:"𞥐𞥑𞥒𞥓𞥔𞥕𞥖𞥗𞥘𞥙", punctuation:"؟“”…⁏⹁𞥞𞥟", symbol:"", other:"\u200C\u200D\u200E\u200F\u202A\u202B\u202C\u2066\u2067\u2068\u2069", 
+region:"afr", countries:"", script:"adlm", speakers:"3000000", 
+letter:"ـ𞤀𞤁𞤂𞤃𞤄𞤅𞤆𞤇𞤈𞤉𞤊𞤋𞤌𞤍𞤎𞤏𞤐𞤑𞤒𞤓𞤔𞤕𞤖𞤗𞤘𞤙𞤚𞤛𞤜𞤝𞤞𞤟𞤠𞤡𞤢𞤣𞤤𞤥𞤦𞤧𞤨𞤩𞤪𞤫𞤬𞤭𞤮𞤯𞤰𞤱𞤲𞤳𞤴𞤵𞤶𞤷𞤸𞤹𞤺𞤻𞤼𞤽𞤾𞤿𞥀𞥁𞥂𞥃𞥋", letteraux:"", mark:"𞥊𞥄𞥅𞥆𞥇𞥈𞥉", markaux:"", number:"𞥐𞥑𞥒𞥓𞥔𞥕𞥖𞥗𞥘𞥙", numberaux:"", punctuation:"؟“”…⁏⹁𞥞𞥟", punctuationaux:"", symbol:"", symbolaux:"", other:"\u{200C}\u{200D}\u{200E}\u{200F}\u{202A}\u{202B}\u{202C}\u{2066}\u{2067}\u{2068}\u{2069}", otheraux:"", aux:"%,;[]§«»ʼ‑–—‘’†‡‰′″‹›", deprecated:"", 
 orth:`[Adlam] &nbsp; Since 1989, in development. <a href="../scripts/adlam/">Details</a>`,
 also:
 ["fuf | latn | ",
 "fuf-arab | arab | Sub-Saharan/Sahelian style."],
 related:`Macrolanguage is Fulah [ff].`,
-type:"alphabet", cs:"yes", gpos:"yes", gsub:"yes", cursive:"yes", direction:"rtl*", wordsep:"space", baseline:"romn", wrap:"word", hyphenation:"yes",  justification:"sp",
-vowels:"let:5 cm:3",
-linked:"adlm/fuf", fonts:"/pickers/adlm/", 
+linked:"adlm/fuf", picker:"adlm", font:"", 
+},
+
+
+"adlm": {name:"Adlam", region:"afr", type:"alpha", 
+blocks:1, chars:88, bletter:69, bmark:7, bnumber:10, bpunctuation:0, bsymbol:0, bother:0, 
+direction:"rtl", rtlnumbers:Y,
+voweltype: { inherent:0, letters:Y, marks:N, hidden:N, visorder:N, vocalics:N, ivowels:N, vbase:"", vcomposite:N, prebase:N, circum:N, },
+case:Y, cursive:Y, mcchars:"m", conjuncts:N, ligs:N, 
+medials:"", finals:"",
+clusters:{ ligation:N, stacks:N, touch:N, conjoined:N, subjoinedcp:N, visviram:N, diacritic:N, killer:""},
+wordsep:"space",
+linebreak:"word", hyphen:"", justification:"sp", spacing:U, gc:Y, baseline:"romn", 
 },
 
 
