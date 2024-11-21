144e886d1099ed5e652a8f747666e8e3c1f5c3bf
r12a <ishida@w3.org>
2024-11-21 17:20:16 +0000

    Regenerate Tai Yo and Tolong Siki

--- tols/kru-langdata.js
+++ tols/kru-langdata.js
@@ -1,18 +1,33 @@
 // this should always be updated when app-charuse/langs.js is updated
 
+    const Y = true
+    const N = false
+    const P = 'partial'
+    const U = '?'
+
+
 var langs = {
 
-"kru": { name:"Kurukh", local:"𑷊𑶲̃𑷗𑶲𑷖", silcode:"", 
+"kru-tols": { name:"Kurukh", local:"𑷊𑶲̃𑷗𑶲𑷖", localtrans:"kũrux", silcode:"", 
 source:"r12a", 
-region:"sasia", countries:"India", script:"tols", speakers:"0", 
-letter:"𑶶𑶷𑶸𑶹𑶺𑶻𑶼𑶽𑶾𑶿𑷀𑷁𑷂𑷃𑷄𑷅𑷆𑷇𑷈𑷉𑷊𑷋𑷌𑷍𑷎𑷏𑷐𑷑𑷒𑷓𑷔𑷕𑷖𑷗𑷘𑷚𑶰𑶱𑶲𑶳𑶴𑶵𑷙𑷛", letteraux:"", mark:"̰̃̆̇", markaux:"", number:"𑷠𑷡𑷢𑷣𑷤𑷥𑷦𑷧𑷨𑷩", punctuation:"।", symbol:"", other:"‌‍", 
-orth:`[Tolong Siki] &nbsp;  
-<a href="../scripts/tols/kru.html" target="_blank">Detailed summary</a>.`,
+region:"sasia", countries:"India", script:"tols", speakers:"?", 
+letter:"ʼ𑶰𑶱𑶲𑶳𑶴𑶵𑶶𑶷𑶸𑶹𑶺𑶻𑶼𑶽𑶾𑶿𑷀𑷁𑷂𑷃𑷄𑷅𑷆𑷇𑷈𑷉𑷊𑷋𑷌𑷍𑷎𑷏𑷐𑷑𑷒𑷓𑷔𑷕𑷖𑷗𑷘𑷙𑷚𑷛", letteraux:"", mark:"̤̰̃̆̇̈", markaux:"", number:"𑷠𑷡𑷢𑷣𑷤𑷥𑷦𑷧𑷨𑷩", numberaux:"", punctuation:"।॥–—•", punctuationaux:"", symbol:"", symbolaux:"", other:"\u200B", otheraux:"", aux:"", deprecated:"", 
+orth:`[Tolong Siki] Invented in 1988, and officially recognised by the state of Jharkhand in 2007.`,
 also:
-["kru-deva | deva | Primary usage."],
-type:"alphabet", cs:"no", gpos:"yes", gsub:"no", cursive:"no", wordsep:"space", baseline:"romn", wrap:"word", hyphenation:"?", hyphen:"",  direction:"ltr",  justification:"?", 
-vowels:"",  clusters:"",  finals:"cm:1", 
-linked:"tols/kru", fonts:"/pickers/tols/", 
+["kru-deva | deva | Primary orthography."],
+linked:"tols/kru", picker:"tols-kru", font:"", 
+},
+
+
+"tols": {name:"Tolong Siki", region:"sasia", type:"alpha", 
+blocks:1, chars:54, bletter:44, bmark:0, bnumber:10, bpunctuation:0, bsymbol:0, bother:0, 
+direction:"ltr", rtlnumbers:N,
+voweltype: { inherent:0, letters:Y, marks:N, hidden:N, visorder:N, vocalics:N, ivowels:N, vbase:"", vcomposite:N, prebase:N, circum:N, },
+case:N, cursive:N, mcchars:Y, conjuncts:N, ligs:N, 
+medials:"cm", finals:"cm",
+clusters:{ ligation:N, stacks:N, touch:N, conjoined:N, subjoinedcp:N, visviram:N, diacritic:N, killer:""},
+wordsep:"space",
+linebreak:"word", hyphen:U, justification:U, spacing:U, gc:Y, baseline:"romn", 
 },
 
 }
