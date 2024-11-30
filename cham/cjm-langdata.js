1edf19eae1e7f601df9e7aff28977f13851483e7
r12a <ishida@w3.org>
2024-11-30 07:34:19 +0000

    cham   update & add to script-features

--- cham/cjm-langdata.js
+++ cham/cjm-langdata.js
@@ -1,15 +1,32 @@
 // this should always be updated when app-charuse/langs.js is updated
 
+    const Y = true
+    const N = false
+    const P = 'partial'
+    const U = '?'
+
+
 var langs = {
 
-"cjm": { name:"Cham", local:"ꨌꩌ", localtrans:"Chăm", 
+"cjm": { name:"Cham", local:"ꨌꩌ", localtrans:"Chăm", silcode:"", 
 source:"r12a", 
 region:"seasia", countries:"Vietnam", script:"cham", speakers:"132000", 
-letter:"ꨀꨁꨂꨃꨄꨅꨆꨇꨈꨉꨊꨋꨌꨍꨎꨏꨐꨑꨒꨓꨔꨕꨖꨗꨘꨙꨚꨛꨜꨝꨞꨟꨠꨡꨢꨣꨤꨥꨦꨧꨨꩀꩁꩂꩄꩅꩆꩇꩈꩉꩊꩋ", mark:"ꨩꨪꨫꨬꨭꨮꨯꨰꨱꨲꨳꨴꨵꨶꩃꩌꩍ", number:"꩐꩑꩒꩓꩔꩕꩖꩗꩘꩙", punctuation:"꩜꩝꩞꩟", 
+letter:"ꨀꨁꨂꨃꨄꨅꨆꨇꨈꨉꨊꨋꨌꨍꨎꨏꨐꨑꨒꨓꨔꨕꨖꨗꨘꨙꨚꨛꨜꨝꨞꨟꨠꨡꨢꨣꨤꨥꨦꨧꨨꩀꩁꩂꩄꩅꩆꩇꩈꩉꩊꩋ", letteraux:"", mark:"ꨩꨪꨫꨬꨭꨮꨯꨰꨱꨲꨳꨴꨵꨶꩃꩌꩍ", markaux:"", number:"꩐꩑꩒꩓꩔꩕꩖꩗꩘꩙", numberaux:"", punctuation:"꩜꩝꩞꩟", punctuationaux:"", symbol:"", symbolaux:"", other:"", otheraux:"", aux:"", deprecated:"", 
 orth:`[Cham] Primary use for Eastern Cham speakers. <a href='../scripts/cham/cjm.html' target='_blank'>Details</a>.`,
-type:"abugida", cs:"no", gpos:"yes", gsub:"yes", cursive:"no", wordsep:"space", baseline:"high", wrap:"syllable", hyphenation:"no",  direction:"ltr",  justification:"?", 
-vowels:"inh:2 vs:10 ind:6 base:ꨀ A pre:2 comp:7+",  medials:"cm:4",  finals:"cm:3 let:15", 
-linked:"cham/cjm", fonts:"cham-cjm", font:"Noto Sans Cham", 
+linked:"cham/cjm", picker:"cham-cjm", font:"Noto Sans Cham", 
+},
+
+
+
+"cham": {name:"Cham", region:"seasia", type:"abug", 
+blocks:1, chars:83, bletter:52, bmark:17, bnumber:10, bpunctuation:4, bsymbol:0, bother:0, 
+direction:"ltr", rtlnumbers:N,
+voweltype: { inherent:2, letters:N, marks:Y, hidden:N, visorder:N, vocalics:N, ivowels:Y, vbase:"A ꨀ", vcomposite:Y, prebase:Y, circum:N, },
+case:N, cursive:N, mcchars:"m", conjuncts:N, ligs:N, 
+medials:"cm", finals:"cm let",
+clusters:{ ligation:N, stacks:N, touch:N, conjoined:N, subjoinedcp:N, visviram:N, diacritic:N, killer:""},
+wordsep:"space",
+linebreak:"word", hyphen:"? ", wordspan:N, justification:U, gc:Y, baseline:"romn", 
 },
 
 }
