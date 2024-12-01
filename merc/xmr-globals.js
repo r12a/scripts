08d58120343cf8d0c627ba1c1da22b69206f7ad5
r12a <ishida@w3.org>
2024-12-01 10:47:39 +0000

    + meroitic cursive

---
+++ merc/xmr-globals.js
@@ -0,0 +1,22 @@
+
+
+langTag='xmr' // BCP47 language tag for this orthography
+scriptTag='merc' // BCP47 script tag for this orthography
+notesLangtag='xmr'
+blockLangtag = 'xmr' // same as langTag; used in block.html
+blockDirection=''  // ltr/rtl or '' for ltr; used by block.html
+
+blockDirectoryName='merc'  // folder name where block file is found, no slash
+orthogFilePath='merc/xmr'  // path below /scripts, including filename, but no extension, eg. lana/nod
+pickerDir='merc'  // directory name for picker, no slash at end
+orthogSummaryTableName='xmr'  // id used for langs record in xx-langdata.js; used to make summary table
+scriptSummaryTableName='merc'  // id used for langs record in xx-langdata.js; used to make summary table
+charUsageBCP='xmr'  // bcp tag to find the orthography in the charuse app; used in makeIndexIntro only
+orthogName = 'Meroitic Cursive' // natural language name for the orthography; used only in makeIndexIntro
+languageName = 'Meroitic' // natural language name of language; used in various places, esp block
+scriptIsIndic = false  // true if there are indic syllabic categories; used only in makeIndexIntro
+
+
+defaultSize = '24px'  // used by block.html to set size of font, including in font mgt panel
+letterClassName = 'xmr'  // classname for a given orthography in the block page
+hideBlockName = 'MEROITIC CURSIVE' // if a value is given, that text is removed from code point names
---
+++ merc/xmr-langdata.js
@@ -0,0 +1,34 @@
+// this should always be updated when app-charuse/langs.js is updated
+
+    const Y = true
+    const N = false
+    const P = 'partial'
+    const U = '?'
+
+
+var langs = {
+
+"xmr": { name:"Hindi", local:" मानक हिन्दी‎ (Mānak Hindī), हिन्दी‎ (Hindī)", silcode:"hin", 
+source:"cldr_hi,udhr_hin,9d8e188be96400153901b55006feaaf9fd4bf3b1", 
+region:"sasia", countries:"India, Nepal, Singapore, South Africa", script:"deva", speakers:"260000000", 
+letter:"अआइईउऊऋएऐओऔकखगघङचछजझञटठडढणतथदधनपफबभमयरलवशषसहक़फ़ज़ख़ग़ड़ढ़", letteraux:"ऍऑऽॐ", mark:"ँंः़ािीुूृेैोौ्", markaux:"ॅॉ", number:"०१२३४५६७८९", punctuation:"।॥॰‘’“”—", symbol:"₹", other:"‌‍", 
+orth:`[Devanagari] &nbsp;  
+<a href="../scripts/devanagari" target="_blank">Detailed summary</a>.`,
+also:
+["hi-newa | newa | Archaic, no longer in use."],
+linked:"devanagari", fonts:"/pickers/deva-hi/", 
+},
+
+
+"merc": {name:"Javanese", region:"oce", type:"abug", 
+blocks:1, chars:91, bletter:48, bmark:18, bnumber:10, bpunctuation:15, bsymbol:0, bother:0, 
+direction:"ltr", rtlnumbers:N,
+voweltype: { inherent:2, letters:Y, marks:Y, hidden:N, visorder:N, vocalics:Y, ivowels:Y, vbase:"HA ꦲ", vcomposite:Y, prebase:Y, circum:N, },
+case:N, cursive:N, mcchars:Y, conjuncts:Y, ligs:N, 
+medials:"cm", finals:"cm vk",
+clusters:{ ligation:N, stacks:N, touch:N, conjoined:N, subjoinedcp:N, visviram:N, diacritic:N, killer:"v"},
+wordsep:"no",
+linebreak:"syllable", hyphen:"", justification:"sp ig", wordspan:Y, gc:N, baseline:"romn", 
+},
+
+}
