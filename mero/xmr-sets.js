41951ed12695116c3108dc83eb1779632d74f9c2
r12a <ishida@w3.org>
2024-12-01 10:48:50 +0000

    mero   Small adjustments

--- mero/xmr-sets.js
+++ mero/xmr-sets.js
@@ -9,17 +9,7 @@
 // 🅰🅱🅲🅳🅴🅵🅶🅷🅸🅹🅺🅻🅼🅽🅾🅿🆀🆁🆂🆃🆄🆅🆆🆇🆈🆉
 
 var collections = [
-{ symbol:'Ⓥ', desc:'Vowels', chars:'(xxx)' },
-{ symbol:'Ⓒ', desc:'Consonants', chars:'(xxx)' },
-{ symbol:'ⓥ', desc:'Dependent vowels', chars:'(xxx)' },
-{ symbol:'Ⓥ', desc:'Independent vowels', chars:'(xxx)' },
-{ symbol:'🅥', desc:'Vocalics', chars:'(xxx)' },
-{ symbol:'Ⓒ', desc:'All consonants', chars:'(xxx)' },
-{ symbol:'Ⓑ', desc:'Basic consonants', chars:'(xxx)' },
-{ symbol:'Ⓔ', desc:'Extended consonants', chars:'(xxx)' },
-{ symbol:'Ⓕ', desc:'Finals', chars:'(xxx)' },
-{ symbol:'Ⓜ', desc:'Medials', chars:'(xxx)' },
-{ symbol:'Ⓣ', desc:'Tones', chars:'(xxx)' },
-{ symbol:'⊙', desc:'Invisibles', chars:'(\u200C|\u200D)' },
+{ symbol:'Ⓥ', desc:'Vowels', chars:'(𐦢|𐦡|𐦣|𐦠)' },
+{ symbol:'Ⓒ', desc:'Simple consonants', chars:'(𐦧|𐦦|𐦴|𐦷|𐦲|𐦳|𐦯|𐦰|𐦭|𐦮|𐦨|𐦩|𐦥|𐦫|𐦬|𐦤)' },
 { symbol:'⊙', desc:'Invisibles', chars:'(\u200C|\u200D|2067|202B|2066|202A|2068|2069|202C|200F|200E|061C|034F)' },
 ]
