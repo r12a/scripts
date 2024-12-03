555d731af9f88db6202b56c67281b41bb99986e9
r12a <ishida@w3.org>
2024-12-03 08:34:02 +0000

    + beria vocab

---
+++ beri/zag-examples.js
@@ -0,0 +1,92 @@
+/*
+native | meaning | IPA① | transcription | other transcriptions② | notes | wiktionary③
+
+
+① separate each text unit (consonant+combining_mark(s)) with §, separate phonetic syllables with .
+   use ‹ and › for a sound that spans 2 text units
+   use – for a text unit that doesn't produce sound
+   syllable boundary markers go inside previous unit, and emphasis markers begin emphasised unit
+
+② the previous transcription field is always Latin. 
+   if there is another key orthography (eg. Mongolian cyrillic) it goes here.
+   vowelled/unvowelled alternatives also go here
+
+③ if Wiktionary spells the word differently, add the spelling here
+*/
+
+
+
+
+
+autoExpandExamples.zag = `
+
+
+
+@ https://www.unicode.org/L2/L2024/24004r-zaghawa-proposal.pdf
+
+𖻎𖻂𖺿𖻂𖻌|zero|s§ɪ§f§ɪ§ɾ|
+𖺽𖻂̄𖻎𖻐𖻂|nine|d§i§s§t§i|
+𖻐𖻂̄𖻆|a thousand|t§i§m
+𖺦𖻂̄𖻍𖻂𖺼𖺻|Iriba|h§i§ɻ§i§b§a
+𖺢𖺻𖻌 𖺡𖻂̄𖻍𖻂𖺻|Dar Bilia|d§a§ɾ§ §b§i§ɻ§i§a
+𖻎𖺾𖻀𖻂́|I ate|s§ɛ́§ɡ§ɪ́|
+𖻎𖺾𖻀𖻂|I eat|s§ɛ́§ɡ§ɪ̄|
+𖺡𖺾𖻍𖻂|Beri|b§ɛ§ɻ§ɪ|
+𖺡𖺾𖻍𖻂𖺻|Beria language|b§ɛ§ɻ§ɪ§a
+𖺼𖺻𖺾𖻌𖻂 𖺻𖻁|alphabet|b§a§ɛ§ɾ§ɪ§ §a§h|
+𖻅𖻊𖻓𖺽𖺾|handwriting|l§ɔ§j§d§ɛ|
+𖻐𖺻𖻌𖻃𖺻𖻆𖺻 𖺽𖺾|practice|t§a§ɾ§ɟ§a§m§a§ §d§ɛ|
+𖺼𖺻𖺾𖻌𖺻𖻌𖺻𖻂|linguistics|b§a§ɛ§ɾ§a§ɾ§a§ɪ|
+𖻆𖻌𖻀𖻂|mouth|m§ɾ§ɡ§ɪ
+𖺼𖺻|hand|b§a
+𖻄𖻌𖻊|knee|k§r§ɔ
+𖺽𖻂 𖻀𖻊𖻅𖻀𖻅𖻊|ankle|d§ɪ§ §ɡ§ɔ§l§ɡ§l§ɔ
+𖺼𖺻 𖻃𖻊|elbow|b§a§ §ɟ§ɔ
+𖺽𖻂|foot|d§ɪ
+𖻐𖺽𖺾|salt|t§d§ɛ
+𖺻𖻇𖺾|meat|a§n§ɛ
+𖻄𖺻𖻎𖻊|scorpion|k§a§s§ɔ
+𖻎𖻊𖻌𖺻|lion|s§ɔ§r§a
+𖺽𖺾𖻆𖺻|cat|d§ɛ§m§a
+𖻄𖺻𖻆𖻌|mouse|k§a§m§ɾ
+𖺼𖻌𖻌𖺾|dog|b§ɾ§ɾ§ɛ
+𖺼𖺾𖻌|he, she|b§ɛ§ɾ
+𖺼𖺾𖻌𖻌|they|b§ɛ§ɾ§ɾ
+𖻇𖺻|you|n§a
+𖻇𖻊𖻒|you (pl)|n§ɔ§w
+
+
+
+
+
+
+
+`
+
+
+
+
+// automatically creates Online Resources section, if markup is uncommented in xx.html
+
+termLists = [
+
+/*
+{
+"title":"Wiktionary, Eastern Cham lemmas",
+"url":"https://en.wiktionary.org/wiki/Category:Eastern_Cham_lemmas",
+},
+
+{
+"title":"Austronesian Basic Vocabulary Database, Phan Rang Cham (Eastern Cham)",
+"url":"https://abvd.eva.mpg.de/austronesian/language.php?id=371&sort=item",
+"notes":"Latin only",
+},
+
+{
+"title":"Living Dictionaries",
+"url":"https://livingdictionaries.app/sora/entries/list",
+"notes":"No native script.",
+},
+*/
+]
+
