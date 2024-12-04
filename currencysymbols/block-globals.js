ed7ad0ed91433fa58fdd78c6e481646b88ddcc1f
r12a <ishida@w3.org>
2024-12-04 08:28:10 +0000

    Redo currencysymbols

---
+++ currencysymbols/block-globals.js
@@ -0,0 +1,11 @@
+langTag = 'en'
+window.version = 'v29'
+window.scriptName = ''
+window.ghDirectory = 'currencysymbols'
+window.blockDirectory = 'currencysymbols'
+window.languageList = ['en'] // lang tags for each orthography handled
+window.detailsfileList = ['en'] // first part of xx-details for each orthography
+window.scriptISO = 'latn'
+window.webfonts = ["Noto Sans WF"]
+window.defaultSize = '24px'
+hideBlockName = ""
---
+++ currencysymbols/refs.js
@@ -0,0 +1,45 @@
+window.langSet = false
+
+
+
+reflist = {
+
+
+/*
+"auth_initials": {
+"authors": "Sort_by_family_name_of_first_contributor",
+"title":"The_Worlds_Writing_Systems",
+"sub": "subtitle",
+"date": "1996",
+"retr": "Feb 2024",
+"publisher":"Oxford_University_Press",
+"isbn":"0-19-507993-0",
+"pages": "xxx",
+"url":"refs",
+"lang": new Set(['ru']),
+"reviewed":false
+},
+*/
+
+
+
+
+}
+
+
+
+
+scriptLanguageList = {
+en:{name:"English"},
+}
+
+
+
+
+
+
+// resources list is at the bottom of the xx-examples file
+
+
+
+
