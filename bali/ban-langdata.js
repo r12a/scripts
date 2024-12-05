df93b0ef404d5e92bf97ca850d06933223353611
r12a <ishida@w3.org>
2024-12-05 08:28:22 +0000

    bali   Update langdata and maps

--- bali/ban-langdata.js
+++ bali/ban-langdata.js
@@ -11,7 +11,8 @@ var langs = {
 "ban-bali": { name:"Bali (Balinese)", local:"ᬪᬵᬱᬩᬮᬶ / ᬩᬲᬩᬮᬶ", localtrans:"/baʃa bali/", silcode:"ban", 
 source:"ff844379ad4006570a536dd0572148858532d87b", 
 region:"oce", countries:"Indonesia", script:"bali", speakers:"3300000", 
-letter:"ᬅᬆᬇᬈᬉᬊᬋᬌᬍᬎᬏᬐᬑᬒᬓᬕᬖᬗᬘᬚᬜᬝᬞᬟᬠᬡᬢᬣᬤᬥᬦᬧᬨᬩᬪᬫᬬᬭᬮᬯᬰᬱᬲᬳ", letteraux:"ᬔᬙᬛ", mark:"ᬀᬁᬂᬃᬄ᬴ᬵᬶᬷᬸᬹᬺᬻᬼᬽᬾᬿᭀᭁᭂᭃ᭄", markaux:"", number:"᭐᭑᭒᭓᭔᭕᭖᭗᭘᭙", numberaux:"", punctuation:"᭚᭛᭜᭝᭞᭟᭠", punctuationaux:"", symbol:"", symbolaux:"", other:"\u{200B}\u{200C}", otheraux:"", aux:"!%()-;?[]§«»ʼ͏‍‑‒–—‘’“”†‡…‰′″‹›", deprecated:"", 
+letter:"ᬅᬆᬇᬈᬉᬊᬋᬌᬍᬎᬏᬐᬑᬒᬓᬕᬖᬗᬘᬚᬜᬝᬞᬟᬠᬡᬢᬣᬤᬥᬦᬧᬨᬩᬪᬫᬬᬭᬮᬯᬰᬱᬲᬳ", letteraux:"ᬔᬙᬛ", mark:"ᬀᬁᬂᬃᬄ᬴ᬵᬶᬷᬸᬹᬺᬻᬼᬽᬾᬿᭀᭁᭂᭃ᭄", number:"᭐᭑᭒᭓᭔᭕᭖᭗᭘᭙", punctuation:"᭎᭏᭚᭛᭜᭝᭞᭟᭠᭽᭾᭿", symbol:"", other:"\u200B\u200C", 
+aux:"!%()-;?[]§«»ʼ͏‍‑‒–—‘’“”†‡…‰′″‹›", deprecated:"", 
 orth:`Balinese. &nbsp; <a href="../scripts/balinese" target="_blank">Details</a>.`,
 also:
 ["ban | latn | Used since early 20th century.",
