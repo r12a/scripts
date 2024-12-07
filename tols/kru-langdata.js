f5c7b62263fec19a38927a9bacf31f7f75036172
r12a <ishida@w3.org>
2024-12-07 04:50:12 +0000

    kru   Redo maps, etc.  Fix character repertoire in index etc.

--- tols/kru-langdata.js
+++ tols/kru-langdata.js
@@ -11,7 +11,7 @@ var langs = {
 "kru-tols": { name:"Kurukh", local:"𑷊𑶲̃𑷗𑶲𑷖", localtrans:"kũrux", silcode:"", 
 source:"r12a", 
 region:"sasia", countries:"India", script:"tols", speakers:"?", 
-letter:"ʼ𑶰𑶱𑶲𑶳𑶴𑶵𑶶𑶷𑶸𑶹𑶺𑶻𑶼𑶽𑶾𑶿𑷀𑷁𑷂𑷃𑷄𑷅𑷆𑷇𑷈𑷉𑷊𑷋𑷌𑷍𑷎𑷏𑷐𑷑𑷒𑷓𑷔𑷕𑷖𑷗𑷘𑷙𑷚𑷛", letteraux:"", mark:"̤̰̃̆̇̈", markaux:"", number:"𑷠𑷡𑷢𑷣𑷤𑷥𑷦𑷧𑷨𑷩", numberaux:"", punctuation:"।॥–—•", punctuationaux:"", symbol:"", symbolaux:"", other:"\u200B", otheraux:"", aux:"", deprecated:"", 
+letter:"ʼ𑶰𑶱𑶲𑶳𑶴𑶵𑶶𑶷𑶸𑶹𑶺𑶻𑶼𑶽𑶾𑶿𑷀𑷁𑷂𑷃𑷄𑷅𑷆𑷇𑷈𑷉𑷊𑷋𑷌𑷍𑷎𑷏𑷐𑷑𑷒𑷓𑷔𑷕𑷖𑷗𑷘𑷙𑷚𑷛", mark:"̤̰̃̆̇̈", number:"𑷠𑷡𑷢𑷣𑷤𑷥𑷦𑷧𑷨𑷩", punctuation:"।", symbol:"", other:"", 
 orth:`[Tolong Siki] Invented in 1988, and officially recognised by the state of Jharkhand in 2007.`,
 also:
 ["kru-deva | deva | Primary orthography."],
