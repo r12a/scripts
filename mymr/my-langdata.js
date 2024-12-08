b78dc59979c1e04dc1d1b27122f10395d428f456
r12a <ishida@w3.org>
2024-12-08 08:37:16 +0000

    my   Complete char usage data.  Redo maps.  (Part 2)

--- mymr/my-langdata.js
+++ mymr/my-langdata.js
@@ -11,7 +11,7 @@ var langs = {
 "my": { name:"Burmese (Myanmar)", local:"", localtrans:"", silcode:"", 
 source:"cldr_my,udhr_mya,e658408933cd9f4d157eac1af6c6373f4c7c6f44", 
 region:"seasia", countries:"Myanmar", script:"mymr", speakers:"33000000", 
-letter:"ကခဂငစဆဇဉညတထဒဓနပဖဗဘမယရလဝသဟအဣဤဥဦဧဩဪဿ", letteraux:"ဃဈဠ", mark:"ါာိီုူေဲံ့း္်ျြွှ", markaux:"", number:"၀၁၂၃၄၅၆၇၈၉", numberaux:"", punctuation:"၊။၌၍၎၏“”", punctuationaux:"", symbol:"", symbolaux:"", other:"\u{200B}\u{200C}\u{2060}", otheraux:"", aux:"!§͏‍–—‘’†‡…′″", deprecated:"", 
+letter:"ကခဂငစဆဇဉညတထဒဓနပဖဗဘမယရလဝသဟအဣဤဥဦဧဩဪဿ", letteraux:"ဃဈဠ", mark:"ါာိီုူေဲံ့း္်ျြွှ", number:"၀၁၂၃၄၅၆၇၈၉", punctuation:"၊။၌၍၎၏“”", symbol:"", other:"\u200B\u2060", otheraux:"\u200C", aux:"!‍–—‘’…", 
 orth:`Myanmar. &nbsp; <a href="../scripts/myanmar" target="_blank">Details</a>.`,
 linked:"mymr/my", picker:"mymr-my", font:"", 
 },
