f1ef30ba579511cc112dd4f4a2c9b0cc5f3d6f33
r12a <ishida@w3.org>
2024-12-08 16:06:40 +0000

    ahom   Complete char usage data.  Redo maps.

--- ahom/aho-langdata.js
+++ ahom/aho-langdata.js
@@ -11,7 +11,7 @@ var langs = {
 "aho": { name:"Ahom", local:"𑜒𑜑𑜪𑜨", localtrans:"", silcode:"", 
 source:"r12a", 
 region:"sasia", countries:"India", script:"ahom", speakers:"", 
-letter:"𑜀𑜁𑜂𑜃𑜄𑜅𑜆𑜇𑜈𑜉𑜊𑜋𑜌𑜍𑜎𑜏𑜐𑜑𑜒𑜓𑜔𑜕𑜖𑜗𑜘𑜙𑜚", letteraux:"𑝀𑝁𑝂𑝃𑝄𑝅𑝆", mark:"𑜝𑜞𑜟𑜠𑜡𑜢𑜣𑜤𑜥𑜦𑜧𑜨𑜩𑜪𑜫", number:"𑜰𑜱𑜲𑜳𑜴𑜵𑜶𑜷𑜸𑜹𑜺𑜻", punctuation:"𑜼𑜽𑜾", symbol:"𑜿", other:"", 
+letter:"𑜀𑜁𑜂𑜃𑜄𑜅𑜆𑜇𑜈𑜉𑜊𑜋𑜌𑜍𑜎𑜏𑜐𑜑𑜒𑜓𑜔𑜕𑜖𑜗𑜘𑜙𑜚", letteraux:"𑝀𑝁𑝂𑝃𑝄𑝅𑝆", mark:"𑜝𑜞𑜟𑜠𑜡𑜢𑜣𑜤𑜥𑜦𑜧𑜨𑜩𑜪𑜫", number:"𑜰𑜱𑜲𑜳𑜴𑜵𑜶𑜷𑜸𑜹𑜺𑜻", punctuation:"𑜼𑜽𑜾", symbol:"𑜿", other:"", aux:"!%'(),-.0123456789:;?[]«»͏٭​‌‍‑–—‘’“”… ‰‹›⁠⋯⹁", 
 orth:`[Ahom] Became extinct but there are attempts to revive it. <a href='../scripts/ahom/aho.html' target='_blank'>Details</a>.`,
 linked:"ahom/aho", picker:"ahom-aho", font:"Noto Serif Ahom", 
 },
