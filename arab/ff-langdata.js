6e81d80fe1923bc72e848270cd02fbddb09e3dff
r12a <ishida@w3.org>
2024-12-10 06:41:35 +0000

    ff   Complete char usage data.  Redo maps.

--- arab/ff-langdata.js
+++ arab/ff-langdata.js
@@ -11,7 +11,7 @@ var langs = {
 "ff-arab": { name:"Adamawa Fulfulde (Ajamiya)", local:"ڢُلْڢُلْدٜ‎", localtrans:"", silcode:"fub", rtl:true, 
 source:"tbd", 
 region:"afr", countries:"Nigeria, Niger, Côte d'Ivoire, Benin, Chad, Sudan", script:"arab", speakers:"5673600", 
-letter:"ءأإابتثجحخدذرزسشصضطظعغقلمنوىيڢکݠࢡࢨࢩ", letteraux:"ه", mark:"͏َُِّْٰٜٛ", markaux:"ٕٔ", number:"٠١٢٣٤٥٦٧٨٩", numberaux:"", punctuation:"«»،؛؟‑–—…⸫", punctuationaux:"", symbol:"", symbolaux:"", other:"\u{061C}\u{200C}\u{200D}\u{200E}\u{200F}\u{202A}\u{202B}\u{202C}\u{2066}\u{2067}\u{2068}\u{2069}", otheraux:"", aux:"", deprecated:"", 
+letter:"ءأإابتثجحخدذرزسشصضطظعغقلمنوىيڢکݠࢡࢨࢩ", letteraux:"ه", mark:"͏َُِّْٰٜٛ", markaux:"ٕٔ", number:"٠١٢٣٤٥٦٧٨٩", punctuation:"«»،؛؟‑–—…⸫", symbol:"", other:"\u{061C}\u{200C}\u{200D}\u{200E}\u{200F}\u{202A}\u{202B}\u{202C}\u{2066}\u{2067}\u{2068}\u{2069}", aux:"", 
 orth:`[Arabic] &nbsp; Used mostly for muslim texts. <a href="../scripts/arab/ff.html" target="_blank">Details.</a>`,
 also:
 ["ff-adlm | adlm | Since 1989, in development.",
