7a6e1f7b6f4c0dd19c6ced280c71a93957955d8b
r12a <ishida@w3.org>
2024-12-10 14:31:00 +0000

    ks-arab   Complete char usage data.  Redo maps.

--- arab/ks-langdata.js
+++ arab/ks-langdata.js
@@ -11,7 +11,7 @@ var langs = {
 "ks": { name:"Kashmiri (Koshur)", local:"کٲشُر‎", localtrans:"(koshur)", silcode:"kas", rtl:true, 
 source:"cldr_ks,57237835618d4daaf27d149d55e14be159182c33", 
 region:"sasia", countries:"India, Pakistan", script:"arab", speakers:"5600000", 
-letter:"ؠآأؤإابتثجحخدذرزسشصضطظعغفقلمنوٲٹپچڈڑژکگںھہۂۄیے", letteraux:"ـ", mark:"͏َُِّْٕٖٟؔٔٗٚ", markaux:"ٓ", number:"۰۱۲۳۴۵۶۷۸۹", numberaux:"", punctuation:"،؛؟٪۔“”", punctuationaux:"", symbol:"", symbolaux:"", other:"\u061C\u200C\u200D\u200E\u200F\u202A\u202B\u202C\u2066\u2067\u2068\u2069", otheraux:"", aux:".[]§«»ʼۓ​‑–—‘’†‡…‰′″‹›₹", deprecated:"", 
+letter:"ؠآأؤإابتثجحخدذرزسشصضطظعغفقلمنوٲٹپچڈڑژکگںھہۂۄیے", letteraux:"ـ", mark:"͏َُِّْٕٖٟؔٔٗٚ", markaux:"ٓ", number:"۰۱۲۳۴۵۶۷۸۹", punctuation:"،؛؟٪۔“”", symbol:"", other:"\u{061C}\u{200C}\u{200D}\u{200E}\u{200F}\u{202A}\u{202B}\u{202C}\u{2066}\u{2067}\u{2068}\u{2069}", aux:".[]§«»ʼۓ​‑–—‘’†‡…‰′″‹›₹", 
 orth:`Arabic. &nbsp; Nastaliq style ([Aran]) is primary usage, but naskh style also used. Associated with the Muslim Kashmiris.
 Kashmiri is one of the languages that indicates all vowel sounds when using Arabic script. <a href="../scripts/arabic/kashmiri" target="_blank">Details</a>`,
 also:
