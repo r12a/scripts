3081fff636190966bc0986412811b26763daddbd
r12a <ishida@w3.org>
2024-12-10 17:04:21 +0000

    pes-arab   Complete char usage data.  Redo maps.

--- arab/pes-langdata.js
+++ arab/pes-langdata.js
@@ -11,7 +11,7 @@ var langs = {
 "pes": { name:"Persian (Farsi)", local:" فارسی‎", localtrans:"(fārsi)", silcode:"", rtl:true, 
 source:"f6c7293e56545fe2044143ef784236e7d524019c,cldr_fa,udhr_pes", 
 region:"wasia", countries:"Iran, Iraq", script:"arab", speakers:"~50000000", 
-letter:"ءآؤابةتثجحخدذرزسشصضطظعغـفقلمنهوپچژکگی", letteraux:"أ", mark:"͏ًٌٍَُِّْٔ", markaux:"", number:"۰۱۲۳۴۵۶۷۸۹", numberaux:"", punctuation:"«»،؛؟", punctuationaux:"", symbol:"", symbolaux:"", other:"\u{200C}\u{200D}\u{200E}\u{200F}\u{202A}\u{202B}\u{202C}\u{2066}\u{2067}\u{2068}\u{2069}", otheraux:"", aux:"%-[]{}§ʼ؉؍٪٫٬٭ٰ۔ ‑–—‘’“”†‡…‰′″‹›", deprecated:"", 
+letter:"ءآؤابةتثجحخدذرزسشصضطظعغـفقلمنهوپچژکگی", letteraux:"أ", mark:"͏ًَُِّْٔ", markaux:"ٌٍٓ", number:"۰۱۲۳۴۵۶۷۸۹", punctuation:"«»،؛؟", symbol:"", other:"\u{200C}\u{200D}\u{200E}\u{200F}\u{202A}\u{202B}\u{202C}\u{2066}\u{2067}\u{2068}\u{2069}", aux:"%-[]{}ʼ؉؍٪٫٬٭ٰ۔ ‑–—‘’“”…‰‹›", 
 orth:`Arabic. &nbsp; Naskh style, but nastaliq ([Aran]) in Iran.`,
 related:`Macrolanguage is Farsi [fa]. Legacy applications often use fa rather than pes.`,
 linked:"arab/pes", picker:"arab-fa", font:"", 
