7ec57cad0f030928f220d2fcb1059898668fb0ec
r12a <ishida@w3.org>
2024-12-11 15:09:02 +0000

    ug-arab   Complete char usage data.  Redo maps.

--- arab/ug-langdata.js
+++ arab/ug-langdata.js
@@ -11,7 +11,7 @@ var langs = {
 "ug": { name:"Uyghur (Uighur, Eastern Turki)", local:"ئۇيغۇر تىلى‎ / ئۇيغۇرچە‎", localtrans:"(uyghur tili / uyghurche)", silcode:"uig", rtl:true, 
 source:"cldr_ug,udhr_uig_arab", 
 region:"casia", countries:"China, Kazakhstan", script:"arab", speakers:"25000000", 
-letter:"ئابتجخدرزسشغفقكلمنوىيپچژڭگھۆۇۈۋېە", letteraux:"", mark:"͏", markaux:"ٔ", number:"", numberaux:"", punctuation:"«»،؛؟", punctuationaux:"", symbol:"", symbolaux:"", other:"\u{061C}\u{200C}\u{200D}\u{200E}\u{200F}\u{202A}\u{202B}\u{202C}\u{2066}\u{2067}\u{2068}\u{2069}", otheraux:"", aux:"-[]§ʼ٪​‑–—‘’“”†‡…′″‹›⁠", deprecated:"", 
+letter:"ئابتجخدرزسشغفقكلمنوىيپچژڭگھۆۇۈۋېە", mark:"͏", markaux:"ٔ", number:"", punctuation:"«»،؛؟", symbol:"", other:"\u{061C}\u{200C}\u{200D}\u{200E}\u{200F}\u{202A}\u{202B}\u{202C}\u{2066}\u{2067}\u{2068}\u{2069}", aux:"-[]ʼ٪​‑–—‘’“”…‹›⁠", 
 orth:`Arabic. &nbsp; Naskh style. Official and primary use in China, also used in Afghanistan. <a href="../scripts/arabic/uighur" target="_blank">Details</a>.`,
 also:
 ["ug-latn | latn | Used in China and Turkey.",
