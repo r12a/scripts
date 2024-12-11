e27f05261f7c378d3681a3847cb50bf368ca5ff1
r12a <ishida@w3.org>
2024-12-11 09:35:49 +0000

    skr-arab   Complete char usage data.  Redo maps.

--- arab/skr-langdata.js
+++ arab/skr-langdata.js
@@ -11,7 +11,7 @@ var langs = {
 "skr": { name:"Saraiki (Siraiki, Seraiki)", local:"سرائیکی‎", localtrans:"sə.ˈɾɛːkiː", silcode:"skr", rtl:true, 
 source:"r12a, udhr_skr", 
 region:"sasia", countries:"Pakistan", script:"arab", speakers:"30000000", 
-letter:"آؤئابتثجحخدذرزسشصضطظعغفقلمنوٹٻپڄچڈڑژکگڳںھہیےۓݙݨ", letteraux:"ي", mark:"ٍَُِ٘", markaux:"ًٌّْٰٖٓٔٗ", number:"۰۱۲۳۴۵۶۷۸۹", numberaux:"", punctuation:"،؛؟۔", punctuationaux:"", symbol:"", symbolaux:"", other:"\u{200C}\u{200D}\u{200E}\u{200F}\u{202A}\u{202B}\u{202C}\u{2066}\u{2067}\u{2068}\u{2069}", otheraux:"", aux:"", deprecated:"", 
+letter:"آؤئابتثجحخدذرزسشصضطظعغفقلمنوٹٻپڄچڈڑژکگڳںھہیےۓݙݨ", letteraux:"ي", mark:"ٍَُِ٘", markaux:"ًٌّْٰٖٓٔٗ", number:"۰۱۲۳۴۵۶۷۸۹", punctuation:"،؛؟۔", symbol:"", other:"\u{200C}\u{200D}\u{200E}\u{200F}\u{2066}\u{2067}\u{2068}\u{2069}", otheraux:"\u{202A}\u{202B}\u{202C}", aux:",-[]«»͏؜ءـكه٪​‑–—‘’“”…‹›⁠﴾﴿", 
 orth:`Arabic. &nbsp; Nastaliq ([Aran]) style. Primary usage.`,
 also:
 ["skr-deva | deva | No longer in use.",
