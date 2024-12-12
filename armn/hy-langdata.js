6bdb7ca20d36783ca2594900ccd87753e1bd9984
r12a <ishida@w3.org>
2024-12-12 16:30:57 +0000

    armn   Complete char usage data.  Redo maps.

--- armn/hy-langdata.js
+++ armn/hy-langdata.js
@@ -11,7 +11,7 @@ var langs = {
 "hy": { name:"Armenian", local:"հայերէն/հայերեն‎", localtrans:"(hayeren)", silcode:"hye", 
 source:"ac321e266f0a5b1374f2b3889d418fc3399daf34,cldr_hy,udhr_hye", 
 region:"wasia", countries:"Armenia, Russia, Azerbaijan, Iran", script:"armn", speakers:"10000000", 
-letter:"ԱԲԳԴԵԶԷԸԹԺԻԼԽԾԿՀՁՂՃՄՅՆՇՈՉՊՋՌՍՎՏՐՑՒՓՔՕՖաբգդեզէըթժիլխծկհձղճմյնշոչպջռսվտրցւփքօֆև", letteraux:"", mark:"", markaux:"", number:"", numberaux:"", punctuation:"«»՛՜՝՞։֊’․…", punctuationaux:"", symbol:"֍֎֏", symbolaux:"", other:"", otheraux:"", aux:"[]§ʼ‌‍‑–—‘“”†‡‰′″‹›★", deprecated:"", 
+letter:"ʼԱԲԳԴԵԶԷԸԹԺԻԼԽԾԿՀՁՂՃՄՅՆՇՈՉՊՋՌՍՎՏՐՑՒՓՔՕՖաբգդեզէըթժիլխծկհձղճմյնշոչպջռսվտրցւփքօֆև", mark:"", number:"", punctuation:"«»՛՜՝՞։֊–—‘’“”․…‹›", symbol:"֍֎֏", other:"", aux:"‌‍‑‰★", 
 orth:`Armenian. &nbsp;
 <a href="../scripts/armenian" target="_blank">Detailed summary</a>.`,
 related:`See also Western Armenian [hyw].`,
