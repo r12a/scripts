3f1a73724444486f432930bf17c0941f83c44638
r12a <ishida@w3.org>
2024-12-12 03:58:41 +0000

    ur-arab   Complete char usage data.  Redo maps.

--- arab/ur-langdata.js
+++ arab/ur-langdata.js
@@ -11,7 +11,7 @@ var langs = {
 "ur": { name:"Urdu", local:"اُردُو‎‎", localtrans:"(urdū)", silcode:"urd", rtl:true, 
 source:"db476b3d6bab736201e0829304afa25fcf4a4223,cldr_urdu,udhr_urd", 
 region:"sasia", countries:"Pakistan, India, Nepal, Bangladesh", script:"arab", speakers:"65000000", 
-letter:"ءآؤئابتثجحخدذرزسشصضطظعغفقلمنويٹپچڈڑژکگںھہۂۃیےۓ", letteraux:"", mark:"͏ًٌٍَُِّْٰٖؐؑؒؓؔٓٔٗ٘", markaux:"", number:"۰۱۲۳۴۵۶۷۸۹", numberaux:"", punctuation:"،؍؛؟٪٫٬۔–“”", punctuationaux:"", symbol:"؎؏﷽", symbolaux:"", other:"\u{0600}\u{0601}\u{0602}\u{0603}\u{0604}\u{061C}\u{06DD}\u{200C}\u{200D}\u{200E}\u{200F}\u{202A}\u{202B}\u{202C}\u{2066}\u{2067}\u{2068}\u{2069}", otheraux:"", aux:"%,;[]§«»ʼ​‑—‘’†‡…‰′″‹›⋯", deprecated:"", 
+letter:"ءآؤئابتثجحخدذرزسشصضطظعغفقلمنويٹپچڈڑژکگںھہۂۃیےۓ", mark:"͏ًٌٍَُِّْٰٖؐؑؒؓؔٓٔٗ٘", number:"۰۱۲۳۴۵۶۷۸۹", punctuation:"،؍؛؟٪٫٬۔–“”", symbol:"؎؏﷽", other:"\u{0600}\u{0601}\u{0602}\u{0603}\u{0604}\u{061C}\u{06DD}\u{200C}\u{200D}\u{200E}\u{200F}\u{2066}\u{2067}\u{2068}\u{2069}", otheraux:"\u{202A}\u{202B}\u{202C}", aux:"%,;[]«»ʼ​‑—‘’…‰‹›⋯", 
 orth:`Arabic. &nbsp; The primary usage is nastaliq ([Aran]) style, though naskh style is used also. <a href="../scripts/arabic/urdu" target="_blank">Details</a>.`,
 linked:"arab/ur", picker:"arab-ur", font:"'Noto Nastaliq Urdu', 'Urdu Typesetting'", 
 },
