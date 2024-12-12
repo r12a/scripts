5ff380342ec672210fe0c9371936eaf8d9c0e3cb
r12a <ishida@w3.org>
2024-12-12 07:55:59 +0000

    wo-arab   Complete char usage data.  Redo maps.

--- arab/wo-langdata.js
+++ arab/wo-langdata.js
@@ -11,7 +11,7 @@ var langs = {
 "wo-arab": { name:"Wolof", local:"Wolof", localtrans:"وࣷلࣷفْ لࣵکّ", silcode:"wol", rtl:true, 
 source:"60f90ae05da3e47b25b07d3fb24cbf634210dac1", 
 region:"afr", countries:"Senegal, Mauritania, Gambia", script:"arab", speakers:"4200000", 
-letter:"آابتجخدرسعفقلمنهويکگݒݖݝݧ", letteraux:"", mark:"͏َُِّْࣹࣺࣴࣵࣷࣸ", markaux:"", number:"", numberaux:"", punctuation:"،؛؟‘’“”…", punctuationaux:"", symbol:"", symbolaux:"", other:"\u{061C}\u{200C}\u{200D}\u{200E}\u{200F}\u{202A}\u{202B}\u{202C}\u{2060}\u{2066}\u{2067}\u{2068}\u{2069}", otheraux:"", aux:"", deprecated:"", 
+letter:"آابتجخدرسعفقلمنهويکگݒݖݝݧ", mark:"͏َُِّْࣹࣺࣴࣵࣷࣸ", number:"", punctuation:"،؛؟‘’“”…", symbol:"", other:"\u{061C}\u{200C}\u{200D}\u{200E}\u{200F}\u{2060}\u{2066}\u{2067}\u{2068}\u{2069}", otheraux:"\u{202A}\u{202B}\u{202C}", aux:"%,;?[]«»–—‰‹›", 
 orth:`Arabic. &nbsp; Limited usage. <a href="../scripts/arab/wo.html" target="_blank">Details</a>.`,
 also:
 ["wo | latn | Primary usage. <a href='../scripts/latn/wo.html' target='_blank'>Details</a>",
