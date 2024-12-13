9b29ea6b0e54e486d4ac0ca2c7a0d2795a8be90d
r12a <ishida@w3.org>
2024-12-13 07:46:48 +0000

    bass   Complete char usage data.  Redo maps. (again)

--- bass/bsq-langdata.js
+++ bass/bsq-langdata.js
@@ -11,7 +11,7 @@ var langs = {
 "bsq-bass": { name:"Bassa", local:"𖫔𖫧𖫳𖫒𖫨𖫰𖫨𖫱", localtrans:"(Ɓǎsɔ́ɔ̀)", silcode:"bsq", 
 source:"feb8d27835761376c21d0c994cb1c128010d1ac0", 
 region:"afr", countries:"Liberia", script:"bass", speakers:"648000", 
-letter:"𖫐𖫑𖫒𖫓𖫔𖫕𖫖𖫗𖫘𖫙𖫚𖫛𖫜𖫝𖫞𖫟𖫠𖫡𖫢𖫣𖫤𖫥𖫧𖫨𖫩𖫪𖫫𖫬𖫭", letteraux:"𖫦", mark:"𖫰𖫱𖫲𖫳𖫴", markaux:"", number:"", numberaux:"", punctuation:"“”𖫵", punctuationaux:"", symbol:"", symbolaux:"", other:"", otheraux:"", aux:"!():;§«»ʼ‐‑–—‘’†‡…′″", deprecated:"", 
+letter:"𖫐𖫑𖫒𖫓𖫔𖫕𖫖𖫗𖫘𖫙𖫚𖫛𖫜𖫝𖫞𖫟𖫠𖫡𖫢𖫣𖫤𖫥𖫦𖫧𖫨𖫩𖫪𖫫𖫬𖫭", mark:"𖫰𖫱𖫲𖫳𖫴", number:"", punctuation:"“”𖫵", symbol:"", other:"", aux:"!():;§«»ʼ‐‑–—‘’†‡…′″", 
 orth:`[Bassa Vah] &nbsp; Rediscovered in the Americas at the start of the 20th century. Now used mainly by older people. <a href="../scripts/bassavah/">Details</a>.`,
 also:
 ["bsq | latn | Primary usage."],
