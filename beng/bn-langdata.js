6807b3ecaca24e54cbb7d3f8b34985a52d91f9bb
r12a <ishida@w3.org>
2024-12-14 09:15:13 +0000

    bn  Complete char usage data.   Redo maps.

--- beng/bn-langdata.js
+++ beng/bn-langdata.js
@@ -11,7 +11,7 @@ var langs = {
 "bn": { name:"Bengali (Bangla)", local:"বাংলা", localtrans:"/ˈbʌŋlə/", silcode:"ben", 
 source:"10e4909416eed2f0279eb7996f90c6bab37281e2,cldr_bn,udhr_ben", 
 region:"sasia", countries:"Bangladesh, India", script:"beng", speakers:"260000000", 
-letter:"ʼঅআইঈউঊঋএঐওঔকখগঘঙচছজঝঞটঠডঢণতথদধনপফবভমযরলশষসহঽৎড়য়", letteraux:"ঢ়", mark:"ঁংঃ়ািীুূৃেৈোৌ্", markaux:"", number:"০১২৩৪৫৬৭৮৯", numberaux:"", punctuation:"।॥‘’“”", punctuationaux:"", symbol:"৳৺", symbolaux:"", other:"\u200C\u200D", otheraux:"", aux:"§͏ঀৼ৽–—†‡…′″", deprecated:"", 
+letter:"ʼঅআইঈউঊঋএঐওঔকখগঘঙচছজঝঞটঠডঢণতথদধনপফবভমযরলশষসহঽৎড়য়", letteraux:"ঢ়", mark:"ঁংঃ়ািীুূৃেৈোৌ্", number:"০১২৩৪৫৬৭৮৯", punctuation:"।॥‘’“”", symbol:"৳৺", other:"\u{200C}\u{200D}", aux:"͏ঀৼ৽–—…", 
 orth:`[Bengali] &nbsp; <a href="../scripts/bengali" target="_blank">Details</a>.`,
 also:
 ["bn-newa | newa | No longer in use."],
