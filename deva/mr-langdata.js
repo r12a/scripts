2f3929d25dc69c6cc575b77cfaa3d822b60c1b9e
r12a <ishida@w3.org>
2025-04-10 14:47:04 +0100

    mr term list

---
+++ deva/mr-langdata.js
@@ -0,0 +1,36 @@
+// this should always be updated when app-charuse/langs.js is updated
+
+    const Y = true
+    const N = false
+    const P = 'partial'
+    const U = '?'
+
+
+var langs = {
+
+"mr": { name:"Marathi", local:"मराठी", localtrans:"marāṭhī", silcode:"mar", 
+source:"cldr_mr,udhr_mar", 
+region:"sasia", countries:"India", script:"deva", speakers:"73000000", 
+letter:"ऱॐअआइईउऊऋऌऍएऐऑओऔकखगघङचछजझञटठडढणतथदधनपफबभमयरलवशषसहळऽ", mark:"़ंँःािीुूृॅेैॉोौ्", punctuation:"‐–—…‘’“”′″","number":"१२३४५६७८९०", other:"‌‍", 
+orth:``,
+also:
+[],
+linked:"", picker:"", font:"",
+notes:"CLDR also includes <span class='codepoint'><span lang='hi'>&#x25CC;&#x0944;</span> [<span class='uname'>U+0944 DEVANAGARI VOWEL SIGN VOCALIC RR</span>]</span>, but I was told that Indian experts determined that this is only needed for Sanskrit, so it is not shown here."
+},
+
+
+"deva": {name:"Devanagari", region:"sasia", type:"abug", 
+blocks:3, chars:170, bletter:90, bmark:53, bnumber:10, bpunctuation:17, bsymbol:0, bother:0, 
+direction:"ltr", rtlnumbers:N,
+voweltype: { inherent:1, letters:Y, marks:Y, hidden:N, visorder:N, vocalics:Y, ivowels:Y, vbase:"", vcomposite:N, prebase:Y, circum:N, },
+case:N, cursive:N, mcchars:Y, conjuncts:Y, ligs:N, 
+medials:"", finals:"cm",
+clusters:{ ligation:Y, stacks:Y, touch:N, conjoined:Y, subjoinedcp:N, visviram:N, diacritic:N, killer:"v"},
+wordsep:"space",
+linebreak:"word", hyphen:"yes -", wordspan:N, justification:"sp", gc:N, baseline:"hang", 
+},
+
+
+
+}
