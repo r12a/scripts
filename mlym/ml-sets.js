5cbee2c9d29c4ed57af2818a4b9d971f51b414f3
r12a <ishida@w3.org>
2025-04-13 09:11:46 +0100

    mlym/ml-sets   Add circumgraphs & prebase

--- mlym/ml-sets.js
+++ mlym/ml-sets.js
@@ -10,6 +10,8 @@ var collections = [
 { symbol:'ⓥ', desc:'Dependent vowels', chars:'(െ|േ|ൈ|ൊ|ോ|ൌ|ി|ീ|ു|ൂ|ാ|ൗ)' },
 { symbol:'Ⓥ', desc:'Independent vowels', chars:'(ഇ|ഈ|ഉ|ഊ|എ|ഏ|ഒ|ഓ|അ|ആ|ഐ|ഔ)' },
 { symbol:'🅥', desc:'Vocalics', chars:'(ഋ|ൠ|ഌ|ൡ|ൃ)' },
+{ symbol:'🆅', desc:'Circumgraphs', chars:'(ൊ|ോ|ൌ)' },
+{ symbol:'🅿', desc:'Pre-base', chars:'(െ|േ|ൈ)' },
 { symbol:'Ⓒ', desc:'All consonants', chars:'(പ|ബ|ത|ദ|ട|ഡ|ക|ഗ|ഫ|ഭ|ഥ|ധ|ഠ|ഢ|ഖ|ഘ|ച|ജ|ഛ|ഝ|സ|ഷ|ശ|ഹ|മ|ന|ണ|ഞ|ങ|വ|ര|റ|ഴ|ല|ള|യ)' },
 { symbol:'Ⓕ', desc:'Finals', chars:'(ൿ|ൺ|ൻ|ർ|ൽ|ൾ|ം|്|ഃ)' },
 { symbol:'ⓒ', desc:'Chillus', chars:'(ൿ|ൺ|ൻ|ർ|ൽ|ൾ)' },
