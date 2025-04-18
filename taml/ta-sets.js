ae6d0c883b2a672fe8aff7df7e3cdb4372116098
r12a <ishida@w3.org>
2025-04-18 07:58:04 +0100

    Tamil refresh.  Added several thousand term entries & rewrote all transcriptions.

--- taml/ta-sets.js
+++ taml/ta-sets.js
@@ -13,4 +13,5 @@ var collections = [
 { symbol:'Ⓑ', desc:'Basic consonants', chars:'(ப|த|ச|ட|க|ம|ந|ன|ண|ஞ|ங|வ|ர|ற|ழ|ல|ள|ய)' },
 { symbol:'Ⓔ', desc:'Extended consonants', chars:'(ஜ|ஸ|ஶ|ஷ|ஹ)' },
 { symbol:'⊙', desc:'Invisibles', chars:'(\u200C|\u200D)' },
+{ symbol:'ⓧ', desc:'Virama', chars:'(்)' },
 ]
