110f491f18ad8cef76b713d6a5c16f9636063c30
r12a <ishida@w3.org>
2025-04-18 11:40:10 +0100

    taml/ta-sets  Add nasals

--- taml/ta-sets.js
+++ taml/ta-sets.js
@@ -12,6 +12,7 @@ var collections = [
 { symbol:'Ⓒ', desc:'All consonants', chars:'(ப|த|ச|ட|க|ம|ந|ன|ண|ஞ|ங|வ|ர|ற|ழ|ல|ள|ய|ஜ|ஸ|ஶ|ஷ|ஹ)' },
 { symbol:'Ⓑ', desc:'Basic consonants', chars:'(ப|த|ச|ட|க|ம|ந|ன|ண|ஞ|ங|வ|ர|ற|ழ|ல|ள|ய)' },
 { symbol:'Ⓔ', desc:'Extended consonants', chars:'(ஜ|ஸ|ஶ|ஷ|ஹ)' },
+{ symbol:'🅽', desc:'Nasals', chars:'(ம|ந|ன|ண|ஞ|ங)' },
 { symbol:'⊙', desc:'Invisibles', chars:'(\u200C|\u200D)' },
 { symbol:'ⓧ', desc:'Virama', chars:'(்)' },
 ]
