d3644d36ad0265eacbead2a5237165ba823f1bba
r12a <ishida@w3.org>
2025-04-30 10:57:15 +0100

    arb   Rewrite

--- arab/arb-sets.js
+++ arab/arb-sets.js
@@ -10,5 +10,6 @@ var collections = [
 { symbol:'ⓗ', desc:'Hamza', chars:'(ء|آ|أ|إ|ؤ|ئ|ة)' },
 { symbol:'Ⓒ', desc:'All consonants', chars:'(ا|و|ي|ج|ظ|ب|ت|د|ط|ض|ك|ق|ف|ث|ذ|س|ص|ز|ش|خ|غ|ه|ح|ع|م|ن|ر|ل)' },
 { symbol:'ⓒ', desc:'Consonants with no matres', chars:'(ج|ظ|ب|ت|د|ط|ض|ك|ق|ف|ث|ذ|س|ص|ز|ش|خ|غ|ه|ح|ع|م|ن|ر|ل)' },
+{ symbol:'🅒', desc:'IPA consonants', chars:'(b|d|f|h|j|k|l|m|n|p|q|r|s|t|v|w|x|z|ð|ħ|ɣ|ɫ|ʃ|ʒ|ʔ|ʕ|ʰ|θ)' },
 { symbol:'⊙', desc:'Invisibles', chars:'(\u200C|\u200D|2067|202B|2066|202A|2068|2069|202C|200F|200E|061C|034F)' },
 ]
