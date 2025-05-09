f68dffb8eab0218028fd0fea1e274d4b45ad7560
r12a <ishida@w3.org>
2025-05-09 07:46:23 +0100

    am   Add 500 terms. Give an example for each syllable. Various editorial improvements.

--- ethi/am-sets.js
+++ ethi/am-sets.js
@@ -9,5 +9,6 @@
 var collections = [
 { symbol:'Ⓥ', desc:'Vowels', chars:'(አ|ኡ|ኢ|ኣ|ኤ|እ|ኦ|ኧ|ዐ|ዑ|ዒ|ዓ|ዔ|ዕ|ዖ)' },
 { symbol:'ⓒ', desc:'Latin consonants', chars:'(b|c|d|f|g|h|j|k|l|m|n|p|q|r|s|t|v|w|x|y|z)' },
-{ symbol:'ⓥ', desc:'Latin  vowels', chars:'(a|e|i|o|u)' },
+{ symbol:'🅥', desc:'Latin  vowels', chars:'(a|e|i|o|u)' },
+{ symbol:'🅒', desc:'IPA  consonants', chars:'(p|b|pʼ|t|d|tʼ|k|ɡ|kʼ|ʦʼ|f|s|z|h|m|n|w|ɾ|r|l|j|ʔ|t͡ʃ|d͡ʒ|v|β|ʃ|ʒ|x|xʼ|ɲ|1|ŋ|kʷ|kʼʷ|ɡʷ|hʷ)' },
 ]
