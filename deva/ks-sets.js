f85d5c5198181eab349ab81355ab9487be525e7a
r12a <ishida@w3.org>
2025-05-15 09:12:27 +0100

    deva-ks  Wide-ranging revisions

--- deva/ks-sets.js
+++ deva/ks-sets.js
@@ -12,4 +12,6 @@ var collections = [
 { symbol:'🅥', desc:'Vocalics', chars:'(ऋ|ृ)' },
 { symbol:'Ⓒ', desc:'All consonants', chars:'(प|फ|ब|त|थ|द|ट|ठ|ड|क|ख|ग|च|छ|ज|व|स|श|ह|म|न|र|ल|य|ज़|ण|ञ|ङ|भ|ध|ढ|झ|घ|ष|ज़|ण|ञ|ङ|भ|ध|ढ|झ|घ|ष)' },
 { symbol:'⊙', desc:'Invisibles', chars:'(\u200C|\u200D)' },
+{ symbol:'🆅', desc:'Virama', chars:'(\u094D)' },
+{ symbol:'🅲', desc:'IPA consonants', chars:'(ŋ|k|kʰ|ɡ|ɡʱ|ŋ|t͡ʃ|t͡ʃʰ|d͡ʒ|ɟʰ|n|ʈ|ʈʰ|ɖ|ɖʱ|ɳ|t|tʰ|d|dʱ|p|pʰ|b|bʱ|m|j|r|l|v|w|ʃ|ʂ|s|h|z)' },
 ]
