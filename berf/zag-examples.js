c10612f790e5467ad87744c9141bce9fb2b56035
r12a <ishida@w3.org>
2025-05-17 06:52:44 +0100

    berf/zag-examples   Add 70 wiktionary entries

--- berf/zag-examples.js
+++ berf/zag-examples.js
@@ -16,16 +16,90 @@ native | meaning | IPA① | transcription | other transcriptions② | notes | wi
 
 
 
+/* THE WIKTIONARY ENTRIES HAVE BEEN GENERATED MANUALLY FROM ROMANISED FORMS */
 
 
 autoExpandExamples.zag = `
+
+@ https://en.wiktionary.org/w/index.php?title=Category:Zaghawa_terms_with_IPA_pronunciation
+
+𖺻𖺼𖺻|father|ʔa§b§a|aba|||aba
+𖺻𖺼𖻂𖻌𖻂|lips|ʔa§b§ɪ§ɾ§ɪ|abiri|||abiri
+𖺻𖺽𖺾|beard|ʔa§d§ɛ|ade|||ade
+𖺻̄𖻆𖺾𖻌𖻂𖻄𖺻𖻇𖻂|high-quality|a§m§e§r§i˥˩§k§a§n§i˥˩|amerîkanî|||amerîkanî
+𖺻𖻇𖻂|ant-eater|ʔa§n§ɪ|ani|||ani
+𖺻𖻌𖺻𖻒|Arab|ʔa§r§a§w|araw|||araw
+𖺻𖻌𖺽𖻂|donkey|ʔa§ɾ§d§ɪ|ardi|||ardi
+𖺻𖻓|iron|a§j|ay|||ay
+𖺼𖺻|arm, hand|b§a|ba|||ba
+𖺼𖺻𖻀𖻊|woman, wife|b§a˥§ɡ§ɔ˥|bago|||bago
+𖺼𖻂̄|water|b§i|bi|||bi
+𖺼𖻂𖻉𖺼𖻂𖻉|wasp|b§ɪ§ŋ§b§ɪ§ŋ|bingbing|||bingbing
+𖺼𖻊̄|bull|b§o|bo|||bo
+𖺼𖻊̄𖻀𖻑|father-in-law|b§o§ɡ§u˥˩|bogû|||bogû
+𖺼𖻊𖻌𖻑|man|b§ɔ§r§ʊ|boru|||boru
+𖺼𖻑𖻌|little, baby|b§ʊ§ɾ|bur|||bur
+𖺽𖺻|and|d§a|da|||da
+𖺽𖺾̄𖺽𖻂|twenty|d§e§d§i˩˥|dedi|||dedi
+𖺽𖺾̄𖻇𖻂|snake|d§e§n§i|denî|||denî
+𖺽𖺾𖻎𖻐𖺾|six|d§ɛ§s§t§ɛ|deste|||deste
+𖺽𖺾̄𖻓|foot, leg|d§e§j|dey|||dey
+𖺽𖻂𖻎𖻐𖻂|seven|d§ɪ§s§t§ɪ|disti|||disti
+𖺽𖻂̄|camel|d§i|dî|||dî
+𖺽𖻂̄𖻎𖻐𖻂|nine|d§i§s˥˩§t§i˥˩|dîstî|||dîstî
+𖺽𖻊̄|tired|d§o˥˩|dô|||dô
+𖺾̄𖺼𖺾|year|e§b§e|ebe|||ebe
+𖺾̄𖻀𖻂|my|ʔe§ɡ§i˥˩|egî|||egî
+𖺿𖻊𖻌𖻊|rabbit|f§ɔ§ɾ§ɔ|foro|||foro
+𖻀𖺻𖻌𖺽𖻂|trail, path|ɡ§a§ɾ§d§ɪ|gardi|||gardi
+𖻀𖺾|sleep|ɡ§ɛ|ge|||ge
+𖻁𖻂𖻎𖺻𖺼|number|h§ɪ§s§a§p|hisab|||hisab
+𖻁𖻂̄𖻌𖻂̄|cow|h§i˥˩§r§i˥˩|hîrî|||hîrî
+𖻁𖻑̄𖻐|fish|h§u§t˥˩|hût|||hût
+𖻁𖻑̄𖻓|five|h§u§j˥˩|hûy|||hûy
+𖻂𖻎𖻐𖻂|four|ʔɪ§s§t§ɪ|isti|||isti
+𖻃𖺾𖻇|fast|ɟ§ɛ§n|jen|||jen
+𖻃𖺾𖻇𖺽𖻑|quickly|ɟ§ɛ§n§d§ʊ|jendu|||jendu
+𖻃𖻂𖻓𖺾|fire|ɟ§ɪ§j§ɛ|jiye|||jiye
+𖻃𖻊𖺼|pocket|ɟ§ɔ§p|job|||job
+𖻄𖻂𖻍𖺻|mother|k§ɪ§ɾ§a|kira|||kira
+𖻄𖻂𖻌𖺻𖻇𖻂|maternal family|k§ɪ§r§a§n§ɪ|kirani|||kirani
+𖻄𖻂𖻎𖻂𖻍|father of|k§ɪ§s§ɪ§ɾ|kisir|||kisir
+𖻄𖻂𖻎𖻂𖻍𖻇𖻂|paternal family|k§ɪ§s§ɪ§ɾ§n§ɪ|kisirni|||kisirni
+𖻄𖻊̄𖻍|very|k§o§ɾ|kor|||kor
+𖻄𖻌𖺻𖻓𖻊𖻇|pencil|k§ɾ§a§j§õ§‹|krayon|||krayon
+𖻆𖺻𖻇𖺽𖺻𖻒𖺻|peanut|m§a§n§d§a§w§a|mandawa|||mandawa
+𖻆𖺻𖻍|star|m§a§ɾ|mar|||mar
+𖻇𖺻|you|n§a|na|||na
+𖻇𖺾𖻉𖻀𖻂𖻌|squirrel|n§ɛ§ŋ§ɡ§ɪ§r|nengir|||nengir
+||␣|-ni|
+𖻇𖻑̄𖻒|you|n§u§w|nuw|||nuw
+𖻇𖻂̄|one|n§i˥˩|nî|||nî
+𖻊𖻐𖻐𖺾|eight|ʔɔ§t§t§ɛ|otte|||otte
+𖻍𖺻|and|ɾ§a|ra|||ra
+𖻎𖺻𖺼𖻂𖻐|Saturday|s§a§b§i§t|sabit|||sabit
+𖻎𖻊𖻀𖻊𖺽𖻂|ten|s§ɔ§ɡ§ɔ§d§ɪ|sogodi|||sogodi
+𖻎𖻑𖻒𖻂|two|s§ʊ§w§ɪ|suwi|||suwi
+𖻐𖺻𖻓𖺾𖻌𖻂|to light a fire|t§a§j§ɛ§ɾ§ɪ˥|tayerí||tayerí|
+𖻐𖺾̄𖻀𖺾𖻌𖺻|candidate|t§e§ɡ§e§ɾ§a|tegera|||tegera
+𖻐𖺾̄𖻐𖻂|short|t§e§t§i˥˩|tetî|||tetî
+𖻐𖺾̄𖻓|orphan|t§e§j|tey|||tey
+𖻐𖻂𖻆𖻎𖺻|crocodile|t§ɪ§m§s§a|timsa|||timsa
+𖻐𖻊̄𖺽𖻑|heart|t§o§d§u|todu|||todu
+𖻐𖻏𖺻𖺽|Chad|c§‹§a§t|tshad|||tshad
+𖻐𖻂̄𖻓𖺾𖻌|axe|t§i§y§e§ɾ|tîyer|||tîyer
+𖻐𖻂̄𖻓𖻂𖻌|root|t§i§y§i§r|tîyîr|||tiyir
+𖻐𖻑̄𖻐𖻑𖻌𖺽𖺻|termite mound|tiyir|tûrtûrda|||tûrtûrda
+𖻑𖻌𖺽𖺾|spear|ʔʊ§ɾ§d§ɛ|urde|||urde
+𖻒𖺾̄|three|w§e|we|||we
+𖻓𖻊𖻆|day|j§ɔ§m|yom|||yom
+
 
 
 
 @ https://www.unicode.org/L2/L2024/24004r-zaghawa-proposal.pdf
 
 𖻎𖻂𖺿𖻂𖻌|zero|s§ɪ§f§ɪ§ɾ|
-𖺽𖻂̄𖻎𖻐𖻂|nine|d§i§s§t§i|
 𖻐𖻂̄𖻆|a thousand|t§i§m
 𖺦𖻂̄𖻍𖻂𖺼𖺻|Iriba|h§i§ɻ§i§b§a
 𖺢𖺻𖻌 𖺡𖻂̄𖻍𖻂𖺻|Dar Bilia|d§a§ɾ§ §b§i§ɻ§i§a
@@ -38,7 +112,6 @@ autoExpandExamples.zag = `
 𖻐𖺻𖻌𖻃𖺻𖻆𖺻 𖺽𖺾|practice|t§a§ɾ§ɟ§a§m§a§ §d§ɛ|
 𖺼𖺻𖺾𖻌𖺻𖻌𖺻𖻂|linguistics|b§a§ɛ§ɾ§a§ɾ§a§ɪ|
 𖻆𖻌𖻀𖻂|mouth|m§ɾ§ɡ§ɪ
-𖺼𖺻|hand|b§a
 𖻄𖻌𖻊|knee|k§r§ɔ
 𖺽𖻂 𖻀𖻊𖻅𖻀𖻅𖻊|ankle|d§ɪ§ §ɡ§ɔ§l§ɡ§l§ɔ
 𖺼𖺻 𖻃𖻊|elbow|b§a§ §ɟ§ɔ
@@ -52,7 +125,6 @@ autoExpandExamples.zag = `
 𖺼𖻌𖻌𖺾|dog|b§ɾ§ɾ§ɛ
 𖺼𖺾𖻌|he, she|b§ɛ§ɾ
 𖺼𖺾𖻌𖻌|they|b§ɛ§ɾ§ɾ
-𖻇𖺻|you|n§a
 𖻇𖻊𖻒|you (pl)|n§ɔ§w
 𖺡𖺾𖻌𖻂𖺻 𖺥𖻂𖻌𖺻𖻓 𖺣𖻌𖺿𖺾𖺾|Beria Giray Erfe|b§ɛ§r§ɪ§a§ §ɡ§ɪ§r§a§j §ɛ§r§f§ɛ§‹
 
@@ -70,7 +142,6 @@ autoExpandExamples.zag = `
 𖻎𖻊̄𖺼𖻑|ashes|s§o§b§u
 𖺼𖻂̄𖻌𖻂|dog|b§i§ɾ§i
 𖺼𖻂̄𖻂|water|b§iː§‹
-𖻂𖻎𖻐𖻂|four|ɪ§s§t§ɪ
 𖻐𖻂𖺼𖺾|field|t§ɪ§b§ɛ
 𖻆𖻂𖻎𖺻|pot|m§ɪ§s§a
 𖻇𖻊𖺽𖻂|heron|n§ɔ§d§ɪ
@@ -81,15 +152,16 @@ autoExpandExamples.zag = `
 𖻆𖻂𖻂|calf|m§ɪː§‹
 𖻑𖻌𖻂|giraffe|ʊ§ɾ§ɪ
 𖻑𖺽𖺻|fresh milk|ʊ§d§a
-𖺼𖻑𖻌|baby|b§ʊ§ɾ
 𖻐𖻑𖺽𖻑|back|t§ʊ§d§ʊ
 𖻂̄𖻎𖺾|food|i§s§e
 𖺽𖻂̄𖻆𖺾|wild cat|d§i§m§e
 𖻐𖻂̄𖻇𖺾|thief|t§i§n§e
 𖺾̄𖻇𖻂|riverbed|e§n§i
 𖻊̄𖻌𖻂|nice smell|o§r§i
-𖻐𖻊̄𖺽𖻑|heart|t§o§d§u
 𖻊̄𖻇𖻂|behind|o§n§i
+
+
+
 
 
 
