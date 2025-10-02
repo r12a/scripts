45615293dc0af867c4b3c44ce994cdfc76cced37
r12a <ishida@w3.org>
2025-10-02 16:07:07 +0100

    adlm/fuf-langdata   Add country

--- adlm/fuf-langdata.js
+++ adlm/fuf-langdata.js
@@ -4,7 +4,7 @@ var langs = {
 
 "fuf-adlm": { name:"Pular", local:"𞤀𞤣𞤤𞤢𞤥", localtrans:"adlam", silcode:"", rtl:true, scriptname:"𞤀𞤣𞤤𞤢𞤥", scriptnametrans:"adlam", 
 source:"9ee024aca4e4745c9b779ef3ee0697c42700d2ca,udhr_fuf_adlm", 
-region:"afr", countries:"", script:"adlm", speakers:"3000000", 
+region:"afr", countries:"Senegal", script:"adlm", speakers:"3000000", 
 letter:"ـ𞤀𞤁𞤂𞤃𞤄𞤅𞤆𞤇𞤈𞤉𞤊𞤋𞤌𞤍𞤎𞤏𞤐𞤑𞤒𞤓𞤔𞤕𞤖𞤗𞤘𞤙𞤚𞤛𞤜𞤝𞤞𞤟𞤠𞤡𞤢𞤣𞤤𞤥𞤦𞤧𞤨𞤩𞤪𞤫𞤬𞤭𞤮𞤯𞤰𞤱𞤲𞤳𞤴𞤵𞤶𞤷𞤸𞤹𞤺𞤻𞤼𞤽𞤾𞤿𞥀𞥁𞥂𞥃𞥋", mark:"𞥊𞥄𞥅𞥆𞥇𞥈𞥉", number:"𞥐𞥑𞥒𞥓𞥔𞥕𞥖𞥗𞥘𞥙", punctuation:"؟“”…⁏⹁𞥞𞥟", symbol:"", other:"\u{200C}\u{200D}\u{200E}\u{200F}\u{202A}\u{202B}\u{202C}\u{2066}\u{2067}\u{2068}\u{2069}", aux:"%,;[]«»ʼ‑–—‘’‰‹›", 
 orth:`[Adlam] &nbsp; Since 1989, in development. <a href="../scripts/adlam/">Details</a>`,
 also:
