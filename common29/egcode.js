25531233f40d7edb0ae4ed35a488bb3b3da46fd9
r12a <ishida@w3.org>
2025-10-05 17:03:33 +0100

    common29/egcode   Make it possible to customise the items listed in an example

--- common29/egcode.js
+++ common29/egcode.js
@@ -13,6 +13,190 @@ var egList = {}
 
 function addExamples (langFilter) {
     // console.log('addExamples(',langFilter,')\n\tConvert all .eg items to full markup.')
+    /*  read the data into egList, in which each record has
+	example, meaning, transcription?, alt?, narrow transcription, wiktionary
+	alt is generally a vowelled form for abjads, an alternative spelling, or alternative script
+    
+    if the classList doesn't contain any of ipa, transc, narrow, meaning:
+    output will include ipa, meaning by default
+    if there's no ipa but there is a transc, that will be output
+    
+    otherwise, output will include only those items called for from the classList
+    ipa: the ipa transcription
+    narrow:a narrow phonetic transcription (some term lists use that column for notes)
+    transc: the default latin transcription
+    meaning: the meaning
+    
+    any of the above can be included together or can be listed alone
+        
+    langFilter, class name used to select blocks to add examples to
+    
+    [GLOBALS]
+    autoExpandExamples, obj, set above, then populated under .<langFiler> in xx-examples.js; holds all vocab
+    
+    [CLASSES]
+    The calling element can have class names with the following meanings:
+    inline      don't make into a separate, large text block
+    transc      used to force display of the transcription rather than the IPA
+    alt         displays the IPA/transcription with the special transcription (eg. vowelled version or other orthography than Latin)
+    narrow      displays the narrow IPA if there is one in the penultimate field
+    vertical    applies a vertical writing mode, eg. for Mongolian
+    
+    [LOCALS]
+    egArray, array, built from autoExpandExamples
+    egList, object, list of vocab filtered out by langFilter with native word as key
+    selector, a selector for searching for letter blocks
+    nodes, node array, example nodes to expand
+    out, str, the generated markup
+    ipa, str, an IPA value
+    transcription, str, a transcription value
+    i, n, counters
+    temp, temptemp
+    */
+    
+	if (typeof langFilter === 'undefined') alert('addExamples call needs to specify a language')
+
+    if (typeof autoExpandExamples[langFilter] === 'undefined') {
+        console.log('%c' + 'autoExpandExamples[langFilter] fails for '+langFilter+'  (addExamples). Check the language setting on the link element.', 'color:' + 'red' + ';font-weight:bold;')
+        return
+        }
+    var egArray = autoExpandExamples[langFilter].split("\n")
+
+    for (var i=0;i<egArray.length;i++) {
+		if (egArray[i] == '') continue
+		var temp = egArray[i].split('|')
+		egList[temp[0]] = egArray[i]
+		}
+
+    // find the nodes that correspond to the language in langFilter
+	var selector = '.eg[lang='+langFilter+']'
+	var nodes = document.querySelectorAll(selector)
+	// console.log(nodes.length,' example nodes to expand')
+	
+	for (var n=0;n<nodes.length;n++) {
+		//console.log('Looking for ',nodes[n].textContent)
+		//console.log('Language is ',nodes[n].lang)
+        
+        // example of raw data: 
+        // 𑻣𑻵𑻣𑻵|mute, dumb|ˈpe§pe|pepe|ᨄᨙᨄᨙ|ˈpʰɛ.pʰɛ|pepe
+        // term|meaning|ipa|transcription|alt|narrow transc/notes|source
+
+		if (nodes[n].lang === langFilter && egList[nodes[n].textContent]) {
+			temp = egList[nodes[n].textContent].split('|')
+            
+            // console.log('addExamples:',egList[nodes[n].textContent])
+            // get the available data
+            var termdata = egList[nodes[n].textContent].replace(/␣/g,'').split('|')
+            var term = termdata[0]
+            var meaning = termdata[1]
+            var ipa = termdata[2]
+            var transc = termdata[3]
+            var notes = termdata[4]
+            var alt = termdata[4]
+            var narrow = termdata[5]
+            
+            // choose the source pointer field depending on whether alt has been used
+            var source
+            if (termdata.length == 7) source = termdata[6]
+            else source = termdata[5]
+            
+            // console.log('term',term,'meaning',meaning,'ipa',ipa,'transc', transc,'notes',notes,'source',source)
+            
+            if (ipa) var cleanIPA = ipa.replace(/§/g,'').replace(/–/g,'').replace(/‹/g,'').replace(/›/g,'')
+            else cleanIPA = ''
+            if (nodes[n].classList.contains('transc')) var forceTranscription = true
+            else forceTranscription = false
+            
+            // figure out which items to show
+            //if (nodes[n].classList.contains('transc')) var forceTranscription = true
+            
+            
+            // start the charExample element
+			var out = '<span class="charExample'
+			if (nodes[n].classList.contains('inline')) out += ' inline'
+            out += '"'
+			if (nodes[n].dir === 'rtl') out += ' dir="rtl"'
+			out += ' translate="no">'
+			
+			// add the .ex element, with onclick
+			out += `<bdi class="ex`
+            if (nodes[n].classList.contains('vertical')) out += ' vertical'
+            out += `" lang="${ nodes[n].lang }"`
+			if (nodes[n].dir === 'rtl') out += ' dir="rtl"'       
+            
+            out += `  onclick="showCharDetailsInline('${ term }', '${ nodes[n].lang }', window.blockDir, '', document.getElementById('panel'), '', '', '${ ipa }', this)"`
+			out += '>'
+			out += term
+			out += '</bdi>'
+            
+            //console.log('CLASSLIST',nodes[n].classList)
+            
+            // add an alternate transcription if requested
+            if (nodes[n].classList.contains('alt')) {
+                out += ` &nbsp;≡&nbsp; <bdi class="ex`
+                if (nodes[n].classList.contains('vertical')) out += ' vertical'
+                out += `" lang="${ nodes[n].lang }"`
+                if (nodes[n].dir === 'rtl') out += ' dir="rtl"'            
+                out += `  onclick="showCharDetailsInline('${ alt }', '${ nodes[n].lang }', window.blockDir, '', document.getElementById('panel'), '', '', '${ ipa }', this)"`
+                out += '>'
+                out += alt
+                out += '</bdi>'
+                }
+                
+			
+            // bail if there is a nometadata class name
+            // this is used principally for maps with non-pointed examples
+            if (nodes[n].classList.contains('short')) {
+                out += '</span>'
+                nodes[n].outerHTML = out
+                continue
+                }
+            
+            
+            // if no other classes, then assume just ipa or transc if there's no ipa
+            if (! nodes[n].classList.contains('ipa') &&! nodes[n].classList.contains('transc') && ! nodes[n].classList.contains('narrow') && ! nodes[n].classList.contains('meaning')) {
+                if (ipa) out += ' <bdi class="ipa">'+cleanIPA+'</bdi>'
+                else if (transc) {
+                    out += ' <bdi class="transc"'
+                    if (nodes[n].dir === 'rtl') out += ' dir="rtl"'
+                    out += `>${ transc }</bdi>`
+                    }
+                out += ` <bdi class="meaning">${ meaning }</bdi>`
+                }
+            
+            // otherwise, output only the items for which a class name exists
+            else {
+            
+                // add the ipa, if the .ipa attribute is set
+                if (nodes[n].classList.contains('ipa')) out += ' <bdi class="ipa">'+cleanIPA+'</bdi>'
+
+                 // add a transcription, if the .transc attribute is set
+                if (nodes[n].classList.contains('transc')) {
+                    out += ' <bdi class="transc"'
+                    if (nodes[n].dir === 'rtl') out += ' dir="rtl"'
+                    out += `>${ transc }</bdi>`
+                    }
+
+                // add the narrow phonetic transcription, if available
+                if (nodes[n].classList.contains('narrow')) out += ` <bdi class="ipa narrow">${ narrow }</bdi>`
+
+                // add the meaning, if available
+                if (nodes[n].classList.contains('meaning')) out += ` <bdi class="meaning">${ meaning }</bdi>`
+                }
+			
+			out += '</span>'
+			
+			nodes[n].outerHTML = out
+			}
+		}
+    if (! typeof showTransliterations === 'undefined') showTransliterations( document.getElementById('translitToggleCheckbox').checked )
+	egArray = []
+	//egList = {}
+	}
+
+
+function addExamplesX (langFilter) {
+    // console.log('addExamples(',langFilter,')\n\tConvert all .eg items to full markup.')
 /*  read the data into egList, in which each record has
 	example, meaning, transcription?, notes?, alt?
 	alt is generally a vowelled form for abjads or an alternative spelling
