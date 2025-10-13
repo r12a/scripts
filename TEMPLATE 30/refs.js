0f2830b7154d3029ada7ce28d5aa76ac4b98c886
r12a <ishida@w3.org>
2025-10-13 19:03:44 +0100

    + template 30

---
+++ TEMPLATE 30/refs.js
@@ -0,0 +1,139 @@
+window.langSet = false
+
+
+
+reflist = {
+
+
+/*
+"auth_initials": {
+"authors": "Sort_by_family_name_of_first_contributor",
+"title":"The_Worlds_Writing_Systems",
+"sub": "subtitle",
+"date": "1996",
+"retr": "Feb 2024",
+"publisher":"Oxford_University_Press",
+"isbn":"0-19-507993-0",
+"pages": "xxx",
+"url":"refs",
+"lang": new Set(['ru']),
+"reviewed":false
+},
+*/
+
+"d": {
+"authors": "Peter T. Daniels and William Bright",
+"title":"The World's Writing Systems",
+"date": "1996",
+"publisher":"Oxford University Press",
+"isbn":"0-19-507993-0",
+"url":"#refs",
+"lang": new Set(['ru']),
+"reviewed":false
+},
+
+"up": {
+"authors": "Everson, Michael",
+"title":"L2/08-350: Proposal to encode modern Bamum in the BMP",
+"url":"https://www.unicode.org/L2/L2008/08350-n3522-bamum.pdf",
+},
+
+"u": {
+"authors": "Unicode Consortium",
+"title":"The Unicode Standard, Version 17.0",
+"sub":"Chapter 13.20: South and Central Asia II, Other Modern Scripts, Tangsa",
+"url":"https://unicode.org/versions/Unicode17.0.0/core-spec/chapter-13/#G51897",
+"reviewed": false
+},
+
+"wl": {
+"authors": "Wikipedia",
+"title":"Rohingya language",
+"retr": "Feb 2024",
+"url":"https://en.wikipedia.org/wiki/Rohingya_language",
+"reviewed":false
+},
+
+"ws": {
+"authors": "Wikipedia",
+"title":"Bamum script",
+"retr": "Feb 2024",
+"url":"https://en.wikipedia.org/wiki/Bamum_script",
+"reviewed":false
+},
+
+"o": {
+"authors": "Omniglot",
+"title":"Bamum syllabary",
+"url":"https://www.omniglot.com/writing/bamum.htm",
+"reviewed":false
+},
+
+"eth": {
+"authors": "Eberhard, David M., Gary F. Simons, and Charles D. Fennig (eds.)",
+"title":"Ethnologue: Languages of the World. Twenty-sixth edition",
+"sub": "Rejang",
+"date": "2023",
+"retr": "Feb 2024",
+"publisher":"SIL International",
+"url":"https://www.ethnologue.com/language/rej/",
+"reviewed":true
+},
+
+"sil": {
+"authors": "SIL Writing Systems Technical Resources",
+"title":"Unicode Status for Tangsa",
+"retr": "Oct 2025",
+"publisher":"SIL International",
+"url":"https://writingsystems.info/scrlang/unicode/tnsa-unicode/",
+"reviewed":true
+},
+
+
+
+}
+
+
+
+
+scriptLanguageList = {
+aio:{name:"Aiton"},
+ahk:{name:"Akha"},
+csh:{name:"Chin, Asho"},
+mya:{name:"Burmese", url:"index"},
+bwe:{name:"Karen, Bwe"},
+kvq:{name:"Karen, Geba"},
+int:{name:"Intha"},
+kht:{name:"Khamti"},
+lmk:{name:"Lamkang"},
+kxf:{name:"Karen, Manumanaw"},
+rmz:{name:"Marma"},
+mwt:{name:"Moken"},
+mnw:{name:"Mon"},
+obr:{name:"Burmese, Old"},
+blk:{name:"Karen, Pa'o"},
+pli:{name:"Pali"},
+phk:{name:"Phake"},
+kjp:{name:"Karen, Pwo Eastern"},
+pwo:{name:"Karen, Pwo Western"},
+rki:{name:"Rakhine"},
+pce:{name:"Palaung, Ruching"},
+rbb:{name:"Palaung, Rumai"},
+ksw:{name:"Karen, S'gaw"},
+san:{name:"Sanskrit"},
+shn:{name:"Shan", url:"mymr-shn"},
+pll:{name:"Palaung, Shwe"},
+tjl:{name:"Tai Laing"},
+kyu:{name:"Kayah, Western"},
+}
+
+
+
+
+
+
+// resources list is at the bottom of the xx-examples file
+
+
+
+
