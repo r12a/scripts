d03ccc4354cca3e8582ffefd6efd9f2e5b78a817
r12a <ishida@w3.org>
2025-10-14 05:42:12 +0100

    wsg, gon   Fix speaker numbers

--- gong/wsg-langdata.js
+++ gong/wsg-langdata.js
@@ -3,7 +3,7 @@ var langs = {
 
 "wsg": { name:"Adilabad Gondi", local:"𑵶𑶓𑶕𑶂𑶋", localtrans:"ɡoːɳɖiː", silcode:"", 
 source:"r12a", 
-region:"sasia", countries:"India", script:"gong", speakers:"300000", 
+region:"sasia", countries:"India", script:"gong", speakers:"2100000", 
 letter:"𑵠𑵡𑵢𑵣𑵤𑵥𑵧𑵨𑵪𑵫𑵬𑵭𑵮𑵯𑵰𑵱𑵲𑵳𑵴𑵵𑵶𑵷𑵸𑵹𑵺𑵻𑵼𑵽𑵾𑵿𑶀𑶁𑶂𑶃𑶄𑶅𑶆𑶇𑶈𑶉𑶘", letteraux:"", mark:"𑶊𑶋𑶌𑶍𑶎𑶐𑶑𑶓𑶔𑶕𑶖𑶗", markaux:"", number:"𑶠𑶡𑶢𑶣𑶤𑶥𑶦𑶧𑶨𑶩", numberaux:"", punctuation:"।॥", punctuationaux:"", symbol:"", symbolaux:"", other:"\u{200C}\u{200D}", otheraux:"", aux:"", deprecated:"", 
 orth:`[Gunjala Gondi] Being reactivated from an 18thC script. Telugu and Devanagari are more commonly used. <a href='../scripts/gong/wsg.html' target='_blank'>Details</a>.`,
 linked:"gong/wsg", picker:"gong", font:"Noto Sans Gunjala Gondi", 
--- gonm/gon-langdata.js
+++ gonm/gon-langdata.js
@@ -3,7 +3,7 @@ var langs = {
 
 "gon": { name:"Gondi", local:"𑴌𑴽𑴥𑴱 𑴢𑴱𑴪𑴱", localtrans:"koːjaː baːʂaː", silcode:"", 
 source:"r12a", 
-region:"sasia", countries:"India", script:"gonm", speakers:"300000", 
+region:"sasia", countries:"India", script:"gonm", speakers:"2100000", 
 letter:"𑴀𑴁𑴂𑴃𑴄𑴅𑴆𑴈𑴉𑴋𑴌𑴍𑴎𑴏𑴐𑴑𑴒𑴓𑴔𑴕𑴖𑴗𑴘𑴙𑴚𑴛𑴜𑴝𑴞𑴟𑴠𑴡𑴢𑴣𑴤𑴥𑴦𑴧𑴨𑴩𑴪𑴫𑴬𑴭𑴮𑴯𑴰𑵆", letteraux:"", mark:"𑴱𑴲𑴳𑴴𑴵𑴶𑴺𑴼𑴽𑴿𑵀𑵁𑵂𑵃𑵄𑵅𑵇", markaux:"", number:"𑵐𑵑𑵒𑵓𑵔𑵕𑵖𑵗𑵘𑵙", numberaux:"", punctuation:"।॥", punctuationaux:"", symbol:"", symbolaux:"", other:"", otheraux:"", aux:"", deprecated:"", 
 orth:`[Masaram Gondi] Emerging script, unrelated to the Gunjala Gondi script which is used for the same language. Most Gondi speakers are illiterate, and the others typically use Devanagari or Telugu to write their language. <a href='../scripts/gonm/gon.html' target='_blank'>Details</a>.`,
 notes:`gon is a macrolanguage that encompasses the following more specific primary language subtags:<br/>Adilabad Gondi [wsg]<br>Aheri Gondi [esg]<br>Northern Gondi [gno].`,
