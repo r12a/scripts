38a07521f2c60c19a7d3d2b5cf94e1461e01b4ac
r12a <ishida@w3.org>
2025-10-19 15:56:11 +0100

    kali/kyu-examples   Convert IPA tone marks to accents.

--- kali/kyu-examples.js
+++ kali/kyu-examples.js
@@ -1,17 +1,31 @@
 /*
-native | meaning | IPA① | transcription | other transcriptions② | notes | wiktionary③
+native | meaning | /IPA/① | transcription | other transcriptions② | [IPA]/notes③ | wiktionary④
 
 
 ① separate each text unit (consonant+combining_mark(s)) with §, separate phonetic syllables with .
    use ‹ and › for a sound that spans 2 text units
-   use – for a text unit that doesn't produce sound
+   use – (en dash) for a text unit that doesn't produce sound
    syllable boundary markers go inside previous unit, and emphasis markers begin emphasised unit
 
 ② the previous transcription field is always Latin. 
    if there is another key orthography (eg. Mongolian cyrillic) it goes here.
    vowelled/unvowelled alternatives also go here
 
-③ if Wiktionary spells the word differently, add the spelling here
+③ where you want to be able to distinguish broad and narrow phonetic descriptions, the narrow goes here
+	alternatively, this field may be used for notes
+
+④ if Wiktionary spells the word differently, add the spelling here
+
+
+Tones are best written in IPA using decomposed accents after the vowel, rather than \u0304 or numeric superscripts. This makes it easier to search for tones in the term lists, and produces easier to read transcriptions. To prevent normalisation problems, the accents should be represented using escapes, like \u0300.
+*/
+
+
+/* Tone markers in this list are:
+\u0300	low ˨	◌̀	¹
+\u030B high	˥	◌̋	²
+\u0304 mid	˧	◌̄	³
+\u0301 mid-high	˦	◌́	-
 */
 
 
@@ -24,162 +38,162 @@ autoExpandExamples.kyu = `
 @ wiktionary
 
 
-ꤊꤚꤝꤤ|bone|k§r§w§i˦
-ꤊꤚꤝꤥ꤭|to follow|k§r§w§o˧
+ꤊꤚꤝꤤ|bone|k§r§w§i\u0301
+ꤊꤚꤝꤥ꤭|to follow|k§r§w§o\u0304
 ꤊꤚꤟꤤ|basket with head strap|kri̤
-ꤊꤜꤛꤢ|road, street|k§l§j§a˦
-ꤊꤜꤛꤢꤩ꤬ꤕꤜꤟꤢꤧ|arrow|k§l§j§e˨§p§l§‹§ɛ̤
-ꤊꤜꤛꤢ꤬|to run|klja˨
+ꤊꤜꤛꤢ|road, street|k§l§j§a\u0301
+ꤊꤜꤛꤢꤩ꤬ꤕꤜꤟꤢꤧ|arrow|k§l§j§e\u0300.§p§l§‹§ɛ̤
+ꤊꤜꤛꤢ꤬|to run|klja\u0300
 ꤊꤜꤢꤧ꤭||␣||
-ꤊꤝꤤ꤭|ask|k§w§i˧
-ꤊꤟꤛꤢꤏꤛꤢꤩ꤬ꤞꤢꤧ꤬|tomato|kja̤˦ʃje˨ʂɛ˨
+ꤊꤝꤤ꤭|ask|k§w§i\u0304
+ꤊꤟꤛꤢꤏꤛꤢꤩ꤬ꤞꤢꤧ꤬|tomato|kja̤\u0301ʃje\u0300ʂɛ\u0300
 ꤊꤢꤨ꤭ꤜꤤꤊꤟꤢꤩ||␣
 ꤊꤢꤪ꤬ꤕꤛꤢꤨ꤬ꤒꤣ꤬||␣
-ꤊꤢ꤬ꤔꤟꤥ꤮ ꤒꤥ꤬|really|k§a˨§n§‹§o̤˦§ː§ §t§o˨
-ꤊꤢ꤬ꤔꤟꤥ꤮|very|ka˨no̤ˈ
-ꤊꤢ꤬ꤛꤢ꤭ꤜꤟꤤ꤬ ꤍꤟꤥ|Red Karen language|k§a˨§y§a˧§l§–§i̤˨§ §ŋ§–§o̤
-ꤊꤢ꤬ꤛꤢ꤭ꤜꤟꤤ꤬ ꤢ꤬ꤜꤤ꤬|Red Karen alphabet|k§a˨§y§a˧§l§–§i̤˨§ §a˨§l§i˨
-ꤊꤢ꤬ꤛꤢ꤭ꤜꤟꤤ꤬|Kayah Li|k§a˨.§j§a˧.§l§›§i̤˨
-ꤋꤢꤧ꤬|bitter|kʰ§ɛ˨
-ꤋꤢꤧ꤬ꤎꤣ꤬|cancer|kʰɛ˨.sə˨
-ꤋꤢꤧ꤬ꤗꤣ꤬ꤚꤢ꤬|camera|kʰɛ˨.mə˨.ra˨
+ꤊꤢ꤬ꤔꤟꤥ꤮ ꤒꤥ꤬|really|k§a\u0300§n§‹§o̤\u0301§ː§ §t§o\u0300
+ꤊꤢ꤬ꤔꤟꤥ꤮|very|ka\u0300no̤ˈ
+ꤊꤢ꤬ꤛꤢ꤭ꤜꤟꤤ꤬ ꤍꤟꤥ|Red Karen language|k§a\u0300§y§a\u0304§l§–§i̤\u0300§ §ŋ§–§o̤
+ꤊꤢ꤬ꤛꤢ꤭ꤜꤟꤤ꤬ ꤢ꤬ꤜꤤ꤬|Red Karen alphabet|k§a\u0300§y§a\u0304§l§–§i̤\u0300§ §a\u0300§l§i\u0300
+ꤊꤢ꤬ꤛꤢ꤭ꤜꤟꤤ꤬|Kayah Li|k§a\u0300.§j§a\u0304.§l§›§i̤\u0300
+ꤋꤢꤧ꤬|bitter|kʰ§ɛ\u0300
+ꤋꤢꤧ꤬ꤎꤣ꤬|cancer|kʰɛ\u0300.sə\u0300
+ꤋꤢꤧ꤬ꤗꤣ꤬ꤚꤢ꤬|camera|kʰɛ\u0300.mə\u0300.ra\u0300
 ꤋꤢ꤭ꤗꤟꤤ||␣
-ꤋꤢ꤭ꤢꤨ꤬|mouth|kʰa˧u˨
-ꤍꤟꤢꤨ꤬|price|ŋṳ˨
-ꤍꤟꤥ|word|ŋ§›§o̤˦
-ꤍꤟꤥꤋꤢꤋꤥ꤬|parable|ŋo̤˦kʰa˦kʰo˨
-ꤍꤟꤥꤘꤢꤪꤟꤌꤣ|password|ŋo̤˦dɔ˦ɯ̤ᵊ
-ꤍꤟꤥꤟꤢꤩꤙꤢꤧ꤬|speech|ŋo̤˦he˦bɛ˨
-ꤍꤟꤥꤢꤧꤓꤥ꤭|slogan|ŋo̤˦ɛ˦tʰo˧
-ꤎꤤꤊꤚꤢꤧ|cigarette|s§i˦.§k§r§ɛ˦
-ꤏꤛꤢꤩ|ten|ʃ§j§e˦
+ꤋꤢ꤭ꤢꤨ꤬|mouth|kʰa\u0304u\u0300
+ꤍꤟꤢꤨ꤬|price|ŋṳ\u0300
+ꤍꤟꤥ|word|ŋ§›§o̤\u0301
+ꤍꤟꤥꤋꤢꤋꤥ꤬|parable|ŋo̤\u0301kʰa\u0301kʰo\u0300
+ꤍꤟꤥꤘꤢꤪꤟꤌꤣ|password|ŋo̤\u0301dɔ\u0301ɯ̤ᵊ
+ꤍꤟꤥꤟꤢꤩꤙꤢꤧ꤬|speech|ŋo̤\u0301he\u0301bɛ\u0300
+ꤍꤟꤥꤢꤧꤓꤥ꤭|slogan|ŋo̤\u0301ɛ\u0301tʰo\u0304
+ꤎꤤꤊꤚꤢꤧ|cigarette|s§i\u0301.§k§r§ɛ\u0301
+ꤏꤛꤢꤩ|ten|ʃ§j§e\u0301
 ꤏꤛꤢꤩ꤬|sour|
-ꤏꤛꤢꤩ꤬ꤊꤜꤥ꤭|weather|sʰ§j§e˨§k§l§o˧
-ꤏꤛꤢꤩ꤭|chicken|ʃje˧
-ꤏꤝꤥ꤬|crab|ʃwo˨
-ꤏꤢꤦ꤭ꤏꤢꤧ꤭|to spear|sʰ§ɯ˧§sʰ§ɛ˧
-ꤏꤢꤦ꤭ꤟꤢꤧ꤭|salty|sʰɯ˧.hɛ˧
-ꤏꤤ꤬ꤜꤤ꤬ꤕꤜꤝꤥ꤭|radio|sʰ§i˨§l§i˨§p§l§w§o˧
-ꤑꤟꤢ꤭|five|ɲ§›§a̤˧
-ꤒꤟꤢꤢꤧ꤬ꤙꤝꤤ|thank you|tɛ̤˨bwi
-ꤒꤟꤢꤧ꤬|thing, object|t§›§ɛ̤˨
-ꤒꤟꤢꤧ꤬ꤋꤢꤨ꤬ꤞꤢꤚꤢꤘꤢꤨ꤬|doctor|tɛ̤˨kʰu˨ʂa˦ra˦du˨
-ꤒꤟꤢꤧ꤬ꤒꤢ꤬ꤚꤟꤢꤪ꤬ꤞꤢꤧ ꤞꤢꤧꤤ꤬ꤏꤢꤩ꤭|danger|tɛ̤˨ta˨rɔ̤˨ʂɛ˦ ʂɛ˦i˨sʰe˧
-ꤒꤟꤢꤧ꤬ꤚꤛꤢꤙꤢꤧ꤬ ꤔꤟꤢꤧ꤬|hello|tɛ̤˨rjabɛ˨ nɛ̤˨
-ꤒꤢ꤬ꤊꤜꤝꤤ|opposite|t§a˨§k§l§w§i˦
-ꤒꤢ꤬ꤊꤜꤤ||␣
-ꤒꤢ꤬ꤍꤢꤨ꤭ꤗꤟꤢꤪ|pine tree|ta˨ŋu˧mɔ̤˦
-ꤒꤢ꤬ꤏꤛꤢ꤭|elephant|ta˨.ʃja˧
-ꤒꤢ꤬ꤞꤤ|horse|t§a˨.§ʂ§i˦
-ꤒꤣ꤬|one|t§ə˨
+ꤏꤛꤢꤩ꤬ꤊꤜꤥ꤭|weather|ʃ§j§e\u0300.§k§l§o\u0304
+ꤏꤛꤢꤩ꤭|chicken|ʃje\u0304
+ꤏꤝꤥ꤬|crab|ʃwo\u0300
+ꤏꤢꤦ꤭ꤏꤢꤧ꤭|to spear|sʰ§ɯ\u0304§sʰ§ɛ\u0304
+ꤏꤢꤦ꤭ꤟꤢꤧ꤭|salty|sʰɯ\u0304.hɛ\u0304
+ꤏꤤ꤬ꤜꤤ꤬ꤕꤜꤝꤥ꤭|radio|ʃ§i\u0300.§l§i\u0300.§p§l§w§o\u0304
+ꤑꤟꤢ꤭|five|ɲ§›§a̤\u0304
+ꤒꤟꤢꤢꤧ꤬ꤙꤝꤤ|thank you|tɛ̤\u0300bwi
+ꤒꤟꤢꤧ꤬|thing, object|t§›§ɛ̤\u0300
+ꤒꤟꤢꤧ꤬ꤋꤢꤨ꤬ꤞꤢꤚꤢꤘꤢꤨ꤬|doctor|tɛ̤\u0300kʰu\u0300ʂa\u0301ra\u0301du\u0300
+ꤒꤟꤢꤧ꤬ꤒꤢ꤬ꤚꤟꤢꤪ꤬ꤞꤢꤧ ꤞꤢꤧꤤ꤬ꤏꤢꤩ꤭|danger|tɛ̤\u0300ta\u0300rɔ̤\u0300ʂɛ\u0301 ʂɛ\u0301i\u0300sʰe\u0304
+ꤒꤟꤢꤧ꤬ꤚꤛꤢꤙꤢꤧ꤬ ꤔꤟꤢꤧ꤬|hello|tɛ̤\u0300rjabɛ\u0300 nɛ̤\u0300
+ꤒꤢ꤬ꤊꤜꤝꤤ|opposite|t§a\u0300§k§l§w§i\u0301
+ꤒꤢ꤬ꤊꤜꤤ|turtle|ta\u0300.kli\u0301|tàkli
+ꤒꤢ꤬ꤍꤢꤨ꤭ꤗꤟꤢꤪ|pine tree|ta\u0300ŋu\u0304mɔ̤\u0301
+ꤒꤢ꤬ꤏꤛꤢ꤭|elephant|ta\u0300.ʃja\u0304
+ꤒꤢ꤬ꤞꤤ|horse|t§a\u0300.§ʂ§i\u0301
+ꤒꤣ꤬|one|t§ə\u0300
 ꤓꤛꤢ||␣
 ꤓꤛꤢꤩ꤭||
-ꤓꤛꤢꤩ꤭ꤏꤤ꤭ꤕꤚꤟꤢꤩ|alcohol|tʰ§j§e˧§ʃ§i˧§p§r§‹§e̤˦
+ꤓꤛꤢꤩ꤭ꤏꤤ꤭ꤕꤚꤟꤢꤩ|alcohol|tʰ§j§e\u0304§ʃ§i\u0304§p§r§‹§e̤\u0301
 ꤓꤛꤢ꤬||␣
 ꤓꤛꤢ꤭||
-ꤓꤛꤥ|cat|tʰ§w§o˦
-ꤓꤝꤤ꤬|dog|tʰwi˨
-ꤓꤢꤦ ꤗꤝꤟꤥ꤭|soap|tʰɯ mwo̤˧
-ꤓꤢꤧ|needle|tʰ§ɛ˦
+ꤓꤛꤥ|cat|tʰ§w§o\u0301
+ꤓꤝꤤ꤬|dog|tʰwi\u0300
+ꤓꤢꤦ ꤗꤝꤟꤥ꤭|soap|tʰɯ mwo̤\u0304
+ꤓꤢꤧ|needle|tʰ§ɛ\u0301
 ꤓꤢꤧ꤬ꤜꤤ꤬ꤖꤥ꤭||␣
 ꤓꤢꤧ꤭||␣
 ꤓꤢꤨ꤬||␣
 ꤓꤤ꤬||␣
-ꤔꤛꤢꤩ꤭|two|nje˧
-ꤔꤛꤣ|that|n§›§ɯə˨
-ꤔꤟꤢꤧ꤬|you s.|nɛ̤˨
-ꤔꤟꤤꤟꤌꤣ꤭|to hear|n§›§i̤§h§›§ɯ̤ə˧§/ni̤xə˧
-ꤔꤢ꤬|year|n§a˨
-ꤔꤤ꤮|particle to soften statements|ni˦
-ꤕꤚꤟꤢꤩꤐꤝꤤ꤬ꤊꤟꤢꤩ|Thailand|pre̤˦.zwi˨.ke̤˦
-ꤕꤝꤟꤥ꤭꤮ ꤒꤣ꤬ꤖꤟꤌꤣ|always|pwo̤˧tə˨pʰɯ̤ᵊ˦
+ꤔꤛꤢꤩ꤭|two|nje\u0304
+ꤔꤛꤣ|that|n§›§ɯə\u0300
+ꤔꤟꤢꤧ꤬|you s.|nɛ̤\u0300
+ꤔꤟꤤꤟꤌꤣ꤭|to hear|n§›§i̤§h§›§ɯ̤ə\u0304§/ni̤xə\u0304
+ꤔꤢ꤬|year|n§a\u0300
+ꤔꤤ꤮|particle to soften statements|ni\u0301
+ꤕꤚꤟꤢꤩꤐꤝꤤ꤬ꤊꤟꤢꤩ|Thailand|pre̤\u0301.zwi\u0300.ke̤\u0301
+ꤕꤝꤟꤥ꤭꤮ ꤒꤣ꤬ꤖꤟꤌꤣ|always|pwo̤\u0304tə\u0300pʰɯ̤ᵊ\u0301
 ꤕꤟꤌꤣ|older sibling|pɯ̤ə
-ꤕꤟꤛꤢꤛꤢꤧꤕꤟꤛꤢꤏꤢꤧ꤭|difficult|pja̤˦jɛ˦pja̤˦sʰɛ˧
+ꤕꤟꤛꤢꤛꤢꤧꤕꤟꤛꤢꤏꤢꤧ꤭|difficult|pja̤\u0301jɛ\u0301pja̤\u0301sʰɛ\u0304
 ꤕꤟꤢꤧ꤬||␣
 ꤕꤟꤢꤪ꤬||␣
 ꤕꤢꤎꤕꤥ|passport|p§a.§s§p§o
-ꤖꤛꤢꤩ|to take|pʰje˦
-ꤖꤢꤛꤝꤥ|rubber|pʰ§a˦§j§w§o˦
-ꤖꤢꤧ꤭|father|pʰɛ˧
-ꤖꤤ꤭|opium|pʰi˧
-ꤖꤥ꤭|flower|pʰ§o˧
-ꤗꤝꤟꤤ꤬|name|m§w§›§i̤˨
+ꤖꤛꤢꤩ|to take|pʰje\u0301
+ꤖꤢꤛꤝꤥ|rubber|pʰ§a\u0301§j§w§o\u0301
+ꤖꤢꤧ꤭|father|pʰɛ\u0304
+ꤖꤤ꤭|opium|pʰi\u0304
+ꤖꤥ꤭|flower|pʰ§o\u0304
+ꤗꤝꤟꤤ꤬|name|m§w§›§i̤\u0300
 ꤗꤟꤌꤣ|mother|m§›§›§ɯ̤ᵊ
 ꤗꤟꤛꤢ꤬ꤊꤟꤢꤩ||␣
-ꤗꤤ꤬|fire|m§i˨
-ꤘꤛꤢꤩ|to give|d§j§e˦
-ꤘꤟꤤ꤬ꤗꤥ꤬ꤊꤚꤢꤩ꤬ꤎꤤ꤬|democracy|di̤˨.mo˨.kre˨.si˨
-ꤘꤤ꤬|frog|d§i˨
+ꤗꤤ꤬|fire|m§i\u0300
+ꤘꤛꤢꤩ|to give|d§j§e\u0301
+ꤘꤟꤤ꤬ꤗꤥ꤬ꤊꤚꤢꤩ꤬ꤎꤤ꤬|democracy|di̤\u0300.mo\u0300.kre\u0300.si\u0300
+ꤘꤤ꤬|frog|d§i\u0300
 ꤘꤤ꤬ꤓꤥ||␣
-ꤙꤛꤢ꤭|yellow|b§j§a˧
-ꤙꤢꤧ꤬ꤓꤚꤟꤤ꤬|battery|bɛ˨.tʰri̤˨
-ꤙꤢꤨ꤭|white|b§u˧
-ꤙꤤꤒꤢꤩ꤭꤮ ꤒꤣ꤬ꤖꤥ꤬ ꤙꤢꤧ꤬ꤙꤢꤧ꤬|anywhere|bi˦te˧ tə˨pʰo˨ bɛ˨bɛ˨
+ꤙꤛꤢ꤭|yellow|b§j§a\u0304
+ꤙꤢꤧ꤬ꤓꤚꤟꤤ꤬|battery|bɛ\u0300.tʰri̤\u0300
+ꤙꤢꤨ꤭|white|b§u\u0304
+ꤙꤤꤒꤢꤩ꤭꤮ ꤒꤣ꤬ꤖꤥ꤬ ꤙꤢꤧ꤬ꤙꤢꤧ꤬|anywhere|bi\u0301te\u0304 tə\u0300pʰo\u0300 bɛ\u0300bɛ\u0300
 ꤙꤤ꤬ꤛꤢ꤬||␣
-ꤚꤟꤢꤨ|snake
-ꤚꤢꤦ꤭|money, silver|r§ɯ˧
-ꤚꤢ꤬ꤠꤟꤢ꤬|zero|ra˨.va̤˨
-ꤛꤢꤡꤢꤦ|to try|ja˦ʤɯ˦
-ꤜꤝꤟꤤ꤭|four|lwi̤˧
+ꤚꤟꤢꤨ|snake||rṳ
+ꤚꤢꤦ꤭|money, silver|r§ɯ\u0304
+ꤚꤢ꤬ꤠꤟꤢ꤬|zero|ra\u0300.va̤\u0300
+ꤛꤢꤡꤢꤦ|to try|ja\u0301ʤɯ\u0301
+ꤜꤝꤟꤤ꤭|four|lwi̤\u0304
 ꤜꤟꤢꤪ|all|lɔ̤
-ꤜꤟꤢꤪꤞꤛꤣ꤬|pot stand|lɔ̤ʂɯə˧
-ꤜꤟꤢ꤭ꤍꤟꤥ|language|l§›§a̤˧§ŋ§›§o̤˦
+ꤜꤟꤢꤪꤞꤛꤣ꤬|pot stand|lɔ̤ʂɯə\u0304
+ꤜꤟꤢ꤭ꤍꤟꤥ|language|l§›§a̤\u0304§ŋ§›§o̤\u0301
 ꤜꤟꤤ||␣
-ꤜꤟꤤ꤬|red|li̤˨
+ꤜꤟꤤ꤬|red|li̤\u0300
 ꤜꤢꤧ꤬||␣
 ꤜꤢꤧ꤭||␣
-ꤜꤤ꤬|letter|li˨
-ꤞꤛꤢꤩ꤭|to die|ʂje˧
-ꤞꤛꤣ꤬ꤞꤛꤣ꤬꤮ ꤥ꤬ꤗꤛꤢ꤮|Be quiet!|ʂɯᵊ˨ʂɯᵊ˨ o˨mja˦
-ꤞꤛꤣ꤭|green|ʂɯᵊ˧
-ꤞꤛꤥ|eight|ʂwo˦
-ꤞꤝꤣ꤭|three|ʂɛ˦
-ꤞꤝꤤ꤬|blood|ʂwi˨
-ꤞꤝꤤ꤬ꤓꤛꤢꤩ꤭|blood|ʂwi˨.tʰje˧
-ꤞꤢꤚꤢ|teacher|ʂa˦ra˦
+ꤜꤤ꤬|letter|li\u0300
+ꤞꤛꤢꤩ꤭|to die|ʂje\u0304
+ꤞꤛꤣ꤬ꤞꤛꤣ꤬꤮ ꤥ꤬ꤗꤛꤢ꤮|Be quiet!|ʂɯᵊ\u0300ʂɯᵊ\u0300 o\u0300mja\u0301
+ꤞꤛꤣ꤭|green|ʂɯᵊ\u0304
+ꤞꤛꤥ|eight|ʂwo\u0301
+ꤞꤝꤣ꤭|three|ʂɛ\u0301
+ꤞꤝꤤ꤬|blood|ʂwi\u0300
+ꤞꤝꤤ꤬ꤓꤛꤢꤩ꤭|blood|ʂwi\u0300.tʰje\u0304
+ꤞꤢꤚꤢ|teacher|ʂa\u0301ra\u0301
 ꤞꤢꤧ ꤢꤪ|flu|θ§ɛ§ §ɔ
-ꤞꤢꤧ|to want|ʂɛ˦
-ꤞꤢꤧꤐꤟꤢꤦ|to want|ʂ§ɛ˦.§z§›§ɯ̤˦
-ꤞꤢꤧꤑꤢꤩ꤭|to know|ʂɛ˦.ɲe˧
+ꤞꤢꤧ|to want|ʂɛ\u0301
+ꤞꤢꤧꤐꤟꤢꤦ|to want|ʂ§ɛ\u0301.§z§›§ɯ̤\u0301
+ꤞꤢꤧꤑꤢꤩ꤭|to know|ʂɛ\u0301.ɲe\u0304
 ꤞꤢꤧꤜꤝꤟꤤ꤭||␣||
-ꤞꤢꤨ|fat, oil|ʂ§u˦
-ꤞꤢꤨ꤬|lungs|ʂ§u˨
-ꤞꤢꤨ꤭|send|ʂ§u˧
-ꤞꤢꤪ꤬|tree|ʂɔ˨
-ꤞꤢꤪ꤬ꤜꤢꤧ꤬|leaf|ʂɔ˨.lɛ˨
-ꤞꤢ꤬ꤒꤢ꤬ꤕꤚꤢꤩ꤭|fishnet|ʂa˨ta˨pre˧
-ꤞꤤꤗꤟꤤ|sheep|ʂi˦.mi̤˦
-ꤞꤤ꤭|you (pl)|ʂi˧
-ꤞꤥ꤬ꤕꤜꤟꤛꤢꤩ꤬|rope|s§o˨§p§l§‹§j§e̤˨
-ꤞꤥ꤭ꤙꤛꤢ|water jar|ʂo˧bja
-ꤟꤌꤣ꤭|uncooked rice|h§›§ɯ̤ə§/xə˧
-ꤟꤛꤢ꤭|to come|hja˧
+ꤞꤢꤨ|fat, oil|ʂ§u\u0301
+ꤞꤢꤨ꤬|lungs|ʂ§u\u0300
+ꤞꤢꤨ꤭|send|ʂ§u\u0304
+ꤞꤢꤪ꤬|tree|ʂɔ\u0300
+ꤞꤢꤪ꤬ꤜꤢꤧ꤬|leaf|ʂɔ\u0300.lɛ\u0300
+ꤞꤢ꤬ꤒꤢ꤬ꤕꤚꤢꤩ꤭|fishnet|ʂa\u0300ta\u0300pre\u0304
+ꤞꤤꤗꤟꤤ|sheep|ʂi\u0301.mi̤\u0301
+ꤞꤤ꤭|you (pl)|ʂi\u0304
+ꤞꤥ꤬ꤕꤜꤟꤛꤢꤩ꤬|rope|s§o\u0300.§p§l§‹§j§e̤\u0300
+ꤞꤥ꤭ꤙꤛꤢ|water jar|ʂo\u0304bja
+ꤟꤌꤣ꤭|uncooked rice|h§›§ɯ̤ə§/xə\u0304
+ꤟꤛꤢ꤭|to come|hja\u0304
 ꤟꤢꤩ꤭ꤊꤟꤢꤩ||␣
-ꤟꤢꤪ|already|h§ɔ˦
+ꤟꤢꤪ|already|h§ɔ\u0301
 ꤟꤢꤪ꤬ꤒꤢꤧ꤬||␣
 ꤟꤤ꤬||␣
-ꤠꤟꤢꤧ꤬|bamboo|v§›§ɛ̤˨
-ꤠꤟꤤ꤭ꤗꤟꤌꤣ|capital city|vi̤˧.mɯ̤ᵊ
+ꤠꤟꤢꤧ꤬|bamboo|v§›§ɛ̤\u0300
+ꤠꤟꤤ꤭ꤗꤟꤌꤣ|capital city|vi̤\u0304.mɯ̤ᵊ
 ꤠꤢꤧ꤬|husband|v§ɛ^
 ꤠꤢ꤭||␣
 ꤡꤛꤢꤪꤊꤜꤢꤧ||␣
 ꤡꤛꤣ|to go|d͡ʒɯᵊ
 ꤡꤟꤛꤢ|to be able to|d͡ʒ§››§j§a̤
-ꤡꤢꤧ|machine|d͡ʒ§ɛ˦
-ꤢꤛꤢꤡꤢꤦꤢꤛꤢꤚꤢ|to try|ja˦ʤɯ˦ja˦re˨
-ꤢꤛꤢ꤭ꤞꤤ꤭|lion|–§j§a˧§ʂ§i˧
+ꤡꤢꤧ|machine|d͡ʒ§ɛ\u0301
+ꤢꤛꤢꤡꤢꤦꤢꤛꤢꤚꤢ|to try|ja\u0301ʤɯ\u0301ja\u0301re\u0300
+ꤢꤛꤢ꤭ꤞꤤ꤭|lion|–§j§a\u0304§ʂ§i\u0304
 ꤢꤟꤌꤣꤢꤛꤢ|foolish|–§›§›§ɯ̤́ᵊ§–§j§á
-ꤢꤧ꤬ꤞꤤ꤭|they|ɛ˨ʂi˧
-ꤢꤩ꤬ꤓꤣ꤬ꤔꤢꤧ|the Internet|ʔe˨.tʰə˨.nɛ˦
+ꤢꤧ꤬ꤞꤤ꤭|they|ɛ\u0300ʂi\u0304
+ꤢꤩ꤬ꤓꤣ꤬ꤔꤢꤧ|the Internet|ʔe\u0300.tʰə\u0300.nɛ\u0301
 ꤢꤪ|brown|ɔ
 ꤢ꤬||␣||
-ꤢ꤬ꤍꤟꤥꤜꤢ꤬ꤏꤢ|meaning|a˨ŋo̤˦la˨sʰa˦
-ꤢ꤬ꤏꤛꤢꤊꤟꤢꤩ|Asia|ʔa˨.ʃja˦.ke̤˦
-ꤢ꤬ꤕꤟꤛꤢ|difficult|a˨§p§‹§j§a̤
+ꤢ꤬ꤍꤟꤥꤜꤢ꤬ꤏꤢ|meaning|a\u0300ŋo̤\u0301la\u0300sʰa\u0301
+ꤢ꤬ꤏꤛꤢꤊꤟꤢꤩ|Asia|ʔa\u0300.ʃja\u0301.ke̤\u0301
+ꤢ꤬ꤕꤟꤛꤢ|difficult|a\u0300.§p§‹§j§a̤
 ꤢ꤬ꤗꤥ꤬ꤢ꤬ꤚꤢ|tax|ʔà.§m§ò.§ʔà.§r§á
-ꤢ꤬ꤠꤖꤚꤤꤊꤢ|Africa|a˨fri˦ka˦
-ꤤ꤬ꤕꤟꤢ|leap|i˨pa̤˦
-ꤥ꤬ꤊꤟꤌꤣ|to rest|o˨§k§‹§›§ɯ̤ə
+ꤢ꤬ꤠꤖꤚꤤꤊꤢ|Africa|a\u0300fri\u0301ka\u0301
+ꤤ꤬ꤕꤟꤢ|leap|i\u0300pa̤\u0301
+ꤥ꤬ꤊꤟꤌꤣ|to rest|o\u0300§k§‹§›§ɯ̤ə
 ꤥ꤬ꤗꤛꤢꤩ|to sleep|ò.§m§j§ē|òmye
 
 @ http://www.karennirefugees.com/uploads/3/0/1/9/3019325/learning_kayah.pdf
