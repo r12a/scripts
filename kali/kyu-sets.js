2a5c82dd6266610236c1e35adb94635e7e099c8e
r12a <ishida@w3.org>
2025-10-19 15:58:28 +0100

    kyu   Review & update

--- kali/kyu-sets.js
+++ kali/kyu-sets.js
@@ -10,6 +10,10 @@ var collections = [
 { symbol:'Ⓒ', desc:'Consonants', chars:'(ꤟ|ꤛ|ꤕ|ꤙ|ꤖ|ꤒ|ꤘ|ꤓ|ꤊ|ꤋ|ꤌ|ꤡ|ꤠ|ꤞ|ꤎ|ꤐ|ꤏ|ꤗ|ꤔ|ꤑ|ꤍ|ꤝ|ꤚ|ꤜ)' },
 { symbol:'ⓥ', desc:'Dependent vowels', chars:'(ꤦ|ꤧ|ꤨ|ꤩ|ꤪ)' },
 { symbol:'Ⓥ', desc:'Independent vowels', chars:'(ꤢ|ꤤ|ꤥ|ꤣ)' },
+{ symbol:'Ⓓ', desc:'Diphthongs', chars:'(ꤣꤧ|ꤣꤦ|ꤣꤨ|ꤣꤩ|ꤣꤪ|ꤛꤣ|ꤟꤌꤣ)' },
 { symbol:'Ⓣ', desc:'Tones', chars:'(꤬|꤫|꤭)' },
 { symbol:'⊙', desc:'Invisibles', chars:'(\u200C|\u200D)' },
+{ symbol:'🅲', desc:'Latin consonants', chars:'(b|c|d|f|g|h|j|k|l|m|n|p|q|r|s|t|v|w|x|y|z|d͡ʒ|ɡ|kʰ|pʰ|ʂ|sʰ|tʰ|ɲ|ŋ|θ)' },
+{ symbol:'🆅', desc:'Latin vowels', chars:'(a|e|i|o|u|ê|ɛ|ə|ô|ɔ|ơ|ư|ɯ)' },
+{ symbol:'🅱', desc:'Latin breathiness', chars:'(̤)' },
 ]
