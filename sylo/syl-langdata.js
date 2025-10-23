b939f1624479540ca60d608620cd80c5f8b4da96
r12a <ishida@w3.org>
2025-10-23 05:07:24 +0100

    Various, fix console msgs

--- sylo/syl-langdata.js
+++ sylo/syl-langdata.js
@@ -6,7 +6,7 @@ var langs = {
 "syl": { name:"Sylheti", local:"ꠍꠤꠟꠐꠤ", localtrans:"sílɔʈi", silcode:"", type:"abug", scriptname:"ꠍꠤꠟꠐꠤ ꠘꠣꠉꠞꠤ", scriptnametrans:"sílɔʈi nagɾi",
 source:"r12a", 
 region:"sasia", countries:"Bangladesh", script:"sylo", speakers:"11000000", 
-letter:"ʼꠀꠁꠃꠄꠅꠇꠈꠉꠊꠌꠍꠎꠏꠐꠑꠒꠓꠔꠕꠖꠗꠘꠙꠚꠛꠜꠝꠞꠟꠠꠡꠢ", letteraux:"", mark:"ꠂ꠆ꠋꠣꠤꠥꠦꠧ꠬", markaux:"", number:"০১২৩৪৫৬৭৮৯", numberaux:"", punctuation:"।॥–—‘’“”…⁕", punctuationaux:"", symbol:"৳꠨꠩꠪꠫", symbolaux:"", other:"", otheraux:"", aux:"", deprecated:"", 
+letter:"ʼꠀꠁꠃꠄꠅꠇꠈꠉꠊꠌꠍꠎꠏꠐꠑꠒꠓꠔꠕꠖꠗꠘꠙꠚꠛꠜꠝꠞꠟꠠꠡꠢ", mark:"ꠂ꠆ꠋꠣꠤꠥꠦꠧ꠬", number:"০১২৩৪৫৬৭৮৯", punctuation:"।॥“”⁕", symbol:"৳꠨꠩꠪꠫", other:"", aux:"0123456789͏​‌‍–—‘’•… ‰⁠",	
 orth:`[Syloti Nagri] &nbsp; <a href="../scripts/sylo/syl.html" target="_blank">Details</a>.`,
 also:
 ["syl | beng | Principal orthography."],
