6b805ebd2c9a89347622e3951844af6546925a9e
r12a <ishida@w3.org>
2025-10-30 18:08:38 +0000

    su   Detailed review & rewrite

--- sund/su-examples.js
+++ sund/su-examples.js
@@ -22,19 +22,19 @@ autoExpandExamples.su = `
 
 @ wiktionary
 
-ᮃᮀᮊᮒ᮪|to leave||angkat
+ᮃᮀᮊᮒ᮪|to leave|aŋ§ˈka§t|angkat
 ᮃᮀᮊᮣᮥᮀ||
 ᮃᮀᮊᮥᮒᮔ᮪|transport||angkutan||
 ᮃᮀᮊᮩᮘ᮪||
-ᮃᮄᮀ||
-ᮃᮊᮀ||
-ᮃᮊ᮪ᮞᮛ|syllable||aksara||
+ᮃᮄᮀ|I, me|a§iŋ|aing|||aing
+ᮃᮊᮀ|older brother|a.§kaŋ|akang|||akang
+ᮃᮊ᮪ᮞᮛ|syllable|a§k§sa§ra|aksara||
 ᮃᮌᮢᮤᮊᮥᮜ᮪ᮒᮥᮁ|agriculture||agrikultur||
 ᮃᮌᮩᮀ||
 ᮃᮍᮣᮧ||
 ᮃᮍᮤᮀ||
 ᮃᮍᮤᮔ᮪||
-ᮃᮍᮨᮔ᮪||
+ᮃᮍᮨᮔ᮪|liver|a.§ŋə§n|angen|||angen
 ᮃᮏᮊ᮪|to invite||ajak
 ᮃᮏᮨᮌ᮪||
 ᮃᮒᮤ||
@@ -49,11 +49,11 @@ autoExpandExamples.su = `
 ᮃᮕᮧᮒᮦᮊ᮪||
 ᮃᮘᮂ||
 ᮃᮘᮂ-ᮃᮘᮂ|goods||abah-abah||
-ᮃᮘᮧᮒ᮪||
+ᮃᮘᮧᮒ᮪|heavy|a§bo§t|abot|||abot
 ᮃᮘ᮪ᮓᮤ||
 ᮃᮙᮨᮀ||
 ᮃᮙ᮪ᮕᮛᮔ᮪||
-ᮃᮙ᮪ᮘᮥᮀ||
+ᮃᮙ᮪ᮘᮥᮀ|to smell|a§m§ˈbuŋ|ambung|aˈm̪ʊŋ||ambung
 ᮃᮙ᮪ᮘᮨᮊᮔ᮪||
 ᮃᮛᮄᮀ||
 ᮃᮛᮔ᮪ᮏᮩᮔ᮪||
@@ -78,7 +78,7 @@ autoExpandExamples.su = `
 ᮄᮛᮥᮀ||
 ᮄᮜᮒ᮪||
 ᮅᮃᮕ᮪|steam||uap||
-ᮅᮒᮩᮊ᮪||
+ᮅᮒᮩᮊ᮪|brain|u.§tɨ§k|uteuk|||uteuk
 ᮅᮔᮤᮍ|to know||uninga
 ᮅᮕᮙᮤ||
 ᮅᮘᮁ||
@@ -99,12 +99,12 @@ autoExpandExamples.su = `
 ᮈᮙᮞ᮪|gold||emas
 ᮈᮙ᮪ᮘᮥᮀ||
 ᮈᮝᮒᮩᮔ᮪||
-ᮉᮞᮤ|content, essence||eusi
+ᮉᮞᮤ|content, essence|ˈɨ§si|eusi|||eusi
 ᮊᮄ|wood||kai
 ᮊᮓᮠᮛᮔ᮪||
 ᮊᮓᮥ||
 ᮊᮔ᮪ᮎᮔ||
-ᮊᮔ᮪ᮏᮥᮒ᮪||
+ᮊᮔ᮪ᮏᮥᮒ᮪|purse, money bag|ka§n.§d͡ʒu§t|kanjut|ka.ɲut||kanjut
 ᮊᮔ᮪ᮒᮧᮁ||
 ᮊᮕᮥᮒ᮪||
 ᮊᮙ᮪ᮘᮀ||
@@ -132,7 +132,7 @@ autoExpandExamples.su = `
 ᮊᮧᮞᮧᮊ᮪||
 ᮊᮨᮁᮞ||
 ᮊᮨᮊᮨᮘᮥᮜ᮪||
-ᮊᮨᮎᮕ᮪|word||kecap
+ᮊᮨᮎᮕ᮪|word|kə.§t͡ʃa§p|kecap
 ᮊᮨᮓᮨᮀ||
 ᮊᮨᮓᮨᮜᮦ||
 ᮊᮨᮔ᮪ᮓᮀ||
@@ -157,7 +157,7 @@ autoExpandExamples.su = `
 ᮌᮧᮌᮧᮒ᮪||
 ᮌᮨᮒᮤᮂ||
 ᮌᮨᮓᮀ|papaya||gedang
-ᮌᮨᮜᮥᮒ᮪||
+ᮌᮨᮜᮥᮒ᮪|to fight|ɡə§ˈlu§t|gelut|||gelut
 ᮌᮨᮜᮧᮙ᮪ᮘᮀ||
 ᮌᮨᮜᮧᮙ᮪ᮘᮀ ᮙᮤᮊᮢᮧ|microwave||gelombang mikro||
 ᮌᮩᮜᮤᮞ᮪||
@@ -170,12 +170,12 @@ autoExpandExamples.su = `
 ᮍᮜᮠᮤᮁᮊᮩᮔ᮪||
 ᮍᮠᮥᮛᮥᮀᮊᮩᮔ᮪||
 ᮍᮧᮏᮄ||
-ᮎᮀᮊᮀ||
+ᮎᮀᮊᮀ|fruit peel|t͡ʃaŋ§kaŋ|cangkang|||cangkang
 ᮎᮄ||
 ᮎᮄ ᮒᮔᮩᮂ||
 ᮎᮊᮨᮒ᮪||
 ᮎᮎᮁ||
-ᮎᮎᮄ|liquid||cacai
+ᮎᮎᮄ|liquid|t͡ʃa.§t͡ʃa.§i|cacai|||cacai
 ᮎᮎᮤᮀ||
 ᮎᮛᮤᮇᮞ᮪||
 ᮎᮛᮧᮌᮦ||
@@ -215,7 +215,7 @@ autoExpandExamples.su = `
 ᮒᮔᮩᮂ||
 ᮒᮔ᮪ᮓᮥᮊ᮪||
 ᮒᮙ᮪ᮕᮧᮜᮧᮀ||
-ᮒᮛᮥᮀ||
+ᮒᮛ|never|ta.§ra|tara|||tara
 ᮒᮜᮌ||
 ᮒᮤᮀᮌᮀ||
 ᮒᮤᮄᮞ᮪||
@@ -240,14 +240,14 @@ autoExpandExamples.su = `
 ᮒᮨᮜᮞ᮪||
 ᮒᮨᮝᮨᮊ᮪||
 ᮒᮩᮊ᮪ᮒᮩᮊ᮪||
-ᮓᮀᮓᮩᮁ||
+ᮓᮀᮓᮩᮁ|cassava|daŋ§ˈdɨr|dangdeur|||dangdeur
 ᮓᮅᮁ ᮎᮄ||
 ᮓᮅᮔ᮪||
 ᮓᮍᮥ||
 ᮓᮓᮝᮥᮠᮔ᮪||
 ᮓᮚᮀ||
 ᮓᮚᮩᮂ||
-ᮓᮛᮒ᮪||
+ᮓᮛᮒ᮪|ground|da.§ra§t|darat|||darat
 ᮓᮜᮤᮀᮓᮤᮀ|fragrant||dalingding
 ᮓᮜᮥᮛᮥᮀ||
 ᮓᮝᮥᮂ||
@@ -270,7 +270,7 @@ autoExpandExamples.su = `
 ᮕᮌᮛᮚᮥ||
 ᮕᮌᮥᮔᮥᮍᮔ᮪||
 ᮕᮍᮔ᮪||
-ᮕᮍᮙ᮪ᮘᮥᮀ||
+ᮕᮍᮙ᮪ᮘᮥᮀ|nose|paŋamˈbuŋ|pangambung|paŋaˈm̪uŋ||pangambung
 ᮕᮍᮝᮨᮛᮥᮂ||
 ᮕᮍᮤᮛᮤᮙᮔ᮪||
 ᮕᮍᮦᮛᮔ᮪||
@@ -291,7 +291,7 @@ autoExpandExamples.su = `
 ᮕᮞᮤᮠᮔ᮪||
 ᮕᮤᮃᮔᮊᮔ᮪||
 ᮕᮤᮔᮛᮨᮕ᮪||
-ᮕᮤᮔᮥᮂ||
+ᮕᮤᮔᮥᮂ|full|pi§nuh|pinuh|||pinuh
 ᮕᮥᮀᮊᮥᮁ||
 ᮕᮥᮕᮥᮞ᮪||
 ᮕᮥᮞ᮪ᮕ|flower||puspal
@@ -305,12 +305,12 @@ autoExpandExamples.su = `
 ᮘᮌᮨᮓᮥᮁ||
 ᮘᮎ||
 ᮘᮏᮤᮀ||
-ᮘᮒᮥ||
+ᮘᮒᮥ|brick|ba§ˈta|bata|||bata
 ᮘᮚᮂ||
 ᮘᮚᮥ||
 ᮘᮛᮔᮠᮔ᮪||
 ᮘᮛᮥ||
-ᮘᮛᮩᮂ||
+ᮘᮛᮩᮂ|swollen|ba§rɤh|bareuh|||bareuh
 ᮘᮜᮧᮀ||
 ᮘᮞ||
 ᮘᮞᮩᮂ||
@@ -334,14 +334,14 @@ autoExpandExamples.su = `
 ᮘᮥᮜᮩᮓ᮪|spherical||buleud
 ᮘᮥᮠᮚ||
 ᮘᮦᮃᮞ᮪||
-ᮘᮦᮔ᮪ᮒᮀ||
+ᮘᮦᮔ᮪ᮒᮀ|star|bɛ§n.§taŋ|béntang|||béntang
 ᮘᮧᮏᮧ||
 ᮘᮧᮜᮧᮊᮧᮒ᮪||
 ᮘᮨᮓᮧᮜ᮪||
 ᮘᮨᮔᮨᮁ||
 ᮘᮨᮜᮨᮝᮥᮊ᮪||
 ᮘᮩᮀᮊᮩᮒ᮪||
-ᮘᮩᮀᮠᮁ||
+ᮘᮩᮀᮠᮁ|rich|bɨŋ.§har|beunghar|||beunghar
 ᮘᮩᮘᮩᮛᮤᮒᮔ᮪||
 ᮘᮩᮛᮩᮙ᮪||
 ᮘᮩᮜᮂ||
@@ -373,7 +373,7 @@ autoExpandExamples.su = `
 ᮛᮊᮤᮒ᮪||
 ᮛᮎᮥᮔ᮪||
 ᮛᮔ᮪ᮏᮀ|bed||risbang
-ᮛᮙ||
+ᮛᮙ|father|ra§ma|rama|||rama
 ᮛᮙᮒᮣᮧᮊ||
 ᮛᮙᮒ᮪ᮊᮎ||
 ᮛᮙᮨᮞ᮪||
@@ -391,7 +391,7 @@ autoExpandExamples.su = `
 ᮜᮅᮒ᮪ ᮏᮨᮕᮀ||
 ᮜᮍᮣᮚᮍᮔ᮪|kite||langlayangan
 ᮜᮍᮤᮒ᮪|sky||langit
-ᮜᮏᮥ||
+ᮜᮏᮥ|galangal|ˈla§d͡ʒa|laja|||laja
 ᮜᮏᮥ ᮎᮠᮚ||
 ᮜᮔ᮪ᮓᮊ᮪||
 ᮜᮘᮥᮂ||
@@ -415,7 +415,7 @@ autoExpandExamples.su = `
 ᮜᮨᮙᮂ||
 ᮜᮨᮙᮨᮞ᮪||
 ᮜᮨᮙ᮪ᮕᮨᮀ||
-ᮜᮨᮙ᮪ᮘᮥᮒ᮪||
+ᮜᮨᮙ᮪ᮘᮥᮒ᮪|tiny|lə§m.§bu§t|lembut|||lembut
 ᮜᮨᮛᮨᮞ᮪||
 ᮜᮩᮉᮒ᮪||
 ᮜᮩᮍᮩᮔ᮪||
@@ -427,7 +427,7 @@ autoExpandExamples.su = `
 ᮝᮔ||
 ᮝᮔᮧᮏ|woman||wanoja
 ᮝᮜᮤᮛᮀ||
-ᮝᮜᮥᮍᮔ᮪||
+ᮝᮜᮥᮍᮔ᮪|river|wa§lu§ŋa§n|walungan|||walungan
 ᮝᮠᮍᮔ᮪|river||wahangan
 ᮝᮤᮜᮍᮔ᮪||
 ᮝᮤᮜᮍᮔ᮪ ᮘᮥᮜᮩᮓ᮪||
@@ -487,20 +487,20 @@ autoExpandExamples.su = `
 ᮞᮨᮘᮦᮁ||
 ᮞᮨᮛᮀᮌ||
 ᮞᮩᮉᮁ|many||seueur
-ᮞᮩᮊᮩᮒ᮪||
+ᮞᮩᮊᮩᮒ᮪|sharp|ˈsɨ.§kɨ§t|seukeut|||seukeut
 ᮞᮩᮔᮩ||
-ᮞᮩᮞᮩᮕ᮪||
+ᮞᮩᮞᮩᮕ᮪|to suck|sɨ§ˈsɨ§p|seuseup|||seuseup
 ᮞ᮪ᮕᮦᮞᮤᮆᮞ᮪||
 ᮠᮊᮔ᮪||
 ᮠᮓᮦ||
 ᮠᮔᮩᮒ᮪||
-ᮠᮛᮤᮒ||
+ᮠᮛᮤᮒ|past|ha§ri.§ta|harita|||harita
 ᮠᮜᮤᮙᮥᮔ᮪||
 ᮠᮜᮩᮀᮠᮩᮙ᮪|overcast||haleungheum
 ᮠᮝᮥ||
-ᮠᮞᮩᮕ᮪||
+ᮠᮞᮩᮕ᮪|smoke|ha§ˈsɨ§p|haseup|||haseup
 ᮠᮤᮅ||
-ᮠᮤᮏᮤ||
+ᮠᮤᮏᮤ|one|hi.§d͡ʒi|hiji|||hiji
 ᮠᮤᮓᮩᮀ||
 ᮠᮤᮘᮨᮁ||
 ᮠᮤᮛᮥᮕ᮪||
@@ -510,28 +510,222 @@ autoExpandExamples.su = `
 ᮠᮩᮔ᮪ᮎᮩᮒ᮪||
 ᮠᮩᮘᮩᮜ᮪||
 ᮠᮩᮛᮩᮒ᮪||
-ᮮᮥᮯᮥ|state of grace||xušu خاشع||
+ᮮᮥᮯᮥ|state of grace|xu§ʃu|xušu|خاشع||
 ᮃᮊ᮪ᮞᮛ ᮞᮥᮔ᮪ᮓ|Sundanese alphabet|a§k§sa§ra§ §su§n§da
-ᮍᮕᮊ᮪|to flap|ŋa.§pak|ngapak
-ᮞᮛᮥᮀ|sarong|sa§rʊŋ|
+ᮍᮕᮊ᮪|to flap|ŋa.§pa§k|ngapak|||ngapak
+ᮞᮛᮥᮀ|sarong|sa§rʊŋ|sarung|||sarung
 
 
-
-
-
-
-@ https://en.wikipedia.org/wiki/Sundanese_language
-
-ᮘᮞ ᮞᮥᮔ᮪ᮓ|Sundanese language|ba§sa§ §sʊ§n§da|basa sunda
-
-
-
-
-
-
-
-
-
+ᮃᮊ᮪ᮞᮛ ᮍᮜᮌᮨᮔ|Sundanese script|a§k§sa§ra§ §ŋa§la§ɡə§na|aksara ngalagena|||aksara ngalagena
+ᮃᮜ᮪ᮙᮨᮔᮊ᮪|calendar|a§l.§mə§ˈna§k|almenak|||almenak
+ᮃᮙ᮪ᮒᮨᮔᮁ|civil servant|a§m§tə§ˈna§r|amtenar|||amtenar
+ᮃᮀᮌᮛ|Tuesday|aŋ§ɡa§ra|Anggara|||Anggara
+ᮃᮇᮞᮩᮔ᮪|reading material|a.§o§ˈsɨ§n|aoseun|||aoseun
+ᮃᮕᮥᮀ|to float|a.§puŋ|apung|||apung
+ᮃᮛᮩᮚ᮪|jungle rope|a.§rɨ§j|areuj|||areuj
+ᮃᮞᮨᮙ᮪|tamarind|a.§sə§m|asem|||asem
+ᮃᮞ᮪ᮙ|asthma|ˈa§s.§maʔ|asma|||asma
+ᮃᮒᮂ|raw, uncooked|a.§tah|atah|||atah
+ᮃᮒᮤᮊᮔ᮪ ᮜᮥᮠᮥᮁ|higher education|aˌ§ti.§ka§n§ §ˈlu.§hur|atikan luhur|||atikan luhur
+ᮃᮚᮩᮔ|now|a§ˈjɨ.§na|ayeuna|aˈjɤ.na||ayeuna
+ᮘᮌᮧᮍ᮪|wild boar|ba.§ɡo§ŋ|bagong|||bagong
+ᮘᮔ᮪ᮓᮦᮛ|flag|ba§n§ˈde§ra|bandéra|||bandéra
+ᮘᮀᮌ|rebellious|baŋ§ˈɡa|bangga|||bangga
+ᮘᮍᮧᮁ|naughty|ba§ˈŋɔr|bangor|||bangor
+ᮘᮑᮧ|water (rare dialectal)|ba§ˈɲɔ|banyo|||banyo
+ᮘᮛᮦᮒᮧ|previously|ba§ˈrɛ.§to|baréto|||baréto
+ᮘᮞ ᮞᮥᮔ᮪ᮓ|Sundanese language|ba§sa§ §sʊ§n§da|Basa Sunda|||Basa Sunda
+ᮘᮒᮤ|profit|ba.§ti|bati|||bati
+ᮘᮚᮝᮊ᮪|monitor lizard|ba.§ja§ˈwa§k|bayawak|bɐ.jaˈwak||bayawak
+ᮘᮨᮓᮧᮌ᮪|golok, machete|bə§ˈdɔ§ɡ|bedog|||bedog
+ᮘᮨᮀᮊᮥᮀ|bent|bəŋ.§kuŋ|bengkung|||bengkung
+ᮘᮩᮊᮤ|more|ˈbɤ.§ki|beuki|||beuki
+ᮘᮩᮍᮩᮒ᮪|face|bɨ§ˈŋɨ§t|beungeut|||beungeut
+ᮘᮩᮛᮀ|day|bɨ.§raŋ|beurang|||beurang
+ᮘᮩᮛᮒ᮪|heavy|bɤ.§ra§t|beurat|||beurat
+ᮘᮩᮛᮤᮒ᮪|mouse|bɨ§ˈri§t|beurit|bɤˈrit||beurit
+ᮘᮤᮊᮀ|female|bi§kaŋ|bikang|||bikang
+ᮘᮤᮔᮞ|destroyed|bi.§na.§sa|binasa|||binasa
+ᮘᮤᮔᮨᮊᮞ᮪|intelligent|bi§nə§ka§s|binekas|||binekas
+ᮘᮤᮔ᮪ᮒᮤᮂ|to strike|ˈbintih|bintih|||bintih
+ᮘᮤᮒᮤᮞ᮪|calf (leg)|bi§ˈti§s|bitis|||bitis
+ᮘᮧᮜ|ball|ˈbɔ.§la|bola|||bola
+ᮘᮥᮘᮥ|fish trap|bu§bu|bubu|||bubu
+ᮘᮥᮓᮥᮌ᮪|scabies|ˈbu.§du§ɡ|budug|||budug
+ᮘᮥᮛᮥᮀ|crazy, foolish|bu.§ruŋ|burung|||burung
+ᮘᮥᮒ|blind|ˈbu.§ta|buta|||buta
+ᮘᮦᮘᮦᮔᮦ|girlfriend|be§ˈbe§ne|bébéné|||bébéné
+ᮘᮦᮘᮦᮛᮦᮒᮦᮃᮔ᮪|mountain raspberry|bɛ§bɛ§rɛ§ˈtɛ§a§n|bébérétéan|||bébérétéan
+ᮘᮦᮔ᮪ᮒᮀ ᮎᮊᮢ|southern cross|bɛ§n§ˈtaŋ§ §ˈt͡ʃa§kra|Béntang Cakra|||Béntang Cakra
+ᮘᮦᮔ᮪ᮒᮀ ᮜᮍᮣᮚᮍᮔ᮪|crux constellation|bɛ§n.§taŋ§ §laŋ.§la.§ja§ˈŋa§n|Béntang Langlayangan|||Béntang Langlayangan
+ᮘᮦᮛᮦ|to give|bɛ§ˈrɛ|béré|||béré
+ᮎᮃᮀ|light, bright|t͡ʃa§ʔaŋ|caang|||caang
+ᮘᮒᮀ|log (tree)|t͡ʃa.§taŋ|catang|||catang
+ᮎᮥᮒᮒ᮪|to quote, cite|t͡ʃu§ˈta§t|cutat|||cutat
+ᮓᮓᮕ᮪|coral tree|da§da§p|dadap|||dadap
+ᮓᮜᮕᮔ᮪|eight|da.§la.§pa§n|dalapan|||dalapan
+ᮓᮜᮥᮃᮀ|mulberry paper|da§ˈlu.§aŋ|daluang|||daluang
+ᮓᮙ᮪ᮕᮜ᮪|sole of foot|da§m.§pa§l|dampal|||dampal
+ᮓᮀᮓᮀ|earthenware pot|daŋ§ˈdaŋ|dangdang|danˈdaŋ||dangdang
+ᮓᮩᮄ|again|dɤ§i|deui|||deui
+ᮓᮤᮓᮤᮉ|here|di.§di§ɨ̯|di dieu|||di dieu
+ᮓᮤ ᮓᮤᮑ|there|di§ §di§ɲa|di dinya|||di dinya
+ᮓᮧᮙᮞ᮪|eight hundred|dɔ§ma§s|domas|||domas
+ᮓᮥᮃ|two|du.§(w)a|dua|||dua
+ᮓᮥᮄᮒ᮪|money|du§ˈi§t|duit|duˈwit̪||duit
+ᮓᮥᮔᮡ|world|du§n.ˈja|dunya|duɲ.ˈja||dunya
+ᮈᮙ᮪ᮘᮦ|goat|ə§m§ˈbeʔ|embé|əˈmeʔ||embé
+ᮈᮙ᮪ᮕᮜ᮪|stew|ə§m.§ˈpa§l|empal|||empal
+ᮈᮀᮊᮦ|later|(ʔ)əŋ§ˈkɛ|engké|||engké
+ᮈᮔᮌᮊᮦ ᮠᮨᮅᮜᮃᮃ|wait a minute|ʔə§ŋˌ§‹§ke§ §ˈhɨ.§‹§la§‹§‹|engké heula|||engké heula
+ᮉᮉᮜᮩᮌᮩᮌᮔ᮪|swallow repeatedly to clear throat|ʔɨ.§ʔɨ.§lɨ.§ɡɨ.§ɡa§n|eueuleugeugan|||eueuleugeugan
+ᮉᮜᮩᮂ|Oh!|ɤ§ˈlɤh|euleuh|||euleuh
+ᮉᮛᮩᮔ᮪|to stop|ɨ§rɨ§n|eureun|||eureun
+ᮌᮜᮧ|mixed|ɡa§ˈlo|galo|||galo
+ᮌᮨᮜᮕ᮪|lightning, thunder|ɡə§ˈla§p̚|gelap|||gelap
+ᮌᮨᮔ᮪ᮎᮦᮒ᮪|to crush|ɡə§n.§t͡ʃɛ§t|gencét|||gencét
+ᮌᮤᮛᮀ|upstream|ɡi§ˈraŋ|girang|||girang
+ᮌᮧᮔ᮪ᮓᮧᮍᮩᮔ᮪|mumps|ɡo§n.§do§ˈŋɤ§n|gondongeun|||gondongeun
+ᮌᮧᮛᮦᮀ|bad, awful|ɡɔ.§rɛŋ|goréng|||goréng
+ᮌᮧᮞᮜᮤ|smithy|ɡɔ§sa§li|gosali|||gosali
+ᮠᮏᮤ|king|ha§d͡ʒi|haji|||haji
+ᮠᮙ᮪ᮕᮀ|light (weight)|ha§m§ˈpaŋ|hampang|||hampang
+ᮠᮙ᮪ᮕᮥᮛ|forgiveness|ha§m§ˈpu§ra|hampura|||hampura
+ᮠᮔᮞ᮪|remorse|ha.§nas|hanas|||hanas
+ᮠᮔ᮪ᮏᮊᮜ᮪|to regret|ha§n§ˈd͡ʒa.§ka§l|hanjakal|haˈɲa.kal||hanjakal
+ᮠᮛᮤᮝᮀ|worried|ha§ˈri.§waŋ|hariwang|||hariwang
+ᮠᮞᮩᮙ᮪|sour|ha.§sɨ§m|haseum|||haseum
+ᮠᮚᮙ᮪|chicken|ha§ja§m|hayam|||hayam
+ᮠᮚᮀ|to want|ha§jaŋ|hayang|||hayang
+ᮠᮩᮉᮂ|yeah|hɨ§ˈʔɨh|heyeuh|||heueuh
+ᮠᮨᮅᮜᮃᮃ|before, earlier|hɨ§‹§la§‹§‹|heula|||heula
+ᮠᮩᮜᮀ|eagle|ˈhɯ.§laŋ|heulang|||heulang
+ᮠᮤᮛᮤ|envious|hi.§ri|hiri|||hiri
+ᮠᮧᮚᮧᮀ|to want|hɔ§jɔŋ|hoyong|||hoyong
+ᮠᮧᮆ|rattan|ho.§ˈ(w)ɛ|hoé|||hoé
+ᮠᮥᮙ᮪ᮘᮥᮒ᮪|inner palm|hu§m.§bu§t|humbut|||humbut
+ᮠᮥᮔ᮪ᮒᮥ|tooth|hu§n§ˈtu|huntu|||huntu
+ᮠᮥᮒᮀ|debt|hu.§taŋ|hutang|||hutang
+ᮠᮡᮀ|god|ˈhjaŋ|hyang|||Hyang
+ᮠᮦᮌᮁ|open, broad|ˈhɛ.§ɡar|hégar|||hégar
+ᮄᮃᮒ᮪ᮔ|cautious, careful|i.§ˈ(j)a§t.§na|iatna|||iatna
+ᮄᮉ|this thing|i.§ɨ|ieu|i.ɤ||ieu
+ᮄᮔ᮪ᮓᮥᮀ|mother|ʔi§n§duŋ|indung|ʔinuŋ||indung
+ᮊᮓᮥᮠᮥᮀ|regret, remorse|ka.§du.§ˈhuŋ|kaduhung|||kaduhung
+ᮊᮓᮦ|to be careful|ˈka.§dɛ|kadé|||kadé
+ᮊᮜᮦᮎᮤ|a marble|ka§ˈlɛ§t͡ʃi|kaléci|||kaléci
+ᮊᮙᮜᮤᮁ|ditch|ka.§ma.§lir|kamalir|||kamalir
+ᮊᮛᮩᮉᮒ᮪|sickly sweet|ka.§rɤ.§ʔɤ§t|kareueut|||kareueut
+ᮊᮛᮥᮠᮥᮔ᮪|ancestors|ka§ˈru§hu§n|karuhun|||karuhun
+ᮊᮛᮥᮑ|pitiful|ka§ˈru.§ɲa|karunya|||karunya
+ᮊᮛᮦᮆᮞ᮪|sandy riverbank|ka.§re.§ɛ§s|karéés|||karéés
+ᮊᮒᮥᮠᮥ|right|ka§tu§ˈhu|katuhu|||katuhu
+ᮊᮝᮞ᮪|like, similar to|ka.§wa§s|kawas|||kawas
+ᮊᮝᮚ᮪|shirt|ka§ˈwa§j|kaway|||kaway
+ᮊᮚᮞ᮪|pink|ka§ˈja§s|kayas|||kayas
+ᮊᮨᮔ᮪ᮒᮦᮀ|roof tile|kə§n§ˈtɛŋ|kenténg|||kenténg
+ᮊᮩᮉᮙ᮪|to stand in water|kɤ.§ɤ§m|keueum|||keueum
+ᮊᮩᮉᮀ|lonely, abandoned|ˈkɤ.§ʔɤŋ|keueung|||keueung
+ᮊᮩᮠᮩᮜ᮪|annoyed|kɤ.§hɤ§l|keuheul|||keuheul
+ᮊᮩᮛᮩᮒ᮪|to slice|kɨ.§rɨ§t|keureut|||keureut
+ᮊᮤᮃᮛ|banyan tree|ki.§(j)a.§ra|kiara|||kiara
+ᮊᮤᮏᮥ|cheese|ki.§d͡ʒu|kiju|||kiju
+ᮊᮤᮕ᮪ᮞᮇ|earthen teakettle|ki§p§ˈsa§o|kipsao|kɪpˈsao||kipsao
+ᮊᮤᮛᮚ᮪|sago palm|kiˈraj|kiray|kiˈɾaɪ̯||kiray
+ᮊᮤᮝᮛᮤ|now, today|ki.§wa.§ri|kiwari|||kiwari
+ᮊᮧᮘᮧᮀ|ship cabin|ˈko.§bɔŋ|kobong|||kobong
+ᮊᮧᮊᮧᮛᮧ|poor|ko§ko§ro|kokoro|||kokoro
+ᮊᮧᮜᮦᮎᮦᮁ|bamboo windmill|ko§ˈle.§t͡ʃer|kolécér|||kolécér
+ᮊᮧᮜᮦᮞᮧᮙ᮪|ginseng|ko.§le.§so§m|kolésom|||kolésom
+ᮊᮥᮆᮂ|cake, cookie|ku§ˈeh|kuéh|||kuéh
+ᮊᮦᮏᮧ|rice|kɛ.§d͡ʒo|kéjo|||kéjo
+ᮊᮦᮜᮨᮁ|jar|ˈkɛ§lər|kéler|||kéler
+ᮊᮦᮜᮦᮊ᮪|armpit|kɛ.§lɛ§k|kélék|||kélék
+ᮊᮦᮔ᮪ᮎ|left|ˈkɛ§n§t͡ʃa|kénca|||kénca
+ᮜᮓ|pepper|la§ˈda|lada|||lada
+ᮜᮙ᮪ᮘᮥᮞᮤᮁ|loin|la§m.§bu§sir|lambusir|||lambusir
+ᮜᮨᮘᮊ᮪|valley|lə§ba§k|lebak|||lebak
+ᮜᮨᮘᮦ|mosque official|lə§ˈbɛ|lebé|||lebé
+ᮜᮨᮅᮝᮤᮠ᮪|more|lɨ§wi§h|leuwih|||leuwih
+ᮜᮤᮃᮔ᮪|different|li§a§n|lian|||lian
+ᮜᮤᮞ|louse egg|li.§sa|lisa|||lisa
+ᮜᮧᮊ|world|lo.§ka|loka|||loka
+ᮜᮧᮊᮦᮒ᮪|wallet|lo.§kɛ§t|lokét|||lokét
+ᮙᮠᮤ|enough|ma§hi|mahi|||mahi
+ᮙᮨᮔ᮪ᮎᮨᮊ᮪|muntjac deer|ˈmə§n§t͡ʃə§k|mencek|||mencek
+ᮙᮨᮍᮤ|dyspnea|mə§ˈŋiʔ|mengi|||mengi
+ᮙᮨᮔᮤᮒ᮪|minute|mə§ˈni§t|menit|||menit
+ᮙᮧᮠ|ingratitude|ˈmo.§ha|moha|||moha
+ᮙᮧᮊᮣ|blood|ˈmɔ§k.laʔ|mokla|||mokla
+ᮙᮧᮞᮤ|motion, proposal|ˈmo.§si|mosi|||mosi
+ᮙᮥᮒᮦ|bead|muˈte|muté|||muté
+ᮔᮨᮓ|to eat|nə§da|neda|||neda
+ᮍᮧᮏᮚ᮪|to swim|ŋɔ§ˈd͡ʒa§j|ngojay|||ngojay
+ᮑᮤᮛᮥ|winnowing basket|ɲi§ru|nyiru|||nyiru
+ᮕᮎᮧᮊ᮪|to peck|pa.§t͡ʃɔ§k|pacok|||pacok
+ᮕᮌᮩᮂ|firm, steady|pa.§ɡɨh|pageuh|||pageuh
+ᮕᮙᮜᮤ|taboo|pa§ma§li|pamali|||pamali
+ᮕᮔ᮪ᮓᮦᮌᮣᮀ|Pandeglang (Indonesia)|pa§n.§ˈdɛ.§ɡlaŋ|Pandéglang|||Pandéglang
+ᮕᮀᮊᮦᮀ|room|paŋ§ˈkeŋ|pangkéng|||pangkéng
+ᮕᮔ᮪ᮒᮧ|door|ˈpa§n.§to|panto|||panto
+ᮕᮕᮒᮧᮀ|dragonfly|pa§pa§ˈtɔŋ|papatong|||papatong
+ᮕᮛᮨᮊ᮪|close, near|ˈpa§rə§k|parek|||parek
+ᮕᮛᮨᮊᮔ᮪|concubine|pa§ˈrə§ka§n|parekan|||parekan
+ᮕᮛᮥᮓ᮪|grater|pa§ˈru§d|parud|||parud
+ᮕᮞᮛᮔ᮪|bier|pa§ˈsa§ra§n|pasaran|||pasaran
+ᮕᮨᮜᮨᮀᮊᮥᮀ|arch|pə.§ləŋ§ˈkʊŋ|pelengkung|||pelengkung
+ᮕᮨᮁᮏᮥᮛᮤᮒ᮪|army|pər.§ˈd͡ʒu.§ri§t|perjurit|||perjurit
+ᮕᮩᮛᮩᮂ|eyedrops|pɤ§rɤh|peureuh|||peureuh
+ᮕᮩᮒᮩᮚ᮪|bitter bean|pɯ§ˈtɯ§j|peuteuy|||peuteuy
+ᮕᮤᮃᮔᮧ|piano|pi§ˈ(j)a.§no|piano|||piano
+ᮕᮧᮆ|day|po§ɛ|poé|||poé
+ᮕᮧᮆᮊ᮪|dark|pɔ.§ɛ§k|poék|||poék
+ᮕ᮪ᮛᮧᮖᮦᮞᮧᮛ᮪|professor|p§ro§ˈfɛ.§sɔ§r|profesor|||profesor
+ᮕᮥᮔᮂ|settled (debt)|ˈpu§nah|punah|||punah
+ᮕᮦᮛᮊ᮪|silver|pɛ.§ra§k|pérak|||pérak
+ᮛᮌᮏᮤ|saw|ra.§ɡa§ˈd͡ʒi|ragaji|||ragaji
+ᮛᮏᮦᮊᮔ᮪|reduplication|ra§ˈd͡ʒe.§ka§n|rajékan|||rajékan
+ᮛᮙ᮪ᮘᮨᮒᮥᮊ᮪|fruit fly|ra§m§ˈbə.§tu§k|rambetuk|raˈmə.tʊk||rambetuk
+ᮛᮔ᮪ᮎ|swamp|ra§n§ˈt͡ʃa|ranca|||ranca
+ᮛᮛᮚ᮪|face|ˈra§ra§j|raray|||raray
+ᮛᮩᮔᮩᮂ|pregnant|rɨ.§nɨh|reuneuh|||reuneuh
+ᮛᮩᮍᮤᮒ᮪|mosquito|ˈrɤ§ŋi§t|reungit|||reungit
+ᮞᮜᮝᮦ|twenty-five|sa§ˈla§we|salawé|||salawé
+ᮞᮜᮨᮙ᮪ᮕᮚ᮪|handkerchief|sa§lə§m§ˈpa§j|salempay|||salempay
+ᮞᮔ᮪ᮓᮦᮊᮜ|evening, dusk|sa§n§dɛ§ka§la|sandékala|||sandékala
+ᮞᮞᮛᮤ|daily|sa§ˈsa§ri|sasari|||sasari
+ᮞᮝᮨᮓ᮪|strap|ˈsa§wə§d|sawed|||sawed
+ᮞᮩᮛᮤ|to laugh|ˈsɨ.§ri|seuri|||seuri
+ᮞᮤᮉᮔ᮪|afraid|si.§(j)ɤ§n|sieun|||sieun
+ᮞᮤᮀᮊᮥᮁ|well hidden|ˈsiŋ.§kur|singkur|||singkur
+ᮞᮤᮕᮒ᮪|characteristic|si§ˈpa§t|sipat|||sipat
+ᮞᮧᮞᮤ|key|so§si|sosi|||sosi
+ᮞᮢᮍᮦᮍᮦ|sun|sra.§ŋɛ.§ŋɛ|srangéngé|||srangéngé
+ᮞᮥᮏᮥᮓ᮪|prostration|ˈsu§d͡ʒʊ§t̚|sujud|||sujud
+ᮞᮥᮆ|unlucky|su§ˈwe|sué|suˈwɛʔ||sué
+ᮞᮦᮛᮀ|Serang (Indonesia)|ˈsɛ.§raŋ|Sérang|||Sérang
+ᮒᮏᮥᮌ᮪|prayer room|ta§ˈd͡ʒʊ§ɡ|tajug|||tajug
+ᮒᮕ᮪ᮜᮊ᮪|tablecloth|ta.§pla§k|taplak|||taplak
+ᮒᮨᮍᮍᮦ|midday|tə§ŋa§ˈŋɛ|tengangé|||tengangé
+ᮒᮩᮄᮀ|very|tɤ.§iŋ|teuing|||teuing
+ᮒᮤᮙ᮪ᮘᮥᮛᮥ|apprehensive|ti§m.§bu.§ru|timburu|||timburu
+ᮒᮤᮝᮥ|sugar cane|ti§ˈwu|tiwu|||tiwu
+ᮒᮥᮜᮥᮚ᮪|to continue|tu§ˈlu§j|tuluy|t̪uˈlʊɪ̯||tuluy
+ᮒᮥᮍᮤ|introverted|tu§ˈŋi|tungi|||tungi
+ᮒᮥᮅᮁ|knee|ˈtu.§ʔur|tuur|||tuur
+ᮅᮎᮤᮀ|cat|u.§t͡ʃiŋ|ucing|||ucing
+ᮅᮓᮥᮓ᮪|cigarette|u.§du§d|udud|||udud
+ᮅᮓᮥᮂ|soft, tender|u.§duh|uduh|||uduh
+ᮅᮀᮌᮂ|to go up|ˈuŋ.§ɡah|unggah|||unggah
+ᮝᮃᮞ᮪|nostalgic|wa§ˈa§s|waas|waˈʔas||waas
+ᮝᮜᮀ|grasshopper|wa.§laŋ|walang|||walang
+ᮝᮜᮤᮊᮒ᮪|shoulder blade|ba§ˈli.§ka§t̪|walikat|||walikat
+ᮝᮔᮤ|brave|wa.§ni|wani|||wani
+ᮝᮛᮨᮌ᮪|full, sated|ˈwa.rəɡ|wareg|||wareg
+ᮝᮨᮛ|angry|ˈwə§ra|wera|||wera
+ᮝᮤᮝᮠ|reason, consideration|wi.§wa.§ha|wiwaha|||wiwaha
+ᮚᮥᮞ᮪ᮝ|age|ju§s.§wa|yuswa|||yuswa
+ᮆᮔᮦᮀ|water buffalo calf|ˈe.§neŋ|énéng|||énéng
 
 
 
