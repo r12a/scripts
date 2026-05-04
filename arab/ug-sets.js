e2d0d3ec4262bf453f37e704de70433a7e24fa0d
r12a <ishida@w3.org>
2026-05-04 12:45:53 +0100

    Add voiceless consonant set

--- arab/ug-sets.js
+++ arab/ug-sets.js
@@ -8,5 +8,6 @@
 var collections = [
 { symbol:'Ⓥ', desc:'Independent vowels', chars:'[ىۈۇېوەۆا]' },
 { symbol:'Ⓒ', desc:'All consonants', chars:'[پبتدكگقچجفسزژشخغھرلمنڭۋيئ]' },
+{ symbol:'🅲', desc:'Voiceless consonants', chars:'[پتكقفسشخھ]' },
 { symbol:'⊙', desc:'Invisibles', chars:'[\u200C\u200D\u2067\u202B\u2066\u202A\u2068\u2069\u202C\u200F\u200E\u061C\u034F]' },
 ]
