b59222d0771b41e8eec472a301fd2926d5addde7
r12a <ishida@w3.org>
2026-05-20 07:49:12 +0100

    lis   Add tone letters to set

--- lisu/lis-sets.js
+++ lisu/lis-sets.js
@@ -10,4 +10,5 @@ var collections = [
 { symbol:'Ⓒ', desc:'All consonants', chars:'[ꓑꓐꓒꓔꓓꓕꓗꓖꓘꓝꓜꓞꓚꓙꓛꓩꓢꓤꓣꓫꓬꓧꓭꓦꓨꓟꓠꓥꓪꓡ]' },														
 { symbol:'Ⓣ', desc:'Tones', chars:'[ꓸꓹꓺꓻꓼꓽ]' },														
 { symbol:'⊙', desc:'Invisibles', chars:'[\u200C\u200D]' },														
+{ symbol:'🅣', desc:'IPA tone letters', chars:'[˥˦˧˨˩]' },														
 ]														
