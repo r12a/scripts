110043f2b83c5d682b71883a34ecf98db34392fb
r12a <ishida@w3.org>
2026-05-20 07:07:28 +0100

    lis   Convert diacritics to tone letters.  Add apostrophe.

--- lisu/lis-examples.js
+++ lisu/lis-examples.js
@@ -24,88 +24,88 @@ autoExpandExamples.lis = `
 
 @ wiktionary
 
-ꓑꓺ ꓦꓯꓹ|bamboo rat|pa,˦˦.,h,ɛ̃,˧˥|pɑ̱ h̃æ̌|pā hæ̌̃|/../
-ꓔꓲꓹ|to dye|t,ǐ,‹
-ꓞꓽ ꓐꓳꓺ|salt|tsʰà,‹ ,b,o,‹
-ꓠꓴꓻ|you|n,u,‹
+ꓑꓺ ꓦꓯꓹ|bamboo rat|pa,˦˦.,,h,ɛ̃,˧˥|pā hæ̌̃|pɑ̱ h̃æ̌|/../
+ꓔꓲꓹ|to dye, to immerse in water|t,i,˦˥ˀ|tǐ|
+ꓞꓽ ꓐꓳꓺ|salt|tsʰà,˩, ,b,o,˧|tsʰɑ̀ bo̱|tsʰà bo
+ꓠꓴꓻ|you|n,u,˨˨|nu|
 ꓚꓳ-ꓗꓴꓷꓼ|China|t͡ɕ,o˧˧.,,k,u,ə,˧˩|tʃo-kù̱ɤ
-ꓐꓵꓺ|to owe|by̠³³|by̱
-ꓒꓵꓽ|pumpkin|pʰ,y˧˩|pʰỳ
-ꓡꓲ-ꓢꓴ|Lisu language|l,i,,s,u|li̱-su̱
-ꓡꓲꓺ-ꓢꓴꓺ|Lisu language|l,i,,s,u|li̱-su̱
-ꓥꓪ|I, me|ŋ,wɑ|ŋwɑ
-ꓥꓳꓽ|language|ŋ,o˧˩|ŋò
-ꓦ ꓐꓻ|moon|hã˦˦.,ba˧˧|h̃ɑ bɑ
-ꓬꓲꓻ|water|ʑ,i˧˧|ji
+ꓐꓵꓺ|to owe|b,y,˧˧|by̱|by̠³³|/../
+ꓒꓵꓽ|pumpkin|pʰ,y,˧˩|pʰỳ
+ꓡꓲ-ꓢꓴ|Lisu language|l,i˧˧.,,s,u˧˧|li̱-su̱||[..]
+ꓡꓲꓺ-ꓢꓴꓺ|Lisu language|l,i,˧˧,,s,u,˧˧|li̱-su̱
+ꓥꓪ|I, me|ŋ,wɑ˧|ŋwɑ||[..]
+ꓥꓳꓽ|language|ŋ,o,˧˩|ŋò
+ꓦ ꓐꓻ|moon|hã˧.,,ba,˨|hɑ̃ bɑ̰||/../
+ꓬꓲꓻ|water|ʑ,i,˨|ji||/../
 
 
 
 
 @ Handbook of the Lisu language – Lisu text is generated from transcription; IPA is generated from Lisu text
 
-ꓡꓳꓺ ꓗꓶꓺ|abandon|l,ō,‹, ,k,ɯ̄,‹|law3-krgh3
-ꓬꓹ|to abase|jɑ̌,‹|ya2||Chinese
-ꓠꓲꓸ ꓡꓰꓺ ꓝꓲꓺ|to abate|n,í,‹, ,l,ē,‹, ,t͡s,ī,‹|ni1-lye3 tsi3
-ꓠꓲꓹ ꓟꓺ|abdomen|n,ǐ,‹, ,mɑ̄,‹|ni2-ma3
-ꓠꓬꓲꓹ ꓔꓸ|to dwell|ɲ,‹,ǐ,‹, ,tɑ́,‹|nyi2-ta1
-ꓨʼꓲꓻ|house|ɦ,›,ḭ̃,‹||h'i4
-ꓒꓰꓽ ꓷꓻ|to appear|pʰ,e,˩, ,ə,˨˨|hpye5-rghe4|pʰè ə̰
-ꓐꓴꓽ ꓡꓴꓻ|butterfly|b,ù,‹, ,l,ṵ,‹|bu5 lu4
-ꓑꓶꓸ|to dare|p,ɯ́,‹|prgh1
-ꓑꓽ ꓡꓰꓹ|scythe|pa,˩, ,l,e,˦˥ˀ|pa5 lye2|pà lě||x
-ꓙꓲꓽ|to sew|d͡ʑ,ì,‹||ji6
-ꓣꓼ ꓟꓶʼꓻ ꓨꓪ|to marry|ʒa,˧˩ˀ, ,m,ɯ̃,‹,˨˨, ,ɦ,wa˧˧|ra5 mrghʼ4 hwa3|ʒa᷆ mɯ̰̃ ɦwa
-ꓩꓴꓹ ꓣꓼ|idol|f,ǔ,‹, ,ʒa᷆,‹||fu2 ra5
+ꓡꓳꓺ ꓗꓶꓺ|abandon|l,o,˧, ,k,ɯ,˧|lō kɯ̄|law3-krgh3
+ꓬꓹ|to abase|jɑ̌,˦˥ˀ|yɑ̌|ya2|Chinese
+ꓠꓲꓸ ꓡꓰꓺ ꓝꓲꓺ|to abate|n,i,˥˥, ,l,e,˧˧, ,t͡s,i,˧˧|ní lē t͡sī|ni1-lye3 tsi3
+ꓠꓲꓹ ꓟꓺ|abdomen|n,i,˦˥ˀ, ,mɑ,˧˧|nǐ mɑ̄|ni2-ma3
+ꓠꓬꓲꓹ ꓔꓸ|to dwell|ɲ,‹,ǐ,˦˥ˀ, ,tɑ́,˥˥|ɲǐ tɑ́|nyi2-ta1
+ꓨʼꓲꓻ|house|ɦ,›,ĩ,˨|ɦḭ̃|h'i4
+ꓒꓰꓽ ꓷꓻ|to appear|pʰ,e,˩, ,ə,˨˨|pʰè ə̰|hpye5-rghe4
+ꓐꓴꓽ ꓡꓴꓻ|butterfly|b,u,˩, ,l,u,˨˨|bù lṵ|bu5 lu4
+ꓑꓶꓸ|to dare|p,ɯ,˥˥|pɯ́|prgh1
+ꓑꓽ ꓡꓰꓹ|scythe|pa,˩, ,l,e,˦˥ˀ|pà lě|pa5 lye2||x
+ꓙꓲꓽ|to sew|d͡ʑ,i,˩|d͡ʑì|ji6
+ꓣꓼ ꓟꓶʼꓻ ꓨꓪ|to marry|ʒa,˧˩ˀ, ,m,ɯ̃,‹,˨˨, ,ɦ,wa˧˧|ʒa᷆ mɯ̰̃ ɦwa|ra5 mrghʼ4 hwa3
+ꓩꓴꓹ ꓣꓼ|idol|f,u,˦˥ˀ, ,ʒa,˧˩ˀ|fǔ ʒa᷆|fu2 ra5
 ꓵ|to count|ʔỹ||üʼ3
 
 
 
 @ video
 
-ꓠꓸ ꓑꓳ|ear||ná,‹, ,p,ō|na1 paw3
-ꓕꓲ|one|tʰ,i||hti5
-ꓠꓬꓲ|two|ɲ,‹,i||nyï5
+ꓠꓸ ꓑꓳ|ear|na,˥˥, ,p,o˧|ná pō|na1 paw3
+ꓕꓲ|one|tʰ,i|hti5
+ꓠꓬꓲ|two|ɲ,‹,i˩||nyï5
 ꓢ|three|sa||sa3
-ꓡꓲꓸ|four|l,í,‹||li3
-ꓥꓪꓽ|five|ŋ,wà,‹||ngwa5
-ꓚꓳ|six|t͡ɕ,o||hchaw6
-ꓫꓵꓼ|seven|ʃ,y᷆,‹||shï5
-ꓦꓲꓼ|eight|h,i᷆,‹||hʼi6
-ꓗꓴꓸ|nine|k,ú,‹||ku1
+ꓡꓲꓸ|four|l,i,˥||li3
+ꓥꓪꓽ|five|ŋ,wa,˩|ŋwà|ngwa5
+ꓚꓳ|six|t͡ɕ,o|t͡ɕo|hchaw6
+ꓫꓵꓼ|seven|ʃ,y,˧˩ˀ|ʃy᷆|shï5
+ꓦꓲꓼ|eight|h,i,˧˩ˀ|hi᷆|hʼi6
+ꓗꓴꓸ|nine|k,u,˥˥|ku1|kú
 ꓞꓲ|ten|ʦʰ,i||htsi4
-ꓡꓯꓽ ꓒꓶ|shoulder|l,ɛ̀,‹, ,pʰ,ɯ||lá6 hprgh5
-ꓡꓯꓽ ꓒꓯꓹ|hand|l,ɛ̀,‹, ,pʰ,ɛ̌,‹||lá2 hpá2
-ꓪꓴꓸ ꓓꓴ|head|w,ú,‹, ,d,u||wu1 dü3
+ꓡꓯꓽ ꓒꓶ|shoulder|l,ɛ,˩, ,pʰ,ɯ|lɛ̀ pʰɯ|lá6 hprgh5
+ꓡꓯꓽ ꓒꓯꓹ|hand|l,ɛ̀,˩, ,pʰ,ɛ̌,˦˥ˀ|lɛ̀ pʰɛ̌|lá2 hpá2
+ꓪꓴꓸ ꓓꓴ|head|w,u,˥, ,d,u|wú du|wu1 dü3
 ꓡ ꓛꓱ|tongue|la, ,t͡ɕʰ,ø||la3 hchē3
-ꓗꓶꓸ ꓝꓲꓸ|neck|k,ɯ,˥˥, ,t͡s,i,˥˥|krgh1 tsē3|kɯ́ t͡sí
-ꓠꓲꓹ M|heart|n,ǐ,‹, ,ma||ni2 ma3
-ꓟꓶꓽ ꓡꓶꓹ|lips|m,ɯ̀,‹, ,l,ɯ̌,‹||mrgh5 lrge2
-ꓦꓲꓼ ꓟ|belly|h,i᷆,‹, ,ma||hʼi6 hchi6
+ꓗꓶꓸ ꓝꓲꓸ|neck|k,ɯ,˥˥, ,t͡s,i,˥˥|kɯ́ t͡sí|krgh1 tsē3
+ꓠꓲꓹ M|heart|n,i,˦˥ˀ, ,ma|nǐ ma|ni2 ma3
+ꓟꓶꓽ ꓡꓶꓹ|lips|m,ɯ,˩, ,l,ɯ,˦˥ˀ|mɯ̀ lɯ̌|mrgh5 lrge2
+ꓦꓲꓼ ꓟ|belly|h,i,˧˩ˀ, ,ma|hi᷆ ma|hʼi6 hchi6
 ꓐꓵꓼ ꓟꓴ|lung|b,y,˧˩ˀ, ,m,u˧˧||by᷆ mu|
-ꓮꓸ ꓬꓲꓼ|elder brother|ʔɑ́,‹, ,j,i᷆,‹||a1 yi6
-ꓠꓬꓲ ꓤꓽ|younger brother|ɲ,‹,i, ,za,˩|nyi3 ra5|ɲi zà
-ꓢꓲꓽ|blood|s,ì,‹||si5
-ꓤꓽ ꓟꓶꓼ|daughter|zà,‹, ,m,ɯ᷆,‹||á1 mi5
-ꓢꓲꓹ ꓜꓲ|tree|s,ǐ,‹, ,d͡z,i||si2 zhi3
-ꓕꓳꓽ ꓜꓲ|pine tree|tʰ,ò,‹, ,d͡z,i
-ꓟꓺ ꓓ|bamboo|m,ā,‹, ,da||ma4 da4
-ꓘꓪꓽ ꓢꓱ|garlic|kʰ,wa,˩, ,s,ø|khwa5 sye4|kʰwà sø
-ꓢ ꓡꓽ|cotton|sa, ,là,‹||sa3 la5
-ꓢꓲꓹ ꓢꓶꓽ|fruit|s,ǐ,‹, ,s,ɯ̀,‹||sï2 sï5
+ꓮꓸ ꓬꓲꓼ|elder brother|ʔɑ,˥, ,j,i,˧˩ˀ|ʔɑ́ ji᷆|a1 yi6
+ꓠꓬꓲ ꓤꓽ|younger brother|ɲ,‹,i, ,za,˩|ɲi zà|nyi3 ra5
+ꓢꓲꓽ|blood|s,i,˩|sì|si5
+ꓤꓽ ꓟꓶꓼ|daughter|za,˩, ,m,ɯ,˧˩ˀ|zà mɯ᷆|á1 mi5
+ꓢꓲꓹ ꓜꓲ|tree|s,i,˦˥ˀ, ,d͡z,i˧˧|s,ǐ,‹, ,d͡z,i|si2 zhi3
+ꓕꓳꓽ ꓜꓲ|pine tree|tʰ,o,˩, ,d͡z,i||tʰò d͡zi
+ꓟꓺ ꓓ|bamboo|m,a,˧, ,da|mā da|ma4 da4
+ꓘꓪꓽ ꓢꓱ|garlic|kʰ,wa,˩, ,s,ø|kʰwà sø|khwa5 sye4
+ꓢ ꓡꓽ|cotton|sa, ,la,˩|sa là|sa3 la5
+ꓢꓲꓹ ꓢꓶꓽ|fruit|s,i,˦˥ˀ, ,s,ɯ,˩|sǐ sɯ̀|sï2 sï5
 ꓮꓸ ꓭꓸ|chicken|ʔɑ̃,˥˥, ,ɣa,˥˥||ʔɑ́̃ ɣá
-ꓯꓹ|duck|ʔɛ̌̃,‹||áʼ2
-ꓳ|goose|ʔõ||awʼ3
-ꓕꓳꓼ ꓡ|rabbit|tʰ,o,‹,᷆ ,la||htaw5 la4
-ꓥꓪꓸ|fish|ŋ,wa,˥˥|ngwa1|ŋwá|
-ꓪꓴꓸ ꓖꓳꓽ ꓡꓳ|pillow|w,ú,‹, ,ɡ,ò,‹, ,l,o||wu1 gaw6 law3
+ꓯꓹ|duck|ʔɛ̃,˦˥ˀ|ʔɛ̌̃|áʼ2
+ꓳ|goose|ʔõ|ʔõ|awʼ3
+ꓕꓳꓼ ꓡ|rabbit|tʰ,o,˧˩ˀ, ,la|tʰo᷆ la|htaw5 la4
+ꓥꓪꓸ|fish|ŋ,wa,˥˥|ŋwá|ngwa1|
+ꓪꓴꓸ ꓖꓳꓽ ꓡꓳ|pillow|w,u,˥, ,ɡ,o,˩, ,l,o˧|wú ɡò lo|wu1 gaw6 law3
 
 
 
 
 @ Unicode Lisu proposal http://std.dkuug.dk/jtc1/sc2/wg2/docs/n3317.pdf
 
-ꓠꓴ ꓙꓰꓻˍ ꓥꓳꓻ|you will go|n,u˧˧, ,d͡ʑ,e,˨˨,a˩, ,ŋ,o,˨˨||nu d͡ʑḛà ŋo̰
-ꓖꓳꓻ ꓡꓱꓻ ꓮˍ ꓟꓲ|but|ɡ,o,˨˨, ,l,ø,˨˨, ,ŋɑ˧˩ˀ,a˩, ,m,i˧˧||ɡo̰ lø̰ ŋɑ᷆à mi
+ꓠꓴ ꓙꓰꓻˍ ꓥꓳꓻ|you will go|n,u˧˧, ,d͡ʑ,e,˨˨,a˩, ,ŋ,o,˨˨|nu d͡ʑḛà ŋo̰
+ꓖꓳꓻ ꓡꓱꓻ ꓮˍ ꓟꓲ|but|ɡ,o,˨˨, ,l,ø,˨˨, ,ŋɑ˧˩ˀ,a˩, ,m,i˧˧|ɡo̰ lø̰ ŋɑ᷆à mi
 
 
 @ https://live.bible.is/bible/LISTCM/MAT/17?audio_type=audio  Pronunciation manually added
@@ -115,7 +115,7 @@ autoExpandExamples.lis = `
 
 @ https://en.wikipedia.org/wiki/Fraser_script
 
-ꓡꓬꓮꓳ ꓩꓷꓠ|jelly|l,i̯,ɛ,w˧˧, ,f,ə,n˧˧||凉粉 li̯ɛw fən
+ꓡꓬꓮꓳ ꓩꓷꓠ|jelly|l,i̯,ɛ,w˧˧, ,f,ə,n˧˧|li̯ɛw fən|凉粉
 
 
 
